9bd2323ae480df033111d52d0e90161973d58a09
dcreager <dcreager@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-01-27 17:41:11 +0000

    We need this after all
    git-svn-id: file:///home/svn/shoola-omero/trunk@23 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/env/data/DataManagementService.java
@@ -0,0 +1,18 @@
+package org.openmicroscopy.shoola.env.data;
+
+import org.openmicroscopy.shoola.env.data.dto.*;
+
+/** 
+ *
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ *              <a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ *              <a href="mailto:a.falconi@dundee.ac.uk">a.falconi@dundee.ac.uk</a>
+ */
+
+public interface DataManagementService {
+    
+    public Project retrieveProject(int id);
+    
+}
