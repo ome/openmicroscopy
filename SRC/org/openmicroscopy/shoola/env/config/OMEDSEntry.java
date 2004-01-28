89994a9afdb9fffdecea832d314e7454af2b1e66
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-01-28 14:56:59 +0000

    added OME-header
    git-svn-id: file:///home/svn/shoola-omero/trunk@51 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/config/OMEDSEntry.java
+++ SRC/org/openmicroscopy/shoola/env/config/OMEDSEntry.java
@@ -1,3 +1,41 @@
+/*
+ * org.openmicroscopy.shoola.env.config.OMEDSEntry
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+/*------------------------------------------------------------------------------
+ *
+* Written by:     Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ *                      <a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ *                      Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ *                      <a href="mailto:a.falconi@dundee.ac.uk">a.falconi@dundee.ac.uk</a>
+ *
+ *------------------------------------------------------------------------------
+ */
 package org.openmicroscopy.shoola.env.config;
 
 // Java imports 
