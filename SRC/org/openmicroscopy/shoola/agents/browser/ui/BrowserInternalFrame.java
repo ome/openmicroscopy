117fb421a31ac43a14c87cd5a61d5a5b5249368b
jeffm <jeffm@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-02-01 06:28:38 +0000

    adding UI folder for Swing UI widgets... all this is tentative but will start to freeze over the next week as the implementations start getting nailed down
    git-svn-id: file:///home/svn/shoola-omero/trunk@59 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/browser/ui/BrowserInternalFrame.java
@@ -0,0 +1,73 @@
+/*
+ * org.openmicroscopy.shoola.agents.browser.ui.BrowserInternalFrame
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Jeff Mellen <jeffm@alum.mit.edu>
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.agents.browser.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JInternalFrame;
+
+import org.openmicroscopy.shoola.agents.browser.BrowserView;
+
+/**
+ * Wraps a BrowserView in a JInternalFrame for use in MDI applications.
+ * 
+ * @author Jeff Mellen, <a href="mailto:jeffm@alum.mit.edu">jeffm@alum.mit.edu</a>
+ * <b>Internal version:</b> $Revision$ $Date$
+ * @version 2.2
+ * @since OME2.2
+ */
+public class BrowserInternalFrame extends JInternalFrame
+{
+    private IntraDragAdapter dragAdapter;
+    private BrowserView embeddedView;
+    
+    public BrowserInternalFrame(BrowserView view)
+    {
+        setSize(600,600);
+        setTitle("Image Browser--"); // TODO: get name of dataset
+        setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
+        
+        if(view != null)
+        {
+            this.embeddedView = view;
+        }
+        
+        Container container = getContentPane();
+        container.setLayout(new BorderLayout());
+        container.add(view);
+    }
+}
