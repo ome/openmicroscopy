8d594a5e9c66b240b236b7328aadae1bbdacc937
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-02-13 12:44:48 +0000

    handle the type "icons" instead of icon
    git-svn-id: file:///home/svn/shoola-omero/trunk@203 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/config/Entry.java
+++ SRC/org/openmicroscopy/shoola/env/config/Entry.java
@@ -78,7 +78,7 @@ abstract class Entry
         contentHandlers.put("OMEIS", OMEISEntry.class);
         contentHandlers.put("OMEDS", OMEDSEntry.class);
         contentHandlers.put("font", FontEntry.class);
-        contentHandlers.put("icon", IconEntry.class);
+        contentHandlers.put("icons", IconFactoryEntry.class);
         contentHandlers.put("agents", AgentsEntry.class);
     }
     
