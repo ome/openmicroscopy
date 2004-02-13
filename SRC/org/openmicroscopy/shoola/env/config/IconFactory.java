b67976d0004a12bb9e0a0cb23a8234d11c73807f
afalconi <afalconi@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-02-13 16:19:52 +0000

    Added javadoc.
    git-svn-id: file:///home/svn/shoola-omero/trunk@210 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/config/IconFactory.java
+++ SRC/org/openmicroscopy/shoola/env/config/IconFactory.java
@@ -39,7 +39,28 @@ import javax.swing.ImageIcon;
 //Application-internal dependencies
 
 /** 
- * 
+ * Factory to build {@link Icon}s from image files within a given directory.
+ * An entry of type <i>"icons"</i> in a configuration file will be turned into
+ * an instance of this class.  The content of the <i>location</i> tag is used
+ * for locating the directory where the image files are.  This directory can
+ * be anywhere within the application code and is specified in terms of a 
+ * fully qualified package name.
+ * <p>For example, the icons used by the code within the container are located
+ * in the <code>org/openmicroscopy/shoola/env/ui/graphx</code>, so the
+ * corresponding location has to be specified as
+ * <code>org.openmicroscopy.shoola.env.ui.graphx</code> in the container's 
+ * configuration file &#151; note that the contents of this directory will be
+ * part of a <i>jar</i> file.</p>
+ * <p>So after application start-up, the container's registry will contain an
+ * <code>IconFactory</code> object which is configured to read any image file
+ * (file type must be one supported by <i>Swing</i>) within the specifed
+ * directory: <br>
+ * <code>
+ * IconFactory factory = (IconFactory)<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;registry.lookup("/resources/icons/DefaultFactory");
+ * <br>Icon logo = factory.getIcon("OME16.png");
+ * </code> 
+ * </p>
  *
  * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
  * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
@@ -55,30 +76,41 @@ import javax.swing.ImageIcon;
 public class IconFactory
 {
 
+	/** Points to the directory specified by the <i>location</i> tag.
+	 * The path is relative to the application classpath.
+	 */
 	private String location;
 
-	public IconFactory(String location)
+
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param location	The FQN of the package containing the icons.
+	 */
+	IconFactory(String location)
 	{
 		this.location = "/"+location.replace('.', '/');
 	}
 
 	/** 
-	 * Creates an icon.
+	 * Creates an {@link Icon} from the specified file.
 	 * 
-	 * @param name	icon's name.
-	 * @return
+	 * @param name	The file name.  Must be a valid name within the location
+	 * 				specified in the configuration file.
+	 * @return	An {@link Icon} object created from the image file.
 	 */
 	public Icon getIcon(String name)
 	{
 		ImageIcon icon = null;
 		if (location != null) {
 			try {
-				//NOTE: check if it's going to work in a jar file.
+				//TODO: check if it's going to work in a jar file.
 				String path = location + "/" + name;
 				URL url = IconFactory.class.getResource(path);
 				icon = new ImageIcon(url);
 			} catch (Exception e) {
 				throw new RuntimeException(e);
+				//TODO: handle properly.
 			} 
 		} else {
 			//TODO: errorMsg via logService
