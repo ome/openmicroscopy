baf74b340c705660df6579330c42fc6df34e4949
jeffm <jeffm@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-02-19 22:05:14 +0000

    additional file, written on AA flight 1761
    git-svn-id: file:///home/svn/shoola-omero/trunk@250 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/browser/BrowserModelListener.java
@@ -0,0 +1,58 @@
+/*
+ * org.openmicroscopy.shoola.agents.browser.BrowserModelListener
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Jeff Mellen <jeffm@alum.mit.edu>
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.agents.browser;
+
+/**
+ * Defines an interface that is responsive to changes made to the browser
+ * model by a controller or external source.  The BrowserView should
+ * implement this interface in order to listen to external changes in the
+ * model (and redraw itself appropriately)
+ * 
+ * @author Jeff Mellen, <a href="mailto:jeffm@alum.mit.edu">jeffm@alum.mit.edu</a><br>
+ * <b>Internal version:</b> $Revision$ $Date$
+ * @version 2.2
+ * @since OME2.2
+ */
+public interface BrowserModelListener
+{
+    /**
+     * Indicates that the model has updated, and the view should
+     * redraw its children, or repaint, or something to indicate the
+     * underlying change.  This isn't very informative, but the view
+     * should know how to draw a model each time.
+     */
+    public void modelUpdated();
+}
