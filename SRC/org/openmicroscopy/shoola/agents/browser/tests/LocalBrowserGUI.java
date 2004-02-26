129d121718ac81a64ddf1dbc08d2e44acb022063
jeffm <jeffm@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-02-26 20:29:14 +0000

    test additions
    git-svn-id: file:///home/svn/shoola-omero/trunk@275 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/browser/tests/LocalBrowserGUI.java
@@ -0,0 +1,89 @@
+/*
+ * org.openmicroscopy.shoola.agents.browser.tests.LocalBrowserGUI
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Jeff Mellen <jeffm@alum.mit.edu>
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.agents.browser.tests;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+
+import org.openmicroscopy.shoola.agents.browser.ui.BrowserInternalFrame;
+
+/**
+ * A local browser GUI (testbed for thumbnail/browser stuff)
+ * 
+ * @author Jeff Mellen, <a href="mailto:jeffm@alum.mit.edu">jeffm@alum.mit.edu</a><br>
+ * <b>Internal version:</b> $Revision$ $Date$
+ * @version 2.2
+ * @since OME2.2
+ */
+public class LocalBrowserGUI extends JDesktopPane
+{
+    /**
+     * The constructor for the LocalBrowserGUI.
+     *
+     */
+    public LocalBrowserGUI()
+    {
+        super();
+    }
+    
+    /**
+     * Not sure if this is the right call yet.
+     * @param frame
+     */
+    public void addBrowser(BrowserInternalFrame frame)
+    {
+        frame.setIconifiable(true);
+        frame.setClosable(true);
+        frame.setMaximizable(true);
+        frame.setResizable(true);
+        add(frame,new Integer(1));
+        frame.show();
+    }
+    
+    /**
+     * Ditto this.
+     * @param frame
+     */
+    public void removeBrowser(BrowserInternalFrame frame)
+    {
+        remove(frame);
+    }
+}
---
+++ SRC/org/openmicroscopy/shoola/agents/browser/tests/LocalBrowserTest.java
@@ -0,0 +1,88 @@
+/*
+ * org.openmicroscopy.shoola.agents.browser.tests.LocalBrowserTest
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Jeff Mellen <jeffm@alum.mit.edu>
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.agents.browser.tests;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+import org.openmicroscopy.shoola.agents.browser.BrowserController;
+import org.openmicroscopy.shoola.agents.browser.BrowserEnvironment;
+import org.openmicroscopy.shoola.agents.browser.BrowserManager;
+import org.openmicroscopy.shoola.agents.browser.BrowserModel;
+import org.openmicroscopy.shoola.agents.browser.BrowserTopModel;
+import org.openmicroscopy.shoola.agents.browser.BrowserView;
+import org.openmicroscopy.shoola.agents.browser.ThumbnailSourceModel;
+import org.openmicroscopy.shoola.agents.browser.layout.PlateLayoutMethod;
+import org.openmicroscopy.shoola.agents.browser.ui.BrowserInternalFrame;
+
+/**
+ * @author Jeff Mellen, <a href="mailto:jeffm@alum.mit.edu">jeffm@alum.mit.edu</a><br>
+ * <b>Internal version:</b> $Revision$ $Date$
+ * @version 2.2
+ * @since OME2.2
+ */
+public class LocalBrowserTest
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(800,600);
+        LocalBrowserGUI gui = new LocalBrowserGUI();
+        
+        Container container = frame.getContentPane();
+        container.add(gui);
+        
+        BrowserEnvironment env = BrowserEnvironment.getInstance();
+        BrowserManager manager = env.getBrowserManager();
+        
+        LocalTSMLoader loader = new LocalTSMLoader();
+        ThumbnailSourceModel tsm = loader.getModel();
+        
+        BrowserTopModel model = new BrowserTopModel();
+        BrowserModel bm = new BrowserModel();
+        bm.setLayoutMethod(new PlateLayoutMethod(8,12));
+        BrowserView view = new BrowserView(bm,model);
+        BrowserController controller = new BrowserController(bm);
+        controller.setView(view);
+        controller.displayCurrentDataModel();
+        
+        BrowserInternalFrame bif = new BrowserInternalFrame(controller);
+        gui.addBrowser(bif);
+        
+        frame.show();
+    }
+}
