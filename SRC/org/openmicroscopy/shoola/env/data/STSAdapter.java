d13102b790526b3a4b42ebc9d3dd130c1229538c
jeffm <jeffm@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-03-11 23:10:06 +0000

    STSAdapter meaty; STS scaled back (there's no project granularity, stupid me...)
    git-svn-id: file:///home/svn/shoola-omero/trunk@376 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/data/STSAdapter.java
+++ SRC/org/openmicroscopy/shoola/env/data/STSAdapter.java
@@ -29,10 +29,17 @@
 
 package org.openmicroscopy.shoola.env.data;
 
+import java.util.Iterator;
 import java.util.List;
 
+import org.openmicroscopy.ds.Criteria;
 import org.openmicroscopy.ds.DataFactory;
+import org.openmicroscopy.ds.RemoteAuthenticationException;
+import org.openmicroscopy.ds.RemoteConnectionException;
+import org.openmicroscopy.ds.RemoteServerErrorException;
 import org.openmicroscopy.ds.dto.Attribute;
+import org.openmicroscopy.ds.dto.DataColumn;
+import org.openmicroscopy.ds.dto.SemanticElement;
 import org.openmicroscopy.ds.dto.SemanticType;
 import org.openmicroscopy.shoola.env.config.Registry;
 
@@ -66,6 +73,16 @@ class STSAdapter
     private DataFactory proxy;
     private Registry context;
     
+    private final String GLOBAL_GRANULARITY = "G";
+    private final String DATASET_GRANULARITY = "D";
+    private final String IMAGE_GRANULARITY = "I";
+    private final String FEATURE_GRANULARITY = "F";
+    
+    private final String GLOBAL_KEY = "id";
+    private final String DATASET_KEY = "dataset_id";
+    private final String IMAGE_KEY = "image_id";
+    private final String FEATURE_KEY = "feature_id";
+    
     public STSAdapter(DataFactory proxy, Registry context)
     {
         if(proxy == null || context == null)
@@ -83,17 +100,6 @@ class STSAdapter
      * @see org.openmicroscopy.shoola.env.data.SemanticTypesService#getAvailableGlobalTypes()
      */
     public List getAvailableGlobalTypes()
-        throws DSOutOfServiceException, DSAccessException
-    {
-        // TODO flesh out this method
-        return null;
-    }
-    
-    /**
-     * TODO write documentation
-     * @see org.openmicroscopy.shoola.env.data.SemanticTypesService#getAvailableProjectTypes()
-     */
-    public List getAvailableProjectTypes()
         throws DSOutOfServiceException, DSAccessException
     {
         // TODO flesh out this method
@@ -132,17 +138,6 @@ class STSAdapter
         // TODO flesh out this method
         return null;
     }
-
-    /**
-     * TODO write documentation
-     * @see org.openmicroscopy.shoola.env.data.SemanticTypesService#countProjectAttributes(org.openmicroscopy.ds.dto.SemanticType, int)
-     */
-    public int countProjectAttributes(SemanticType type, int projectID)
-        throws DSOutOfServiceException, DSAccessException
-    {
-        // TODO flesh out this method
-        return 0;
-    }
     
     /**
      * TODO write documentation
@@ -151,8 +146,8 @@ class STSAdapter
     public int countDatasetAttributes(SemanticType type, int datasetID)
         throws DSOutOfServiceException, DSAccessException
     {
-        // TODO flesh out this method
-        return 0;
+        Criteria criteria = buildCountCriteria(DATASET_GRANULARITY,datasetID);
+        return count(type,criteria);
     }
     
     /**
@@ -162,8 +157,8 @@ class STSAdapter
     public int countImageAttributes(SemanticType type, int imageID)
         throws DSOutOfServiceException, DSAccessException
     {
-        // TODO flesh out this method
-        return 0;
+        Criteria criteria = buildCountCriteria(IMAGE_GRANULARITY,imageID);
+        return count(type,criteria);
     }
     
     /**
@@ -173,19 +168,8 @@ class STSAdapter
     public int countFeatureAttributes(SemanticType type, int featureID)
         throws DSOutOfServiceException, DSAccessException
     {
-        // TODO flesh out this method
-        return 0;
-    }
-    
-    /**
-     * TODO write documentation
-     * @see org.openmicroscopy.shoola.env.data.SemanticTypesService#retrieveProjectAttributes(org.openmicroscopy.ds.dto.SemanticType, int)
-     */
-    public List retrieveProjectAttributes(SemanticType type, int projectID)
-        throws DSOutOfServiceException, DSAccessException
-    {
-        // TODO flesh out this method
-        return null;
+        Criteria criteria = buildCountCriteria(FEATURE_GRANULARITY,featureID);
+        return count(type,criteria);
     }
     
     /**
@@ -195,8 +179,9 @@ class STSAdapter
     public List retrieveDatasetAttributes(SemanticType type, int datasetID)
         throws DSOutOfServiceException, DSAccessException
     {
-        // TODO flesh out this method
-        return null;
+        Criteria criteria =
+            buildDefaultRetrieveCriteria(type,DATASET_GRANULARITY,datasetID);
+        return (List)retrieveListData(type,criteria);
     }
     
     /**
@@ -206,8 +191,9 @@ class STSAdapter
     public List retrieveImageAttributes(SemanticType type, int imageID)
         throws DSOutOfServiceException, DSAccessException
     {
-        // TODO flesh out this method
-        return null;
+        Criteria criteria =
+            buildDefaultRetrieveCriteria(type,IMAGE_GRANULARITY,imageID);
+        return (List)retrieveListData(type,criteria);
     }
     
     /**
@@ -217,8 +203,9 @@ class STSAdapter
     public List retrieveFeatureAttributes(SemanticType type, int featureID)
         throws DSOutOfServiceException, DSAccessException
     {
-        // TODO flesh out this method
-        return null;
+        Criteria criteria =
+            buildDefaultRetrieveCriteria(type,FEATURE_GRANULARITY,featureID);
+        return (List)retrieveListData(type,criteria);
     }
 
     /**
@@ -228,7 +215,199 @@ class STSAdapter
     public Attribute retrieveAttribute(SemanticType type, int attributeID)
         throws DSOutOfServiceException, DSAccessException
     {
-        // TODO flesh out this method
-        return null;
+        // cheap trick, I maybe should clear this confusion up (although this
+        // is the only way to get global attributes; it applies to any
+        // attribute which you select by individual ID)
+        Criteria criteria =
+            buildDefaultRetrieveCriteria(type,GLOBAL_GRANULARITY,attributeID);
+        return (Attribute)retrieveData(type,criteria);
+    }
+    
+    /**
+     * Returns a count of the number of objects of the specified type that
+     * meet the specified criteria.
+     * @param type The attribute type to query.
+     * @param criteria The search parameters.
+     * @return The number of attributes that meet the parameters.
+     * @throws DSOutOfServiceException If the connection is broken or if the
+     *                                 user is not logged in.
+     * @throws DSAccessException If there is a communication or server error.
+     */
+    private int count(SemanticType type, Criteria criteria)
+        throws DSOutOfServiceException, DSAccessException
+    {
+        try
+        {
+            return proxy.count(type,criteria);
+        }
+        catch(RemoteConnectionException rce)
+        {
+            throw new DSOutOfServiceException("Can't connect to OMEDS", rce);
+        }
+        catch(RemoteAuthenticationException rae)
+        {
+            throw new DSOutOfServiceException("Not logged in", rae);
+        }
+        catch(RemoteServerErrorException rsee)
+        {
+            throw new DSAccessException("Can't count attributes", rsee);
+        }
+    }
+    
+    /**
+     * Load a single attribute defined by the criteria.
+     * 
+     * @param type The type of attribute to retrieve.
+     * @param criteria The criteria to search by.
+     * @return An Attribute of the specified type that adheres to that criteria.
+     * @throws DSOutOfServiceException If the connection is broken, or if the
+     *                                 user isn't logged in.
+     * @throws DSAccessException If a server communication error occurs.
+     */
+    private Object retrieveData(SemanticType type, Criteria criteria)
+        throws DSOutOfServiceException, DSAccessException
+    {
+        Object retVal = null;
+        
+        try
+        {
+            retVal = proxy.retrieve(type,criteria);
+        }
+        catch(RemoteConnectionException rce)
+        {
+            throw new DSOutOfServiceException("Can't connect to OMEDS", rce);
+        }
+        catch(RemoteAuthenticationException rae)
+        {
+            throw new DSOutOfServiceException("Not logged in", rae);
+        }
+        catch(RemoteServerErrorException rsee)
+        {
+            throw new DSAccessException("Can't load data", rsee);
+        }
+        return retVal;
+    }
+    
+    /**
+     * Loads the attributes defined by the criteria.
+     * @param type The type of attribute to retrieve.
+     * @param criteria The criteria to search by.
+     * @return All attributes of the specified type that adhere to the
+     *         specified criteria.
+     * @throws DSOutOfServiceException If the connection is broken, or if the
+     *                                 user isn't logged in.
+     * @throws DSAccessException If a server communication error occurs.
+     */
+    private Object retrieveListData(SemanticType type, Criteria criteria)
+        throws DSOutOfServiceException, DSAccessException
+    {
+        Object retVal = null;
+        
+        try
+        {
+            retVal = proxy.retrieveList(type,criteria);
+        }
+        catch(RemoteConnectionException rce)
+        {
+            throw new DSOutOfServiceException("Can't connect to OMEDS",rce);
+        }
+        catch(RemoteAuthenticationException rae)
+        {
+            throw new DSOutOfServiceException("Not logged in",rae);
+        }
+        catch(RemoteServerErrorException rsee)
+        {
+            throw new DSAccessException("Can't retrieve data",rsee);
+        }
+        
+        return retVal;
+    }
+    
+    /**
+     * Returns a Criteria object which contains the amount of information
+     * required to call <code>count()</code>: that is, the granularity of
+     * the attribute desired and the target ID.
+     * 
+     * @param granularity The granularity of the attribute to count.
+     * @param targetID The ID of the target to count.
+     * @return A criteria conforming to the above parameters.
+     */
+    private Criteria buildCountCriteria(String granularity, int targetID)
+    {
+        Criteria criteria = new Criteria();
+        if(granularity.equals(GLOBAL_GRANULARITY))
+        {
+            criteria.addFilter(GLOBAL_KEY,new Integer(targetID));
+        }
+        else if(granularity.equals(DATASET_GRANULARITY))
+        {
+            criteria.addFilter(DATASET_KEY, new Integer(targetID));
+        }
+        else if(granularity.equals(IMAGE_GRANULARITY))
+        {
+            criteria.addFilter(IMAGE_KEY, new Integer(targetID));
+        }
+        else if(granularity.equals(FEATURE_GRANULARITY))
+        {
+            criteria.addFilter(FEATURE_KEY, new Integer(targetID));
+        }
+        else return null;
+        return criteria;
+    }
+    
+    /**
+     * Returns a Criteria object which contains the default amount of
+     * information to be returned in an attribute-- all the primitive fields,
+     * and all references with just the ID object returned (such that
+     * successive calls may be made to the server to retrieve those attributes
+     * as well)
+     *
+     * @param st The type of object to specify.
+     * @param granularity The type of target to specify.
+     * @param targetID The ID of the target to filter by.
+     * @return A Criteria object with the default depth with the specified
+     *         parameters.
+     */
+    private Criteria buildDefaultRetrieveCriteria(SemanticType st,
+                                                  String granularity,
+                                                  int targetID)
+    {
+        if(st == null)
+        {
+            return null;
+        }
+        Criteria criteria = new Criteria();
+        List elements = st.getElements();
+        
+        for(Iterator iter = elements.iterator(); iter.hasNext();)
+        {
+            SemanticElement element = (SemanticElement)iter.next();
+            String elementName = element.getName();
+            DataColumn columnType = element.getDataColumn();
+            criteria.addWantedField(elementName);
+            if(columnType.getSQLType().equals("reference"))
+            {
+                criteria.addWantedField(elementName,"id");
+            }
+        }
+        
+        if(granularity.equals(DATASET_GRANULARITY))
+        {
+            criteria.addFilter(DATASET_KEY, new Integer(targetID));
+        }
+        else if(granularity.equals(IMAGE_GRANULARITY))
+        {
+            criteria.addFilter(IMAGE_KEY, new Integer(targetID));
+        }
+        else if(granularity.equals(FEATURE_GRANULARITY))
+        {
+            criteria.addFilter(FEATURE_KEY, new Integer(targetID));
+        }
+        // the attribute is itself the target in the global case
+        else if(granularity.equals(GLOBAL_GRANULARITY))
+        {
+            criteria.addFilter(GLOBAL_KEY, new Integer(targetID));
+        }
+        return criteria;
     }
 }
--- SRC/org/openmicroscopy/shoola/env/data/SemanticTypesService.java
+++ SRC/org/openmicroscopy/shoola/env/data/SemanticTypesService.java
@@ -71,16 +71,6 @@ public interface SemanticTypesService
         throws DSOutOfServiceException, DSAccessException;
     
     /**
-     * Gets a list of all available semantic types with project granularity.
-     * @return A list of all available project types.
-     * @throws DSOutOfServiceException If the user is not logged in or the
-     *                                 connection with the server is lost.
-     * @throws DSAccessException If there was a communication error.
-     */
-    public List getAvailableProjectTypes()
-        throws DSOutOfServiceException, DSAccessException;
-    
-    /**
      * Gets a list of all available semantic types with dataset granularity.
      * @return A list of all available dataset types.
      * @throws DSOutOfServiceException If the user is not logged in or the
@@ -108,22 +98,6 @@ public interface SemanticTypesService
      * @throws DSAccessException If there was a communication error.
      */
     public List getAvailableFeatureTypes()
-        throws DSOutOfServiceException, DSAccessException;
-        
-    /**
-     * Counts the number of attributes of the given semantic type that
-     * correspond to the project with the specified ID.  If the answer is 0,
-     * the client should not attempt to retrieve a list of attributes.
-     * 
-     * @param type The type of attribute to count.
-     * @param projectID Which project to search.
-     * @return The number of attributes of the given type associated with
-     *         The specified project.
-     * @throws DSOutOfServiceException If the user is not logged in or the
-     *                                 connection with the server is lost.
-     * @throws DSAccessException If there was a communication error.
-     */
-    public int countProjectAttributes(SemanticType type, int projectID)
         throws DSOutOfServiceException, DSAccessException;
         
     /**
@@ -184,20 +158,6 @@ public interface SemanticTypesService
      * @throws DSAccessException
      */
     public Attribute retrieveAttribute(SemanticType type, int attributeID)
-        throws DSOutOfServiceException, DSAccessException;
-    
-    /**
-     * Retrieves all the Attributes with the given SemanticType that belong
-     * to a project with the specified ID.
-     * @param type The type of attribute to retrieve.
-     * @param projectID The ID of the project to search.
-     * @return A list of attributes (ordered by attribute ID) of the specified
-     *         type that belong to the specified project.
-     * @throws DSOutOfServiceException If the user is not logged in or the
-     *                                 connection with the server is lost.
-     * @throws DSAccessException If there was a communication error.
-     */
-    public List retrieveProjectAttributes(SemanticType type, int projectID)
         throws DSOutOfServiceException, DSAccessException;
     
     /**
