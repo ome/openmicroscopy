5a86d126a661570ef806a3fde3b84f0cd57c2b76
afalconi <afalconi@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-04-01 00:11:27 +0000

    Initial commit.
    git-svn-id: file:///home/svn/shoola-omero/trunk@514 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/env/rnd/events/RenderImage.java
@@ -0,0 +1,101 @@
+/*
+ * org.openmicroscopy.shoola.env.rnd.events.RenderImage
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.env.rnd.events;
+
+//Java imports
+
+//Third-party libraries
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.env.event.RequestEvent;
+import org.openmicroscopy.shoola.env.rnd.defs.PlaneDef;
+
+/** 
+ * Encapsulates a request to the <i>Rendering Engine</i> to render a given
+ * pixels set within an image.
+ * The <i>Rendering Engine</i> replies to this request with a 
+ * {@link ImageRendererd} event.
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:a.falconi@dundee.ac.uk">
+ * 					a.falconi@dundee.ac.uk</a>
+ * @version 2.2
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
+public class RenderImage
+	extends RequestEvent
+{
+	
+	/** The ID of the pixels set. */
+	private int			pixelsID;
+		
+	/** Defines what data, within the pixels set, to render. */
+	private PlaneDef	planeDef;
+	
+	
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param pixelsID	The ID of the pixels set.
+	 */
+	public RenderImage(int pixelsID, PlaneDef planeDef)
+	{
+		if (planeDef == null)
+			throw new NullPointerException("No plane definition.");
+		this.planeDef = planeDef;
+		this.pixelsID = pixelsID;
+	}
+
+	/**
+	 * Return the ID of the pixels set.
+	 * 
+	 * @return	See above.
+	 */
+	public int getPixelsID()
+	{
+		return pixelsID;
+	}
+
+	/**
+	 * Returns the definition of what data, within the pixels set, to render.
+	 * 
+	 * @return	See above.
+	 */
+	public PlaneDef getPlaneDef() 
+	{
+		return planeDef;
+	}
+
+}
