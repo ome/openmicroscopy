11f7b4449dcd0a9d191d43917e7625c18ce5cabe
afalconi <afalconi@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-04-01 00:09:32 +0000

    Implemented all functionality for internal release.
    git-svn-id: file:///home/svn/shoola-omero/trunk@511 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/rnd/RenderingEngine.java
+++ SRC/org/openmicroscopy/shoola/env/rnd/RenderingEngine.java
@@ -30,14 +30,27 @@
 package org.openmicroscopy.shoola.env.rnd;
 
 //Java imports
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
 
 //Third-party libraries
 
 //Application-internal dependencies
 import org.openmicroscopy.shoola.env.Container;
 import org.openmicroscopy.shoola.env.config.Registry;
+import org.openmicroscopy.shoola.env.event.AgentEvent;
+import org.openmicroscopy.shoola.env.event.AgentEventListener;
+import org.openmicroscopy.shoola.env.event.EventBus;
 import org.openmicroscopy.shoola.env.rnd.data.DataSink;
+import org.openmicroscopy.shoola.env.rnd.defs.RenderingDef;
+import org.openmicroscopy.shoola.env.rnd.events.ImageLoaded;
+import org.openmicroscopy.shoola.env.rnd.events.ImageRendered;
+import org.openmicroscopy.shoola.env.rnd.events.LoadImage;
+import org.openmicroscopy.shoola.env.rnd.events.RenderImage;
+import org.openmicroscopy.shoola.env.rnd.events.RenderingPropChange;
 import org.openmicroscopy.shoola.env.rnd.metadata.MetadataSource;
+import org.openmicroscopy.shoola.env.rnd.metadata.MetadataSourceException;
 
 /** 
  * 
@@ -54,6 +67,7 @@ import org.openmicroscopy.shoola.env.rnd.metadata.MetadataSource;
  * @since OME2.2
  */
 public class RenderingEngine
+	implements AgentEventListener
 {
 
 	private static RenderingEngine		singleton;
@@ -72,10 +86,63 @@ public class RenderingEngine
 	
 	
 	private Registry		registry;
+	private Map				renderers;
+	
 	
 	private RenderingEngine(Container c)
 	{
 		registry = c.getRegistry();
+		renderers = new HashMap();
+	}
+	
+	private void hanldeException(String message, Exception cause)
+	{
+		throw new RuntimeException(message, cause);
+		//TODO: post an event on Swing thread that launches UserNotifier.
+	}
+	
+	private void handleLoadImage(LoadImage request)
+	{
+		Renderer rnd = new Renderer(request.getImageID(), request.getPixelsID(),
+																		this);
+		try {
+			rnd.initialize();
+			renderers.put(new Integer(request.getPixelsID()), rnd);
+			RenderingDef original = rnd.getRenderingDef(), copy;
+			copy = original.copy();
+			EventBus eventBus = registry.getEventBus();
+			RenderingControlImpl facade = new RenderingControlImpl(rnd);
+			RenderingControlProxy proxy =
+								new RenderingControlProxy(facade, copy, eventBus);
+			ImageLoaded response = new ImageLoaded(request, proxy);
+			eventBus.post(response);  //TODO: this has to be run w/in Swing thread.
+		} catch (MetadataSourceException mse) {
+			hanldeException("Can't load image metadata. Image id: "+
+													request.getImageID(), mse);
+		}																
+	}
+	
+	private void handleRenderImage(RenderImage request)
+	{
+		Renderer rnd = (Renderer) renderers.get(
+											new Integer(request.getPixelsID()));
+		//TODO: if null, log?
+		if (rnd != null) {
+			try {
+				BufferedImage img = rnd.render(request.getPlaneDef());  //Never null, see RenderImage.
+				ImageRendered response = new ImageRendered(request, img);
+				EventBus eventBus = registry.getEventBus();
+				eventBus.post(response);  //TODO: this has to be run w/in Swing thread.
+			} catch (Exception dse) {  //TODO: DataSourceException, omeis.
+				hanldeException("Can't load pixels data. Pixels id: "+
+													request.getPixelsID(), dse);
+			}
+		}
+	}
+	
+	private void handleRenderingPropChange(RenderingPropChange event)
+	{
+		event.doUpdate();
 	}
 	
 	MetadataSource getMetadataSource(int imageID, int pixelsID)
@@ -91,12 +158,25 @@ public class RenderingEngine
 	
 	public void activate()
 	{
-		//TODO: implement.	
+		EventBus eventBus = registry.getEventBus();
+		eventBus.register(this, LoadImage.class);
+		eventBus.register(this, RenderImage.class);
+		eventBus.register(this, RenderingPropChange.class);
+		//TODO: start event loop in its own thread.
 	}
 	
 	public void terminate()
 	{
 		//TODO: implement.
+	}
+
+	public void eventFired(AgentEvent e) 
+	{
+		//TODO: put event on the queue and remove the following.
+		if (e instanceof LoadImage)	handleLoadImage((LoadImage) e);
+		else if	(e instanceof RenderImage)	handleRenderImage((RenderImage) e);
+		else if (e instanceof RenderingPropChange)
+			handleRenderingPropChange((RenderingPropChange) e);
 	}
 
 }
