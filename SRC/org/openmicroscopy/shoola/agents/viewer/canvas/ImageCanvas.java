3bbb34b3c14c3a2c4720f13da0bae0999ececda1
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-04-02 12:04:08 +0000

    initial commit
    git-svn-id: file:///home/svn/shoola-omero/trunk@544 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/viewer/canvas/ImageCanvas.java
@@ -0,0 +1,214 @@
+/*
+ * org.openmicroscopy.shoola.agents.viewer.canvas.ImageCanvas
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.viewer.canvas;
+
+//Java imports
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+
+//Third-party libraries
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.agents.viewer.ViewerUIF;
+
+/** 
+ * 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:a.falconi@dundee.ac.uk">
+ * 					a.falconi@dundee.ac.uk</a>
+ * @version 2.2 
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
+public class ImageCanvas
+	extends JPanel
+{
+	/** default space size between images i.e. top-centre left-centre*/
+	static final int wSpace = 20, hSpace = 20; 
+	 
+	private static final Color	BACKGROUND_COLOR = new Color(204, 204, 255); 
+	
+	/** Its icon holds the current image. */        
+	private JLabel          	picture;
+
+	/** Two layers, bottom for the image and top for the glass pane. */
+	private JLayeredPane    	imgLayers;
+	
+	/** Glass pane laid on top of the image. */
+	private Component       	imgGlassPane;
+	
+	/** 
+	* A reference to the content pane of the viewer's internal frame. 
+	* Needed to do better layout.
+	*/
+	private Component       	contentPane;
+	
+	/** A reference to viewer . */
+	private ViewerUIF       	viewer; 
+	   
+	/** 
+	* Creates the panel to host the image. 
+	* 
+	* @param contentPane	A reference to the content pane of the viewer's 
+	* 						internal frame. Needed to do better layout.
+	*/
+	public ImageCanvas(ViewerUIF viewer, Component contentPane)
+	{
+		this.viewer = viewer;
+		this.contentPane = contentPane;
+		setBackground(BACKGROUND_COLOR);  
+		setLayout(new ImageCanavasLM());
+		imgLayers = new JLayeredPane();
+		add(imgLayers);
+	}
+	
+	/** 
+	* Sets the specified <code>pane</code> to be the image glass pane.
+	* The passed component will be laid over the image and will have the same 
+	* origin and dimensions as the underlying image. 
+	* The glass pane should normally be transparent.
+	*
+	* @param   pane    The glass pane.
+	*/
+	void setImageGlassPane(Component pane)
+	{
+		if (pane != null) {
+			if (imgGlassPane != null)	imgLayers.remove(imgGlassPane);
+			imgGlassPane = pane;
+			imgLayers.add(imgGlassPane, new Integer(1));
+			revalidate();
+		}
+	}
+	
+	void removeImageGlassPane()
+	{
+		if (imgGlassPane != null)	imgLayers.remove(imgGlassPane);
+		imgGlassPane = null;
+		revalidate();   
+	}
+    
+	//TODO: override paintComponent?
+	public void display(BufferedImage img)
+	{
+		//TODO: check that double-buffering is performed under the hood
+		ImageIcon icon = new ImageIcon(img); 
+		if (picture != null) imgLayers.remove(picture);
+		picture = new JLabel(icon);
+		imgLayers.add(picture, new Integer(0));
+	}
+
+	/** 
+	 * Custom manager that takes care of the layout of {@link ImageCanvas}.
+	 * This layout manager is tightly coupled to the specific structure of 
+	 * {@link ImageCanvas} and its constituent components.
+	 */
+	private class ImageCanavasLM implements LayoutManager 
+	{
+		/** 
+		 * Lays out the components of the image canvas.
+		 *
+		 * @param canvas     The image canvas. 
+		 */
+		public void layoutContainer(Container canvas)
+		{
+			ImageCanvas c = (ImageCanvas) canvas;
+			if (c.picture != null) {
+				int imgW = c.picture.getIcon().getIconWidth(),
+					imgH = c.picture.getIcon().getIconHeight(),
+					x = 0, y = 0;
+				Dimension d = c.contentPane.getSize();
+				if (imgW < d.width) x = (d.width-imgW)/2;
+				if (imgH < d.height) y = (d.height-imgH)/2; 
+				c.imgLayers.setBounds(x, y, imgW, imgH);
+				c.picture.setBounds(wSpace, hSpace, imgW, imgH);	
+				if (c.imgGlassPane != null) 
+					c.imgGlassPane.setBounds(wSpace, hSpace, imgW, imgH);
+			}
+		}
+
+		/** 
+		 * Returns the preferred amount of space for the layout, 
+		 * which is the image size.
+		 *
+		 * @param canvas	The image canvas. 
+		 * @return The above mentioned dimensions.
+		 */
+		public Dimension preferredLayoutSize(Container canvas)
+		{
+			ImageCanvas c = (ImageCanvas)canvas;
+			int w = 0, h = 0;
+			if (c.picture != null) {
+				w = c.picture.getIcon().getIconWidth()+wSpace;
+				h = c.picture.getIcon().getIconHeight()+hSpace;
+			}
+			return new Dimension(w, h);
+		}
+        
+		/** 
+		 * Returns the minimum amount of space the layout needs.
+		 * This is the same as the preferred dimensions of the image canvas.
+		 *
+		 * @param   canvas     The image canvas. 
+		 * @return  The above mentioned dimensions.
+		 */ 
+		public Dimension minimumLayoutSize(Container canvas)
+		{
+			return preferredLayoutSize(canvas);
+		}
+
+		/** 
+		 * Required by I/F but not actually needed in our case, 
+		 * no op implementation.
+		 */
+		public void addLayoutComponent(String name, Component comp) {}
+		
+		/** 
+		 * Required by I/F but not actually needed in our case, 
+		 * no op implementation.
+		 */
+		public void removeLayoutComponent(Component comp) {}
+   
+	}
+    
+}
---
+++ SRC/org/openmicroscopy/shoola/agents/viewer/util/ImageSaver.java
@@ -0,0 +1,194 @@
+/*
+ * org.openmicroscopy.shoola.agents.viewer.util.ImageSaver
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.viewer.util;
+
+//Java imports
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Iterator;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+
+//Third-party libraries
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.agents.viewer.ViewerCtrl;
+import org.openmicroscopy.shoola.env.ui.UserNotifier;
+
+/** 
+ * 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:a.falconi@dundee.ac.uk">
+ * 					a.falconi@dundee.ac.uk</a>
+ * @version 2.2 
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
+public class ImageSaver
+	extends JFileChooser
+{
+	private static final String 	JPEG = "jpeg";
+	private static final String 	JPG = "jpg";
+	private static final String 	PNG = "png";
+	
+	private ViewerCtrl				controller;
+	public ImageSaver(ViewerCtrl controller)
+	{
+		this.controller = controller;
+		createChooser();
+	}
+	
+	/**
+	 * Save a specified buffered image as a <code>jpeg</code> or 
+	 * <code>png</code>.
+	 * 
+	 * @param format	one the format defined above.
+	 * @param img		buffered image to save.
+	 * @param fileName	image's name.
+	 * @param message	message to display when the image has been saved.
+	 */
+	private void saveImageAs(String format, BufferedImage img, String fileName, 
+							String message)
+	{
+		UserNotifier un = controller.getRegistry().getUserNotifier();
+		
+		try {
+			Iterator writers = ImageIO.getImageWritersByFormatName(format);
+			ImageWriter writer = (ImageWriter) writers.next();
+			File f = new File(fileName);
+			ImageOutputStream ios = ImageIO.createImageOutputStream(f);
+			writer.setOutput(ios);
+			writer.write(img);
+			ios.close();
+			un.notifyInfo("Image saved", message);
+			//TODO: forward event to server.
+		} catch (Exception ex){
+			un.notifyError("Save image failure", "Unable to save the image",
+								ex);
+		}
+	}
+	
+	/** Build the file chooser. */
+	private void createChooser()
+	{ 
+		setFileSelectionMode(FILES_ONLY);
+		JpegFilter jpegFilter = new JpegFilter();
+		setFileFilter(jpegFilter);
+		PngFilter pngFilter = new PngFilter();
+		addChoosableFileFilter(pngFilter); 
+		setFileFilter(pngFilter);
+		setAcceptAllFileFilterUsed(false);
+		int returnVal = showDialog(controller.getReferenceFrame(), 
+									"Save Image");
+		
+		//Process the result
+		File file = getSelectedFile();
+		if (file != null && returnVal == JFileChooser.APPROVE_OPTION) {
+			String format = PNG;
+			if (getFileFilter() instanceof JpegFilter) format = JPG;
+			String  fileName = file.getAbsolutePath()+"."+format, 
+					name = file.getName()+"."+format;;
+			String message = "The image "+name+", has been saved in \n"
+							+getCurrentDirectory();
+			saveImageAs(format, controller.getBufferedImage(), fileName, 
+						message);
+			setSelectedFile(null);
+		}      
+	}
+	
+	/** 
+	 * Filter the files which extension is <code>jpeg</code> or 
+	 * <code>jpg</code>.
+	 */
+	static class JpegFilter extends FileFilter
+	{
+		private String description = JPEG;
+		
+		public String getDescription()
+		{
+			return description;
+		}
+		
+		public boolean accept(File f)
+		{
+			if (f.isDirectory()) return true;
+			String s = f.getName();
+			String extension = null;
+			int i = s.lastIndexOf('.');
+			if (i > 0 && i < s.length()-1)
+				extension = s.substring(i+1).toLowerCase();
+			if (extension != null) {
+				boolean b = false;
+				if (extension.equals(JPEG) || extension.equals(JPG)) b =  true;
+				return b;
+			}
+			return false;
+		}
+	}
+	
+	/** 
+	 * Filter the files which extension is <code>png</code> or 
+	 * <code>jpg</code>.
+	 */
+	static class PngFilter extends FileFilter
+	{
+		private String description = PNG;
+	
+		public String getDescription()
+		{
+			return description;
+		}
+		
+		public boolean accept(File f)
+		{
+			if (f.isDirectory()) return true;
+			String s = f.getName();
+			String extension = null;
+			int i = s.lastIndexOf('.');
+			if (i > 0 && i < s.length()-1)
+				extension = s.substring(i+1).toLowerCase();
+			if (extension != null) {
+				boolean b = false;
+				if (extension.equals(PNG)) b = true;
+				return b;
+			}
+			return false;
+		}
+	}
+	
+}
