4835187efe827f397eb0a667bafaa8f767672f5b
jeffm <jeffm@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-04-05 07:23:33 +0000

    adding several heatmap files
    git-svn-id: file:///home/svn/shoola-omero/trunk@579 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/browser/heatmap/HeatMapModel.java
@@ -0,0 +1,96 @@
+/*
+ * org.openmicroscopy.shoola.agents.browser.heatmap.HeatMapModel
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Jeff Mellen <jeffm@alum.mit.edu>
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.agents.browser.heatmap;
+
+import java.util.List;
+
+import org.openmicroscopy.ds.dto.SemanticType;
+import org.openmicroscopy.shoola.agents.browser.BrowserModel;
+
+/**
+ * Creates a basis for the heat map, based on a particular browser model.
+ * 
+ * @author Jeff Mellen, <a href="mailto:jeffm@alum.mit.edu">jeffm@alum.mit.edu</a><br>
+ * <b>Internal version:</b> $Revision$ $Date$
+ * @version 2.2
+ * @since OME2.2
+ */
+public class HeatMapModel
+{
+    private BrowserModel source;
+    
+    private SemanticTypeTree treeModel;
+    
+    /**
+     * Creates an initial heat map model for the particular browser agent.
+     * Picks out the scalar values for display (and classifications)
+     * @param source The BrowserModel to base the HeatMapModel off.  Since
+     *               the HeatMapModel will draw from the BrowserModel's
+     *               relevant semantic types, setSemanticTypes() should have
+     *               already been called on the backing model before
+     *               instantiating the heat map model.
+     */
+    public HeatMapModel(BrowserModel source)
+    {
+        this.source = source;
+        updateTreeModel();
+    }
+    
+    public BrowserModel getInfoSource()
+    {
+        return source;
+    }
+    
+    public void setInfoSource(BrowserModel source)
+    {
+        this.source = source; 
+        updateTreeModel();
+    }
+    
+    private void updateTreeModel()
+    {
+        if(source == null)
+        {
+            treeModel = new SemanticTypeTree("(empty)");
+            return;
+        }
+        List typesList = source.getRelevantTypes();
+        SemanticType[] types = new SemanticType[typesList.size()];
+        typesList.toArray(types);
+        treeModel = new SemanticTypeTree(source.getDataset().getName()+
+                                         " Attributes",types);
+    }
+}
---
+++ SRC/org/openmicroscopy/shoola/agents/browser/heatmap/SemanticTypeTree.java
@@ -0,0 +1,266 @@
+/*
+ * org.openmicroscopy.shoola.agents.browser.heatmap.SemanticTypeTree
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Jeff Mellen <jeffm@alum.mit.edu>
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.agents.browser.heatmap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.openmicroscopy.ds.dto.SemanticElement;
+import org.openmicroscopy.ds.dto.SemanticType;
+import org.openmicroscopy.shoola.agents.browser.datamodel.DataElementType;
+
+/**
+ * Creates a hierarchy of semantic types.
+ * 
+ * @author Jeff Mellen, <a href="mailto:jeffm@alum.mit.edu">jeffm@alum.mit.edu</a><br>
+ * <b>Internal version:</b> $Revision$ $Date$
+ * @version 2.2
+ * @since OME2.2
+ */
+public class SemanticTypeTree
+{
+    private TreeNode root;
+    private Set children;
+    
+    /**
+     * Creates an empty semantic type tree.
+     *
+     */
+    public SemanticTypeTree()
+    {
+        root = new TreeNode("Empty");
+        children = new HashSet();
+    }
+    
+    public SemanticTypeTree(String rootName)
+    {
+        root = new TreeNode(rootName);
+        children = new HashSet();
+    }
+    
+    /**
+     * Builds a tree (hopefully there are no cycles within STs) out of
+     * the list of given semantic types.  Populates with DFS.
+     * @param types
+     */
+    public SemanticTypeTree(String rootName, SemanticType[] types)
+    {
+        root = new TreeNode(rootName);
+        List dfsQueue = new ArrayList();
+        
+        if(types == null || types.length == 0)
+        {
+            return;
+        }
+        else
+        {
+            for(int i=0;i<types.length;i++)
+            {
+                SemanticType type = types[i];
+                TypeNode node = new TypeNode(type);
+                dfsQueue.add(node);
+            }
+        }
+        while(dfsQueue.size() > 0)
+        {
+            TypeNode node = (TypeNode)dfsQueue.get(0);
+            dfsQueue.remove(0);
+            SemanticType st = node.getType();
+            System.err.println("workin' on " + st.getName());
+            List elements = st.getElements();
+            
+            // right now, don't hit the DB to go deeper
+            for(Iterator iter = elements.iterator(); iter.hasNext();)
+            {
+                SemanticElement element = (SemanticElement)iter.next();
+                String type = element.getDataColumn().getSQLType();
+                DataElementType det = DataElementType.forName(type);
+                String fqName = "";
+                if(node.getFQName() != null)
+                {
+                    fqName = fqName + node.getFQName() + ".";
+                }
+                fqName = fqName + element.getDataColumn().getColumnName();
+                if(det == DataElementType.ATTRIBUTE)
+                {
+                    SemanticType childType = element.getSemanticType();
+                    TypeNode typeNode = new TypeNode(childType);
+                    typeNode.setFQName(fqName);
+                    node.addChild(typeNode);
+                    dfsQueue.add(typeNode);
+                }
+                else
+                {
+                    ElementNode elNode = new ElementNode(element.getName(),det);
+                    elNode.setFQName(fqName);
+                    node.addChild(elNode);
+                }
+            }
+        }
+    }
+    
+    /**
+     * Returns the root of the tree.
+     * @return See above.
+     */
+    public TreeNode getRootNode()
+    {
+        return root;
+    }
+    
+    /**
+     * A node in the SemanticType tree encapsulating an instance of a
+     * SemanticType.
+     */
+    public static class TypeNode extends TreeNode
+    {
+        private SemanticType content;
+        private Set children;
+        
+        public TypeNode()
+        {
+            super("null");
+            children = new HashSet();
+        }
+        
+        public TypeNode(SemanticType type)
+        {
+            super(type.getName());
+            this.content = type;
+            children = new HashSet();
+        }
+        
+        public SemanticType getType()
+        {
+            return content;
+        }
+        
+        public void setType(SemanticType type)
+        {
+            if(type != null)
+            {
+                this.content = type;
+            }
+        }
+    }
+    
+    /**
+     * A node that has a primitive value attached to it, and thus has no
+     * children.  Attribute children of other Attributes should also be
+     * TypeNodes.
+     */
+    public static class ElementNode extends TreeNode
+    {
+        private DataElementType type;
+        
+        public ElementNode(String name, DataElementType type)
+        {
+            super(name);
+            this.type = type;
+        }
+        
+        public DataElementType getType()
+        {
+            return type;
+        }
+    }
+    
+    /**
+     * A node within this tree.
+     */
+    static class TreeNode
+    {
+        protected Set children;
+        protected String name;
+        protected String fqName; // fully qualified name
+        
+        public TreeNode(String nodeName)
+        {
+            this.name = nodeName;
+            children = new HashSet();
+        }
+        
+        public void addChild(TreeNode node)
+        {
+            if(node != null)
+            {
+                children.add(node);
+            }
+        }
+        
+        public Set getChildren()
+        {
+            return Collections.unmodifiableSet(children);
+        }
+        
+        public String getName()
+        {
+            return name;
+        }
+        
+        /**
+         * Gets the fully qualified (criteria query) name of this
+         * node.
+         * @return The fully qualified name of this node.
+         */
+        public String getFQName()
+        {
+            return fqName;
+        }
+        
+        /**
+         * Sets the fully qualified (criteria query) name of this
+         * node.
+         * @param fqName The fully qualified name of this node.
+         */
+        public void setFQName(String fqName)
+        {
+            this.fqName = fqName;
+        }
+        
+        public void removeChild(TreeNode node)
+        {
+            if(node != null && children.contains(node))
+            {
+                children.remove(node);
+            }
+        }
+    }
+}
