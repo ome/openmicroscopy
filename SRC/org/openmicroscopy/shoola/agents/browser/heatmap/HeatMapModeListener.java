7ea1ba1c2e080d28604407d256bff8dcd373d6e2
jeffm <jeffm@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-04-07 16:48:28 +0000

    whoops
    git-svn-id: file:///home/svn/shoola-omero/trunk@632 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/browser/heatmap/HeatMapModeListener.java
@@ -0,0 +1,59 @@
+/*
+ * org.openmicroscopy.shoola.agents.browser.heatmap.HeatMapModeListener
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Jeff Mellen <jeffm@alum.mit.edu>
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.agents.browser.heatmap;
+
+/**
+ * Listener interface for changing heat map modes.
+ * 
+ * @author Jeff Mellen, <a href="mailto:jeffm@alum.mit.edu">jeffm@alum.mit.edu</a><br>
+ * <b>Internal version:</b> $Revision$ $Date$
+ * @version 2.2
+ * @since OME2.2
+ */
+public interface HeatMapModeListener
+{
+    /**
+     * Indicates that the mode changed to the specified mode.
+     * @param newMode The new mode.
+     */
+    public void modeChanged(HeatMapMode newMode);
+    
+    /**
+     * Indicates that the scale changed to the specified scale.
+     * @param newScale The new scale.
+     */
+    public void scaleChanged(Scale newScale);
+}
