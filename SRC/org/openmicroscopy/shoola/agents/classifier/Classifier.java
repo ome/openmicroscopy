31e52fc2bdf241d975bb7b2586a744d664238ea0
jeffm <jeffm@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-04-26 19:12:10 +0000

    adds base classifier classes
    git-svn-id: file:///home/svn/shoola-omero/trunk@822 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/classifier/Classifier.java
@@ -0,0 +1,327 @@
+/*
+ * org.openmicroscopy.shoola.agents.classifier.Classifier
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Jeff Mellen <jeffm@alum.mit.edu>
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.agents.classifier;
+
+import java.util.List;
+
+import org.openmicroscopy.ds.st.Category;
+import org.openmicroscopy.ds.st.CategoryGroup;
+import org.openmicroscopy.ds.st.Classification;
+import org.openmicroscopy.shoola.env.Agent;
+import org.openmicroscopy.shoola.env.config.Registry;
+import org.openmicroscopy.shoola.env.data.DSAccessException;
+import org.openmicroscopy.shoola.env.data.DSOutOfServiceException;
+import org.openmicroscopy.shoola.env.data.SemanticTypesService;
+import org.openmicroscopy.shoola.env.data.events.ServiceActivationRequest;
+import org.openmicroscopy.shoola.env.event.AgentEvent;
+import org.openmicroscopy.shoola.env.event.AgentEventListener;
+import org.openmicroscopy.shoola.env.ui.TopFrame;
+import org.openmicroscopy.shoola.env.ui.UserNotifier;
+
+/**
+ * The base agent for the Classification module.  The module handles the
+ * creation of Classification attributes and Category types.  Other agents
+ * can have their own UIs for representing the categories.
+ * 
+ * @author Jeff Mellen, <a href="mailto:jeffm@alum.mit.edu">jeffm@alum.mit.edu</a><br>
+ * <b>Internal version:</b> $Revision$ $Date$
+ * @version 2.2
+ * @since OME2.2
+ */
+public class Classifier implements Agent, AgentEventListener
+{
+    private static final String CATEGORY = "Category";
+    private static final String CATEGORY_GROUP = "CategoryGroup";
+    private static final String CLASSIFICATION = "Classification";
+    
+    
+    
+    private Registry registry;
+    
+    private TopFrame topFrame;
+    
+    /* (non-Javadoc)
+     * @see org.openmicroscopy.shoola.env.Agent#activate()
+     */
+    public void activate()
+    {
+        // TODO Auto-generated method stub
+
+    }
+    
+    /* (non-Javadoc)
+     * @see org.openmicroscopy.shoola.env.Agent#canTerminate()
+     */
+    public boolean canTerminate()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    /* (non-Javadoc)
+     * @see org.openmicroscopy.shoola.env.Agent#setContext(org.openmicroscopy.shoola.env.config.Registry)
+     */
+    public void setContext(Registry ctx)
+    {
+        this.registry = ctx;
+        topFrame = registry.getTopFrame();
+    }
+    
+    /* (non-Javadoc)
+     * @see org.openmicroscopy.shoola.env.Agent#terminate()
+     */
+    public void terminate()
+    {
+        // TODO Auto-generated method stub
+
+    }
+    
+    /* (non-Javadoc)
+     * @see org.openmicroscopy.shoola.env.event.AgentEventListener#eventFired(org.openmicroscopy.shoola.env.event.AgentEvent)
+     */
+    public void eventFired(AgentEvent e)
+    {
+        // TODO Auto-generated method stub
+
+    }
+    
+    /**
+     * Creates a new category group (set of phenotypes)
+     * @param groupName
+     * @param description
+     * @param datasetID
+     * @return
+     */
+    CategoryGroup createCategoryGroup(String groupName,
+                                      String description,
+                                      int datasetID)
+    {
+        CategoryGroup group = null;
+        try
+        {
+            SemanticTypesService sts = registry.getSemanticTypesService();
+            group = (CategoryGroup)sts.createAttribute(CATEGORY_GROUP,datasetID);
+            group.setName(groupName);
+            group.setDescription(description);
+        }
+        catch(DSAccessException dsae)
+        {
+            UserNotifier un = registry.getUserNotifier();
+            un.notifyError("Phenotype creation error",
+                           "Unable to create the semantic type "+CATEGORY_GROUP,
+                           dsae);
+        }
+        catch(DSOutOfServiceException dsoe)
+        {
+            ServiceActivationRequest request = new ServiceActivationRequest(
+                                        ServiceActivationRequest.DATA_SERVICES);
+            registry.getEventBus().post(request);
+        }
+        return group;
+    }
+    
+    /**
+     * Creates a new category within a CategoryGroup.
+     * @param categoryName
+     * @param description
+     * @param datasetID
+     * @return
+     */
+    Category createCategory(CategoryGroup parent,
+                            String categoryName,
+                            String description,
+                            int datasetID)
+    {
+        Category category = null;
+        try
+        {
+            SemanticTypesService sts = registry.getSemanticTypesService();
+            category = (Category)sts.createAttribute(CATEGORY,datasetID);
+            category.setCategoryGroup(parent);
+            category.setName(categoryName);
+            category.setDescription(description);
+        }
+        catch(DSAccessException dsae)
+        {
+            dsae.printStackTrace();
+            UserNotifier un = registry.getUserNotifier();
+            un.notifyError("Phenotype creation error",
+                           "Unable to create the semantic type "+CATEGORY,
+                           dsae);
+        }
+        catch(DSOutOfServiceException dsoe)
+        {
+            ServiceActivationRequest request = new ServiceActivationRequest(
+                                        ServiceActivationRequest.DATA_SERVICES);
+            registry.getEventBus().post(request);
+        }
+        return category;
+    }
+    
+    Classification createClassification(Category category, int imageID)
+    {
+        Classification classification = null;
+        try
+        {
+            SemanticTypesService sts = registry.getSemanticTypesService();
+            classification =
+                (Classification)sts.createAttribute(CLASSIFICATION,imageID);
+            classification.setCategory(category);
+            classification.setConfidence(new Float(1.0f));
+        }
+        catch(DSAccessException dsae)
+        {
+            dsae.printStackTrace();
+            UserNotifier un = registry.getUserNotifier();
+            un.notifyError("Phenotype creation error",
+                           "Unable to create the semantic type "+CATEGORY,
+                           dsae);
+        }
+        catch(DSOutOfServiceException dsoe)
+        {
+            ServiceActivationRequest request = new ServiceActivationRequest(
+                                        ServiceActivationRequest.DATA_SERVICES);
+            registry.getEventBus().post(request);
+        }
+        return classification;
+    }
+    
+    void commitNewAttributes(List attributes)
+    {
+        if(attributes == null || attributes.size() == 0)
+        {
+            return;
+        }
+        try
+        {    
+            SemanticTypesService sts = registry.getSemanticTypesService();
+            sts.updateUserInputAttributes(attributes);
+        } catch(DSAccessException dsae) {
+            UserNotifier un = registry.getUserNotifier();
+            un.notifyError("Data Creation Failure", 
+                "Unable to update the semantic type ", dsae);
+        } catch(DSOutOfServiceException dsose) {    
+            ServiceActivationRequest request = new ServiceActivationRequest(
+                                        ServiceActivationRequest.DATA_SERVICES);
+            dsose.printStackTrace();
+            registry.getEventBus().post(request);
+        }
+    }
+    
+    void updateAttributes(List attributes)
+    {
+        if(attributes == null || attributes.size() == 0 ) return;
+        try {
+            SemanticTypesService sts = registry.getSemanticTypesService();
+            sts.updateAttributes(attributes);
+        } catch(DSAccessException dsae) {
+            UserNotifier un = registry.getUserNotifier();
+            un.notifyError("Data Creation Failure", 
+                "Unable to update the semantic type ", dsae);
+                dsae.printStackTrace();
+        } catch(DSOutOfServiceException dsose) {    
+            ServiceActivationRequest request = new ServiceActivationRequest(
+                                        ServiceActivationRequest.DATA_SERVICES);
+            dsose.printStackTrace();
+            registry.getEventBus().post(request);
+        }
+    }
+    
+    public List getCategoryGroups(int datasetID)
+    {
+        List categoryGroups = null;
+        try
+        {
+            SemanticTypesService sts = registry.getSemanticTypesService();
+            categoryGroups =
+                sts.retrieveDatasetAttributes(CATEGORY_GROUP,datasetID);
+        }
+        catch(DSAccessException dsa) {
+            UserNotifier un = registry.getUserNotifier();
+            un.notifyError("Could not retrieve category groups",
+                           dsa.getMessage(), dsa);
+        } catch(DSOutOfServiceException dso) {
+            ServiceActivationRequest request = new ServiceActivationRequest(
+                                        ServiceActivationRequest.DATA_SERVICES);
+            registry.getEventBus().post(request);
+        }
+        return categoryGroups;
+    }
+    
+    // likely much faster to piece together who belongs to whom locally (with
+    // all categories available) than over wicked expensive (currently) DB call
+    public List getCategories(int datasetID)
+    {
+        List categories = null;
+        try
+        {
+            SemanticTypesService sts = registry.getSemanticTypesService();
+            categories =
+                sts.retrieveDatasetAttributes(CATEGORY,datasetID);
+        }
+        catch(DSAccessException dsa) {
+            UserNotifier un = registry.getUserNotifier();
+            un.notifyError("Could not retrieve category groups",
+                           dsa.getMessage(), dsa);
+        } catch(DSOutOfServiceException dso) {
+            ServiceActivationRequest request = new ServiceActivationRequest(
+                                        ServiceActivationRequest.DATA_SERVICES);
+            registry.getEventBus().post(request);
+        }
+        return categories;
+    }
+    
+    public List getClassifications(int imageID)
+    {
+        List classifications = null;
+        try
+        {
+            SemanticTypesService sts = registry.getSemanticTypesService();
+            classifications =
+                sts.retrieveImageAttributes(CLASSIFICATION,imageID);
+        }
+        catch(DSAccessException dsa) {
+            UserNotifier un = registry.getUserNotifier();
+            un.notifyError("Could not retrieve category groups",
+                           dsa.getMessage(), dsa);
+        } catch(DSOutOfServiceException dso) {
+            ServiceActivationRequest request = new ServiceActivationRequest(
+                                        ServiceActivationRequest.DATA_SERVICES);
+            registry.getEventBus().post(request);
+        }
+        return classifications;
+    }
+}
