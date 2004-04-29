83cec316d7d4704b4202c62887fead23177a62f3
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-04-29 10:27:27 +0000

    Initial commit:  so far only zooming is possible
    git-svn-id: file:///home/svn/shoola-omero/trunk@831 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/viewer/transform/ImageInspector.java
@@ -0,0 +1,102 @@
+/*
+ * org.openmicroscopy.shoola.agents.viewer.transform.ImageInspector
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.viewer.transform;
+
+
+
+//Java imports
+import java.awt.BorderLayout;
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+
+//Third-party libraries
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.agents.viewer.ViewerCtrl;
+import org.openmicroscopy.shoola.agents.viewer.transform.zooming.ZoomPanel;
+
+/** 
+ * 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:a.falconi@dundee.ac.uk">
+ * 					a.falconi@dundee.ac.uk</a>
+ * @version 2.2 
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
+public class ImageInspector
+	extends JDialog
+{
+	
+	/** Maximum width of the window. */
+	private static final int		WIN_W = 500;
+	
+	/** Maximum height of the window. */
+	private static final int		WIN_H = 500;
+	
+	ToolBar 						toolBar;
+	MenuBar							menuBar;
+
+	private ImageInspectorManager	manager;
+	private ZoomPanel				zoomPanel;
+	
+	public ImageInspector(ViewerCtrl control)
+	{
+		super(control.getReferenceFrame(), "Image Inspector", true);
+		manager = new ImageInspectorManager(this, control.getBufferedImage());
+		init(control);
+		setJMenuBar(menuBar);
+		buildGUI();
+		setSize(WIN_W, WIN_H);
+	}
+	
+	/** Initializes the components. */
+	private void init(ViewerCtrl control)
+	{
+		zoomPanel = new ZoomPanel(control.getBufferedImage());
+		manager.setZoomPanel(zoomPanel);
+		menuBar = new MenuBar(manager);
+		toolBar = new ToolBar(control.getRegistry(), manager);
+	}
+	
+	/** Build and lay out the GUI. */
+	private void buildGUI()
+	{
+		JScrollPane scroll = new JScrollPane(zoomPanel);
+		getContentPane().add(toolBar, BorderLayout.NORTH);
+		getContentPane().add(scroll, BorderLayout.CENTER);
+	}
+	
+}
---
+++ SRC/org/openmicroscopy/shoola/agents/viewer/transform/ImageInspectorManager.java
@@ -0,0 +1,132 @@
+/*
+ * org.openmicroscopy.shoola.agents.viewer.transform.ImageInspectorManager
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.viewer.transform;
+
+//Java imports
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+
+//Third-party libraries
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.agents.viewer.transform.zooming.ZoomBar;
+import org.openmicroscopy.shoola.agents.viewer.transform.zooming.ZoomMenu;
+import org.openmicroscopy.shoola.agents.viewer.transform.zooming.ZoomPanel;
+
+/** 
+ * 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:a.falconi@dundee.ac.uk">
+ * 					a.falconi@dundee.ac.uk</a>
+ * @version 2.2 
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
+public class ImageInspectorManager
+{
+
+	public static final double 		MIN_ZOOM_LEVEL = 0.25;
+	public static final double 		MAX_ZOOM_LEVEL = 3.0;
+	public static final double		ZOOM_DEFAULT = 1.0;
+	public static final double 		ZOOM_INCREMENT = 0.25;
+	
+	private ImageInspector			view;
+	private BufferedImage			image;
+	private int						imageWidth, imageHeight;
+	private Dimension				imageDim;
+	private ZoomPanel				zoomPanel;
+	private double					curZoomLevel;
+	
+	public ImageInspectorManager(ImageInspector view, BufferedImage image)
+	{
+		this.view = view;
+		this.image = image;	
+		imageWidth = image.getWidth();
+		imageHeight = image.getHeight();
+		imageDim = new Dimension(imageWidth, imageHeight);
+		curZoomLevel = ZOOM_DEFAULT;
+	}
+	
+	void setZoomPanel(ZoomPanel zoomPanel)
+	{
+		this.zoomPanel = zoomPanel;
+		zoomPanel.setPreferredSize(imageDim);
+		zoomPanel.setSize(imageDim);
+	}
+	
+	/** Zoom in or out. */
+	public void setZoomLevel(double level)
+	{
+		if (curZoomLevel != level) {
+			zoom(level);
+			curZoomLevel = level;
+		}
+	}
+	
+	/** 
+	 * Zoom in or out according to the level.
+	 * 
+	 * @param level	value in the range MIN_ZOOM_LEVEL and MAX_ZOOM_LEVEL.
+	 */
+	private void zoom(double level)
+	{
+		ZoomBar zoomBar = view.toolBar.getZoomBar();
+		zoomBar.getManager().setText(level);
+		ZoomMenu zoomMenu = view.menuBar.getZoomMenu();
+		zoomMenu.getManager().setItemSelected(level);
+		int w = (int) (imageWidth*level);
+	   	int h = (int) (imageHeight*level);
+	   	int x, y;
+	   	Dimension d;
+
+	   	if (level >= ZOOM_DEFAULT) {
+			x = 0;
+		   	y = 0;
+		   	w = (int) (imageWidth*level);
+		   	h = (int) (imageHeight*level);
+		   	d = new Dimension(w, h);
+	   	} else {
+		   	x = (int) (imageWidth*(ZOOM_DEFAULT-level))/2;
+		   	y = (int) (imageHeight*(ZOOM_DEFAULT-level))/2;
+			w = imageWidth;
+			h = imageHeight;
+		   	d = imageDim;
+		}
+	   	zoomPanel.setPreferredSize(d);
+	   	zoomPanel.setSize(d);
+	   	zoomPanel.paintImage(level, x, y);
+	}
+	
+}
