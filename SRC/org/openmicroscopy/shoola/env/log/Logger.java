b5db6fed762cef9d2c8038d2aad0e5a279b68b6a
afalconi <afalconi@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-05-03 14:19:39 +0000

    Overloaded all methods to accept a LogMessage object too.
    git-svn-id: file:///home/svn/shoola-omero/trunk@872 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/log/Logger.java
+++ SRC/org/openmicroscopy/shoola/env/log/Logger.java
@@ -51,12 +51,14 @@ package org.openmicroscopy.shoola.env.log;
  * 		terminate.</li>
  * <ul>
  * <p>Every method takes in two parameters: the originator of the log message
- * and the log message itself.  A configuration file (in the configuration
- * directory under the installation directory) provides for fine-tuning of the
- * log settings on a per-class basis.  Those settings include the choice of
- * output locations and verbosity based on priority levels &#151;
- * <i>DEBUG</i> has a lower priority than <i>INFO</i>, which, in turn, is
- * lower priority than <i>WARN</i>, and so on.</p>
+ * and the log message itself.  If the message spans multiple lines, then
+ * a {@link LogMessage} object should be used to contruct it.</p>  
+ * <p>A configuration file (in the configuration directory under the
+ * installation directory) provides for fine-tuning of the log settings on a
+ * per-class basis.  Those settings include the choice of output locations and
+ * verbosity based on priority levels &#151; <i>DEBUG</i> has a lower priority
+ * than <i>INFO</i>, which, in turn, is lower priority than <i>WARN</i>, and so
+ * on.</p>
  * <p>The implementation of the service is thread-safe.  Methods can be called
  * from different threads without compromising the integrity of the log records.
  * </p>
@@ -85,12 +87,28 @@ public interface Logger
     public void debug(Object originator, String logMsg);
     
 	/**
+	 * Logs a debug message.
+	 * 
+	 * @param originator The originator of the message.
+	 * @param msg	The log message.
+	 */
+	public void debug(Object originator, LogMessage msg);
+    
+	/**
 	 * Logs an info message.
 	 * 
 	 * @param originator The originator of the message.
 	 * @param logMsg	The log message.
 	 */
     public void info(Object originator, String logMsg);
+    
+	/**
+	 * Logs an info message.
+	 * 
+	 * @param originator The originator of the message.
+	 * @param msg	The log message.
+	 */
+	public void info(Object originator, LogMessage msg);
     
 	/**
 	 * Logs a warn message.
@@ -101,12 +119,28 @@ public interface Logger
     public void warn(Object originator, String logMsg);
     
 	/**
+	 * Logs a warn message.
+	 * 
+	 * @param originator The originator of the message.
+	 * @param msg	The log message.
+	 */
+	public void warn(Object originator, LogMessage msg);
+	
+	/**
 	 * Logs an error message.
 	 * 
 	 * @param originator The originator of the message.
 	 * @param logMsg	The log message.
 	 */
 	public void error(Object originator, String logMsg);
+	
+	/**
+	 * Logs an error message.
+	 * 
+	 * @param originator The originator of the message.
+	 * @param msg	The log message.
+	 */
+	public void error(Object originator, LogMessage msg);
     
 	/**
 	 * Logs a fatal message.
@@ -115,5 +149,13 @@ public interface Logger
 	 * @param logMsg	The log message.
 	 */
 	public void fatal(Object originator, String logMsg);
+	
+	/**
+	 * Logs a fatal message.
+	 * 
+	 * @param originator The originator of the message.
+	 * @param msg	The log message.
+	 */
+	public void fatal(Object originator, LogMessage msg);
     
 }
