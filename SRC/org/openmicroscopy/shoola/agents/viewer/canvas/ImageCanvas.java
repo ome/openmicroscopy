e505938ee57e5c941c5a4b5734ac4e2fb615cd2d
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-05-03 17:01:55 +0000

    refactored
    git-svn-id: file:///home/svn/shoola-omero/trunk@882 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/viewer/canvas/ImageCanvas.java
+++ SRC/org/openmicroscopy/shoola/agents/viewer/canvas/ImageCanvas.java
@@ -31,14 +31,11 @@ package org.openmicroscopy.shoola.agents.viewer.canvas;
 
 //Java imports
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.LayoutManager;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 //Third-party libraries
@@ -64,140 +61,60 @@ public class ImageCanvas
 	extends JPanel
 {
 	 
-	private static final Color	BACKGROUND_COLOR = new Color(204, 204, 255); 
+	/** Background color. */
+	private static final Color	BACKGROUND_COLOR = new Color(204, 204, 255);
 	
-	/** Its icon holds the current image. */        
-	private JLabel          	picture;
-
-	/** Two layers, bottom for the image and top for the glass pane. */
-	private JLayeredPane    	imgLayers;
+	/** The BufferedImage to display. */	
+	private BufferedImage 		image;
 	
-	/** Glass pane laid on top of the image. */
-	private Component       	imgGlassPane;
+	/** Width and height. */
+	private int					imageWidth, imageHeight;
 	
-	private int					iconHeight, iconWidth;
-	   
-	/** Creates the panel to host the image. */
+	/** Location of the image. */
+	private int					x, y;
+	
 	public ImageCanvas()
 	{
-		setBackground(BACKGROUND_COLOR);  
-		setLayout(new ImageCanavasLM());
-		imgLayers = new JLayeredPane();
-		add(imgLayers);
+		setBackground(BACKGROUND_COLOR); 
 	}
-	
+ 
 	/** 
-	* Sets the specified <code>pane</code> to be the image glass pane.
-	* The passed component will be laid over the image and will have the same 
-	* origin and dimensions as the underlying image. 
-	* The glass pane should normally be transparent.
-	*
-	* @param pane    The glass pane.
-	*/
-	void setImageGlassPane(Component pane)
+	 * Display the image. 
+	 * 
+	 * @param image		buffered image to display.
+	 * 	
+	 */	
+	public void paintImage(BufferedImage image)
 	{
-		if (pane != null) {
-			if (imgGlassPane != null)	imgLayers.remove(imgGlassPane);
-			imgGlassPane = pane;
-			imgLayers.add(imgGlassPane, new Integer(1));
-			revalidate();
-		}
-	}
-	
-	void removeImageGlassPane()
+		this.image = image;
+		imageWidth = image.getWidth();
+		imageHeight = image.getHeight();
+		repaint();
+	} 
+
+	/** Overrides the paintComponent. */
+	public void paint(Graphics g)
 	{
-		if (imgGlassPane != null)	imgLayers.remove(imgGlassPane);
-		imgGlassPane = null;
-		revalidate();   
-	}
-    
-	//TODO: override paintComponent?
-	public void display(BufferedImage img)
-	{
-		//TODO: check that double-buffering is performed under the hood
-		ImageIcon icon = new ImageIcon(img); 
-		iconHeight = icon.getIconHeight();
-		iconWidth = icon.getIconWidth();
-		if (picture != null) imgLayers.remove(picture);
-		picture = new JLabel(icon);
-		imgLayers.add(picture, new Integer(0));
+		super.paintComponent(g);
+		Graphics2D g2D = (Graphics2D) g;
+		setLocation();
+		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+							RenderingHints.VALUE_ANTIALIAS_ON);
+		g2D.setRenderingHint(RenderingHints.KEY_RENDERING,
+							RenderingHints.VALUE_RENDER_QUALITY);
+
+		g2D.setColor(Color.black);
+		g2D.drawImage(image, null, x, y);
 	}
 
-	public int getIconHeight() { return iconHeight; }
-	
-	public int getIconWidth() { return iconWidth; }
-	
-	/** 
-	 * Custom manager that takes care of the layout of {@link ImageCanvas}.
-	 * This layout manager is tightly coupled to the specific structure of 
-	 * {@link ImageCanvas} and its constituent components.
-	 */
-	private class ImageCanavasLM implements LayoutManager 
+	/** Set the coordinate of the top-left corner of the image. */
+	private void setLocation()
 	{
-		/** 
-		 * Lays out the components of the image canvas.
-		 *
-		 * @param canvas     The image canvas. 
-		 */
-		public void layoutContainer(Container canvas)
-		{
-			ImageCanvas c = (ImageCanvas) canvas;
-			if (c.picture != null) {
-				int imgW = c.picture.getIcon().getIconWidth(),
-					imgH = c.picture.getIcon().getIconHeight(),
-					x = 0, y = 0;
-				Dimension d = c.getSize();
-				if (imgW < d.width) x = (d.width-imgW)/2;
-				if (imgH < d.height) y = (d.height-imgH)/2; 
-				c.imgLayers.setBounds(x, y, imgW, imgH);
-				c.picture.setBounds(0, 0, imgW, imgH);	
-				//if (c.imgGlassPane != null) 
-				//	c.imgGlassPane.setBounds(wSpace, hSpace, imgW, imgH);
-			}
-		}
-
-		/** 
-		 * Returns the preferred amount of space for the layout, 
-		 * which is the image size.
-		 *
-		 * @param canvas	The image canvas. 
-		 * @return The above mentioned dimensions.
-		 */
-		public Dimension preferredLayoutSize(Container canvas)
-		{
-			ImageCanvas c = (ImageCanvas) canvas;
-			int w = 0, h = 0;
-			if (c.picture != null) {
-				w = c.picture.getIcon().getIconWidth();
-				h = c.picture.getIcon().getIconHeight();
-			}
-			return new Dimension(w, h);
-		}
-        
-		/** 
-		 * Returns the minimum amount of space the layout needs.
-		 * This is the same as the preferred dimensions of the image canvas.
-		 *
-		 * @param   canvas     The image canvas. 
-		 * @return  The above mentioned dimensions.
-		 */ 
-		public Dimension minimumLayoutSize(Container canvas)
-		{
-			return preferredLayoutSize(canvas);
-		}
-
-		/** 
-		 * Required by I/F but not actually needed in our case, 
-		 * no op implementation.
-		 */
-		public void addLayoutComponent(String name, Component comp) {}
-		
-		/** 
-		 * Required by I/F but not actually needed in our case, 
-		 * no op implementation.
-		 */
-		public void removeLayoutComponent(Component comp) {}
-   
+		Rectangle d = getBounds();
+		x = (int) (d.width-imageWidth)/2;
+		y = (int) (d.height-imageHeight)/2;
+		if (x < 0) x = 0;
+		if (y < 0) y = 0;
 	}
-    
+	
 }
