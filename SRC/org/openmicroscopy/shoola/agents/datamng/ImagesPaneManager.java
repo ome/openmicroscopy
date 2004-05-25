c6299f0b699148a239b317d812b26b404bb093dd
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-05-25 14:01:30 +0000

    table view
    git-svn-id: file:///home/svn/shoola-omero/trunk@1049 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/datamng/ImagesPaneManager.java
+++ SRC/org/openmicroscopy/shoola/agents/datamng/ImagesPaneManager.java
@@ -34,15 +34,21 @@ package org.openmicroscopy.shoola.agents.datamng;
 //Java imports
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
+import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
-
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
 //Third-party libraries
 
 //Application-internal dependencies
 import org.openmicroscopy.shoola.env.data.model.ImageSummary;
+import org.openmicroscopy.shoola.util.ui.TableHeaderTextAndIcon;
+import org.openmicroscopy.shoola.util.ui.TableIconRenderer;
+import org.openmicroscopy.shoola.util.ui.TableSorter;
 
 /** 
  * 
@@ -63,20 +69,30 @@ class ImagesPaneManager
 {
 
 	/** Action id. */
-	private static final int		LOAD = 0, BY_NAME = 1, BY_DATE = 2;
+	private static final int				NAME = 0, DATE = 1, LOAD = 2;
+			
+	protected static final String[]			columnNames;
 	
+	static {
+		columnNames  = new String[2];
+		columnNames[NAME] = "Name";
+		columnNames[DATE] = "Date";
+	}
+
 	/** This UI component's view. */
-	private ImagesPane 				view;
+	private ImagesPane 						view;
 	
 	/** The agent's control component. */
-	private DataManagerCtrl 		agentCtrl;
+	private DataManagerCtrl 				agentCtrl;
 
-	private ImagesTableModel		tableModel;
+	private ImagesTableModel				tableModel;
 	
-	private JButton					load, orderByName, orderByDate;
+	private TableSorter 					sorter;
 	
-	private boolean					loaded;
+	private JButton							load;
 	
+	private boolean							loaded;
+
 	ImagesPaneManager(ImagesPane view, DataManagerCtrl agentCtrl)
 	{
 		this.view = view;
@@ -85,6 +101,22 @@ class ImagesPaneManager
 		initListeners();
 	}
 
+	/** update the view when an image's name has been modified. */
+	void updateImageInTable(ImageSummary is)
+	{
+		if (loaded) {
+			int rows = sorter.getRowCount();
+			ImageSummary summary;
+			for (int i = 0; i < rows; i++) {
+				summary = (ImageSummary) sorter.getValueAt(i, NAME);
+				if (summary.getID() == is.getID()) {
+					tableModel.setValueAt(is, i);
+					break;
+				}		
+			}
+		}
+	}
+	
 	/** 
 	 * Attach a mouse adapter to the tree in the view to get notified 
 	 * of mouse events on the tree.
@@ -92,20 +124,14 @@ class ImagesPaneManager
 	private void initListeners()
 	{
 		load = view.bar.getLoad();
-		orderByName = view.bar.getOrderByName();
-		orderByDate = view.bar.getOrderByDate();
 		load.addActionListener(this);
 		load.setActionCommand(""+LOAD);		
-		orderByName.addActionListener(this);
-		orderByName.setActionCommand(""+BY_NAME);
-		orderByDate.addActionListener(this);
-		orderByDate.setActionCommand(""+BY_DATE);
-		/*
-		view.tree.addMouseListener(new MouseAdapter() {
+		
+		view.table.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) { onClick(e); }
 			public void mouseReleased(MouseEvent e) { onClick(e); }
 		});
-		*/
+		
 	}
 
 	/** Handles event fired by the buttons. */
@@ -116,69 +142,79 @@ class ImagesPaneManager
 			switch (index) { 
 				case LOAD:
 					loadImages(); break;
-				case BY_NAME:
-					orderByName(); break;
-				case BY_DATE:
-					orderByDate(); break;
 			}
 		} catch(NumberFormatException nfe) {
 			throw new Error("Invalid Action ID "+index, nfe);
 		} 
 	}
 
+	/** Load all the images. */
 	private void loadImages()
 	{
 		if (!loaded) {
+			JTable table = view.table;
 			tableModel = new ImagesTableModel();
-			view.table.setModel(tableModel);
+			sorter = new TableSorter(tableModel);  
+			table.setModel(sorter);
+			sorter.addMouseListenerToHeaderInTable(table);
+			setTableLayout(table);
 			loaded = true;
-			setButtonsEnabled(loaded);
 		}
 	}
 	
-	private void orderByName()
+	/** Set icons in the tableHeader. */
+	private void setTableLayout(JTable table)
 	{
-		if (loaded) {
-		}
-
+		IconManager im = IconManager.getInstance(agentCtrl.getRegistry());
+		TableIconRenderer iconHeaderRenderer = new TableIconRenderer();
+		TableColumnModel tcm = table.getTableHeader().getColumnModel();
+		TableColumn tc = tcm.getColumn(NAME);
+		tc.setHeaderRenderer(iconHeaderRenderer);
+		TableHeaderTextAndIcon 
+		txt = new TableHeaderTextAndIcon(columnNames[NAME], 
+				im.getIcon(IconManager.ORDER_BY_NAME), "Order by name.");
+		tc.setHeaderValue(txt);
+		tc = tcm.getColumn(DATE);
+		tc.setHeaderRenderer(iconHeaderRenderer); 
+		txt = new TableHeaderTextAndIcon(columnNames[DATE], 
+				im.getIcon(IconManager.ORDER_BY_DATE), "Order by date.");
+		tc.setHeaderValue(txt);
 	}
 	
-	private void orderByDate()
-	{
-		if (loaded) {
-		}
-	}
-	
-	private void setButtonsEnabled(boolean b)
-	{
-		orderByDate.setEnabled(b);
-		orderByName.setEnabled(b);
-	}
-	
-
 	/** 
 	 * Handles mouse clicks within the tree component in the view.
 	 * If the mouse event is the platform popup trigger event, then the context 
-	 * popup menu is brought up. Otherwise, double-clicking on a image, 
-	 * node brings up the corresponding property sheet dialog.
+	 * popup menu is brought up. Otherwise, double-clicking on a project, 
+	 * dataset node brings up the corresponding property sheet dialog.
 	 *
 	 * @param e   The mouse event.
 	 */
 	private void onClick(MouseEvent e)
 	{
-		
+		int selRow = view.table.getSelectedRow();
+		if (selRow != -1) {
+			
+			if (e.isPopupTrigger()) {
+				ImageSummary 
+					target = (ImageSummary) sorter.getValueAt(selRow, NAME);
+				DataManagerUIF presentation = 
+						agentCtrl.getAbstraction().getPresentation();
+				TreePopupMenu popup = presentation.getPopupMenu();
+				popup.setTarget(target);  
+				popup.show(view.table, e.getX(), e.getY());
+			} 
+		}	
 	}
-	
+
 	/** 
 	 * A <code>2</code>-column table model to view the summary of 
-	 * the user's image
+	 * the user's image.
 	 * The first column contains the image names, the second column 
 	 * the <code>created date</code>.
 	 */
 	private final class ImagesTableModel 
 		extends AbstractTableModel
 	{
-		private final String[]	columnNames = {"Name", "Date"};
 		private final Object[]	images = agentCtrl.getUserImages().toArray();
 		private Object[][] 		data = new Object[images.length][2];
 		private ImagesTableModel() 
@@ -186,8 +222,8 @@ class ImagesPaneManager
 			ImageSummary is;
 			for (int i = 0; i < images.length; i++) {
 				is = (ImageSummary) images[i];
-				data[i][0] = is.getName();
-				data[i][1] = ""; //TODO: is.getDate();
+				data[i][NAME] = is;
+				data[i][DATE] = is.getDate(); 
 			}
 		}
 
@@ -200,21 +236,31 @@ class ImagesPaneManager
 		public Class getColumnClass(int col)
 		{
 			return getValueAt(0, col).getClass();
+			//return JTextField.class;
 		}
 		
 		public Object getValueAt(int row, int col) { return data[row][col]; }
 		
-		public boolean isCellEditable(int row, int col) {
-			boolean isEditable = false;
-			if (col == 0) isEditable = true;
-			return isEditable;
-		}
+		public boolean isCellEditable(int row, int col) { return (col == 0); }
 
 		//only name column is editable
 		public void setValueAt(Object value, int row, int col)
 		{
-			//TODO
+			if (col == NAME) {
+				ImageSummary is = (ImageSummary) sorter.getValueAt(row, col);
+				//is.setName(((JTextField) value).getText());
+				is.setName(((ImageSummary) value).getName());
+				agentCtrl.updateImage(is);
+			}
+		} 
+		
+		/** invoke when the view is updated. */
+		public void setValueAt(ImageSummary is, int row)
+		{
+			ImageSummary summary = (ImageSummary) sorter.getValueAt(row, NAME);
+			summary.setName(is.getName());
+			//fireTableCellUpdated(row, NAME);
 		} 
 	}
-
+	
 }
