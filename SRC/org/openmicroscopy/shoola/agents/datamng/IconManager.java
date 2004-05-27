33f7301432339669a827d57b95b7bf153a1b0f03
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-05-27 18:28:22 +0000

    added icons
    git-svn-id: file:///home/svn/shoola-omero/trunk@1065 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/datamng/IconManager.java
+++ SRC/org/openmicroscopy/shoola/agents/datamng/IconManager.java
@@ -136,21 +136,29 @@ public class IconManager
 	public static final int		IMPORT_IMAGE_BIG = 23;	
 	
 	/** ID of the orderByName icon. */
-	public static final int		ORDER_BY_NAME = 24;
+	public static final int		ORDER_BY_NAME_UP = 24;
+	
+	/** ID of the orderByName icon. */
+	public static final int		ORDER_BY_NAME_DOWN = 25;
 	
 	/** ID of the orderByDate icon. */
-	public static final int		ORDER_BY_DATE = 25;
+	public static final int		ORDER_BY_DATE_UP = 26;
+	
+	/** ID of the orderByDate icon. */
+	public static final int		ORDER_BY_DATE_DOWN = 27;
+	
+	public static final int		ORDER_BY_SELECTED_UP = 28;
+	
+	public static final int		ORDER_BY_SELECTED_DOWN = 29;
 	
 	/** ID of the filter icon. */
-	public static final int		FILTER = 26;
-	
-	public static final int		ORDER_BY_SELECTED = 27;
+	public static final int		FILTER = 30;
 	
 	/** 
 	 * The maximum ID used for the icon IDs.
 	 * Allows to correctly build arrays for direct indexing. 
 	 */
-	private static int          MAX_ID = 27;
+	private static int          MAX_ID = 30;
 	
 	/** Paths of the icon files. */
 	private static String[]     relPaths = new String[MAX_ID+1];
@@ -179,10 +187,13 @@ public class IconManager
 		relPaths[DATASET_BIG] = "nuvola_folder_image48.png";
 		relPaths[IMPORT_IMAGE_BIG] = "nuvola_digikam48.png";
 		relPaths[IMAGE_BIG] = "nuvola_image48.png";
-		relPaths[ORDER_BY_NAME] = "eclipse_alphab_sort_co16.png";
-		relPaths[ORDER_BY_DATE] = "nuvola_clock16.png";
-		relPaths[FILTER] = "eclipse_filter_ps16.png";
-		relPaths[ORDER_BY_SELECTED] = "nuvola_endturn16.png";
+		relPaths[ORDER_BY_NAME_UP] = "alphab_sort_co_with_arrow_up.png";
+		relPaths[ORDER_BY_NAME_DOWN] = "alphab_sort_co_with_arrow_down.png";
+		relPaths[ORDER_BY_DATE_UP] = "clock_with_arrow_up.png";
+		relPaths[ORDER_BY_DATE_DOWN] = "clock_with_arrow_down.png";
+		relPaths[ORDER_BY_SELECTED_UP] = "endturn_with_arrow_up.png";
+		relPaths[ORDER_BY_SELECTED_DOWN] = "endturn_with_arrow_down.png";
+		relPaths[FILTER] = "eclipse_filter_ps16.png";		
 	}
 	
 	/** The sole instance. */
