9110b6242a3180f23be1bdc2d086732674cb5eb8
hsh <hsh@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-07-15 20:17:15 +0000

    add code to only fire exited event on chain box if i am actually outside of bounds. also added setLastHighlighted() to make sure that boxes do not inadvertantly get left highlighted.
    git-svn-id: file:///home/svn/shoola-omero/trunk@1271 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/chainbuilder/piccolo/ChainPaletteEventHandler.java
+++ SRC/org/openmicroscopy/shoola/agents/chainbuilder/piccolo/ChainPaletteEventHandler.java
@@ -39,13 +39,19 @@ package org.openmicroscopy.shoola.agents.chainbuilder.piccolo;
 
 
 //Java imports
+import java.awt.geom.Point2D;
 
 //Third-party libraries
 import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.event.PInputEvent;
+import edu.umd.cs.piccolo.util.PBounds;
+
 
 //Application-internal dependencies
+import org.openmicroscopy.shoola.agents.events.SelectAnalysisChain;
 import org.openmicroscopy.shoola.agents.zoombrowser.piccolo.BufferedObject;
 
+import org.openmicroscopy.shoola.env.config.Registry;
 
 /** 
  * An event handler for a canvas containing {@link ModuleView} objects in the
@@ -62,10 +68,47 @@ import org.openmicroscopy.shoola.agents.zoombrowser.piccolo.BufferedObject;
 
 public class ChainPaletteEventHandler extends ModuleNodeEventHandler  {
 
+	/** the registry */
+	private Registry registry;
 	
-	public ChainPaletteEventHandler(ChainPaletteCanvas canvas) {
+	/** the last highlighted box..*/
+	private ChainBox lastHighlighted = null;
+	
+	public ChainPaletteEventHandler(ChainPaletteCanvas canvas,Registry registry) {
 		super(canvas);
+		this.registry = registry;
 	}	
+	
+	
+	public void mouseExited(PInputEvent e) {
+		PNode n = e.getPickedNode();
+		if (n instanceof ChainBox)
+			checkExit((ChainBox) n,e);
+		else 
+			super.mouseExited(e);
+		e.setHandled(true);
+	}
+	
+	private void checkExit(ChainBox n,PInputEvent e) { 
+		PBounds b = n.getBounds();
+		Point2D pickedPos = e.getPositionRelativeTo(n);
+		double pickX =  pickedPos.getX();
+		double pickY =  pickedPos.getY();
+		
+		// it's only really an exit if I've gone outside of the bounds
+		// of the chainbox. Otherwise, I'm over one of the chain box's components
+		//if (pickX < 0 || pickX > b.getWidth() || pickY < 0 || pickY > 
+		//			b.getHeight()) {
+		if (!b.contains(pickedPos)) {
+			super.mouseExited(e);
+		}
+	}
+	
+	public void mouseEntered(PInputEvent e) {
+		PNode n = e.getPickedNode();
+		System.err.println("entering..."+n);
+		super.mouseEntered(e);
+	}
 	
 	protected void setLastEntered(PNode node) {
 		super.setLastEntered(node);
@@ -89,6 +132,22 @@ public class ChainPaletteEventHandler extends ModuleNodeEventHandler  {
 			((ChainPaletteCanvas) canvas).setDraggingChain(chain.getChain());;
 		}
 		
+	}
+	
+	public void handleBackgroundClick() {
+		super.handleBackgroundClick();
+		SelectAnalysisChain event = new SelectAnalysisChain(null);
+		registry.getEventBus().post(event);
+	}
+	
+	public void setLastHighlighted(ChainBox box) {
+		if (lastHighlighted != null)
+			lastHighlighted.setHighlighted(false);
+		ChainView chain = null;
+		if (box != null)
+			chain = box.getChainView();
+		setLastEntered(chain);
+		lastHighlighted = box;
 	}
 }
 
