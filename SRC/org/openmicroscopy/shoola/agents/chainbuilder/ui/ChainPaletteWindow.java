b257fffd6118945efb513975007458dbf28af15c
hsh <hsh@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-07-15 20:13:56 +0000

    pass registry into chaipalettecanvas so we can handle shoola events
    git-svn-id: file:///home/svn/shoola-omero/trunk@1268 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/chainbuilder/ui/ChainPaletteWindow.java
+++ SRC/org/openmicroscopy/shoola/agents/chainbuilder/ui/ChainPaletteWindow.java
@@ -77,7 +77,7 @@ public class ChainPaletteWindow extends JFrame  {
 		Container content = getContentPane();
 		content.setLayout(new BorderLayout());
 		
-		chainCanvas = new ChainPaletteCanvas();
+		chainCanvas = new ChainPaletteCanvas(dataManager.getRegistry());
 		chainCanvas.setContents(dataManager.getChains());
 		chainCanvas.layoutContents();
 		chainCanvas.completeInitialization();
