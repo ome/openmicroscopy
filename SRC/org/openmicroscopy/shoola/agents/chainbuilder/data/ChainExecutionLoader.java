7f2eb81495a658d1ab0b3f2e7da3734fadbdde42
hsh <hsh@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-08-06 19:17:47 +0000

    remove reconciliation code. not readed
    git-svn-id: file:///home/svn/shoola-omero/trunk@1440 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/chainbuilder/data/ChainExecutionLoader.java
+++ SRC/org/openmicroscopy/shoola/agents/chainbuilder/data/ChainExecutionLoader.java
@@ -31,18 +31,11 @@ package org.openmicroscopy.shoola.agents.chainbuilder.data;
 
 //Java imports
 import java.util.Collection;
-import java.util.Iterator;
 
 //Third-party libraries
 
 //Application-internal dependencies
 import org.openmicroscopy.shoola.agents.chainbuilder.ChainDataManager;
-import org.openmicroscopy.shoola.agents.chainbuilder.data.layout.LayoutChainData;
-import org.openmicroscopy.shoola.agents.chainbuilder.data.layout.LayoutNodeData;
-import org.openmicroscopy.shoola.agents.zoombrowser.data.BrowserDatasetData;
-import org.openmicroscopy.shoola.env.data.model.ChainExecutionData;
-import org.openmicroscopy.shoola.env.data.model.ModuleExecutionData;
-import org.openmicroscopy.shoola.env.data.model.NodeExecutionData;
 import org.openmicroscopy.shoola.util.data.ContentGroup;
 import org.openmicroscopy.shoola.util.data.ContentLoader;
 
@@ -76,102 +69,6 @@ public class ChainExecutionLoader extends ContentLoader
 			chainExecutions = ((ChainDataManager) dataManager).getChainExecutions();
 		}
 		// reconcile
-		reconcileExecutions();
-	//	dumpExecutions();
 		return chainExecutions;
-	}
-	
-	private void reconcileExecutions() {
-		ChainExecutionData chainExecution;
-		BrowserDatasetData dataset;
-		LayoutChainData  chain;
-		int id;
-		Iterator iter = chainExecutions.iterator();
-		ChainDataManager chainDataManager = (ChainDataManager) dataManager;
-		
-		while (iter.hasNext()) {
-			chainExecution = (ChainExecutionData) iter.next();
-			chain = (LayoutChainData) chainExecution.getChain();
-			id = chain.getID();
-			chainExecution.setChain(chainDataManager.getChain(id));
-			dataset = (BrowserDatasetData) chainExecution.getDataset();
-			id = dataset.getID();
-			
-			/* if I've already seen this in the hash, then replace it with what
-			 * I saw before. If the hash doesn't have it, this means that the 
-			 * dataset actually belongs to a different user, so it wouldn't
-			 * be in the hash. Leave the dataset
-			 * that I retrieved with the execution 
-			 */
-			BrowserDatasetData otherDataset = chainDataManager.getDataset(id);
-			if (otherDataset != null)
-				chainExecution.setDataset(otherDataset);
-			reconcileNodeExecutions(chainExecution);
-		}
-	}
-	
-	public void reconcileNodeExecutions(ChainExecutionData chainExecution) {
-		Collection nodeExecs = chainExecution.getNodeExecutions();
-		if (nodeExecs == null || nodeExecs.size() == 0)
-			return;
-		NodeExecutionData ne;
-		Iterator iter = nodeExecs.iterator();
-		while (iter.hasNext()) {
-			ne = (NodeExecutionData) iter.next();
-			reconcileNodeExecution(ne);
-		}		
-	}
-	
-	public void reconcileNodeExecution(NodeExecutionData ne) {
-		LayoutNodeData n = (LayoutNodeData) ne.getAnalysisNode();
-		ChainDataManager chainDataManager = (ChainDataManager) dataManager;
-		int id = n.getID();
-		ne.setAnalysisNode(chainDataManager.getAnalysisNode(id));
-	}
-	
-	private void dumpExecutions() {
-		Iterator iter = chainExecutions.iterator();
-		ChainExecutionData exec;
-		while (iter.hasNext()) {
-			exec = (ChainExecutionData) iter.next();
-			if (exec.getID() == 1)
-				dumpExecution(exec);
-		}
-	}
-		
-	private void dumpExecution(ChainExecutionData exec) {
-		System.err.println("\n\nChain excution: "+exec.getID());
-		LayoutChainData chain = (LayoutChainData) exec.getChain();
-		System.err.println(" .. chain "+chain.getID()+", "+chain.getName());
-		
-		BrowserDatasetData ds = (BrowserDatasetData) exec.getDataset();
-		System.err.println(".. dataset "+ds.getID()+", "+ds.getName());
-		System.err.println(".. time: "+exec.getTimestamp());
-		dumpNodeExecutions(exec);
-	}
-	
-	private void dumpNodeExecutions(ChainExecutionData exec) {
-		Collection nodeExecs = exec.getNodeExecutions();
-		if (nodeExecs == null || nodeExecs.size() == 0)
-			return;
-		NodeExecutionData ne;
-		System.err.println("Nod executions...");
-		Iterator iter = nodeExecs.iterator();
-		while (iter.hasNext()) {
-			ne = (NodeExecutionData) iter.next();
-			dumpNodeExecution(ne);
-		}
-	}
-	
-	private void dumpNodeExecution(NodeExecutionData ne) {
-		System.err.println("node execution..."+ne.getID());
-		LayoutNodeData node = (LayoutNodeData) ne.getAnalysisNode();
-		System.err.println("Node is "+ node.getID());
-		ChainModuleData module = (ChainModuleData) node.getModule();
-		System.err.println(" .. module .."+module.getID()+", "+module.getName());
-		ModuleExecutionData mex = ne.getModuleExecution();
-		System.err.println(" .. mex id is "+mex.getID());
-		System.err.println(" .. mex status is "+mex.getStatus());
-		System.err.println("... mex time is "+mex.getTimestamp());
 	}
 }
