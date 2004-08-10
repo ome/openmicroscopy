225654467823c67890a467894fd58d9f1ffa0389
hsh <hsh@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-08-10 13:53:42 +0000

    event for when chain executions are selected
    git-svn-id: file:///home/svn/shoola-omero/trunk@1455 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/events/SelectChainExecutionEvent.java
@@ -0,0 +1,67 @@
+/*
+ * org.openmicroscopy.shoola.env.event.SelectChainExecutionEvent
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.events;
+
+//Java imports
+
+//Third-party libraries
+
+//Application-internal dependencies
+
+import org.openmicroscopy.shoola.env.data.model.ChainExecutionData;
+import org.openmicroscopy.shoola.env.event.AgentEvent;
+
+/** 
+ * 
+  * 
+ * An event indicating that the user has clicked on a chain execution
+ *
+ * @author  Harry Hochheiser &nbsp;&nbsp;&nbsp;&nbsp;
+ *              <a href="mailto:hsh@nih.gov">hsh@nih.gov</a>
+ * 
+ * @version 2.2 
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
+
+public class SelectChainExecutionEvent extends AgentEvent {
+	
+	   public SelectChainExecutionEvent(ChainExecutionData chainExecution) {
+   		super();
+   		setSource(chainExecution);
+   	}
+ 
+    
+    public ChainExecutionData getChainExecutions() {
+    		return (ChainExecutionData) getSource();
+    }
+}
