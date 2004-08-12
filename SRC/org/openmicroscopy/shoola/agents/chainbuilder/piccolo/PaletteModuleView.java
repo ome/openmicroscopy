6d352d10aa723d3feae07403f6be949267f40864
hsh <hsh@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-08-12 20:14:10 +0000

    specialization of c moduleiew for use on palette canvas
    git-svn-id: file:///home/svn/shoola-omero/trunk@1478 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/chainbuilder/piccolo/PaletteModuleView.java
@@ -0,0 +1,143 @@
+/*
+ * org.openmicroscopy.shoola.agents.chainbuilder.piccolo.PaletteModuleView;
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Harry Hochheiser <hsh@nih.gov>
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+package org.openmicroscopy.shoola.agents.chainbuilder.piccolo;
+
+
+//Java imports
+
+//Third-party libraries
+import edu.umd.cs.piccolo.PNode;
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.agents.chainbuilder.data.ChainFormalInputData;
+import org.openmicroscopy.shoola.agents.chainbuilder.data.ChainFormalOutputData;
+import org.openmicroscopy.shoola.agents.chainbuilder.data.ChainModuleData;
+import org.openmicroscopy.shoola.util.ui.piccolo.GenericEventHandler;
+
+
+
+/** 
+ * A Piccolo widget for an OME analysis  module. This widget will consist of a 
+ * rounded rectangle, which is a border. This node will have two children:
+ * a node with the name of the Module, and a second child which will itself
+ * have multiple children - one for each input and output of the module. These 
+ * children will be instances of FormalInput and FormalOutput (or appropriate
+ * subclasses thereof).  
+ *
+ * @author  Harry Hochheiser &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:hsh@nih.gov">hsh@nih.gov</a>
+ *
+ * @version 2.2
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ */
+
+
+
+public class PaletteModuleView extends ModuleView {
+	
+	public PaletteModuleView() {
+		super();
+	}
+	
+	
+	public PaletteModuleView(ChainModuleData module,float x,float y) {
+		super(module,x,y);
+	}
+	
+	 
+	/**
+	 * The main constructor 
+	 * @param module The OME Module being represented
+	 */
+	public PaletteModuleView(ChainModuleData module) {
+		super(module);
+	}
+	
+	
+	// pass in delegates to formal inputs and outs,
+	// in order to get my special purpose handling.
+	protected FormalInput getFormalInput(ChainFormalInputData paramIn) {
+		PaletteFormalParameterMouseDelegate delegate =
+			new PaletteFormalParameterMouseDelegate();
+		return new FormalInput(this,paramIn,delegate);
+	}
+	
+	protected FormalOutput getFormalOutput(ChainFormalOutputData paramOut) {
+		PaletteFormalParameterMouseDelegate delegate =
+			new PaletteFormalParameterMouseDelegate();
+		return new FormalOutput(this,paramOut,delegate);
+	}
+	
+	public void mouseEntered(GenericEventHandler handler) {
+		super.mouseEntered(handler);
+		ChainBox cb = getChainBoxParent();
+		if (cb != null)
+			cb.mouseEntered(handler);
+	}
+
+	public void mouseExited(GenericEventHandler handler) {
+		super.mouseEntered(handler);
+		ChainBox cb = getChainBoxParent();
+		if (cb != null)
+			cb.mouseExited(handler);
+	}
+	
+	private ChainBox getChainBoxParent() {
+		PNode p = getParent();
+		while (p != null) {
+			if (p instanceof ChainBox)
+				return (ChainBox) p;
+			p = p.getParent();
+		}
+		return null;
+	}
+	
+	protected LinkTarget getLinkTarget() {
+		LinkTarget link = super.getLinkTarget();
+		link.setPickable(false);
+		return link;
+	}
+}
+	
