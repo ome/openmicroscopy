d64934157ceefc2af54f25e1086d90b4be43b821
afalconi <afalconi@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-11-29 19:26:13 +0000

    Added init task for Command Processor.
    git-svn-id: file:///home/svn/shoola-omero/trunk@1935 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/init/Initializer.java
+++ SRC/org/openmicroscopy/shoola/env/init/Initializer.java
@@ -91,6 +91,7 @@ public class Initializer
         
         initList.add(LoggerInit.class);
         initList.add(EventBusInit.class);
+        initList.add(CmdProcessorInit.class);
         initList.add(DataServicesInit.class);
         initList.add(TaskBarInit.class);
         initList.add(UserNotifierInit.class);
