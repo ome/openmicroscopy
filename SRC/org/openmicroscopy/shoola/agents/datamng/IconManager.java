2f3abcfbca99506e316ce1119eaa50affd5e916f
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-12-20 17:43:16 +0000

    added new icons
    git-svn-id: file:///home/svn/shoola-omero/trunk@2054 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/datamng/IconManager.java
+++ SRC/org/openmicroscopy/shoola/agents/datamng/IconManager.java
@@ -156,11 +156,23 @@ public class IconManager
 	
     public static final int     SEND_TO_DB = 31;
     
+    public static final int     CREATE_CG = 32;
+    
+    public static final int     CREATE_CG_BIG = 33;
+    
+    public static final int     CATEGORY = 34;
+    
+    public static final int     CATEGORY_BIG = 35;
+    
+    public static final int     CATEGORY_GROUP = 36;
+    
+    public static final int     CATEGORY_GROUP_BIG = 37;
+    
 	/** 
 	 * The maximum ID used for the icon IDs.
 	 * Allows to correctly build arrays for direct indexing. 
 	 */
-	private static int          MAX_ID = 31;
+	private static int          MAX_ID = 37;
 	
 	/** Paths of the icon files. */
 	private static String[]     relPaths = new String[MAX_ID+1];
@@ -197,6 +209,12 @@ public class IconManager
 		relPaths[ORDER_BY_SELECTED_DOWN] = "endturn_with_arrow_down.png";
 		relPaths[FILTER] = "eclipse_filter_ps16.png";		
         relPaths[SEND_TO_DB] = "nuvola_package_games_board32.png";
+        relPaths[CREATE_CG] = "create_cg16.png";
+        relPaths[CREATE_CG_BIG] = "create_cg48.png";  
+        relPaths[CATEGORY_GROUP] = "category_group16.png";
+        relPaths[CATEGORY_GROUP_BIG] = "category_group48.png";
+        relPaths[CATEGORY] = "category16.png"; 
+        relPaths[CATEGORY_BIG] = "category48.png";
 	}
 	
 	/** The sole instance. */
