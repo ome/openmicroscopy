97be6e7d06074558fdf58064d38aafb0620fb360
hsh <hsh@6f2cb1de-eb0d-0410-b157-e593188b5901>
2004-12-30 20:11:32 +0000

    add instrumentation code. Change thumbnails to pngs (bigger, but faster). ImagePixels noreturns buffered image, so no cast necessary
    git-svn-id: file:///home/svn/shoola-omero/trunk@2091 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/zoombrowser/data/ThumbnailRetriever.java
+++ SRC/org/openmicroscopy/shoola/agents/zoombrowser/data/ThumbnailRetriever.java
@@ -30,7 +30,7 @@
 package org.openmicroscopy.shoola.agents.zoombrowser.data;
 
 //Java imports
-import java.awt.Image;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -67,12 +67,15 @@ public class ThumbnailRetriever {
 	/** the registry that we'll use */
 	private Registry registry;
 	
+	/** The Pixels Service */
+	private PixelsService ps;
 	/** path to cache */
 	String cachePath=null;
 	
 	public ThumbnailRetriever(Registry registry) {
 		this.registry = registry;
 		Environment env = (Environment) registry.lookup(LookupNames.ENV);
+		ps = registry.getPixelsService();
 		if (env != null) {
 			URL url = env.getOMEDSAddress();
 			String host = url.getHost();
@@ -94,33 +97,42 @@ public class ThumbnailRetriever {
 		
 		long omeisID = image.getDefaultPixels().getImageServerID();
 
-		String imageFileName = new String("thumb-"+omeisID+".jpg");
+		String imageFileName = new String("thumb-"+omeisID+".png");
 		File imageFile = new File(cachePath,imageFileName);
 		return imageFile;
 	}
 	
-	private Image getCachedImage(File imageFile) {
+	private BufferedImage getCachedImage(File imageFile) {
 				BufferedImage bufImage=null;
-		
-		if (imageFile.exists()) {
+		//if (imageFile.exists()) {
 			try {
 				bufImage = ImageIO.read(imageFile);
 			} catch(Exception e) {
+					// don't need to check to see if it exists - will throw 
+				     // an exception if it doesn't.
 					bufImage = null;
 			}
-		}
+		//}
 		return bufImage;
 	
 	}
 		
-	
-	public Image getImage(BrowserImageSummary image) {
+	long cacheElapsed=0;
+	long getFileElapsed = 0;
+	long isElapsed;
+	long cacheWriteElapsed;
+	int count;
+	public BufferedImage getImage(BrowserImageSummary image) {
 		
-		Image im=null;
-		
+		BufferedImage im=null;
+		long getFileStart = System.currentTimeMillis();
 		File imageFile = getImageFile(image);
+		getFileElapsed += System.currentTimeMillis()-getFileStart;
 		if (imageFile != null) {
+			long start = System.currentTimeMillis();
 			im = getCachedImage(imageFile);
+			cacheElapsed += System.currentTimeMillis()-start;
+			count++;
 			if (im != null) {
 				return im;
 			}
@@ -129,14 +141,18 @@ public class ThumbnailRetriever {
 		// else, not in cache
 		try {
 			Pixels pix = image.getDefaultPixels().getPixels();
-			PixelsService ps = registry.getPixelsService();
+			
+			long start = System.currentTimeMillis();
 			im = ps.getThumbnail(pix);
+			isElapsed += System.currentTimeMillis()-start;
+			count++;
 			
 			if (imageFile != null) {
 				// write to cache
-				BufferedImage bim = (BufferedImage) im;
-				if (bim != null)
-					ImageIO.write(bim,"jpg",imageFile);
+				start = System.currentTimeMillis();
+				if (im != null)
+					ImageIO.write(im,"png",imageFile);
+				cacheWriteElapsed += System.currentTimeMillis()-start;
 			}
 			return im;
 		}
@@ -150,6 +166,13 @@ public class ThumbnailRetriever {
 			un.notifyError("Thumbnail Cache Error","Can't cache thumbnail");
 			return im;
 		}
+	}
+	
+	public void dumpTime() {
+		System.err.println("get file time .."+getFileElapsed);
+		System.err.println("thumbnail cache read time .."+cacheElapsed);
+		System.err.println("image server time "+isElapsed);
+		System.err.println("cache write elapsed "+cacheWriteElapsed);
 	}
 	
 }
