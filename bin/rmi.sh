45a0e8005adc297615a96fc2036d354a4360b3ff
josh <josh@05709c45-44f0-0310-885b-81a1db45b4a6>
2005-02-19 23:25:57 +0000

    minor other changes
    git-svn-id: file:///home/svn/omero/trunk@28 05709c45-44f0-0310-885b-81a1db45b4a6

--- bin/rmi.sh
+++ bin/rmi.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-export BASE=/home/josh/workspace/OME-Generation
-export CLASSPATH=target
+export OME=/home/josh/code/ome3
+export BASE=$OME/components/jars/srv
+export CLASSPATH=$BASE/target/classes:$BASE/../model/target/classes
 
-rmic -d target org.ome.srv.net.rmi.RMIAdministrationFacade
-java  -Djava.rmi.server.codebase=file://$BASE/target/ -Djava.security.policy=$BASE/policy  \
-	-agentlib:hprof=cpu=samples,interval=20,depth=3 \
+rmic -d $BASE/target/classes \
+	org.ome.srv.net.rmi.RMIServiceFactory \
+	org.ome.srv.net.rmi.RMIAdministrationFacade \
+	org.ome.srv.net.rmi.RMIContainerFacade 
+
+java  \
+	-Djava.rmi.server.codebase="file://$BASE/target/classes/ file://$BASE/../model/target/ome3-model-1.0.jar" \
+	-Djava.security.policy=$OME/policy  \
 	org.ome.srv.net.rmi.RMIServer
 
 exit
 #=cpu=times,thread=y \
+#-agentlib:hprof=cpu=samples,interval=20,depth=3 \
