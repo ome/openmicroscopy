4f9a1efd0eb8117715ab437d631cd7168a21928f
afalconi <afalconi@6f2cb1de-eb0d-0410-b157-e593188b5901>
2005-04-20 12:34:34 +0000

    Refactored to inherit from HiLoader.
    git-svn-id: file:///home/svn/shoola-omero/trunk@2582 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/hiviewer/DatasetLoader.java
+++ SRC/org/openmicroscopy/shoola/agents/hiviewer/DatasetLoader.java
@@ -31,24 +31,13 @@ package org.openmicroscopy.shoola.agents.hiviewer;
 
 
 //Java imports
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
 
 //Third-party libraries
 
 //Application-internal dependencies
-import org.openmicroscopy.shoola.agents.hiviewer.browser.Browser;
-import org.openmicroscopy.shoola.agents.hiviewer.browser.ImageNode;
 import org.openmicroscopy.shoola.agents.hiviewer.view.HiViewer;
-import org.openmicroscopy.shoola.env.config.Registry;
-import org.openmicroscopy.shoola.env.data.events.DSCallAdapter;
-import org.openmicroscopy.shoola.env.data.events.DSCallFeedbackEvent;
 import org.openmicroscopy.shoola.env.data.model.DatasetSummaryLinked;
-import org.openmicroscopy.shoola.env.data.model.ImageSummary;
-import org.openmicroscopy.shoola.env.data.model.ThumbnailData;
 import org.openmicroscopy.shoola.env.data.views.HierarchyBrowsingView;
 
 /** 
@@ -66,105 +55,28 @@ import org.openmicroscopy.shoola.env.data.views.HierarchyBrowsingView;
  * @since OME2.2
  */
 class DatasetLoader
-    extends DSCallAdapter
+    extends HiLoader
 {
 
-    private HiViewerAgent   abstraction;
-    private HiViewer        view;
     private int             datasetID;
-    private boolean         loadingTree;
-    private Map             thumbProviders;
     
     
-    DatasetLoader(HiViewerAgent abstraction, HiViewer view, int datasetID)
+    protected void loadHierarchies(HierarchyBrowsingView hbw)
     {
-        this.abstraction = abstraction;
-        this.view = view;
-        this.datasetID = datasetID;
-        thumbProviders = new HashMap();
-    }
-    
-    void load()
-    {
-        loadingTree = true;
-        Registry reg = abstraction.getRegistry();
-        HierarchyBrowsingView hbw = (HierarchyBrowsingView)
-                        reg.getDataServicesView(HierarchyBrowsingView.class);
         hbw.loadHierarchy(DatasetSummaryLinked.class, datasetID, this);
     }
     
-    public void update(DSCallFeedbackEvent fe) 
+    protected Set getVisRootNodes(Object result)
     {
-        String status = fe.getStatus();
-        int percDone = fe.getPercentDone();
-        if (loadingTree) percDone = 5;  //We've got to load all thumbs yet.
-        if (status == null) 
-            status = (percDone == 100) ? "Done" :  //Else
-                                       ""; //Description wasn't available.   
-        view.setStatus(status, false, percDone);
-        if (!loadingTree) {  //We're loading tumbnails.
-            ThumbnailData td = (ThumbnailData) fe.getPartialResult();
-            if (td == null) return;  //last fe has null object.
-            ThumbnailProvider prv = (ThumbnailProvider) 
-                                        thumbProviders.get(
-                                                new Integer(td.getImageID()));
-            prv.setFullScaleThumb(td.getThumbnail());
-        }
+        DatasetSummaryLinked ds = (DatasetSummaryLinked) result; 
+        Set roots = HiTranslator.transform(ds);
+        return roots;
     }
     
-    public void onEnd()  //Called twice: 1 for tree, 2 for end of thumbs loadin.
+    DatasetLoader(HiViewerAgent abstraction, HiViewer view, int datasetID)
     {
-        if (!loadingTree)  //We're done this time!
-            view.setStatus(null, true, 0);
-    }
-    
-    //Called twice: 1 for tree, 2 for end of thumbs loadin.
-    public void handleResult(Object result) 
-    {
-        if (loadingTree) {  
-            
-            //Create browser so that we can start displaying the vis tree.
-            DatasetSummaryLinked ds = (DatasetSummaryLinked) result; 
-            Set topNodes = HiTranslator.transform(ds);
-            HiViewerUIF presentation = HiViewerUIF.getInstance(
-                                                    abstraction.getControl());
-            Browser brw = presentation.createBrowserFor(topNodes, view);
-            
-            //Done with tree, load thumbnails.  Build the thumbProviders map,
-            //so that we can then connect (see update) a thumb to an image node.
-            Set imgs = new HashSet();
-            Iterator i = brw.getImageNodes().iterator();
-            ImageNode node;
-            ImageSummary is;
-            while (i.hasNext()) {
-                node = (ImageNode) i.next();
-                is = (ImageSummary) node.getHierarchyObject();
-                imgs.add(is);
-                thumbProviders.put(new Integer(is.getID()), 
-                                   node.getThumbnail());
-            }
-            Registry reg = abstraction.getRegistry();
-            HierarchyBrowsingView hbw = (HierarchyBrowsingView)
-                           reg.getDataServicesView(HierarchyBrowsingView.class);
-            hbw.loadThumbnails(imgs, ThumbnailProvider.THUMB_MAX_WIDTH,
-                                ThumbnailProvider.THUMB_MAX_WIDTH, this);
-            
-            //After the first call we're loading thumbs.
-            loadingTree = false;
-        }
-    }
-    
-    public void handleNullResult() {}
-    
-    public void handleCancellation() {}
-    
-    public void handleException(Throwable exc) 
-    {
-        String s = "Can't browse the selected dataset: ";
-        Registry reg = abstraction.getRegistry();
-        reg.getLogger().error(abstraction, s+exc);
-        reg.getUserNotifier().notifyError("Data Retrieval Failure", s, exc);
-        view.closeViewer();
+        super(abstraction, view);
+        this.datasetID = datasetID;
     }
     
 }
--- SRC/org/openmicroscopy/shoola/agents/hiviewer/PDILoader.java
+++ SRC/org/openmicroscopy/shoola/agents/hiviewer/PDILoader.java
@@ -31,25 +31,12 @@ package org.openmicroscopy.shoola.agents.hiviewer;
 
 
 //Java imports
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 //Third-party libraries
 
 //Application-internal dependencies
-import org.openmicroscopy.shoola.agents.hiviewer.browser.Browser;
-import org.openmicroscopy.shoola.agents.hiviewer.browser.ImageNode;
 import org.openmicroscopy.shoola.agents.hiviewer.view.HiViewer;
-import org.openmicroscopy.shoola.env.config.Registry;
-import org.openmicroscopy.shoola.env.data.events.DSCallAdapter;
-import org.openmicroscopy.shoola.env.data.events.DSCallFeedbackEvent;
-import org.openmicroscopy.shoola.env.data.model.ImageSummary;
-import org.openmicroscopy.shoola.env.data.model.ThumbnailData;
 import org.openmicroscopy.shoola.env.data.views.HierarchyBrowsingView;
 
 /** 
@@ -67,105 +54,27 @@ import org.openmicroscopy.shoola.env.data.views.HierarchyBrowsingView;
  * @since OME2.2
  */
 class PDILoader
-    extends DSCallAdapter
+    extends HiLoader
 {
 
-    private HiViewerAgent   abstraction;
-    private HiViewer        view;
     private Set             images;
-    private boolean         loadingTree;
-    private Map             thumbProviders;
     
     
-    PDILoader(HiViewerAgent abstraction, HiViewer view, Set images)
+    protected void loadHierarchies(HierarchyBrowsingView hbw)
     {
-        this.abstraction = abstraction;
-        this.view = view;
-        this.images = images;
-        thumbProviders = new HashMap();
-    }
-    
-    void load()
-    {
-        loadingTree = true;
-        Registry reg = abstraction.getRegistry();
-        HierarchyBrowsingView hbw = (HierarchyBrowsingView)
-                        reg.getDataServicesView(HierarchyBrowsingView.class);
         hbw.findPDIHierarchies(images, this);
     }
     
-    public void update(DSCallFeedbackEvent fe) 
+    protected Set getVisRootNodes(Object result)
     {
-        String status = fe.getStatus();
-        int percDone = fe.getPercentDone();
-        if (loadingTree) percDone = 5;  //We've got to load all thumbs yet.
-        if (status == null) 
-            status = (percDone == 100) ? "Done" :  //Else
-                                       ""; //Description wasn't available.   
-        view.setStatus(status, false, percDone);
-        if (!loadingTree) {  //We're loading tumbnails.
-            ThumbnailData td = (ThumbnailData) fe.getPartialResult();
-            if (td == null) return;  //last fe has null object.
-            ThumbnailProvider prv = (ThumbnailProvider) 
-                                        thumbProviders.get(
-                                                new Integer(td.getImageID()));
-            prv.setFullScaleThumb(td.getThumbnail());
-        }
+        Set roots = HiTranslator.transformHierarchy((Set) result);
+        return roots;
     }
     
-    public void onEnd()  //Called twice: 1 for tree, 2 for end of thumbs loadin.
+    PDILoader(HiViewerAgent abstraction, HiViewer view, Set images)
     {
-        if (!loadingTree)  //We're done this time!
-            view.setStatus(null, true, 0);
-    }
-    
-    //Called twice: 1 for tree, 2 for end of thumbs loadin.
-    public void handleResult(Object result) 
-    {
-        if (loadingTree) {  
-            
-            //Create browser so that we can start displaying the vis tree.
-            List dataObjects = new ArrayList((Set) result); 
-            Set topNodes = HiTranslator.transformHierarchy(dataObjects);
-            HiViewerUIF presentation = HiViewerUIF.getInstance(
-                                                    abstraction.getControl());
-            Browser brw = presentation.createBrowserFor(topNodes, view);
-            
-            //Done with tree, load thumbnails.  Build the thumbProviders map,
-            //so that we can then connect (see update) a thumb to an image node.
-            Set imgs = new HashSet();
-            Iterator i = brw.getImageNodes().iterator();
-            ImageNode node;
-            ImageSummary is;
-            while (i.hasNext()) {
-                node = (ImageNode) i.next();
-                is = (ImageSummary) node.getHierarchyObject();
-                imgs.add(is);
-                thumbProviders.put(new Integer(is.getID()), 
-                                   node.getThumbnail());
-            }
-            Registry reg = abstraction.getRegistry();
-            HierarchyBrowsingView hbw = (HierarchyBrowsingView)
-                           reg.getDataServicesView(HierarchyBrowsingView.class);
-            hbw.loadThumbnails(imgs, ThumbnailProvider.THUMB_MAX_WIDTH,
-                                ThumbnailProvider.THUMB_MAX_WIDTH, this);
-            
-            //After the first call we're loading thumbs.
-            loadingTree = false;
-        }
-    }
-    
-    public void handleNullResult() {}
-    
-    public void handleCancellation() {}
-    
-    public void handleException(Throwable exc) 
-    {
-        String s = "Can't browse the selected hierarchy: ";
-        Registry reg = abstraction.getRegistry();
-        reg.getLogger().error(abstraction, s+exc);
-        reg.getUserNotifier().notifyError("Data Retrieval Failure", s, exc);
-        view.closeViewer();
+        super(abstraction, view);
+        this.images = images;
     }
     
 }
