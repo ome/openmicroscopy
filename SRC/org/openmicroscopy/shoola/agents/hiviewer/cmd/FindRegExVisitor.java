3e840223ad5b25290ecce81540a1da02a635c815
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2005-04-21 09:12:35 +0000

    initial commit
    git-svn-id: file:///home/svn/shoola-omero/trunk@2611 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/hiviewer/cmd/FindRegExVisitor.java
@@ -0,0 +1,103 @@
+/*
+ * org.openmicroscopy.shoola.agents.hiviewer.cmd.FindRegExVisitor
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.hiviewer.cmd;
+
+
+//Java imports
+import java.util.regex.Pattern;
+
+//Third-party libraries
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.agents.hiviewer.browser.ImageDisplayVisitor;
+import org.openmicroscopy.shoola.agents.hiviewer.browser.ImageNode;
+import org.openmicroscopy.shoola.agents.hiviewer.browser.ImageSet;
+
+/** 
+ * 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:a.falconi@dundee.ac.uk">
+ * 					a.falconi@dundee.ac.uk</a>
+ * @version 2.2
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
+public class FindRegExVisitor
+    implements ImageDisplayVisitor
+{
+
+    /** Find at the level of an image only. */
+    public static final int     IMAGE_LEVEL = 0;
+    
+    /** Find at the level of a container only. */
+    public static final int     CONTAINER_LEVEL = 1;
+    
+    /** Find at all levels. */
+    public static final int     IMAGE_AND_CONTAINER_LEVEL = 2;
+    
+    /** The pattern object created from the specified regular expression. */
+    protected Pattern   pattern;
+    
+    /** Level index. Must be one of the constant defined above. */
+    protected int       levelIndex;
+    
+    public FindRegExVisitor(String regEx, int index) {
+        
+        if (!checkIndex(index)) 
+            throw new IllegalArgumentException("index not valid");
+        levelIndex = index;
+        pattern = RegExFactory.createCaseInsensitivePattern(regEx);
+    }
+    
+    /** Check if the levelIndex is one of the constants defined above. */
+    private boolean checkIndex(int i)
+    {
+        boolean b = false;
+        switch (i) {
+            case IMAGE_LEVEL:
+            case CONTAINER_LEVEL:
+            case IMAGE_AND_CONTAINER_LEVEL:
+                b = true; break;
+        }
+        return b;
+    }
+    
+    /** Required by the I/F. */
+    public void visit(ImageNode node) {}
+
+    /** Required by the I/F. */
+    public void visit(ImageSet node) {}
+
+}
