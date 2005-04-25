3335afaeefdb09d9805946387cc89e413d0f88ee
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2005-04-25 10:38:55 +0000

    added new static methods used by TreeLayout and SquaryLayout
    git-svn-id: file:///home/svn/shoola-omero/trunk@2663 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/hiviewer/layout/LayoutFactory.java
+++ SRC/org/openmicroscopy/shoola/agents/hiviewer/layout/LayoutFactory.java
@@ -29,6 +29,15 @@
 
 package org.openmicroscopy.shoola.agents.hiviewer.layout;
 
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openmicroscopy.shoola.agents.hiviewer.browser.ImageDisplay;
+import org.openmicroscopy.shoola.agents.hiviewer.browser.ImageSet;
+
 
 //Java imports
 
@@ -59,6 +68,126 @@ public class LayoutFactory
     /** Identifies the <i>Tree</i> layout.*/
     public static final int     TREE_LAYOUT = 2;
     
+    
+    /**
+     * Returns the largest dimensions object among two given ones.
+     * That is, it returns the dimension object with the largest area.
+     *  
+     * @param a The first dimensions.
+     * @param b The second dimensions.
+     * @return The largest dimensions.
+     */
+    private static Dimension max(Dimension a, Dimension b)
+    {
+        int areaA = a.width*a.height, areaB = b.width*b.height;
+        if (areaA < areaB) return b;
+        return a;
+    }
+    
+    /**
+     * Finds out the dimensions of the largest child node in the specified
+     * {@link ImageSet}.
+     * 
+     * @param node The parent node.
+     * @return See above.
+     */
+    private static Dimension maxChildDim(ImageSet node)
+    {
+        Dimension maxDim = new Dimension(0, 0);
+        Iterator children = node.getChildrenDisplay().iterator();
+        ImageDisplay child;
+        while (children.hasNext()) {
+            child = (ImageDisplay) children.next();
+            maxDim = max(maxDim, child.getPreferredSize());
+        }
+        return maxDim;  //[0, 0] if no children.
+    }
+    
+    /** Return the child with the highest width. */
+    private static ImageDisplay getObjectbyWidth(Set set)
+    {
+        Iterator i = set.iterator();
+        double w = 0, maxW = 0;
+        ImageDisplay child, obj = null;
+        while (i.hasNext()) {
+            child = (ImageDisplay) i.next();
+            w = child.getPreferredSize().getWidth();
+            if (w > maxW) {
+                maxW = w;
+                obj = child;
+            }
+        }
+        return obj;
+    }
+    
+    /** 
+     * Order the children of a specified node by width (decreasing order). 
+     * 
+     * @param node  specified node.
+     * @return array of ImageDisplay.
+     * */
+    static Object[] orderedChildrenbyWidth(ImageSet node)
+    {
+        Set set = node.getChildrenDisplay();
+        //Create a modifiable set b/c set is "read-only".
+        Set newSet = new HashSet();
+        newSet.addAll(set);
+        int n = set.size();
+        Object[] children = new Object[n];
+        Object child;
+        for (int j = 0; j < n; j++) {
+            child = getObjectbyWidth(newSet);
+            newSet.remove(child);
+            children[j] = child;
+        }
+        return children;  
+    }
+    
+    /** Visit an ImageSet node that contains imageNode nodes. */
+    static void visitNodeWithLeaves(ImageSet node)
+    {
+        //First find out the max dim among children.
+        Dimension maxDim = maxChildDim(node);
+        
+        //Then figure out the number of columns, which is the same as the
+        //number of rows.
+        int n = node.getChildrenDisplay().size();        
+        if (n == 0) {   //node with no child
+            node.getInternalDesktop().setPreferredSize(
+                    node.getTitleBar().getMinimumSize());
+            node.setVisible(true);
+            return;
+        }
+        n = (int) Math.floor(Math.sqrt(n)) + 1;  //See note.
+        
+        //Finally do layout.
+        Dimension d;
+        Iterator children = node.getChildrenDisplay().iterator();
+        ImageDisplay child;
+        try {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (!children.hasNext()) //Done, less than n^2 children.
+                        return;  //Go to finally.
+                    child = (ImageDisplay) children.next();
+                    d = child.getPreferredSize();
+                    child.setBounds(j*maxDim.width, i*maxDim.height, d.width, 
+                                    d.height);
+                    child.setVisible(true);
+                }
+            }    
+        } finally {
+            Rectangle bounds = node.getContentsBounds();
+            d = bounds.getSize();
+            node.getInternalDesktop().setPreferredSize(d);
+            node.setVisible(true);
+        }
+    }
+    //NOTE: Let A be the function that calculates the area of a Dimension,
+    //sz the number of children, and r = sqr(sz).
+    //The required area for the layout mustn't be less than sz*A(maxDim).
+    //B/c: r < [r]+1  =>  sz=r^2 < ([r]+1)^2  
+    //Then: sz*A(maxDim) < [([r]+1)^2]*A(maxDim)
     
     /**
      * Creates the specified layout.
