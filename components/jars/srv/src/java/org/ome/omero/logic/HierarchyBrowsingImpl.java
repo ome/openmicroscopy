6d8bbe85138c14026191dd630946451e1dc730ee
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2005-04-29 18:45:44 +0000

    (Forgot to add HierarchyBrowsingImpl.java on last committ)
    git-svn-id: file:///home/svn/omero/branches/omero@50 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/jars/srv/src/java/org/ome/omero/logic/HierarchyBrowsingImpl.java
@@ -0,0 +1,91 @@
+/*
+ * Created on Feb 19, 2005
+ */
+package org.ome.omero.logic;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.ome.omero.interfaces.HierarchyBrowsing;
+
+import pojos.DataObject;
+
+/**
+ * @author josh
+ */
+public class HierarchyBrowsingImpl implements HierarchyBrowsing {
+
+    /* (non-Javadoc)
+     * @see org.ome.omero.interfaces.HierarchyBrowsing#loadPDIHierarchy(java.lang.Class, int)
+     */
+    public DataObject loadPDIHierarchy(Class arg0, int arg1) {
+        // TODO Auto-generated method stub
+        /* return null; */
+        throw new RuntimeException("implement me");
+    }
+
+    /* (non-Javadoc)
+     * @see org.ome.omero.interfaces.HierarchyBrowsing#loadCGCIHierarchy(java.lang.Class, int)
+     */
+    public DataObject loadCGCIHierarchy(Class arg0, int arg1) {
+        // TODO Auto-generated method stub
+        /* return null; */
+        throw new RuntimeException("implement me");
+    }
+
+    /* (non-Javadoc)
+     * @see org.ome.omero.interfaces.HierarchyBrowsing#findPDIHierarchies(java.util.Set)
+     */
+    public Set findPDIHierarchies(Set arg0) {
+        // TODO Auto-generated method stub
+        /* return null; */
+        throw new RuntimeException("implement me");
+    }
+
+    /* (non-Javadoc)
+     * @see org.ome.omero.interfaces.HierarchyBrowsing#findCGCIHierarchies(java.util.Set)
+     */
+    public Set findCGCIHierarchies(Set arg0) {
+        // TODO Auto-generated method stub
+        /* return null; */
+        throw new RuntimeException("implement me");
+    }
+
+    /* (non-Javadoc)
+     * @see org.ome.omero.interfaces.HierarchyBrowsing#findImageAnnotations(java.util.Set)
+     */
+    public Map findImageAnnotations(Set arg0) {
+        // TODO Auto-generated method stub
+        /* return null; */
+        throw new RuntimeException("implement me");
+    }
+
+    /* (non-Javadoc)
+     * @see org.ome.omero.interfaces.HierarchyBrowsing#findImageAnnotations(java.util.Set, int)
+     */
+    public Map findImageAnnotations(Set arg0, int arg1) {
+        // TODO Auto-generated method stub
+        /* return null; */
+        throw new RuntimeException("implement me");
+    }
+
+    /* (non-Javadoc)
+     * @see org.ome.omero.interfaces.HierarchyBrowsing#findDatasetAnnotations(java.util.Set)
+     */
+    public Map findDatasetAnnotations(Set arg0) {
+        // TODO Auto-generated method stub
+        /* return null; */
+        throw new RuntimeException("implement me");
+    }
+
+    /* (non-Javadoc)
+     * @see org.ome.omero.interfaces.HierarchyBrowsing#findDatasetAnnotations(java.util.Set, int)
+     */
+    public Map findDatasetAnnotations(Set arg0, int arg1) {
+        // TODO Auto-generated method stub
+        /* return null; */
+        throw new RuntimeException("implement me");
+    }
+
+	
+}
