5d08a92a86f661ed6843bcd4c68f930396e7eac3
afalconi <afalconi@6f2cb1de-eb0d-0410-b157-e593188b5901>
2005-05-04 19:43:24 +0000

    Initial commit.
    git-svn-id: file:///home/svn/shoola-omero/trunk@2736 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/hiviewer/tframe/TinyFrameModel.java
@@ -0,0 +1,396 @@
+/*
+ * org.openmicroscopy.shoola.agents.hiviewer.tframe.TinyFrameModel
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.hiviewer.tframe;
+
+
+//Java imports
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import javax.swing.JDesktopPane;
+
+//Third-party libraries
+
+//Application-internal dependencies
+
+/** 
+ * The Model sub-component in the {@link TinyFrame}'s MVC triad.
+ * This is a presentation Model which holds and manages presentation data
+ * in behalf of the {@link TinyFrame}, which has pass-through methods that
+ * delegate to this class, but manage the change notification process.  So
+ * all the other sub-components regard the {@link TinyFrame} as the Model.
+ * 
+ * <p><i>Note</i>: Not all presentation data is held by this class; for example
+ * the frame's title and icon are part of the <code>JInternalFrame</code> from
+ * which the {@link TinyFrame} inherits.  This is unavoidable because the 
+ * <code>JInternalFrame</code> doesn't have a separate Model from which we can
+ * inherit and, to keep things more manageable, we don't want to collapse the
+ * Model into the {@link TinyFrame}.</p> 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:a.falconi@dundee.ac.uk">
+ * 					a.falconi@dundee.ac.uk</a>
+ * @version 2.2
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
+class TinyFrameModel
+{
+    
+    /** The frame's content pane. */
+    private Container       contentPane;
+    
+    /** 
+     * Tells if the frame has to be highlighted.
+     * If <code>null</code>, the frame's title bar will display the normal
+     * background.  If a color is specified, the title bar will be highlighted
+     * using the specified color. 
+     */
+    private Color           highlight;
+    
+    /** Tells if the frame is expanded or collapsed. */
+    private boolean         collapsed;
+    
+    /** The size of the frame before the last collapse request. */
+    private Dimension       restoreSize;
+    
+    /** Identifies the title bar the frame is fitted with. */
+    private int             titleBarType;
+    
+    /** Substitutes the original content pane, all contents are added here. */
+    private JDesktopPane    desktopPane;
+    
+    /** Tells if the frame is in sinlge or multi-view mode. */
+    private boolean         singleViewMode;
+    
+    /** Replaces the {@link #desktopPane} when we're in single-view mode. */
+    private JDesktopPane    singleViewDesktop;
+    
+    /** 
+     * The bounds of the currently displayed child view relative to the
+     * {@link #desktopPane} in which it was originally contained, if we're
+     * in single-view mode or <code>null</code> when in multi-view mode.
+     * We save this object so that we can restore the original bounds when we
+     * add the component back to the internal desktop &#151; that is, after
+     * the user selects another child to display in the single-view desktop
+     * or when the user switches back to the multi-view mode.
+     */
+    private Rectangle       childViewBounds;
+    
+    /** 
+     * The type of the currently displayed child view's title bar, if we're in
+     * single-view mode.
+     * We save this value so that we can restore it when the child view is
+     * returned to the original {@link #desktopPane}.
+     */
+    private int             childViewTitleBarType;
+    
+    /** 
+     * The collapsed state of the the currently displayed child view, if we're
+     * in single-view mode.
+     * We save this value so that we can restore it when the child view is
+     * returned to the original {@link #desktopPane}.
+     */
+    private boolean         childViewCollapsed;
+    
+    
+    /**
+     * Checks that <code>c</code> is one of the components that were added
+     * to the internal desktop.
+     * 
+     * @param c The component to check.
+     * @return <code>true</code> if <code>c</code> is a child to the internal
+     *         desktop; <code>false</code> otherwise.
+     */
+    private boolean isChild(Component c)
+    {
+        if (c != null) {
+            //First check if it's a child to the desktopPane.
+            Component[] children = desktopPane.getComponents();
+            for (int i = 0; i < children.length; ++i) 
+                if (c == children[i]) return true;
+            
+            //We might be in single-view mode and c was removed from the
+            //desktopPane.  Check.
+            if (c == getChildView()) return true;   
+        }
+        return false;
+    }
+    
+    /**
+     * Utility method to pick the first child component out of a given
+     * container.
+     * 
+     * @param c The container.
+     * @return Returns the first component in <code>c</code> if <code>c</code>
+     *         is not <code>null</code> and has children; returns <code>null
+     *         </code> if <code>c</code> is <code>null</code> or has no
+     *         children.
+     */
+    private Component getFirstChild(Container c)
+    {
+        Component child = null;
+        if (c != null) {
+            Component[] comp = c.getComponents();
+            if (0 < comp.length) child = comp[0];
+        }
+        return child;
+    }
+    
+    /**
+     * Creates a new instance.
+     * After creation the model is not collapsed, the title bar is the 
+     * {@link TinyFrame#FULL_BAR}, and there's no highlight.
+     * However, the internal desktop is not installed yet and you need
+     * to call {@link #setSingleViewMode(boolean, TinyFrameUI)} passing
+     * <code>false</code> to complete intialization.  At which point, the
+     * model will be in multi-view mode and the internal desktop will be 
+     * installed.  
+     * 
+     * @param contentPane The frame's content pane.  
+     *                    Mustn't be <code>null</code>.
+     * @param restoreSize The initial frame's size.
+     *                    Mustn't be <code>null</code>.
+     * @param uiDelegate The frame's View.  We need it here to decorate the
+     *                   desktops.  Mustn't be <code>null</code>.
+     */
+    //TODO: The ugly call to setSingleViewMode() after creation is needed
+    //to avoid circular dependencies.  In fact, that call use to be in this
+    //constructor, but then b/c the uiDelegate is required we had to strip
+    //it out.  Reason? Simple, the uiDelegate needs to access the Model at
+    //creation time and the Model (TinyFrame) just forwards calls to this
+    //object, which doesn't exist yet.  Pleeeaze come up with a better
+    //solution!
+    TinyFrameModel(Container contentPane, Dimension restoreSize)
+    {
+        if (contentPane == null) 
+            throw new NullPointerException("No content pane.");
+        this.contentPane = contentPane;
+        setRestoreSize(restoreSize);
+        desktopPane = new JDesktopPane();
+        setTitleBarType(TinyFrame.FULL_BAR);
+    }
+    
+    /** Tells if we're in single or multi-view mode. */
+    boolean isSingleViewMode() { return singleViewMode; }
+    
+    /**
+     * Sets the frame's view mode.
+     *  
+     * @param singleViewMode <code>true</code> to switch to the single-view
+     *                       mode, <code>false</code> to return to the default
+     *                       multi-view mode.
+     * @param uiDelegate The frame's View.  We need it here to decorate the
+     *                   desktops.  Mustn't be <code>null</code>.
+     */
+    void setSingleViewMode(boolean singleViewMode, TinyFrameUI uiDelegate)
+    {
+        if (uiDelegate == null) 
+            throw new NullPointerException("No UI delegate.");
+        this.singleViewMode = singleViewMode;
+        if (singleViewMode) {  //Create the single-view tmp desktop.
+            singleViewDesktop = new JDesktopPane();
+            contentPane.removeAll();
+            contentPane.add(
+                    uiDelegate.decorateDesktopPane(singleViewDesktop),
+                    BorderLayout.CENTER);
+            
+            //Make the first child (if any) of the internal desktop the 
+            //current child view.
+            setChildView(getFirstChild(desktopPane));
+        } else {  //Normal multi-view mode, restore the internal desktop.
+            contentPane.removeAll();
+            contentPane.add(
+                    uiDelegate.decorateDesktopPane(desktopPane),
+                    BorderLayout.CENTER);
+            
+            //Return the current child view to the internal desktop.
+            setChildView(null);
+            singleViewDesktop = null;
+        }
+    }
+    
+    /**
+     * Returns the component that is showing in the single-view desktop when
+     * in single-view mode.
+     * The returned value will always be <code>null</code> if the frame is
+     * not in single-view mode.  However, it can also be <code>null</code>
+     * if the frame is in single-view mode but no components were added to
+     * the internal desktop.
+     * 
+     * @return The child view, that is the component that is showing in the
+     *         single-view desktop when in single-view mode.
+     */
+    Component getChildView() { return getFirstChild(singleViewDesktop); }
+    
+    /**
+     * Sets the given component in the single-view desktop.
+     * This method does nothing if the frame is not in single-view mode.
+     * Otherwise, the passed component is assumed to be a child of the 
+     * internal desktop or is <code>null</code> if the single-view desktop
+     * has to be cleared.
+     * 
+     * @param child The child component. 
+     */
+    void setChildView(Component child)
+    {
+        Component oldChild = getChildView();
+        if (oldChild != null) {  //Return it to the internal desktop.
+            singleViewDesktop.remove(oldChild);
+            desktopPane.add(oldChild);
+            oldChild.setBounds(childViewBounds);
+            if (oldChild instanceof TinyFrame) {
+                TinyFrame tf = (TinyFrame) oldChild;
+                tf.setTitleBarType(childViewTitleBarType);
+                tf.setCollapsed(childViewCollapsed);
+            }
+        }
+        if (isChild(child)) {  //We've got a new child to display.
+            desktopPane.remove(child);
+            singleViewDesktop.add(child);
+            childViewBounds = child.getBounds();
+            singleViewDesktop.setPreferredSize(
+                    new Dimension(childViewBounds.width, childViewBounds.height)
+                    );
+            child.setLocation(2, 2);
+            child.setSize(child.getPreferredSize());
+            
+            if (child instanceof TinyFrame) {
+                TinyFrame tf = (TinyFrame) child;
+                childViewTitleBarType = tf.getTitleBarType();
+                childViewCollapsed = tf.isCollapsed();
+                tf.setTitleBarType(TinyFrame.HEADER_BAR);
+                tf.setCollapsed(false);
+            }
+        }
+        contentPane.validate();
+        contentPane.repaint();
+    }
+    
+    /** 
+     * Returns the title bar type.
+     * 
+     * @return One of the constants defined by {@link TinyFrame}. 
+     */
+    int getTitleBarType() { return titleBarType; }
+    
+    /**
+     * Sets the title bar type.
+     * Does nothing if <code>type</code> is not a valid one.
+     * 
+     * @param type One of the constants defined by {@link TinyFrame}.
+     * @return <code>true</code> if <code>type</code> is a valid flag and so
+     *         it has been set; <code>false</code> if <code>type</code> is
+     *         not a valid flag and so it has been ignored.
+     */
+    boolean setTitleBarType(int type)
+    {
+        switch (type) {
+            case TinyFrame.FULL_BAR:
+            case TinyFrame.SMALL_BAR:
+            case TinyFrame.HEADER_BAR: 
+            case TinyFrame.STATIC_BAR:
+            case TinyFrame.NO_BAR:
+                titleBarType = type;
+                return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Returns the bounds of the area, within the internal desktop, which is
+     * occupied by the contained components.
+     * 
+     * @return See above.
+     */
+    Rectangle getContentsBounds()
+    {
+        Component[] comp = desktopPane.getComponents();
+        int x = 0, y = 0, //Top left corner coordinates.
+            br_x = 0, br_y = 0;  //Bottom right corner coordinates.
+        Rectangle bounds;
+        for (int i = 0; i < comp.length; ++i) {
+            bounds = comp[i].getBounds();
+            x = Math.min(x, bounds.x);
+            y = Math.min(y, bounds.y);
+            br_x = Math.max(br_x, bounds.x+bounds.width);
+            br_y = Math.max(br_y, bounds.y+bounds.height);
+        }
+        if (singleViewMode) {  
+            //Then one of the desktopPane's components could
+            //be in the tmp single-view desktop.
+            comp = singleViewDesktop.getComponents();
+            if (comp.length != 0) {
+                x = Math.min(x, childViewBounds.x);
+                y = Math.min(y, childViewBounds.y);
+                br_x = Math.max(br_x, childViewBounds.x+childViewBounds.width);
+                br_y = Math.max(br_y, childViewBounds.y+childViewBounds.height);
+            }
+        }
+        return new Rectangle(x, y, br_x-x, br_y-y);
+    }
+    
+    /** Returns the restore size. */
+    Dimension getRestoreSize() { return restoreSize; }
+    
+    /** 
+     * Sets the restore size.
+     * 
+     * @param rs The restore size.  Mustn't be <code>null</code>. 
+     */
+    void setRestoreSize(Dimension rs) 
+    { 
+        if (rs == null) throw new NullPointerException("No restore size.");
+        this.restoreSize = rs;
+    }
+    
+    /** Returns the frame's internal desktop. */
+    JDesktopPane getDesktopPane() { return desktopPane; }
+    
+    /** Returns the highlight property. */
+    Color getHighlight() { return highlight; }
+    
+    /** Sets the highlight property. */
+    void setHighlight(Color highlight) { this.highlight = highlight; }
+    
+    /** Returns the collapsed state. */
+    boolean isCollapsed() { return collapsed; }
+    
+    /** Sets the collapsed state. */
+    void setCollapsed(boolean collapsed) { this.collapsed = collapsed; }
+    
+}
---
+++ SRC/org/openmicroscopy/shoola/agents/hiviewer/tframe/ViewModeButton.java
@@ -0,0 +1,262 @@
+/*
+ * org.openmicroscopy.shoola.agents.hiviewer.tframe.ViewModeButton
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.hiviewer.tframe;
+
+
+//Java imports
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+
+//Third-party libraries
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.agents.hiviewer.IconManager;
+
+/** 
+ * The switch view mode button in the {@link TitleBar}.
+ * This is a small MVC component that is aggregated into the bigger MVC set
+ * of the {@link TinyFrame}.  The MVC parts of this button are all collapsed
+ * in this class.
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:a.falconi@dundee.ac.uk">
+ * 					a.falconi@dundee.ac.uk</a>
+ * @version 2.2
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
+class ViewModeButton
+    extends JButton
+    implements TinyFrameObserver, PropertyChangeListener, ActionListener
+{
+
+    /** Tooltip text when the button allows to switch to single-view mode. */
+    static final String SINGLE_VIEW_TOOLTIP = "Switch to single-view mode: "+
+                                              "view one item at a time.";
+    
+    /** 
+     * Text of the drop-down menu item that allows to switch back to 
+     * multi-view mode.
+     */
+    static final String MULTI_VIEW = "Multi-view Mode";
+    
+    /** Tooltip text when the button allows to display the drop-down menu. */
+    static final String DROP_DOWN_TOOLTIP = "List all contained items.";
+    
+    
+    /** The Model this button is working with. */
+    private TinyFrame   model;
+    
+    /**
+     * The drop-down menu that contains the children that can be displayed 
+     * along with a button to switch back to the multi-view mode.
+     * The items corresponding to the children in the Model are built every
+     * time the menu is requested &#151; this way we avoid having to sync with
+     * the Model if children are added/removed.  However, the item that allows
+     * to switch back to the multi-view mode is never replaced.
+     */
+    private JPopupMenu  dropDownMenu;
+    
+    
+    /**
+     * Creates the item to switch back to multi-view mode and adds it to 
+     * the {@link #dropDownMenu}.
+     * This item should always be the first item in the menu.
+     */
+    private void initDropDownMenu()
+    {
+        IconManager icons = IconManager.getInstance();
+        JMenuItem switchItem = new JMenuItem(MULTI_VIEW, 
+                                    icons.getIcon(IconManager.MULTI_VIEW_MODE));
+        dropDownMenu.add(switchItem);
+        switchItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae)
+            {
+                model.setSingleViewMode(false);
+            }
+        });
+    }
+    
+    /**
+     * Removes all components in the {@link #dropDownMenu} except the item
+     * that allows to switch back to the multi-view mode.
+     */
+    private void clearDropDownMenu()
+    {
+        int items = dropDownMenu.getComponentCount();
+        while (1 < items) dropDownMenu.remove(--items);
+    }
+    
+    /**
+     * Pops up the drop-down menu that contains the children that can be
+     * displayed along with a button to switch back to the multi-view mode.
+     */
+    private void displayMenu()
+    {
+        String title;
+        
+        //First off, get rid of all previous entries (if any) for desktop 
+        //children.
+        clearDropDownMenu();
+        
+        //Now make an item for the current child view and have it display
+        //a checkmark.
+        Component curView = model.getChildView();
+        if (curView != null) {
+            title = curView.getName();
+            if (curView instanceof TinyFrame) 
+                title = ((TinyFrame) curView).getTitle();
+            JCheckBoxMenuItem cvi = new JCheckBoxMenuItem(title);
+            cvi.setSelected(true);
+            dropDownMenu.add(cvi);
+        }
+        
+        //Then make an entry for every child in the inner desktop.
+        //Note that when in single-view mode, the inner desktop contains
+        //all its original children except the current child view which
+        //is showing in the tmp single-view desktop.
+        Component[] comp = model.getInternalDesktop().getComponents();
+        if (0 < comp.length) {
+            dropDownMenu.add(new JSeparator(SwingConstants.HORIZONTAL));
+            for (int i = 0; i < comp.length; ++i) {
+                final Component child = comp[i];
+                title = child.getName();
+                if (child instanceof TinyFrame) 
+                    title = ((TinyFrame) child).getTitle();
+                JMenuItem ci = new JMenuItem(title);
+                ci.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent ae)
+                    {
+                        model.setChildView(child);
+                    }
+                });
+                dropDownMenu.add(ci);
+            }
+        }
+        
+        //Finally bring the menu on screen.
+        Dimension d = getPreferredSize();
+        dropDownMenu.show(this, 0, d.height);
+    }
+    
+    /**
+     * Creates a new instance.
+     * 
+     * @param model The Model this button will be working with.
+     *              Mustn't be <code>null</code>.
+     */
+    ViewModeButton(TinyFrame model)
+    {
+        if (model == null) throw new NullPointerException("No model.");
+        this.model = model;
+        dropDownMenu = new JPopupMenu();
+        initDropDownMenu();
+        setBorder(BorderFactory.createEmptyBorder());  //No border around icon.
+        setMargin(new Insets(0,0,0,0));  //Just to make sure button sz=icon sz.
+        setOpaque(false);  //B/c button=icon.
+        setFocusPainted(false);  //Don't paint focus box on top of icon.
+        setRolloverEnabled(true);
+    }
+    
+    /**
+     * Registers this button with the Model.
+     * @see TinyFrameObserver#attach()
+     */
+    public void attach() 
+    { 
+        addActionListener(this);
+        model.addPropertyChangeListener(TinyFrame.SINGLE_VIEW_PROPERTY, this);
+        propertyChange(null);  //Synch button w/ current state.
+    }
+
+    /**
+     * Detaches this button from the Model's change notification registry.
+     * @see TinyFrameObserver#detach()
+     */
+    public void detach() 
+    { 
+        model.removePropertyChangeListener(TinyFrame.SINGLE_VIEW_PROPERTY, 
+                                           this); 
+    }
+    
+    /** Overridden to make sure no focus is painted on top of the icon. */
+    public boolean isFocusable() { return false; }
+    
+    /** Overridden to make sure no focus is painted on top of the icon. */
+    public void requestFocus() {}
+    
+    /**
+     * Sets the button appearence according to the single-view state of the 
+     * Model.
+     * @see PropertyChangeListener#propertyChange(PropertyChangeEvent)
+     */
+    public void propertyChange(PropertyChangeEvent pce)
+    {
+        //NOTE: We can only receive SINGLE_VIEW_PROPERTY changes, see attach().
+        IconManager icons = IconManager.getInstance();
+        if (model.isSingleViewMode()) {
+            setIcon(icons.getIcon(IconManager.VIEWS_LIST));
+            setRolloverIcon(icons.getIcon(IconManager.VIEWS_LIST_OVER));
+            setToolTipText(DROP_DOWN_TOOLTIP);
+        } else {
+            setIcon(icons.getIcon(IconManager.SINGLE_VIEW_MODE));
+            setRolloverIcon(icons.getIcon(IconManager.SINGLE_VIEW_MODE_OVER));
+            setToolTipText(SINGLE_VIEW_TOOLTIP);
+        }
+    }
+
+    /**
+     * Switches to single-view mode or displays the drop down menu if we're
+     * already in single-view mode.
+     * @see ActionListener#actionPerformed(ActionEvent)
+     */
+    public void actionPerformed(ActionEvent ae)
+    {
+        if (model.isSingleViewMode()) displayMenu();
+        else model.setSingleViewMode(true);
+    }
+    
+}
