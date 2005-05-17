0284f2f945e20bd315d3b44cb246efe86357e9a8
afalconi <afalconi@6f2cb1de-eb0d-0410-b157-e593188b5901>
2005-05-17 16:58:33 +0000

    Kept the class name, but completely changed the impl.
    git-svn-id: file:///home/svn/shoola-omero/trunk@2831 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/data/login/LoginManager.java
+++ SRC/org/openmicroscopy/shoola/env/data/login/LoginManager.java
@@ -35,19 +35,10 @@ package org.openmicroscopy.shoola.env.data.login;
 //Third-party libraries
 
 //Application-internal dependencies
-import org.openmicroscopy.shoola.env.Container;
-import org.openmicroscopy.shoola.env.config.Registry;
-import org.openmicroscopy.shoola.env.data.DSOutOfServiceException;
-import org.openmicroscopy.shoola.env.data.DataServicesFactory;
-import org.openmicroscopy.shoola.env.data.events.ServiceActivationRequest;
-import org.openmicroscopy.shoola.env.data.events.ServiceActivationResponse;
 import org.openmicroscopy.shoola.env.event.AgentEvent;
-import org.openmicroscopy.shoola.env.event.AgentEventListener;
-import org.openmicroscopy.shoola.env.ui.LoginOMEDS;
-import org.openmicroscopy.shoola.util.ui.UIUtilities;
 
 /** 
- * 
+ * Decorates a {@link LoginService} to add thread-safety.
  *
  * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
  * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
@@ -61,64 +52,75 @@ import org.openmicroscopy.shoola.util.ui.UIUtilities;
  * @since OME2.2
  */
 public class LoginManager
-	implements AgentEventListener
+    implements LoginService
 {
-	
-	private static LoginManager		singleton;
-	
-	//NB: this can't be called outside of container b/c agents have no refs
-	//to the singleton container. So we can be sure this method is going to
-	//create services just once.
-	public static LoginManager getInstance(Container c)
-	{
-		if (c == null)
-			throw new NullPointerException();  //An agent called this method?
-		if (singleton == null)	singleton = new LoginManager(c);
-		return singleton;
-	}
 
-	private Container			c;
-	
-	private LoginManager(Container c)
-	{
-		this.c = c;
-	}
-	
-	private void handleSARequest(ServiceActivationRequest request) 
-	{
-		//We can pop up the dialog without problems b/c run within 
-		//the Swing thread
-		Registry registry = c.getRegistry();
-		try {
-			DataServicesFactory dsf = DataServicesFactory.getInstance(c);
-			LoginOMEDS editor = new LoginOMEDS(registry, dsf);
-			UIUtilities.centerAndShow(editor);
-			boolean result = editor.getManager().isActivationSuccessful();
-			ServiceActivationResponse 
-			response = new ServiceActivationResponse(request, result);
-			registry.getEventBus().post(response);
-		} catch (DSOutOfServiceException e) {
-			String s = "Can't connect to OMEDS. Try later.";
-			registry.getUserNotifier().notifyError("Connexion failure", s, e);
-			registry.getLogger().error(this, s+" Error: "+e);
-		}
-	}
-	
-	public void activate()
-	{
-		Registry registry = c.getRegistry();
-		registry.getEventBus().register(this, ServiceActivationRequest.class);
-	}
-	
-	public void terminate()
-	{
-		//TODO: implement.
-	}
+    /** The service's logic. */
+    private LoginService    service;
+    
+    
+    /**
+     * Creates a new instance to decorate the given Login Service in order to
+     * obtain a thread-safe service.
+     * 
+     * @param service The actual Login Service.  Mustn't be <code>null</code>.
+     */
+    public LoginManager(LoginService service)
+    {
+        if (service == null) throw new NullPointerException("No service.");
+        this.service = service;
+    }
+    
+    /**
+     * Implemented as specified by the {@link LoginService} interface.
+     * @see LoginService#getState()
+     */
+    public synchronized int getState() { return service.getState(); }
 
-	public void eventFired(AgentEvent e) 
-	{
-		//We have only registered for this event.
-		handleSARequest((ServiceActivationRequest) e);
-	}
+    /**
+     * Implemented as specified by the {@link LoginService} interface.
+     * @see LoginService#login()
+     */
+    public synchronized void login()
+    {
+        if (service.getState() == IDLE) service.login();
+        //Else another thread has already hit this method, the Login Service
+        //has attempted to connect but failed and brought up the Login dialog.
+        //So it's now waiting for the user to enter their credentials.  The
+        //Login dialog will call login(UserCredentials) when the credentials
+        //have been entered.  So this thread can just return because an attempt
+        //is already ongoing.
+        //Note that we can assert this because *both* login methods acquire the
+        //same lock and the login(UserCredentials) always leaves the state to
+        //IDLE upon return.
+    }
+
+    /**
+     * Implemented as specified by the {@link LoginService} interface.
+     * @see LoginService#login(UserCredentials)
+     */
+    public synchronized boolean login(UserCredentials uc) 
+    { 
+        return service.login(uc); 
+    }
+
+    /**
+     * Implemented as specified by the {@link LoginService} interface.
+     * @see LoginService#eventFired(AgentEvent)
+     */
+    public synchronized void eventFired(AgentEvent serviceActivationRequest)
+    {
+        service.eventFired(serviceActivationRequest);
+    }
+    //NOTE: We do need to acquire the lock here, even though this method is
+    //only called within the UI thread.  The reason is that we need to make
+    //sure that working memories are flushed -- this is a side-effect of
+    //acquiring the lock.  In fact, if another thread has called login() and
+    //the attempt was successful, then the connected field in the OMEDSGateway
+    //will be set to true.  Later on when the UI thread calls this method, we
+    //want to be sure that the connected field has been flushed and so is
+    //visible to the UI thread.  In fact, the implementation of eventFired()
+    //queries (indirectly) the connected field to find out if there's a valid
+    //link to OMEDS.
 	
 }
