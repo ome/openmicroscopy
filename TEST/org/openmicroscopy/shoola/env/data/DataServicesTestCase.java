b9573461b66afb01630b000bf6c56d41206470fa
afalconi <afalconi@6f2cb1de-eb0d-0410-b157-e593188b5901>
2005-05-18 19:59:04 +0000

    Intial commit.
    git-svn-id: file:///home/svn/shoola-omero/trunk@2844 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ TEST/org/openmicroscopy/shoola/env/data/DataServicesTestCase.java
@@ -0,0 +1,111 @@
+/*
+ * org.openmicroscopy.shoola.env.data.DataServicesTestCase
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.env.data;
+
+
+//Java imports
+
+//Third-party libraries
+import junit.framework.TestCase;
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.env.config.Registry;
+
+/** 
+ * Ancestor of all Data Services test cases.
+ * A Data Service test case verifies the operation of any of the Data Services
+ * made available by the Container: Data Management Service, Semantic Types
+ * Service, Pixels Service, and Data Services Views. 
+ * <p>This class conveniently inherits from <code>TestCase</code> so that a 
+ * Data Services test case can be run by JUnit.  The JUnit's hook method 
+ * {@link #setUp() setUp} is overridden to factor out the functionality common
+ * to all test cases &#151; so sublcasses should first call <code>super.setUp()
+ * </code> if they override this method too.</p>
+ * <p>Altough you write a Data Services test case as a regular JUnit test case,
+ * there are several things to keep in mind, regarding the test environment:</p>
+ * <ul>
+ *  <li><b>OMEDS</b>.  A live instance of <i>OMEDS</i> is needed by the 
+ *   underlying Data Services.  So a Data Services test case depends on
+ *   <i>external</i> resources.</li>
+ *  <li><b>OMEDS Connnection</b>.  The connection to the server is managed
+ *   transparently by the Login Service, which operates in test mode &#151;
+ *   see {@link org.openmicroscopy.shoola.env.data.login.BatchLoginService}.
+ *   (So no UI is ever brought on screen to ask for user's credentials, which
+ *   are specified through system properties &#151; along with the server's
+ *   address, see {@link Env}.)  The <i>connection</i> is used for all tests  
+ *   in a session.  Note that if a test fails because of an invalid link to 
+ *   <i>OMEDS</i>, the Login Service will try reestablishing a valid link under
+ *   the hood and so the next test may run just fine.  However, if a vaild link
+ *   can't be reestablished (for example, a permanent network failure) then all
+ *   subsequent tests will obviously fail.</li>
+ *  <li><b>Container</b>.  It's started in test mode and the only available
+ *   services will be the Data Services.  (All other services are replaced
+ *   with no-op services.)  In particular, no UI is linked to the Container and
+ *   thus no Agents either.  The Container instance is static and spans an 
+ *   entire test session &#151; which, in most cases, is the same as the JVM
+ *   process life-time.  You can access the Container's registry directly by
+ *   using the protected field made available by this class.</li>
+ *  <li><b>Threading</b>.  The Container and all its services are run in a 
+ *   single thread, which is the same as the one in which JUnit is run.
+ *   (Even the Data Services Views use this thread.)  So a Data Services test
+ *   case is single-threaded &#151; unless you explicitly spawn other threads,
+ *   which we reccommend you <i>don't</i> do.</li>
+ * </ul>
+ * <p>Finally some guidelines to run Data Services test cases.  First off, you
+ * should set up a test <i>OMEDS</i> instance, which in turn runs on a test DB.
+ * This is the server that you're then going to use for the Data Services tests.
+ * Second, run all the Data Services tests together in a separate JVM process.
+ * This is a good idea because of the dependency on external resources and
+ * the singleton instance of the Container (and its services) which is shared
+ * among all tests in a session.</p>
+ * 
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:a.falconi@dundee.ac.uk">
+ * 					a.falconi@dundee.ac.uk</a>
+ * @version 2.2
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
+public abstract class DataServicesTestCase
+    extends TestCase
+{
+
+    /**
+     * Reference to the Container's registry.
+     * Subclasses mainly use this to get references to the various
+     * Data Services. 
+     */
+    protected Registry      registry;
+    
+}
