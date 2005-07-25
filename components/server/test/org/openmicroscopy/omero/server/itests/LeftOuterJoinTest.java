5cac399f6b14143f58b1663e22acd96d0650fee6
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2005-07-25 19:03:47 +0000

    * Undoing changes to LeftOuterJoin in r158.
    git-svn-id: file:///home/svn/omero/trunk@162 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/server/test/org/openmicroscopy/omero/server/itests/LeftOuterJoinTest.java
+++ components/server/test/org/openmicroscopy/omero/server/itests/LeftOuterJoinTest.java
@@ -1,5 +1,5 @@
 /*
- * org.openmicroscopy.omero.server.itests.JmsTest
+ * org.openmicroscopy.omero.server.itests.LeftOuterJoinTest
  *
  *------------------------------------------------------------------------------
  *
@@ -29,16 +29,45 @@
 package org.openmicroscopy.omero.server.itests;
 
 //Java imports
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 //Third-party libraries
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.jms.core.JmsTemplate;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.jmock.Mock;
+import org.jmock.core.constraint.IsSame;
+import org.jmock.core.matcher.InvokeOnceMatcher;
+import org.jmock.core.stub.ReturnStub;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
 //Application-internal dependencies
+import org.openmicroscopy.omero.OMEModel;
+import org.openmicroscopy.omero.interfaces.HierarchyBrowsing;
+import org.openmicroscopy.omero.logic.ContainerDao;
+import org.openmicroscopy.omero.logic.DaoCleanUpHibernate;
+import org.openmicroscopy.omero.logic.HierarchyBrowsingImpl;
+import org.openmicroscopy.omero.logic.ReturnLogger;
+import org.openmicroscopy.omero.model.Category;
+import org.openmicroscopy.omero.model.CategoryGroup;
+import org.openmicroscopy.omero.model.Classification;
+import org.openmicroscopy.omero.model.Dataset;
+import org.openmicroscopy.omero.model.Image;
+import org.openmicroscopy.omero.model.ImageAnnotation;
+import org.openmicroscopy.omero.model.Project;
+import org.openmicroscopy.omero.tests.OMEData;
+import org.openmicroscopy.omero.util.Utils;
 
-
+import sun.security.krb5.internal.i;
 
 /** 
  * tests for a HQL join bug.
@@ -55,17 +84,14 @@ public class LeftOuterJoinTest
         extends
             AbstractDependencyInjectionSpringContextTests {
 
-    private static Log log = LogFactory.getLog(JmsTest.class);
-    JmsTemplate jt;
+    private static Log log = LogFactory.getLog(LeftOuterJoinTest.class);
+    HierarchyBrowsing hb;
 
-    public void setJmsTemplate(JmsTemplate template){
-    	this.jt=template;
-    }
-    
     /**
      * @see org.springframework.test.AbstractDependencyInjectionSpringContextTests#onSetUp()
      */
     protected void onSetUp() throws Exception {
+        hb = (HierarchyBrowsing) applicationContext.getBean("hierarchyBrowsingService");
     }
     
     /**
@@ -74,10 +100,69 @@ public class LeftOuterJoinTest
     protected String[] getConfigLocations() {
 
         return new String[] { 
-                "WEB-INF/jms.xml"
-        };
+                "WEB-INF/services.xml",
+                "WEB-INF/dao.xml",
+                "WEB-INF/data.xml", 
+                "WEB-INF/test/config-test.xml",
+                "WEB-INF/test/test.xml"};
     }
 
-    public void testA(){
+    public void testNewCleaningMethods(){
+        Object o = hb.loadPDIHierarchy(Project.class,63);
+        Utils.structureSize(o);
     }
+    
+    public void testImageThumbnailExplodsOnHessianSerialization() {
+        Set imgIds = new HashSet();
+        imgIds.add(new Integer(1191));
+        imgIds.add(new Integer(4665));
+        imgIds.add(new Integer(1304));
+        imgIds.add(new Integer(4977));
+        imgIds.add(new Integer(3540));
+        imgIds.add(new Integer(2064));
+        Set result = hb.findPDIHierarchies(imgIds);
+        Set test = Utils.getImagesinPDI(result);
+        assertTrue("Images in should eq. images out",imgIds.size()==test.size());
+    }
+
+    public void testDuplicateImages() {
+        OMEData data = (OMEData) applicationContext.getBean("data");
+        Set result = hb.findPDIHierarchies(data.imgsPDI);
+        Set test = Utils.getImagesinPDI(result);
+        assertTrue("Images in should eq. images out",data.imgsPDI.size()==test.size());
+        
+        Set noDupesPlease = new HashSet(); 
+        for (Iterator i = test.iterator(); i.hasNext();) {
+            Image img = (Image) i.next();
+            if (noDupesPlease.contains(img.getImageId())) 
+                fail("But also the IDs should be unique!");
+          	noDupesPlease.add(img.getImageId());
+        }
+        
+    }
+
+    public void testWhereAreTheImageAnnsLoadCGCI(){
+    	Category c =(Category) hb.loadCGCIAnnotatedHierarchy(Category.class,250,1);
+    	Set<Classification> clas = c.getClassifications();
+    	boolean annsThere = false;
+    	for (Classification cla : clas){
+    		Image img = cla.getImage();
+    		Set<ImageAnnotation> anns = img.getImageAnnotations();
+    		log.info("Annotations for image "+img.getImageId()+":"+anns);
+    		if (anns !=null && anns.size() > 0) annsThere = true;
+    	}
+    	assertTrue("There should be an annotation",annsThere);
+    }
+    
+    public void testAndNowAFrigginStackOverFLow() throws Throwable{
+    	Set<OMEModel> set = hb.findPDIAnnotatedHierarchies(TestUtils.getSetFromInt(new int[]{2,3}),1);
+    	log.info(set);
+    	// Already applied below:
+//    	DaoCleanUpHibernate clean = (DaoCleanUpHibernate)this.applicationContext.getBean("daoCleanUp");
+//    	ReturnLogger log = (ReturnLogger) this.applicationContext.getBean("logging");
+//    	clean.clean(set);
+//    	log.log(set);
+    	
+    }
+    
 }
