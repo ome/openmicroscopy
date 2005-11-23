0bc035fad91fa0a79bd615ce9b8c059d286ad99c
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2005-11-23 15:34:39 +0000

    added new constant
    git-svn-id: file:///home/svn/shoola-omero/trunk@2975 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/LookupNames.java
+++ SRC/org/openmicroscopy/shoola/env/LookupNames.java
@@ -56,6 +56,8 @@ public class LookupNames
 
 	public static final  String ENV = "/env";
 	
+    public static final String USER_CREDENTIALS = "/user/credentials";
+    
 	public static final String USER_DETAILS = "/user/details";
     
 	public static final String AGENTS = "/agents";
