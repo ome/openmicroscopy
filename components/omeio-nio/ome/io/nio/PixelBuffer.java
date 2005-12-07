b352ca037471b58986f74ab771644536fdca3dd5
callan <callan@05709c45-44f0-0310-885b-81a1db45b4a6>
2005-12-07 09:49:02 +0000

    * Add banners and author Javadoc * Fixed "long" promotion assumptions * Added initial region setting
    git-svn-id: file:///home/svn/omero/trunk@267 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/omeio-nio/ome/io/nio/PixelBuffer.java
+++ components/omeio-nio/ome/io/nio/PixelBuffer.java
@@ -1,14 +1,47 @@
+/*
+ * ome.io.nio.PixelBuffer
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2005 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
 package ome.io.nio;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
 
 
+/**
+ * @author callan
+ *
+ */
 public class PixelBuffer
 {
     Integer id;
@@ -87,8 +120,8 @@ public class PixelBuffer
         Integer stackSize = getStackSize();
         Integer planeSize = getPlaneSize();
         
-        return new Long((timepointSize * t) + (stackSize * c) + 
-                        (planeSize * z));
+        return ((long)timepointSize * t) + ((long)stackSize * c) + 
+               ((long)planeSize * z);
     }
     
     private Long getStackOffset(Integer c, Integer t)
@@ -96,14 +129,14 @@ public class PixelBuffer
         Integer timepointSize = getTimepointSize();
         Integer stackSize = getStackSize();
         
-        return new Long((timepointSize * t) + (stackSize * c));
+        return ((long) timepointSize * t) + ((long) stackSize * c);
     }
     
     private Long getTimepointOffset(Integer t)
     {
         Integer timepointSize = getTimepointSize();
         
-        return new Long((timepointSize * t));
+        return ((long) timepointSize * t);
     }
     
     public MappedByteBuffer getRegion(Integer size, Long offset)
@@ -142,5 +175,27 @@ public class PixelBuffer
         Integer size = getTimepointSize();
         
         return getRegion(size, offset);
+    }
+    
+    public void setRegion(Integer size, Long offset, byte[] buffer)
+        throws IOException
+    {
+        FileChannel fileChannel = getFileChannel(false);
+        
+        /* fileChannel should not be "null" as it will throw an exception if
+         * there happens to be an error.
+         */
+        
+        MappedByteBuffer byteBuffer =
+            fileChannel.map(MapMode.READ_ONLY, size, offset);
+        
+        byteBuffer.put(buffer);
+        byteBuffer.force();
+    }
+    
+    public void setRegion(Integer size, Long offset, ByteBuffer buffer)
+        throws IOException
+    {
+        setRegion(size, offset, buffer.array());
     }
 }
