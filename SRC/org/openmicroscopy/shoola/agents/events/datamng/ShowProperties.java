52dbf70ec9f7daf3cfcfbb73c70d986baed25754
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-01-05 15:58:20 +0000

    Added new constructor
    git-svn-id: file:///home/svn/shoola-omero/trunk@3007 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/events/datamng/ShowProperties.java
+++ SRC/org/openmicroscopy/shoola/agents/events/datamng/ShowProperties.java
@@ -43,62 +43,111 @@ import org.openmicroscopy.shoola.env.event.RequestEvent;
 import pojos.ImageData;
 import pojos.PixelsData;
 
-/** 
-* 
-*
-* @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
-*               <a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
-* @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
-*               <a href="mailto:a.falconi@dundee.ac.uk">
-*                   a.falconi@dundee.ac.uk</a>
-* @version 2.2
-* <small>
-* (<b>Internal version:</b> $Revision$ $Date$)
-* </small>
-* @since OME2.2
-*/
+/**
+ * Request to edit the specified <code>DataObject</code>.
+ * This event should be caught by agents managing the users' data.
+ * 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author  <br>Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ *               <a href="mailto:a.falconi@dundee.ac.uk">
+ *                   a.falconi@dundee.ac.uk</a>
+ * @version 2.2
+ * <small>
+ * (<b>Internal version:</b> $Revision$ $Date$)
+ * </small>
+ * @since OME2.2
+ */
 public class ShowProperties
   extends RequestEvent
 {
+    
+    /** Indicates that the request is for a <code>CREATE</code> editor. */
+    public static final int CREATE = 0;
+    
+    /** Indicates that the request is for a <code>EDIT</code> editor. */
+    public static final int EDIT = 1;
+    
+    //TO BE REMOVED
+    private Component   parent;
+      
+    /** The <code>DataObject</code> to edit. */
+    private pojos.DataObject    userObject;
+    
+    /** One of the constants defined by this class. */
+    private int                 editorType;
+      
+    /**
+     * Checks if the type specified is one of the constants defined 
+     * by this class.
+     * 
+     * @param type The type to check.
+     */
+    private void checkEditorType(int type)
+    {
+        switch (type) {
+            case CREATE:
+            case EDIT:    
+                return;
+            default:
+                throw new IllegalArgumentException("Editor type not supported");
+        }
+    }
+    
+    //TO REMOVE ASAP
+    public ShowProperties(DataObject object) 
+    {
+        if (object instanceof ImageSummary) {
+            ImageSummary is = (ImageSummary) object;
+            ImageData data = new ImageData();
+            data.setId(is.getID());
+            data.setName(is.getName());
+            data.setCreated(is.getDate());
+            PixelsDescription pix = is.getDefaultPixels();
+            PixelsData pixData = new PixelsData();
+            pixData.setId(pix.getID());
+            pixData.setImageServerID(pix.getImageServerID());
+            pixData.setImageServerURL(pix.getImageServerUrl());
+            data.setDefaultPixels(pixData);
+            userObject = data;
+        } else
+            throw new IllegalArgumentException("Dataobject not valid.");
+      
+        parent = null;
+    }
   
-      private Component   parent;
+    
+    public ShowProperties(pojos.DataObject userObject, int type) 
+    {
+        if (userObject == null)
+            throw new IllegalArgumentException("Object to edit cannot be null");
+        checkEditorType(type);
+        this.userObject = userObject;
+        editorType = type;
+        parent = null;
+    }
       
-      private pojos.DataObject  userObject;
+    public ShowProperties(pojos.DataObject userObject, Component parent) 
+    {
+        this.userObject = userObject;
+        this.parent = parent;
+    }
       
-      public ShowProperties(DataObject object) 
-      {
-          if (object instanceof ImageSummary) {
-              ImageSummary is = (ImageSummary) object;
-              ImageData data = new ImageData();
-              data.setId(is.getID());
-              data.setName(is.getName());
-              data.setCreated(is.getDate());
-              PixelsDescription pix = is.getDefaultPixels();
-              PixelsData pixData = new PixelsData();
-              pixData.setId(pix.getID());
-              pixData.setImageServerID(pix.getImageServerID());
-              pixData.setImageServerURL(pix.getImageServerUrl());
-              data.setDefaultPixels(pixData);
-              userObject = data;
-          } else
-              throw new IllegalArgumentException("Dataobject not valid.");
-          
-          parent = null;
-      }
-      public ShowProperties(pojos.DataObject userObject) 
-      {
-          this.userObject = userObject;
-          parent = null;
-      }
+    public Component getParent() { return parent; }
       
-      public ShowProperties(pojos.DataObject userObject, Component parent) 
-      {
-          this.userObject = userObject;
-          this.parent = parent;
-      }
-      
-      public Component getParent() { return parent; }
-      
-      public pojos.DataObject getUserObject() { return userObject; }
+    /**
+     * Returns the <code>DataObject</code> this request is for.
+     * 
+     * @return See above.
+     */
+    public pojos.DataObject getUserObject() { return userObject; }
+    
+    /**
+     * Returns the type of editor requested.
+     * 
+     * @return See above.
+     */
+    public int getEditorType() { return editorType; }
   
 }
