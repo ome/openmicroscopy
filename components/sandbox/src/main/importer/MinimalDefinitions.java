e99a726db8505ff3b798bedd2f2b105921f4aa05
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2006-01-10 10:29:39 +0000

    [Examples] Added RenderingDef to minimal defs
    git-svn-id: file:///home/svn/omero/trunk@319 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/sandbox/src/main/importer/MinimalDefinitions.java
+++ components/sandbox/src/main/importer/MinimalDefinitions.java
@@ -1,10 +1,22 @@
 package main.importer;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.orm.hibernate3.HibernateCallback;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 
+import ome.model.core.Channel;
+import ome.model.core.Pixels;
+import ome.model.display.ChannelBinding;
+import ome.model.display.Color;
+import ome.model.display.QuantumDef;
+import ome.model.display.RenderingDef;
+import ome.model.enums.FamilyType;
+import ome.model.enums.ModelType;
 import ome.model.enums.PIType;
 import ome.model.meta.Event;
 import ome.model.meta.Experimenter;
@@ -19,7 +31,8 @@ public class MinimalDefinitions
     public static void main(final String[] args) throws Exception
     {
         MinimalDefinitions defs = new MinimalDefinitions();
-        defs.createPixels();
+        Long pixel_id = defs.createPixels().getId();
+        defs.createRenderingDef(pixel_id);
     }
 
     public MinimalDefinitions()
@@ -33,10 +46,10 @@ public class MinimalDefinitions
 
     }
 
-    public void createPixels() throws Exception
+    public ome.model.core.Pixels createPixels() throws Exception
     {
 
-        ht.execute(new HibernateCallback()
+        return (ome.model.core.Pixels) ht.execute(new HibernateCallback()
         {
 
             public Object doInHibernate(org.hibernate.Session session)
@@ -44,22 +57,9 @@ public class MinimalDefinitions
                     java.sql.SQLException
             {
 
-                Experimenter o = (Experimenter) session.get(Experimenter.class,
-                        new Long(1));
-                if (o == null)
-                {
-                    o = new Experimenter();
-                    o.setOmeName("test");
-                    session.save(o);
-                }
+                Experimenter o = getOwner(session);
 
-                Event e = (Event) session.get(Event.class, new Long(1));
-                if (e == null)
-                {
-                    e = new Event();
-                    e.setName("test");
-                    session.save(e);
-                }
+                Event e = getEvent(session);
 
                 // CHANNELS
                 ome.model.core.Channel c = new ome.model.core.Channel();
@@ -78,7 +78,7 @@ public class MinimalDefinitions
                 lc.setOwner(o);
                 session.save(lc);
                 
-                //ACQContext
+                // ACQContext
                 ome.model.acquisition.AcquisitionContext ac 
                     = new ome.model.acquisition.AcquisitionContext();
                 // Needed
@@ -135,13 +135,127 @@ public class MinimalDefinitions
                 p.setCreationEvent(e);
                 p.setOwner(o);
                 session.save(p);
-                
-
-                
-                return null;
-            };
+                           
+                return p;
+            }
         });
+    }
 
+            public RenderingDef createRenderingDef(final Long pixId) throws Exception
+            {
+
+                return (RenderingDef) ht.execute(new HibernateCallback()
+                {
+
+                    public Object doInHibernate(org.hibernate.Session session)
+                            throws org.hibernate.HibernateException,
+                            java.sql.SQLException
+                    {
+
+                        Experimenter o = getOwner(session);
+
+                        Event e = getEvent(session);
+
+                        Pixels p = (Pixels) session.load(Pixels.class, pixId);
+                                                
+                        QuantumDef qDef = new QuantumDef();
+                        // not-null
+                        qDef.setBitResolution(new Integer(255)); // TODO get QuantumFactory in common code
+                        qDef.setCdStart(new Integer(0));
+                        qDef.setCdStop(new Integer(255)); // QuantumFactory
+                        // admin
+                        qDef.setCreationEvent(e);
+                        qDef.setOwner(o);
+                        session.save(qDef);
+                        
+                        FamilyType ft = new FamilyType();
+                        //not-null
+                        ft.setValue("TODO");
+                        //admin
+                        ft.setCreationEvent(e);
+                        ft.setOwner(o);
+                        session.save(ft);
+                        
+                        Color c = new Color();
+                        // not-null
+                        c.setRed(new Integer(1));
+                        c.setBlue(new Integer(1));
+                        c.setGreen(new Integer(1));
+                        c.setAlpha(new Integer(1));
+                        // admin
+                        c.setOwner(o);
+                        c.setCreationEvent(e);
+                        session.save(c);
+                        
+                        ChannelBinding[] waves = new ChannelBinding[p.getSizeC().intValue()];
+                        for (int i = 0; i < waves.length; i++)
+                        {
+                            waves[i] = new ChannelBinding();
+                            // not-null
+                            waves[i].setFamily(ft);
+                            waves[i].setCoefficient(new Double(0));
+                            waves[i].setIndex(new Integer(0));
+                            waves[i].setInputStart(new Float(0));
+                            waves[i].setInputEnd(new Float(255)); // FIXME
+                            waves[i].setActive(Boolean.TRUE);
+                            waves[i].setColor(c);
+                            // admin
+                            waves[i].setOwner(o);
+                            waves[i].setCreationEvent(e);
+                            session.save(waves[i]);
+                        }
+                        
+                        ModelType mt = new ModelType();
+                        // not-null
+                        mt.setValue("TODO");
+                        // admin
+                        mt.setCreationEvent(e);
+                        mt.setOwner(o);
+                        session.save(mt);
+                        
+                        int z_size = p.getSizeZ().intValue();
+                        RenderingDef newRD = new RenderingDef();
+                        int defaultZ = z_size/2+z_size%2-1;
+                        // not-null
+                        newRD.setDefaultZ(Integer.valueOf(defaultZ));
+                        newRD.setDefaultT(Integer.valueOf(0));
+                        // needed
+                        newRD.setModel(mt);
+                        newRD.setQuantization(qDef);
+                        newRD.setWaveRendering(new HashSet(Arrays.asList(waves)));
+                        // admin
+                        newRD.setOwner(o);
+                        newRD.setCreationEvent(e);
+                        session.save(newRD);
+                        
+                        return newRD;
+                    }
+                });
+            }
+                    
+            private Event getEvent(org.hibernate.Session session)
+            {
+                Event e = (Event) session.get(Event.class, new Long(1));
+                if (e == null)
+                {
+                    e = new Event();
+                    e.setName("test");
+                    session.save(e);
+                }
+                return e;
+            }
+
+            private Experimenter getOwner(org.hibernate.Session session)
+            {
+                Experimenter o = (Experimenter) session.get(Experimenter.class,
+                        new Long(1));
+                if (o == null)
+                {
+                    o = new Experimenter();
+                    o.setOmeName("test");
+                    session.save(o);
+                }
+                return o;
     }
 
 }
