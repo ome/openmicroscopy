78455c050877bccfd818bd0e83853e01e826b8aa
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-01-25 11:52:24 +0000

    modified constructor
    git-svn-id: file:///home/svn/shoola-omero/trunk@3067 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/data/views/calls/DataObjectSaver.java
+++ SRC/org/openmicroscopy/shoola/env/data/views/calls/DataObjectSaver.java
@@ -31,15 +31,18 @@ package org.openmicroscopy.shoola.env.data.views.calls;
 
 
 //Java imports
+import java.util.ArrayList;
 
 //Third-party libraries
 
 //Application-internal dependencies
-import java.util.ArrayList;
-
 import org.openmicroscopy.shoola.env.data.DataManagementService;
+import org.openmicroscopy.shoola.env.data.SemanticTypesService;
+import org.openmicroscopy.shoola.env.data.model.CategoryData;
+import org.openmicroscopy.shoola.env.data.model.CategoryGroupData;
 import org.openmicroscopy.shoola.env.data.model.DatasetData;
 import org.openmicroscopy.shoola.env.data.model.DatasetSummary;
+import org.openmicroscopy.shoola.env.data.model.ImageData;
 import org.openmicroscopy.shoola.env.data.model.ProjectData;
 import org.openmicroscopy.shoola.env.data.model.ProjectSummary;
 import org.openmicroscopy.shoola.env.data.views.BatchCall;
@@ -66,6 +69,9 @@ public class DataObjectSaver
     
     /** Indicates to update the <code>DataObject</code>. */
     public static final int UPDATE = 1;
+    
+    /** Indicates to remove the <code>DataObject</code>. */
+    public static final int REMOVE = 2;
     
     /** The save call. */
     private BatchCall       saveCall;
@@ -102,6 +108,39 @@ public class DataObjectSaver
         return d;
     }
     
+    private ImageData daToImageData(pojos.ImageData data)
+    {
+        ImageData i = new ImageData();
+        i.setName(data.getName());
+        i.setDescription(data.getDescription());
+        return i;
+    }
+    
+    private CategoryGroupData daToCategoryGroupData(pojos.CategoryGroupData d)
+    {
+        CategoryGroupData c = new CategoryGroupData();
+        c.setName(d.getName());
+        c.setDescription(d.getDescription());
+        return c;
+    }
+    
+    private CategoryData daToCategoryData(pojos.CategoryData d)
+    {
+        CategoryData c = new CategoryData();
+        c.setName(d.getName());
+        c.setDescription(d.getDescription());
+        return c;
+    }
+    
+    private pojos.CategoryGroupData reverseCategoryGroup(CategoryGroupData c)
+    {
+        pojos.CategoryGroupData pojo = new  pojos.CategoryGroupData();
+        pojo.setId(c.getID());
+        pojo.setName(c.getName());
+        pojo.setDescription(c.getDescription());
+        return pojo;
+    }
+    
     private pojos.DatasetData summaryToDataObject(DatasetSummary s)
     {
         pojos.DatasetData data = new pojos.DatasetData();
@@ -112,34 +151,19 @@ public class DataObjectSaver
     }
     
     /**
-     * Checks if the specified index is supported.
-     * 
-     * @param i The index to check.
-     */
-    private void checkIndex(int i)
-    {
-        switch (i) {
-            case CREATE:
-            case UPDATE:
-                return;
-            default:
-                throw new IllegalArgumentException("Index not supported");   
-        }
-    }
-    
-    /**
      * Creates a {@link BatchCall} to create the specified {@link DataObject}.
      * 
-     * @param userObject The <code>DataObject</code> to create.
-     * @param parentID The Id of the parent.
+     * @param userObject 	The <code>DataObject</code> to create.
+     * @param parent 		The parent of the <code>DataObject</code>.
      * @return The {@link BatchCall}.
      */
-    private BatchCall create(final DataObject userObject, final int parentID)
+    private BatchCall create(final DataObject userObject, final Object parent)
     {
         return new BatchCall("Create Data object.") {
             public void doCall() throws Exception
             {
                 DataManagementService dms = context.getDataManagementService();
+                SemanticTypesService sts = context.getSemanticTypesService();
                 //tempo
                 if (userObject instanceof pojos.ProjectData) {
                     ProjectData data = 
@@ -148,12 +172,18 @@ public class DataObjectSaver
                 } else if (userObject instanceof pojos.DatasetData) {
                     DatasetData data = 
                         daToDatasetData((pojos.DatasetData) userObject);
-                    ProjectSummary parent = new ProjectSummary();
-                    parent.setID(parentID);
+                    ProjectSummary p = new ProjectSummary();
+                    p.setID(((pojos.ProjectData) parent).getId());
                     ArrayList l = new ArrayList(1);
                     l.add(parent);
                     result = summaryToDataObject(
                                     dms.createDataset(l, null, data));
+                } else if (userObject instanceof pojos.CategoryGroupData) {
+                    CategoryGroupData c = daToCategoryGroupData(
+                            	(pojos.CategoryGroupData) userObject);
+                    result = reverseCategoryGroup(sts.createCategoryGroup(c));
+                } else if (userObject instanceof pojos.CategoryData) {
+                    
                 }
             }
         };
@@ -166,6 +196,51 @@ public class DataObjectSaver
      * @return The {@link BatchCall}.
      */
     private BatchCall update(final DataObject userObject)
+    {
+        return new BatchCall("Update Data object.") {
+            public void doCall() throws Exception
+            {
+                //TODO: REMOVE ASAP
+                DataManagementService dms = context.getDataManagementService();
+                SemanticTypesService sts = context.getSemanticTypesService();
+                if (userObject instanceof pojos.ProjectData) {
+                    ProjectData data = 
+                        daToProjectData((pojos.ProjectData) userObject);
+                    dms.updateProject(data, null, null);
+                    result = userObject;
+                } else if (userObject instanceof pojos.DatasetData) {
+                    DatasetData data = 
+                        daToDatasetData((pojos.DatasetData) userObject);
+                    dms.updateDataset(data, null, null);
+                    result = userObject;
+                } else if (userObject instanceof pojos.ImageData) {
+                    ImageData data = daToImageData(
+                            		(pojos.ImageData) userObject);
+                    dms.updateImage(data);
+                    result = userObject;
+                } else if (userObject instanceof pojos.CategoryGroupData) {
+                    CategoryGroupData data = daToCategoryGroupData(
+                            	(pojos.CategoryGroupData) userObject);
+                    sts.updateCategoryGroup(data, null);
+                    result = userObject;
+                } else if (userObject instanceof pojos.CategoryData) {
+                    CategoryData data = daToCategoryData(
+                        		(pojos.CategoryData) userObject);
+                    sts.updateCategory(data, null, null);
+                    result = userObject;
+                }
+            }
+        };
+    }
+    
+    /**
+     * Creates a {@link BatchCall} to update the specified {@link DataObject}.
+     * 
+     * @param userObject	The <code>DataObject</code> to remove.
+     * @param parent		The parent of the <code>DataObject</code>.
+     * @return The {@link BatchCall}.
+     */
+    private BatchCall remove(final DataObject userObject, final Object parent)
     {
         return new BatchCall("Update Data object.") {
             public void doCall() throws Exception
@@ -186,7 +261,7 @@ public class DataObjectSaver
      */
     protected Object getResult()
     {
-        return result;
+        return Boolean.TRUE;
     }
 
     /**
@@ -195,28 +270,36 @@ public class DataObjectSaver
      * @param userObject    The {@link DataObject} to create or update.
      *                      Mustn't be <code>null</code>.
      * @param index         One of the constants defined by this class.
-     * @param parentID      The ID of the parent. The value is <code>-1</code>
-     *                      if there is no parent.
+     * @param parent     	The parent of the <code>DataObject</code>. 
+     * 						The value is <code>null</code> if there 
+     * 						is no parent.
      */
-    public DataObjectSaver(DataObject userObject, int index, int parentID)
+    public DataObjectSaver(DataObject userObject, int index, Object parent)
     {
         if (userObject == null)
             throw new IllegalArgumentException("No DataObject.");
-        checkIndex(index);
         objectToSave = userObject; //tempo
-        if (index == CREATE) {
-            if ((userObject instanceof pojos.DatasetData) ||
-                    (userObject instanceof pojos.CategoryData)) {
-                if (parentID == -1) 
-                    throw new IllegalArgumentException("ParentID not valid.");
+        if (index == CREATE || index == REMOVE) {
+            if (userObject instanceof pojos.DatasetData) {
+                if (!(parent instanceof pojos.ProjectData))
+                throw new IllegalArgumentException("Parent not valid.");
+            } else if (userObject instanceof pojos.CategoryData) {
+                if (!(parent instanceof pojos.CategoryGroupData))
+                    throw new IllegalArgumentException("Parent not valid.");
             }
         }
         switch (index) {
             case CREATE:
-                saveCall = create(userObject, parentID);
+                saveCall = create(userObject, parent);
                 break;
             case UPDATE:
                 saveCall = update(userObject);
+                break;
+            case REMOVE:
+                saveCall = remove(userObject, parent);   
+                break;
+            default:
+                throw new IllegalArgumentException("Operation not supported.");
         }
     }
   
