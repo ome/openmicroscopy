25a45a2b3c70f5ed0cfb1fd6d1665ef0a244a421
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2006-03-27 13:33:38 +0000

    [Build] Maven/Ant madness!
    * pom.xml now are purely dependency definitions.
    * Now using maven-artifact-ant (jar is under antlib/).
    * Updated all classpath.xml & .classpath files.
    * Cleaned up *.groovy files.
    * About to up version and commit libraries.
    
    
    git-svn-id: file:///home/svn/omero/trunk@545 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/antlib/resources/groovy/antClasspath.groovy
+++ components/antlib/resources/groovy/antClasspath.groovy
@@ -1,90 +1,88 @@
-                genresrc  = properties["resrc.dest"]   
-                group     = properties["mvn.group"]   
-                artifact  = properties["mvn.artifact"]   
-                version   = properties["mvn.version"]   
-                packaging = properties["mvn.packaging"]   
-                new File("${genresrc}/classpath.xml").withOutputStream{|o|
+genresrc  = properties["resrc.dest"]   
+group     = properties["mvn.group"]   
+artifact  = properties["mvn.artifact"]   
+version   = properties["mvn.version"]   
+sep=":"
 
-		  repo = "repository/"
-		  def entry = {|x|
-		    if (x.contains(repo))  {
-		      path = x.substring(x.lastIndexOf(repo)+repo.length())
-		      o << """\n\t<file name="${path}"/>"""
-                    }
-		  }
+def run(pathname,closure) {
+    properties[pathname].split(sep).toList().collect( { x ->
+      if ( x.contains("javax/transaction/jta") )
+      {
+          x = x.replaceAll("javax/transaction","geronimo-spec")
+          x = x.replaceAll("jta","geronimo-spec-jta")
+          x = x.replaceAll("1.0.1B","1.0.1B-rc4")
+      }
+      return x
 
-		  def get = {|x|
-		    if (x.contains(repo))  {
-		      path = x.substring(x.lastIndexOf(repo)+repo.length())
-		      o << """\n\t<get src="\${omero.repo.remote}/${path}" dest="\${omero.repo.local}/${path}" usetimestamp="true"/>"""
-                    }
-		  }
+    }).collect(closure).findAll({it!=null}).sort().join("\n")
+}
 
-		  def dist = {|x|
-		    if (x.contains(repo))  {
-		      path = x.substring(x.lastIndexOf(repo)+repo.length())
-		      o << """
-	<copy file="\${omero.repo.local}/${path}" todir="\${dist.dir}/repository/${path.substring(0,path.lastIndexOf("/"))}"/>"""
-                    }
-		  }
 
-		  def jars = {|x|
-		    if (x.contains(repo))  {
-		      path = x.substring(x.lastIndexOf(repo)+repo.length())
-		      o << """
-	<copy file="\${omero.repo.local}/${path}" todir="@{todir}"/>"""
-                    }
-		  }
+repo = "repository/"
+def entry = { x ->
+    if (x.contains(repo))  
+    {
+        path = x.substring(x.lastIndexOf(repo)+repo.length())
+        return """\t<file name="${path}"/>"""
+    }
+}
 
-		  o << """
+def get = {|x|
+    if (x.contains(repo))  {
+        path = x.substring(x.lastIndexOf(repo)+repo.length())
+        return """\t<get src="\${omero.repo.remote}/${path}" dest="\${omero.repo.local}/${path}" usetimestamp="true"/>"""
+    }
+}
+
+def dist = {|x|
+    if (x.contains(repo))  {
+        path = x.substring(x.lastIndexOf(repo)+repo.length())
+        return """\t<copy file="\${omero.repo.local}/${path}" todir="\${dist.dir}/repository/${path.substring(0,path.lastIndexOf("/"))}"/>"""
+    }
+}
+
+def jars = {|x|
+    if (x.contains(repo))  {
+        path = x.substring(x.lastIndexOf(repo)+repo.length())
+        return """\t<copy file="\${omero.repo.local}/${path}" todir="@{todir}"/>"""
+    }
+}
+
+new File("${genresrc}/classpath.xml").withOutputStream{ o ->
+o << """
+
 <!-- Code-generated ant classpath for ${artifact} component -->
 <project name="${artifact}_classpath" default="classpath-generate" basedir=".">
   <property name="artifact.name" value="${artifact}"/>
   <property name="artifact.group" value="${group}"/>
   <property name="artifact.version" value="${version}"/>
-  <property name="artifact.packaging" value="${packaging}"/>
-  <property name="artifact.final.name" value="${artifact}-${version}.${packaging}"/>
+  <property name="artifact.packaging" value="jar"/><!-- Default; override in component/build.xml -->
+  <property name="artifact.final.name" value="${artifact}-${version}.\${artifact.packaging}"/>
   <property name="artifact.path" value="${group}/${artifact}/${version}/\${artifact.final.name}"/>
   <path id="generated.compile.classpath">
-    <filelist dir="\${omero.repo.local}">"""
-
-		  properties["compile.path"].split(":").toList().sort().each(entry)
-
-		  o << """
+    <filelist dir="\${omero.repo.local}">
+${run("compile.path",entry)}
     </filelist>
   </path>
   <path id="generated.test.classpath">
-    <filelist dir="\${omero.repo.local}">"""
-
-		  properties["test.path"].split(":").toList().sort().each(entry)
-
-		  o << """
+    <filelist dir="\${omero.repo.local}">
+${run("test.path",entry)}
     </filelist>
   </path>
-
-  <target name="classpath-download">"""
-
-		  properties["omero.path"].split(":").toList().sort().each(get)
-
-		o << """
+  <target name="classpath-download">
+${run("omero.path",get)}
   </target>
-
-  <target name="dist-copy">"""
-
-		  properties["omero.path"].split(":").toList().sort().each(dist)
-
-		o << """
+  <target name="dist-copy">
+${run("omero.path",dist)}
   </target>
-
   <macrodef name="jars-copy">
     <attribute name="todir"/>
-    <sequential>"""
-
-		  properties["omero.path"].split(":").toList().sort().each(jars)
-
-		o << """
+    <sequential>
+${run("omero.path",jars)}
     </sequential>
   </macrodef>
+</project>
 
-</project>"""
-		}
+"""
+
+}
--- components/antlib/resources/groovy/eclipseClasspath.groovy
+++ components/antlib/resources/groovy/eclipseClasspath.groovy
@@ -1,38 +1,51 @@
-		src      = properties["src.dir"]	
-		resrc    = properties["resrc.dir"]	
-		gensrc   = properties["src.dest"]	
-		genresrc = properties["resrc.dest"]	
-		test     = properties["test.dir"]
-		classrel = properties["classes.rel"]
-		basedir  = properties["basedir"]	
-                new File("${genresrc}/.classpath").withOutputStream{|o|
-                        def entryMaker = {|extras|
-                          return {|path|
-			    path = path.substring(basedir.size()+1,path.size())
-                            f = new File(path)
-                            if (f.isDirectory())
-                              o << """\t<classpathentry excluding="**/.svn" kind="src" path="${path}" ${extras}/>\n"""
-                          }
-                          
-                        }
-                        o << "<classpath>\n"
-                        o << """\t<classpathentry kind="output" path="${classrel}"/>\n"""
-                        o << """\t<classpathentry kind="var" rootpath="JRE_SRCROOT" path="JRE_LIB" sourcepath="JRE_SRC"/>\n"""
+// dir = properties["import.dir"]
+// this.evaluate(new File("${dir}/groovy/common.groovy"))
+// println test
 
-                        // 
-                        [src,resrc,gensrc,genresrc].each(entryMaker(""))
-                        [test].each(entryMaker("output=\"target/test-classes\""))
+src      = properties["src.dir"]	
+resrc    = properties["resrc.dir"]	
+gensrc   = properties["src.dest"]	
+genresrc = properties["resrc.dest"]	
+test     = properties["test.dir"]
+classrel = properties["classes.rel"]
+basedir  = properties["basedir"]	
 
-			paths = []
-                        properties["omero.path"].split(":").each{ |x|
-                          repo = "repository/"
-                          if (x.contains(repo))  {
-                            paths << x.substring(x.lastIndexOf(repo)+repo.length())
-                          }
-                        }
+new File("${genresrc}/.classpath").withOutputStream{|o|
 
-			paths.sort().each{|path|
-                          o << """\t<classpathentry kind="var" path="M2_REPO/${path}"/>\n"""
-			}
-                        o << "</classpath>\n"
-                }
+    def entryMaker = {|extras|
+        return {|path|
+            path = path.substring(basedir.size()+1,path.size())
+            f = new File(path)
+            if (f.isDirectory())
+                o << """\t<classpathentry excluding="**/.svn" kind="src" path="${path}" ${extras}/>\n"""
+            }
+    }
+
+    o << "<classpath>\n"
+    o << """\t<classpathentry kind="output" path="${classrel}"/>\n"""
+    o << """\t<classpathentry kind="var" rootpath="JRE_SRCROOT" path="JRE_LIB" sourcepath="JRE_SRC"/>\n"""
+
+    [src,resrc,gensrc,genresrc].each(entryMaker(""))
+    [test].each(entryMaker("output=\"target/test-classes\""))
+
+    paths = []
+    properties["omero.path"].split(":").each{ |x|
+        repo = "repository/"
+        if (x.contains(repo))  {
+        if ( x.contains("javax/transaction/jta") )
+        {
+            x = x.replaceAll("javax/transaction","geronimo-spec")
+            x = x.replaceAll("jta","geronimo-spec-jta")
+            x = x.replaceAll("1.0.1B","1.0.1B-rc4")
+        }
+          paths << x.substring(x.lastIndexOf(repo)+repo.length())
+        }
+    }
+
+    paths.sort().each{|path|            
+        o << """\t<classpathentry kind="var" path="M2_REPO/${path}"/>\n"""
+    }
+
+    o << "</classpath>\n"
+
+}
