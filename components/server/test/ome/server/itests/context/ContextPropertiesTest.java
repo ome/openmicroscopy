7e6c8082acda3a329fbeca5fe84d78e686ca92ef
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2006-05-22 14:26:20 +0000

    [Ticket #85] Tracking down weird non-exceptions with bad Hibernate urls.
    git-svn-id: file:///home/svn/omero/trunk@666 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/server/test/ome/server/itests/context/ContextPropertiesTest.java
@@ -0,0 +1,98 @@
+package ome.server.itests.context;
+
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+import org.testng.annotations.Configuration;
+import org.testng.annotations.Test;
+
+import ome.model.meta.Experimenter;
+
+
+public class ContextPropertiesTest extends AbstractDependencyInjectionSpringContextTests
+{
+
+    HibernateTemplate template;
+    
+    // =========================================================================
+    // ~ Testng Adapter
+    // =========================================================================
+    @Configuration(beforeTestMethod = true)
+    public void adaptSetUp() throws Exception
+    {
+        super.setUp();
+    }
+
+    @Configuration(afterTestMethod = true)
+    public void adaptTearDown() throws Exception
+    {
+        super.tearDown();
+    }
+    // =========================================================================
+    
+    @Override
+    protected String[] getConfigLocations()
+    {
+        return new String[]{
+                "ome/services/dbcp.xml",
+                "ome/services/hibernate.xml",
+                "ome/server/itests/context/ContextPropertiesTest.xml"}; 
+        // sets initial url (for *first* call)
+    }
+    
+    @Test
+    public void test_bad_db() throws Exception
+    {
+        template = doGetTemplate("jdbc:postgresql://localhost/blah");
+        testTemplate(template,"bad db");
+    }
+    
+    @Test
+    public void test_bad_server() throws Exception
+    {
+        template = doGetTemplate("jdbc:postgresql://blah");
+        testTemplate(template,"bad server");
+    }
+
+    @Test
+    public void test_wrong_db_type() throws Exception
+    {
+        template = doGetTemplate("jdbc:mysql://localhost/ome-meta");
+        testTemplate(template,"bad db type");
+    }
+    
+    @Test
+    public void test_bad_protocol() throws Exception
+    {
+        template = doGetTemplate("jdbxx:postgresql://localhost/ome-meta");
+        testTemplate(template,"bad protocol");
+    }
+
+    // ~ Private helpers
+    // =========================================================================
+    
+    private HibernateTemplate doGetTemplate(String url)
+    {
+        HibernateTemplate ht = 
+            (HibernateTemplate) applicationContext.getBean("hibernateTemplate");
+        setUrl(url);
+        this.setDirty();
+        return ht;
+    }
+    
+    // sets url for SUBSEQUENT call
+    private void setUrl(String url)
+    {
+        System.getProperties().setProperty("hibernate.connection.url",url);
+    }
+
+    private void testTemplate(HibernateTemplate template, String msg)
+    {        
+        try {
+            template.get(Experimenter.class,0);
+            fail("Should not work with:"+msg);
+        } catch (Exception e) {
+            // ok.
+        }
+    }
+
+}
