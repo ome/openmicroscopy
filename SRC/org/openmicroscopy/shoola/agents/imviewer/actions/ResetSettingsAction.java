412654120fa1d20100303670b0bb6fa9a7f04c82
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-08-08 14:05:02 +0000

    initial commit
    git-svn-id: file:///home/svn/shoola-omero/trunk@3449 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/imviewer/actions/ResetSettingsAction.java
@@ -0,0 +1,84 @@
+/*
+ * org.openmicroscopy.shoola.agents.imviewer.actions.ResetSettingsAction
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.imviewer.actions;
+
+
+
+//Java imports
+import javax.swing.Action;
+
+
+//Third-party libraries
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.agents.imviewer.IconManager;
+import org.openmicroscopy.shoola.agents.imviewer.rnd.Renderer;
+import org.openmicroscopy.shoola.util.ui.UIUtilities;
+
+/** 
+ * 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author	Andrea Falconi &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:a.falconi@dundee.ac.uk">a.falconi@dundee.ac.uk</a>
+ * @author	Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
+ * @version 3.0
+ * <small>
+ * (<b>Internal version:</b> $Revision: $ $Date: $)
+ * </small>
+ * @since OME2.2
+ */
+public class ResetSettingsAction
+    extends RndAction
+{
+
+    /** The name of the action. */
+    public static final String  NAME = "Reset settings";
+    
+    /** The description of the action. */
+    private static final String DESCRIPTION = "Reset the default settings.";
+    
+    /**
+     * Creates a new instance.
+     * 
+     * @param model The {@link Renderer} model. Mustn't be <code>null</code>.
+     */
+    public ResetSettingsAction(Renderer model)
+    {
+        super(model);
+        //putValue(Action.NAME, NAME);
+        putValue(Action.SHORT_DESCRIPTION, 
+                UIUtilities.formatToolTipText(DESCRIPTION));
+        IconManager icons = IconManager.getInstance();
+        putValue(Action.SMALL_ICON, icons.getIcon(IconManager.RESET_SETTINGS));
+    }
+}
