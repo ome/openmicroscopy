a628f3653f686c5abfd8669d8b26b5535dd458f1
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-09-11 08:40:22 +0000

    initial commit: replace rnd/metadata/ChannelMetadata
    git-svn-id: file:///home/svn/shoola-omero/trunk@3681 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/env/data/model/ChannelMetadata.java
@@ -0,0 +1,145 @@
+/*
+ * org.openmicroscopy.shoola.env.rnd.metadata.ChannelMetadata
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.env.data.model;
+
+
+
+
+//Java imports
+
+//Third-party libraries
+
+//Application-internal dependencies
+import ome.model.core.Channel;
+
+/** 
+ * 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author	Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
+ * @version 3.0
+ * <small>
+ * (<b>Internal version:</b> $Revision: $ $Date: $)
+ * </small>
+ * @since OME2.2
+ */
+public class ChannelMetadata
+{
+
+    /** The OME index of the channel. */
+    private final int               index;
+    
+    private final Channel           channel;
+    
+    /**
+     * Creates a new instance.
+     * 
+     * @param index     The index of the channel.
+     * @param channel
+     */
+    public ChannelMetadata(final int index, final Channel channel)
+    {
+        if (channel == null) 
+            throw new IllegalArgumentException("Channel cannot be null.");
+        this.index = index;
+        this.channel = channel;
+    }
+    
+    /**
+     * Returns the emission wavelength of the channel.
+     * 
+     * @return See above
+     */
+    public int getEmissionWavelength()
+    {
+        Integer wave = channel.getLogicalChannel().getEmissionWave();
+        if (wave == null) return index;
+        return wave.intValue();
+    }
+    
+    /**
+     * Returns the excitation wavelength of the channel.
+     * 
+     * @return See above
+     */
+    public int getExcitationWavelength()
+    {
+        Integer wave = channel.getLogicalChannel().getExcitationWave();
+        if (wave == null) return getEmissionWavelength();
+        return wave.intValue();
+    }
+    
+    /**
+     * Returns the excitation wavelength of the channel.
+     * 
+     * @return See above
+     */
+    public int getPinholeSize()
+    {
+        Integer v = channel.getLogicalChannel().getPinHoleSize();
+        if (v == null) return 0;
+        return v.intValue();
+    }
+    
+    /**
+     * Returns the excitation wavelength of the channel.
+     * 
+     * @return See above
+     */
+    public float getNDFilter()
+    {
+        Float v = channel.getLogicalChannel().getNdFilter();
+        if (v == null) return 0;
+        return v.floatValue();
+    }
+    
+    /** 
+     * Returns the global minimum of the channel i.e. the minimum of all minima.
+     * 
+     * @return See above.
+     */
+    public double getGlobalMin()
+    {
+        return channel.getStatsInfo().getGlobalMin().doubleValue();
+    }
+    
+    /** 
+     * Returns the global maximum of the channel i.e. the maximum of all maxima.
+     * 
+     * @return See above.
+     */
+    public double getGlobalMax()
+    {
+        return channel.getStatsInfo().getGlobalMax().doubleValue();
+    }
+    
+}
