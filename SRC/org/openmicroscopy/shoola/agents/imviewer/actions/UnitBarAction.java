60ec834cc2ebd4e457f6d257e86dfa01b57173f9
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-09-14 10:13:52 +0000

    initial commit. See ticket #355
    git-svn-id: file:///home/svn/shoola-omero/trunk@3714 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/imviewer/actions/UnitBarAction.java
@@ -0,0 +1,91 @@
+/*
+ * org.openmicroscopy.shoola.agents.imviewer.actions.UnitBarAction
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.imviewer.actions;
+
+
+
+//Java imports
+import java.awt.event.ActionEvent;
+
+import javax.swing.Action;
+
+//Third-party libraries
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.agents.imviewer.view.ImViewer;
+import org.openmicroscopy.shoola.util.ui.UIUtilities;
+
+/** 
+ * 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author	Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
+ * @version 3.0
+ * <small>
+ * (<b>Internal version:</b> $Revision: $ $Date: $)
+ * </small>
+ * @since OME2.2
+ */
+public class UnitBarAction
+    extends ViewerAction
+{
+
+    /** The name of the action. */
+    private static final String NAME = "Show/Hide unit bar";
+    
+    /** The description of the action. */
+    private static final String DESCRIPTION = "Show or hide the unit bar " +
+            "displayed on top of the image.";
+    
+    /**
+     * Creates a new instance.
+     * 
+     * @param model Reference to the model. Mustn't be <code>null</code>.
+     */
+    public UnitBarAction(ImViewer model)
+    {
+        super(model);
+        putValue(Action.NAME, NAME);
+        putValue(Action.SHORT_DESCRIPTION, 
+                UIUtilities.formatToolTipText(DESCRIPTION));
+    }
+    
+    /** 
+     * Shows or hides the title bar depending on the state.
+     * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+        model.setUnitBar(!model.isUnitBar());
+    }
+    
+}
