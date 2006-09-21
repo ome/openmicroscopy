2a823b183e08ca670e97efac79a17532107eb874
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-09-21 15:49:28 +0000

    refactored
    git-svn-id: file:///home/svn/shoola-omero/trunk@3757 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/util/ui/ColouredButton.java
+++ SRC/org/openmicroscopy/shoola/util/ui/ColouredButton.java
@@ -65,85 +65,10 @@ import javax.swing.plaf.basic.BasicButtonUI;
  */
 public class ColouredButton
     extends JButton
-    implements PropertyChangeListener
 {
     
-    /** Bound property indicating to bring up the info dialog. */
-    public static final String  INFO_PROPERTY = "info";
-    
-    /** 
-     * Bound property name indicating that the channel is or is not selected. 
-     */
-    public static final String  CHANNEL_SELECTED_PROPERTY = "channelSelected";
-    
-    /** 
-     * Bound property name indicating that the channel is mapped to a new color. 
-     */
-    public static final String  CHANNEL_COLOR_PROPERTY = "channelColor";
-    
-    /** The border set when the channel is selected. */
-    public static final Border  SELECTED_BORDER =
-        new BevelBorder(BevelBorder.LOWERED, Color.yellow, Color.yellow);
-    
-    /** The border set when the channel is not selected. */
-    public static final Border  DESELECTED_BORDER = 
-                BorderFactory.createBevelBorder(BevelBorder.RAISED);
-    
-    /** 
-     * Default color used when the color mode is set to
-     * <code>GreyScale</code>.
-     */
-    private static final Color  LAYER_COLOR = new Color(192, 192, 192);
-    
-    /** The OME index of the channel. */
-    private final int   index;
-    
-    /** The color of the node. */
-    private Color       originalColor;
-    
-    /** Fires an event to select the channel. */
-    private final void setChannelSelected()
-    {
-        Boolean value = null;
-        if (isSelected()) value = Boolean.FALSE;
-        else value = Boolean.TRUE;
-        HashMap map = new HashMap(1);
-        map.put(new Integer(index), value);
-        firePropertyChange(CHANNEL_SELECTED_PROPERTY, null, map);
-    }
-    
-    /**
-     * Fires property depending on the keys pressed.
-     * If Ctrl+left click, we bring the color picker.
-     * If Ctrl+right click, we bring the info button.
-     * otherwise the select the channel.
-     * 
-     * @param e         The mouse event to handle.
-     */
-    private void onClick(MouseEvent e)
-    {
-        //Ctrl+ click to bring color picker
-        //Shift+ click to bring up info
-        if (e.isControlDown()) {
-            
-        } else if (e.isShiftDown())
-            firePropertyChange(INFO_PROPERTY, null, new Integer(index));
-        else setChannelSelected();
-    }
-    
-    
-    /**
-     * Nested class used to set the color of the button when a mouse pressed
-     * event occured.
-     * @see BasicButtonUI#paintButtonPressed(Graphics, AbstractButton)
-     */
-    private class ColouredButtonUI
-        extends BasicButtonUI
-    {
-        protected void paintButtonPressed(Graphics g, AbstractButton b) {
-            b.setBackground(b.getBackground());
-        }
-    }
+    /** The UI for this button. */
+    private ColouredButtonUI uiDelegate;
     
     /**
      * Creates a new instance.
@@ -152,88 +77,27 @@ public class ColouredButton
      *                  the emission wavelength.
      * @param color     The background color of the button. Corresponds to the
      *                  color associated to the channel.
-     * @param index     The channel index.
-     * @param selected  Passed <code>true</code> to select the channel (i.e.
-     *                  the channel is rendered), <code>false</code> otherwise
-     *                  (i.e. the channel is not rendered.)
      */
-    public ColouredButton(String text, Color color, int index, 
-                        boolean selected)
+    public ColouredButton(String text, Color color)
     {
         super(text);
         if (color == null) 
             throw new IllegalArgumentException("No color.");
-        originalColor = color;
-        this.index = index;
-        //setEnabled(false);
         setBackground(color);
-        setUI(new ColouredButtonUI());
+        uiDelegate = new ColouredButtonUI(this, color);
+        setUI(uiDelegate);
         setRolloverEnabled(false);
-        setSelected(selected);
-        addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent e) { onClick(e); }
-        });
     }
     
     /**
-     * Creates a deselected button. 
-     * 
-     * @param text      The text of the button. The text should correspond to
-     *                  the emission wavelength.
-     * @param color     The background color of the button. Corresponds to the
-     *                  color associated to the channel.
-     * @param index     The channel index.
-     */
-    public ColouredButton(String text, Color color, int index)
-    {
-        this(text, color, index, false);
-    }
-    
-    /**
-     * Setst the border and the background color depending on the given values.
+     * Sets the button to be greyed out.
      *  
-     * @param selected  Pass <code>true</code> if the node is selected,
-     *                  <code>false</code> otherwise.
-     * @param gs        Pass <code>true</code> to add the greyscale layer,
+     * @param greyedOut Pass <code>true</code> to gray out the button,
      *                  <code>false</code> otherwise.
      */
-    public void setChannelSelected(boolean selected, boolean gs)
+    public void setGrayedOut(boolean greyedOut)
     {
-        if (gs) setBackground(LAYER_COLOR);
-        else setBackground(originalColor);
-        setSelected(selected);
+        uiDelegate.setGrayedOut(greyedOut);
     }
     
-    /**
-     * Overriden to set the border depending on the selection state.
-     * @see AbstractButton#setSelected(boolean)
-     */
-    public void setSelected(boolean b)
-    {
-        if (b) setBorder(SELECTED_BORDER);
-        else setBorder(DESELECTED_BORDER); 
-        super.setSelected(b);
-    }
-    
-    /**
-     * Returns the index of the channel.
-     * 
-     * @return See above.
-     */
-    public int getChannelIndex() { return index; }
-    
-    /**
-     * Reacts to color selection in the ColorChooser
-     * @see PropertyChangeListener#propertyChange(PropertyChangeEvent)
-     */
-    public void propertyChange(PropertyChangeEvent pce)
-    {
-        //Will listen to a property fired by ColorChooser
-        Map map = (Map) pce.getNewValue();
-        Object o = map.get(new Integer(index));
-        if (o != null) {
-            setBackground((Color) o);
-            firePropertyChange(CHANNEL_COLOR_PROPERTY, null, map);
-        }
-    }
 }
