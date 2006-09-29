49bba34237aab205b6e3f2fa0ba6ea5f1b7efe73
donald <donald@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-09-29 11:14:35 +0000

    Updated unfiedButtonLookandFeel margins.
    git-svn-id: file:///home/svn/shoola-omero/trunk@3895 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/util/ui/UIUtilities.java
+++ SRC/org/openmicroscopy/shoola/util/ui/UIUtilities.java
@@ -294,7 +294,7 @@ public class UIUtilities
      */
     public static void unifiedButtonLookAndFeel(AbstractButton b)
     {
-        b.setMargin(new Insets(0, 0, 0, 0));
+        b.setMargin(new Insets(0, 2, 0, 3));
         b.setBorderPainted(false);
         b.setFocusPainted(false);
     }
