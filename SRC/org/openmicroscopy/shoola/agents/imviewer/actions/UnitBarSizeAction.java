9d4bf0456fef480b10f903bd3eaf574fc0fa390d
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-10-06 17:20:56 +0000

    Modified action implementation
    git-svn-id: file:///home/svn/shoola-omero/trunk@4021 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/imviewer/actions/UnitBarSizeAction.java
+++ SRC/org/openmicroscopy/shoola/agents/imviewer/actions/UnitBarSizeAction.java
@@ -31,13 +31,19 @@ package org.openmicroscopy.shoola.agents.imviewer.actions;
 
 
 //Java imports
+import java.awt.Component;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.Action;
 
 //Third-party libraries
 
 //Application-internal dependencies
 import org.openmicroscopy.shoola.agents.imviewer.IconManager;
+import org.openmicroscopy.shoola.agents.imviewer.util.UnitBarSizeDialog;
 import org.openmicroscopy.shoola.agents.imviewer.view.ImViewer;
 import org.openmicroscopy.shoola.util.ui.UIUtilities;
 
@@ -59,50 +65,120 @@ public class UnitBarSizeAction
     extends ViewerAction
 {
 
+    /** Identifies the scale bar of size <code>1</code>. */
+    public static final int ONE = 0;
+    
+    /** Identifies the scale bar of size <code>2</code>. */
+    public static final int TWO = 1;
+    
+    /** Identifies the scale bar of size <code>5</code>. */
+    public static final int FIVE = 2;
+    
+    /** Identifies the scale bar of size <code>10</code>. */
+    public static final int TEN = 3;
+    
+    /** Identifies the scale bar of size <code>20</code>. */
+    public static final int TWENTY = 4;
+    
+    /** Identifies the scale bar of size <code>50</code>. */
+    public static final int FIFTY = 5;
+    
+    /** Identifies the scale bar of size <code>100</code>. */
+    public static final int HUNDRED = 6;
+    
+    /** Identifies the scale bar of size customized. */
+    public static final int CUSTOMIZED = 7;
+    
+    /** The number of supported ids. */
+    private static final int    MAX = 7;
+    
+    
     /** The description of the action. */
-    private static final String DESCRIPTION_INCREASE = "Increase the size of " +
+    private static final String DESCRIPTION = "Selects the size of " +
             "the Scale bar displayed on top of the image.";
     
-    private static final String DESCRIPTION_DECREASE = "Decrease the size of " +
-            "the Scale bar displayed on top of the image.";
     
     /** 
-     * If <code>true</code> increase the size of the unit bar, 
-     * decrease otherwise.
+     * Array of action names associated to the identifiers defined by this 
+     * class.
      */
-    private boolean increase;
+    private static String[]     names;
+    
+    /** 
+     * Array of values associated to the identifiers defined by this 
+     * class.
+     */
+    private static int[]        values;
+    
+    static {
+        names = new String[MAX+1];
+        names[ONE] = "1";
+        names[TWO] = "2";
+        names[FIVE] = "5";
+        names[TEN] = "10";
+        names[TWENTY] = "20";
+        names[FIFTY] = "50";
+        names[HUNDRED] = "100";
+        names[CUSTOMIZED] = "Custom";
+        values = new int[MAX+1];
+        values[ONE] = 1;
+        values[TWO] = 2;
+        values[FIVE] = 5;
+        values[TEN] = 10;
+        values[TWENTY] = 20;
+        values[FIFTY] = 50;
+        values[HUNDRED] = 100;
+    }
+
+    /** One of the constant defined by this class. */
+    private int     index;
+    
+    /** 
+     * Controls if the specified index is valid.
+     * 
+     * @param i The index to check.
+     */
+    private void checkIndex(int i)
+    {
+        switch (i) {
+            case ONE:
+            case TWO:
+            case FIVE:
+            case TEN:
+            case TWENTY:
+            case FIFTY:
+            case HUNDRED:
+            case CUSTOMIZED:
+                return;
+            default:
+                throw new IllegalArgumentException("Index not supported.");
+        }
+    }
     
     /**
      * Creates a new instance.
      * 
      * @param model     Reference to the model. Mustn't be <code>null</code>.
-     * @param increase  Pass <code>true</code> to increase the size of the 
-     *                  unit bar, pass <code>false</code> to decrease the size.
+     * @param index     One of the constant defined by this class.
      */
-    public UnitBarSizeAction(ImViewer model, boolean increase)
+    public UnitBarSizeAction(ImViewer model, int index)
     {
         super(model);
-        this.increase = increase;
-        IconManager icons = IconManager.getInstance();
-        if (increase) {
-            putValue(Action.SHORT_DESCRIPTION, 
-                    UIUtilities.formatToolTipText(DESCRIPTION_INCREASE));
-            putValue(Action.SMALL_ICON, icons.getIcon(IconManager.PLUS));
-        } else {
-            putValue(Action.SHORT_DESCRIPTION, 
-                    UIUtilities.formatToolTipText(DESCRIPTION_DECREASE));
-            putValue(Action.SMALL_ICON, icons.getIcon(IconManager.MINUS));
-        }
+        checkIndex(index);
+        this.index = index;
+        putValue(Action.SHORT_DESCRIPTION, 
+                UIUtilities.formatToolTipText(DESCRIPTION));
+        putValue(Action.NAME, names[index]);
     }
     
     /**
-     * Increases or decreases the size of the unit bar depending on the 
-     * value of {@link #increase}.
+     * Sets the size of the unit bar.
      * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
      */
     public void actionPerformed(ActionEvent e)
     {
-        model.setUnitBarSize(increase);
+        if (index != CUSTOMIZED) model.setUnitBarSize(values[index]);
+        else model.showUnitBarSelection();
     }
-    
+
 }
