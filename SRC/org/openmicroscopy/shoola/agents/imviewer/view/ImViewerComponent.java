fc3565c56534b8056c33a9a131b4395823e8037a
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-10-09 13:05:47 +0000

    display SizeBar unit dialog
    git-svn-id: file:///home/svn/shoola-omero/trunk@4043 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/imviewer/view/ImViewerComponent.java
+++ SRC/org/openmicroscopy/shoola/agents/imviewer/view/ImViewerComponent.java
@@ -848,6 +848,7 @@ class ImViewerComponent
     {
         UnitBarSizeDialog d = new UnitBarSizeDialog(view);
         d.addPropertyChangeListener(controller);
+        UIUtilities.centerAndShow(d);
     }
 
 }
