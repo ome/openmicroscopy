7c3a2957e5bd0c8d57be54fd77b1eac1eb447b41
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-10-11 12:14:26 +0000

    implemented the onDisplaychange(TreeImageDisplay) method
    git-svn-id: file:///home/svn/shoola-omero/trunk@4086 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/treeviewer/actions/SortAction.java
+++ SRC/org/openmicroscopy/shoola/agents/treeviewer/actions/SortAction.java
@@ -41,6 +41,7 @@ import javax.swing.Action;
 //Application-internal dependencies
 import org.openmicroscopy.shoola.agents.treeviewer.IconManager;
 import org.openmicroscopy.shoola.agents.treeviewer.browser.Browser;
+import org.openmicroscopy.shoola.agents.treeviewer.browser.TreeImageDisplay;
 import org.openmicroscopy.shoola.util.ui.UIUtilities;
 
 /** 
@@ -61,13 +62,30 @@ public class SortAction
     /** Description of the action. */
     private static final String DESCRIPTION = "Sort by name";
     
-    /** Reacts to {@link Browser}'s state change  */
+    /**
+     * Enables the action depending on the currently selected node.
+     * @see BrowserAction#onDisplayChange(TreeImageDisplay)
+     */
+    protected void onDisplayChange(TreeImageDisplay selectedDisplay)
+    {
+        if (selectedDisplay == null) setEnabled(false);
+        else if (selectedDisplay.containsImages()) {
+            setEnabled(selectedDisplay.isChildrenLoaded());
+        } else setEnabled(false);
+    }
+    
+    /** 
+     * Reacts to {@link Browser}'s state change.
+     *  @see BrowserAction#onStateChange()
+     */
     protected void onStateChange()
     {
         int state = model.getState();
-        if (state == Browser.READY)
-            setEnabled(model.getBrowserType() == Browser.IMAGES_EXPLORER);
-        else setEnabled(false);
+        if (state == Browser.READY) {
+            if (model.getBrowserType() == Browser.IMAGES_EXPLORER) 
+                setEnabled(true);
+            else onDisplayChange(model.getLastSelectedDisplay());
+        } else setEnabled(false);
     }
     
     /**
