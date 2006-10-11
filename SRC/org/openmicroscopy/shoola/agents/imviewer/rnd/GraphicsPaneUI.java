2994a9004c35902fe63c397737ea9549c7427ee5
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-10-11 14:48:13 +0000

    added control
    git-svn-id: file:///home/svn/shoola-omero/trunk@4092 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/imviewer/rnd/GraphicsPaneUI.java
+++ SRC/org/openmicroscopy/shoola/agents/imviewer/rnd/GraphicsPaneUI.java
@@ -71,10 +71,10 @@ class GraphicsPaneUI
     private final static BasicStroke STROKE = new BasicStroke(2.0f);
     
 	/** A temporary image of a histogram */
-	private ImageIcon histogramImage;
+	private ImageIcon       histogramImage;
 
 	/** A reference to the model. */
-    private RendererModel model;
+    private RendererModel   model;
 
 	/** 
      * Creates a new instance. 
@@ -98,23 +98,23 @@ class GraphicsPaneUI
 		Graphics2D g = (Graphics2D) og;
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
-
-		g.drawImage(histogramImage.getImage(), 0, 0, getWidth()-1, 
-				getHeight()-1, null);
-		double width = this.getWidth();
-		double height = this.getHeight();
+        double width = getWidth();
+        double height = getHeight();
+		g.drawImage(histogramImage.getImage(), 0, 0, (int) width-1, 
+                    (int) height-1, null);
+		
 		double codomainMin = model.getCodomainStart();
 		double codomainMax = model.getCodomainEnd();
 		double domainGlobalMax = model.getGlobalMax();
 		double domainMin = model.getWindowStart();
 		double domainMax = model.getWindowEnd();
 		
-		double domainMinScreenX = (domainMin / domainGlobalMax) * width;
-		double domainMaxScreenX = (domainMax / domainGlobalMax) * width;
-		double codomainMinScreenY = ((255 - codomainMin) / 255.0f) * height;
-		double codomainMaxScreenY = ((255 - codomainMax) / 255.0f) * height;
-		double domainRangeScreen = domainMaxScreenX - domainMinScreenX;
-		double codomainRangeScreen = codomainMinScreenY - codomainMaxScreenY;
+		double domainMinScreenX = (domainMin/domainGlobalMax)*width;
+		double domainMaxScreenX = (domainMax/domainGlobalMax)*width;
+		double codomainMinScreenY = ((255-codomainMin)/255.0f)*height;
+		double codomainMaxScreenY = ((255-codomainMax)/255.0f)*height;
+		double domainRangeScreen = domainMaxScreenX-domainMinScreenX;
+		double codomainRangeScreen = codomainMinScreenY-codomainMaxScreenY;
 
 		g.setColor(GREYCOLOUR);
 		g.fillRect(0, 0, (int) domainMinScreenX, (int) height);
@@ -127,15 +127,16 @@ class GraphicsPaneUI
 		double coeff = model.getCurveCoefficient();
 		double a = 0;
 		if (family.equals(RendererModel.LINEAR)) 
-			a = codomainRangeScreen/(domainRangeScreen);
+			a = codomainRangeScreen/domainRangeScreen;
 		else if (family.equals(RendererModel.POLYNOMIAL)) 
 			a = codomainRangeScreen/Math.pow(domainRangeScreen,	coeff);
 		else if (family.equals(RendererModel.EXPONENTIAL)) 
 			a = codomainRangeScreen/Math.exp(Math.pow(domainRangeScreen,
 					coeff));
-		else if (family.equals(RendererModel.LOGARITHMIC)) 
-			a = codomainRangeScreen/Math.log(domainRangeScreen);
-			
+		else if (family.equals(RendererModel.LOGARITHMIC)) {
+            if (domainRangeScreen <= 1) domainRangeScreen = 1;
+            a = codomainRangeScreen/Math.log(domainRangeScreen);
+        }	
 		double b = codomainMinScreenY;
 		double currentX = domainMinScreenX-1;
 		double currentY = b;
@@ -147,16 +148,16 @@ class GraphicsPaneUI
 		for (double x = 1; x < domainRangeScreen; x += 1) {
 				oldX = currentX;
 				oldY = currentY;
-				currentX = x + domainMinScreenX-1;
-				
+				currentX = x+domainMinScreenX-1;
+                //currentX = x;
 				if (family.equals(RendererModel.LINEAR)) 
-					currentY = b - a * x;
+					currentY = b-a*x;
 				else if (family.equals(RendererModel.EXPONENTIAL)) 
-					currentY = b - a * Math.exp(Math.pow(x,	coeff));
+					currentY = b-a*Math.exp(Math.pow(x,	coeff));
 				else if (family.equals(RendererModel.POLYNOMIAL)) 
-					currentY = b - a * Math.pow(x, coeff);
+					currentY = b-a*Math.pow(x, coeff);
 				else if (family.equals(RendererModel.LOGARITHMIC)) 
-					currentY = b - a * Math.log(x);
+					currentY = b-a*Math.log(x);
 				
 				g.drawLine((int) oldX, (int) oldY, (int) currentX,
 						(int) currentY);
