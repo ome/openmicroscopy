24339ee4ccf9965255c8ec511bb32422b8df0139
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-10-16 08:56:07 +0000

    initial commit
    git-svn-id: file:///home/svn/shoola-omero/trunk@4116 6f2cb1de-eb0d-0410-b157-e593188b5901

---
+++ SRC/org/openmicroscopy/shoola/agents/treeviewer/actions/CutAction.java
@@ -0,0 +1,138 @@
+/*
+ * org.openmicroscopy.shoola.agents.treeviewer.actions.CutAction
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.agents.treeviewer.actions;
+
+
+
+
+//Java imports
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+
+//Third-party libraries
+
+//Application-internal dependencies
+import org.openmicroscopy.shoola.agents.treeviewer.IconManager;
+import org.openmicroscopy.shoola.agents.treeviewer.browser.Browser;
+import org.openmicroscopy.shoola.agents.treeviewer.browser.TreeImageDisplay;
+import org.openmicroscopy.shoola.agents.treeviewer.cmd.CutCmd;
+import org.openmicroscopy.shoola.agents.treeviewer.view.TreeViewer;
+import org.openmicroscopy.shoola.util.ui.UIUtilities;
+import pojos.CategoryData;
+import pojos.DataObject;
+import pojos.DatasetData;
+import pojos.ImageData;
+
+/** 
+ * 
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author	Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
+ * @version 3.0
+ * <small>
+ * (<b>Internal version:</b> $Revision: $ $Date: $)
+ * </small>
+ * @since OME2.2
+ */
+public class CutAction
+    extends TreeViewerAction
+{
+
+    /** The name of the action. */
+    private static final String NAME = "Cut";
+    
+    /** The description of the action. */
+    private static final String DESCRIPTION = "Cut the selected elements.";
+    
+    /** 
+     * Sets the action enabled depending on the state of the {@link Browser}.
+     * @see TreeViewerAction#onBrowserStateChange(Browser)
+     */
+    protected void onBrowserStateChange(Browser browser)
+    {
+        if (browser == null) return;
+        switch (browser.getState()) {
+            case Browser.LOADING_DATA:
+            case Browser.LOADING_LEAVES:
+            case Browser.COUNTING_ITEMS:  
+                setEnabled(false);
+                break;
+            default:
+                onDisplayChange(browser.getLastSelectedDisplay());
+                break;
+        }
+    }
+    
+    /**
+     * Sets the action enabled depending on the selected type.
+     * @see TreeViewerAction#onDisplayChange(TreeImageDisplay)
+     */
+    protected void onDisplayChange(TreeImageDisplay selectedDisplay)
+    {
+        if (selectedDisplay == null) {
+            setEnabled(false);
+            return;
+        }
+        Object ho = selectedDisplay.getUserObject(); 
+        if ((ho instanceof DatasetData) ||(ho instanceof ImageData) || 
+             (ho instanceof CategoryData))
+            setEnabled(model.isObjectWritable((DataObject) ho));
+        else setEnabled(false);
+    }
+    
+    /**
+     * Creates a new instance.
+     * 
+     * @param model Reference to the Model. Mustn't be <code>null</code>.
+     */
+    public CutAction(TreeViewer model)
+    {
+        super(model);
+        name = NAME;
+        putValue(Action.NAME, NAME);
+        putValue(Action.SHORT_DESCRIPTION, 
+                UIUtilities.formatToolTipText(DESCRIPTION));
+        IconManager im = IconManager.getInstance();
+        putValue(Action.SMALL_ICON, im.getIcon(IconManager.COPY));
+    }
+    
+    /**
+     * Creates a {@link CutCmd} command to execute the action.
+     * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+        CutCmd cmd = new CutCmd(model);
+        cmd.execute();
+    }
+    
+}
