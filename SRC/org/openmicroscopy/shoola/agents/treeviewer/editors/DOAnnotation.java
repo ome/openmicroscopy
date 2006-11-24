a2b4d1023b418f4698321b19cceef1c5dff5ca53
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2006-11-24 11:40:05 +0000

    remove System.out
    git-svn-id: file:///home/svn/shoola-omero/trunk@4576 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/treeviewer/editors/DOAnnotation.java
+++ SRC/org/openmicroscopy/shoola/agents/treeviewer/editors/DOAnnotation.java
@@ -302,7 +302,6 @@ class DOAnnotation
     private void showSingleAnnotation()
     {
         int index = annotatedByList.getSelectedIndex();
-        System.out.println("index "+index);
         view.handleAnnotationAreaInsert();
         if (index == -1) {
             ExperimenterData details = model.getUserDetails();
@@ -371,8 +370,6 @@ class DOAnnotation
         setComponentsEnabled(true);
         formatUsersList(owners);
         if (userIndex != -1) annotatedByList.setSelectedIndex(userIndex);
-        //annotatedByList.clearSelection();
-        //annotatedByList.setSelectedIndex(userIndex);
         showSingleAnnotation();
     }
     
