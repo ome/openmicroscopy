a4cb35fd05f51b7feff041c730fa1aaeab06894d
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-04-04 18:50:23 +0000

    Applying r1393 to the new importer component
    git-svn-id: file:///home/svn/omero/trunk@1398 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/importer/src/ome/formats/importer/OMEROWrapper.java
+++ components/importer/src/ome/formats/importer/OMEROWrapper.java
@@ -8,9 +8,10 @@ import loci.formats.ChannelSeparator;
 import loci.formats.FormatException;
 import loci.formats.FormatTools;
 import loci.formats.ImageReader;
+import loci.formats.MinMaxCalculator;
 import loci.formats.ReaderWrapper;
 
-public class OMEROWrapper extends ReaderWrapper
+public class OMEROWrapper extends MinMaxCalculator
 {
 	/**
 	 * Reference copy of <i>reader</i> so that we can be compatible with the
@@ -18,26 +19,27 @@ public class OMEROWrapper extends ReaderWrapper
 	 * that we require.
 	 */
 	public ChannelSeparator separator;
-	
+
 	public OMEROWrapper()
 	{
-		reader = separator = new ChannelSeparator(new ImageReader());
+	    reader = separator = new ChannelSeparator(new ImageReader());
+            reader.setColorTableIgnored(true);
 	}
-	
-	/** 
-	 * Obtains an object which represents a given plane within the file. 
-	 * @param id The path to the file. 
-	 * @param no The plane or section within the file to obtain. 
+
+	/**
+	 * Obtains an object which represents a given plane within the file.
+	 * @param id The path to the file.
+	 * @param no The plane or section within the file to obtain.
 	 * @param buf Pre-allocated buffer which has a <i>length</i> that can fit
 	 * the byte count of an entire plane.
-	 * @return an object which represents the plane. 
-	 * @throws FormatException if there is an error parsing the file. 
-	 * @throws IOException if there is an error reading from the file or 
-	 *   acquiring permissions to read the file. 
-	 */ 
-	public Plane2D openPlane2D(String id, int no, byte[] buf) 
-		throws FormatException, IOException 
-	{ 
+	 * @return an object which represents the plane.
+	 * @throws FormatException if there is an error parsing the file.
+	 * @throws IOException if there is an error reading from the file or
+	 *   acquiring permissions to read the file.
+	 */
+	public Plane2D openPlane2D(String id, int no, byte[] buf)
+		throws FormatException, IOException
+	{
 		// FIXME: HACK! The ChannelSeparator isn't exactly what one would call
 		// "complete" so we have to work around the fact that it still copies
 		// all of the plane data (all three channels) from the file if the file
@@ -47,15 +49,15 @@ public class OMEROWrapper extends ReaderWrapper
 			plane = ByteBuffer.wrap(openBytes(id, no));
 		else
 			plane = ByteBuffer.wrap(openBytes(id, no, buf));
-		
+
 		return new Plane2D(plane, getPixelType(id), isLittleEndian(id),
-				           getSizeX(id), getSizeY(id)); 
-	} 
-	
+				           getSizeX(id), getSizeY(id));
+	}
+
 	/**
 	 * Retrieves the global min and max for each channel and sets those values
 	 * in the MetadataStore.
-	 * @param id The path to the file. 
+	 * @param id The path to the file.
 	 * @throws FormatException if there is an error parsing metadata.
 	 * @throws IOException if there is an error reading the file.
 	 */
@@ -64,7 +66,7 @@ public class OMEROWrapper extends ReaderWrapper
 	{
 		int planeSize = getSizeX(id) * getSizeY(id) *
 		FormatTools.getBytesPerPixel(getPixelType(id));
-		
+
 		byte[] buf = new byte[planeSize];
 		for (int c = 0; c < getSizeC(id); c++) {
 			double min = Double.MAX_VALUE;
@@ -75,6 +77,7 @@ public class OMEROWrapper extends ReaderWrapper
 					Plane2D plane = openPlane2D(id, index, buf);
 					for (int x = 0; x < getSizeX(id); x++) {
 						for (int y = 0; y < getSizeY(id); y++) {
+                            //System.err.println("x: " + x + " y: " + y + " z: " + z + " c: " + c + " t: " + t);
 							double pixelValue = plane.getPixelValue(x, y);
 							if (pixelValue < min) min = pixelValue;
 							if (pixelValue > max) max = pixelValue;
@@ -85,11 +88,11 @@ public class OMEROWrapper extends ReaderWrapper
 			getMetadataStore(id).setChannelGlobalMinMax(c, min, max, null);
 		}
 	}
-	
+
 	/**
 	 * Makes sure that the reader's <code>MetadataStore</code> has all the
 	 * relevant metadata that the OMERO server requires.
-	 * @param id The path to the file. 
+	 * @param id The path to the file.
 	 * @throws FormatException if there is an error parsing metadata.
 	 * @throws IOException if there is an error reading the file.
 	 */
