71821f2d65680662bf8bbafdca35e0111e03fafc
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-04-26 12:38:23 +0000

    Missing file.
    git-svn-id: file:///home/svn/omero/trunk@1487 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/blitz/src/cpp/OMERO/errors.h
@@ -0,0 +1,36 @@
+/*
+ *   $Id$
+ *
+ *   Copyright 2007 Glencoe Software, Inc. All rights reserved.
+ *   Use is subject to license terms supplied in LICENSE.txt
+ *
+ */
+
+#include <ostream>
+#include <iostream>
+#include <exception>
+
+namespace omero {
+
+  class ClientError : public std::exception
+  {
+  protected:
+    int _line;
+    const char* _file;
+    const char* _msg;
+  public:
+    ClientError(const char*, int, const char* message);
+    virtual ~ClientError() throw();
+    virtual const char* name() const throw();
+    virtual const char* file() const throw();
+    virtual int line() const throw();
+    virtual char const* what() const throw();
+  };
+
+  class UnloadedEntityException : public ClientError {};
+  class UnloadedCollectionException : public ClientError {};
+  
+}
+
+std::ostream& operator<<(std::ostream&, const omero::ClientError&);
+  
