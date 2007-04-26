8bff80aa387062c9533d31189d6f068384c224bf
david <david@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-04-26 02:40:19 +0000

    fixes ticket #571, test uses omero.data.dir property now
    git-svn-id: file:///home/svn/omero/trunk@1479 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/romio/test/ome/io/nio/utests/PixelServiceCreatesDirectoryUnitTest.java
+++ components/romio/test/ome/io/nio/utests/PixelServiceCreatesDirectoryUnitTest.java
@@ -11,8 +11,10 @@ import ome.io.nio.PixelBuffer;
 import ome.io.nio.PixelsService;
 import ome.model.core.Pixels;
 import ome.model.enums.PixelsType;
-
+import java.util.Properties;
 import junit.framework.TestCase;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class PixelServiceCreatesDirectoryUnitTest extends TestCase {
     private Pixels pixels;
@@ -20,6 +22,8 @@ public class PixelServiceCreatesDirectoryUnitTest extends TestCase {
     private PixelBuffer pixelBuffer;
 
     private PixelsService service;
+	
+	private ApplicationContext testContext;
 
     @Override
     @Configuration(beforeTestMethod = true)
@@ -35,8 +39,11 @@ public class PixelServiceCreatesDirectoryUnitTest extends TestCase {
         PixelsType type = new PixelsType();
         type.setValue("uint16");
         pixels.setPixelsType(type);
-
-        service = new PixelsService(PixelsService.ROOT_DEFAULT);
+		testContext = new FileSystemXmlApplicationContext(
+    			"components/romio/resources/beanRefContext.xml");
+		PropertiesUtil util = (PropertiesUtil) testContext.getBean("properties");
+		String path = util.getProperties().getProperty("omero.data.dir");		
+        service = new PixelsService(path);
     }
 
     @Test
