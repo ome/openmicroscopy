b86b779ff998d51fc3017db4a79f6fe77741dc55
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-05-05 20:50:48 +0000

    More testing of generated C++
    git-svn-id: file:///home/svn/omero/trunk@1518 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/blitz/test/cpp/boost_api.cpp
@@ -0,0 +1,27 @@
+/*
+ *   $Id$
+ *
+ *   Copyright 2007 Glencoe Software, Inc. All rights reserved.
+ *   Use is subject to license terms supplied in LICENSE.txt
+ *
+ */
+
+#include <boost_fixture.h>
+#include <iterator>
+#include <algorithm>
+#include <OMERO/API.h>
+#include <OMERO/System.h>
+
+BOOST_AUTO_TEST_CASE( VectorArgs )
+{
+  Ice::Long ids[] = {1L, 2L, 3L};
+  omero::sys::LongList idList(ids,ids+3);
+  copy(idList.begin(), idList.end(), std::ostream_iterator<Ice::Long>(std::cout, "\n"));
+  omero::sys::ParamMap pm;
+  pm["user"] = new omero::CLong(1L);
+
+  // Need a way to mock
+  //omero::api::IPojosPrx pojoPrx();
+  //pojoPrx->loadContainerHierarchies
+  //("Dataset", idsList, pm);
+}
