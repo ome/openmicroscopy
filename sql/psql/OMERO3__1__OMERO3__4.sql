71a60c412d8767cfbf61d4963d72734611622679
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-06-03 19:41:38 +0000

    #712 Upgrade script for valewalker (OMERO3__1) to trunk (OMERO3__4)
    git-svn-id: file:///home/svn/omero/trunk@1584 05709c45-44f0-0310-885b-81a1db45b4a6

--- sql/psql/OMERO3__1__OMERO3__4.sql
+++ sql/psql/OMERO3__1__OMERO3__4.sql
@@ -1,9 +1,13 @@
 BEGIN;
 
- -- Manually added items
+ --
+ -- Manually added items missed by <schemaupdate/>
+ --
  alter table originalfile alter column size type int8;
 
+ --
  -- Items calculated by <schemaupdate/>
+ --
  create table binning (id int8 not null, owner_id int8 not null, group_id int8 not null, creation_id int8 not null, permissions int8 not null, external_id int8 unique, value varchar(255) not null unique, primary key (id));
  create table coating (id int8 not null, owner_id int8 not null, group_id int8 not null, creation_id int8 not null, permissions int8 not null, external_id int8 unique, value varchar(255) not null unique, primary key (id));
  alter table customizedfilterset add column transmittanceRange int8;
@@ -144,4 +148,262 @@ BEGIN;
  create sequence seq_projectannotation;
  create sequence seq_transmittancerange;
 
-COMMIT;
+--
+-- Calculated by apg_diff
+--
+
+ALTER TABLE objective
+	DROP COLUMN correctioncollar,
+	DROP COLUMN irisdiaphragm,
+	DROP COLUMN aberrationcorrection,
+	DROP COLUMN immersionmedium,
+	DROP COLUMN phasecondenserannulus,
+	ALTER COLUMN serialnumber DROP NOT NULL;
+
+
+DROP SEQUENCE seq_acquisitioncontext;
+
+DROP SEQUENCE seq_correctioncollar;
+
+DROP SEQUENCE seq_dyelasermedia;
+
+DROP SEQUENCE seq_excimerlasermedia;
+
+DROP SEQUENCE seq_freeelectronlasermedia;
+
+DROP SEQUENCE seq_gaslasermedia;
+
+DROP SEQUENCE seq_immersionmedium;
+
+DROP SEQUENCE seq_metalvaporlasermedia;
+
+DROP SEQUENCE seq_semiconductorlasermedia;
+
+DROP TABLE acquisitioncontext CASCADE;
+
+DROP TABLE correctioncollar CASCADE;
+
+DROP TABLE dyelaser CASCADE;
+
+DROP TABLE dyelasermedia CASCADE;
+
+DROP TABLE excimerlaser CASCADE;
+
+DROP TABLE excimerlasermedia CASCADE;
+
+DROP TABLE freeelectronlaser CASCADE;
+
+DROP TABLE freeelectronlasermedia CASCADE;
+
+DROP TABLE gaslaser CASCADE;
+
+DROP TABLE gaslasermedia CASCADE;
+
+DROP TABLE immersionmedium CASCADE;
+
+DROP TABLE metalvaporlaser CASCADE;
+
+DROP TABLE metalvaporlasermedia CASCADE;
+
+DROP TABLE semiconductorlaser CASCADE;
+
+DROP TABLE semiconductorlasermedia CASCADE;
+
+ALTER TABLE arc
+	DROP COLUMN version;
+
+ALTER TABLE contraststretchingcontext
+	DROP COLUMN version;
+
+ALTER TABLE customizedfilterset
+	ALTER COLUMN serialnumber DROP NOT NULL;
+
+ALTER TABLE dbpatch
+	ALTER COLUMN id SET DEFAULT nextval('seq_dbpatch'::regclass),
+	ALTER COLUMN permissions SET DEFAULT -35,
+	ALTER COLUMN message SET DEFAULT 'Updating'::character varying;
+
+ALTER TABLE detector
+	ALTER COLUMN serialnumber DROP NOT NULL;
+
+ALTER TABLE detectorsettings
+	DROP COLUMN readrate,
+	ALTER COLUMN voltage DROP NOT NULL,
+	ALTER COLUMN gain DROP NOT NULL,
+	ALTER COLUMN offsetvalue DROP NOT NULL;
+
+ALTER TABLE dummystatistics
+	DROP COLUMN version;
+
+UPDATE event set permissions = -35;
+UPDATE eventlog set permissions = -35;
+UPDATE experimenter set permissions = -35;
+
+ALTER TABLE event
+	ALTER COLUMN permissions SET NOT NULL;
+
+ALTER TABLE eventlog
+	ALTER COLUMN permissions SET NOT NULL;
+
+ALTER TABLE experimenter
+	ALTER COLUMN permissions SET NOT NULL;
+
+ALTER TABLE filament
+	DROP COLUMN version;
+
+ALTER TABLE filterset
+	ALTER COLUMN serialnumber DROP NOT NULL;
+
+ALTER TABLE image
+	DROP COLUMN acquisition;
+
+ALTER TABLE laser
+	DROP COLUMN version,
+	DROP COLUMN frequencydoubled,
+	ALTER COLUMN type SET NOT NULL,
+	ALTER COLUMN lasermedium SET NOT NULL,
+	ALTER COLUMN frequencymultiplication SET NOT NULL,
+	ALTER COLUMN pockelcell SET NOT NULL;
+
+ALTER TABLE lightsource
+	ALTER COLUMN power SET NOT NULL,
+	ALTER COLUMN serialnumber DROP NOT NULL;
+
+ALTER TABLE microscope
+	ALTER COLUMN serialnumber DROP NOT NULL;
+
+ALTER TABLE overlay
+	DROP COLUMN version;
+
+ALTER TABLE pixels
+	DROP COLUMN acquisitioncontext;
+
+ALTER TABLE planeslicingcontext
+	DROP COLUMN version;
+
+ALTER TABLE reverseintensitycontext
+	DROP COLUMN version;
+
+ALTER TABLE square
+	DROP COLUMN version;
+
+ALTER TABLE uroi
+	DROP COLUMN version;
+
+ALTER TABLE usquare
+	DROP COLUMN version;
+
+ALTER TABLE xy
+	DROP COLUMN version;
+
+ALTER TABLE xyc
+	DROP COLUMN version;
+
+ALTER TABLE xyct
+	DROP COLUMN version;
+
+ALTER TABLE xyt
+	DROP COLUMN version;
+
+ALTER TABLE xyz
+	DROP COLUMN version;
+
+ALTER TABLE xyzc
+	DROP COLUMN version;
+
+ALTER TABLE xyzct
+	DROP COLUMN version;
+
+ALTER TABLE xyzt
+	DROP COLUMN version;
+
+ --
+ -- Calculated by hand
+ --
+
+ insert into binning (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_binning'),-35,0,0,0,'8x8';
+ insert into binning (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_binning'),-35,0,0,0,'4x4';
+ insert into binning (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_binning'),-35,0,0,0,'2x2';
+ insert into binning (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_binning'),-35,0,0,0,'1x1';
+
+ insert into coating (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_coating'),-35,0,0,0,'UV';
+ insert into coating (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_coating'),-35,0,0,0,'SuperFluor';
+ insert into coating (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_coating'),-35,0,0,0,'PlanFluor';
+ insert into coating (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_coating'),-35,0,0,0,'PlanApo';
+
+ insert into frequencymultiplication (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_frequencymultiplication'),-35,0,0,0,'x4';
+ insert into frequencymultiplication (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_frequencymultiplication'),-35,0,0,0,'x3';
+ insert into frequencymultiplication (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_frequencymultiplication'),-35,0,0,0,'x2';
+ insert into frequencymultiplication (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_frequencymultiplication'),-35,0,0,0,'x1';
+
+ insert into immersion (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_immersion'),-35,0,0,0,'Wl';
+ insert into immersion (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_immersion'),-35,0,0,0,'Water';
+ insert into immersion (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_immersion'),-35,0,0,0,'Wasser';
+ insert into immersion (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_immersion'),-35,0,0,0,'Oil';
+ insert into immersion (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_immersion'),-35,0,0,0,'Oel';
+ insert into immersion (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_immersion'),-35,0,0,0,'Hl';
+ insert into immersion (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_immersion'),-35,0,0,0,'Gly';
+
+ insert into jobstatus (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_jobstatus'),-35,0,0,0,'Waiting';
+ insert into jobstatus (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_jobstatus'),-35,0,0,0,'Submitted';
+ insert into jobstatus (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_jobstatus'),-35,0,0,0,'Running';
+ insert into jobstatus (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_jobstatus'),-35,0,0,0,'Resubmitted';
+ insert into jobstatus (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_jobstatus'),-35,0,0,0,'Requeued';
+ insert into jobstatus (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_jobstatus'),-35,0,0,0,'Queued';
+ insert into jobstatus (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_jobstatus'),-35,0,0,0,'Finished';
+ insert into jobstatus (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_jobstatus'),-35,0,0,0,'Error';
+
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'XeFl';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'XeCl';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'XeBr';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'Xe';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'Rhodamine-5G';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'N';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'KrFl';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'KrCl';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'Kr';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'HFl';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'HeNe';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'HeCd';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'H2O';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'GaAs';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'GaAlAs';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'e-';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'Cu';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'Coumaring-C30';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'CO2';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'CO';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'ArFl';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'ArCl';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'Ar';
+ insert into lasermedium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasermedium'),-35,0,0,0,'Ag';
+
+ insert into lasertype (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasertype'),-35,0,0,0,'SolidState';
+ insert into lasertype (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasertype'),-35,0,0,0,'Semiconductor';
+ insert into lasertype (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasertype'),-35,0,0,0,'MetalVapor';
+ insert into lasertype (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasertype'),-35,0,0,0,'Gas';
+ insert into lasertype (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasertype'),-35,0,0,0,'FreeElectron';
+ insert into lasertype (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasertype'),-35,0,0,0,'Excimer';
+ insert into lasertype (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_lasertype'),-35,0,0,0,'Dye';
+
+ insert into medium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_medium'),-35,0,0,0,'Water';
+ insert into medium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_medium'),-35,0,0,0,'Oil';
+ insert into medium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_medium'),-35,0,0,0,'Glycerol';
+ insert into medium (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_medium'),-35,0,0,0,'Air';
+
+ insert into photometricinterpretation (id,permissions,owner_id,group_id,creation_id,value) select nextval('seq_photometricinterpretation'),-35,0,0,0,'ColorMap';
+
+ --
+ -- Copied from OMERO3__4__data.sql
+ --
+
+ alter table dbpatch add constraint unique_dbpatch unique (currentVersion, currentPatch, previousVersion, previousPatch);
+ alter table dbpatch alter id set default nextval('seq_dbpatch'),
+                    alter permissions set default -35,
+                    alter message set default 'Updating';
+ update dbpatch set message = 'Updated.', finished = now() 
+ where currentVersion   = 'OMERO3'    and
+        currentPatch    = 4           and
+        previousVersion = 'OMERO3'    and
+        previousPatch   = 1;
+ commit;
