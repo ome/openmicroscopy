60099a274d03b2ec5166fcca3ca0045d5b055471
david <david@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-06-11 13:56:31 +0000

    ticket #730, fix mistake
    git-svn-id: file:///home/svn/omero/trunk@1610 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/blitz/src/omero/util/IceMapper.java
+++ components/blitz/src/omero/util/IceMapper.java
@@ -93,8 +93,19 @@ public class IceMapper extends ome.util.ModelMapper implements ReverseModelMappe
     }
 
     public static String stackAsString(Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        t.printStackTrace(pw);
         Throwable cause = t.getCause();
-        return cause.getMessage();
+        while (cause != null && cause != t ) {
+            cause.printStackTrace(pw);
+            t = cause;
+            cause = t.getCause();
+        }
+        pw.flush();
+        pw.close();
+
+        return sw.getBuffer().toString();
     }
 
     private static Class<? extends IObject> _class(String className) {
