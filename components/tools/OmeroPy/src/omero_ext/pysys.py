7b88cc0037195180b53531155433febb5651c87e
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-06-19 15:12:43 +0000

    OmeroPy : Added Python sys module wrapper. Now functional.
    git-svn-id: file:///home/svn/omero/trunk@1653 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/tools/OmeroPy/src/omero_ext/pysys.py
@@ -0,0 +1,10 @@
+#
+#   $Id$
+#
+#   Copyright 2007 Glencoe Software, Inc. All rights reserved.
+#   Use is subject to license terms supplied in LICENSE.txt
+#
+
+import sys
+argv = sys.argv
+stderr = sys.stderr
