f6dc1f728c2c2ed05d00e418af67d75d57d024df
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-07-06 13:41:21 +0000

    Ivy changes: * Added licenses/ivy.xml (removed classpath.xml) * Removed all version names from .classpath & .project * Removed versions from antlib/resources/* and product.properties * Updated ome.system.Version
    git-svn-id: file:///home/svn/omero/trunk@1701 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/common/test/ome/system/utests/VersionTest.java
+++ components/common/test/ome/system/utests/VersionTest.java
@@ -16,31 +16,58 @@ import org.testng.annotations.Test;
 
 import junit.framework.TestCase;
 
-@Test(groups="broken")
+@Test
 public class VersionTest extends TestCase {
 
+	public void testGetVersionFromBundle() throws Exception {
+		String versionStr = Version.OMERO;
+		assertNotNull(versionStr);
+	}
+	
+	@Test( groups = {"svnonly","ignore"})
     public void testGetSvnKeyword() throws Exception {
         String versionStr = Version.stringFromSvnString("$Revision$");
         Integer version = Integer.valueOf(versionStr);
         checkVersion(version);
     }
 
-    public void testGetRevision() throws Exception {
+	@Test( groups = {"svnonly","ignore"})
+    public void testGetBlankRevision() throws Exception {
         Integer version = Version.getRevision(Blank.class);
         checkVersion(version);
     }
 
-    public void testGetDate() throws Exception {
+	@Test( groups = {"svnonly","ignore"})
+    public void testGetBlankDate() throws Exception {
         assertNotNull(Version.getDate(Blank.class));
     }
 
-    public void testSvnDateFormat() throws Exception {
+	@Test( groups = {"svnonly","ignore"})
+    public void testBlankSvnDateFormat() throws Exception {
         SimpleDateFormat formatter = new SimpleDateFormat(
                 Version.SVN_DATE_FORMAT);
         String dateStr = Blank.class.getAnnotation(RevisionDate.class).value();
         java.util.Date date = formatter.parse(Version
                 .stringFromSvnString(dateStr));
     }
+    
+    public void testGetDummyRevision() throws Exception {
+        Integer version = Version.getRevision(Dummy.class);
+        checkVersion(version);
+    }
+
+    public void testGetDummyDate() throws Exception {
+        assertNotNull(Version.getDate(Dummy.class));
+    }
+
+    public void testDummySvnDateFormat() throws Exception {
+        SimpleDateFormat formatter = new SimpleDateFormat(
+                Version.SVN_DATE_FORMAT);
+        String dateStr = Dummy.class.getAnnotation(RevisionDate.class).value();
+        java.util.Date date = formatter.parse(Version
+                .stringFromSvnString(dateStr));
+    }
+    
     
     // ~ Helpers
     // =========================================================================
@@ -54,4 +81,9 @@ public class VersionTest extends TestCase {
 @RevisionNumber("$Revision$")
 @RevisionDate("$Date$")
 class Blank {
+}
+
+@RevisionDate("$Date: 1970-01-01 00:00:01 +0100 (Thu, 01 Jan 1970) $")
+@RevisionNumber("$Revision: 1 $")
+class Dummy {
 }
