5ee9133e785d36e11f115cf7ca87348c7e60f6ba
Aleksandra Tarkowska <Aleksandra Tarkowska@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-10-19 14:29:43 +0000

    Test for Category.
    git-svn-id: file:///home/svn/omero/trunk@1864 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/server/test/ome/server/itests/RenderingSettingsTest.java
+++ components/server/test/ome/server/itests/RenderingSettingsTest.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.List;
 
 import ome.api.IRenderingSettings;
+import ome.model.containers.Category;
+import ome.model.containers.CategoryImageLink;
 import ome.model.containers.Dataset;
 import ome.model.containers.DatasetImageLink;
 import ome.model.containers.Project;
@@ -75,25 +77,33 @@ public class RenderingSettingsTest extends AbstractManagedContextTest {
 
 		Project p1 = new Project();
 		p1.setName("prtest1");
+		
 		Project p2 = new Project();
 		p2.setName("prtest2");
+		
 		Dataset d1 = new Dataset();
 		d1.setName("dstest1");
+		p1.linkDataset(d1);
+		
 		Dataset d2 = new Dataset();
 		d2.setName("dstest2");
-		p1.linkDataset(d1);
 		p2.linkDataset(d2);
+		
+		Category c1 = new Category();
+		c1.setName("ctest1");
+		
+		Category c2 = new Category();
+		c2.setName("ctest2");
 
 		// and using proxies works
 		p1 = iUpdate.saveAndReturnObject(p1);
-		//p1 = new Project(p1.getId(), false);
 		p2 = iUpdate.saveAndReturnObject(p2);
-		//p2 = new Project(p2.getId(), false);
 
 		d1 = iUpdate.saveAndReturnObject(d1);
-		//d1 = new Dataset(d1.getId(), false);
 		d2 = iUpdate.saveAndReturnObject(d2);
-		//d2 = new Dataset(d2.getId(), false);
+		
+		c1 = iUpdate.saveAndReturnObject(c1);
+		c2 = iUpdate.saveAndReturnObject(c2);
 		
 		ProjectDatasetLink link1 = new ProjectDatasetLink();
 		link1.link(p1, d1);
@@ -105,14 +115,27 @@ public class RenderingSettingsTest extends AbstractManagedContextTest {
 		DatasetImageLink ilink2 = new DatasetImageLink();
 		ilink2.link(d2, iQuery.get(Image.class, p2.getId()));
 
+		CategoryImageLink clink1 = new CategoryImageLink();
+		clink1.link(c1, iQuery.get(Image.class, p1.getId()));
+
+		CategoryImageLink clink2 = new CategoryImageLink();
+		clink2.link(c2, iQuery.get(Image.class, p2.getId()));
+		
 		iUpdate.saveAndReturnObject(link1);
 		iUpdate.saveAndReturnObject(link2);
 
 		iUpdate.saveAndReturnObject(ilink1);
 		iUpdate.saveAndReturnObject(ilink2);
+		
+		iUpdate.saveAndReturnObject(clink1);
+		iUpdate.saveAndReturnObject(clink2);
 
-		rsx.applySettingsToDataset(d1.getId(), d2.getId());
 		rsx.applySettingsToProject(p1.getId(), p2.getId());
+		rsx.applySettingsToDataset(d1.getId(), d2.getId());
+		rsx.applySettingsToCategory(c1.getId(), c2.getId());
+		
+		rsx.resetDefaultsInDataset(d2.getId());
+		rsx.resetDefaultsInCategory(c2.getId());
 
 	}
 
