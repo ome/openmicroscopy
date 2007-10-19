be53d28ce79faf3e3b6c4c36cda4d5950e122fc2
Aleksandra Tarkowska <Aleksandra Tarkowska@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-10-19 13:40:20 +0000

    RenderingSetting test needs data creation before test.
    git-svn-id: file:///home/svn/omero/trunk@1862 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/server/test/ome/server/itests/RenderingSettingsTest.java
+++ components/server/test/ome/server/itests/RenderingSettingsTest.java
@@ -1,12 +1,37 @@
 package ome.server.itests;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import ome.api.IPixels;
+import ome.api.IQuery;
 import ome.api.IRenderingSettings;
+import ome.conditions.ValidationException;
+import ome.io.nio.PixelBuffer;
+import ome.io.nio.PixelsService;
 import ome.model.containers.Category;
 import ome.model.containers.Dataset;
+import ome.model.containers.DatasetImageLink;
+import ome.model.containers.Project;
+import ome.model.containers.ProjectDatasetLink;
+import ome.model.core.Channel;
 import ome.model.core.Image;
+import ome.model.core.Pixels;
+import ome.model.core.PixelsDimensions;
+import ome.model.display.ChannelBinding;
+import ome.model.display.CodomainMapContext;
+import ome.model.display.QuantumDef;
+import ome.model.display.RenderingDef;
+import ome.model.enums.Family;
+import ome.model.enums.RenderingModel;
+import ome.testing.ObjectFactory;
+import ome.util.PathUtil;
+import omeis.providers.re.Renderer;
+import omeis.providers.re.RenderingEngine;
+import omeis.providers.re.quantum.QuantumFactory;
 
 import org.testng.annotations.Test;
 
@@ -14,41 +39,99 @@ public class RenderingSettingsTest extends AbstractManagedContextTest {
 
 	private IRenderingSettings rsx;
 
+	private Pixels p1;
+
+	private Pixels p2;
+
 	// =========================================================================
 
 	@Override
 	protected void onSetUp() throws Exception {
 		super.onSetUp();
 		rsx = factory.getRenderingSettingsService();
+
+		p1 = makePixels();
+		p2 = makePixels();
+
+	}
+
+	private Pixels makePixels() {
+		Pixels p = ObjectFactory.createPixelGraph(null);
+		p.setDefaultPixels(true);
+
+		RenderingDef rdef = ObjectFactory.createRenderingDef();
+		CodomainMapContext enhancement = ObjectFactory
+				.createPlaneSlicingContext();
+		ChannelBinding binding = ObjectFactory.createChannelBinding();
+
+		List enhancements = Collections.singletonList(enhancement);
+		List bindings = Collections.singletonList(binding);
+
+		rdef.setWaveRendering(bindings);
+		rdef.setSpatialDomainEnhancement(enhancements);
+
+		p.addRenderingDef(rdef);
+		p = factory.getUpdateService().saveAndReturnObject(p);
+		return p;
+
 	}
 
 	@Test
 	public void testApply() {
-		Long from = 1L;
-		Long to = 2L;
 
-		rsx.applySettingsToImage(from, to);
-		rsx.applySettingsToProject(from, to);
-		rsx.applySettingsToDataset(from, to);
-		rsx.applySettingsToCategory(from, to);
+		Long from = p1.getId();
+		Long to = p2.getId();
+
+		rsx.applySettingsToPixel(from, to);
+
+		Image img = iQuery.get(Image.class, to);
+		rsx.resetDefaultsInImage(img.getId());
 	}
-	
+
 	@Test
-	public void testReset() {
-		Long id = 2L;
-		rsx.resetDefaultsInImage(id);
-		rsx.resetDefaultsInCategory(id);
-		rsx.resetDefaultsInDataset(id);
+	public void testApplyToCollections() {
+
+		Project p1 = new Project();
+		p1.setName("prtest1");
+		Project p2 = new Project();
+		p2.setName("prtest2");
+		Dataset d1 = new Dataset();
+		d1.setName("dstest1");
+		Dataset d2 = new Dataset();
+		d2.setName("dstest2");
+		p1.linkDataset(d1);
+		p2.linkDataset(d2);
+
+		// and using proxies works
+		p1 = iUpdate.saveAndReturnObject(p1);
+		//p1 = new Project(p1.getId(), false);
+		p2 = iUpdate.saveAndReturnObject(p2);
+		//p2 = new Project(p2.getId(), false);
+
+		d1 = iUpdate.saveAndReturnObject(d1);
+		//d1 = new Dataset(d1.getId(), false);
+		d2 = iUpdate.saveAndReturnObject(d2);
+		//d2 = new Dataset(d2.getId(), false);
+		
+		ProjectDatasetLink link1 = new ProjectDatasetLink();
+		link1.link(p1, d1);
+		ProjectDatasetLink link2 = new ProjectDatasetLink();
+		link2.link(p2, d2);
+
+		DatasetImageLink ilink1 = new DatasetImageLink();
+		ilink1.link(d1, iQuery.get(Image.class, p1.getId()));
+		DatasetImageLink ilink2 = new DatasetImageLink();
+		ilink2.link(d2, iQuery.get(Image.class, p2.getId()));
+
+		iUpdate.saveAndReturnObject(link1);
+		iUpdate.saveAndReturnObject(link2);
+
+		iUpdate.saveAndReturnObject(ilink1);
+		iUpdate.saveAndReturnObject(ilink2);
+
+		rsx.applySettingsToDataset(d1.getId(), d2.getId());
+		rsx.applySettingsToProject(p1.getId(), p2.getId());
+
 	}
-	
-	@Test
-	public void testSet() {
-		Set<Long> s = new HashSet<Long>();
-		s.add(1L);
-		s.add(2L);
-		rsx.resetDefaultsInSet(Image.class, s);
-		rsx.resetDefaultsInSet(Category.class, s);
-		rsx.resetDefaultsInSet(Dataset.class, s);
-	}
-	
+
 }
