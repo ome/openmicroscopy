b0ae43e682ce78ff8a82aaaa9a51a36827de2db1
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2007-10-26 10:28:54 +0000

    #818 : Workaround for `JBossLoginModule` memory leak
    Now calling IAdmin.sychronizeLoginCache() every 30 minutes
    to keep memory usage low. Have added comment to (closed)
    JBoss ticket.
    
    git-svn-id: file:///home/svn/omero/trunk@1890 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/client/test/ome/client/itests/EjbTest.java
+++ components/client/test/ome/client/itests/EjbTest.java
@@ -6,16 +6,16 @@
  */
 package ome.client.itests;
 
-import org.testng.annotations.*;
 import java.util.Date;
 
 import junit.framework.TestCase;
-
 import ome.api.IUpdate;
 import ome.model.containers.Project;
 import ome.system.ServiceFactory;
 
-@Test(groups = { "client", "integration" })
+import org.testng.annotations.Test;
+
+@Test(groups = { "client", "integration", "manual" })
 public class EjbTest extends TestCase {
 
     ServiceFactory sf = new ServiceFactory();
@@ -26,6 +26,22 @@ public class EjbTest extends TestCase {
         Project p = new Project();
         p.setName("ejb test:" + new Date());
         iUpdate.saveObject(p);
+    }
+
+    @Test(groups = "ticket:818")
+    public void testMemoryLeak() throws Exception {
+        sf.getAdminService().getEventContext();
+        Thread t = null;
+        for (int i = 0; i < 100; i++) {
+            t = new Thread() {
+                @Override
+                public void run() {
+                    sf.getConfigService().getServerTime();
+                };
+            };
+            t.run();
+        }
+        t.join();
     }
 
 }
