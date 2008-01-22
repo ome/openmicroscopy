f048be7eda590d0375b4ac52a3f18a21f3f18f74
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2008-01-22 16:38:03 +0000

    QueryBuilder : Simple `StringBuilder` based query builder to overcome Criteria dificienies
    git-svn-id: file:///home/svn/omero/trunk@2094 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/server/src/ome/tools/hibernate/QueryBuilder.java
@@ -0,0 +1,229 @@
+/*
+ *   $Id$
+ *
+ *   Copyright 2008 Glencoe Software, Inc. All rights reserved.
+ *   Use is subject to license terms supplied in LICENSE.txt
+ */
+
+package ome.tools.hibernate;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import ome.conditions.ApiUsageException;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ * Very thin wrapper around a {@link StringBuilder} to generate HQL queries.
+ * This comes from the very real deficiencies of the Criteria API when trying to
+ * implement ome.services.SearchBean.
+ * 
+ * Note: It is the responsibility of each method here to end with a blank space,
+ * meaning that each method may also begin without one.
+ * 
+ * @author Josh Moore, josh at glencoesoftware.com
+ * @since 3.0-Beta3
+ */
+public class QueryBuilder {
+
+    private final StringBuilder sb;
+
+    private final Map<String, Object> params = new HashMap<String, Object>();
+
+    private final Map<String, Collection> listParams = new HashMap<String, Collection>();
+
+    private String self;
+
+    /** Booleans which represent what is already complete */
+    private boolean select, from, join, where, order, group;
+
+    public QueryBuilder() {
+        sb = new StringBuilder();
+    }
+
+    public QueryBuilder(int size) {
+        sb = new StringBuilder(size);
+    }
+
+    public void throwUsage() throws ApiUsageException {
+        StringBuilder err = new StringBuilder();
+        err.append("It is required to call in the following order:\n");
+        err.append("select, from, join*, where, [and,or]*, order*, group*");
+        throw new ApiUsageException(err.toString());
+    }
+
+    /**
+     * Simple delegate method to allow appending arbitrary strings.
+     */
+    public QueryBuilder append(String string) {
+        sb.append(string);
+        return this;
+    }
+
+    public QueryBuilder select(String... selects) {
+        if (select || from || join || where || order || group) {
+            throwUsage();
+        }
+
+        if (selects == null || selects.length == 0) {
+            throwUsage();
+        }
+
+        sb.append("select ");
+        for (int i = 0; i < selects.length; i++) {
+            sb.append(selects[i]);
+            if (i > 0) {
+                sb.append(", ");
+            }
+        }
+        appendSpace();
+        select = true;
+        return this;
+    }
+
+    /**
+     * 
+     * @param type
+     * @param alias
+     * @return
+     */
+    public QueryBuilder from(String type, String alias) {
+        if (!select || join || where || order || group) {
+            throwUsage();
+        }
+        this.self = alias;
+        sb.append("from ");
+        sb.append(type);
+        appendSpace();
+        sb.append("as ");
+        sb.append(alias);
+        appendSpace();
+        select = true;
+        from = true;
+        return this;
+    }
+
+    public QueryBuilder join(String path, String alias, boolean outer,
+            boolean fetch) {
+        if (!select || !from || where || order || group) {
+            throwUsage();
+        }
+        if (outer) {
+            sb.append("left outer ");
+        }
+        sb.append("join ");
+        if (fetch) {
+            sb.append("fetch ");
+        }
+        sb.append(path);
+        appendSpace();
+        sb.append("as ");
+        sb.append(alias);
+        appendSpace();
+        return this;
+    }
+
+    /**
+     * Marks the end of all fetches by adding a "where" clause to the string.
+     */
+    public QueryBuilder where() {
+        if (!select || !from || where || order || group) {
+            throwUsage();
+        }
+        sb.append("where ");
+        join = true;
+        return this;
+    }
+
+    /**
+     * Appends "and" plus your string unless this is the first where-spec in
+     * which case it is simply appended.
+     * 
+     * @param str
+     * @return
+     */
+    public QueryBuilder and(String str) {
+        if (!select || !from || !join || order || group) {
+            throwUsage();
+        }
+        if (!where) {
+            sb.append(str);
+            where = true;
+        } else {
+            sb.append("and ");
+            sb.append(str);
+        }
+        appendSpace();
+        return this;
+    }
+
+    /**
+     * Appends "or" plus your string unless this is the first where-spec in
+     * which case it is simply appended.
+     * 
+     * @param str
+     * @return
+     */
+    public QueryBuilder or(String str) {
+        if (!select || !from || !join || order || group) {
+            throwUsage();
+        }
+        if (!where) {
+            sb.append(str);
+            where = true;
+        } else {
+            sb.append("or ");
+            sb.append(str);
+        }
+        appendSpace();
+        return this;
+    }
+
+    public QueryBuilder order(String path, boolean ascending) {
+        if (!select || !from || !join || !where || group) {
+            throwUsage();
+        }
+        sb.append("order by ");
+        sb.append(path);
+        appendSpace();
+        if (ascending) {
+            sb.append("asc ");
+        } else {
+            sb.append("desc ");
+        }
+        order = true;
+        return this;
+    }
+
+    public QueryBuilder param(String key, Object o) {
+        params.put(key, o);
+        return this;
+    }
+
+    public QueryBuilder paramList(String key, Collection c) {
+        listParams.put(key, c);
+        return this;
+    }
+
+    public Query query(Session session) {
+        Query q = session.createQuery(sb.toString());
+        for (String key : params.keySet()) {
+            q.setParameter(key, params.get(key));
+        }
+        for (String key : listParams.keySet()) {
+            q.setParameterList(key, listParams.get(key));
+        }
+        return q;
+    }
+
+    protected QueryBuilder appendSpace() {
+        if (sb.charAt(sb.length() - 1) != ' ') {
+            sb.append(' ');
+        }
+        return this;
+
+    }
+}
---
+++ components/server/test/ome/server/utests/QueryBuilderMockTest.java
@@ -0,0 +1,84 @@
+/*
+ *   $Id$
+ *
+ *   Copyright 2008 Glencoe Software, Inc. All rights reserved.
+ *   Use is subject to license terms supplied in LICENSE.txt
+ */
+package ome.server.utests;
+
+import java.util.Date;
+import java.util.HashSet;
+
+import ome.tools.hibernate.QueryBuilder;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * @author Josh Moore, josh at glencoesoftware.com
+ * @since 3.0-Beta3
+ */
+public class QueryBuilderMockTest extends MockObjectTestCase {
+
+    protected Mock mockSession, mockQuery;
+
+    @Override
+    @BeforeMethod
+    protected void setUp() throws Exception {
+        super.setUp();
+        createMocks();
+    }
+
+    @Override
+    @AfterMethod
+    protected void tearDown() throws Exception {
+        super.verify();
+        super.tearDown();
+    }
+
+    protected void createMocks() {
+        mockSession = mock(Session.class);
+        mockQuery = mock(Query.class);
+        mockSession.expects(once()).method("createQuery").will(
+                returnValue(mockQuery.proxy()));
+
+    }
+
+    protected Session session() {
+        return (Session) mockSession.proxy();
+    }
+
+    @Test
+    public void testBasicUsage() {
+
+        QueryBuilder qb = new QueryBuilder();
+
+        qb.select("img");
+        qb.from("Image", "img");
+        qb.join("img.pixels", "pix", true, false);
+        qb.join("img.datasetImageLinks", "dil", false, false);
+        qb.join("pix.pixelsType", "pt", true, true);
+        // qb.cond(Dataset.class.equals(cls)).fetch("img.datasetLink")
+
+        // Can't join anymore after this
+        qb.where(); // First
+        qb.append("(");
+        qb.and("pt.details.creationTime > :time");
+        qb.param("time", new Date());
+        qb.append(")");
+        qb.and("img.id in (:ids)");
+        qb.paramList("ids", new HashSet());
+        qb.order("img.id", true);
+
+        mockQuery.expects(once()).method("setParameter").with(this.ANYTHING,
+                this.ANYTHING);
+        mockQuery.expects(once()).method("setParameterList").with(
+                this.ANYTHING, this.ANYTHING);
+        Query q = qb.query(session());
+    }
+}
