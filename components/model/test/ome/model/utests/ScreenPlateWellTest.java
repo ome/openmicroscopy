75b6073dd775b0bc66302e1ccf251f3a9dba4f3b
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2008-02-15 21:45:04 +0000

    ScreenPlateWell : Modifications for ticket:876
    git-svn-id: file:///home/svn/omero/trunk@2194 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/model/test/ome/model/utests/ScreenPlateWellTest.java
@@ -0,0 +1,105 @@
+/*
+ *   $Id$
+ *
+ *   Copyright 2008 Glencoe Software, Inc. All rights reserved.
+ *   Use is subject to license terms supplied in LICENSE.txt
+ */
+package ome.model.utests;
+
+import junit.framework.TestCase;
+import ome.model.core.Image;
+import ome.model.screen.Plate;
+import ome.model.screen.Reagent;
+import ome.model.screen.Screen;
+import ome.model.screen.ScreenAcquisition;
+import ome.model.screen.Well;
+import ome.model.screen.WellSample;
+
+import org.testng.annotations.Test;
+
+public class ScreenPlateWellTest extends TestCase {
+
+    Screen screen = new Screen();
+
+    Plate plate1 = new Plate(), plate2 = new Plate();
+
+    Well well1_1 = new Well(), well1_2 = new Well(), well2_1 = new Well(),
+            well2_2 = new Well();
+
+    WellSample ws1_1a = new WellSample(), ws1_1b = new WellSample(),
+            ws1_2a = new WellSample(), ws1_2b = new WellSample(),
+            ws2_1a = new WellSample(), ws2_1b = new WellSample(),
+            ws2_2a = new WellSample(), ws2_2b = new WellSample();
+
+    ScreenAcquisition acq1 = new ScreenAcquisition(),
+            acq2 = new ScreenAcquisition(), acq3 = new ScreenAcquisition(),
+            acq4 = new ScreenAcquisition();
+
+    Reagent reagentA = new Reagent(), reagentB = new Reagent(),
+            reagentC = new Reagent();
+
+    Image i1_1a = new Image(), i1_1b = new Image(), i1_2a = new Image(),
+            i1_2b = new Image(), i2_1a = new Image(), i2_1b = new Image(),
+            i2_2a = new Image(), i2_2b = new Image();
+
+    @Test
+    public void testBuildingAScreen() {
+
+        screen.setName("screen-name");
+        screen.setDescription("screen-description");
+
+        screen.linkPlate(plate1);
+        screen.linkPlate(plate2);
+
+        plate1.addWell(well1_1);
+        plate1.addWell(well1_2);
+
+        plate2.addWell(well2_1);
+        plate2.addWell(well2_2);
+
+        well1_1.addWellSample(ws1_1a);
+        well1_1.addWellSample(ws1_1b);
+
+        well1_2.addWellSample(ws1_2a);
+        well1_2.addWellSample(ws1_2b);
+
+        well2_1.addWellSample(ws2_1a);
+        well2_1.addWellSample(ws2_1b);
+
+        well2_2.addWellSample(ws2_2a);
+        well2_2.addWellSample(ws2_2b);
+
+        ws1_1a.linkImage(i1_1a);
+        ws1_1b.linkImage(i1_1b);
+        ws1_2a.linkImage(i1_2a);
+        ws1_2b.linkImage(i1_2b);
+        ws2_1a.linkImage(i2_1a);
+        ws2_1b.linkImage(i2_1b);
+        ws2_2a.linkImage(i2_2a);
+        ws2_2b.linkImage(i2_2b);
+
+        screen.addScreenAcquisition(acq1);
+        screen.addScreenAcquisition(acq2);
+        screen.addScreenAcquisition(acq3);
+        screen.addScreenAcquisition(acq4);
+
+        acq1.addWellSample(ws1_1a);
+        acq1.addWellSample(ws1_1b);
+        acq1.addWellSample(ws1_2a);
+        acq1.addWellSample(ws1_2b);
+        acq2.addWellSample(ws2_1a);
+        acq2.addWellSample(ws2_1b);
+        acq3.addWellSample(ws2_2a);
+        acq4.addWellSample(ws2_2b);
+
+        reagentA.linkScreen(screen);
+        reagentB.linkScreen(screen);
+        reagentC.linkScreen(screen);
+
+        reagentA.addWell(well1_1);
+        reagentA.addWell(well2_2);
+        reagentB.addWell(well2_1);
+        reagentC.addWell(well1_2);
+
+    }
+}
