2dd40c0df2216ad4fb4d948ee60f2bc280f1be0b
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2008-02-25 10:00:36 +0000

    DbUpgrade Adding `FullText` eventtype to upgrade scripts for OMERO3A__4
    git-svn-id: file:///home/svn/omero/trunk@2266 05709c45-44f0-0310-885b-81a1db45b4a6

--- sql/psql/OMERO3A__4/OMERO3A__3.sql
+++ sql/psql/OMERO3A__4/OMERO3A__3.sql
@@ -7,8 +7,11 @@ BEGIN;
 
 ALTER TABLE annotation RENAME COLUMN name TO ns;
 
-INSERT INTO FORMAT (id,permissions,owner_id,group_id,creation_id,value)
+INSERT INTO format (id,permissions,owner_id,group_id,creation_id,value)
     SELECT nextval('seq_format'),-35,0,0,0,'application/pdf';
+
+INSERT INTO eventtype (id,permissions,owner_id,group_id,creation_id,value)
+        SELECT nextval('seq_eventtype'),-35,0,0,0,'FullText';
 
 INSERT INTO dbpatch (currentVersion, currentPatch, previousVersion, previousPatch, message, finished)
         VALUES ('OMERO3A',  4, 'OMERO3A', 3, 'Database updated.', now());
--- sql/psql/OMERO3A__4/OMERO3__5.sql
+++ sql/psql/OMERO3A__4/OMERO3__5.sql
@@ -1891,6 +1891,9 @@ ALTER TABLE annotation RENAME COLUMN name TO ns;
 INSERT INTO format (id,permissions,owner_id,group_id,creation_id,value)
     SELECT nextval('seq_format'),-35,0,0,0,'application/pdf';
 
+INSERT INTO eventtype (id,permissions,owner_id,group_id,creation_id,value)
+        SELECT nextval('seq_eventtype'),-35,0,0,0,'FullText';
+
 INSERT INTO dbpatch (currentVersion, currentPatch, previousVersion, previousPatch, message, finished)
         VALUES ('OMERO3A',  4, 'OMERO3', 5, 'Database updated.', now());
 
