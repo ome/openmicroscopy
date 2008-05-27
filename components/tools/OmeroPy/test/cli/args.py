ab9639869a71abffb61fc35971d1123ab27220d2
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2008-05-27 08:39:48 +0000

    OmeroShell : Adding minor tests before commit
    git-svn-id: file:///home/svn/omero/trunk@2423 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/tools/OmeroPy/test/cli/args.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+"""
+   Test of the omero cli argument passing logic. It is assumed that
+   all dispatchable methods, i.e. all methods on a control object
+   which are callable by the user (typically these are the methods
+   which start with an alpha character), will take a single argument
+   of type string list. This is what the default __call__ assumes.
+
+   Individual controls are allowed to override this logic.
+
+   Copyright 2008 Glencoe Software, Inc. All rights reserved.
+   Use is subject to license terms supplied in LICENSE.txt
+
+"""
+import unittest, os, subprocess, StringIO
+from exceptions import Exception as Exc
+from path import path
+from omero.cli import Context, BaseControl, Arguments
+
+omeroDir = path(os.getcwd()) / "build"
+
+class TestArgs(unittest.TestCase):
+
+    def testArgumentsCtors(self):
+        Arguments()
+        Arguments("a string")
+        Arguments("a list of strings".split())
+        Arguments(*(["list2"],))
+
+        # Failing calls
+        self.assertRaises(Exc, lambda: Arguments({"a":"map"}))
+        self.assertRaises(Exc, lambda: Arguments(*([],[])))
+
+    def testPop(self):
+
+        # None
+        self.assertRaises(Exc, lambda: Arguments().popFirst())
+
+        # One
+        a = Arguments(["1"])
+        a.popFirst()
+        self.assertRaises(Exc, lambda: a.popFirst())
+
+    def testCmdArgs(self):
+
+        a = Arguments("a")
+        cmd, args = a.firstOther()
+        self.assert_( cmd == "a" )
+        self.assert_( args == [] )
+
+        a = Arguments(["a","b"])
+        cmd, args = a.firstOther()
+        self.assert_( cmd == "a" )
+        self.assert_( args == ["b"] )
+
+        a = Arguments("a b c")
+        cmd, args = a.firstOther()
+        self.assert_( cmd == "a" )
+        self.assert_( args == ["b", "c"] )
+
+    def testIteration(self):
+        a = Arguments("a b c")
+        to_find = ["a","b","c"]
+        for f in a:
+            to_find.remove(f)
+        self.assert_(len(to_find) == 0, str(f))
+
+    def testLength(self):
+        a = Arguments(" a b c ")
+        self.assert_(len(a) == 3)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ components/tools/OmeroPy/test/cli/rcode.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+   Test of the return code functionality
+
+   Copyright 2008 Glencoe Software, Inc. All rights reserved.
+   Use is subject to license terms supplied in LICENSE.txt
+
+"""
+
+import unittest, os, subprocess, StringIO
+from path import path
+from omero.cli import Context, BaseControl, CLI
+
+omeroDir = path(os.getcwd()) / "build"
+
+class TestRCode(unittest.TestCase):
+
+    class T(BaseControl):
+        def __call__(self, *args):
+            self.ctx.rv = 1
+
+    def testOne(self):
+        cli = CLI()
+        cli.register("t", TestRCode.T)
+        cli.invoke(["t"])
+        self.assert_(cli.rv == 1, cli.rv)
+if __name__ == '__main__':
+    unittest.main()
