03ead68412620194fa0cbcb0b18996c227b3fda2
cblackburn <cblackburn@05709c45-44f0-0310-885b-81a1db45b4a6>
2008-06-19 09:25:47 +0000

    Updated  slice-generated python module
    git-svn-id: file:///home/svn/omero/trunk@2519 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroFS/omerofs_ice.py
+++ components/tools/OmeroFS/omerofs_ice.py
@@ -92,15 +92,16 @@ if not _M_monitors.__dict__.has_key('MonitorServer'):
         # def startMonitor(self, id, current=None):
         # def stopMonitor(self, id, current=None):
         # def destroyMonitor(self, id, current=None):
-        # def getDirectory(self, id, path, filter, current=None):
-        # def getBaseName(self, id, fileId, current=None):
-        # def getStats(self, id, fileId, current=None):
-        # def getSize(self, id, fileId, current=None):
-        # def getOwner(self, id, fileId, current=None):
-        # def getCTime(self, id, fileId, current=None):
-        # def getMTime(self, id, fileId, current=None):
-        # def getATime(self, id, fileId, current=None):
-        # def readBlock(self, id, fileId, offset, size, current=None):
+        # def getMonitorDirectory(self, id, relPath, filter, current=None):
+        # def getDirectory(self, absPath, filter, current=None):
+        # def getBaseName(self, fileId, current=None):
+        # def getStats(self, fileId, current=None):
+        # def getSize(self, fileId, current=None):
+        # def getOwner(self, fileId, current=None):
+        # def getCTime(self, fileId, current=None):
+        # def getMTime(self, fileId, current=None):
+        # def getATime(self, fileId, current=None):
+        # def readBlock(self, fileId, offset, size, current=None):
 
         def __str__(self):
             return IcePy.stringify(self, _M_monitors._t_MonitorServer)
@@ -122,32 +123,35 @@ if not _M_monitors.__dict__.has_key('MonitorServer'):
         def destroyMonitor(self, id, _ctx=None):
             return _M_monitors.MonitorServer._op_destroyMonitor.invoke(self, (id, ), _ctx)
 
-        def getDirectory(self, id, path, filter, _ctx=None):
-            return _M_monitors.MonitorServer._op_getDirectory.invoke(self, (id, path, filter), _ctx)
+        def getMonitorDirectory(self, id, relPath, filter, _ctx=None):
+            return _M_monitors.MonitorServer._op_getMonitorDirectory.invoke(self, (id, relPath, filter), _ctx)
 
-        def getBaseName(self, id, fileId, _ctx=None):
-            return _M_monitors.MonitorServer._op_getBaseName.invoke(self, (id, fileId), _ctx)
+        def getDirectory(self, absPath, filter, _ctx=None):
+            return _M_monitors.MonitorServer._op_getDirectory.invoke(self, (absPath, filter), _ctx)
 
-        def getStats(self, id, fileId, _ctx=None):
-            return _M_monitors.MonitorServer._op_getStats.invoke(self, (id, fileId), _ctx)
+        def getBaseName(self, fileId, _ctx=None):
+            return _M_monitors.MonitorServer._op_getBaseName.invoke(self, (fileId, ), _ctx)
 
-        def getSize(self, id, fileId, _ctx=None):
-            return _M_monitors.MonitorServer._op_getSize.invoke(self, (id, fileId), _ctx)
+        def getStats(self, fileId, _ctx=None):
+            return _M_monitors.MonitorServer._op_getStats.invoke(self, (fileId, ), _ctx)
 
-        def getOwner(self, id, fileId, _ctx=None):
-            return _M_monitors.MonitorServer._op_getOwner.invoke(self, (id, fileId), _ctx)
+        def getSize(self, fileId, _ctx=None):
+            return _M_monitors.MonitorServer._op_getSize.invoke(self, (fileId, ), _ctx)
 
-        def getCTime(self, id, fileId, _ctx=None):
-            return _M_monitors.MonitorServer._op_getCTime.invoke(self, (id, fileId), _ctx)
+        def getOwner(self, fileId, _ctx=None):
+            return _M_monitors.MonitorServer._op_getOwner.invoke(self, (fileId, ), _ctx)
 
-        def getMTime(self, id, fileId, _ctx=None):
-            return _M_monitors.MonitorServer._op_getMTime.invoke(self, (id, fileId), _ctx)
+        def getCTime(self, fileId, _ctx=None):
+            return _M_monitors.MonitorServer._op_getCTime.invoke(self, (fileId, ), _ctx)
 
-        def getATime(self, id, fileId, _ctx=None):
-            return _M_monitors.MonitorServer._op_getATime.invoke(self, (id, fileId), _ctx)
+        def getMTime(self, fileId, _ctx=None):
+            return _M_monitors.MonitorServer._op_getMTime.invoke(self, (fileId, ), _ctx)
 
-        def readBlock(self, id, fileId, offset, size, _ctx=None):
-            return _M_monitors.MonitorServer._op_readBlock.invoke(self, (id, fileId, offset, size), _ctx)
+        def getATime(self, fileId, _ctx=None):
+            return _M_monitors.MonitorServer._op_getATime.invoke(self, (fileId, ), _ctx)
+
+        def readBlock(self, fileId, offset, size, _ctx=None):
+            return _M_monitors.MonitorServer._op_readBlock.invoke(self, (fileId, offset, size), _ctx)
 
         def checkedCast(proxy, facetOrCtx=None, _ctx=None):
             return _M_monitors.MonitorServerPrx.ice_checkedCast(proxy, '::monitors::MonitorServer', facetOrCtx, _ctx)
@@ -166,15 +170,16 @@ if not _M_monitors.__dict__.has_key('MonitorServer'):
     MonitorServer._op_startMonitor = IcePy.Operation('startMonitor', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string),), (), IcePy._t_bool, ())
     MonitorServer._op_stopMonitor = IcePy.Operation('stopMonitor', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string),), (), IcePy._t_bool, ())
     MonitorServer._op_destroyMonitor = IcePy.Operation('destroyMonitor', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string),), (), IcePy._t_bool, ())
-    MonitorServer._op_getDirectory = IcePy.Operation('getDirectory', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string), ((), IcePy._t_string)), (), _M_Ice._t_StringSeq, ())
-    MonitorServer._op_getBaseName = IcePy.Operation('getBaseName', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string)), (), IcePy._t_string, ())
-    MonitorServer._op_getStats = IcePy.Operation('getStats', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string)), (), _M_monitors._t_FileStats, ())
-    MonitorServer._op_getSize = IcePy.Operation('getSize', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string)), (), IcePy._t_long, ())
-    MonitorServer._op_getOwner = IcePy.Operation('getOwner', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string)), (), IcePy._t_string, ())
-    MonitorServer._op_getCTime = IcePy.Operation('getCTime', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string)), (), IcePy._t_float, ())
-    MonitorServer._op_getMTime = IcePy.Operation('getMTime', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string)), (), IcePy._t_float, ())
-    MonitorServer._op_getATime = IcePy.Operation('getATime', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string)), (), IcePy._t_float, ())
-    MonitorServer._op_readBlock = IcePy.Operation('readBlock', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string), ((), IcePy._t_long), ((), IcePy._t_int)), (), _M_Ice._t_ByteSeq, ())
+    MonitorServer._op_getMonitorDirectory = IcePy.Operation('getMonitorDirectory', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string), ((), IcePy._t_string)), (), _M_Ice._t_StringSeq, ())
+    MonitorServer._op_getDirectory = IcePy.Operation('getDirectory', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_string)), (), _M_Ice._t_StringSeq, ())
+    MonitorServer._op_getBaseName = IcePy.Operation('getBaseName', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string),), (), IcePy._t_string, ())
+    MonitorServer._op_getStats = IcePy.Operation('getStats', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string),), (), _M_monitors._t_FileStats, ())
+    MonitorServer._op_getSize = IcePy.Operation('getSize', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string),), (), IcePy._t_long, ())
+    MonitorServer._op_getOwner = IcePy.Operation('getOwner', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string),), (), IcePy._t_string, ())
+    MonitorServer._op_getCTime = IcePy.Operation('getCTime', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string),), (), IcePy._t_float, ())
+    MonitorServer._op_getMTime = IcePy.Operation('getMTime', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string),), (), IcePy._t_float, ())
+    MonitorServer._op_getATime = IcePy.Operation('getATime', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string),), (), IcePy._t_float, ())
+    MonitorServer._op_readBlock = IcePy.Operation('readBlock', Ice.OperationMode.Normal, Ice.OperationMode.Normal, False, (), (((), IcePy._t_string), ((), IcePy._t_long), ((), IcePy._t_int)), (), _M_Ice._t_ByteSeq, ())
 
     _M_monitors.MonitorServer = MonitorServer
     del MonitorServer
