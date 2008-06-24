ef6fda9197749503dd8524a3f628005a1fda594c
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2008-06-24 09:12:41 +0000

    Removing bioformats-omero. Now replaced by importer and OmeroImporter
    git-svn-id: file:///home/svn/omero/trunk@2536 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/bioformats-omero/src/ome/formats/importer/IObservable.java
+++
@@ -1,9 +0,0 @@
-package ome.formats.importer;
-
-
-public interface IObservable
-{
-    boolean addObserver(IObserver object);
-    boolean deleteObserver(IObserver object);
-    void notifyObservers(Object message, Object[] args);
-}
--- components/bioformats-omero/src/ome/formats/importer/IObserver.java
+++
@@ -1,7 +0,0 @@
-package ome.formats.importer;
-
-
-public interface IObserver
-{
-    void update(IObservable importLibrary, Object message, Object[] args);
-}
