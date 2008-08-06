fc884887dbbc364457566fa79de29324cc744d98
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2008-08-06 12:17:57 +0000

    ScalingOmero : Reducing the number of calls to `FileSystem.size()`
    In all the binary __methods__, `IRepoInfo.sanityCheckRepository()`
    is called to throw a `ResourceError` in case the disk is filling
    up. However, there is no reason for multiple threads to be
    concurrently calling this. (While importing tinyTest, the sanity
    check was taking >80% of the profiled total time)
    
    Here, only one thread will ever call sanity check within a
    variable time frame. If we would like to keep the value more
    up-to-date, we can add a background thread to peridically call
    sanityCheckRepository(). A further benefit: the `ResourceError`
    will then show up in the log as soon as the disk fills up, rather
    than when a user tries to save something.
    
    
    git-svn-id: file:///home/svn/omero/trunk@2675 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/server/src/ome/logic/RepositoryInfoImpl.java
+++ components/server/src/ome/logic/RepositoryInfoImpl.java
@@ -50,197 +50,269 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Stateless
 @Remote(IRepositoryInfo.class)
-@RemoteBindings({
-    @RemoteBinding(jndiBinding = "omero/remote/ome.api.IRepositoryInfo"),
-    @RemoteBinding(jndiBinding = "omero/secure/ome.api.IRepositoryInfo",
-		   clientBindUrl="sslsocket://0.0.0.0:3843")
-})
+@RemoteBindings( {
+        @RemoteBinding(jndiBinding = "omero/remote/ome.api.IRepositoryInfo"),
+        @RemoteBinding(jndiBinding = "omero/secure/ome.api.IRepositoryInfo", clientBindUrl = "sslsocket://0.0.0.0:3843") })
 @Local(IRepositoryInfo.class)
 @LocalBinding(jndiBinding = "omero/local/ome.api.IRepositoryInfo")
 @Interceptors( { SimpleLifecycle.class })
 public class RepositoryInfoImpl extends AbstractLevel2Service implements
-		IRepositoryInfo {
+        IRepositoryInfo {
 
-	/* The logger for this class. */
-	private transient static Log log = LogFactory
-			.getLog(RepositoryInfoImpl.class);
+    /**
+     * Time (2 minutes) between successive calls to
+     * {@link #sanityCheckRepository()} needed to trigger an actual call to
+     * {@link #getUsageFraction()}
+     */
+    public final static long INITIAL_DELAY = 2 * 60 * 1000L;
 
-	/* repository filesystem */
-	private transient String datadir;
+    /**
+     * Percentage (100.0 - 0.0) of disk use which will cause an exception during
+     * {@link #sanityCheckRepository()}
+     */
+    public final static double CRITICAL_USAGE = 95.0;
 
-	/* The ROMIO thumbnail service. */
-	private transient ThumbnailService thumbnailService;
+    /* The logger for this class. */
+    private transient static Log log = LogFactory
+            .getLog(RepositoryInfoImpl.class);
 
-	/* The ROMIO pixels service. */
-	private transient PixelsService pixelsService;
+    /* repository filesystem */
+    private transient String datadir;
 
-	/* The ROMIO file service. */
-	private transient OriginalFilesService fileService;
+    /* The ROMIO thumbnail service. */
+    private transient ThumbnailService thumbnailService;
 
-	/**
-	 * Bean injection setter for ROMIO thumbnail service
-	 * 
-	 * @param rootdir
-	 */
-	public void setThumbnailService(ThumbnailService thumbnailService) {
-		getBeanHelper().throwIfAlreadySet(this.thumbnailService, thumbnailService);
-		this.thumbnailService = thumbnailService;
-	}
+    /* The ROMIO pixels service. */
+    private transient PixelsService pixelsService;
 
-	/**
-	 * Bean injection setter for ROMIO pixels service
-	 * 
-	 * @param rootdir
-	 */
-	public void setPixelsService(PixelsService pixelsService) {
-		getBeanHelper().throwIfAlreadySet(this.pixelsService, pixelsService);
-		this.pixelsService = pixelsService;
-	}
+    /* The ROMIO file service. */
+    private transient OriginalFilesService fileService;
 
-	/**
-	 * Bean injection setter for ROMIO file service
-	 * 
-	 * @param rootdir
-	 */
-	public void setFileService(OriginalFilesService fileService) {
-		getBeanHelper().throwIfAlreadySet(this.fileService, fileService);
-		this.fileService = fileService;
-	}
+    // Static state
+    // =========================================================================
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ome.api.IRepositoryInfo#getFreeSpaceInKilobytes()
-	 */
-	@RolesAllowed("user")
-	public long getFreeSpaceInKilobytes() {
+    /**
+     * Time of the last true {@link #sanityCheckRepository()}
+     */
+    private volatile long lastCheck = 0;
 
-		FileSystem f;
-		long result = 0L;
+    /**
+     * Cached value from the last true {@link #sanityCheckRepository()}
+     */
+    private volatile double lastUsage = 0.0;
 
-		try {
-			f = new FileSystem(datadir);
-			result = f.free(datadir);
-			if (log.isInfoEnabled()) {
-				log.info("Total kilobytes free: " + f.free(datadir));
-			}
-		} catch (Throwable t) {
-		    log.error("Error retrieving usage in KB.", t);
-			throw new ResourceError(t.getMessage());
-		}
+    /**
+     * Lock-object around the call to the true {@link #sanityCheckRepository()}.
+     * Should be used to protected all sections of code which change
+     * {@link #lastCheck} and {@link #lastUsage}
+     */
+    private final Object lastLock = new Object();
 
-		return result;
-	}
+    /**
+     * Bean injection setter for ROMIO thumbnail service
+     * 
+     * @param rootdir
+     */
+    public void setThumbnailService(ThumbnailService thumbnailService) {
+        getBeanHelper().throwIfAlreadySet(this.thumbnailService,
+                thumbnailService);
+        this.thumbnailService = thumbnailService;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ome.api.IRepositoryInfo#getUsedSpaceInKilobytes()
-	 */
-	@RolesAllowed("user")
-	public long getUsedSpaceInKilobytes() {
+    /**
+     * Bean injection setter for ROMIO pixels service
+     * 
+     * @param rootdir
+     */
+    public void setPixelsService(PixelsService pixelsService) {
+        getBeanHelper().throwIfAlreadySet(this.pixelsService, pixelsService);
+        this.pixelsService = pixelsService;
+    }
 
-		FileSystem f;
-		long result = 0L;
+    /**
+     * Bean injection setter for ROMIO file service
+     * 
+     * @param rootdir
+     */
+    public void setFileService(OriginalFilesService fileService) {
+        getBeanHelper().throwIfAlreadySet(this.fileService, fileService);
+        this.fileService = fileService;
+    }
 
-		try {
-			f = new FileSystem(datadir);
-			result = f.used();
-			if (log.isInfoEnabled()) {
-				log.info("Total kilobytes used: " + f.used());
-			}
-		} catch (Throwable t) {
-		    log.error("Error retrieving usage in KB.", t);
-			throw new ResourceError(t.getMessage());
-		}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ome.api.IRepositoryInfo#getFreeSpaceInKilobytes()
+     */
+    @RolesAllowed("user")
+    public long getFreeSpaceInKilobytes() {
 
-		return result;
-	}
+        FileSystem f;
+        long result = 0L;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ome.api.IRepositoryInfo#getUsageFraction()
-	 */
-	@RolesAllowed("user")
-	public double getUsageFraction() {
-		double result = 0.0;
+        try {
+            f = new FileSystem(datadir);
+            result = f.free(datadir);
+            if (log.isInfoEnabled()) {
+                log.info("Total kilobytes free: " + f.free(datadir));
+            }
+        } catch (Throwable t) {
+            log.error("Error retrieving usage in KB.", t);
+            throw new ResourceError(t.getMessage());
+        }
 
-		try {
-			if (getUsedSpaceInKilobytes() > 0) {
-				Double used = new Double(getUsedSpaceInKilobytes());
-				Double free = new Double(getFreeSpaceInKilobytes());
-				result = used / free;
-			}
-		} catch (Throwable t) {
-		    log.error("Could not obtain usage fraction.", t);
-			throw new ResourceError(t.getMessage());
-		}
+        return result;
+    }
 
-		return result;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ome.api.IRepositoryInfo#getUsedSpaceInKilobytes()
+     */
+    @RolesAllowed("user")
+    public long getUsedSpaceInKilobytes() {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ome.logic.AbstractBean#getServiceInterface()
-	 */
-	public final Class<? extends ServiceInterface> getServiceInterface() {
-		return IRepositoryInfo.class;
-	}
+        FileSystem f;
+        long result = 0L;
 
-	/**
-	 * Bean injection setter for data repository directory
-	 * 
-	 * @param datadir
-	 */
-	public void setDatadir(String datadir) {
-		this.datadir = datadir;
-	}
+        try {
+            f = new FileSystem(datadir);
+            result = f.used();
+            if (log.isInfoEnabled()) {
+                log.info("Total kilobytes used: " + f.used());
+            }
+        } catch (Throwable t) {
+            log.error("Error retrieving usage in KB.", t);
+            throw new ResourceError(t.getMessage());
+        }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ome.api.IRepository#sanityCheckRepository()
-	 */
-	@RolesAllowed("user")
-	public void sanityCheckRepository() throws InternalException {
-		try {
-		    double usage = getUsageFraction() * 100;
-			if (usage > 95.0) {
-				throw new ResourceError(
-						"Server repository disk space usage " + (int) usage + 
-						" exceeds 95%.");
-			}
-		} catch (Throwable t) {
-		    log.error("Critical failure sanity checking repository.", t);
-			throw new InternalException(
-			        "Error in sanityCheckRepository(): " + t.getMessage());
-		}
-	}
+        return result;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ome.api.IRepository#removeUnusedFiles()
-	 */
-	@RolesAllowed("user")
-	public void removeUnusedFiles() {
-	    RepositoryTask task = new RepositoryTask();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ome.api.IRepositoryInfo#getUsageFraction()
+     */
+    @RolesAllowed("user")
+    public double getUsageFraction() {
+        double result = 0.0;
 
-	    // get ids for any objects marked as deleted
-	    List<Long> files = task.getFileIds();
-	    List<Long> pixels = task.getPixelIds();
-	    List<Long> thumbs = task.getThumbnailIds();
+        try {
+            if (getUsedSpaceInKilobytes() > 0) {
+                Double used = new Double(getUsedSpaceInKilobytes());
+                Double free = new Double(getFreeSpaceInKilobytes());
+                result = used / free;
+            }
+        } catch (Throwable t) {
+            log.error("Could not obtain usage fraction.", t);
+            throw new ResourceError(t.getMessage());
+        }
 
-	    // cleanup any files
-	    fileService.removeFiles(files);
+        return result;
+    }
 
-	    // cleanup any pixels
-	    pixelsService.removePixels(pixels);
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ome.logic.AbstractBean#getServiceInterface()
+     */
+    public final Class<? extends ServiceInterface> getServiceInterface() {
+        return IRepositoryInfo.class;
+    }
 
-	    // cleanup any thumbnails
-	    thumbnailService.removeThumbnails(thumbs);
-	}
+    /**
+     * Bean injection setter for data repository directory
+     * 
+     * @param datadir
+     */
+    public void setDatadir(String datadir) {
+        this.datadir = datadir;
+    }
+
+    /**
+     * Calculates based on the {@link #lastUsage cached usage} and the
+     * {@link #lastCheck elapsed time} whether or not a real
+     * {@link #sanityCheckRepository()} should be calculated.
+     */
+    public boolean needsSanityCheck() {
+
+        long time = System.currentTimeMillis();
+        long elapsed = time - lastCheck;
+
+        // If the usage is within 5% of the critical usage
+        // return true regarless
+        if (lastUsage > CRITICAL_USAGE - 5.0) {
+            return true;
+        }
+
+        // Otherwise, scale the initial delay down based on how high our
+        // usage is, and if we've passed that value, then return true.
+        // For example, if the initial delay 2 minutes and our previous usage is
+        // 80 percent, then 100 - 80 / 100 = .2 * 2 minute = 24 seconds.
+        double scale = (100 - lastUsage) / 100;
+        if (elapsed > scale * INITIAL_DELAY) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    /**
+     * @see ome.api.IRepository#sanityCheckRepository()
+     */
+    @RolesAllowed("user")
+    public void sanityCheckRepository() throws InternalException {
+        if (needsSanityCheck()) {
+            synchronized (lastLock) {
+                // Check the time again, in case another thread
+                // updated the values.
+                if (needsSanityCheck()) {
+                    try {
+                        lastUsage = getUsageFraction() * 100;
+                    } catch (Throwable t) {
+                        log.error(
+                                "Critical failure sanity checking repository.",
+                                t);
+                        throw new InternalException(
+                                "Error in sanityCheckRepository(): "
+                                        + t.getMessage());
+                    }
+                    lastCheck = System.currentTimeMillis();
+                }
+            }
+        }
+
+        if (lastUsage > CRITICAL_USAGE) {
+            throw new ResourceError(String.format(
+                    "Server repository disk space usage (%s%%) exceeds %s%%",
+                    lastUsage, CRITICAL_USAGE));
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ome.api.IRepository#removeUnusedFiles()
+     */
+    @RolesAllowed("user")
+    public void removeUnusedFiles() {
+        RepositoryTask task = new RepositoryTask();
+
+        // get ids for any objects marked as deleted
+        List<Long> files = task.getFileIds();
+        List<Long> pixels = task.getPixelIds();
+        List<Long> thumbs = task.getThumbnailIds();
+
+        // cleanup any files
+        fileService.removeFiles(files);
+
+        // cleanup any pixels
+        pixelsService.removePixels(pixels);
+
+        // cleanup any thumbnails
+        thumbnailService.removeThumbnails(thumbs);
+    }
 
 }
