3a458772297fe35eaf49c9163a009a30b30e9300
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2008-08-27 14:11:10 +0000

    Adding documentation on `BlitzOnly`
    git-svn-id: file:///home/svn/omero/trunk@2760 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/blitz/src/ome/services/blitz/util/BlitzOnly.java
+++ components/blitz/src/ome/services/blitz/util/BlitzOnly.java
@@ -9,11 +9,14 @@
 package ome.services.blitz.util;
 
 /**
- * Marker interface which specifies that an servant is intended for use in
- * 
+ * Marker interface which specifies that an servant should not be checked for
+ * api consistency via {@link ApiConsistencyCheck}. This is necessary both
+ * where there is no underlying ome.api.* service as well as when the method
+ * arguments sufficiently differ, for example in the case of enums and value
+ * objects.
  * 
  * @author Josh Moore, josh at glencoesoftware.com
- * @since 3.0-Beta4
+ * @since 3.0-Beta3.1
  */
 public interface BlitzOnly {
 
