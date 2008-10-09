34ca2cf25fd1c2a14b106b667fb02d53b2872180
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2008-10-09 12:23:59 +0000

    ticket:1106 - Adding headless=true to xargs in server.py
    git-svn-id: file:///home/svn/omero/trunk@2911 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/src/omero/plugins/server.py
+++ components/tools/OmeroPy/src/omero/plugins/server.py
@@ -37,6 +37,9 @@ class ServerControl(BaseControl):
         xargs = self._prop(data, "blitz.xargs")
         if len(xargs) == 0:
             xargs = "-Xmx400M"
+        # Appending the to the end of xargs since required
+        xargs = xargs + " -Djava.awt.headless=true"
+
         debug = self._prop(data, "blitz.debug")
         if debug == "true":
             debug = True
