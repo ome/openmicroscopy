eef25b716680a0ad68bedd16f1438cc088c82012
jburel <jburel@05709c45-44f0-0310-885b-81a1db45b4a6>
2008-11-06 15:34:37 +0000

    Modified value returned
    git-svn-id: file:///home/svn/omero/trunk@3056 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroJava/src/pojos/ImageAcquisitionData.java
+++ components/tools/OmeroJava/src/pojos/ImageAcquisitionData.java
@@ -37,7 +37,9 @@ import omero.model.ImagingEnvironmentI;
 import omero.model.Immersion;
 import omero.model.Medium;
 import omero.model.Objective;
+import omero.model.ObjectiveI;
 import omero.model.ObjectiveSettings;
+import omero.model.ObjectiveSettingsI;
 import omero.model.StageLabel;
 import omero.model.StageLabelI;
 
@@ -67,6 +69,27 @@ public class ImageAcquisitionData
 	/** The objective used to acquire the image. */
 	private ObjectiveSettings	objective;
 	
+	/** Flag indicating if the position is dirty. */
+	private boolean				labelDirty;
+	
+	/** Flag indicating if the condition is dirty. */
+	private boolean				conditionDirty;
+	
+	/** Flag indicating if the objective settings is dirty. */
+	private boolean				objectiveSettingsDirty;
+	
+	/** Flag indicating if the objective is dirty. */
+	private boolean				objectiveDirty;
+	
+	/** The objective's medium. */
+	private String				medium;
+	
+	/** The objective's immersion. */
+	private String				immersion;
+	
+	/** The objective's coating. */
+	private String				coating;
+	
 	/**
 	 * Creates a new instance.
 	 * 
@@ -80,6 +103,9 @@ public class ImageAcquisitionData
         label = image.getPosition();
         environment = image.getCondition();
         objective = image.getObjectiveSettings();
+        medium = null;
+        immersion = null;
+        coating = null;
 	}
 	
 	/**
@@ -217,9 +243,10 @@ public class ImageAcquisitionData
 	 */
 	public String getMedium()
 	{
-		if (objective == null) return "";
+		if (medium != null) return medium;
+		if (objective == null) return medium;
 		Medium value = objective.getMedium();
-		if (value == null) return "";
+		if (value == null) return medium;
 		return value.getValue().getValue();
 	}
 	
@@ -260,11 +287,12 @@ public class ImageAcquisitionData
 	 */
 	public String getImmersion()
 	{
-		if (objective == null) return "";
+		if (immersion != null) return immersion;
+		if (objective == null) return immersion;
 		Objective obj = objective.getObjective();
-		if (obj == null) return "";
+		if (obj == null) return immersion;
 		Immersion value = obj.getImmersion();
-		if (value == null) return "";
+		if (value == null) return immersion;
 		return value.getValue().getValue();
 	}
 	
@@ -275,11 +303,12 @@ public class ImageAcquisitionData
 	 */
 	public String getCoating()
 	{
-		if (objective == null) return "";
+		if (coating != null) return coating;
+		if (objective == null) return coating;
 		Objective obj = objective.getObjective();
-		if (obj == null) return "";
+		if (obj == null) return coating;
 		Coating value = obj.getCoating();
-		if (value == null) return "";
+		if (value == null) return coating;
 		return value.getValue().getValue();
 	}
 	
@@ -297,45 +326,127 @@ public class ImageAcquisitionData
 	}
 	
 	/**
+	 * Returns the serial number of the objective.
+	 * 
+	 * @return See above.
+	 */
+	public String getSerialNumber()
+	{
+		if (objective == null) return "";
+		Objective obj = objective.getObjective();
+		if (obj == null) return "";
+		return obj.getSerialNumber().getValue();
+	}
+	
+	/**
+	 * Returns the model of the objective.
+	 * 
+	 * @return See above.
+	 */
+	public String getModel()
+	{
+		if (objective == null) return "";
+		Objective obj = objective.getObjective();
+		if (obj == null) return "";
+		return obj.getModel().getValue();
+	}
+	
+	/**
+	 * Returns the manufacturer of the objective.
+	 * 
+	 * @return See above.
+	 */
+	public String getManufacturer()
+	{
+		if (objective == null) return "";
+		Objective obj = objective.getObjective();
+		if (obj == null) return "";
+		return obj.getManufacturer().getValue();
+	}
+	
+	/**
+	 * Sets the serial number.
+	 * 
+	 * @param number The value to set.
+	 */
+	public void setSerialNumber(String number)
+	{
+		objectiveDirty = true;
+		Objective ob = objective.getObjective();
+		if (ob == null) ob = new ObjectiveI();
+		ob.setSerialNumber(omero.rtypes.rstring(number));
+	}
+	
+	/**
+	 * Sets the model.
+	 * 
+	 * @param model The value to set.
+	 */
+	public void setModel(String model)
+	{
+		objectiveDirty = true;
+		Objective ob = objective.getObjective();
+		if (ob == null) ob = new ObjectiveI();
+		ob.setModel(omero.rtypes.rstring(model));
+	}
+
+	/**
+	 * Sets the manufacturer.
+	 * 
+	 * @param manufacturer The value to set.
+	 */
+	public void setManufacturer(String manufacturer)
+	{
+		objectiveDirty = true;
+		Objective ob = objective.getObjective();
+		if (ob == null) ob = new ObjectiveI();
+		ob.setManufacturer(omero.rtypes.rstring(manufacturer));
+	}
+	
+	/**
 	 * Sets the name of the stage label.
 	 * 
 	 * @param name The value to set.
 	 */
 	public void setLabelName(String name)
 	{
+		labelDirty = true;
 		if (label == null) label = new StageLabelI();
 		label.setName(omero.rtypes.rstring(name));
 	}
 	
 	/**
-	 * Sets the x-position
+	 * Sets the x-position.
 	 * 
 	 * @param value The value to set.
 	 */
 	public void setPositionX(float value)
 	{
+		labelDirty = true;
 		if (label == null) label = new StageLabelI();
 		label.setPositionX(omero.rtypes.rfloat(value));
 	}
 	
 	/**
-	 * Sets the y-position
+	 * Sets the y-position.
 	 * 
 	 * @param value The value to set.
 	 */
 	public void setPositionY(float value)
 	{
+		labelDirty = true;
 		if (label == null) label = new StageLabelI();
 		label.setPositionY(omero.rtypes.rfloat(value));
 	}
 	
 	/**
-	 * Sets the y-position
+	 * Sets the z-position.
 	 * 
 	 * @param value The value to set.
 	 */
 	public void setPositionZ(float value)
 	{
+		labelDirty = true;
 		if (label == null) label = new StageLabelI();
 		label.setPositionZ(omero.rtypes.rfloat(value));
 	}
@@ -347,6 +458,7 @@ public class ImageAcquisitionData
 	 */
 	public void setTemperature(float temperature)
 	{
+		conditionDirty = true;
 		if (environment == null) environment = new ImagingEnvironmentI();
 		environment.setTemperature(omero.rtypes.rfloat(temperature));
 	}
@@ -358,6 +470,7 @@ public class ImageAcquisitionData
 	 */
 	public void setAirPressure(float pressure)
 	{
+		conditionDirty = true;
 		if (environment == null) environment = new ImagingEnvironmentI();
 		environment.setAirPressure(omero.rtypes.rfloat(pressure));
 	}
@@ -372,6 +485,7 @@ public class ImageAcquisitionData
 		if (humidity < 0 || humidity > 1)
 			throw new IllegalArgumentException("Humidity must " +
 					"be a value in [0, 1]");
+		conditionDirty = true;
 		if (environment == null) environment = new ImagingEnvironmentI();
 		environment.setHumidity(omero.rtypes.rfloat(humidity));
 	}
@@ -385,6 +499,7 @@ public class ImageAcquisitionData
 	{
 		if (co2 < 0 || co2 > 1)
 			throw new IllegalArgumentException("Co2 must be a value in [0, 1]");
+		conditionDirty = true;
 		if (environment == null) environment = new ImagingEnvironmentI();
 		environment.setCo2percent(omero.rtypes.rfloat(co2));
 	}
@@ -396,8 +511,9 @@ public class ImageAcquisitionData
 	 */
 	public void setCorrectionCollar(float correction)
 	{
-		if (objective != null) 
-			objective.setCorrectionCollar(omero.rtypes.rfloat(correction));
+		objectiveSettingsDirty = true;
+		if (objective == null) objective = new ObjectiveSettingsI();	
+		objective.setCorrectionCollar(omero.rtypes.rfloat(correction));
 	}
 	
 	/**
@@ -407,8 +523,163 @@ public class ImageAcquisitionData
 	 */
 	public void setRefractiveIndex(float index)
 	{
-		if (objective != null) 
-			objective.setRefractiveIndex(omero.rtypes.rfloat(index));
+		objectiveSettingsDirty = true;
+		if (objective == null) objective = new ObjectiveSettingsI();	
+		objective.setRefractiveIndex(omero.rtypes.rfloat(index));
+	}
+
+	/**
+	 * Sets the medium of the objective.
+	 * 
+	 * @param medium The value to set.
+	 */
+	public void setMedium(String medium)
+	{
+		this.medium = medium;
+		objectiveSettingsDirty = true;
+	}
+	
+	/**
+	 * Sets the magnification factor of the objective.
+	 * 
+	 * @param factor The value to set.
+	 */
+	public void setMagnification(double factor)
+	{
+		objectiveDirty = true;
+		Objective ob = objective.getObjective();
+		if (ob == null) ob = new ObjectiveI();
+		ob.setMagnificiation(omero.rtypes.rdouble(factor));
+	}
+	
+	/**
+	 * Sets the lens numerical aperture.
+	 * 
+	 * @param na The value to set.
+	 */
+	public void setLensNA(float na)
+	{
+		objectiveDirty = true;
+		Objective ob = objective.getObjective();
+		if (ob == null) ob = new ObjectiveI();
+		ob.setLensNA(omero.rtypes.rfloat(na));
+	}
+	
+	/**
+	 * Sets the working distance.
+	 * 
+	 * @param distance The value to set.
+	 */
+	public void setWorkingDistance(float distance)
+	{
+		objectiveDirty = true;
+		Objective ob = objective.getObjective();
+		if (ob == null) ob = new ObjectiveI();
+		//ob.setLensNA(omero.rtypes.rfloat(na));
+	}
+	
+	/**
+	 * Sets the immersion.
+	 * 
+	 * @param immersion The value to set.
+	 */
+	public void setImmersion(String immersion)
+	{
+		objectiveDirty = true;
+		this.immersion = immersion;
+	}
+	
+	/**
+	 * Sets the coating.
+	 * 
+	 * @param coating The value to set.
+	 */
+	public void setCoating(String coating)
+	{
+		objectiveDirty = true;
+		this.coating = coating;
+	}
+	
+	/**
+	 * Returns <code>true</code> if the position has been updated,
+	 * <code>false</code> otherwise.
+	 * 
+	 * @return See above.
+	 */
+	public boolean isPositionDirty() { return labelDirty; }
+	
+	/**
+	 * Returns <code>true</code> if the position has been updated,
+	 * <code>false</code> otherwise.
+	 * 
+	 * @return See above.
+	 */
+	public boolean isConditionDirty() { return conditionDirty; }
+	
+	/**
+	 * Returns <code>true</code> if the objective settings has been updated,
+	 * <code>false</code> otherwise.
+	 * 
+	 * @return See above.
+	 */
+	public boolean isObjectiveSettingsDirty() { return objectiveSettingsDirty; }
+	
+	/**
+	 * Returns <code>true</code> if the objective has been updated,
+	 * <code>false</code> otherwise.
+	 * 
+	 * @return See above.
+	 */
+	public boolean isObjectiveDirty() { return objectiveDirty; }
+	
+	/**
+	 * Returns the id of the <code>StageLabel</code> or <code>-1</code>
+	 * if not already linked to the image.
+	 * 
+	 * @return See above
+	 */
+	public long getPositionId()
+	{
+		if (label == null) return -1;
+		return label.getId().getValue();
+	}
+	
+	/**
+	 * Returns the id of the <code>StageLabel</code> or <code>-1</code>
+	 * if not already linked to the image.
+	 * 
+	 * @return See above
+	 */
+	public long getConditionId()
+	{
+		if (environment == null) return -1;
+		return environment.getId().getValue();
+	}
+	
+	/**
+	 * Returns the id of the <code>Objective settings</code> or <code>-1</code>
+	 * if not already linked to the image.
+	 * 
+	 * @return See above
+	 */
+	public long getObjectiveSettingsId()
+	{
+		if (objective == null) return -1;
+		return objective.getId().getValue();
+	}
+	
+	/**
+	 * Returns the id of the <code>Objective</code> or <code>-1</code>
+	 * if not already linked to the image.
+	 * 
+	 * @return See above
+	 */
+	public long getObjectiveId()
+	{
+		if (objective == null) return -1;
+		Objective ob = objective.getObjective();
+		if (ob == null) return -1;
+		return ob.getId().getValue();
 	}
 	
 }
