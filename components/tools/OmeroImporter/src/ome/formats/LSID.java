bc2a86e975a2a372c17d36f3a2f59bb9c5e047ba
callan <callan@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-01-23 13:09:35 +0000

    Adding missing LSID class.
    git-svn-id: file:///home/svn/omero/trunk@3343 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/tools/OmeroImporter/src/ome/formats/LSID.java
@@ -0,0 +1,97 @@
+package ome.formats;
+
+import omero.model.IObject;
+
+
+/**
+ * This class represents an LSID as used by the OME-XML data model.
+ * 
+ * @author Chris Allan <callan at blackcat dot ca>
+ *
+ */
+public class LSID
+{
+	/** Concrete Java class which qualifies the type of object. */
+	private Class<? extends IObject> klass;
+	
+	/** Indexes within the OME-XML data model. */
+	private int[] indexes;
+	
+	/** The LSID as a string. */
+	private String asString;
+	
+	/**
+	 * Default constructor.
+	 * @param klass Concrete Java class which qualifies the type of object
+	 * this LSID represents.
+	 * @param indexes Indexes for this LSID within the OME-XML data model.
+	 */
+	public LSID(Class<? extends IObject> klass, int... indexes)
+	{
+		this.klass = klass;
+		this.indexes = indexes;
+        asString = klass.getName();
+        for (int index : indexes)
+        {
+            asString = asString + ":" + index;
+        }
+	}
+	
+	/**
+	 * Constructor for non-standard LSIDs.
+	 * @param asString The LSID as a string.
+	 */
+	public LSID(String asString)
+	{
+		this.asString = asString;
+	}
+	
+	/**
+	 * Returns the Java class which qualifies the type of object this
+	 * LSID represents.
+	 * @return See above.
+	 */
+	public Class<? extends IObject> getJavaClass()
+	{
+		return klass;
+	}
+	
+	/**
+	 * Returns the indexes for this LSID within the OME-XML data model.
+	 * @return See above.
+	 */
+	public int[] getIndexes()
+	{
+		return indexes;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return asString;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof LSID)
+		{
+			LSID comparator = (LSID) obj;
+			Class<? extends IObject> comparatorClass = comparator.getJavaClass();
+			if (comparatorClass != null && comparatorClass.equals(klass))
+			{
+				int[] comparatorIndexes = comparator.getIndexes();
+				for (int i = 0; i < indexes.length; i++)
+				{
+					if (indexes[i] != comparatorIndexes[i])
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			return false;
+		}
+		return super.equals(obj);
+	}
+}
