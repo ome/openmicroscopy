ae8dfd141ddb7a7d98076ed1d5da9f73b766e609
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-02-05 21:43:20 +0000

    ticket:703 - '''More''' missing files
    git-svn-id: file:///home/svn/omero/trunk@3479 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/tools/OmeroPy/test/integration/icontainer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+   Integration test focused on the omero.api.IPojos interface
+   a running server.
+
+   Copyright 2008 Glencoe Software, Inc. All rights reserved.
+   Use is subject to license terms supplied in LICENSE.txt
+
+"""
+
+import test.integration.library as lib
+import omero, unittest
+from omero_model_PixelsI import PixelsI
+from omero_model_ImageI import ImageI
+from omero_model_DatasetI import DatasetI
+from omero_model_ExperimenterI import ExperimenterI
+from omero_model_ExperimenterGroupI import ExperimenterGroupI
+from omero_model_GroupExperimenterMapI import GroupExperimenterMapI
+from omero_model_DatasetImageLinkI import DatasetImageLinkI
+from omero.rtypes import rstring
+
+class TestIContainer(lib.ITest):
+
+    def testFindAnnotations(self):
+        ipojo = self.client.sf.getContainerService()
+        i = ImageI()
+        i.setName(rstring("name"))
+        i = ipojo.createDataObject(i,None)
+
+    def testCreateAfterBlitzPort(self):
+        ipojo = self.client.sf.getContainerService()
+        i = ImageI()
+        i.setName(rstring("name"))
+        i = ipojo.createDataObject(i,None)
+        o = i.getDetails().owner
+        self.assertEquals( -1, o.sizeOfGroupExperimenterMap() )
+
+if __name__ == '__main__':
+    unittest.main()
