507ae91fd3a6a952b2dbe433ca1a5fcea3145292
ola <ola@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-02-09 11:32:14 +0000

    annotation retrieval test for private data.
    git-svn-id: file:///home/svn/omero/trunk@3505 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/test/integration/icontainer.py
+++ components/tools/OmeroPy/test/integration/icontainer.py
@@ -28,6 +28,89 @@ class TestIContainer(lib.ITest):
         i.setName(rstring("name"))
         i = ipojo.createDataObject(i,None)
 
+    
+    def testFindAndCountAnnotationsForPrivateData(self):
+        uuid = self.root.sf.getAdminService().getEventContext().sessionUuid
+        query = self.root.sf.getQueryService()
+        update = self.root.sf.getUpdateService()
+        admin = self.root.sf.getAdminService()
+        ipojo = self.root.sf.getContainerService()
+        
+        ### create new user
+        #group1
+        new_gr1 = ExperimenterGroupI()
+        new_gr1.name = rstring("group1_%s" % uuid)
+        gid = admin.createGroup(new_gr1)
+        
+        #new user1
+        new_exp = ExperimenterI()
+        new_exp.omeName = rstring("user1_%s" % uuid)
+        new_exp.firstName = rstring("New")
+        new_exp.lastName = rstring("Test")
+        new_exp.email = rstring("newtest@emaildomain.com")
+        
+        defaultGroup = admin.getGroup(gid)
+        listOfGroups = list()
+        listOfGroups.append(admin.lookupGroup("user"))
+        
+        eid = admin.createExperimenterWithPassword(new_exp, rstring("ome"), defaultGroup, listOfGroups)
+        
+        ## get user
+        user1 = admin.getExperimenter(eid)
+        
+        ## login as user1 
+        cl1 = omero.client()
+        cl1.createSession(user1.omeName.val,"ome")
+        update1 = cl1.sf.getUpdateService()
+        ipojo1 = cl1.sf.getContainerService()
+        
+        # create image
+        img = ImageI()
+        img.setName(rstring('test1154-img-%s' % (uuid)))
+        img.setAcquisitionDate(rtime(0))
+        
+        # permission 'rw----':
+        img.details.permissions.setUserRead(True)
+        img.details.permissions.setUserWrite(True)
+        img.details.permissions.setGroupRead(False)
+        img.details.permissions.setGroupWrite(False)
+        img.details.permissions.setWorldRead(False)
+        img.details.permissions.setWorldWrite(False)
+        img = update1.saveAndReturnObject(img)
+        img.unload()
+        
+        ann1 = CommentAnnotationI()
+        ann1.textValue = rstring("user comment - %s" % uuid)
+        l_ann1 = ImageAnnotationLinkI()
+        l_ann1.setParent(img)
+        l_ann1.setChild(ann1)
+        update1.saveObject(l_ann1)
+        
+        #user retrives the annotations for image
+        self.assert_(ipojo1.getCollectionCount("Image", "ome.model.containers.Image_annotationLinks", [img.id.val], None).get(img.id.val, []) == 1)
+        self.assert_(len(ipojo1.findAnnotations("Image", [img.id.val], None, None).get(img.id.val, [])) == 1)
+        
+        ann = CommentAnnotationI()
+        ann.textValue = rstring("root comment - %s" % uuid)
+        l_ann = ImageAnnotationLinkI()
+        l_ann.setParent(img)
+        l_ann.setChild(ann)
+        update.saveObject(l_ann)
+        
+        #do they see the same vals?
+        #print ipojo1.getCollectionCount("Image", "ome.model.containers.Image_annotationLinks", [img.id.val], None)
+        #print ipojo.getCollectionCount("Image", "ome.model.containers.Image_annotationLinks", [img.id.val], None)
+        #print len(ipojo1.findAnnotations("Image", [img.id.val], None, None).get(img.id.val, []))
+        #print len(ipojo.findAnnotations("Image", [img.id.val], None, None).get(img.id.val, []))
+        self.assert_(ipojo1.getCollectionCount("Image", "ome.model.containers.Image_annotationLinks", [img.id.val], None).get(img.id.val, []) == 2)
+        anns = ipojo1.findAnnotations("Image", [img.id.val], None, None).get(img.id.val, [])
+        self.assert_(len(anns) == 2)
+        
+        self.assert_(anns[0].details.permissions == 'rw----')
+        self.assert_(anns[1].details.permissions == 'rw----')
+        
+        cl1.sf.closeOnDestroy()
+    
     def testCreateAfterBlitzPort(self):
         ipojo = self.client.sf.getContainerService()
         i = ImageI()
