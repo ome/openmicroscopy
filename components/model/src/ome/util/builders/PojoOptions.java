be4d12302b418809182b9b3c2687c15e771e4d62
jburel <jburel@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-02-09 21:37:28 +0000

    Added new Field: Orphan
    git-svn-id: file:///home/svn/omero/trunk@3516 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/model/src/ome/util/builders/PojoOptions.java
+++ components/model/src/ome/util/builders/PojoOptions.java
@@ -28,30 +28,42 @@ import java.util.Map;
 public class PojoOptions
 
 {
+	/** Field identifying the <code>leaf</code> parameter. */
     public static final String LEAF = "leaves";
 
+    /** Field identifying the <code>Experimenter</code> parameter. */
     public static final String EXPERIMENTER = "experimenter";
 
+    /** Field identifying the <code>group</code> parameter. */
     public static final String GROUP = "group";
 
+    /** Field identifying the <code>start time</code> parameter. */
     public static final String START_TIME = "startTime";
 
+    /** Field identifying the <code>end time</code> parameter. */
     public static final String END_TIME = "endTime";
 
+    /** Field identifying the <code>offset</code> parameter. */
     public static final String OFFSET = "offset";
 
+    /** Field identifying the <code>limit</code> parameter. */
     public static final String LIMIT = "limit";
+    
+    /** Field identifying the <code>orphan</code> parameter. */
+    public static final String ORPHAN = "orphan";
 
-    private final Map options = new HashMap();
+    /** The map hosting the parameters. */
+    private final Map<String, Object> options = new HashMap<String, Object>();
 
+    /** Creates a default instance. */
     public PojoOptions() {
         this.noLeaves();
     }
 
     /**
-     * builds a PojoOptions from a passed map. Empty maps and null maps have the
+     * Builds a PojoOptions from a passed map. Empty maps and null maps have the
      * same effect. Further they <b>override</b> the defaults. For defaults,
-     * use {@see #PojoOptions() they null-arg constructor}.
+     * use {@link #PojoOptions()} they null-arg constructor.
      * 
      * @param map
      */
@@ -63,9 +75,14 @@ public class PojoOptions
         }
     }
 
+    /**
+     * Copies the passed map.
+     * 
+     * @param map The map to copy.
+     */
     protected void copy(Map map) {
         String[] s = new String[] { LEAF, EXPERIMENTER, GROUP, START_TIME,
-                END_TIME, OFFSET, LIMIT };
+                END_TIME, OFFSET, LIMIT, ORPHAN };
         for (int i = 0; i < s.length; i++) {
             if (map.containsKey(s[i])) {
                 this.options.put(s[i], map.get(s[i]));
@@ -78,39 +95,107 @@ public class PojoOptions
      * ==============================
      */
 
+    /**
+     * Sets the <code>leaf</code> parameter to <code>true</code>.
+     * 
+     * @return Returns the current object.
+     */
     public PojoOptions leaves() {
         options.put(LEAF, Boolean.TRUE);
         return this;
     }
 
+    /**
+     * Sets the <code>leaf</code> parameter to <code>false</code>.
+     * 
+     * @return Returns the current object.
+     */
     public PojoOptions noLeaves() {
         options.remove(LEAF);
         return this;
     }
 
+    /**
+     * Returns <code>true</code> if the map contains the <code>leaf</code>
+     * parameter, <code>false</code> otherwise.
+     * 
+     * @return See above
+     */
     public boolean isLeaves() {
         return options.containsKey(LEAF);
     }
 
+    /**
+     * Sets the <code>orphan</code> parameter to <code>true</code>.
+     * 
+     * @return Returns the current object.
+     */
+    public PojoOptions orphan() {
+    	 options.put(ORPHAN, Boolean.TRUE);
+         return this;
+    }
+    
+    /**
+     * Sets the <code>orphan</code> parameter to <code>false</code>.
+     * 
+     * @return Returns the current object.
+     */
+    public PojoOptions noOrphan() {
+        options.remove(ORPHAN);
+        return this;
+    }
+
+    /**
+     * Returns <code>true</code> if the map contains the <code>orphan</code>
+     * parameter, <code>false</code> otherwise.
+     * 
+     * @return See above
+     */
+    public boolean isOrphan() {
+        return options.containsKey(ORPHAN);
+    }
+    
     /*
      * ============================== Filtered by Experimenter
      * ==============================
      */
 
+    /**
+     * Sets the value of the <code>experimenter</code> parameter.
+     * 
+     * @param i The Id of the experimenter.
+     * @return Returns the current object.
+     */
     public PojoOptions exp(Long i) {
         options.put(EXPERIMENTER, i);
         return this;
     }
 
+    /**
+     * Removes the <code>experimenter</code> parameter from the map.
+     * 
+     * @return Returns the current object.
+     */
     public PojoOptions allExps() {
         remove(EXPERIMENTER);
         return this;
     }
 
+    /**
+     * Returns <code>true</code> if the map contains the 
+     * <code>experimenter</code> parameter, <code>false</code> otherwise.
+     * 
+     * @return See above.
+     */
     public boolean isExperimenter() {
         return options.containsKey(EXPERIMENTER);
     }
 
+    /**
+     * Returns the value of the  <code>experimenter</code> parameter.
+     * 
+     * @return See above.
+     */
     public Long getExperimenter() {
         return (Long) options.get(EXPERIMENTER);
     }
@@ -120,34 +205,73 @@ public class PojoOptions
      * ==============================
      */
 
+    /**
+     * Sets the value of the <code>start time</code> parameter.
+     * 
+     * @param startTime The time to set.
+     * @return Returns the current object.
+     */
     public PojoOptions startTime(Timestamp startTime) {
         options.put(START_TIME, startTime);
         return this;
     }
 
+    /**
+     * Sets the value of the <code>end time</code> parameter.
+     * 
+     * @param endTime The time to set.
+     * @return Returns the current object.
+     */
     public PojoOptions endTime(Timestamp endTime) {
         options.put(END_TIME, endTime);
         return this;
     }
 
+    /**
+     * Removes the time parameters from the map.
+     * 
+     * @return Returns the current object.
+     */
     public PojoOptions allTimes() {
         remove(START_TIME);
         remove(END_TIME);
         return this;
     }
 
+    /**
+     * Returns <code>true</code> if the map contains the 
+     * <code>start time</code> parameter, <code>false</code> otherwise.
+     * 
+     * @return See above.
+     */
     public boolean isStartTime() {
         return options.containsKey(START_TIME);
     }
 
+    /**
+     * Returns <code>true</code> if the map contains the 
+     * <code>end time</code> parameter, <code>false</code> otherwise.
+     * 
+     * @return See above.
+     */
     public boolean isEndTime() {
         return options.containsKey(END_TIME);
     }
 
+    /**
+     * Returns the value of the  <code>start time</code> parameter.
+     * 
+     * @return See above.
+     */
     public Timestamp getStartTime() {
         return (Timestamp) options.get(START_TIME);
     }
 
+    /**
+     * Returns the value of the  <code>end time</code> parameter.
+     * 
+     * @return See above.
+     */
     public Timestamp getEndTime() {
         return (Timestamp) options.get(END_TIME);
     }
@@ -156,26 +280,56 @@ public class PojoOptions
      * ============================== Pagination ==============================
      */
 
+    /**
+     * Sets the value of the <code>offset</code> and <code>limit</code>
+     * parameters.
+     * 
+     * @param offset The value of the <code>offset</code>.
+     * @param limit The value of the <code>limit</code>.
+     * @return See above.
+     */
     public PojoOptions paginate(int offset, int limit) {
         options.put(OFFSET, offset);
         options.put(LIMIT, limit);
         return this;
     }
 
+    /**
+     * Removes the <code>offset</code> and <code>limit</code> parameters 
+     * from the map.
+     * 
+     * @return Returns the current object.
+     */
     public PojoOptions noPagination() {
         remove(OFFSET);
         remove(LIMIT);
         return this;
     }
 
+    /**
+     * Returns <code>true</code> if the map contains the 
+     * <code>offset</code> parameter, <code>false</code> otherwise.
+     * 
+     * @return See above
+     */
     public boolean isPagination() {
         return options.containsKey(OFFSET);
     }
 
+    /**
+     * Returns the value of the <code>offset</code> parameter.
+     * 
+     * @return See above.
+     */
     public Integer getOffset() {
         return (Integer) options.get(OFFSET);
     }
 
+    /**
+     * Returns the value of the <code>limit</code> parameter.
+     * 
+     * @return See above.
+     */
     public Integer getLimit() {
         return (Integer) options.get(LIMIT);
     }
@@ -185,20 +339,42 @@ public class PojoOptions
      * ==============================
      */
 
+    /**
+     * Sets the value of the  <code>group</code> parameter.
+     * 
+     * @param i The value to set.
+     * @return See above.
+     */
     public PojoOptions grp(Long i) {
         options.put(GROUP, i);
         return this;
     }
 
+    /**
+     * Removes the <code>group</code> parameter from the map.
+     * 
+     * @return Returns the current object.
+     */
     public PojoOptions allGrps() {
         remove(GROUP);
         return this;
     }
 
+    /**
+     * Returns <code>true</code> if the map contains the 
+     * <code>group</code> parameter, <code>false</code> otherwise.
+     * 
+     * @return See above.
+     */
     public boolean isGroup() {
         return options.containsKey(GROUP);
     }
 
+    /**
+     * Returns the value of the <code>group</code> parameter.
+     * 
+     * @return See above.
+     */
     public Long getGroup() {
         return (Long) options.get(GROUP);
     }
@@ -207,12 +383,22 @@ public class PojoOptions
      * ============================== Helpers ==============================
      */
 
+    /**
+     * Removes the passed from the map.
+     * 
+     * @param key The key to remove.
+     */
     protected void remove(String key) {
         if (options.containsKey(key)) {
             options.remove(key);
         }
     }
 
+    /**
+     * Returns the map.
+     * 
+     * @return See above.
+     */
     public Map map() {
         return options;
     }
