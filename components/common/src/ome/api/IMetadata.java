500121039c10ef20347787b64144423f02dcb7f3
jburel <jburel@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-02-12 21:12:48 +0000

    Added methods to Metadata I/F
    git-svn-id: file:///home/svn/omero/trunk@3596 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/common/src/ome/api/IMetadata.java
+++ components/common/src/ome/api/IMetadata.java
@@ -24,6 +24,7 @@ package ome.api;
 
 
 //Java imports
+import java.util.Map;
 import java.util.Set;
 
 //Third-party libraries
@@ -31,7 +32,10 @@ import java.util.Set;
 //Application-internal dependencies
 import ome.annotations.NotNull;
 import ome.annotations.Validate;
+import ome.model.IObject;
+import ome.model.annotations.Annotation;
 import ome.model.core.LogicalChannel;
+
 
 /** 
  * Provides method to interact with acquisition metadata and 
@@ -51,6 +55,24 @@ public interface IMetadata
 	extends ServiceInterface
 {
 
+	/** The name space indicating that the tag is used a tag set. */
+	public static final String NS_INSIGHT_TAG_SET = 
+		"openmicroscopy.org/omero/insight/tagset";
+	
+	 /**
+     * The name space indicating that the <code>Long</code> annotation is
+     * a rating annotation i.e. an integer in the interval <code>[0, 5]</code>.
+     */
+    public static final String NS_INSIGHT_RATING = 
+    	"openmicroscopy.org/omero/insight/rating";
+    
+    /**
+     * The name space indicating that the <code>Boolean</code> annotation 
+     * indicated if an archived image is imported with the image. 
+     */
+    public static final String NS_IMPORTER_ARCHIVED = 
+    	"openmicroscopy.org/omero/importer/archived";
+    
 	/**
 	 * Loads the <code>logical channels</code> and the acquisition metadata 
 	 * related to them.
@@ -62,4 +84,54 @@ public interface IMetadata
 	public Set<LogicalChannel> loadChannelAcquisitionData(@NotNull 
 			@Validate(Long.class) Set<Long> ids);
 	
+    /**
+     * Loads all the annotations of given types, 
+     * that have been attached to the specified <code>rootNodes</code> 
+     * for the specified <code>annotatorIds</code>.
+     * If no types specified, all annotations will be loaded.
+     * This method looks for the annotations that have been attached to each of
+     * the specified objects. It then maps each <code>rootNodeId</code> onto
+     * the set of annotations that were found for that node. If no
+     * annotations were found for that node, then the entry will be
+     * <code>null</code>. Otherwise it will be a <code>Set</code>
+     * containing {@link Annotation} objects.
+     * 
+     * @param nodeType The type of the nodes the annotations are linked to. 
+     *                 Mustn't be <code>null</code>.
+     * @param nodeIds  Ids of the objects of type <code>rootNodeType</code>.
+     * 				   Mustn't be <code>null</code>.
+     * @param annotationType The types of annotation to retrieve. 
+     * 						 If <code>null</code> all annotations will be
+     *                       loaded. String of the type
+     *                       <code>ome.model.annotations.*</code>.
+     * @param annotatorIds Ids of the users for whom annotations should be 
+     *                     retrieved. 
+     *                     If <code>null</code>, all annotations returned.
+     * @return A map whose key is rootNodeId and value the <code>Set</code> of
+     *         all annotations for that node or <code>null</code>.
+     */
+    public <T extends IObject, A extends Annotation> 
+    Map<Long, Set<A>> loadAnnotations(
+            @NotNull Class<T> nodeType, @NotNull @Validate(Long.class)
+            Set<Long> rootNodeIds, @NotNull @Validate(String.class) 
+            Set<String> annotationType,
+            @Validate(Long.class) Set<Long> annotatorIds);
+    
+    /**
+     * Loads the annotations of a given type.
+     * 
+     * @param type      The type of annotations to load.
+     * @param nameSpace The name space, one of the constants defined by 
+     *                  this class or <code>null</code>.
+     * @param annotatorIds Ids of the users for whom annotations should be 
+     *                     retrieved. 
+     * @param linkedObjects Pass <code>true</code> to load the 
+     * 						<code>IObject</code> related to the annotations.
+     * @return A collection of found annotations.
+     */
+    public <A extends Annotation> Set<A> loadSpecifiedAnnotations(
+    		@NotNull Class<A> type, String nameSpace, 
+    		 @Validate(Long.class) Set<Long> annotatorIds, 
+    		 boolean linkedObjects);
+    
 }
