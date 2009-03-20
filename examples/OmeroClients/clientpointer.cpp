fbaf262a30adfad3d3a5e1a3263f4d0dddfec397
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-03-20 13:22:02 +0000

    OmeroClients : Workaround for ticket:1246 in clientpointer.cpp
    git-svn-id: file:///home/svn/omero/trunk@4108 05709c45-44f0-0310-885b-81a1db45b4a6

--- examples/OmeroClients/clientpointer.cpp
+++ examples/OmeroClients/clientpointer.cpp
@@ -1,8 +1,15 @@
 #include <omero/client.h>
 int main(int argc, char* argv[])
 {
+    // Duplicating the argument list. ticket:1246
+    Ice::StringSeq args1 = Ice::argsToStringSeq(argc, argv);
+    Ice::StringSeq args2(args1);
+    Ice::InitializationData id1, id2;
+    id1.properties = Ice::createProperties(args1);
+    id2.properties = Ice::createProperties(args2);
+
     // Either
-    omero::client client(argc, argv);
+    omero::client client(id1);
     try {
         // Do something like
         // client.createSession();
@@ -14,7 +21,7 @@ int main(int argc, char* argv[])
     // Or
     //
     {
-        omero::client_ptr client = new omero::client(argc, argv);
+        omero::client_ptr client = new omero::client(id2);
 
         // Do something like
         // client->createSession();
