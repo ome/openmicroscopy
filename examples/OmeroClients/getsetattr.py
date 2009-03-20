00e9611bcbe92537391e5698f0b93333a67efd8a
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-03-20 14:40:11 +0000

    OmeroClients : Adding getsetattr.py
    git-svn-id: file:///home/svn/omero/trunk@4111 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ examples/OmeroClients/getsetattr.py
@@ -0,0 +1,28 @@
+import omero
+from omero.rtypes import *
+
+i = omero.model.ImageI()
+
+#
+# Without __getattr__ and __setattr__
+#
+i.setName( rstring("name") )
+assert i.getName().getValue() == "name"
+
+#
+# With __getattr__ and __setattr__
+#
+i = omero.model.ImageI()
+i.name = rstring("name")
+assert i.name.val == "name"
+
+#
+# Collections, however, cannot be accessed
+# via the special methods due to the dangers
+# outlined above
+#
+try:
+    i.datasetLinks[0]
+except AttributeError, ae:
+    pass
+    
