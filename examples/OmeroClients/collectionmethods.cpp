cf40e580d2a4b228cc0e2f7e9a8b2ea54d7b0871
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-03-20 09:29:56 +0000

    OmeroClients : Adding collectionmethods examples
    git-svn-id: file:///home/svn/omero/trunk@4084 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ examples/OmeroClients/collectionmethods.cpp
@@ -0,0 +1,65 @@
+#include <omero/model/DatasetI.h>
+#include <omero/model/DatasetImageLinkI.h>
+#include <omero/model/EventI.h>
+#include <omero/model/ImageI.h>
+#include <omero/model/PixelsI.h>
+
+using namespace omero::model;
+int main(int argc, char* argv[]) {
+
+    ImagePtr image = new ImageI(1, true);
+    image->getDetails()->setUpdateEvent( new EventI(1L, false) );
+
+    // On creation, all collections are
+    // initialized to empty, and can be added
+    // to.
+    assert(image->sizeOfDatasetLinks() == 0);
+    DatasetPtr dataset = new DatasetI(1L, false);
+    DatasetImageLinkPtr link = image->linkDataset(dataset);
+    assert(image->sizeOfDatasetLinks() == 1);
+
+    // If you want to work with this collection,
+    // you'll need to get a copy.
+    ImageDatasetLinksSeq links = image->copyDatasetLinks();
+
+    // When you are done working with it, you can
+    // unload the datasets, assuming the changes
+    // have been persisted to the server.
+    image->unloadDatasetLinks();
+    assert(image->sizeOfDatasetLinks() < 0);
+    try {
+        image->linkDataset( new DatasetI() );
+    } catch (...) {
+        // Can't access an unloaded collection
+    }
+
+    // The reload...() method allows one instance
+    // to take over a collection from another, if it
+    // has been properly initialized on the server.
+    // sameImage will have it's collection unloaded.
+    ImagePtr sameImage = new ImageI(1L, true);
+    sameImage->getDetails()->setUpdateEvent( new EventI(1L, false) );
+    sameImage->linkDataset( new DatasetI(1L, false) );
+    image->reloadDatasetLinks( sameImage );
+    assert(image->sizeOfDatasetLinks() == 1);
+    assert(sameImage->sizeOfDatasetLinks() < 0);
+
+    // If you would like to remove all the member
+    // elements from a collection, don't unload it
+    // but "clear" it.
+    image->clearDatasetLinks();
+    // Saving this to the database will remove
+    // all dataset links!
+
+    // Finally, all collections can be unloaded
+    // to use an instance as a single row in the db.
+    image->unloadCollections();
+
+    // Ordered collections have slightly different methods.
+    image = new ImageI(1L, true);
+    image->addPixels( new PixelsI() );
+    image->getPixels(0);
+    image->getPrimaryPixels(); // Same thing
+    image->removePixels( image->getPixels(0) );
+
+}
---
+++ examples/OmeroClients/collectionmethods.py
@@ -0,0 +1,61 @@
+import omero
+ImageI = omero.model.ImageI
+DatasetI = omero.model.DatasetI
+EventI = omero.model.EventI
+PixelsI = omero.model.PixelsI
+
+image = ImageI(long(1), True)
+image.getDetails().setUpdateEvent( EventI(1L, False) )
+
+# On creation, all collections are
+# initialized to empty, and can be added
+# to.
+assert image.sizeOfDatasetLinks() == 0
+dataset = DatasetI(long(1), False)
+link = image.linkDataset(dataset)
+assert image.sizeOfDatasetLinks() == 1
+
+# If you want to work with this collection,
+# you'll need to get a copy.
+links = image.copyDatasetLinks()
+
+# When you are done working with it, you can
+# unload the datasets, assuming the changes
+# have been persisted to the server.
+image.unloadDatasetLinks()
+assert image.sizeOfDatasetLinks() < 0
+try:
+    image.linkDataset( DatasetI() )
+except:
+    # Can't access an unloaded collection
+    pass
+
+# The reload...() method allows one instance
+# to take over a collection from another, if it
+# has been properly initialized on the server.
+# sameImage will have it's collection unloaded.
+sameImage = ImageI(1L, True)
+sameImage.getDetails().setUpdateEvent( EventI(1L, False) )
+sameImage.linkDataset( DatasetI(long(1), False) )
+image.reloadDatasetLinks( sameImage )
+assert image.sizeOfDatasetLinks() == 1
+assert sameImage.sizeOfDatasetLinks() < 0
+
+# If you would like to remove all the member
+# elements from a collection, don't unload it
+# but "clear" it.
+image.clearDatasetLinks()
+# Saving this to the database will remove
+# all dataset links!
+
+# Finally, all collections can be unloaded
+# to use an instance as a single row in the db.
+image.unloadCollections()
+
+# Ordered collections have slightly different methods.
+image = ImageI(long(1), True)
+image.addPixels( PixelsI() )
+image.getPixels(0)
+image.getPrimaryPixels() # Same thing
+image.removePixels( image.getPixels(0) )
+
