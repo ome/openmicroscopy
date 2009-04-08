dc9974b2f6280add8dc2499b18ba811b92dafea5
andrew <andrew@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-04-08 12:55:43 +0000

    Adding code to create a Image_5D viewer of the correct size (x,y,z,c,t) of image selected for omero server. No pixel data is yet copied into the viewer.
    git-svn-id: file:///home/svn/omero/trunk@4237 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroImageJ/Omero_ImageJ/src/ome/ij/Omero_ImageJ.java
+++ components/tools/OmeroImageJ/Omero_ImageJ/src/ome/ij/Omero_ImageJ.java
@@ -6,25 +6,19 @@ import java.util.List;
 import Glacier2.CannotCreateSessionException;
 import Glacier2.PermissionDeniedException;
 import ij.IJ;
+
 import ij.plugin.PlugIn;
+
 import ij.gui.NewImage;
 import ij.ImagePlus;
+import i5d.Image5D;
 
-
-import static omero.rtypes.*;
-import omero.RString;
 import omero.ServerError;
 import omero.client;
-import omero.api.IAdminPrx;
 import omero.api.IContainerPrx;
 import omero.api.RawPixelsStorePrx;
 import omero.api.ServiceFactoryPrx;
-import omero.api.ThumbnailStorePrx;
-import omero.model.Dataset;
-import omero.model.IObject;
-import omero.model.Project;
 import omero.model.Image;
-import omero.sys.EventContext;
 import omero.sys.ParametersI;
 
 public class Omero_ImageJ implements PlugIn {
@@ -78,30 +72,39 @@ public class Omero_ImageJ implements PlugIn {
 			RawPixelsStorePrx theRawPixelsStore = theNewServiceFactory.createRawPixelsStore();
 			theRawPixelsStore.setPixelsId(theImage.getPrimaryPixels().getId().getValue(), true);
 			theArray = theRawPixelsStore.getPlane(0, 0, 0);
+		    IJ.showStatus("Reading Image... (got pixel store)");
 		} catch (ServerError e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-//		ImagePlus theIjImagePlus = NewImage.createByteImage(, , , , );
-//		NewImage. theIjImage = null;
-//		theIjImage.
-/*        
-        ru.exec("import i5d.Image5D");
-        ru.setVar("title", imp.getTitle());
-        ru.setVar("stack", imp.getStack());
-        ru.setVar("sizeC", c);
-        ru.setVar("sizeZ", z);
-        ru.setVar("sizeT", t);
-        ru.exec("i5d = new Image5D(title, stack, sizeC, sizeZ, sizeT)");
-        ru.setVar("cal", imp.getCalibration());
-        ru.setVar("fi", imp.getOriginalFileInfo());
-        ru.exec("i5d.setCalibration(cal)");
-        ru.exec("i5d.setFileInfo(fi)");
-        //ru.exec("i5d.setDimensions(sizeC, sizeZ, sizeT)");
-        ru.exec("i5d.show()");
-*/
+
+        try {
+    	    IJ.showStatus("Creating Image5D...");
+    	    int imageCreationOptions = 0;
+    	    
+        	Image5D i5d = createImage5D(
+            		theImage.getName().getValue(), 
+            		theImage.getPrimaryPixels().getSizeX().getValue(), 
+            		theImage.getPrimaryPixels().getSizeY().getValue(), 
+            		theImage.getPrimaryPixels().getSizeC().getValue(), 
+            		theImage.getPrimaryPixels().getSizeZ().getValue(), 
+            		theImage.getPrimaryPixels().getSizeT().getValue(), 
+            		8, /* 8, 16 or 32 */
+            		imageCreationOptions
+            		);
+            i5d.setDefaultColors();
+            i5d.setDefaultChannelNames();
+            if (i5d!=null) {
+                i5d.show();
+            }
+
+        }
+        catch(OutOfMemoryError e) {
+        	IJ.outOfMemory("New_Image5D");
+        }
+	    IJ.showStatus("Saving Preferances...");
 		options.savePreferences();
+	    IJ.showStatus("Omero Reader - Done");
 	}
 
 	public Image getImage(IContainerPrx iContainer, Long theImageId)
@@ -110,10 +113,10 @@ public class Omero_ImageJ implements PlugIn {
 		{
 			List<Long> ids = new ArrayList<Long>(1);
 			ids.add(theImageId);
-			ParametersI po = new ParametersI();
-			po.leaves();
+			ParametersI param = new ParametersI();
+			param.leaves();
 			List<Image> images = 
-				iContainer.getImages(Image.class.getName(), ids, po.map());
+				iContainer.getImages(Image.class.getName(), ids, param);
 			if (images.size() > 0)
 			{
 				Image image = (Image) images.get(0);
@@ -126,6 +129,49 @@ public class Omero_ImageJ implements PlugIn {
 			throw new RuntimeException(e);
 		}
 	}
+
+    public static Image5D createImage5D(
+    		String title, 
+    		int width, 
+    		int height, 
+    		int nChannels, 
+    		int nSlicesZ, 
+    		int nFramesT, 
+    		int bitDepth, 
+    		int options) 
+    {
+        int imageType = ImagePlus.GRAY8;
+        switch (bitDepth) {
+            case 8:
+                imageType = ImagePlus.GRAY8;
+                break;
+            case 16:
+                imageType = ImagePlus.GRAY16;
+                break;
+            case 32:
+                imageType = ImagePlus.GRAY32;
+                break;
+            default:
+                return null;
+        }
+
+        options |= NewImage.CHECK_AVAILABLE_MEMORY;
+
+        // Create Image5D
+        Image5D i5d = new Image5D(title, imageType, width, height, nChannels, nSlicesZ, nFramesT, true /* was fill */);
+
+        for (int c=1; c<=nChannels; c++) {
+            for (int s=1; s<=nSlicesZ; s++) {
+                for (int f=1; f<=nFramesT; f++) {
+                    ImagePlus imp = NewImage.createImage(title, width, height, 1, bitDepth, options);
+                    i5d.setPixels(imp.getProcessor().getPixels(), c, s, f);
+                }
+            }
+        }
+
+        i5d.updateImageAndDraw();
+        return i5d;        
+    }
 
 }
 
