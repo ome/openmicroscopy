4bd761717e22b186a348771d6b5c2ce7399f11df
donald <donald@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-05-07 12:55:47 +0000

    fix
    git-svn-id: file:///home/svn/omero/trunk@4402 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/scripts/makemovie.py
+++ components/tools/OmeroPy/scripts/makemovie.py
@@ -352,7 +352,7 @@ def writeMovie(commandArgs, session):
 	buildAVI(sizeX, sizeY, filelist, commandArgs["fps"], commandArgs["output"], commandArgs["format"]);
 	uploadMovie(client, session, omeroImage, commandArgs["output"], commandArgs["format"])
 	
-client = scripts.client('makemovie12337','MakeMovie creates a movie of the image and attaches it to the originating image.', \
+client = scripts.client('makemovie','MakeMovie creates a movie of the image and attaches it to the originating image.', \
 scripts.Long("imageId").inout(), scripts.String("output").inout(), scripts.Long("zStart").inout(),\
 scripts.Long("zEnd").inout(), scripts.Long("tStart").inout(), scripts.Long("tEnd").inout(), \
 scripts.Set("channels").inout(), scripts.Bool("splitView").inout(), scripts.Bool("showTime").inout(),scripts.Bool("showPlaneInfo").inout(), \
