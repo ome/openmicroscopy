af7aa591ba7ca8570227ee4521edf2f0f7ca4d70
donald <donald@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-05-29 12:48:39 +0000

    new util methods for scripts
    git-svn-id: file:///home/svn/omero/trunk@4451 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/tools/OmeroPy/src/omero/util/script_utils.py
@@ -0,0 +1,178 @@
+import getopt, sys, os, subprocess
+import numpy;
+from struct import *
+import hashlib
+
+import PIL
+from PIL import Image
+import ImageDraw
+
+import omero
+import omero_Constants_ice
+from omero.rtypes import *
+import omero.util.pixelstypetopython as pixelstopython;
+
+
+def drawTextOverlay(draw, x, y, text, colour='0xffffff'):
+	draw.text((x, y), text, fill=colour)
+
+def drawLineOverlay(draw, x0, y0, text, colour='0xffffff'):
+	draw.text((x, y), text, fill=colour)
+
+def rgbToRGBInt(red, green, blue):
+	RGBInt = (red<<16)+(green<<8)+blue;
+	return int(RGBInt);
+	
+def RGBToPIL(RGB):
+	hexval = hex(int(RGB));
+	return '#'+(6-len(hexval[2:]))*'0'+hexval[2:];
+
+def rangeToStr(range):
+	first = 1;
+	string = "";
+	for value in range:
+		if(first==1):
+			string = str(value);
+			first = 0;
+		else:
+			string = string + ','+str(value)
+	return string;
+
+def rmdir_recursive(dir):
+	for name in os.listdir(dir):
+		full_name = os.path.join(dir, name)
+		# on Windows, if we don't have write permission we can't remove
+		# the file/directory either, so turn that on
+		if not os.access(full_name, os.W_OK):
+			os.chmod(full_name, 0600)
+		if os.path.isdir(full_name):
+			rmdir_recursive(full_name)
+		else:
+			os.remove(full_name)
+	os.rmdir(dir)
+
+def calcSha1(filename):
+	fileHandle = open(filename)
+	h = hashlib.sha1()
+	h.update(fileHandle.read())
+	hash = h.hexdigest()
+	fileHandle.close()
+	return hash;	
+
+def getFormat(queryService, format):
+	return queryService.findByQuery("from Format as f where f.value='"+format+"'", None)
+
+def createFile(updateService, filename, format):
+ 	originalFile = omero.model.OriginalFileI();
+	originalFile.setName(omero.rtypes.rstring(filename));
+	originalFile.setPath(omero.rtypes.rstring(filename));
+	originalFile.setFormat(format);
+	originalFile.setSize(omero.rtypes.rlong(os.path.getsize(filename)));
+	originalFile.setSha1(omero.rtypes.rstring(calcSha1(filename)));
+	return updateService.saveAndReturnObject(originalFile);	
+	
+def uploadFile(rawFileStore, file):
+	rawFileStore.setFileId(file.getId().getValue());
+	fileSize = file.getSize().getValue();
+	increment = 10000;
+	cnt = 0;
+	fileHandle = open(file.getName().getValue(), filename, 'rb');
+	done = 0
+	while(done!=1):
+		if(increment+cnt<fileSize):
+			blockSize = increment;
+		else:
+			blockSize = fileSize-cnt;
+			done = 1;
+		fileHandle.seek(cnt);
+		block = fileHandle.read(blockSize);
+		rawFileStore.write(block, cnt, blockSize);
+		cnt = cnt+blockSize;
+	fileHandle.close();
+	
+def attachFileToImage(updateService, image, file, nameSpace):
+	fa = omero.model.FileAnnotationI();
+	fa.setFile(file);
+	fa.setNs(omero.rtypes.rstring(nameSpace))
+	l = omero.model.ImageAnnotationLinkI();
+	l.setParent(image);
+	l.setChild(fa);
+	return updateService.saveAndReturnObject(l);
+
+def addAnnotationToImage(updateService, image, annotation):
+	l = omero.model.ImageAnnotationLinkI();
+	l.setParent(image);
+	l.setChild(annotation);
+	return updateService.saveAndReturnObject(l);
+
+def downloadPlane(rawPixelStore, pixels, z, c, t):
+	rawPlane = rawPixelStore.getPlane(z, c, t);
+	sizeX = pixels.getSizeX().getValue();
+	sizeY = pixels.getSizeY().getValue();
+	pixelType = pixels.getPixelsType().getValue().getValue();
+	convertType ='>'+str(sizeX*sizeY)+pixelstypetopython.toPython(pixelType);
+	convertedPlane = unpack(convertType, rawPlane);
+	remappedPlane = numpy.array(convertedPlane, dtype=(pixelType));
+	remappedPlane.resize(sizeX, sizeY);
+	return remappedPlane;
+
+def uploadPlane(rawPixelStore, plane, z, c, t):
+	byteSwappedPlane = plane.byteswap();
+	convertedPlane = byteSwappedPlane.tostring();
+	rawPixelStore.setPlane(convertedPlane, z, c, t)
+
+def getRenderingEngine(session, pixelsId):	
+	renderingEngine = session.createRenderingEngine();
+	renderingEngine.lookupPixels(pixelsId);
+	if(renderingEngine.lookupRenderingDef(pixelsId)==0):
+		renderingEngine.resetDefaults();
+	renderingEngine.lookupRenderingDef(pixelsId);
+	renderingEngine.load();
+	return renderingEngine;
+
+def createPlaneDef(z,t):
+	planeDef = omero.romio.PlaneDef()
+	planeDef.t = t;
+	planeDef.z = z;
+	planeDef.x = 0;
+	planeDef.y = 0;
+	planeDef.slice = 0;
+	return planeDef;
+
+def getPlaneAsPackedInt(renderingEngine, z, t):
+	planeDef = createPlaneDef(z, t);
+	return renderingEngine.renderAsPackedInt(planeDef);
+	
+def getRawPixelsStore(session, pixelsId):
+	rawPixelsStore = session.createRawPixelsStore();
+	rawPixelsStore.setPixelsId(pixelsId);
+	return rawPixelsStore;
+
+def getRawFileStore(session, fileId):
+	rawFileStore = session.createRawFileStore();
+	rawFileStore.setFileId(fileId);
+	return rawFileStore;
+
+def getPlaneInfo(iQuery, pixelsId, asOrderedList=0):
+	query = "from PlaneInfo as Info where pixels.id='"+str(pixelsId)+"' orderby info.deltaT"
+	infoList = queryService.findAllByQuery(query,None)
+
+	if(asOrderedList==0):
+		map = {}
+		for info in infoList:
+			key = "z:"+str(info.theZ.getValue())+"t:"+str(info.theT.getValue())+"c:"+str(info.theC.getValue());
+			map[key] = info.deltaT.getValue();
+		return map;	 
+	else:
+		return infoList;	
+
+def IdentityFn(commandArgs):
+	return commandArgs;
+
+def parseInputs(client, session, processFn=IdentityFn):
+	inputKeys = client.getInputKeys();
+	commandArgs = {};
+	for key in inputKeys:
+		commandArgs[key]=client.getInput(key).getValue();
+	return processFn(commandArgs);	
+	
