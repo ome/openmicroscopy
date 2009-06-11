55d6762dd88c4bafaa5bce883d1c88c2a6919d91
brain <brain@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-06-11 13:40:26 +0000

    increase to testengine memory useage flag
    git-svn-id: file:///home/svn/omero/trunk@4462 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/src/omero/plugins/import.py
+++ components/tools/OmeroPy/src/omero/plugins/import.py
@@ -25,7 +25,7 @@ class ImportControl(BaseControl):
         client_dir = self.ctx.dir / "lib" / "client"
         log4j = "-Dlog4j.configuration=log4j.properties"
         classpath = [ file.abspath() for file in client_dir.files("*.jar") ]
-        xargs = [ log4j, "-Xmx256M", "-cp", os.pathsep.join(classpath) ]
+        xargs = [ log4j, "-Xmx1024M", "-cp", os.pathsep.join(classpath) ]
         p = omero.java.popen(self.command + args.args, debug=False, xargs = xargs, stdout=sys.stdout)
         self.ctx.rv = p.wait()
 
