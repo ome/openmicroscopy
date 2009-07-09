2a68d9a6437b858c81188fcb6f2eccfbc125420b
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-07-09 14:42:02 +0000

    OmeroClients : Fixing hangs in configuration example
    git-svn-id: file:///home/svn/omero/trunk@4585 05709c45-44f0-0310-885b-81a1db45b4a6

--- examples/OmeroClients/configuration.cpp
+++ examples/OmeroClients/configuration.cpp
@@ -7,17 +7,20 @@ int main(int argc, char* argv[]) {
     // --Ice.Config=file.config
     // No username, password entered
     omero::client client11(argc, argv);
-    /* client1.createSession(); */
+    client1.createSession();
+    client1.closeSession());
 
     // Most basic configuration.
     // Uses default port 4063
     // createSession needs username and password
     omero::client client2("localhost");
-    /* client2.createSession("root", "ome"); */
+    client2.createSession("root", "ome");
+    client2.closeSession();
 
     // Configuration with port information
     omero::client client3("localhost", 24063);
-    /* client3.createSession("root", "ome"); */
+    client3.createSession("root", "ome");
+    client3.closeSession();
 
     // Advanced configuration in C++ takes place
     // via an InitializationData instance.
@@ -25,7 +28,8 @@ int main(int argc, char* argv[]) {
     data.properties = Ice::createProperties();
     data.properties->setProperty("omero.host", "localhost");
     omero::client client4(data);
-    /* client4.createSession("root", "ome"); */
+    client4.createSession("root", "ome");
+    client4.closeSession();
 
     // std::map to be added (ticket:1278)
     data.properties->setProperty("omero.user", "root");
@@ -34,5 +38,6 @@ int main(int argc, char* argv[]) {
     // Again, no username or password needed
     // since present in the data. But they *can*
     // be overridden.
-    /* client5.createSession(); */
+    client5.createSession();
+    client5.closeSession();
 }
--- examples/OmeroClients/configuration.py
+++ examples/OmeroClients/configuration.py
@@ -4,17 +4,20 @@ import omero
 # --Ice.Config=file.config
 # No username, password entered
 client1 = omero.client(args)
-## client1.createSession()
+client1.createSession()
+client1.closeSession()
 
 # Most basic configuration.
 # Uses default port 4063
 # createSession needs username and password
 client2 = omero.client("localhost")
-## client2.createSession("root","ome")
+client2.createSession("root","ome")
+client2.closeSession()
 
 # Configuration with port information
 client3 = omero.client("localhost", 10463)
-## client3.createSession("root","ome")
+client3.createSession("root","ome")
+client3.closeSession()
 
 # Advanced configuration can also be done
 # via an InitializationData instance.
@@ -22,7 +25,8 @@ data = Ice.InitializationData()
 data.properties = Ice.Util.createProperties()
 data.properties.setProperty("omero.host", "localhost")
 client4 = omero.client(data)
-## client4.createSession("root","ome")
+client4.createSession("root","ome")
+client4.closeSession()
 
 # Or alternatively via a dict instance
 m = {"omero.host":"localhost",
@@ -32,4 +36,5 @@ client5 = omero.client(m)
 # Again, no username or password needed
 # since present in the map. But they *can*
 # be overridden.
-## client5.createSession()
+client5.createSession()
+client5.closeSession()
