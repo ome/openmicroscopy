803e8a39e8ae3f43d97dd28cf81239d4e1699fb3
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-07-09 21:09:43 +0000

    A partner-script for update_bioformats_jars
    git-svn-id: file:///home/svn/omero/trunk@4588 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/tools/OmeroImporter/commit_bioformats_jars.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# This script is intended to be called from lib/repository
+# at the top-level (along with update_bioformats_jars)
+#
+
+set -e
+set -u
+set -x
+
+OLD=$1
+NEW=$2
+
+perl -i -pe "s/versions.bio-formats=$OLD/versions.bio-formats=$NEW/" ../../etc/omero.properties
+svn add ../../etc/omero.properties
+LIST="bio-formats jai_imageio loci-common mdbtools-java ome-xml poi-loci"
+for l in $LIST; do
+    svn del $l-$OLD.jar
+    svn add $l-$NEW.jar
+    perl -i -pe "s/$l-$OLD.jar/$l-$NEW.jar/" ../../.classpath
+done
+svn add ../../.classpath
