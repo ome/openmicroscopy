4580929354d0ed6f38a7685d5148a1ea223c561f
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-07-13 21:11:34 +0000

    OmeroMatlab : Improving output of `help OmeroMatlab`
    git-svn-id: file:///home/svn/omero/trunk@4615 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroM/src/Contents.m
+++ components/tools/OmeroM/src/Contents.m
@@ -1,3 +1,24 @@
-%   Access Image data stored in OMERO:
-%      <a href="matlab:open_content_html">OMERO Matlab Contents</a>
-
+%
+% Access Image data stored in OMERO image database
+% ------------------------------------------------
+%
+% The OmeroMatlab toolbox can be used to access image data stored
+% in an OMERO server. You must have a valid login on the server
+% in order to use this toolbox.
+%
+% Basic usage:
+% -----------
+%   omero_client = loadOmero;
+%   session = omero_client.createSession();
+%   try
+%     query_svc = session.getQueryService();
+%     params = omero.sys.ParametersI();
+%     images = query_svc.findAllByQuery('select i from Image i where i.name like ''%2009%'' ', params);
+%   catch ME1
+%     % Handle it somehow
+%
+%   % But be sure to close the connetion.
+%   omero_client.closeSession()
+%
+% See the <a href="http://trac.openmicroscopy.org.uk/omero/wiki/OmeroMatlab">OMERO Matlab developers' page</a> for more information.
+%
