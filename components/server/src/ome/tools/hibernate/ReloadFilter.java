26a0e17f3041e7cd5195e77b4e866454131ada45
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-07-13 10:32:14 +0000

    ticket:1408 - Adding api usage exception if an UPDATE is suspected
    git-svn-id: file:///home/svn/omero/trunk@4607 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/server/src/ome/tools/hibernate/ReloadFilter.java
+++ components/server/src/ome/tools/hibernate/ReloadFilter.java
@@ -9,6 +9,7 @@ package ome.tools.hibernate;
 import java.util.HashMap;
 import java.util.Map;
 
+import ome.conditions.ApiUsageException;
 import ome.model.IEnum;
 import ome.model.IObject;
 import ome.util.Filterable;
@@ -45,45 +46,61 @@ public class ReloadFilter extends UpdateFilter {
             return f;
         }
 
-        // If the object is unloaded, then we load it.
         if (f instanceof IObject) {
+
+            // For the moment we are only worrying with IObjects.
+            // The logic for handling Details, and other Filterable objects
+            // should be the default.
             IObject o = (IObject) f;
+
+            // If the object is unloaded, then we load it.
             if (!o.isLoaded()) {
                 return (IObject) session.get(o.getClass(), o.getId());
             }
-        }
 
-        // If the object is an enum, then we try to match based on value.
-        if (f instanceof IEnum) {
+            // If the object is an enum, then we try to match based on value.
+            if (f instanceof IEnum) {
 
-            IEnum e = (IEnum) f;
-            String val = e.getValue();
+                IEnum e = (IEnum) f;
+                String val = e.getValue();
 
-            // First, heck if in map.
-            Map<String, IEnum> enums = enumsMap.get(f.getClass());
-            if (enums != null) {
-                IEnum enu = enums.get(val);
-                if (enu != null) {
-                    return enu;
+                // First, heck if in map.
+                Map<String, IEnum> enums = enumsMap.get(f.getClass());
+                if (enums != null) {
+                    IEnum enu = enums.get(val);
+                    if (enu != null) {
+                        return enu;
+                    }
+                }
+
+                // We haven't seen this enum yet. Try to load it, and if found
+                // put it in enumsMap
+                Query q = session.createQuery(String.format(
+                        "select e from %s e where e.value = :val", f.getClass()
+                                .getName()));
+                q.setString("val", val);
+                IEnum existing = (IEnum) q.uniqueResult();
+                if (existing != null) {
+                    enums = enumsMap.get(f.getClass());
+                    if (enums == null) {
+                        enums = new HashMap<String, IEnum>();
+                        enumsMap.put(f.getClass(), enums);
+                    }
+                    enums.put(val, existing);
+                    return existing;
                 }
             }
 
-            // We haven't seen this enum yet. Try to load it, and if found
-            // put it in enumsMap
-            Query q = session.createQuery(String.format(
-                    "select e from %s e where e.value = :val", f.getClass()
-                            .getName()));
-            q.setString("val", val);
-            IEnum existing = (IEnum) q.uniqueResult();
-            if (existing != null) {
-                enums = enumsMap.get(f.getClass());
-                if (enums == null) {
-                    enums = new HashMap<String, IEnum>();
-                    enumsMap.put(f.getClass(), enums);
-                }
-                enums.put(val, existing);
-                return existing;
+            // Finally, if we've reached here, then this object is not an enum
+            // and not unloaded. If it has an id, that means a user *might* be
+            // trying to make an UPDATE, but these operations assume INSERT
+            // only.
+            if (o.getId() != null) {
+                throw new ApiUsageException(
+                        "INSERTs only! Pass only new objects, enums, or "
+                                + "unloaded objects to this method.");
             }
+
         }
 
         return super.filter(fieldId, f);
