f28df35e80994bb54b0cb285652b66635fb0dbaa
donald <donald@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-08-17 15:15:25 +0000

    updated for new gateway.utils
    git-svn-id: file:///home/svn/omero/trunk@4727 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroM/demo/drawImagetest.m
+++ components/tools/OmeroM/demo/drawImagetest.m
@@ -4,8 +4,10 @@ channel = 0;
 figure(1);
 for z=1:pixels.getSizeZ().getValue(),
     for t=1:pixels.getSizeT().getValue(),
-        plane = gatway.getPlane(pixelsId, z-1, channel, t-1);
+        rawPlane = gateway.getPlane(pixelsId, z-1, channel, t-1);
         figure(1);
+        plane2D = omerojava.util.GatewayUtils.getPlane2D(pixels,rawPlane);
+        plane = plane2D.getPixelsArrayAsDouble(1);
         imagesc(plane);
         drawnow;
     end
