1d33c487505c1bd73faa6d350570fd61e09a9df7
donald <donald@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-08-18 09:32:34 +0000

    updated to work with new methods
    git-svn-id: file:///home/svn/omero/trunk@4733 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroM/demo/findAllImages.m
+++ components/tools/OmeroM/demo/findAllImages.m
@@ -6,14 +6,14 @@ projects = gateway.getProjects([], true);
 datasetList = java.util.ArrayList;
 
 for projectCnt = 0:projects.size()-1,
-	projectDatasetList = getDatasetsFromProject(omerojService, projects.get(projectCnt));
+	projectDatasetList = projects.get(projectCnt).linkedDatasetList;
     for j = 0:projectDatasetList.size()-1,
     	datasetList.add(projectDatasetList.get(j));
     end
 end
 images = java.util.ArrayList;
 for i = 0:datasetList.size()-1,
-    imageList = getImagesFromDataset(omerojService, datasetList.get(i));
+    imageList = datasetList.get(i).linkedImageList;
     for j = 0:imageList.size()-1,
         images.add(imageList.get(j));
     end
