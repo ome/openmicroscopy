ebb267cf4a878856f025a7494b8d19a07b74c751
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-08-25 11:48:50 +0000

    OmeroMatlab : More fixes for testing on Windows
    git-svn-id: file:///home/svn/omero/trunk@4761 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroM/test/omero_test.bat
+++ components/tools/OmeroM/test/omero_test.bat
@@ -8,5 +8,5 @@ REM OMERO_HOME must be set in the calling environment.
 REM
 
 cd %~dp0
-start matlab -nosplash -nodesktop -minimize -r "omero_test" -logfile ..\target
+start matlab -nosplash -nodesktop -minimize -r "omero_test" -logfile ..\target\omero_test.log
 
--- components/tools/OmeroM/test/omero_test.m
+++ components/tools/OmeroM/test/omero_test.m
@@ -2,13 +2,18 @@
 %  Copyright (c) 2009, Glencoe Software, Inc.
 %  See LICENSE for details.
 
-omero_home = getenv('OMERO_HOME');
-cd(omero_home)
-
-[c,s,g] = loadOmero;
-disp(s.getConfigService().getVersion());
-c.closeSession();
-clear g;
-clear s;
-clear c;
-unloadOmero;
+try
+  omero_home = getenv('OMERO_HOME');
+  cd(omero_home)
+  [c,s,g] = loadOmero;
+  disp(s.getConfigService().getVersion());
+  c.closeSession();
+  clear g;
+  clear s;
+  clear c;
+  unloadOmero;
+catch ME
+  disp('FAILED');
+  disp(ME);
+end
+exit;
