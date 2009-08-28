954840790ad8b01cce1c25d4fdf5f1f09f65fde4
donald <donald@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-08-28 13:30:32 +0000

    added documentation
    git-svn-id: file:///home/svn/omero/trunk@4784 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroM/helper/toMatrix.m
+++ components/tools/OmeroM/helper/toMatrix.m
@@ -1,4 +1,8 @@
-function [mat] = toMatrix(data, pixels)
-a = typecast(data, char(pixels.getPixelsType().getValue().getValue()));
+% toMatrix: This function converts the binaryData from the server to a
+% matlab matrix.
+% binaryData: The data obtained using gateway.getPlane(...)
+% pixels: the pixels object of the image the plane came from.
+function [mat] = toMatrix(binaryData, pixels)
+a = typecast(binaryData, char(pixels.getPixelsType().getValue().getValue()));
 b = reshape(a, pixels.getSizeX().getValue(), pixels.getSizeY().getValue());
 mat = swapbytes(b);
