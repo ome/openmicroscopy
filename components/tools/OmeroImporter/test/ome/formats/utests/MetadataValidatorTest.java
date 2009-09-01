2993dde378c0431cbb6016bf48042ad73700d068
callan <callan@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-09-01 15:32:32 +0000

    Ditto for logical channels.
    git-svn-id: file:///home/svn/omero/trunk@4802 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroImporter/test/ome/formats/utests/MetadataValidatorTest.java
+++ components/tools/OmeroImporter/test/ome/formats/utests/MetadataValidatorTest.java
@@ -462,16 +462,17 @@ public class MetadataValidatorTest
 		for (IObjectContainer container : containers)
 		{
 			LSID lsid = new LSID(container.LSID);
-			String e = String.format(
-					"%s %s not found in reference cache", klass, lsid);
-			assertTrue(e, referenceCache.containsKey(lsid));
+			if (!referenceCache.containsKey(lsid))
+			{
+				continue;
+			}
 			List<LSID> references = referenceCache.get(lsid);
 			assertTrue(references.size() > 0);
 			for (LSID referenceLSID : references)
 			{
 				assertNotNull(referenceLSID);
 				klass = FilterSet.class;
-				e = String.format(
+				String e = String.format(
 						"%s with LSID %s not found in container cache",
 						klass, referenceLSID);
 				assertTrue(e, authoritativeLSIDExists(klass, referenceLSID));
