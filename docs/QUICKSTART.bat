faf5c159c88db804fefc1b0c747a8562d6247496
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-09-07 08:28:28 +0000

    OmeroWindows : QUICKSTART.bat tested on Windows Server 2003, 64bit
    git-svn-id: file:///home/svn/omero/trunk@4842 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ docs/QUICKSTART.bat
@@ -0,0 +1,33 @@
+
+REM To prevent needing a restart, this script uses the
+REM current user to run the service.
+
+ECHO "Using current user for service: %USERDOMAIN%\%USERNAME%"
+SET /P PASSWORD=Password:
+
+cd %~dp0\..
+python build.py
+cd dist
+
+set OMERO_CONFIG=quickstart
+dropdb -U postgres %OMERO_CONFIG%
+createdb -U postgres %OMERO_CONFIG%
+createlang -U postgres plpgsql %OMERO_CONFIG%
+pythonl bin\omero db script OMERO4-DEV 4 ome
+psql -U postgres -f OMERO4-DEV__4.sql %OMERO_CONFIG%
+
+set PYTHONPATH=%PYTHONPATH%;lib\python
+python lib\python\omero\install\win_set_path.py
+
+REM Required because of environment-less service
+python bin\omero config def %OMERO_CONFIG%
+
+
+mkdir data
+python bin\omero config set omero.data.dir %CD%\data
+
+python bin\omero config set omero.windows.user %USERDOMAIN%\%USERNAME%
+python bin\omero config set omero.windows.pass "%PASSWORD%"
+python bin\omero config set omero.db.user postgres
+python bin\omero config set omero.db.name %OMERO_CONFIG%
+python bin\omero admin start
