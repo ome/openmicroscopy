a3f83853616768ac90d435078ae1d1cf3cfbc913
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-09-09 15:15:43 +0000

    OmeroExamples : Adding try/finally blocks in configuration.java to prevent hangs
    git-svn-id: file:///home/svn/omero/trunk@4901 05709c45-44f0-0310-885b-81a1db45b4a6

--- examples/OmeroClients/configuration.java
+++ examples/OmeroClients/configuration.java
@@ -1,46 +1,62 @@
 public class configuration {
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-	// All configuration in file pointed to by
-	// --Ice.Config=file.config
-	// No username, password entered
-	omero.client client1 = new omero.client(args);
-	client1.createSession();
-	client1.closeSession();
+    // All configuration in file pointed to by
+    // --Ice.Config=file.config
+    // No username, password entered
+    omero.client client1 = new omero.client(args);
+        try {
+        client1.createSession();
+        } finally {
+        client1.closeSession();
+        }
 
-	// Most basic configuration.
-	// Uses default port 4063
-	// createSession needs username and password
-	omero.client client2 = new omero.client("localhost");
-	client2.createSession("root", "ome");
-	client2.closeSession();
-
-	// Configuration with port information
-	omero.client client3 = new omero.client("localhost", 10463);
-	client3.createSession("root", "ome");
-	client3.closeSession();
-
-	// Advanced configuration can also be done
-	// via an InitializationData instance.
-	Ice.InitializationData data = new Ice.InitializationData();
-	data.properties = Ice.Util.createProperties();
-	data.properties.setProperty("omero.host", "localhost");
-	omero.client client4 = new omero.client(data);
-	client4.createSession("root", "ome");
-	client4.closeSession();
-
-	// Or alternatively via a java.util.Map instance
-	java.util.Map<String, String> map = new java.util.HashMap<String, String>();
-	map.put("omero.host", "localhost");
-	map.put("omero.user", "root");
-	map.put("omero.pass", "ome");
-	omero.client client5 = new omero.client(map);
-	// Again, no username or password needed
-	// since present in the map. But they *can*
-	// be overridden.
-	client5.createSession();
-	client5.closeSession();
+    // Most basic configuration.
+    // Uses default port 4063
+    // createSession needs username and password
+    omero.client client2 = new omero.client("localhost");
+    try {
+        client2.createSession("root", "ome");
+    } finally {
+        client2.closeSession();
     }
+
+    // Configuration with port information
+    omero.client client3 = new omero.client("localhost", 10463);
+    try {
+        client3.createSession("root", "ome");
+    } finally {
+        client3.closeSession();
+    }
+
+    // Advanced configuration can also be done
+    // via an InitializationData instance.
+    Ice.InitializationData data = new Ice.InitializationData();
+    data.properties = Ice.Util.createProperties();
+    data.properties.setProperty("omero.host", "localhost");
+    omero.client client4 = new omero.client(data);
+    try {
+        client4.createSession("root", "ome");
+    } finally {
+        client4.closeSession();
+    }
+
+    // Or alternatively via a java.util.Map instance
+    java.util.Map<String, String> map = new java.util.HashMap<String, String>();
+    map.put("omero.host", "localhost");
+    map.put("omero.user", "root");
+    map.put("omero.pass", "ome");
+    omero.client client5 = new omero.client(map);
+    // Again, no username or password needed
+    // since present in the map. But they *can*
+    // be overridden.
+    try {
+        client5.createSession();
+    } finally {
+        client5.closeSession();
+    }
+
+  }
 
 }
