d3091cbfa193c2551042a3b2a5cb0aeb7e4a0928
donald <donald@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-09-25 13:35:56 +0000

    script upload from default scripts module
    git-svn-id: file:///home/svn/omero/trunk@5093 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/src/omero/plugins/upload.py
+++ components/tools/OmeroPy/src/omero/plugins/upload.py
@@ -12,27 +12,139 @@
 
 from omero.cli import BaseControl
 import omero.util.originalfileutils;
+import omero;
+import omero.rtypes
+from omero.rtypes import rlong
+from omero.rtypes import rint
+from omero.rtypes import rstring
+from omero.rtypes import rdouble
+from omero.rtypes import rfloat
+
+
+try: 
+	import hashlib 
+ 	hash_sha1 = hashlib.sha1 
+except: 
+	import sha 
+	hash_sha1 = sha.new 
+
 class UploadControl(BaseControl):
 
     def help(self, args = None):
         return \
             """
-Syntax: %(program_name)s upload <filename>
-        Upload the given file name
+Syntax: %(program_name)s upload <filename> [1..n]
+        Upload the given files to omero.
+
+Syntax: %(program_name)s upload script
+        Upload default scripts defined in default scripts to the server.
+
+Syntax: %(program_name)s upload pytable <filename> [1..n]
+        Upload the given files to pytables in omero.
+        
     """
+    SCRIPT_ARG='scripts';
+    PYTABLE_ARG='pytable';
+    FILE_ARG='files';
+
+    def calcSha1(self, filename):
+        fileHandle = open(filename)
+        h = hash_sha1()
+        h.update(fileHandle.read())
+        hash = h.hexdigest()
+        fileHandle.close()
+        return hash;
+
+    def createOriginalFile(self, id, name, filename):
+        file = open(filename, 'rb')
+        ofile = omero.model.OriginalFileI(rlong(id))
+        try:
+            size = os.path.getsize(file.name)
+            ofile.size = rlong(size)
+            ofile.sha1 = rstring(self.calcSha1(file.name))
+            ofile.name = rstring(name)
+            ofile.path = rstring(os.path.abspath(file.name));
+            fmt = omero.util.originalfileutils.getFormat(filename);
+            ofile.format =  omero.model.FormatI();
+            ofile.format.value = rstring(fmt[1]);
+            up = self.client.getSession().getUpdateService()
+            ofile = up.saveAndReturnObject(ofile)
+        finally:
+            file.close();
+        return ofile;
+
+    def uploadFile(self, filename, originalFile = None):
+        format = omero.util.originalfileutils.getFormat(filename);
+        omeroFormat = format[1];    
+        if(format[0]==omero.util.originalfileutils.IMPORTER):
+            self.ctx.out("This file should be imported using omero import");
+        return self.client.upload(filename, filename, filename, omeroFormat, originalFile)
+    
+    def uploadFromString(self, string, originalFile):
+        prx = self.client.getSession().createRawFileStore()
+        prx.setFileId(originalFile.id.val)
+        strlen = len(string);
+        prx.write(string, 0, strlen)
+        prx.close()
+        
+    def readCommandArgs(self, commandline):
+        script = False;
+        pytable = False;
+        files = list();
+        for arg in commandline: 
+            if arg in (self.SCRIPT_ARG):
+                script = True;
+            elif arg in (self.PYTABLE_ARG): 
+                pytable = True;	
+            else:
+                files.append(arg);
+        return {self.SCRIPT_ARG:script, self.PYTABLE_ARG:pytable, self.FILE_ARG:files}	         
+
+    def returnSource(self, filename):
+        if(filename[len(filename)-3:] == 'pyc'):
+            return filename[:len(filename)-1]
+        return filename;
+    
+    def uploadDefaultScripts(self, args):
+        import defaultscripts;
+        scripts = defaultscripts.defaultscripts; 
+        for id in scripts:
+            script = scripts[id];
+            filename = "";
+            try:
+                importedScript = __import__(script);
+                filename = self.returnSource(importedScript.__file__);
+            except:
+                raise Exception("Script: " + script + " does not exist");
+            if not filename:
+                raise Exception("Non-null filename must be provided")
+
+            if not os.path.exists(filename):
+                raise Exception("File does not exist: " + filename)
+            
+            originalFile = self.createOriginalFile(id, script, filename);
+            self.uploadFile(filename, originalFile);
+
+    def uploadFromCommandline(self, commandline):
+        fileList = commandline[self.FILE_ARG];
+        for file in fileList:
+            obj = self.uploadFile(file);
+            self.ctx.out("Uploaded %s as " % file + str(obj.id.val))
+
 
     def __call__(self, *args):
         import omero
         args = Arguments(*args)
-        client = self.ctx.conn()
-        for f in args:
-            format = omero.util.originalfileutils.getFormat(f);
-            omeroFormat = format[1];    
-            if(format[0]==omero.util.originalfileutils.IMPORTER):
-                self.ctx.out("This file should be imported using omero import");
-            obj = client.upload(f, f, f, omeroFormat)
-            self.ctx.out("Uploaded %s as " % f + str(obj.id.val))
+        self.client = self.ctx.conn()
+        argMap = self.readCommandArgs(args);
+        # if(argMap[self.SCRIPT_ARG]==True): # and argMap[self.PYTABLE_ARG] == True):
+        #   raise ClientError("upload can only be used with %s or %s arguments" % (self.SCRIPT_ARG, self.PYTABLE_ARG));
 
+        if(argMap[self.SCRIPT_ARG]):
+            self.uploadDefaultScripts(argMap);
+        else:
+            self.uploadFromCommandline(argMap);
+       
 try:
     register("upload", UploadControl)
 except NameError:
