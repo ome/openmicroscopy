351cb398436af0b40e267c494140fb3df0ad417d
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-09-29 14:50:38 +0000

    Fixing bat files
    git-svn-id: file:///home/svn/omero/trunk@5139 05709c45-44f0-0310-885b-81a1db45b4a6

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ REM  Windows build bat for OMERO
 REM  Copyright (c) 2009, Glencoe Software, Inc.
 REM  See LICENSE for details.
 
-python %~dp0\build.py %*
+python "%~dp0\build.py" %*
--- docs/QUICKSTART.bat
+++ docs/QUICKSTART.bat
@@ -8,9 +8,9 @@ ECHO "Logging in user for service: %USERDOMAIN%\%USERNAME%"
 ECHO
 SET /P PASSWORD=Password:
 
-cd %~dp0\..
+cd "%~dp0\.."
 python build.py
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 cd dist
 
 python bin\omero admin stop
@@ -18,46 +18,46 @@ REM Errors ok
 
 set OMERO_CONFIG=quickstart
 dropdb -U postgres %OMERO_CONFIG%
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 createdb -O omero -U postgres %OMERO_CONFIG%
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 createlang -U postgres plpgsql %OMERO_CONFIG%
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 python bin\omero db script OMERO4.1 0 ome
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 psql -U omero -f OMERO4.1__0.sql %OMERO_CONFIG%
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 set PYTHONPATH=%PYTHONPATH%;lib\python
 python lib\python\omero\install\win_set_path.py
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 REM Required because of environment-less service
 python bin\omero config def %OMERO_CONFIG%
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 mkdir data
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 python bin\omero config set omero.data.dir %CD%\data
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 python bin\omero config set omero.windows.user %USERDOMAIN%\%USERNAME%
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 python bin\omero config set omero.windows.pass "%PASSWORD%"
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 python bin\omero config set omero.db.user postgres
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 python bin\omero config set omero.db.name %OMERO_CONFIG%
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 python bin\omero admin start
-if errorlevel 1 exit \b %ERRORLEVEL%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
