9bffd5f577853e9f23a614cc7752608a9f4aca14
ola <ola@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-10-01 11:39:43 +0000

    tests for #1436
    git-svn-id: file:///home/svn/omero/trunk@5182 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/test/integration/itypes.py
+++ components/tools/OmeroPy/test/integration/itypes.py
@@ -12,6 +12,7 @@ import unittest, time
 import test.integration.library as lib
 import omero
 import omero_Constants_ice
+from omero.rtypes import *
 
 class TestTypes(lib.ITest):
 
@@ -19,5 +20,37 @@ class TestTypes(lib.ITest):
     def testGetEnumerationTypes(self):
         i = self.client.sf.getTypesService().getEnumerationTypes()
 
+    def testAllEnumerations(self):
+        types = self.root.sf.getTypesService()
+        rv = dict()
+        for e in types.getOriginalEnumerations():
+            if rv.get(e.__class__.__name__) is None:
+                rv[e.__class__.__name__] = list()
+            rv[e.__class__.__name__].append(e)
+        
+        for r in rv:
+            types.allEnumerations(str(r))
+
+    def testGetEnumerationWithEntries(self):
+        i = self.root.sf.getTypesService().getEnumerationsWithEntries().items()
+    
+    def testManageEnumeration(self):
+        from omero_model_ExperimentTypeI import ExperimentTypeI
+        uuid = self.root.sf.getAdminService().getEventContext().sessionUuid
+        types = self.root.sf.getTypesService()
+        
+        # create enums
+        obj = ExperimentTypeI()
+        obj.setValue(rstring("test_value_%s" % uuid))
+        enum = types.createEnumeration(obj)
+        types.deleteEnumeration(enum)
+        
+        obj = ExperimentTypeI()
+        obj.setValue(rstring("test_value2_%s" % (uuid)))
+        new_entries=[obj]
+        types.updateEnumerations(new_entries)
+        
+        types.resetEnumerations("ExperimentTypeI")
+        
 if __name__ == '__main__':
     unittest.main()
