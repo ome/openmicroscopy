7c855376b4250ab7f1dea617986113771b08a3a7
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-10-05 11:23:04 +0000

    OmeroWindows : Improved QUICKSTART error handling.
    git-svn-id: file:///home/svn/omero/trunk@5217 05709c45-44f0-0310-885b-81a1db45b4a6

--- docs/QUICKSTART.bat
+++ docs/QUICKSTART.bat
@@ -1,63 +1,105 @@
-
-ECHO To prevent needing a restart, this script uses the
-ECHO current user to run the service. You will need to
-ECHO enter your NT password below. Careful: your password
-ECHO will be displayed in cleartext.
-ECHO
-ECHO "Logging in user for service: %USERDOMAIN%\%USERNAME%"
-ECHO
+@echo OFF
+echo.
+echo -----------------------------------------------------
+echo To prevent needing a restart, this script uses the
+echo current user to run the service. You will need to
+echo enter your NT password below. Careful: your password
+echo will be displayed in cleartext.
+echo -----------------------------------------------------
+echo.
+echo Logging in user for service: %USERDOMAIN%\%USERNAME%
+echo.
 SET /P PASSWORD=Password:
 
 cd "%~dp0\.."
-python build.py
-if errorlevel 1 exit /b %ERRORLEVEL%
+if exist dist goto AlreadyBuilt
+  echo Building...
+  python build.py
+  if errorlevel 1 goto ERROR
+  goto Ready
+:AlreadyBuilt
+  echo Server already built. To rebuild, use "build clean"
+  goto Ready
+
+:Ready
 cd dist
 
+echo Stopping server...
 python bin\omero admin stop
-REM Errors ok
+if errorlevel 1 echo "Wasn't running?"
 
-set OMERO_CONFIG=quickstart
+if "x%OMERO_CONFIG%" == "x" (set OMERO_CONFIG=quickstart)
+echo Using OMERO_CONFIG=%OMERO_CONFIG%
+
+echo Dropping %OMERO_CONFIG% db
 dropdb -U postgres %OMERO_CONFIG%
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 echo Didn't exist?
 
+echo Creating omero database user
+createuser -S -D -R -U postgres omero
+if errorlevel 1 echo Already exists?
+
+echo Creating %OMERO_CONFIG% db
 createdb -O omero -U postgres %OMERO_CONFIG%
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 goto ERROR
 
+echo Adding pgsql to db
 createlang -U postgres plpgsql %OMERO_CONFIG%
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 echo Already installed?
 
+echo Creating DB script
 python bin\omero db script OMERO4.1 0 ome
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 goto ERROR
 
-psql -U omero -f OMERO4.1__0.sql %OMERO_CONFIG%
-if errorlevel 1 exit /b %ERRORLEVEL%
+echo Iniitializing DB
+psql -U omero -f OMERO4.1__0.sql %OMERO_CONFIG% 2>quickstart.err >quickstart.out
+if errorlevel 1 goto ERROR
 
-set PYTHONPATH=%PYTHONPATH%;lib\python
+echo Setting PYTHONPATH
+bin\setpythonpath
+if errorlevel 1 goto ERROR
+
+echo Changing etc\grid directory paths
 python lib\python\omero\install\win_set_path.py
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 goto ERROR
 
 REM Required because of environment-less service
+echo Setting config
 python bin\omero config def %OMERO_CONFIG%
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 goto ERROR
 
-mkdir data
-if errorlevel 1 exit /b %ERRORLEVEL%
+if exist data echo Data directory already exists!
+if not exist data (echo Creating data directory && mkdir data)
+if errorlevel 1 goto ERROR
 
+echo Configuring data directory
 python bin\omero config set omero.data.dir %CD%\data
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 goto ERROR
 
+echo Configuring Windows user
 python bin\omero config set omero.windows.user %USERDOMAIN%\%USERNAME%
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 goto ERROR
 
+echo Configuring password
 python bin\omero config set omero.windows.pass "%PASSWORD%"
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 goto ERROR
 
+echo Configuring database user
 python bin\omero config set omero.db.user postgres
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 goto ERROR
 
+echo Configuring database name
 python bin\omero config set omero.db.name %OMERO_CONFIG%
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 goto ERROR
 
+echo Starting server
 python bin\omero admin start
-if errorlevel 1 exit /b %ERRORLEVEL%
+if errorlevel 1 goto ERROR
 
+
+cd "%~dp0\.."
+exit /b 0
+ERROR:
+  echo Failed %ERRORLEVEL%
+  cd "%~dp0\.."
+  exit /b %ERRORLEVEL%
