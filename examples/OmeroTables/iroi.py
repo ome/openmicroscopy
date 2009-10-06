ae3bd0a943546e1a08eba256f9f9f282d63d8d35
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-10-06 05:57:58 +0000

    OmeroExamples : Fixing iroi.py
    git-svn-id: file:///home/svn/omero/trunk@5235 05709c45-44f0-0310-885b-81a1db45b4a6

--- examples/OmeroTables/iroi.py
+++ examples/OmeroTables/iroi.py
@@ -39,8 +39,9 @@ image_id = plate.copyWells()[0].copyWellSamples()[0].image.id.val
 # http://hudson.openmicroscopy.org.uk/job/OMERO/javadoc/slice2html/omero/api/IRoi.html#IRoi
 roi_svc = s.getRoiService()
 roi_meas = roi_svc.getRoiMeasurements(image_id, None)
-roi_result = roi_svc.getMeasuredRois(image_id, roi_meas[0].id.val, None)
 
-table = roi_svc.getTable(roi_meas[0].id.val)
-print table.slice([0],None) # All of column 1
+if len(roi_meas) > 0:
+    roi_result = roi_svc.getMeasuredRois(image_id, roi_meas[0].id.val, None)
+    table = roi_svc.getTable(roi_meas[0].id.val)
+    print table.slice([0],None) # All of column 1
 
