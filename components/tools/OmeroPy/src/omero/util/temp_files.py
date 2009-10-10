29ef013ff963b7692d84879e2cbd40924431b13e
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-10-10 20:27:10 +0000

    TempFileManager : Moving to lock file $TMP/omero_$USER/$PID/.lock
    git-svn-id: file:///home/svn/omero/trunk@5359 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/src/omero/util/temp_files.py
+++ components/tools/OmeroPy/src/omero/util/temp_files.py
@@ -27,76 +27,41 @@ from path import path
 
 class TempFileManager(object):
     """
+    Creates temporary files and folders and makes a best effort
+    to remove them on exit (or sooner). Typically only a single
+    instance of this class will exist ("manager" variable in this
+    module below)
     """
+
     def __init__(self, prefix = "omero"):
         self.logger = logging.getLogger("omero.util.TempFileManager")
         self.is_win32 = ( sys.platform == "win32" )
         self.prefix = prefix
-        self._lock = threading.RLock()
-        self._values = {}
+        self.userdir = path(tempfile.gettempdir()) / ("%s_%s" % (self.prefix, self.username()))
+        # If the given userdir is not accessible, we attempt to use an alternative
+        if not self.create(self.userdir) and not self.access(self.userdir):
+            i = 0
+            while i < 10:
+                t = path("%s_%s" % (self.userdir, i))
+                if self.create(t) or self.access(t):
+                    self.userdir = t
+                    break
+            raise exceptions.Exception("Failed to create temporary directory: %s" % self.userdir)
+        self.dir = self.userdir / str(os.getpid())
+        if not self.dir.exists():
+            self.dir.makedirs()
+        self.lock = open(str(self.dir / ".lock"), "a+")
+        try:
+            portalocker.lock(self.lock, portalocker.LOCK_EX|portalocker.LOCK_NB)
+        except:
+            self.lock.close()
+            raise
         atexit.register(self.cleanup)
-
-    #
-    # Methods requiring locking on state
-    #
-
-    def add_path(self, name, folder):
-        name = str(name)
-        self._lock.acquire()
-        try:
-
-            if name in self._values:
-                raise exceptions.Exception("%s already added!!" % name)
-
-            # DISABLED f = open(name,"r")
-            # portalocker.lock(f, portalocker.LOCK_EX|portalocker.LOCK_NB)
-            self._values[name] = None # f
-
-            self.logger.debug("Added %s%s (total=%s)",\
-                (folder and "folder " or ""), name, len(self._values))
-
-        finally:
-            self._lock.release()
-
-    def remove_path(self, name):
-        name = str(name)
-        self._lock.acquire()
-        try:
-            try:
-                f = self._values[name]
-                # DISABLED f.close()
-                del self._values[name]
-            except KeyError:
-                pass
-        finally:
-            self._lock.release()
-
-        p = path(name)
-        if p.exists():
-            if p.isdir():
-                p.rmtree(onerror = self.on_rmtree)
-                self.logger.debug("Removed folder %s (total=%s)", name, len(self._values))
-            else:
-                p.remove()
-                self.logger.debug("Removed %s (total=%s)", name, len(self._values))
-
 
     def cleanup(self):
         self.logger.debug("Cleaning...")
-        self._lock.acquire()
-        try:
-            for i in self._values.keys():
-                try:
-                    self.remove_path(i)
-                except:
-                    self.logger.info("Failed to remove tempfile: %s" % i)
-            del self._values
-        finally:
-            self._lock.release()
-
-    #
-    # Other methods
-    #
+        self.clean_tempdir()
+        self.lock.close() # Allow others access
 
     def username(self):
         if self.is_win32:
@@ -117,16 +82,7 @@ class TempFileManager(object):
         return False
 
     def gettempdir(self):
-        dir = path(tempfile.gettempdir()) / ("%s_%s" % (self.prefix, self.username()))
-        if self.create(dir) or self.access(dir):
-            return dir
-
-        i = 0
-        while i < 10:
-            t = path("%s_%s" % (dir, i))
-            if self.create(t) or self.access(t):
-                return t
-        raise exceptions.Exception("Failed to create temporary directory")
+        return self.dir
 
     def tempsubdir(self, categories):
         cat = list(categories)
@@ -141,28 +97,52 @@ class TempFileManager(object):
 
         if folder:
             name = tempfile.mkdtemp(prefix = prefix, suffix = suffix, dir = dir)
+            self.logger.debug("Added folder %s", name)
         else:
             fd, name = tempfile.mkstemp(prefix = prefix, suffix = suffix, dir = dir, text = text)
+            self.logger.debug("Added file %s", name)
             try:
                 os.close(fd)
             except:
                 self.logger.warn("Failed to close fd %s" % fd)
 
-        try:
-            self.add_path(name, folder)
-        except:
-            try:
-                self.remove_path(name)
-            except:
-                self.logger.warn("Error during exception cleanup", exc_info = True)
-            raise
-
         return path(name)
+
+    def remove_path(self, name):
+        p = path(name)
+        parpath = p.parpath(self.dir)
+        if len(parpath) < 1:
+            raise exceptions.Exception("%s is not in %s" % (p, self.dir))
+
+        if p.exists():
+            if p.isdir():
+                p.rmtree(onerror = self.on_rmtree)
+                self.logger.debug("Removed folder %s", name)
+            else:
+                p.remove()
+                self.logger.debug("Removed %s", name)
 
     def clean_tempdir(self):
         dir = self.gettempdir()
-        self.logger.info("Removing tree: %s", dir)
+        self.logger.debug("Removing tree: %s", dir)
         dir.rmtree(onerror = self.on_rmtree)
+
+    def clean_userdir(self):
+        self.logger.debug("Cleaning user dir: %s" % self.userdir)
+        dirs = self.userdir.dirs()
+        for dir in dirs:
+            if str(dir) == str(self.dir):
+                self.logger.debug("Skipping self: %s", dir)
+                continue
+            lock = dir / ".lock"
+            f = open(str(lock),"r")
+            try:
+                portalocker.lock(f, portalocker.LOCK_EX|portalocker.LOCK_NB)
+            except:
+                print "Locked: %s" % dir
+                continue
+            dir.rmtree(self.on_rmtree)
+            print "Deleted: %s" % dir
 
     def on_rmtree(self, func, name, exc):
         self.logger.error("rmtree error: %s of %s => %s", func, name, exc)
@@ -192,18 +172,24 @@ def gettempdir():
 if __name__ == "__main__":
 
     from omero.util import configure_logging
-    configure_logging()
 
     if len(sys.argv) > 1:
         args = sys.argv[1:]
+
         if "--debug" in args:
-            logging.getLogger().setLevel(logging.DEBUG)
+            configure_logging(loglevel=logging.DEBUG)
+        else:
+            configure_logging()
+
         if "clean" in args:
-            manager.clean_tempdir()
+            manager.clean_userdir()
             sys.exit(0)
         elif "dir" in args:
             print manager.gettempdir()
             sys.exit(0)
+        elif "lock" in args:
+            print "Locking %s" % manager.gettempdir()
+            raw_input("Waiting on user input...")
     else:
         print "Usage: %s clean" % sys.argv[0]
         print "   or: %s dir  " % sys.argv[0]
