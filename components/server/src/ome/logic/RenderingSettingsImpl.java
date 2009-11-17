3bd3e5242eb0b8e0ccf0076e81d9cca2d4ea7349
jburel <jburel@05709c45-44f0-0310-885b-81a1db45b4a6>
2009-11-17 09:35:45 +0000

    Fixed remaining bug in applySettingsToSet
    git-svn-id: file:///home/svn/omero/trunk@5771 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/server/src/ome/logic/RenderingSettingsImpl.java
+++ components/server/src/ome/logic/RenderingSettingsImpl.java
@@ -836,6 +836,7 @@ public class RenderingSettingsImpl extends AbstractLevel2Service implements
     		                           RenderingDef settingsTo)
     {
     	// Sanity checks
+    	log.info("apply settings: " + pixelsFrom+" "+pixelsTo+" "+settingsFrom+" "+settingsTo);
         boolean b = sanityCheckPixels(pixelsFrom, pixelsTo);
         if (!b)
         {
@@ -1019,6 +1020,12 @@ public class RenderingSettingsImpl extends AbstractLevel2Service implements
     		if (l.size() != 1) 
     			throw new ValidationException("No pixels set with ID: " + from);
     		pixelsFrom = l.get(0);
+    		if (settingsFrom == null) {
+    			List<Pixels> list = new ArrayList<Pixels>(1);
+    			list.add(pixelsFrom);
+    			Map<Long, RenderingDef> map = loadRenderingSettings(list);
+            	settingsFrom = map.get(from);
+    		}
     	}
     		
     	RenderingDef settingsTo;
