9bcabf3768338ce2ee8fe959b3757eae7679e630
jburel <jburel@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-01-15 12:38:54 +0000

    Added to support FS
    git-svn-id: file:///home/svn/omero/trunk@5981 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/tools/OmeroJava/src/pojos/FileData.java
@@ -0,0 +1,116 @@
+/*
+ * pojos.FileData 
+ *
+ *------------------------------------------------------------------------------
+ *  Copyright (C) 2006-2010 University of Dundee. All rights reserved.
+ *
+ *
+ * 	This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *------------------------------------------------------------------------------
+ */
+package pojos;
+
+//Java imports
+import java.io.File;
+
+//Third-party libraries
+
+//Application-internal dependencies
+import omero.model.OriginalFile;
+
+/** 
+ * DataObject used to handle the file visible via FS.
+ *
+ * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * <a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
+ * <a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
+ * @version 3.0
+ * <small>
+ * (<b>Internal version:</b> $Revision: $Date: $)
+ * </small>
+ * @since 3.0-Beta4
+ */
+public class FileData 
+	extends DataObject
+{
+
+	/** The file representing the original file. */
+	private File file;
+	
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param oFile The file to store.
+	 */
+	public FileData(OriginalFile oFile)
+	{
+		setValue(oFile);
+		String path = null;
+		if (oFile != null) path = oFile.getName().getValue();
+		file = new File(path);
+	}
+	
+	/**
+	 * Returns the name of the file.
+	 * 
+	 * @return See above.
+	 */
+	public String getName() { return file.getName(); }
+	
+	/**
+	 * Returns <code>true</code> if the file is hidden, <code>false</code>
+	 * otherwise.
+	 * 
+	 * @return See above.
+	 */
+	public boolean isHidden() { return file.isHidden(); }
+	
+	/**
+	 * Returns when the file was last modified.
+	 * 
+	 * @return See above.
+	 */
+	public long lastModified()
+	{
+		OriginalFile of = (OriginalFile) asIObject();
+		if (of == null) return -1;
+		return of.getCtime().getValue();
+	}
+	
+	/**
+	 * Returns <code>true</code> if the file is a directory, <code>false</code>
+	 * otherwise.
+	 * 
+	 * @return See above.
+	 */
+	public boolean isDirectory() { return file.isDirectory(); }
+
+	/**
+	 * Returns <code>true</code> if the file is a file, <code>false</code>
+	 * otherwise.
+	 * 
+	 * @return See above.
+	 */
+	public boolean isFile() { return file.isFile(); }
+	
+	/**
+     * Returns the absolute pathname string.
+     * 
+     * @return  See above.
+     */
+	public String getAbsolutePath() { return file.getAbsolutePath(); }
+	
+}
