2a372ed731a095988574202e5bbf0a9f571f56c6
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-03-03 15:46:20 +0000

    OmeroCli : Whitespace handling on Windows
    git-svn-id: file:///home/svn/omero/trunk@6176 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/bin/omero.bat
+++ components/tools/OmeroPy/bin/omero.bat
@@ -3,5 +3,5 @@ REM  Windows bat for OMERO
 REM  Copyright (c) 2009, Glencoe Software, Inc.
 REM  See LICENSE for details.
 
-call %~dp0\setpythonpath.bat
-python %~dp0\omero %*
+call "%~dp0\setpythonpath.bat"
+python "%~dp0\omero" %*
