8603b3bd1c9f072c384bcfe0f4e7786f4d8d1e5d
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-03-03 15:46:23 +0000

    ticket:1672 - Improved `script` plugin plus tests
    git-svn-id: file:///home/svn/omero/trunk@6186 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/src/omero/plugins/script.py
+++ components/tools/OmeroPy/src/omero/plugins/script.py
@@ -18,8 +18,18 @@
 """
 
 import subprocess, os, sys
-from omero.cli import BaseControl
+from omero.cli import BaseControl, Arguments
+from omero.util.temp_files import create_path
 from omero_ext.strings import shlex
+from omero_ext.functional import wraps
+
+
+def wrapper(func):
+    def wrap(*args, **kwargs):
+        print "Wrapped", func
+        return func(*args, **kwargs)
+    wrap = wraps(func)(wrap)
+    return wrap
 
 class ScriptControl(BaseControl):
 
@@ -28,43 +38,167 @@ class ScriptControl(BaseControl):
 Syntax: %(program_name)s script file [configuration parameters]
         Executes a file as a script. Can be used to test scripts
         for later deployment on the grid.
+
+        set
+        set script=[id|name]
+        set user=[id|name]
+        set group=[id|name]
+        set user=
+
+        Note: Some of the other actions call set internally.
+
+
+        cat
+        cat script=[id|name]
+
+        launch
+        launch script=[id|name]
+
+        list
+        list user
+        list group
+        list user=[id|name]
+        list group=[id|name]
+
+        log
+        log user
+        log group
+        log script
+        log user=[id|name]
+        log group=[id|name]
+        log script=[id|name]
+
+        params
+        params script=[id|name]
+
+        serve
+        serve user
+        serve group
+        serve user=[id|name]
+        serve group=[id|name]
+        serve group=[id|name] user=[id|name]
+        serve background=[true|false]
+        server timeout={min}
+
+        upload file=/tmp/my_script.py
+
+        #
+        # Other
+        #
+
+        run
         """)
 
-    def __call__(self, *args):
-        args = Arguments(*args)
+    def TODO(self, *args):
 
         if hasattr(self, "secure"):
             self.ctx.err("Secure cli cannot execture python scripts")
 
-        if len(args) < 1:
-            self.ctx.err("No file given")
-        elif len(args) == 1:
-            if not os.path.exists(args[0]):
-                self.ctx.error("No such file: %s" % args[0])
-            else:
-                _file_ = args[0]
-                _cmnd_ = "run"
-        else:
-            try:
-                _cmnd_, _file_ = args
-                print _cmnd_, _file_
-            except ValueError:
-                self.ctx.error("usage: script [run|serve] file")
+    @wrapper
+    def cat(self, *args):
+        pass
 
-        if _cmnd_ == "run":
+    @wrapper
+    def launch(self, *args):
+        pass
+
+    #@wrapper
+    def list(self, *args):
+        args = Arguments(args)
+        args.acquire(self.ctx)
+
+        user = args.get("user", None)
+        if user:
+            sf = self.ctx._client.sf
+            id = sf.getAdminService().getEventContext().userId
+            q = sf.getQueryService()
+            objs = q.findAllByQuery("select s from OriginalFile s where s.format.value = 'text/x-python' and s.details.owner.id = %s" % id, None)
+            scripts = {}
+            for obj in objs:
+                scripts[obj.id.val] = obj.name.val
+            self._parse(scripts, "Scripts for user=%s" % id)
+        else:
+            import omero_api_IScript_ice
+            svc = self.ctx._client.sf.getScriptService()
+            scripts = svc.getScripts()
+            self._parse(scripts, "Primary scripts")
+
+    @wrapper
+    def log(self, *args):
+        pass
+
+    @wrapper
+    def params(self, *args):
+        pass
+
+    def serve(self, *args):
+        args = Arguments(args)
+        client = self.ctx.conn(args)
+        background = args.getBool("background", False)
+        timeout = args.getInt("timeout", 0)
+
+        # Similar to omero.util.Server starting here
+        import omero
+        from omero.util import ServerContext
+        from omero.processor import ProcessorI
+
+        try:
+            stop_event = omero.util.concurrency.get_event()
+            serverid = "omero.scripts.serve"
+            ctx = ServerContext(serverid, client.ic, stop_event)
+            impl = ProcessorI(ctx)
+            prx = client.adapter.addWithUUID(impl)
+            ADD TO SESSION HERE. UPDATE REGISTRY METHODS TO CHECK
+        except:
+            self.ctx.die(100, "Failed initialization")
+
+        if background:
+            REGISTER_CLEANUP(timeout)
+        else:
+            Handle interrupt
+            stop_event.wait(timeout)
+            stop_event.set()
+            impl.cleanup()
+
+    @wrapper
+    def upload(self, *args):
+        args = Arguments(args)
+        args.acquire(self.ctx)
+        file, other = args.firstOther()
+        self.ctx.pub(["upload", file])
+        self.ctx.assertRC()
+
+    #
+    # Other
+    #
+    def run(self, *args):
+        args = Arguments(args)
+        first, other = args.firstOther()
+
+        if not first:
+            self.ctx.err("No file given")
+        elif len(other) == 0:
+            if not os.path.exists(first):
+                self.ctx.error("No such file: %s" % first)
+
             env = os.environ
             env["PYTHONPATH"] = self.ctx.pythonpath()
             p = subprocess.Popen(args,env=os.environ)
             p.wait()
             if p.poll() != 0:
                 self.ctx.die(p.poll(), "Execution failed.")
-        else:
-            # TODO : support more than one file. Whole directory? FS.
-            id = self._checkAndUpload(_file_)
-            self._serve(id)
 
-    def _checkAndUpload(self, _file_):
+    #
+    # Helpers
+    #
 
+    def _parse(self, scripts, msg):
+        ids = list(scripts.keys())
+        ids.sort()
+        self.ctx.out(msg)
+        self.ctx.out("="*100)
+        for id in ids:
+            self.ctx.out("%8.8s - %s" % (id, scripts[id]))
 
 try:
     register("script", ScriptControl)
---
+++ components/tools/OmeroPy/test/cli/script.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+"""
+   Test of the scripts plugin
+
+   Copyright 2010 Glencoe Software, Inc. All rights reserved.
+   Use is subject to license terms supplied in LICENSE.txt
+
+"""
+
+import unittest, os, subprocess, StringIO
+from path import path
+from omero.cli import Context, BaseControl, CLI
+from omero.plugins.script import ScriptControl
+from omero.plugins.sessions import SessionsControl
+from omero.plugins.upload import UploadControl
+from omero.util.temp_files import create_path
+from test.integration.library import ITest
+
+omeroDir = path(os.getcwd()) / "build"
+
+
+class TestScript(ITest):
+
+    def cli(self):
+        cli = CLI()
+        cli.register("upload", UploadControl)
+        cli.register("sessions", SessionsControl)
+        cli.register("s", ScriptControl)
+        return cli
+
+    def test1(self):
+        cli = self.cli()
+        cli.invoke("s list -s localhost -u root".split(), strict=True)
+        self.assertEquals(0, cli.rv)
+
+    def testFullSession(self):
+        cli = self.cli()
+        p = create_path(suffix=".py")
+        p.write_text("""
+import omero, omero.scripts as s
+from omero.rtypes import *
+
+client = s.client("testFullSession", "simple ping script", s.Long("a").inout(), s.String("b").inout())
+client.setOutput("a", rlong(0))
+client.setOutput("b", rstring("c"))
+client.closeSession()
+""")
+        args = ["s", "-q"] + self.login_args()
+        #import pdb
+        #pdb.set_trace()
+        cli.invoke(args + ["upload", str(p)], strict=True) # Sets current script
+        cli.invoke(args + ["list", "user"], strict=True)
+        cli.invoke(args + ["serve", "user", "requests=1", "timeout=1", "background=true"], strict=True)
+        cli.invoke(args + ["launch"], strict=True) # Uses current script
+
+if __name__ == '__main__':
+    unittest.main()
