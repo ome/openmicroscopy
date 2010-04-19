0b9a681ffbd0aae5cd13ef0b1cf4c2d8ee782f50
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-04-19 15:04:38 +0000

    fix #1645 - Permitting use of a var/ belonging to root
    git-svn-id: file:///home/svn/omero/trunk@6681 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/tools/OmeroPy/test/t_bin.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+"""
+   Tests the sudo/var functionality of bin/omero
+
+   Copyright 2010 Glencoe Software, Inc. All rights reserved.
+   Use is subject to license terms supplied in LICENSE.txt
+
+"""
+
+from omero.util.temp_files import create_path
+import subprocess
+import unittest
+import signal
+import path
+import time
+import os
+
+orig = path.path(".") / "bin" / "omero"
+
+def has_sudo():
+    """
+    This tests if we have password-less sudo
+    """
+    proc = subprocess.Popen(["sudo","sh","-c","true"])
+    time.sleep(0.5)
+    rc = proc.poll()
+
+    # Looks like we have sudo, EARLY EXIT
+    if rc is not None and rc == 0:
+        return True
+
+    try:
+        os.kill(rc.pid, signal.SIGKILL)
+    except:
+        print "Error on kill process"
+
+    return False
+
+
+if not has_sudo():
+    print "NO SUDO"
+
+else:
+
+    class TestBin(unittest.TestCase):
+
+        def preMethod(self):
+            self.dir = create_path("bin_test", folder=True)
+            self.bin = self.dir / "bin"
+            self.bin.makedirs()
+            self.omero = self.bin / "omero"
+            self.omero.write_text( orig.text() )
+            self.exe = """sh -c "python '%s' '' " """ % self.omero.abspath()
+            self.mkdir = """sudo sh -c "mkdir '%s/var'" """ % self.dir.abspath()     # Always sudo
+            self.chmod = """sudo sh -c "chmod %%s '%s/var'" """ % self.dir.abspath() # "
+
+        def testSudoFails(self):
+            self.preMethod()
+            self.assertFalse(0 == os.system("sudo %s" % self.exe))
+
+        def testVarIsCreated(self):
+            self.preMethod()
+            v = self.dir / "var"
+            self.assertFalse(v.exists())
+            os.system(self.exe)
+            self.assertTrue(v.exists())
+
+        def testVarIsNotReadable(self):
+            self.preMethod()
+            os.system(self.mkdir)
+            os.system(self.chmod % "700")
+            self.assertFalse(0 == os.system(self.exe))
+
+        def testVarIsNotWriteable(self):
+            self.preMethod()
+            os.system(self.mkdir)
+            os.system(self.chmod % "740")
+            self.assertFalse(0 == os.system(self.exe))
+
+if __name__ == '__main__':
+    unittest.main()
