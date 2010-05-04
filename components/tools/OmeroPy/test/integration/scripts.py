a7147143757cbef84816e1f3539b67abb403f975
wmoore <wmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-05-04 09:10:10 +0000

    Test for editScript(file, text). See #2313
    git-svn-id: file:///home/svn/omero/trunk@6828 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/test/integration/scripts.py
+++ components/tools/OmeroPy/test/integration/scripts.py
@@ -69,6 +69,7 @@ class TestScripts(lib.ITest):
         # Trying to run script as described: 
         #http://trac.openmicroscopy.org.uk/omero/browser/trunk/components/blitz/resources/omero/api/IScript.ice#L40
         scriptService = self.root.sf.getScriptService()
+        uuid = self.root.sf.getAdminService().getEventContext().sessionUuid
         client = self.root
         
         scriptLines = [
@@ -101,8 +102,8 @@ class TestScripts(lib.ITest):
         self.assertFalse("returnMessage" in results, "Script should not have run. No user processor!")
         
         
-        # should be OK for root to upload as official script and run
-        officialScriptId = scriptService.uploadOfficialScript("offical/test/script.py", script)
+        # should be OK for root to upload as official script (unique path) and run
+        officialScriptId = scriptService.uploadOfficialScript("offical/test/script%s.py" % uuid, script)
         proc = scriptService.runScript(officialScriptId, map, None)
         try:
             cb = omero.scripts.ProcessCallbackI(client, proc)
@@ -115,6 +116,30 @@ class TestScripts(lib.ITest):
             
         self.assertTrue("returnMessage" in results, "Script should have run as Official script")
         
+        
+    def testEditScript(self):
+        scriptService = self.root.sf.getScriptService()
+        uuid = self.root.sf.getAdminService().getEventContext().sessionUuid
 
+        scriptLines = [
+        "import omero",
+        "from omero.rtypes import rstring",
+        "import omero.scripts as scripts",
+        "if __name__ == '__main__':",
+        "    client = scripts.client('HelloWorld.py', 'Hello World example script',",
+        "    scripts.String('message', optional=True))",
+        "    client.setOutput('returnMessage', rstring('Script ran OK!'))"]
+        script = "\n".join(scriptLines)
+        map = {"message": omero.rtypes.rstring("Sending this message to the server!"), }
+        
+        scriptPath = "/test/edit/script%s.py" % uuid
+        scriptId = scriptService.uploadOfficialScript(scriptPath, script)
+        
+        scripts = scriptService.getScripts()
+        namedScripts = [s for s in scripts if s.path.val + s.name.val == scriptPath]
+        scriptFile = namedScripts[0]
+        
+        scriptService.editScript(scriptFile, script)
+        
 if __name__ == '__main__':
     unittest.main()
