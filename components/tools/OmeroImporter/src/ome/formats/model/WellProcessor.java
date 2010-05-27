c8dffac130ad7cdd4a30b3c2b51eebc68a0fb3bc
cxallan <cxallan@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-05-27 08:05:36 +0000

    Adding row and column population to the well processor. (Fixes #2429)
    git-svn-id: file:///home/svn/omero/trunk@7111 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroImporter/src/ome/formats/model/WellProcessor.java
+++ components/tools/OmeroImporter/src/ome/formats/model/WellProcessor.java
@@ -23,12 +23,11 @@
 
 package ome.formats.model;
 
+import static omero.rtypes.rint;
 import static omero.rtypes.rstring;
 
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Set;
 
 import ome.formats.Index;
 import omero.metadatastore.IObjectContainer;
@@ -40,7 +39,7 @@ import org.apache.commons.logging.LogFactory;
 
 /**
  * Processes the Wells of an IObjectContainerStore and ensures that the Plate 
- * has been populated. It also ensures that the Plate has a name.
+ * has been populated and that it is validated.
  * 
  * @author Chris Allan <callan at blackcat dot ca>
  *
@@ -50,6 +49,9 @@ public class WellProcessor implements ModelProcessor
     /** Logger for this class */
     private Log log = LogFactory.getLog(WellProcessor.class);
 
+    /** Object container store to process. */
+    private IObjectContainerStore store;
+
     /**
      * Processes the OMERO client side metadata store.
      * @param store OMERO metadata store to process.
@@ -58,27 +60,55 @@ public class WellProcessor implements ModelProcessor
     public void process(IObjectContainerStore store)
     throws ModelException
     {
+        this.store = store;
         List<IObjectContainer> containers = 
             store.getIObjectContainers(Well.class);
-        Set<Integer> plateIndexes = new HashSet<Integer>();
         for (IObjectContainer container : containers)
         {
-        	Integer plateIndex = container.indexes.get(Index.PLATE_INDEX.getValue());
-        	plateIndexes.add(plateIndex);
+            Integer plateIndex = 
+                container.indexes.get(Index.PLATE_INDEX.getValue());
+            Plate plate = validatePlate(plateIndex);
+            Well well = (Well) container.sourceObject;
+            if (well.getColumn() != null
+                && well.getColumn().getValue() >= plate.getCols().getValue())
+            {
+                plate.setCols(rint(well.getColumn().getValue() + 1));
+            }
+            if (well.getRow() != null
+                && well.getRow().getValue() >= plate.getRows().getValue())
+            {
+                plate.setRows(rint(well.getRow().getValue() + 1));
+            }
         }
-        for (Integer plateIndex : plateIndexes)
+    }
+
+    /**
+     * Validates that a Plate object container exists and that the Plate source
+     * object has a name and that its row and column count are initialized.
+     * @param plateIndex Index of the plate within the data model.
+     * @return The existing or created plate.
+     */
+    private Plate validatePlate(int plateIndex)
+    {
+        LinkedHashMap<Index, Integer> indexes = 
+            new LinkedHashMap<Index, Integer>();
+        indexes.put(Index.PLATE_INDEX, plateIndex);
+        IObjectContainer container = 
+            store.getIObjectContainer(Plate.class, indexes);
+        Plate plate = (Plate) container.sourceObject;
+        if (plate.getName() == null)
         {
-        	LinkedHashMap<Index, Integer> indexes = 
-        		new LinkedHashMap<Index, Integer>();
-        	indexes.put(Index.PLATE_INDEX, plateIndex);
-        	IObjectContainer container = 
-        		store.getIObjectContainer(Plate.class, indexes);
-        	Plate plate = (Plate) container.sourceObject;
-        	if (plate.getName() == null)
-        	{
-        		log.warn("Missing plate name for: " + container.LSID);
-        		plate.setName(rstring("Plate"));
-        	}
+            log.warn("Missing plate name for: " + container.LSID);
+            plate.setName(rstring("Plate"));
         }
+        if (plate.getRows() == null)
+        {
+            plate.setRows(rint(1));
+        }
+        if (plate.getCols() == null)
+        {
+            plate.setCols(rint(1));
+        }
+        return plate;
     }
 }
