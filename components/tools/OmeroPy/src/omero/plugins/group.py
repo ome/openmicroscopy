f0c6b02d93a7326f0173b39ff3f23989750a5d8b
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-06-08 14:21:28 +0000

    OmeroCli: Improvements to user.py and group.py including 'list' commands. See #1944
    git-svn-id: file:///home/svn/omero/trunk@7209 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/src/omero/plugins/group.py
+++ components/tools/OmeroPy/src/omero/plugins/group.py
@@ -20,10 +20,13 @@ class GroupControl(BaseControl):
         sub = parser.sub()
         add = parser.add(sub, self.add, "Add a new group with given permissions")
         add.add_argument("name", help="ExperimenterGroup.name value")
+
         group = add.add_mutually_exclusive_group()
         group.add_argument("--perms", default="rw----", help="Group permissions set as string, e.g. 'rw----' ")
         group.add_argument("--type", default="private", help="Group permission set symbollically",
             choices=("private", "read-only", "collaborative"))
+
+        list = parser.add(sub, self.list, "List current groups")
 
     def add(self, args):
 
@@ -49,6 +52,25 @@ class GroupControl(BaseControl):
 	admin = c.getSession().getAdminService()
 	id = admin.createGroup(g)
 	self.ctx.out("Added group %s with permissions %s" % (id, perms))
+
+    def list(self, args):
+        c = self.ctx.conn(args)
+        groups = c.sf.getAdminService().lookupGroups()
+        perms_to_groups = {"other":[]}
+        for perms in ("rw----", "rwr---", "rwrw--"):
+            perms_to_groups[perms] = []
+        for g in groups:
+            if g.name.val in ("user"):
+                continue
+            try:
+                perms_to_groups[str(g.details.permissions)].append(g.name.val)
+            except KeyError:
+                perms_to_groups["other"].append("%s (%s)" % (g.name.val, str(g.details.permissions)))
+        for x in ("rw----", "rwr---", "rwrw--", "other"):
+            if perms_to_groups[x]:
+                self.ctx.out("# %s" % x)
+                for g in perms_to_groups[x]:
+                    self.ctx.out(g)
 
 try:
     register("group", GroupControl, HELP)
--- components/tools/OmeroPy/src/omero/plugins/user.py
+++ components/tools/OmeroPy/src/omero/plugins/user.py
@@ -19,9 +19,6 @@ class UserControl(BaseControl):
     def _configure(self, parser):
         sub = parser.sub()
 
-        password = parser.add(sub, self.password, help = "Set user's password")
-        password.add_argument("user", nargs="?", help = "Username if not the current user")
-
         add = parser.add(sub, self.add, help = "Add users")
         add.add_argument("-m", "--middlename", help = "Middle name, if available")
         add.add_argument("-e", "--email")
@@ -34,17 +31,29 @@ class UserControl(BaseControl):
         add.add_argument("lastname", help = "User's surname name")
         add.add_argument("group", nargs="+", help = "Groups which the user is a member of")
 
+        list = parser.add(sub, self.list, help = "List current users")
+
+        password = parser.add(sub, self.password, help = "Set user's password")
+        password.add_argument("username", nargs="?", help = "Username if not the current user")
+
     def password(self, args):
         from omero.rtypes import rstring
         client = self.ctx.conn(args)
         admin = client.sf.getAdminService()
-        pw = self._ask_for_password()
+        pw = self._ask_for_password(" to be set")
         pw = rstring(pw)
-        if args.user:
-            admin.changeUserPassword(args.user, pw)
+        if args.username:
+            admin.changeUserPassword(args.username, pw)
         else:
             admin.changePassword(pw)
         self.ctx.out("Password changed")
+
+    def list(self, args):
+        c = self.ctx.conn(args)
+        users = c.sf.getAdminService().lookupExperimenters()
+        users = sorted([x.omeName.val for x in users])
+        for u in users:
+            self.ctx.out(u)
 
     def add(self, args):
         email = args.email
@@ -52,7 +61,7 @@ class UserControl(BaseControl):
         first = args.firstname
         middle = args.middlename
         last = args.lastname
-        inst = args.institute
+        inst = args.institution
         pasw = args.userpassword
 
         from omero.rtypes import rstring
@@ -75,11 +84,12 @@ class UserControl(BaseControl):
         if args.admin:
             groups.append(Grp(roles.systemGroupId, False))
 
+        group = groups.pop(0)
+
         if pasw is None:
-            id = admin.createExperimenter(e, groups)
+            id = admin.createExperimenter(e, group, groups)
             self.ctx.out("Added user %s" % id)
         else:
-            group = groups.pop(0)
             id = admin.createExperimenterWithPassword(e, rstring(pasw), group, groups)
             self.ctx.out("Added user %s with password" % id)
 
