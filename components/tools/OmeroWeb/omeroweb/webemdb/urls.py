1cff53bc9825d28934e74f9809964a641611bda7
wmoore <wmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-07-19 11:00:08 +0000

    Latest changes to webemdb.
    git-svn-id: file:///home/svn/omero/trunk@7584 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroWeb/omeroweb/webemdb/urls.py
+++ components/tools/OmeroWeb/omeroweb/webemdb/urls.py
@@ -2,14 +2,17 @@ from django.conf.urls.defaults import *
 from django.views.static import serve
 
 from omeroweb.webemdb import views
+import os
 
 urlpatterns = patterns('',
     url( r'^$', views.index, name='webemdb_index' ),
-    url( r'^login/$', views.login, name='webemdb_login' ),
     url( r'^logout/$', views.logout, name='webemdb_logout' ),
     
     url( r'^entry/(?P<entryId>[0-9]+)/$', views.entry, name='webemdb_entry' ),
-    url( r'^entry/(?P<entryId>[0-9]+)/gif/(?P<fileId>[0-9]+)/$', views.gif, name='webemdb_gif' ),
-    url( r'^entry/(?P<entryId>[0-9]+)/xml/(?P<fileId>[0-9]+)/$', views.xml, name='webemdb_xml' ),
+    url( r'^entry/(?P<entryId>[0-9]+)/file/(?P<fileId>[0-9]+)/$', views.file, name='webemdb_file' ),
     url( r'^img/(?P<imageId>[0-9]+)/map/(?P<fileId>[0-9]+)/$', views.map, name='webemdb_map' ),
+    
+    # page for OpenAstex Viewer. Project name and bit mask fileId
+    url( r'^oa_viewer/(?P<entryId>[0-9]+)/file/(?P<fileId>[0-9]+)/$', views.oa_viewer, name='webemdb_oa_viewer' ),
+    
 )
--- components/tools/OmeroWeb/omeroweb/webemdb/views.py
+++ components/tools/OmeroWeb/omeroweb/webemdb/views.py
@@ -11,11 +11,10 @@ import omero
 import omero.constants
 
 logger = logging.getLogger('webemdb')
+    
 
 def entry (request, entryId):
-    conn = getBlitzConnection (request)
-    if conn is None or not conn.isConnected():
-        return HttpResponseRedirect(reverse('webemdb_login'))
+    conn = getConnection(request)
         
     entryName = str(entryId)
     project = conn.findProject(entryName)
@@ -38,43 +37,50 @@ def entry (request, entryId):
                         if imgName == a.getFileName() and a.getNs() == namespace:
                             mrcMap = a
                     break
-    #print dir(project)
+    
     xml = None
     gif = None
+    bit = None
     xmlName = "emd-%s.xml" % entryName
     gifName = "%s.gif" % entryName
+    bitName = "%s.bit" % entryName
+    
     for a in project.listAnnotations():
+        if a.getFileName == None: continue
         if xmlName == a.getFileName():
             xml = a
         elif gifName == a.getFileName():
             gif = a
+        elif bitName == a.getFileName():
+            bit = a
     
-    #if xml:
-        #print dir(xml)
-        #print xml.getFile()
     return render_to_response('webemdb/entries/entry.html', 
-        {'project':project, 'xml': xml, 'gif': gif, 'img': img, 'map': mrcMap})
+        {'project':project, 'xml': xml, 'gif': gif, 'img': img, 'map': mrcMap, 'bit': bit})
         
         
+def oa_viewer(request, entryId, fileId): 
+    conn = getConnection(request)
+    
+    return render_to_response('webemdb/entries/oa_viewer.html', {'entryId': entryId, 'fileId': fileId})
+    
 
 def map (request, imageId, fileId):
     """
     Gets the file by Id and returns it as a mrc map.
-    N.B. We get the file from the Project it is attached to, because the 
+    N.B. We get the file from the Image it is attached to, because the 
     project.listAnnotations() method returns the original file in a wrapper which provides
     the file data with getFile(). 
     see http://djangosnippets.org/snippets/365/  for zip, temp file, etc
     """
-    conn = getBlitzConnection (request)
-    if conn is None or not conn.isConnected():
-        return HttpResponseRedirect(reverse('webemdb_login'))
+    conn = getConnection(request)
         
+    """
     annotation = BaseAnnotation(conn)
     annotation.getFileAnnotation(fileId)    
     rsp = HttpResponse(annotation.originalFile_data)
     rsp['ContentType'] = 'application/octet-stream'
     rsp['Content-Disposition'] = 'attachment; filename=%s' % (a.getFileName())
-    return rsp
+    return rsp"""
     
     image = conn.getImage(long(imageId))
     imgName = image.getName()  # map named same as image
@@ -82,7 +88,6 @@ def map (request, imageId, fileId):
     mrcMap = None
     for a in image.listAnnotations():
         if imgName == a.getFileName() and a.getNs() == namespace:
-            print type(a)
             data = a.getFile()
             rsp = HttpResponse(annotation.originalFile_data)
             rsp['ContentType'] = 'application/octet-stream'
@@ -90,77 +95,42 @@ def map (request, imageId, fileId):
             return rsp
             
     return HttpResponse()
-    
-def gif (request, entryId, fileId):
+
+
+def file (request, entryId, fileId):
     """
-    Gets the file by Id and returns it as a gif image.
+    Gets the file by Id and returns it according to mime type for display.
     N.B. We get the file from the Project it is attached to, because the 
     project.listAnnotations() method returns the original file in a wrapper which provides
     the file data with getFile(). 
     """
-    conn = getBlitzConnection (request)
-    if conn is None or not conn.isConnected():
-        return HttpResponseRedirect(reverse('webemdb_login'))
+    
+    conn = getConnection(request)
         
     entryName = str(entryId)
     project = conn.findProject(entryName)
     
     if project == None: return HttpResponse()
         
-    gif = None
+    oFile = None
     for a in project.listAnnotations():
         if a.id == long(fileId):
-            gif = a
-            print type(a)
+            oFile = a
     
-    if gif:
-        gif_data = gif.getFile()
-        return HttpResponse(gif_data, mimetype='image/gif')
-    return HttpResponse()
-
-
-def xml (request, entryId, fileId):
-    """
-    Gets the file by Id and returns it as an xml for display.
-    N.B. We get the file from the Project it is attached to, because the 
-    project.listAnnotations() method returns the original file in a wrapper which provides
-    the file data with getFile(). 
-    """
-    
-    conn = getBlitzConnection (request)
-    if conn is None or not conn.isConnected():
-        return HttpResponseRedirect(reverse('webemdb_login'))
-        
-    entryName = str(entryId)
-    project = conn.findProject(entryName)
-    
-    if project == None: return HttpResponse()
-        
-    xml = None
-    for a in project.listAnnotations():
-        if a.id == long(fileId):
-            xml = a
-    
-    if xml:
-        xml_data = xml.getFile()
-        return HttpResponse(xml_data, mimetype='text/xml')
+    if oFile:
+        file_data = oFile.getFile()
+        fileName = oFile.getFileName()
+        mimetype = "text/plain"
+        if fileName.endswith(".bit"): mimetype='application/oav'
+        if fileName.endswith(".xml"): mimetype='text/xml'
+        if fileName.endswith(".gif"): mimetype='image/gif'
+        return HttpResponse(file_data, mimetype=mimetype)
     return HttpResponse()
     
-    
-def login (request):
-    if request.method == 'POST' and request.REQUEST['server']:
-        blitz = settings.SERVER_LIST.get(pk=request.REQUEST['server'])
-        request.session['server'] = blitz.id
-        request.session['host'] = blitz.host
-        request.session['port'] = blitz.port
-    
-    conn = getBlitzConnection (request)
-    logger.debug(conn)
-    if conn is not None:
-        return HttpResponseRedirect(reverse('webemdb_index'))
-    return render_to_response('webemdb/login.html', {'gw':settings.SERVER_LIST})
 
 def logout (request):
+    """ Shouldn't ever be used (public db)"""
+    
     _session_logout(request, request.session['server'])
     try:
         del request.session['username']
@@ -170,12 +140,11 @@ def logout (request):
         del request.session['password']
     except KeyError:
         logger.error(traceback.format_exc())
-    return HttpResponseRedirect(reverse('webemdb_login'))
+    return HttpResponseRedirect(reverse('webemdb_index'))
+
 
 def index (request):
-    conn = getBlitzConnection (request)
-    if conn is None or not conn.isConnected():
-        return HttpResponseRedirect(reverse('webemdb_login'))
+    conn = getConnection(request)
 
     entryIds = [] # names of projects
     for p in conn.listProjects():
@@ -186,7 +155,6 @@ def index (request):
     entryIds.reverse()
     # truncate. 
     lastIds = entryIds[:10]
-    print lastIds
     
     projects = []
     for entryName in lastIds:
@@ -194,115 +162,20 @@ def index (request):
     
     return render_to_response('webemdb/index.html', {'projects': projects})
 
-def metadata (request, iid):
-    
-    from omeroweb.webclient.forms import MetadataFilterForm, MetadataDetectorForm, MetadataChannelForm, \
-                        MetadataEnvironmentForm, MetadataObjectiveForm, MetadataStageLabelForm, \
-                        MetadataLightSourceForm, MetadataDichroicForm, MetadataMicroscopeForm
-                        
+
+def getConnection(request):
     conn = getBlitzConnection (request)
-    if conn is None or not conn.isConnected():
-        return HttpResponseRedirect(reverse('webemdb_login'))
-    
-    form_environment = None
-    form_objective = None
-    form_microscope = None
-    form_stageLabel = None
-    form_filters = list()
-    form_detectors = list()
-    form_channels = list()
-    form_lasers = list()
-    
-    image = conn.getImage(iid)
-    original_metadata = image.loadOriginalMetadata()
-    for ch in image.getChannels():
-        if ch.getLogicalChannel() is not None:
-            channel = dict()
-            channel['form'] = MetadataChannelForm(initial={'logicalChannel': ch.getLogicalChannel(), 
-                                    'illuminations': list(conn.getEnumerationEntries("IlluminationI")), 
-                                    'contrastMethods': list(conn.getEnumerationEntries("ContrastMethodI")), 
-                                    'modes': list(conn.getEnumerationEntries("AcquisitionModeI"))})
-            if ch.getLogicalChannel().getSecondaryEmissionFilter()._obj is not None:
-                channel['form_emission_filter'] = MetadataFilterForm(initial={'filter': ch.getLogicalChannel().getSecondaryEmissionFilter(),
-                                    'types':list(conn.getEnumerationEntries("FilterTypeI"))})
-            if ch.getLogicalChannel().getDetectorSettings()._obj is not None:
-                channel['form_detector_settings'] = MetadataDetectorForm(initial={'detectorSettings':ch.getLogicalChannel().getDetectorSettings(), 'detector': ch.getLogicalChannel().getDetectorSettings().getDetector(),
-                                    'types':list(conn.getEnumerationEntries("DetectorTypeI"))})
-            if ch.getLogicalChannel().getLightSourceSettings()._obj is not None:      
-                channel['form_light_source'] = MetadataLightSourceForm(initial={'lightSource': ch.getLogicalChannel().getLightSourceSettings(),
-                                    'types':list(conn.getEnumerationEntries("FilterTypeI")), 
-                                    'mediums': list(conn.getEnumerationEntries("LaserMediumI")),
-                                    'pulses': list(conn.getEnumerationEntries("PulseI"))})
-            if ch.getLogicalChannel().getFilterSet()._obj is not None and ch.getLogicalChannel().getFilterSet().getDichroic()._obj:
-                channel['form_dichroic'] = MetadataDichroicForm(initial={'logicalchannel': ch.getLogicalChannel().getFilterSet().getDichroic()})
-            channel['name'] = ch.getEmissionWave()
-            channel['color'] = ch.getColor().getHtml()
-            form_channels.append(channel)
-            
-    if image.getObjectiveSettings() is not None:
-        form_objective = MetadataObjectiveForm(initial={'objectiveSettings': image.getObjectiveSettings(), 
-                                'mediums': list(conn.getEnumerationEntries("MediumI")), 
-                                'immersions': list(conn.getEnumerationEntries("ImmersionI")), 
-                                'corrections': list(conn.getEnumerationEntries("CorrectionI")) })
-    if image.getImagingEnvironment() is not None:
-        form_environment = MetadataEnvironmentForm(initial={'image': image})
-    if image.getStageLabel() is not None:
-        form_stageLabel = MetadataStageLabelForm(initial={'image': image })
-
-    if image.getInstrument() is not None:
-        if image.getInstrument().getMicroscope() is not None:
-            form_microscope = MetadataMicroscopeForm(initial={'microscopeTypes':list(conn.getEnumerationEntries("MicroscopeTypeI")), 'microscope': image.getInstrument().getMicroscope()})
-
-        if image.getInstrument().getFilters() is not None:
-            filters = list(image.getInstrument().getFilters())    
-            for f in filters:
-                form_filter = MetadataFilterForm(initial={'filter': f, 'types':list(conn.getEnumerationEntries("FilterTypeI"))})
-                form_filters.append(form_filter)
-    
-        if image.getInstrument().getDetectors() is not None:
-            detectors = list(image.getInstrument().getDetectors())    
-            for d in detectors:
-                form_detector = MetadataDetectorForm(initial={'detectorSettings':None, 'detector': d, 'types':list(conn.getEnumerationEntries("DetectorTypeI"))})
-                form_detectors.append(form_detector)
-    
-        if image.getInstrument().getLightSources() is not None:
-            lasers = list(image.getInstrument().getLightSources())
-            for l in lasers:
-                form_laser = MetadataLightSourceForm(initial={'lightSource': l, 
-                                'types':list(conn.getEnumerationEntries("FilterTypeI")), 
-                                'mediums': list(conn.getEnumerationEntries("LaserMediumI")),
-                                'pulses': list(conn.getEnumerationEntries("PulseI"))})
-                form_lasers.append(form_laser)
-
-    # Annotations #
-    text_annotations = list()
-    long_annotations = {'rate': 0.00 , 'votes': 0}
-    url_annotations = list()
-    file_annotations = list()
-    tag_annotations = list()
-    
-    from omero.model import CommentAnnotationI, LongAnnotationI, TagAnnotationI, FileAnnotationI
-                            
-    for ann in image.listAnnotations():
-        if isinstance(ann._obj, CommentAnnotationI):
-            text_annotations.append(ann)
-        elif isinstance(ann._obj, LongAnnotationI):
-            long_annotations['votes'] += 1
-            long_annotations['rate'] += int(ann.longValue)
-        elif isinstance(ann._obj, FileAnnotationI):
-            file_annotations.append(ann)
-        elif isinstance(ann._obj, TagAnnotationI):
-            tag_annotations.append(ann)
-
-    txannSize = len(text_annotations)
-    urlannSize = len(url_annotations)
-    fileannSize = len(file_annotations)
-    tgannSize = len(tag_annotations)
-    if long_annotations['votes'] > 0:
-        long_annotations['rate'] /= long_annotations['votes']
-    
-    return render_to_response('webemdb/metadata.html', {'image': image, 'text_annotations': text_annotations, 'txannSize':txannSize, 'long_annotations': long_annotations, 'url_annotations': url_annotations, 'urlannSize':urlannSize, 'file_annotations': file_annotations, 'fileannSize':fileannSize, 'tag_annotations': tag_annotations, 'tgannSize':tgannSize, 'global_metadata':original_metadata[0], 'serial_metadata':original_metadata[1], 'form_channels':form_channels, 'form_environment':form_environment, 'form_objective':form_objective, 'form_microscope':form_microscope, 'form_filters':form_filters, 'form_detectors':form_detectors, 'form_lasers':form_lasers, 'form_stageLabel':form_stageLabel})
-    
+    if conn is None or not conn.isConnected() and request.REQUEST['server']:
+        blitz = settings.SERVER_LIST.get(pk=1)
+        request.session['server'] = blitz.id
+        request.session['host'] = blitz.host
+        request.session['port'] = blitz.port
+        request.session['password'] = "ome"
+        request.session['username'] = "emdb"
+        request.session['server'] = 'localhost'
+        conn = getBlitzConnection (request)
+        logger.debug(conn)
+    return conn
 
 def image_viewer (request, iid, **kwargs):
     """ This view is responsible for showing pixel data as images """
