969f56c91a5f309ae83da44d61fde039a23e0b11
wmoore <wmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-08-05 12:50:07 +0000

    Bug fix EMDB
    git-svn-id: file:///home/svn/omero/trunk@7674 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroWeb/omeroweb/webemdb/urls.py
+++ components/tools/OmeroWeb/omeroweb/webemdb/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # look up the preview gif file for an entry based on name
     url( r'^entry/(?P<entryId>[0-9]+)/gif/$', views.gif, name='webemdb_gif' ),
     # for downloading whole mrc.map   
-    url( r'^img/(?P<imageId>[0-9]+)/map/(?P<fileId>[0-9]+)/$', views.map, name='webemdb_map' ),
+    #url( r'^img/(?P<imageId>[0-9]+)/map/(?P<fileId>[0-9]+)/$', views.map, name='webemdb_map' ),
     
     # page for OpenAstex Viewer. Project name and bit mask fileId
     url( r'^oa_viewer/(?P<entryId>[0-9]+)/file/(?P<fileId>[0-9]+)/$', views.oa_viewer, name='webemdb_oa_viewer' ),
