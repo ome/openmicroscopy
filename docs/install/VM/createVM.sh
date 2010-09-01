044ec7a74a439c6651b9bdc1f76584839fdda3a2
dzmacdonald <dzmacdonald@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-09-01 13:52:59 +0000

    Fixed error where cloned VM was getting new /dev/eth* because udev assigned devices based on mac address, close #2773
    git-svn-id: file:///home/svn/omero/trunk@7889 05709c45-44f0-0310-885b-81a1db45b4a6

--- docs/install/VM/createVM.sh
+++ docs/install/VM/createVM.sh
@@ -4,7 +4,7 @@ if [ -n $VMNAME ]
 then 
     if [ -n $1 ]
     then
-        export VMNAME="OMEROTEST"
+        export VMNAME="SEPT1"
     else
         export VMNAME=$1
     fi
@@ -36,11 +36,11 @@ VBoxManage storagectl "$VMNAME" --name "IDE CONTROLLER" --add ide
 VBoxManage storagectl "$VMNAME" --name "SATA CONTROLLER" --add sata
 VBoxManage storageattach "$VMNAME" --storagectl "SATA CONTROLLER" --port 0 --device 0 --type hdd --medium $HARDDISKS$VMNAME.vdi
 # VBoxManage storageattach "$VMNAME" --storagectl "IDE CONTROLLER" --device 0 --port 1 --type dvddrive --medium ~/Desktop/linux\ distros/ubuntu\ 10.04/ubuntu-10.04-server-amd64.iso
-VBoxManage modifyvm "$VMNAME" --nic1 nat --nictype1 "Am79C973" 
+VBoxManage modifyvm "$VMNAME" --nic1 nat --nictype1 "Am79C973" --macaddress1 08002789F0C3
 VBoxManage modifyvm "$VMNAME" --memory $MEMORY --acpi on
 VBoxManage setextradata "$VMNAME" "VBoxInternal/Devices/pcnet/0/LUN#0/Config/ssh/HostPort" $SSH_PF
 VBoxManage setextradata "$VMNAME" "VBoxInternal/Devices/pcnet/0/LUN#0/Config/ssh/GuestPort" 22
 VBoxManage setextradata "$VMNAME" "VBoxInternal/Devices/pcnet/0/LUN#0/Config/ssh/Protocol" TCP
-VBoxManage setextradata "$VMNAME" "VBoxInternal/Devices/pcnet/0/LUN#0/Config/omeroserver/HostPort" $OMERO_PF
-VBoxManage setextradata "$VMNAME" "VBoxInternal/Devices/pcnet/0/LUN#0/Config/omeroserver/GuestPort" $OMERO_PORT
-VBoxManage setextradata "$VMNAME" "VBoxInternal/Devices/pcnet/0/LUN#0/Config/omeroserver/Protocol" TCP
+#VBoxManage setextradata "$VMNAME" "VBoxInternal/Devices/pcnet/0/LUN#0/Config/omeroserver/HostPort" $OMERO_PF
+#VBoxManage setextradata "$VMNAME" "VBoxInternal/Devices/pcnet/0/LUN#0/Config/omeroserver/GuestPort" $OMERO_PORT
+#VBoxManage setextradata "$VMNAME" "VBoxInternal/Devices/pcnet/0/LUN#0/Config/omeroserver/Protocol" TCP
--- docs/install/VM/installVM.sh
+++ docs/install/VM/installVM.sh
@@ -4,7 +4,7 @@ if [ -n $VMNAME ]
 then 
     if [ -n $1 ]
     then
-        export VMNAME="OMEROTEST"
+        export VMNAME="SEPT1"
     else
         export VMNAME=$1
     fi
@@ -27,12 +27,22 @@ then
         export SSH_PF=$3
     fi
 fi
+echo "Stopping VM " 
 VBoxManage controlvm "$VMNAME" poweroff
+sleep 10
+echo "Starting VM " 
 VBoxManage startvm "$VMNAME" --type headless
+sleep 30
+echo "Copying ubuntu-install" 
 scp -P $SSH_PF ubuntu-install.sh omero@localhost:~/
+echo "Copying omero.sh" 
 scp -P $SSH_PF omero.sh omero@localhost:~/omero
+echo "Copying installDaemon.sh" 
 scp -P $SSH_PF installDaemon.sh omero@localhost:~/
+echo "Copying ubuntu-install" 
 export DISPLAY=:0
 export SSH_ASKPASS=`pwd`/fakepass.sh
+echo "ssh : exec ubuntu-install.sh"
 ssh -l omero -p $SSH_PF localhost 'echo "$PASSWORD" | sudo -S sh /home/omero/ubuntu-install.sh'
+echo "ssh : exec installDaemon.sh"
 ssh -l omero -p $SSH_PF localhost 'echo "$PASSWORD" | sudo -S sh /home/omero/installDaemon.sh'
