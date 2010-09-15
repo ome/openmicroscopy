0180f613e3cbcf89b4972c02cc9163dfee7570e7
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2010-09-15 11:23:11 +0000

    Added options to plot the results of CSV files returned by FLIM script (closed #1594)
    git-svn-id: file:///home/svn/shoola-omero/trunk@7598 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/env/ui/AnalysisResultsDialog.java
+++ SRC/org/openmicroscopy/shoola/env/ui/AnalysisResultsDialog.java
@@ -29,8 +29,6 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.BufferedReader;
