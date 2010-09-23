070018cf2d1cd9d13694ed54f2c9569c5f2f5837
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-09-23 14:58:50 +0000

    OmeroTables examples for creating and finding measurement files.
    git-svn-id: file:///home/svn/omero/trunk@8116 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ examples/OmeroTables/FindMeasurements.java
@@ -0,0 +1,95 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import omero.ServerError;
+import omero.api.IAdminPrx;
+import omero.api.IMetadataPrx;
+import omero.api.ServiceFactoryPrx;
+import omero.constants.namespaces.NSMEASUREMENT;
+import omero.grid.Column;
+import omero.grid.Data;
+import omero.grid.LongColumn;
+import omero.grid.SharedResourcesPrx;
+import omero.grid.TablePrx;
+import omero.model.FileAnnotation;
+import omero.model.FileAnnotationI;
+import omero.model.IObject;
+import omero.model.ImageAnnotationLink;
+import omero.model.ImageAnnotationLinkI;
+import omero.model.ImageI;
+import omero.model.OriginalFile;
+
+/**
+ * Take an image id and find any attached measurements. If no image id is
+ * passed, then will use {@link MeasurementTable} to create one.
+ */
+public class FindMeasurements {
+
+    /**
+     * The main method takes an optional long from the command line which should
+     * be the id of a valid Image for the current user. If no id is provided, a
+     * test image is created.
+     */
+    public static void main(String[] args) throws Exception {
+
+        // Configuration for the client object in this case comes
+        // from the ICE_CONFIG environment variable.
+        omero.client client = new omero.client();
+        client.createSession();
+
+        try {
+
+            // Get or create a valid Image id.
+            long imageId;
+            if (args.length > 0) {
+                imageId = Long.valueOf(args[0]);
+            } else {
+                MeasurementTable table = new MeasurementTable(client, null);
+                imageId = table.imageId;
+            }
+
+            new FindMeasurements(client, imageId);
+
+        } finally {
+            client.closeSession();
+        }
+    }
+
+    /**
+     * Finds all measurement tables attached to the given image.
+     */
+    FindMeasurements(omero.client client, long imageId) throws ServerError {
+
+        final ServiceFactoryPrx factory = client.getSession();
+        final IAdminPrx admin = factory.getAdminService();
+        final SharedResourcesPrx resources = factory.sharedResources();
+        final IMetadataPrx metadata = factory.getMetadataService();
+
+        final long userId = admin.getEventContext().userId;
+        final Map<Long, List<IObject>> annotations = metadata.loadAnnotations(
+                "Image", Collections.singletonList(imageId),
+                null, null, null);
+                //Collections.singletonList("Annotation"),  // Ways to limit the annotations
+                //Collections.singletonList(userId), null); // by type or user.
+
+        if (!annotations.containsKey(imageId)) {
+            System.out.println("No annotations.");
+            return;
+        }
+
+        for (IObject obj : annotations.get(imageId)) {
+            FileAnnotation ann = (FileAnnotation) obj;
+            if (NSMEASUREMENT.value.equals(ann.getNs().getValue())) {
+                TablePrx table = resources.openTable(ann.getFile());
+                try {
+                    System.out.println(String.format("Found measurement file %s: rows=%s", table
+                            .getOriginalFile().getId().getValue(),
+                            table.getNumberOfRows()));
+                } finally {
+                    table.close();
+                }
+            }
+        }
+    }
+}
---
+++ examples/OmeroTables/MeasurementTable.java
@@ -0,0 +1,152 @@
+import omero.ServerError;
+import omero.api.ServiceFactoryPrx;
+import omero.grid.Column;
+import omero.grid.Data;
+import omero.grid.LongColumn;
+import omero.grid.SharedResourcesPrx;
+import omero.grid.TablePrx;
+import omero.model.FileAnnotation;
+import omero.model.FileAnnotationI;
+import omero.model.Image;
+import omero.model.ImageAnnotationLink;
+import omero.model.ImageAnnotationLinkI;
+import omero.model.ImageI;
+import omero.model.OriginalFile;
+
+/**
+ * Example of working with the {@link omero.grid.TablePrx OMERO.tables API}. A
+ * Table is created, attached to an {@link Image} and then queried.
+ */
+public class MeasurementTable {
+
+    /**
+     * The main method takes an optional long from the command line which should
+     * be the id of a valid Image for the current user. If no id is provided, a
+     * test image is created.
+     */
+    public static void main(String[] args) throws Exception {
+
+        // Configuration for the client object in this case comes
+        // from the ICE_CONFIG environment variable.
+        omero.client client = new omero.client();
+        client.createSession();
+
+        try {
+
+            // Get or create a valid Image id.
+            Long imageId = null;
+            if (args.length > 0) {
+                imageId = Long.valueOf(args[0]);
+            }
+
+            MeasurementTable table = new MeasurementTable(client, imageId);
+            table.query();
+
+        } finally {
+            client.closeSession();
+        }
+    }
+
+    public final Long imageId;
+    TablePrx table;
+    OriginalFile file;
+    ServiceFactoryPrx factory;
+    SharedResourcesPrx resources;
+
+    /**
+     * Creates a new Table service and saves some sample data in it.
+     */
+    MeasurementTable(omero.client client, Long imageId) throws ServerError {
+        factory = client.getSession();
+        resources = factory.sharedResources();
+
+        if (imageId != null) {
+            System.out.println("Using image " + imageId);
+        } else {
+            imageId = makeImage(client);
+            System.out.println("Created image " + imageId);
+        }
+        this.imageId = imageId;
+
+        table = resources.newTable(1, "/examples/test.h5");
+        try {
+
+            attachAnnotation(factory, imageId, table);
+
+            // Nulls here mean that column has no data
+            // We first pass a truncated column to prevent unnecessary network
+            // communication
+            LongColumn lc = new LongColumn("a1", "desc", null);
+            Column[] cols = new Column[] { lc };
+
+            table.initialize(cols);
+
+            // To send data, we need to initialize the array and set values.
+            lc.values = new long[10];
+            for (int i = 0; i < 10; i++) {
+                lc.values[i] = i;
+            }
+
+            // addData can be called as many times as necessary.
+            table.addData(cols);
+
+        } finally {
+            table.close();
+        }
+    }
+
+    /**
+     * Creates a simple Image with no attached binary data for the example.
+     */
+    Long makeImage(omero.client client) throws ServerError {
+        Image image = new ImageI();
+        image.setName(omero.rtypes.rstring("MeasurementTable Example"));
+        image.setAcquisitionDate(omero.rtypes.rtime(System.currentTimeMillis()));
+        image = (Image) factory.getUpdateService().saveAndReturnObject(image);
+        return image.getId().getValue();
+    }
+
+    /**
+     * Creates a {@link FileAnnotation} and attaches it to the given image id.
+     */
+    void attachAnnotation(ServiceFactoryPrx factory, long imageId,
+            TablePrx table) throws ServerError {
+
+        file = table.getOriginalFile();
+        FileAnnotation annotation = new FileAnnotationI();
+        annotation.setNs(omero.rtypes
+                .rstring(omero.constants.namespaces.NSMEASUREMENT.value));
+        annotation.setFile(file);
+        ImageAnnotationLink link = new ImageAnnotationLinkI();
+        link.link(new ImageI(imageId, false), annotation);
+        link = (ImageAnnotationLink) factory.getUpdateService()
+                .saveAndReturnObject(link);
+
+    }
+
+    /**
+     * A few example queries are made on the table.
+     */
+    void query() throws ServerError {
+
+        table = resources.openTable(file);
+
+        try {
+
+            System.out.println("Rows found:"
+                    + table.getWhereList("(a1==5)", null, 0, -1, 0).length);
+
+            Data data = table.readCoordinates(new long[] { 0, 1, 2, 3, 4, 5, 6,
+                    7, 8, 9 });
+            LongColumn lc = (LongColumn) data.columns[0];
+            for (int i = 0; i < lc.values.length; i++) {
+                System.out.println(i + ":" + lc.values[i]);
+            }
+
+        } finally {
+
+            table.close();
+
+        }
+    }
+}
