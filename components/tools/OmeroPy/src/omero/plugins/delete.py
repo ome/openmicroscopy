5a21c258d0166b1d8df09767aa8764e608d3ee72
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-10-06 14:21:13 +0000

    Added support for delete specs to `bin/omero delete` (Fix #2687)
    git-svn-id: file:///home/svn/omero/trunk@8240 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/src/omero/plugins/delete.py
+++ components/tools/OmeroPy/src/omero/plugins/delete.py
@@ -26,22 +26,47 @@ Examples:
     bin/omero delete Image:50
     bin/omero delete Plate:1
 
+    # New-style delete
+    bin/omero delete /Image:1 /Screen:5
+
 """
 
 class DeleteControl(BaseControl):
 
     def _configure(self, parser):
-        parser.add_argument("obj", nargs="+", help="""Objects to be deleted in the form "<Classs>:<Id>""")
+        parser.add_argument("--wait", type=long, help="""Number of seconds to wait for the delete to complete (Indefinite < 0; No wait=0).""", default=-1)
+        parser.add_argument("--list", action="store_true", help="""Print a list of all available delete specs""")
+        parser.add_argument("--list-details", action="store_true", help="""Print a list of all available delete specs along with detailed info""")
+        parser.add_argument("obj", nargs="*", help="""Objects to be deleted in the form "<Class>:<Id>""")
         parser.set_defaults(func=self.delete)
 
     def delete(self, args):
 
         import omero
+        import omero.callbacks
+
         client = self.ctx.conn(args)
+        delete = client.sf.getDeleteService()
+        specs = delete.availableCommands()
+        keys = sorted([spec.type for spec in specs])
+
+        if args.list_details:
+            map = dict()
+            for spec in specs:
+                map[spec.type] = spec
+            for key in keys:
+                spec = map[key]
+                self.ctx.out("=== %s ===" % key)
+                for k, v in spec.options.items():
+                    self.ctx.out("%s" % (k,))
+        elif args.list:
+            self.ctx.out("\n".join(keys))
+            return # Early exit.
 
         images = []
         plates = []
         objects = []
+        commands = []
         for arg in args.obj:
             if 0 > arg.find(":"):
                 self.ctx.die(5, "Format: 'Image:<id>'")
@@ -50,6 +75,8 @@ class DeleteControl(BaseControl):
                 images.append(long(id))
             elif klass == "Plate":
                 plates.append(long(id))
+            elif klass in keys:
+                commands.append(omero.api.delete.DeleteCommand(klass, long(id), None))
             else:
                 ctor = getattr(omero.model, "%sI" % klass)
                 if not ctor:
@@ -60,9 +87,12 @@ class DeleteControl(BaseControl):
                     self.ctx.dbg("Exception on ctor: %s" % e)
                     self.ctx.die(5, "Can't delete type: %s" % klass)
 
+        def status(klass, args):
+            self.ctx.out(("Deleting %s %s... " % (klass, args)), newline = False)
+
         def action(klass, method, *args):
             import omero
-            self.ctx.out(("Deleting %s %s... " % (klass, args)), newline = False)
+            status(klass, args)
             try:
                 method(*args)
                 self.ctx.out("ok.")
@@ -76,6 +106,50 @@ class DeleteControl(BaseControl):
         for image in images: action("Image", deleteSrv.deleteImage, image, True)
         for plate in plates: action("Plate", deleteSrv.deletePlate, plate)
         for object in objects: action(object.__class__.__name__, updateSrv.deleteObject, object)
+
+        handle = deleteSrv.queueDelete(commands)
+        if args.wait == 0:
+            self.ctx.out("Not waiting for delete")
+            return
+        elif args.wait > 0:
+            self.ctx.die(321, "Unsupported wait: %s" % args.wait)
+
+        callback = omero.callbacks.DeleteCallbackI(client, handle)
+        try:
+            try:
+                # Wait for finish
+                rv = None
+                while True:
+                    rv = callback.block(500)
+                    if rv is not None:
+                        break
+
+                # Print report
+                for i, report in enumerate(handle.report()):
+                    command = commands[i]
+                    status(command.type, command.id)
+                    if rv:
+                        msg = "error"
+                    else:
+                        msg = "ok"
+                    self.ctx.out(msg, newline=False)
+                    if report:
+                        self.ctx.out(" (%s)" % report)
+                    else:
+                        self.ctx.out("")
+
+                if rv:
+                    self.ctx.die(rv, "Failed")
+
+            # If user uses Ctrl-C, then cancel
+            except KeyboardInterrupt:
+                self.ctx.out("Attempting cancel...")
+                if handle.cancel():
+                    self.ctx.out("Cancelled")
+                else:
+                    self.ctx.out("Failed to cancel")
+        finally:
+            callback.close()
 
 try:
     register("delete", DeleteControl, HELP)
