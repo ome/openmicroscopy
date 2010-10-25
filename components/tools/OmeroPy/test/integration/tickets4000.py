a293845c22abc7c2d6951abd33fd7e4477753a16
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-10-25 19:28:02 +0000

    Modifying `setSecurityContext` test to pass (See #2339, #3189)
    Two services were not closed and therefore the exception
    is expected. See comments in test for more information.
    
    git-svn-id: file:///home/svn/omero/trunk@8402 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/test/integration/tickets4000.py
+++ components/tools/OmeroPy/test/integration/tickets4000.py
@@ -14,7 +14,7 @@ import Glacier2
 from omero.rtypes import *
 
 
-class TestTickets3000(lib.ITest):
+class TestTickets4000(lib.ITest):
 
     def test3138(self):
         """
@@ -50,82 +50,102 @@ class TestTickets3000(lib.ITest):
                 self.assertTrue(T > t, "%s < %s" % (T, t))
         finally:
             c.__del__()
-    
-    def chageActiveGroup(self):
+
+    def testChangeActiveGroup(self):
         admin = self.client.sf.getAdminService()
-        
+
         self.assertEquals(2, len(admin.getEventContext().memberOfGroups))
-                
+
         # AS ROOT: adding user to extra group
         admin_root = self.root.sf.getAdminService()
         exp = admin_root.getExperimenter(admin.getEventContext().userId)
         grp = self.new_group()
-        admin_root.addGroups(exp, [grp])    
-        
+        admin_root.addGroups(exp, [grp])
+
         self.assertEquals(3, len(admin.getEventContext().memberOfGroups))
-        
+
         proxies = dict()
-        # creating stateful services 
+        # creating stateful services
         proxies['search'] = self.client.sf.createSearchService()
         proxies['thumbnail'] = self.client.sf.createThumbnailStore()
-        proxies['admin'] = self.client.sf.getAdminService()                
-                
+        proxies['admin'] = self.client.sf.getAdminService()
+
         # changing group
-        for k in proxies.keys():            
+        for k in proxies.keys():
             try:
                 proxies[k].close()
             except AttributeError:
                 pass
-        
+
         self.client.sf.setSecurityContext(omero.model.ExperimenterGroupI(grp.id.val, False))
         admin.setDefaultGroup(admin.getExperimenter(admin.getEventContext().userId), omero.model.ExperimenterGroupI(grp.id.val, False))
-        self.assertEquals(grp.id.val, self.client.sf.getAdminService().groupId)
-    
-    def chageActiveGroupWhenConnectionLost(self):
+        self.assertEquals(grp.id.val, self.client.sf.getAdminService().getEventContext().groupId)
+
+    def testChageActiveGroupWhenConnectionLost(self):
         import os
         admin = self.client.sf.getAdminService()
         uuid = self.client.sf.getAdminService().getEventContext().sessionUuid
         self.assertEquals(2, len(admin.getEventContext().memberOfGroups))
-                
+
         # AS ROOT: adding user to extra group
         admin_root = self.root.sf.getAdminService()
         exp = admin_root.getExperimenter(admin.getEventContext().userId)
         grp = self.new_group()
-        admin_root.addGroups(exp, [grp])    
-        
+        admin_root.addGroups(exp, [grp])
+
         self.assertEquals(3, len(admin.getEventContext().memberOfGroups))
-        
+
         proxies = dict()
-        # creating stateful services 
-        proxies['search'] = self.client.sf.createSearchService()
-        proxies['thumbnail'] = self.client.sf.createThumbnailStore()
+        # creating stateful services
+        proxies['search'] = self.client.sf.createSearchService() #1A
+        proxies['thumbnail'] = self.client.sf.createThumbnailStore() #1B
         proxies['admin'] = self.client.sf.getAdminService()
-                
+        copy = dict(proxies)
+
         # loosing the connection
         # ...
-        
+
         # joining session
-        
+
         c = omero.client(pmap=['--Ice.Config='+(os.environ.get("ICE_CONFIG"))])
         host = c.ic.getProperties().getProperty('omero.host')
         port = int(c.ic.getProperties().getProperty('omero.port'))
         c = omero.client(host=host, port=port)
         sf = c.joinSession(uuid)
-        
-        # retriving stateful services 
-        proxies['search'] = sf.createSearchService()
-        proxies['thumbnail'] = sf.createThumbnailStore()
+
+        # retriving stateful services
+        proxies['search'] = sf.createSearchService() #2A
+        proxies['thumbnail'] = sf.createThumbnailStore() #2B
         proxies['admin'] = sf.getAdminService()
-        
+
         # changing group
-        for k in proxies.keys():            
-            try:
-                proxies[k].close()
-            except AttributeError:
-                pass
+        for k in proxies.keys():
+            prx = proxies[k]
+            if isinstance(prx, omero.api.StatefulServiceInterfacePrx):
+                prx.close()
+
+        try:
+            sf.setSecurityContext(omero.model.ExperimenterGroupI(grp.id.val, False))
+            self.fail("""
+            A security violation must be thrown here because the first instances
+            which are stored in proxies (#1A and #1B) are never closed since #2A
+            and #2B overwrite them. Using the copy instance, we can close them.
+            """)
+        except omero.SecurityViolation, sv:
+            pass
+
+
+        for k in copy.keys():
+            prx = copy[k]
+            if isinstance(prx, omero.api.StatefulServiceInterfacePrx):
+                prx.close()
+
         sf.setSecurityContext(omero.model.ExperimenterGroupI(grp.id.val, False))
+
+        ec = admin.getEventContext()
         sf.getAdminService().setDefaultGroup(sf.getAdminService().getExperimenter(ec.userId), omero.model.ExperimenterGroupI(grp.id.val, False))
-        self.assertEquals(grp.id.val, sf.getAdminService().groupId)
-        
+        self.assertEquals(grp.id.val, ec.groupId)
+
+
 if __name__ == '__main__':
     unittest.main()
