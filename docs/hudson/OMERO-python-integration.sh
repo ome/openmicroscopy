1fb33942d0d24e3d1c4d459ebd7754d7ef0a2f81
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-11-03 11:31:20 +0000

    Adding drop box testing to OMERO-python-integration (See #1736, #3200)
    git-svn-id: file:///home/svn/omero/trunk@8497 05709c45-44f0-0310-885b-81a1db45b4a6

--- docs/hudson/OMERO-python-integration.sh
+++ docs/hudson/OMERO-python-integration.sh
@@ -1,13 +1,34 @@
+set -e
+set -u
+set -x
+
 ulimit -n 8192
 ulimit -a
-cd trunk
 
+
+#
+# Try DropBox, Hudson will look for ERROR in the output log.
+#
 rm -f ice.config
 wget http://hudson.openmicroscopy.org.uk/job/OMERO-trunk/lastSuccessfulBuild/artifact/trunk/ice.config
 export ICE_CONFIG=`pwd`/ice.config
 
+#
+# Run tests
+#
 ./build.py build-default test-compile
 ./build.py -f components/tools/OmeroPy/build.xml -Dtest.with.fail=true test
 ./build.py -f components/tools/OmeroPy/build.xml -Dtest.with.fail=true integration
 ./build.py -f components/tools/OmeroFS/build.xml  -Dtest.with.fail=true test
 ./build.py -f components/tools/OmeroFS/build.xml  -Dtest.with.fail=true integration
+
+#
+# Try DropBox, Hudson will look for ERROR in the output log.
+#
+cd dist
+wget 'http://hudson.openmicroscopy.org.uk/userContent/very_small.d3d%20with%20spaces.dv'
+echo omero.fstest.srcFile=very_small.d3d with spaces.dv >> etc\testdropbox.config
+echo omero.fs.watchDir=TestDropBox >> etc\testdropbox.config
+
+mkdir -p TestDropBox
+bin/omero admin ice server start TestDropBox
