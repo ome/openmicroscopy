4ecceb65750ca4a1d0319574bb74340771638b18
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-11-03 07:40:35 +0000

    Added test for blank passwords (See #3201)
    git-svn-id: file:///home/svn/omero/trunk@8489 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroPy/test/integration/admin.py
+++ components/tools/OmeroPy/test/integration/admin.py
@@ -89,6 +89,37 @@ class TestAdmin(lib.ITest):
             finally:
                 client3.closeSession()
 
+    def testChangePasswordWhenUnset(self):
+        """
+        Shows that it's possible to use the
+        changePasswordWithOldPassword when
+        previously no password was set.
+
+        See ticket:3201
+        """
+        client = self.new_client()
+        admin = client.sf.getAdminService()
+
+        # By setting the user's password to the empty string
+        # any password will be allowed as the old password
+        admin.changePassword(rstring(""))
+        admin.changePasswordWithOldPassword("IGNORED", rstring("ome"))
+        self.assertRaises(omero.SecurityViolation, admin.changePasswordWithOldPassword, "BADPW", rstring("foo"))
+        admin.changePasswordWithOldPassword("ome", rstring("foo"))
+
+        # None disables user. No further password checks will pass.
+        # Only the current session or an admin will be able to
+        # reset the password
+        admin.changePassword(None)
+        self.assertRaises(omero.SecurityViolation, admin.changePasswordWithOldPassword, "", rstring("foo"))
+        self.assertRaises(omero.SecurityViolation, admin.changePasswordWithOldPassword, None, rstring("foo"))
+        self.assertRaises(omero.SecurityViolation, admin.changePasswordWithOldPassword, "ome", rstring("foo"))
+        joined_client = client.createClient(True)
+        try:
+            self.assertRaises(omero.SecurityViolation, joined_client.sf.getAdminService().changePasswordWithOldPassword, "", rstring("ome"))
+        finally:
+            joined_client.__del__()
+        admin.changePassword(rstring("ome")) # could be an admin
 
 if __name__ == '__main__':
     unittest.main()
