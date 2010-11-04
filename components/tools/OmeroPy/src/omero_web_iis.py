7a1e037b171824fe983e8f0e0f2638ecb468ccfb
cxallan <cxallan@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-11-04 12:10:03 +0000

    Added OMERO.web IIS installation script (requires ISAPI-WSGI; http://code.google.com/p/isapi-wsgi/) (See #3194)
    git-svn-id: file:///home/svn/omero/trunk@8510 05709c45-44f0-0310-885b-81a1db45b4a6

---
+++ components/tools/OmeroPy/src/omero_web_iis.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+#
+# omero_web_iis.py - OMERO.web installer and ISAPI/WSGI handler for IIS
+# 
+# Copyright (c) 2010 University of Dundee. All rights reserved.
+# 
+# This software is distributed under the terms described by the LICENCE file
+# you can find at the root of the distribution bundle, which states you are
+# free to use it only for non commercial purposes.
+# If the file is missing please request a copy by contacting
+# ome-users@lists.openmicroscopy.org.uk.
+#
+# Author: Chris Allan <callan(at)blackcat.ca>
+#
+# Modified version of the example Django script and serving from the IIS root:
+#  * http://code.google.com/docreader/#p=isapi-wsgi&s=isapi-wsgi&t=IntegrationWithDjango
+#  * http://code.google.com/docreader/#p=isapi-wsgi&s=isapi-wsgi&t=ServingFromRoot
+#
+
+import os, sys
+CWD = os.path.dirname(__file__)
+sys.path.append(str(CWD))
+sys.path.append(str(os.path.join(CWD, 'omeroweb'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'omeroweb.settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
+import isapi_wsgi
+# The entry points for the ISAPI extension.
+def __ExtensionFactory__():
+    return isapi_wsgi.ISAPISimpleHandler(application)
+
+if __name__ == '__main__':
+    # If run from the command-line, install ourselves.
+    from isapi.install import *
+    params = ISAPIParameters()
+    # Setup the virtual directories - this is a list of directories our
+    # extension uses - in this case only 1.
+    # Each extension has a "script map" - this is the mapping of ISAPI
+    # extensions.
+    sm = [
+        ScriptMapParams(Extension="*", Flags=0)
+    ]
+    vd = VirtualDirParameters(Name="/",
+                              Description = "ISAPI-WSGI OMERO.web",
+                              ScriptMaps = sm,
+                              ScriptMapUpdate = "replace"
+                              )
+    params.VirtualDirs = [vd]
+    HandleCommandLine(params)
