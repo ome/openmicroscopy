fa848c8c291db2b16a8d876854b766cb03b76e91
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-11-05 15:34:20 +0000

    Moving OMERO_BUILD write to after "clean"
    git-svn-id: file:///home/svn/omero/trunk@8541 05709c45-44f0-0310-885b-81a1db45b4a6

--- docs/hudson/OMERO.sh
+++ docs/hudson/OMERO.sh
@@ -4,16 +4,18 @@ set -x
 
 export OMERO_BUILD=r"$SVN_REVISION"-"$OMERO_BRANCH"-b"$BUILD_NUMBER"
 
-echo OMERO_BUILD=$OMERO_BUILD > target/$OMERO_BRANCH.log
 
 ./build.py clean
 ./build.py build-default
 ./build.py release-docs
 ./build.py release-zip
 
+# Log information
+echo OMERO_BUILD=$OMERO_BUILD > target/$OMERO_BRANCH.log
+svn info > target/SVN_INFO
+
 # Creating source release
 rm -rf target/svn-export
-svn info > target/SVN_INFO
 svn export . target/svn-export
 cd target/svn-export
 zip -r ../OMERO.source-r"$SVN_REVISION"-"$OMERO_BRANCH".zip .
