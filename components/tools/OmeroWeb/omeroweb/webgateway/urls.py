57563f1260d90e5673e9093300b154d19c68f83d
wmoore <wmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2010-12-03 12:27:08 +0000

    Adding docs to webgateway.urls.py. See #3395
    git-svn-id: file:///home/svn/omero/trunk@8708 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroWeb/omeroweb/webgateway/urls.py
+++ components/tools/OmeroWeb/omeroweb/webgateway/urls.py
@@ -13,15 +13,96 @@
 
 from django.conf.urls.defaults import *
 
+appmedia = (r'^appmedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'webgateway/media'})
+""" 
+path to media files for webgateway 
+    - path: path to media file
+"""
+
+render_image = (r'^render_image/(?P<iid>[^/]+)/(?P<z>[^/]+)/(?P<t>[^/]+)/$', 'webgateway.views.render_image')
+"""
+Returns a jpeg of the OMERO image. See L{webgateway.views.render_image}. Rendering settings can be specified
+in the request parameters. See L{webgateway.views.getImgDetailsFromReq} for details. 
+    Params in render_image/<iid>/<z>/<t>/ are:
+    - iid:  Image ID
+    - z:    Z index
+    - t:    T index
+"""
+
+render_split_channel = (r'^render_split_channel/(?P<iid>[^/]+)/(?P<z>[^/]+)/(?P<t>[^/]+)/$', 'webgateway.views.render_split_channel')
+"""
+Returns a jpeg of OMERO Image with channels split into different panes in a grid. See {webgateway.views.render_split_channel}.
+Rendering settings can be specified in the request parameters (as above).
+    Params in render_split_channel/<iid>/<z>/<t> are:
+    - iid:  Image ID
+    - z:    Z index
+    - t:    T index
+"""
+
+render_row_plot = (r'^render_row_plot/(?P<iid>[^/]+)/(?P<z>[^/]+)/(?P<t>[^/]+)/(?P<y>[^/]+)/(?:(?P<w>[^/]+)/)?$', 
+    'webgateway.views.render_row_plot')
+"""
+Returns a gif graph of pixel values for a row of an image plane. See {webgateway.views.render_row_plot}.
+Channels can be turned on/off using request. E.g. c=-1,2,-3,-4
+    Params in render_row_plot/<iid>/<z>/<t>/<y>/<w> are:
+    - iid:  Image ID
+    - z:    Z index
+    - t:    T index
+    - y:    Y position of pixel row
+    - w:    Optional line width of plot
+"""
+
+render_col_plot = (r'^render_col_plot/(?P<iid>[^/]+)/(?P<z>[^/]+)/(?P<t>[^/]+)/(?P<x>[^/]+)/(?:(?P<w>[^/]+)/)?$', 
+    'webgateway.views.render_col_plot')
+"""
+Returns a gif graph of pixel values for a column of an image plane. See {webgateway.views.render_col_plot}.
+Channels can be turned on/off using request. E.g. c=-1,2,-3,-4
+    Params in render_col_plot/<iid>/<z>/<t>/<x>/<w> are:
+    - iid:  Image ID
+    - z:    Z index
+    - t:    T index
+    - x:    X position of pixel column
+    - w:    Optional line width of plot
+"""
+
+render_thumbnail = (r'^render_thumbnail/(?P<iid>[^/]+)/(?:(?P<w>[^/]+)/)?(?:(?P<h>[^/]+)/)?$', 'webgateway.views.render_thumbnail')
+"""
+Returns a thumbnail jpeg of the OMERO Image, optionally scaled to max-width and max-height.
+See L{webgateway.views.render_thumbnail}. Uses current rendering settings. 
+    Params in render_thumbnail/<iid>/<w>/<h> are:
+    - iid:  Image ID
+    - w:    Optional max width
+    - h:    Optional max height
+"""
+
+render_ome_tiff = (r'^render_ome_tiff/(?P<ctx>[^/]+)/(?P<cid>[^/]+)/$', 'webgateway.views.render_ome_tiff')
+"""
+Generates an OME-TIFF of an Image (or zip for multiple OME-TIFFs) and returns the file or redirects 
+to a temp file location. See L{webgateway.views.render_ome_tiff}
+    Params in render_ome_tiff/<ctx>/<cid> are:
+    - ctx:      The container context. 'p' for Project, 'd' for Dataset or 'i' Image. 
+    - cid:      ID of container.
+"""
+
+render_movie = (r'^render_movie/(?P<iid>[^/]+)/(?P<axis>[zt])/(?P<pos>[^/]+)/$', 'webgateway.views.render_movie')
+"""
+Generates a movie file from the image, spanning Z or T frames. See {views.render_movie}
+Returns the file or redirects to temp file location. 
+    Params in render_movie/<iid>/<axis>/<pos> are:
+    - iid:      Image ID
+    - axis:     'z' or 't' dimension that movie plays
+    - pos:      The T index (for 'z' movie) or Z index (for 't' movie)
+"""
+
 urlpatterns = patterns('',
-    (r'^appmedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'webgateway/media'}),
-    (r'^render_image/(?P<iid>[^/]+)/(?P<z>[^/]+)/(?P<t>[^/]+)/$', 'webgateway.views.render_image'),
-    (r'^render_split_channel/(?P<iid>[^/]+)/(?P<z>[^/]+)/(?P<t>[^/]+)/$', 'webgateway.views.render_split_channel'),
-    (r'^render_row_plot/(?P<iid>[^/]+)/(?P<z>[^/]+)/(?P<t>[^/]+)/(?P<y>[^/]+)/(?:(?P<w>[^/]+)/)?$', 'webgateway.views.render_row_plot'),
-    (r'^render_col_plot/(?P<iid>[^/]+)/(?P<z>[^/]+)/(?P<t>[^/]+)/(?P<x>[^/]+)/(?:(?P<w>[^/]+)/)?$', 'webgateway.views.render_col_plot'),
-    (r'^render_thumbnail/(?P<iid>[^/]+)/(?:(?P<w>[^/]+)/)?(?:(?P<h>[^/]+)/)?$', 'webgateway.views.render_thumbnail'),
-    (r'^render_ome_tiff/(?P<ctx>[^/]+)/(?P<cid>[^/]+)/$', 'webgateway.views.render_ome_tiff'),
-    (r'^render_movie/(?P<iid>[^/]+)/(?P<axis>[zt])/(?P<pos>[^/]+)/$', 'webgateway.views.render_movie'),
+    appmedia,
+    render_image,
+    render_split_channel,
+    render_row_plot,
+    render_col_plot,
+    render_thumbnail,
+    render_ome_tiff,
+    render_movie,
 
     # Template views
     (r'^test/$', 'webgateway.views.test'),
