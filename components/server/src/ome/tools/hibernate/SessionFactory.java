e4e7f6b3219d80efc70593ba69d13ee617acdc63
jmoore <jmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2011-01-06 10:19:41 +0000

    Prevent misuse of org.hibernate.Session (See #2985, #2684)
    original-svn-id: file:///home/svn/omero/trunk@8790 05709c45-44f0-0310-885b-81a1db45b4a6
    
    git-svn-id: file:///home/svn/omero/branches/Beta4.2@8843 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/server/src/ome/tools/hibernate/SessionFactory.java
+++ components/server/src/ome/tools/hibernate/SessionFactory.java
@@ -6,8 +6,19 @@
  */
 package ome.tools.hibernate;
 
+import java.lang.reflect.Method;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.hibernate.Session;
+
 import org.springframework.orm.hibernate3.SessionFactoryUtils;
+import org.springframework.aop.framework.ProxyFactory;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 
 /**
  * Simple source of Thread-aware {@link Session} instances. Wraps a
@@ -17,7 +28,29 @@ import org.springframework.orm.hibernate3.SessionFactoryUtils;
  * @author Josh Moore, josh at glencoesoftware.com
  * @since 4.0
  */
-public class SessionFactory {
+public class SessionFactory implements MethodInterceptor {
+
+    private final static Set<String> FORBIDDEN = Collections.unmodifiableSet(
+        new HashSet<String>(
+            Arrays.asList(
+                "createSQLQuery", "getSession", "doWork",
+                "connection", "disconnect", "reconnect")));
+
+    static {
+        // Check for spelling mistakes
+        int found = 0;
+        Method[] methods = Session.class.getMethods();
+        for (Method m : methods) {
+            if (FORBIDDEN.contains(m.getName())) {
+                found++;
+            }
+        }
+        if (found < FORBIDDEN.size()) {
+            throw new RuntimeException("Method name not found! " + FORBIDDEN);
+        }
+
+    }
+
 
     private final org.hibernate.SessionFactory factory;
 
@@ -25,8 +58,35 @@ public class SessionFactory {
         this.factory = factory;
     }
 
+    /**
+     * Returns a session active for the current thread. The returned
+     * instance will be wrapped with AOP to prevent certain usage.
+     * @see ticket:73
+     */
     public Session getSession() {
-        return SessionFactoryUtils.getSession(factory, false);
+
+        Session unwrapped = SessionFactoryUtils.getSession(factory, false);
+
+        ProxyFactory proxyFactory = new ProxyFactory();
+        proxyFactory.setInterfaces(new Class[]{Session.class});
+        proxyFactory.setTarget(unwrapped);
+        proxyFactory.addAdvice(0, this);
+        return (Session) proxyFactory.getProxy();
+
+    }
+
+    /**
+     * Wraps all invocations to Session to prevent certain usages.
+     */
+    public Object invoke(MethodInvocation mi) throws Throwable {
+
+        final String name = mi.getMethod().getName();
+        if (FORBIDDEN.contains(name)) {
+            throw new ome.conditions.InternalException(String.format(
+                "Usage of session.%s is forbidden. See ticket #73", name));
+        }
+        return mi.proceed();
+
     }
 
 }
