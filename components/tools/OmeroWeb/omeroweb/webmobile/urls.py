ee25b9e611f3f88e4862116c7fdd7fc842d73f9a
wmoore <wmoore@05709c45-44f0-0310-885b-81a1db45b4a6>
2011-01-09 23:14:39 +0000

    Webmobile P/D/I browsing and adding comments without javascript.
    git-svn-id: file:///home/svn/omero/trunk@8881 05709c45-44f0-0310-885b-81a1db45b4a6

--- components/tools/OmeroWeb/omeroweb/webmobile/urls.py
+++ components/tools/OmeroWeb/omeroweb/webmobile/urls.py
@@ -10,8 +10,21 @@ urlpatterns = patterns('',
     url( r'^logout/$', views.logout, name='webmobile_logout' ),
     url( r'^img_detail/(?P<iid>[0-9]+)/$', views.image_viewer, name="image_viewer"),
     
+    # browsing P/D/I hierarchy
+    url( r'^projects/$', views.projects, name='webmobile_projects' ),
+    url( r'^project/(?P<id>[0-9]+)/$', views.project, name='webmobile_project' ),
+    url( r'^project_details/(?P<id>[0-9]+)/$', views.object_details, {"obj_type": "project"}, name='webmobile_project_details' ),
     url( r'^dataset/(?P<id>[0-9]+)/$', views.dataset, name='webmobile_dataset' ),
-    url( r'^viewer/(?P<imageId>[0-9]+)/$', views.viewer, name='webmobile_viewer' ),
+    url( r'^dataset_details/(?P<id>[0-9]+)/$', views.object_details, {"obj_type": "dataset"}, name='webmobile_dataset_details' ),
+    url( r'^image/(?P<imageId>[0-9]+)/$', views.image, name='webmobile_image' ),
+    url( r'^viewer/(?P<imageId>[0-9]+)/$', views.viewer, name='webmobile_viewer' ),     # 'full' viewer
+    
+    url( r'^groups_members/$', views.groups_members, name='webmobile_groups_members' ),
+    # switch group, then redirect to index page
+    url( r'^switch_group/(?P<groupId>[0-9]+)/$', views.switch_group, name='webmobile_switch_group' ),
+    
+    # add comment to 'project', 'dataset' or 'image', then redirect to object page
+    url( r'^add_comment/(?P<obj_type>[a-z]+)/(?P<obj_id>[0-9]+)/$', views.add_comment, name='webmobile_add_comment' ),
     
     url(r'^appmedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'webmobile/media'}, name="mobile_static"),
 )
