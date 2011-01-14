a45f8507fca00b6de5ac376bf1b0018d1239ab95
jburel <jburel@6f2cb1de-eb0d-0410-b157-e593188b5901>
2011-01-14 11:57:19 +0000

    Fix NPE but another synch problem noticed. (close #3847 and close #3854)
    git-svn-id: file:///home/svn/shoola-omero/branches/Beta4.2@7915 6f2cb1de-eb0d-0410-b157-e593188b5901

--- SRC/org/openmicroscopy/shoola/agents/treeviewer/view/TreeViewerModel.java
+++ SRC/org/openmicroscopy/shoola/agents/treeviewer/view/TreeViewerModel.java
@@ -1141,7 +1141,8 @@ class TreeViewerModel
 	 */
 	void reloadThumbnails(List<Long> ids)
 	{
-		getDataViewer().reloadThumbnails(ids);
+		DataBrowser db = getDataViewer();
+		if (db != null) db.reloadThumbnails(ids);
 	}
 	
 	/** 
