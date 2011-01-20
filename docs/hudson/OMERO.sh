70b560fdb0c1b42af7206ef421860471f3e9f8a6
Josh Moore <josh@glencoesoftware.com>
2011-01-20 11:32:10 +0000

    Moving OMERO.sh to git (See #3934)

--- docs/hudson/OMERO.sh
+++ docs/hudson/OMERO.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+#
+# Copyright 2010 Glencoe Software, Inc. All rights reserved.
+# Use is subject to license terms supplied in LICENSE.txt
+#
+# Main build script. Primarily launched by hudson.
+#
 set -e
 set -u
 set -x
 
-export OMERO_BUILD=r"$SVN_REVISION"-"$OMERO_BRANCH"-b"$BUILD_NUMBER"
+source docs/hudson/functions.sh
+export OMERO_BUILD=`git_short_rev`
+echo Building $OMERO_BRANCH
 
 ./build.py clean
 ./build.py build-default
@@ -12,10 +21,6 @@ export OMERO_BUILD=r"$SVN_REVISION"-"$OMERO_BRANCH"-b"$BUILD_NUMBER"
 # Log information
 echo OMERO_BUILD=$OMERO_BUILD > target/$OMERO_BRANCH.log
 echo OMERO_BRANCH=$OMERO_BRANCH >> target/$OMERO_BRANCH.log
-svn info > target/SVN_INFO
-
-# Creating source release
-rm -rf target/svn-export
-svn export . target/svn-export
-cd target/svn-export
-zip -r ../OMERO.source-r"$SVN_REVISION"-"$OMERO_BRANCH".zip .
+git_info > target/GIT_INFO
+env | sort >> target/GIT_INFO
+git_zip ../OMERO.source-$OMERO_BUILD.zip
