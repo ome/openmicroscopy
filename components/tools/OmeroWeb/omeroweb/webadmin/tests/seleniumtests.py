487d9f8d70d1ad21266b1d8e3332c6fdece538c4
Will Moore <will@lifesci.dundee.ac.uk>
2011-01-25 20:29:14 +0000

    Partially implemented testCreateGroup()

--- components/tools/OmeroWeb/omeroweb/webadmin/tests/seleniumtests.py
+++ components/tools/OmeroWeb/omeroweb/webadmin/tests/seleniumtests.py
@@ -1,5 +1,6 @@
 from omeroweb.webgateway.tests.seleniumbase import SeleniumTestBase, Utils
 from omero.gateway.scripts import dbhelpers
+from random import random
 
 class WebAdminTestBase (SeleniumTestBase):
 
@@ -33,11 +34,13 @@ class AdminTests (WebAdminTestBase):
         #password = dbhelpers.ROOT.passwd
         #print user, password    # seems to always be 'root', 'ome' 
         self.login('root', 'omero')
+        self.newGroupName = "SeleniumTestGroup%s" % random()
         
-    def testLoginAndPages (self):
+        
+    def testPages (self):
         """
-        This logs in to webadmin as 'root' 'omero' and checks that the links exist for the main pages. 
-        Then visits each page in turn. Starts at '
+        This checks that the links exist for the main pages. 
+        Visits each page in turn. Starts at experimenters and clicks links to each other main page '
         """
         # login done already in setUp()
         sel = self.selenium
@@ -51,6 +54,73 @@ class AdminTests (WebAdminTestBase):
         sel.click("link=Drive Space")
         sel.wait_for_page_to_load("30000")
 
+
+    def testCreateExperimenter (self):
+        """
+        Creates a new experimenter. Tests that ommiting to fill in 'ome-name' gives a correct message to user.
+        Checks that the new user is displayed in the table of experimenters 
+        
+        TODO: need to create a group first
+        """
+        groupName = self.newGroupName
+        
+        # uuid = self.root.sf.getAdminService().getEventContext().sessionUuid
+        uuid = random()
+        omeName = 'OmeName%s' % uuid
+        firstName = 'Selenium'
+        lastName = 'Test'
+        password = 'secretPassword'
+        
+        sel = self.selenium
+        sel.open("/webadmin/experimenters")
+        sel.click("link=Add new scientist")
+        sel.wait_for_page_to_load("30000")
+        # Don't fill out omeName here.
+        sel.type("id_first_name", firstName)
+        sel.type("id_last_name", lastName)
+        sel.type("id_password", password)
+        sel.type("id_confirmation", password)
+        
+        # choose existing group, add to new user, choose one as default group 
+        sel.add_selection("id_available_groups", "label=%s" % groupName)
+        sel.click("add")
+        sel.click("default_group")
+        sel.click("//input[@value='Save']")
+        sel.wait_for_page_to_load("30000")
+        
+        # check that we failed to create experimenter - ome-name wasn't filled out
+        self.failUnless(sel.is_text_present("This field is required."))
+        sel.type("id_omename", omeName)
+        sel.click("//input[@value='Save']")
+        sel.wait_for_page_to_load("30000")
+        
+        # check omeName and 'Full Name' are on the page of Scientists. 
+        self.assertEqual("WebAdmin - Scientists", sel.get_title())
+        self.failUnless(sel.is_text_present(omeName))
+        self.failUnless(sel.is_text_present("%s %s" % (firstName, lastName)))
+        # better to check text in right place
+        self.assert_(sel.is_element_present("jquery=#experimenterTable tbody tr td:containsExactly(%s)" % omeName))
+    
+    
+    def testCreateGroup(self):
+        """
+        This needs to run before testCreateExperimenter()
+        """
+        groupName = self.newGroupName
+        
+        sel = self.selenium
+        sel.open("/webadmin/groups")
+        sel.click("link=Add new group")
+        sel.type("id_name", groupName)
+        sel.click("//input[@value='Save']")
+        sel.wait_for_page_to_load("30000")
+        
+        # check new Group is on the page of Groups. 
+        self.assertEqual("WebAdmin - Groups", sel.get_title())
+        self.failUnless(sel.is_text_present(groupName))
+        self.assert_(sel.is_element_present("jquery=#groupTable tbody tr td:containsExactly(%s)" % groupName))
+        
+        
     def tearDown(self):
         self.logout()
         super(AdminTests, self).tearDown()
