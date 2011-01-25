1f5ce4bf78199e46e883a6cf90019b9ca865c16a
Will Moore <will@lifesci.dundee.ac.uk>
2011-01-25 13:52:18 +0000

    Working webadmin selenium test - but with dbhelpers call removed

--- components/tools/OmeroWeb/omeroweb/webadmin/tests/seleniumtests.py
+++ components/tools/OmeroWeb/omeroweb/webadmin/tests/seleniumtests.py
@@ -5,6 +5,7 @@ class WebAdminTestBase (SeleniumTestBase):
 
         
     def login (self, u, p):
+        sel = self.selenium
         if self.selenium.is_element_present('link=Log out'):
             print "logging out..."
             self.logout()
@@ -14,20 +15,11 @@ class WebAdminTestBase (SeleniumTestBase):
         sel.type("id_password", "omero")
         sel.click("//input[@value='Connect']")
         self.waitForElementPresence('link=Scientists')
-        """
-        self.selenium.click("link=Log in")
-        self.selenium.wait_for_page_to_load("30000")
-        self.selenium.type("username", u)
-        self.selenium.type("password", p)
-        self.selenium.click("//input[@value='LOG IN']")
-        self.selenium.wait_for_page_to_load("30000")
-        self.waitForElementPresence('link=Log out')
-        """
         
     def logout (self):
-        self.selenium.click("link=Log out")
+        self.selenium.click("link=Logout")
         self.selenium.wait_for_page_to_load("30000")
-        self.waitForElementPresence('link=Log in')
+        self.waitForElementPresence("//input[@value='Connect']")
         
         
 class AdminTests (WebAdminTestBase):
@@ -36,26 +28,20 @@ class AdminTests (WebAdminTestBase):
     
     def setUp(self):
         super(AdminTests, self).setUp()
-        dbhelpers.refreshConfig()
-        user = dbhelpers.ROOT.name
-        password = dbhelpers.ROOT.passwd
-        print user, password    # seems to always be 'root', 'ome' 
-        self.login(user, 'omero')
+        #dbhelpers.refreshConfig()
+        #user = dbhelpers.ROOT.name
+        #password = dbhelpers.ROOT.passwd
+        #print user, password    # seems to always be 'root', 'ome' 
+        self.login('root', 'omero')
         
     def testLoginAndPages (self):
         """
         This logs in to webadmin as 'root' 'omero' and checks that the links exist for the main pages. 
         Then visits each page in turn. Starts at '
         """
-        
+        # login done already in setUp()
         sel = self.selenium
-        print sel
-        
-        u = dbhelpers.ROOT.name
-        p = dbhelpers.ROOT.passwd
-        print "testLoginAndPages", u, p  # root, ome  # etc/ice-config says root, omero! 
-        self.login(u, 'omero')
-        
+        sel.open("/webadmin/experimenters")
         sel.wait_for_page_to_load("30000")
         self.assertEqual("WebAdmin - Scientists", sel.get_title())
         sel.click("link=Groups")
@@ -63,8 +49,6 @@ class AdminTests (WebAdminTestBase):
         sel.click("link=My Account")
         sel.wait_for_page_to_load("30000")
         sel.click("link=Drive Space")
-        sel.wait_for_page_to_load("30000")
-        sel.click("link=Logout")
         sel.wait_for_page_to_load("30000")
 
     def tearDown(self):
