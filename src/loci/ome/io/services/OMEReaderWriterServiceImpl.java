ome/bioformats

commit 8da0639b92fd5ccc49bcfd31493e8fb331cec73f

Fix dependency issues identified by scan-deps.pl.

build.xml
@@ -30,32 +30,11 @@ Bio-Formats
     Project deps:  LOCI Common, OME-XML Java library, JAI Image I/O Tools,
                    MDB Tools (Java port), Apache Jakarta POI,
                    Luratech LuraWave stubs
-    Library deps:  JGoodies Forms, NetCDF, Simple Logging Facade for Java API,
-                   TestNG
-    Optional:      OME I/O, Apache log4j,
-                   Simple Logging Facade for Java log4j Binding,
+    Library deps:  JGoodies Forms, Apache log4j, NetCDF,
+                   Simple Logging Facade for Java API, TestNG
+    Optional:      Simple Logging Facade for Java log4j Binding,
                    Xalan Serializer, Xalan
     License:       GPL
-
-WiscScan Flow Cytometry
-    Server application for flow cytometry with WiscScan using JVMLink
-    -=-
-    JAR file:      flow-cytometry.jar
-    Path:          components/flow-cytometry
-    Project deps:  Bio-Formats, LOCI Common, OME-XML Java library
-    Library deps:  ImageJ, VisAD
-    Optional:      (none)
-    License:       BSD
-
-LOCI Checkstyle checks
-    LOCI's Checkstyle extensions, for checking source code style
-    -=-
-    JAR file:      loci-checks.jar
-    Path:          components/checkstyle
-    Project deps:  (none)
-    Library deps:  Checkstyle
-    Optional:      (none)
-    License:       Public domain
 
 LOCI Common
     A library containing common I/O and reflection classes
@@ -85,9 +64,8 @@ OME I/O
     JAR file:      ome-io.jar
     Path:          components/ome-io
     Project deps:  Bio-Formats, LOCI Common
-    Library deps:  Apache log4j, Simple Logging Facade for Java API,
-                   OMERO Client, OMERO Common, TestNG
-    Optional:      OME-Java
+    Library deps:  Apache log4j, Simple Logging Facade for Java API, TestNG
+    Optional:      OME-Java, OMERO Client, OMERO Common
     License:       GPL
 
 OME Plugins for ImageJ
@@ -109,18 +87,7 @@ OME-XML Java library
     Path:          components/ome-xml
     Project deps:  (none)
     Library deps:  Simple Logging Facade for Java API, TestNG
-    Optional:      (none)
-    License:       GPL
-
-SLIM Plotter
-    An application and curve fitting library for visualization and analysis of
-    combined spectral lifetime data
-    -=-
-    JAR file:      SlimPlotter.jar
-    Path:          components/slim-plotter
-    Project deps:  Bio-Formats, LOCI Common, VisBio
-    Library deps:  L-M Fit, VisAD
-    Optional:      (none)
+    Optional:      Apache log4j, Simple Logging Facade for Java log4j Binding
     License:       GPL
 
 LOCI testing framework
@@ -133,39 +100,8 @@ LOCI testing framework
     Optional:      (none)
     License:       BSD
 
-VisBio
-    A multi-purpose biological analysis tool
-    -=-
-    JAR file:      visbio.jar
-    Path:          components/visbio
-    Project deps:  Bio-Formats, LOCI Common, OME I/O, OME-XML Java library,
-                   Apache Jakarta POI
-    Library deps:  Apple eAWT stubs, JGoodies Forms, ImageJ, JUnit,
-                   JGoodies Looks, VisAD
-    Optional:      (none)
-    License:       GPL
-
 ===============================================================================
 The following components are considered "legacy" but still available:
-
-JVMLink
-    A library for communicating between a Java Virtual Machine and other
-    programs (e.g., C++ applications) via IP over localhost (or remotely)
-    -=-
-    JAR file:      jvmlink.jar
-    Path:          components/legacy/jvmlink
-    Project deps:  LOCI Common
-    Optional:      (none)
-    License:       BSD
-
-Multi-LUT
-    A demo application for visually exploring multi-spectral image data
-    -=-
-    JAR file:      MultiLUT.jar
-    Path:          components/legacy/multi-lut
-    Project deps:  (none)
-    Optional:      (none)
-    License:       Public domain
 
 OME Metadata Editor
     An application for exploration and editing of OME-XML and OME-TIFF metadata
@@ -238,13 +174,6 @@ Luratech LuraWave stubs
 
 ===============================================================================
 The following external dependencies (in the jar folder) may be required:
-Apple eAWT stubs
-    JAR file:  AppleJavaExtensions.jar
-    URL:       http://developer.apple.com/samplecode/AppleJavaExtensions/
-    Notes:     required to compile Mac-specific functionality on non-Mac OS X
-               machines
-    License:   BSD
-
 Ant-Contrib
     JAR file:  ant-contrib-1.0b3.jar
     URL:       http://ant-contrib.sourceforge.net/
@@ -281,12 +210,6 @@ JGoodies Forms
     Notes:     used for layout by VisBio, Data Browser and OME Notes
     License:   BSD
 
-Ice
-    JAR file:  Ice-3.3.1.jar
-    URL:       http://www.zeroc.com/ice.html
-    Notes:     used by Bio-Formats Ice framework
-    License:   GPL
-
 ImageJ
     JAR file:  ij.jar
     URL:       http://rsb.info.nih.gov/ij/
@@ -302,29 +225,16 @@ JAI ImageIO wrapper
     License:   BSD
 
 JUnit
-    JAR file:  junit.jar
+    JAR file:  junit-4.8.2.jar
     URL:       http://www.junit.org/
     Notes:     unit testing framework used for a few VisBio unit tests
     License:   Common Public License
-
-L-M Fit
-    JAR file:  lma.jar
-    URL:       http://users.utu.fi/jaolho/
-    Notes:     Levenberg-Marquardt algorithm for exponential curve fitting,
-               used by SLIM Plotter
-    License:   LGPL
 
 Apache log4j
     JAR file:  log4j-1.2.15.jar
     URL:       http://logging.apache.org/log4j/
     Notes:     required by SLF4J implementation
     License:   Apache
-
-JGoodies Looks
-    JAR file:  looks-2.3.1.jar
-    URL:       http://www.jgoodies.com/freeware/looks/index.html
-    Notes:     used for a nicer Look & Feel by VisBio and OME Metadata Editor
-    License:   BSD
 
 NetCDF
     JAR file:  netcdf-4.0.jar
@@ -374,12 +284,6 @@ Xalan Serializer
     Notes:     used for OME-XML transformations
     License:   Apache
 
-Skin Look and Feel
-    JAR file:  skinlf.jar
-    URL:       http://skinlf.l2fprod.com/
-    Notes:     not used (may be used in the future for flexible skinning)
-    License:   Custom (BSD-like)
-
 TestNG
     JAR file:  testng-5.11-jdk15.jar
     URL:       http://testng.org/
@@ -392,13 +296,6 @@ Apache Velocity
     Notes:     used to autogenerate the loci.formats.meta and loci.formats.ome
                Bio-Formats packages
     License:   Apache
-
-VisAD
-    JAR file:  visad-lite.jar
-    URL:       http://www.ssec.wisc.edu/~billh/visad.html
-    Notes:     stripped down VisAD library used by VisBio and SLIM Plotter for
-               interactive visualization
-    License:   LGPL
 
 Xalan
     JAR file:  xalan-2.7.1.jar

components/bio-formats/build.properties
@@ -16,6 +16,7 @@ component.classpath      = ${artifact.dir}/jai_imageio.jar:\
                            ${artifact.dir}/ome-xml.jar:\
                            ${artifact.dir}/poi-loci.jar:\
                            ${lib.dir}/forms-1.3.0.jar:\
+                           ${lib.dir}/log4j-1.2.15.jar:\
                            ${lib.dir}/netcdf-4.0.jar:\
                            ${lib.dir}/slf4j-api-1.5.10.jar:\
                            ${lib.dir}/testng-5.11-jdk15.jar
@@ -36,8 +37,6 @@ component.resources-text = loci/formats/*.txt
 
 component.main-class     = loci.formats.gui.ImageViewer
 component.runtime-cp     = ${component.classpath}:\
-                           ${artifact.dir}/ome-io.jar:\
-                           ${lib.dir}/log4j-1.2.15.jar:\
                            ${lib.dir}/serializer-2.7.1.jar:\
                            ${lib.dir}/slf4j-log4j12-1.5.10.jar:\
                            ${lib.dir}/xalan-2.7.1.jar

components/ome-io/build.properties
@@ -12,9 +12,6 @@ component.version        = 4.3-DEV
 component.classpath      = ${artifact.dir}/bio-formats.jar:\
                            ${artifact.dir}/loci-common.jar:\
                            ${lib.dir}/log4j-1.2.15.jar:\
-                           ${lib.dir}/ome-java.jar:\
-                           ${lib.dir}/omero-client-4.2.0.jar:\
-                           ${lib.dir}/omero-common-4.2.0.jar:\
                            ${lib.dir}/slf4j-api-1.5.10.jar:\
                            ${lib.dir}/testng-5.11-jdk15.jar
 component.java-version   = 1.5
@@ -24,13 +21,7 @@ component.resources-bin  = loci/ome/io/ome-logo.png
 component.resources-text =
 
 component.main-class     = loci.ome.io.OmeroReader
-component.runtime-cp     = ${component.classpath}
-
-
-# NB: do not include ${artifact.dir}/bio-formats.jar,
-#     as its classpath includes ome-io.jar
-component.classpath-no-omeio  = ${root.dir}/components/bio-formats/build/classes:\
-                                ${artifact.dir}/loci-common.jar:\
-                                ${lib.dir}/log4j-1.2.15.jar:\
-                                ${lib.dir}/slf4j-api-1.5.10.jar:\
-                                ${lib.dir}/testng-5.11-jdk15.jar
+component.runtime-cp     = ${component.classpath}:\
+                           ${lib.dir}/ome-java.jar:\
+                           ${lib.dir}/omero-client-4.2.0.jar:\
+                           ${lib.dir}/omero-common-4.2.0.jar

components/ome-io/build.xml
@@ -12,7 +12,7 @@ Type "ant -p" for a list of targets.
   <import file="${root.dir}/ant/java.xml"/>
   <property file="build.properties"/>
 
-  <target name="test" depends="jar, test-no-omeio" description="run tests">
+  <target name="test" depends="jar" description="run tests">
     <!-- NOTE: Overrides default "test" target from java.xml -->
     <copy tofile="${build.dir}/testng.xml" overwrite="true"
       file="${tests.dir}/loci/ome/utests/testng.xml"/>
@@ -21,24 +21,6 @@ Type "ant -p" for a list of targets.
         <pathelement location="${test-classes.dir}"/>
         <pathelement location="${classes.dir}"/>
         <pathelement path="${component.classpath}"/>
-      </classpath>
-      <xmlfileset file="${build.dir}/testng.xml"/>
-      <jvmarg value="-mx${testng.memory}"/>
-    </testng>
-    <fail if="failedTest"/>
-  </target>
-
-  <target name="test-no-omeio" depends="compile-tests" description="run missing OME I/O tests" if="doTests">
-    <echo>${test-classes.dir}</echo>
-    <echo>${classes.dir}</echo>
-    <echo>${component.classpath-no-omeio}</echo>
-    <copy tofile="${build.dir}/testng.xml" overwrite="true"
-      file="${tests.dir}/loci/ome/utests/testng-no-ome-io.xml"/>
-    <testng failureProperty="failedTest">
-      <classpath>
-        <pathelement location="${test-classes.dir}"/>
-        <pathelement location="${classes.dir}"/>
-        <pathelement path="${component.classpath-no-omeio}"/>
       </classpath>
       <xmlfileset file="${build.dir}/testng.xml"/>
       <jvmarg value="-mx${testng.memory}"/>

components/ome-io/src/loci/ome/io/OMEUtils.java
@@ -69,6 +69,8 @@ public final class OMEUtils {
   private static ReflectedUniverse r = createReflectedUniverse();
   private static ReflectedUniverse createReflectedUniverse() {
     r = new ReflectedUniverse();
+    // NB: avoid dependencies on optional ome.api packages
+    // NB: avoid dependencies on optional ome.system packages
     try {
       r.exec("import ome.api.IQuery");
       r.exec("import ome.api.RawPixelsStore");

components/ome-xml/src/ome/xml/OMEXMLNode.java
@@ -43,6 +43,9 @@ import java.util.Hashtable;
 import java.util.Vector;
 import org.w3c.dom.*;
 
+// HACK: for scan-deps.pl: The following packages are not actually "optional":
+// optional org.apache.log4j, optional org.slf4j.impl
+
 /**
  * OMEXMLNode is the superclass of all OME-XML nodes. These nodes are
  * similar to, but more sophisticated than, the nodes obtained from a direct

scan-deps.pl
@@ -397,19 +397,6 @@ ZZ
   VERSION => "1.2.15"
 );
 
-my %looks = (
-  NAME    => "looks",
-  TITLE   => "JGoodies Looks",
-  JAR     => "looks-2.3.1.jar",
-  PACKAGE => "com.jgoodies.looks",
-  LICENSE => "BSD",
-  URL     => "http://www.jgoodies.com/freeware/looks/index.html",
-  NOTES   => <<ZZ,
-used for a nicer Look & Feel by VisBio and OME Metadata Editor
-ZZ
-  VERSION => "2.3.1"
-);
-
 my %netcdf = (
   NAME    => "netcdf",
   TITLE   => "NetCDF",
@@ -452,40 +439,27 @@ ZZ
 my %omeroClient = (
   NAME    => "omero-client",
   TITLE   => "OMERO Client",
-  JAR     => "omero-client-4.1.1.jar",
+  JAR     => "omero-client-4.2.0.jar",
   PACKAGE => "ome.system",
   LICENSE => "GPL",
   URL     => "http://trac.openmicroscopy.org.uk/omero/wiki/MilestoneDownloads",
   NOTES   => <<ZZ,
 used by OME I/O to connect to OMERO servers
 ZZ
-  VERSION => "4.1.1"
+  VERSION => "4.2.0"
 );
 
 my %omeroCommon = (
   NAME    => "omero-common",
   TITLE   => "OMERO Common",
-  JAR     => "omero-common-4.1.1.jar",
+  JAR     => "omero-common-4.2.0.jar",
   PACKAGE => "ome.api",
   LICENSE => "GPL",
   URL     => "http://trac.openmicroscopy.org.uk/omero/wiki/MilestoneDownloads",
   NOTES   => <<ZZ,
 used by OME I/O to connect to OMERO servers
 ZZ
-  VERSION => "4.1.1"
-);
-
-my %skinlf = (
-  NAME    => "skinlf",
-  TITLE   => "Skin Look and Feel",
-  JAR     => "skinlf.jar",
-  PACKAGE => "com.l2fprod",
-  LICENSE => "Custom (BSD-like)",
-  URL     => "http://skinlf.l2fprod.com/",
-  NOTES   => <<ZZ,
-not used (may be used in the future for flexible skinning)
-ZZ
-  VERSION => "6.7"
+  VERSION => "4.2.0"
 );
 
 my %slf4j_api = (
@@ -539,19 +513,6 @@ used to autogenerate the loci.formats.meta and loci.formats.ome Bio-Formats
 packages
 ZZ
   VERSION => "1.6.3"
-);
-
-my %visad = (
-  NAME    => "visad",
-  TITLE   => "VisAD",
-  JAR     => "visad-lite.jar",
-  PACKAGE => "visad",
-  LICENSE => "LGPL",
-  URL     => "http://www.ssec.wisc.edu/~billh/visad.html",
-  NOTES   => <<ZZ,
-stripped down VisAD library used by VisBio and SLIM Plotter for interactive
-visualization
-ZZ
 );
 
 my %xmlrpc = (
@@ -632,20 +593,16 @@ my @components = (@active, @legacy, @forks, @stubs);
 
 # List of external libraries
 my @libs = (
-  \%appleJavaExtensions,
   \%antContrib,
   \%checkstyle,
   \%commonsHTTPClient,
   \%commonsLogging,
   \%findbugs,
   \%forms,
-  \%ice,
   \%ij,
   \%jiio,
   \%junit,
-  \%lma,
   \%log4j,
-  \%looks,
   \%netcdf,
   \%slf4j_api,
   \%slf4j_impl,
@@ -654,10 +611,8 @@ my @libs = (
   \%omeroClient,
   \%omeroCommon,
   \%serializer,
-  \%skinlf,
   \%testng,
   \%velocity,
-  \%visad,
   \%xalan,
   \%xmlrpc,
 );
@@ -814,7 +769,7 @@ foreach my $c (@components) {
 
 # -- DATA VERIFICATION --
 
-print STDERR "--== VERIFYING CLASSPATH MATCHES ==--\n\n";
+print STDERR "--== VERIFYING CLASSPATH MATCHES ==--\n";
 foreach my $c (@components) {
   my @projDeps = @{$$c{PROJ_DEPS}};
   my @libDeps = @{$$c{LIB_DEPS}};
@@ -822,7 +777,6 @@ foreach my $c (@components) {
   my @libOpt = @{$$c{LIB_OPT}};
   my $name = $$c{TITLE};
   my $path = $$c{PATH};
-  my @deps;
 
   # verify compile-time classpath
   my @compile = ();
@@ -836,7 +790,7 @@ foreach my $c (@components) {
   my @cp = @{$$c{COMPILE}};
   my $compileError = 0;
   if (@compile != @cp) {
-    print STDERR "Dependency mismatch for $name compile time classpath:\n";
+    print STDERR "\nDependency mismatch for $name compile time classpath:\n";
     $compileError = 1;
   }
   else {
@@ -844,7 +798,8 @@ foreach my $c (@components) {
       my $depJar = $compile[$i];
       my $cpJar = $cp[$i];
       if ($cpJar ne $depJar) {
-        print STDERR "Dependency mismatch for $name compile time classpath:\n";
+        print STDERR "\nDependency mismatch for $name " .
+          "compile time classpath:\n";
         print STDERR "  #$i: $depJar != $cpJar\n";
         $compileError = 1;
         last;
@@ -854,7 +809,7 @@ foreach my $c (@components) {
   if ($compileError) {
     print STDERR "  component.classpath:\n";
     print STDERR "    Actual    = @cp\n";
-    print STDERR "    Synthetic = @deps\n";
+    print STDERR "    Synthetic = @compile\n";
     print STDERR "\n";
     print STDERR "  project deps =";
     foreach my $q (@projDeps) {
@@ -870,7 +825,7 @@ foreach my $c (@components) {
   }
 
   # verify Eclipse classpath
-  @deps = ();
+  my @deps = ();
   push(@deps, "src");
   if (-e "$path/test") {
     push(@deps, "test");
@@ -895,7 +850,7 @@ foreach my $c (@components) {
   @cp = @{$$c{ECLIPSE}};
   my $eclipseError = 0;
   if (@deps != @cp) {
-    print STDERR "Dependency mismatch for $name Eclipse classpath:\n";
+    print STDERR "\nDependency mismatch for $name Eclipse classpath:\n";
     $eclipseError = 1;
   }
   else {
@@ -903,7 +858,7 @@ foreach my $c (@components) {
       my $depEntry = $deps[$i];
       my $cpEntry = $cp[$i];
       if ($cpEntry ne $depEntry) {
-        print STDERR "Dependency mismatch for $name Eclipse classpath:\n";
+        print STDERR "\nDependency mismatch for $name Eclipse classpath:\n";
         print STDERR "  #$i: $depEntry != $cpEntry\n";
         $eclipseError = 1;
         last;
@@ -941,7 +896,7 @@ foreach my $c (@components) {
   @cp = @{$$c{RUNTIME}};
   my $runtimeError = 0;
   if (@deps != @cp) {
-    print STDERR "Dependency mismatch for $name runtime classpath:\n";
+    print STDERR "\nDependency mismatch for $name runtime classpath:\n";
     $runtimeError = 1;
   }
   else {
@@ -949,7 +904,7 @@ foreach my $c (@components) {
       my $depJar = $deps[$i];
       my $cpJar = $cp[$i];
       if ($cpJar ne $depJar) {
-        print STDERR "Dependency mismatch for $name runtime classpath:\n";
+        print STDERR "\nDependency mismatch for $name runtime classpath:\n";
         print STDERR "  #$i: $depJar != $cpJar\n";
         $runtimeError = 1;
         last;
@@ -980,6 +935,7 @@ foreach my $c (@components) {
     foreach my $q (@libOpt) {
       print STDERR " $$q{NAME}";
     }
+    print STDERR "\n";
     $programErrors++;
   }
 }
@@ -990,7 +946,7 @@ if ($skipSummary) {
 
 # -- FORMATTED DATA OUTPUT --
 
-print STDERR "--== DUMPING RESULTS TO STDOUT ==--\n\n";
+print STDERR "\n--== DUMPING RESULTS TO STDOUT ==--\n\n";
 
 my $div = <<ZZ;
 ===============================================================================

ome/openmicroscopy

commit 294e43bab7b710ab5974862ad6124007e7e7c4af

Fixed ome-io compilation errors introduced in 8da0639b92fd5ccc49bcfd31493e8fb331cec73f.
Also, re-added the missing OME I/O test and updated scan-deps.pl to be
more particular about how it finds the 'component.classpath' properties.

build.properties
@@ -12,6 +12,8 @@ component.version        = 4.3-DEV
 component.classpath      = ${artifact.dir}/bio-formats.jar:\
                            ${artifact.dir}/loci-common.jar:\
                            ${lib.dir}/log4j-1.2.15.jar:\
+                           ${lib.dir}/ome-java.jar:\
+                           ${lib.dir}/omero-client-4.2.0.jar:\
                            ${lib.dir}/slf4j-api-1.5.10.jar:\
                            ${lib.dir}/testng-5.11-jdk15.jar
 component.java-version   = 1.5
@@ -22,6 +24,13 @@ component.resources-text =
 
 component.main-class     = loci.ome.io.OmeroReader
 component.runtime-cp     = ${component.classpath}:\
-                           ${lib.dir}/ome-java.jar:\
-                           ${lib.dir}/omero-client-4.2.0.jar:\
                            ${lib.dir}/omero-common-4.2.0.jar
+
+
+# NB: do not include ${artifact.dir}/bio-formats.jar,
+#     as its classpath includes ome-io.jar
+component.classpath-no-omeio  = ${root.dir}/components/bio-formats/build/classes:\
+                                ${artifact.dir}/loci-common.jar:\
+                                ${lib.dir}/log4j-1.2.15.jar:\
+                                ${lib.dir}/slf4j-api-1.5.10.jar:\
+                                ${lib.dir}/testng-5.11-jdk15.jar

build.xml
@@ -12,7 +12,7 @@ Type "ant -p" for a list of targets.
   <import file="${root.dir}/ant/java.xml"/>
   <property file="build.properties"/>
 
-  <target name="test" depends="jar" description="run tests">
+  <target name="test" depends="jar, test-no-omeio" description="run tests">
     <!-- NOTE: Overrides default "test" target from java.xml -->
     <copy tofile="${build.dir}/testng.xml" overwrite="true"
       file="${tests.dir}/loci/ome/utests/testng.xml"/>
@@ -21,6 +21,24 @@ Type "ant -p" for a list of targets.
         <pathelement location="${test-classes.dir}"/>
         <pathelement location="${classes.dir}"/>
         <pathelement path="${component.classpath}"/>
+      </classpath>
+      <xmlfileset file="${build.dir}/testng.xml"/>
+      <jvmarg value="-mx${testng.memory}"/>
+    </testng>
+    <fail if="failedTest"/>
+  </target>
+
+  <target name="test-no-omeio" depends="compile-tests" description="run missing OME I/O tests" if="doTests">
+    <echo>${test-classes.dir}</echo>
+    <echo>${classes.dir}</echo>
+    <echo>${component.classpath-no-omeio}</echo>
+    <copy tofile="${build.dir}/testng.xml" overwrite="true"
+      file="${tests.dir}/loci/ome/utests/testng-no-ome-io.xml"/>
+    <testng failureProperty="failedTest">
+      <classpath>
+        <pathelement location="${test-classes.dir}"/>
+        <pathelement location="${classes.dir}"/>
+        <pathelement path="${component.classpath-no-omeio}"/>
       </classpath>
       <xmlfileset file="${build.dir}/testng.xml"/>
       <jvmarg value="-mx${testng.memory}"/>

src/loci/ome/io/services/OMEReaderWriterServiceImpl.java
@@ -30,6 +30,8 @@ import loci.ome.io.OMEReader;
 import loci.ome.io.OMEWriter;
 import loci.ome.io.OmeroReader;
 
+import org.openmicroscopy.ds.dto.Image;
+
 /**
  * @author callan
  *
@@ -48,7 +50,7 @@ public class OMEReaderWriterServiceImpl extends AbstractService
     checkClassDependency(OMEWriter.class);
     checkClassDependency(OmeroReader.class);
     checkClassDependency(omero.model.Image.class);
-    checkClassDependency(org.openmicroscopy.ds.dto.Image.class);
+    checkClassDependency(Image.class);
   }
 
   /* (non-Javadoc)