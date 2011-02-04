d0a4e6126744e8f3d3269650b2ae12b034ec0b49
jburel <j.burel@dundee.ac.uk>
2011-02-04 12:06:42 +0000

    Fix visualisation problem of the original metadata file (close #4232)

--- components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/editor/AcquisitionDataUI.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/editor/AcquisitionDataUI.java
@@ -581,6 +581,11 @@ class AcquisitionDataUI
 	/** Lays out the companion files if any. */
 	void layoutCompanionFiles()
 	{
+		boolean b = model.getOriginalMetadata() == null ||
+			!MetadataViewerAgent.isBinaryAvailable();
+		originalMetadataPane.setVisible(!b);
+		if (b) 
+			originalMetadataPane.setCollapsed(true);
 		Collection list = model.getCompanionFiles();
 		if (list == null || list.size() == 0) {
 			companionFilesPane.setVisible(false);
