eace0a3c801e25d6b5123c0c0b1ff66c359b10ab
jmoore <josh@glencoesoftware.com>
2011-02-21 12:01:42 +0000

    Moving OmeroJava package-less tests to unit/ (Fix #4454)

--- components/tools/OmeroJava/test/unit/ModelTest.java
+++ components/tools/OmeroJava/test/unit/ModelTest.java
@@ -4,6 +4,7 @@
  *   Copyright 2007 Glencoe Software, Inc. All rights reserved.
  *   Use is subject to license terms supplied in LICENSE.txt
  */
+package unit;
 
 import java.util.HashMap;
 import java.util.Map;
--- components/tools/OmeroJava/test/unit/ParametersTest.java
+++ components/tools/OmeroJava/test/unit/ParametersTest.java
@@ -4,6 +4,7 @@
  *   Copyright 2008 Glencoe Software, Inc. All rights reserved.
  *   Use is subject to license terms supplied in LICENSE.txt
  */
+package unit;
 
 import static omero.rtypes.rbool;
 import static omero.rtypes.rint;
