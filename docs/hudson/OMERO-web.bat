1c9455b413403970eb425329d567f51d956167f0
Aleksandra Tarkowska <aleksandrat@lifesci.dundee.ac.uk>
2011-02-24 16:48:37 +0000

    minor bug fixes, see #2990

--- docs/hudson/OMERO-web.bat
+++ docs/hudson/OMERO-web.bat
@@ -5,7 +5,12 @@ python build.py clean
 if errorlevel 1 goto ERROR
 python build.py
 if errorlevel 1 goto ERROR
-python dist\bin\omero config set omero.web.server_list '[["%OMERO_HOST%", %OMERO_PREFIX%4064, "omero"]]'
+if %OMERO_PREFIX%==0 (
+set webport=4064
+) else (
+set webport=%OMERO_PREFIX%4064
+)
+python dist\bin\omero config set omero.web.server_list '[["%OMERO_HOST%", %webport%, "omero"]]'
 if errorlevel 1 goto ERROR
 python dist\bin\omero web unittest --config=$ICE_CONFIG --test=webadmin
 if errorlevel 1 goto ERROR
--- docs/hudson/OMERO-web.sh
+++ docs/hudson/OMERO-web.sh
@@ -8,5 +8,11 @@ set -x
 ./build.py clean
 ./build.py
 
-python dist/bin/omero config set omero.web.server_list '[["'$OMERO_HOST'", '$OMERO_PREFIX'4064, "omero"]]'
+if [ $OMERO_PREFIX = 0 ]; then
+    WEBPORT='4064'
+else
+    WEBPORT=$OMERO_PREFIX'4064'
+fi
+
+python dist/bin/omero config set omero.web.server_list '[["'$OMERO_HOST'", '$WEBPORT', "omero"]]'
 python dist/bin/omero web unittest --config=$ICE_CONFIG --test=webadmin
