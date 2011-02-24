eb7a32e3813de6f9aa770bb9d622f7e00e31b38f
Aleksandra Tarkowska <aleksandrat@lifesci.dundee.ac.uk>
2011-02-24 11:05:09 +0000

    minor bug fixed, see #2990

--- docs/hudson/OMERO-web.sh
+++ docs/hudson/OMERO-web.sh
@@ -6,8 +6,7 @@ set -x
 # Run tests
 #
 ./build.py clean
-
-python dist/bin/omero config set omero.web.server_list '[["$OMERO_HOST", $OMERO_PREFIX4064, "omero"]]'
-
 ./build.py
+
+python dist/bin/omero config set omero.web.server_list '[["'$OMERO_HOST'", '$OMERO_PREFIX'4064, "omero"]]'
 python dist/bin/omero web unittest --config=$ICE_CONFIG --test=webadmin
