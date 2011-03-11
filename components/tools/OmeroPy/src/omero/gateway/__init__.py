68c7bafd162fc74621dfdfe047032cc893fa668b
Will Moore <will@lifesci.dundee.ac.uk>
2011-03-11 15:09:48 +0000

    Add BinningWrapper to blitz gateway to lazy load value. See #4609

--- components/tools/OmeroPy/src/omero/gateway/__init__.py
+++ components/tools/OmeroPy/src/omero/gateway/__init__.py
@@ -6264,7 +6264,7 @@ class _DetectorSettingsWrapper (BlitzObjectWrapper):
               'gain',
               'offsetValue',
               'readOutRate',
-              'binning',
+              'binning|BinningWrapper',
               'detector|DetectorWrapper',
               'version')
 
@@ -6272,6 +6272,22 @@ class _DetectorSettingsWrapper (BlitzObjectWrapper):
         self.OMERO_CLASS = 'DetectorSettings'
 
 DetectorSettingsWrapper = _DetectorSettingsWrapper
+
+class _BinningWrapper (BlitzObjectWrapper):
+    """
+    omero_model_BinningI class wrapper extends BlitzObjectWrapper.
+    """
+
+    def __bstrap__ (self):
+        self.OMERO_CLASS = 'Binning'
+
+    def getValue(self):
+        if self._obj is not None:
+            if not self._obj.loaded:
+                self._obj = conn.getQueryService().get(self.OMERO_CLASS, self._obj.id.val)
+            return self._obj.getValue().getValue()
+
+BinningWrapper = _BinningWrapper
 
 class _DetectorWrapper (BlitzObjectWrapper):
     """
