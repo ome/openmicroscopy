79eabfb97ec6cc442b715b6135e6aab1709431f1
jmoore <josh@glencoesoftware.com>
2011-03-15 13:06:53 +0100

    s/$ICE_CONFIG/%ICE_CONFIG%/ in OMERO-web.bat (See #2990, #1736)

--- docs/hudson/OMERO-web.bat
+++ docs/hudson/OMERO-web.bat
@@ -27,7 +27,7 @@ python dist\bin\omero config set omero.web.server_list "[[""%OMERO_HOST%"", %web
 if errorlevel 1 goto ERROR
 python dist\bin\omero config set omero.web.debug True
 if errorlevel 1 goto ERROR
-python dist\bin\omero web unittest --config=$ICE_CONFIG --test=webadmin
+python dist\bin\omero web unittest --config=%ICE_CONFIG% --test=webadmin
 if errorlevel 1 goto ERROR
 
 REM
