27ac352ce590e6c6d9c71fd65f6a3eadaf67859b
Will Moore <will@lifesci.dundee.ac.uk>
2011-03-23 16:59:36 +0000

    gatewaytest for getAnnotationLinks(). Closes #4680

--- components/tools/OmeroPy/test/gatewaytest/get_objects.py
+++ components/tools/OmeroPy/test/gatewaytest/get_objects.py
@@ -117,7 +117,11 @@ class GetObjectTest (lib.GTest):
     def testGetAnnotations(self):
         
         obj = self.TESTIMG
-        ns = "omero.gateway.test_wrapper.test_get_annotations"
+        dataset = self.getTestDataset()
+        ns = "omero.gateway.test.get_objects.test_get_annotations_comment"
+        ns_tag = "omero.gateway.test.get_objects.test_get_annotations_tag"
+        
+        self.loginAsAuthor()
         
         # create Comment
         ann = omero.gateway.CommentAnnotationWrapper(self.gateway)
@@ -127,9 +131,10 @@ class GetObjectTest (lib.GTest):
         ann = obj.getAnnotation(ns)
         # create Tag
         tag = omero.gateway.TagAnnotationWrapper(self.gateway)
-        tag.setNs(ns)
+        tag.setNs(ns_tag)
         tag.setValue("Test Tag")
         tag = obj.linkAnnotation(tag)
+        dataset.linkAnnotation(tag)
         
         # get the Comment 
         a = self.gateway.getAnnotation(ann.id)
@@ -144,9 +149,63 @@ class GetObjectTest (lib.GTest):
         annGen = self.gateway.getObjects("Annotation", [ann.id, tag.id])
         anns = list(annGen)
         self.assertEqual(len(anns), 2)
-        self.assertEqual(anns[0].ns, ns)
-        self.assertEqual(anns[1].ns, ns)
+        self.assertTrue(anns[0].ns in [ns, ns_tag])
+        self.assertTrue(anns[1].ns in [ns, ns_tag])
         self.assertNotEqual(anns[0].OMERO_TYPE, anns[1].OMERO_TYPE)
+        
+        
+        # get all available annotation links on the image
+        annLinks = self.gateway.getAnnotationLinks("Image")
+        for al in annLinks:
+            self.assertTrue(al.getAnnotation(), omero.gateway.AnnotationWrapper)
+            self.assertTrue(al.getParent().__class__ == omero.model.ImageI)
+            
+        # get selected links - On image only
+        annLinks = self.gateway.getAnnotationLinks("Image", parent_ids=[obj.getId()])
+        for al in annLinks:
+            self.assertEqual(obj.getId(), al.parent.id.val)
+            self.assertTrue(al.getParent().__class__ == omero.model.ImageI)
+            
+        # get selected links - On image only
+        annLinks = self.gateway.getAnnotationLinks("Image", parent_ids=[obj.getId()])
+        for al in annLinks:
+            self.assertEqual(obj.getId(), al.parent.id.val)
+            self.assertTrue(al.getParent().__class__ == omero.model.ImageI)
+            
+        # params limit query by owner
+        params = omero.sys.Parameters()
+        params.theFilter = omero.sys.Filter()
+        # should be no links owned by root (in the current group)
+        params.theFilter.ownerId = omero.rtypes.rlong(0) # owned by 'root'
+        annLinks = self.gateway.getAnnotationLinks("Image", parent_ids=[obj.getId()], params=params)
+        self.assertEqual(len( list(annLinks)), 0, "No annotations on this image by root")
+        # links owned by author
+        eid = self.gateway.getEventContext().userId
+        params.theFilter.ownerId = omero.rtypes.rlong(eid) # owned by 'author'
+        omeName = self.gateway.getObject("Experimenter", eid).getName()
+        annLinks = self.gateway.getAnnotationLinks("Image", parent_ids=[obj.getId()], params=params)
+        for al in annLinks:
+            self.assertEqual(al.getOwnerOmeName(), omeName)
+            
+        # all links on Image with specific ns
+        annLinks = self.gateway.getAnnotationLinks("Image", ns=ns)
+        for al in annLinks:
+            self.assertEqual(al.getAnnotation().ns, ns)
+            
+        # get all uses of the Tag - have to check various types separately
+        annList = list( self.gateway.getAnnotationLinks("Image", ann_ids=[tag.id]) )
+        self.assertEqual(len(annList), 1)
+        for al in annList:
+            self.assertEqual(al.getAnnotation().id, tag.id)
+        annList = list( self.gateway.getAnnotationLinks("Dataset", ann_ids=[tag.id]) )
+        self.assertEqual(len(annList), 1)
+        for al in annList:
+            self.assertEqual(al.getAnnotation().id, tag.id)
+            
+        # remove annotations
+        obj.removeAnnotations(ns)
+        dataset.unlinkAnnotations(ns_tag)  # unlink tag
+        obj.removeAnnotations(ns_tag)      # delete tag
         
         
     def testGetImage (self):
