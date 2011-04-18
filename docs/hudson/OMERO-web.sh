a5b1a8454f4e8651c599b493ed46201da4468299
Aleksandra Tarkowska <aleksandrat@lifesci.dundee.ac.uk>
2011-04-18 15:32:09 +0100

    incorrect port for omeroweb

--- docs/hudson/OMERO-web.sh
+++ docs/hudson/OMERO-web.sh
@@ -12,21 +12,27 @@ test -e dist/bin/omero && \
 ./build.py
 
 if [ $OMERO_PREFIX = 0 ]; then
-    WEBPORT='4064'
+    ROUTER='4064'
 else
-    WEBPORT=$OMERO_PREFIX'4064'
+    ROUTER=$OMERO_PREFIX'4064'
+fi
+
+if [ $OMERO_PREFIX = 0 ]; then
+    WEBPORT='4080'
+else
+    WEBPORT=$OMERO_PREFIX'4080'
 fi
 
 #
 # Create a user
 #
 
-python dist/bin/omero login -s $OMERO_HOST -p $WEBPORT -u root -w ome
+python dist/bin/omero login -s $OMERO_HOST -p $ROUTER -u root -w ome
 python dist/bin/omero group add web_group --perms=rwrw-- || echo "Web Group already exists?"
 python dist/bin/omero user add web_user Web User web_group --userpassword abc || echo "Web User already exists?"
 python dist/bin/omero logout
 
-python dist/bin/omero config set omero.web.server_list '[["'$OMERO_HOST'", '$WEBPORT', "omero"]]'
+python dist/bin/omero config set omero.web.server_list '[["'$OMERO_HOST'", '$ROUTER', "omero"]]'
 python dist/bin/omero config set omero.web.debug True
 python dist/bin/omero web unittest --config=$ICE_CONFIG --test=webadmin
 
