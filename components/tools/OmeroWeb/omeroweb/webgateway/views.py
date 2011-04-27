cf817451212e539a6e303261925184fd422f9741
Will Moore <will@lifesci.dundee.ac.uk>
2011-04-27 23:08:16 +0100

    Replacing z_count() t_count() and c_count() with getSizeZ() etc in webgateway. See #4928

--- components/tools/OmeroWeb/omeroweb/webgateway/views.py
+++ components/tools/OmeroWeb/omeroweb/webgateway/views.py
@@ -825,9 +825,9 @@ def render_movie (request, iid, axis, pos, server_id=None, _conn=None, **kwargs)
         else:
             fn = fpath #os.path.join(fpath, img.getName())
         if axis.lower() == 'z':
-            dext, mimetype = img.createMovie(fn, 0, img.z_count()-1, pos-1, pos-1, opts)
+            dext, mimetype = img.createMovie(fn, 0, img.getSizeZ()-1, pos-1, pos-1, opts)
         else:
-            dext, mimetype = img.createMovie(fn, pos-1, pos-1, 0, img.t_count()-1, opts)
+            dext, mimetype = img.createMovie(fn, pos-1, pos-1, 0, img.getSizeT()-1, opts)
         if fpath is None:
             movie = open(fn).read()
             os.close(fo)
@@ -1055,9 +1055,9 @@ def imageMarshal (image, key=None):
             'id': image.id,
             'size': {'width': image.getWidth(),
                      'height': image.getHeight(),
-                     'z': image.z_count(),
-                     't': image.t_count(),
-                     'c': image.c_count(),},
+                     'z': image.getSizeZ(),
+                     't': image.getSizeT(),
+                     'c': image.getSizeC(),},
             'pixel_size': {'x': image.getPixelSizeX(),
                            'y': image.getPixelSizeY(),
                            'z': image.getPixelSizeZ(),},
@@ -1362,7 +1362,7 @@ def list_compatible_imgs_json (request, server_id, iid, _conn=None, **kwargs):
             imgs.extend(ds.listChildren())
         # Filter the ones that would pass the applySettingsToImages call
         img_ptype = img.getPrimaryPixels().getPixelsType().getValue()
-        img_ccount = img.c_count()
+        img_ccount = img.getSizeC()
         img_ew = [x.getLabel() for x in img.getChannels()]
         img_ew.sort()
         def compat (i):
@@ -1371,7 +1371,7 @@ def list_compatible_imgs_json (request, server_id, iid, _conn=None, **kwargs):
             pp = i.getPrimaryPixels()
             if pp is None or \
                i.getPrimaryPixels().getPixelsType().getValue() != img_ptype or \
-               i.c_count() != img_ccount:
+               i.getSizeC() != img_ccount:
                 return False
             ew = [x.getLabel() for x in i.getChannels()]
             ew.sort()
