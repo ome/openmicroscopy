7dc5d904d35cf098b45f5876ebb0df543c2d4218
jburel <j.burel@dundee.ac.uk>
2011-04-29 10:44:18 +0100

    Fix edit icon to be in synch with Web (close #4788).

--- components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/IconManager.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/IconManager.java
@@ -474,7 +474,7 @@ public class IconManager
         relPaths[PLUS_9] = "plus.png";
         relPaths[MINUS_9] = "minus.png";
         relPaths[EDIT_8] = "nuvola_ksig8.png";
-        relPaths[EDIT_12] = "nuvola_ksig11.png";
+        relPaths[EDIT_12] = "color_line-thin12.png";//nuvola_ksig11.png";
         relPaths[EDIT_48] = "nuvola_ksig48.png";
         relPaths[PLUS_12] = "plus11.png";
         relPaths[MINUS_12] = "minus11.png";
