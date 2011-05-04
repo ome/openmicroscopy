9dd056b12f2cd4d41e14201b3463b400d5db5843
jburel <j.burel@dundee.ac.uk>
2011-05-04 10:54:34 +0100

    Display tiles when OpenGL flag is on and improve the display of the scale bar (close #5107).

--- components/insight/SRC/org/openmicroscopy/shoola/agents/imviewer/browser/BrowserCanvas.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/imviewer/browser/BrowserCanvas.java
@@ -27,7 +27,19 @@ package org.openmicroscopy.shoola.agents.imviewer.browser;
 //Java imports
 
 //Third-party libraries
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.util.Map;
+
+import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
+
+import org.openmicroscopy.shoola.env.rnd.data.Region;
+import org.openmicroscopy.shoola.env.rnd.data.Tile;
+
+import com.sun.opengl.util.texture.TextureCoords;
+import com.sun.opengl.util.texture.TextureData;
+import com.sun.opengl.util.texture.TextureIO;
 
 //Application-internal dependencies
 
@@ -67,7 +79,78 @@ class BrowserCanvas
      */
     public void display(GLAutoDrawable drawable) 
 	{
-    	onDisplay(drawable, model.getRenderedImageAsTexture());
+    	if (model.isBigImage()) {
+    		GL gl = drawable.getGL();
+    		// Clear The Screen And The Depth Buffer
+    		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+    		gl.glTexEnvi(GL.GL_TEXTURE_ENV, GL.GL_TEXTURE_ENV_MODE,
+    				GL.GL_REPLACE);
+    		Map<Integer, Tile> tiles = model.getTiles();
+        	int rows = model.getRows();
+        	int columns = model.getColumns();
+        	Tile tile;
+        	int index;
+        	Object img;
+            Region r;
+            TextureCoords coords = new TextureCoords(0f, 0f, 1f, 1f);
+			Color c = ImageCanvas.BACKGROUND;
+    		float xStart, yStart, xEnd = 0, yEnd;
+    		drawScaleBar(gl, model.getTiledImageSizeX(), 
+    				model.getTiledImageSizeY());
+        	for (int i = 0; i < rows; i++) {
+    			for (int j = 0; j < columns; j++) {
+    				index = i*columns+j;
+    				tile = tiles.get(index);
+    				r = tile.getRegion();
+    				img = tile.getImage();
+    				xStart = (float) r.getX()/(r.getWidth()*columns);
+    				xEnd = 
+    					(float) (r.getX()+r.getWidth())/(r.getWidth()*columns);
+    				yStart = (float) r.getY()/(r.getHeight()*rows);
+    				yEnd = 
+    					(float) (r.getY()+r.getHeight())/(r.getHeight()*rows);
+    				if (img != null) {
+    					if (texture == null) 
+    						texture = TextureIO.newTexture((TextureData) img);
+    					else texture.updateImage((TextureData) img);
+    					texture.enable();
+    					texture.bind();
+    					gl.glBegin(GL.GL_QUADS);
+    					gl.glTexCoord2f(coords.left(), coords.bottom());
+    					gl.glVertex3f(xStart, yStart, 0);
+    					gl.glTexCoord2f(coords.right(), coords.bottom());
+    					gl.glVertex3d(xEnd, yStart, 0);
+    					gl.glTexCoord2f(coords.right(), coords.top());
+    					gl.glVertex3f(xEnd, yEnd, 0);
+    					gl.glTexCoord2f(coords.left(), coords.top());
+    					gl.glVertex3f(xStart, yEnd, 0);
+    					gl.glEnd();
+    					
+    				} else { //draw the grid.
+    					gl.glColor3f(c.getRed()/255f, c.getGreen()/255f,
+    							c.getBlue()/255f);
+    					gl.glBegin(GL.GL_LINES);
+    					gl.glVertex3f(xStart, yStart, 0.0f); 
+    					gl.glVertex3f(xEnd, yStart, 0.0f); 
+    					
+    					gl.glVertex3f(xStart, yStart, 0.0f); 
+    					gl.glVertex3f(xStart, yEnd, 0.0f);
+    					
+    					gl.glVertex3f(xStart, yEnd, 0.0f); 
+    					gl.glVertex3f(xEnd, yEnd, 0.0f);
+    					
+    					gl.glVertex3f(xEnd, yStart, 0.0f); 
+    					gl.glVertex3f(xEnd, yEnd, 0.0f);
+    					gl.glEnd();
+    				}
+    			}
+    		}
+        	if (texture != null) texture.disable();
+        	
+        	gl.glFlush();
+    	} else {//image
+    		onDisplay(drawable, model.getRenderedImageAsTexture());
+    	}
 	}
     
 }
