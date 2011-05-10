f1d577986b06f0cf6d967c4d0d7ccc0b5a1ca5ed
jmoore <josh@glencoesoftware.com>
2011-05-10 16:24:08 +0200

    Add missing 'is None' for previous commit

--- components/tools/OmeroPy/src/omero/cli.py
+++ components/tools/OmeroPy/src/omero/cli.py
@@ -577,7 +577,7 @@ class BaseControl:
         to return all properties.
         """
         import Ice
-        if getattr(self, "_props", None):
+        if getattr(self, "_props", None) is None:
             self._props = Ice.createProperties()
             for cfg in self._cfglist():
                 try:
