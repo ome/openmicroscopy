baf3945874ca4d3e1ee725887cdf6497d047813b
Carlos Neves <carlos@glencoesoftware.com>
2011-05-24 21:23:09 +0100

    Adding id to well in plateview.js, adding thumbLoad event too

--- components/tools/OmeroWeb/omeroweb/webgateway/media/js/weblitz-plateview.js
+++ components/tools/OmeroWeb/omeroweb/webgateway/media/js/weblitz-plateview.js
@@ -70,10 +70,13 @@ jQuery._WeblitzPlateview = function (container, options) {
               if (data.grid[i][j] == null) {
 		  tr.append('<td><div class="placeholder"></div></td>');
 	      } else {
-		  var td = $('<td><div class="waiting"></div><img class="loading" src="'+ data.grid[i][j].thumb_url+'"></td>');
+		  var td = $('<td id="'+thisid+'-well-'+data.grid[i][j].wellId+'"><div class="waiting"></div><img class="loading" src="'+ data.grid[i][j].thumb_url+'"></td>');
 		  $('img', td)
 		      .click(tclick(data.grid[i][j]))
-		      .load(function() { $(this).removeClass('loading').siblings().remove(); })
+		      .load(function() { 
+                                $(this).removeClass('loading').siblings().remove();
+                          _this.self.trigger('thumbLoad', [$(this).parent(), $(this)]);
+                           })
 		      .data('wellpos', data.rowlabels[i] + data.collabels[j]);
 		  tr.append(td);
 		  _this.self.trigger('thumbNew', [data.grid[i][j], $('img', td)]);
