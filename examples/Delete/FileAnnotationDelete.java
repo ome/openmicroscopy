fd3b60a19f9ddb602a4f5ef8e8f48aff03c6f368
jmoore <josh@glencoesoftware.com>
2011-05-27 08:47:32 +0200

    OmeroExamples : Adding FileAnnotationDelete example

---
+++ examples/Delete/FileAnnotationDelete.java
@@ -0,0 +1,69 @@
+import omero.LockTimeout;
+import omero.ServerError;
+import omero.api.IDeletePrx;
+import omero.api.ServiceFactoryPrx;
+import omero.api.delete.DeleteCommand;
+import omero.api.delete.DeleteHandlePrx;
+import omero.api.delete.DeleteReport;
+import omero.grid.DeleteCallbackI;
+import omero.model.*;
+import static omero.rtypes.*;
+import Glacier2.CannotCreateSessionException;
+import Glacier2.PermissionDeniedException;
+
+/**
+ * Uses the default {@link DeleteCallbackI} instance
+ * to delete a FileAnnotation along with its associated
+ * OriginalFile and any annotation links.
+ */
+public class FileAnnotationDelete {
+
+    public static void main(String[] args) throws CannotCreateSessionException,
+            PermissionDeniedException, ServerError, java.io.IOException {
+
+        omero.client c = new omero.client();
+
+        try {
+            ServiceFactoryPrx s = c.createSession();
+
+            Dataset d = new DatasetI();
+            d.setName(rstring("FileAnnotationDelete"));
+            FileAnnotation fa = new FileAnnotationI();
+            OriginalFile file = c.upload(new java.io.File("FileAnnotationDelete.java"));
+            fa.setFile(file);
+            d.linkAnnotation(fa);
+            d = (Dataset) s.getUpdateService().saveAndReturnObject(d);
+            fa = (FileAnnotation) d.linkedAnnotationList().get(0);
+
+
+            IDeletePrx deleteServicePrx = s.getDeleteService();
+            DeleteCommand dc = new DeleteCommand("/Annotation", fa.getId().getValue(), null);
+            DeleteHandlePrx deleteHandlePrx = deleteServicePrx
+                    .queueDelete(new DeleteCommand[] { dc });
+            DeleteCallbackI cb = new DeleteCallbackI(c, deleteHandlePrx);
+            try {
+
+                cb.loop(10, 500);
+
+                DeleteReport[] reports = deleteHandlePrx.report();
+                DeleteReport r = reports[0]; // We only sent one command
+                System.out.println(String.format(
+                        "Report:error=%s,warning=%s,deleted=%s", r.error,
+                        r.warning, r.actualDeletes));
+
+            } catch (LockTimeout lt) {
+                System.out.println("Not finished in 5 seconds. Cancelling...");
+                if (!deleteHandlePrx.cancel()) {
+                    System.out.println("ERROR: Failed to cancel");
+                }
+            } finally {
+                cb.close();
+            }
+
+        } finally {
+            c.closeSession();
+        }
+
+    }
+
+}
---
+++ examples/Delete/FileAnnotationDelete.py
@@ -0,0 +1,49 @@
+"""
+Uses the default {@link DeleteCallbackI} instance.
+to delete a FileAnnotation along with its associated
+OriginalFile and any annotation links.
+"""
+
+import omero
+import omero.callbacks
+
+c = omero.client()
+
+from omero.rtypes import *
+from omero.model import *
+
+try:
+    s = c.createSession()
+    d = DatasetI()
+    d.setName(rstring("FileAnnotationDelete"))
+    fa = FileAnnotationI()
+    file = c.upload("FileAnnotationDelete.java")
+    fa.setFile(file)
+    d.linkAnnotation(fa)
+    d = s.getUpdateService().saveAndReturnObject(d)
+    fa = d.linkedAnnotationList()[0]
+
+    deleteServicePrx = s.getDeleteService();
+    dc = omero.api.delete.DeleteCommand("/Annotation", fa.id.val, None)
+    deleteHandlePrx = deleteServicePrx .queueDelete([dc])
+    cb = omero.callbacks.DeleteCallbackI(c, deleteHandlePrx)
+
+    try:
+
+        try:
+            cb.loop(10, 500)
+        except omero.LockTimeout:
+            print "Not finished in 5 seconds. Cancelling..."
+            if not deleteHandlePrx.cancel():
+                print "ERROR: Failed to cancel"
+
+        reports = deleteHandlePrx.report()
+        r = reports[0] # We only sent one command
+        print "Report:error=%s,warning=%s,deleted=%s" % \
+            (r.error, r.warning, r.actualDeletes)
+
+    finally:
+        cb.close()
+
+finally:
+    c.closeSession()
