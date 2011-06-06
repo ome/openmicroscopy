7012c682ab0774ade7a2a9f96e7abef938ecd9fa
jburel <j.burel@dundee.ac.uk>
2011-06-06 16:17:25 +0100

    Comment out code so insight is compiling

--- components/insight/SRC/org/openmicroscopy/shoola/agents/util/flim/resultstable/ResultsTableView.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/util/flim/resultstable/ResultsTableView.java
@@ -30,7 +30,7 @@ import javax.swing.table.TableCellRenderer;
 import org.jdesktop.swingx.JXTable;
 
 //Application-internal dependencies
-import org.openmicroscopy.shoola.agents.util.flim.util.ResultsCellRenderer;
+//import org.openmicroscopy.shoola.agents.util.flim.util.ResultsCellRenderer;
 
 /**
  * The View of the results table.
@@ -69,9 +69,11 @@ extends JXTable
 	 * Overridden to return a customized cell renderer.
 	 * @see JXTable#getCellRenderer(int, int)
 	 */
+	/*
 	public TableCellRenderer getCellRenderer(int row, int column) 
 	{
         return new ResultsCellRenderer(mod);
     }
+    */
 }
 
