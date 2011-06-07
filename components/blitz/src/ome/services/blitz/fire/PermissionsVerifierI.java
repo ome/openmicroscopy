4ea06983d312c06a5e1c61175afac01884a31b0c
jmoore <josh@glencoesoftware.com>
2011-06-07 11:31:56 +0200

    Improving `HasPassword` logic of `PermissionsVerifierI` (See #2212, Fix #3652)

--- components/blitz/src/ome/services/blitz/fire/PermissionsVerifierI.java
+++ components/blitz/src/ome/services/blitz/fire/PermissionsVerifierI.java
@@ -43,24 +43,66 @@ public class PermissionsVerifierI extends _PermissionsVerifierDisp {
     public boolean checkPermissions(String userId, String password,
             StringHolder reason, Current __current) {
 
-        boolean value = false;
         try {
-            // ticket:2212 - At the very first, check if the password is
+            // ticket:2212, ticket:3652
+            //
+            // At the very first, check if the userId or password is
             // actually a session id, if so we enforce that the userId
             // and the password are identical, becauser we have no other
             // method of signalling to the SessionManagerI instance that
             // the user has not provided a real password
-            Session session;
+            //
+            // The addition of the userId check was added from an odd
+            // case in which root added a username equal to the current
+            // session. This prevents any LDAP lookup etc. from happening,
+            // but by "giving" the user a session id (passed via the username)
+            // s/he **will** be able to login **AS ROOT**!
+            //
+
+            Object session = null;
+
+            // Local checks are faster.
             try {
                 session = manager.find(password);
             } catch (SessionException e) {
-                session = null;
+                // pass
             }
 
+            if (session == null) {
+                try {
+                    session = manager.find(userId); // ticket:3652
+                } catch (SessionException e) {
+                    // pass
+                }
+            }
+
+            // If that doesn't work, make sure that the cluster doesn't know
+            // something this instance doesn't. (Default of nullRedirector
+            // returns null immediately)
+            if (session == null) {
+                if (ring.checkPassword(userId)) {
+                    session = "ring.checkPassword(userId)";
+                }
+            }
+
+            if (session == null) {
+                if (ring.checkPassword(password)) {
+                    session = "ring.checkPassword(password)";
+                }
+            }
+
+
+            // If any of the above blocks returned a valid value
+            // then the password and/or userId matches an active
+            // session. As long as userId == password, we return
+            // true. Otherwise we return false, otherwise it
+            // would be possible to circumvent the @HasPassword
+            // restrictions.
             if (session != null) {
                 if (userId.equals(password)) {
                     return true;
                 } else {
+                    log.warn("username and password don't match: " + userId);
                     reason.value = "username and password must be equal; use joinSession";
                     return false;
                 }
@@ -69,16 +111,11 @@ public class PermissionsVerifierI extends _PermissionsVerifierDisp {
             // First check locally. Since we typically use redirects in the
             // cluster, it's most likely that our password will be in memory
             // in this instance.
-            value = manager.executePasswordCheck(userId, password);
-            
-            // If that doesn't work, make sure that the cluster doesn't know
-            // something this instance doesn't.
-            if (!value) {
-                value = ring.checkPassword(userId);
-            }
-
-            if (!value) {
+            if (manager.executePasswordCheck(userId, password)) {
+                return true;
+            } else {
                 reason.value = "Password check failed";
+                return false;
             }
 
         } catch (Throwable t) {
@@ -86,8 +123,8 @@ public class PermissionsVerifierI extends _PermissionsVerifierDisp {
                     + t.getMessage();
             log.error("Exception thrown while checking password for:" + userId,
                     t);
+            return false;
         }
-        return value;
     }
 
 }
