58b4443f32f0cb90f13b10ccfc849d4f6bb8f8da
jmoore <josh@glencoesoftware.com>
2011-06-10 16:09:13 +0200

    More blitz fixes including lib/licenses/README items

--- components/blitz/src/ome/services/blitz/impl/OmeroMetadata.java
+++ components/blitz/src/ome/services/blitz/impl/OmeroMetadata.java
@@ -3,7 +3,7 @@
  *
  *-----------------------------------------------------------------------------
  *
- *    Copyright (C) 2009 Glencoe Software, Inc.
+ *    Copyright (C) 2009-2011 Glencoe Software, Inc. All rights reserved.
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
