80adf4a20a2ba7af9a369240b6d81ea5aedd70e7
Will Moore <will@lifesci.dundee.ac.uk>
2011-06-23 17:02:03 +0100

    Updating Edit_Descriptions.py as part of 4.3 documentation. See #4785

--- examples/ScriptingService/Edit_Descriptions.py
+++ examples/ScriptingService/Edit_Descriptions.py
@@ -2,7 +2,7 @@
  examples/ScriptingService/Edit_Descriptions.py
 
 -----------------------------------------------------------------------------
-  Copyright (C) 2006-2010 University of Dundee. All rights reserved.
+  Copyright (C) 2006-2011 University of Dundee. All rights reserved.
 
 
   This program is free software; you can redistribute it and/or modify
@@ -20,10 +20,14 @@
 
 ------------------------------------------------------------------------------
 
-This script demonstrates the minimum framework of a script that can be run by the 
-scripting service on an OMERO server. 
-It defines a name, description and parameter list for the script. 
-    
+This script demonstrates a number of features that must be included in OMERO scripts,
+as well as some that improve the usability etc.
+ * Definition of script name, description and parameters
+ * Inputs designed to be auto-populated based on currently selected objects in OMERO.insight client
+ * Outputs designed to display a useful message and provide link to Image in Insight (Datasets handled too)
+ * Using print for logging to stdout
+
+
 @author  Will Moore &nbsp;&nbsp;&nbsp;&nbsp;
 <a href="mailto:will@lifesci.dundee.ac.uk">will@lifesci.dundee.ac.uk</a>
 @version 3.0
@@ -38,73 +42,107 @@ import omero
 from omero.gateway import BlitzGateway
 from omero.rtypes import *
 import omero.scripts as scripts
-
-import time
-startTime = 0
-
-def printDuration():
-    global startTime
-    if startTime == 0:
-        startTime = time.time()
-    print "script time = %s secs" % (time.time() - startTime)
     
-def editDescriptions(conn, parameterMap):
+def editDescriptions(conn, scriptParams):
     """
-    Does the main work of the script, setting Description for each Image in a Dataset
+    Does the main work of the script, setting Description for each Image
     
-    @param conn             Blitz Gateway connection wrapper
-    @param parameterMap     A map of the input parameters
+    @param conn:             Blitz Gateway connection wrapper
+    @param scriptParams:     A map of the input parameters
     """
-    # we know parameterMap will have "Dataset_ID" since this parameter is not optional
-    datasetId = parameterMap["Dataset_ID"]
-    
+    # we know scriptParams will have "Data_Type" and "IDs" since these parameters are not optional
+    dataType = scriptParams["Data_Type"]
+    ids = scriptParams["IDs"]
+
+    # dataType is 'Dataset' or 'Image' so we can use it directly in getObjects()
+    obs = conn.getObjects(dataType, ids)    # generator of images or datasets
+    objects = list(obs)
+
+    if len(objects) == 0:
+        print "No %ss found for specified IDs" % dataType
+        return
+
+    images = []
+
+    if dataType == 'Dataset':
+        for ds in objects:
+            print "Processing Images from Dataset: %s" % ds.getName()
+            imgs = list(ds.listChildren())
+            images.extend(imgs)
+    else:
+        print "Processing Images identified by ID"
+        images = objects
+
     # for optional parameters - need to test if present
     newDescription = "No description specified"  
-    if "New_Description" in parameterMap:
-        newDescription = parameterMap["New_Description"]
+    if "New_Description" in scriptParams:
+        newDescription = scriptParams["New_Description"]
 
-    dataset = conn.getObject('Dataset', datasetId)
-    print "Dataset:", dataset.getName()
+    print "Editing images with this description: \n%s\n" % newDescription
 
-    for image in dataset.listChildren():
-        image.setDescription(newDescription)
-        image.save()
+    editedImgIds = []     # keep track of what we've edited, to provide feedback message
+    for i in images:
+        print "   Editing image ID: %d Name: %s" % (i.id, i.name)
+        i.setDescription(newDescription)
+        i.save()
+        editedImgIds.append(i.id)
         
     # use this in Output_Message
-    return dataset.getName()
+    return editedImgIds
 
 
 if __name__ == "__main__":
     """
     The main entry point of the script, as called by the client via the scripting service, passing the required parameters. 
     """
-    printDuration()
-    client = scripts.client('Edit_Descriptions.py', 'Edits the descriptions for all the images in a given Dataset.', 
-    scripts.Long("Dataset_ID", optional=False, description="Specifiy the Dataset by ID"),
-    scripts.String("New_Description", description="The new description to set for each Image in the Dataset"),
+
+    dataTypes = [rstring('Dataset'),rstring('Image')]
+
+    # Here we define the script name and description.
+    # Good practice to put url here to give users more guidance on how to run your script.
+    client = scripts.client('Edit_Descriptions.py', """Edits the descriptions of multiple Images,
+either specified via Image IDs or by the Dataset IDs.
+See http://trac.openmicroscopy.org.uk/ome/wiki/OmeroPy/ScriptingServiceGuide for the tutorial that uses this script.""",
+
+    scripts.String("Data_Type", optional=False, grouping="1",
+        description="The data you want to work with.", values=dataTypes, default="Dataset"),
+
+    scripts.List("IDs", optional=False, grouping="2",
+        description="List of Dataset IDs or Image IDs").ofType(rlong(0)),
+
+    scripts.String("New_Description", grouping="3",
+        description="The new description to set for each Image in the Dataset"),
     )
-    
-    session = client.getSession()
-    
+
     try:
         # process the list of args above. Not scrictly necessary, but useful for more complex scripts
-        parameterMap = {}
+        scriptParams = {}
         for key in client.getInputKeys():
             if client.getInput(key):
-                parameterMap[key] = client.getInput(key, unwrap=True) # unwrap rtypes to String, Integer etc
+                scriptParams[key] = client.getInput(key, unwrap=True) # unwrap rtypes to String, Integer etc
+
+        print scriptParams    # handy to have inputs in the std-out log
 
         # wrap client to use the Blitz Gateway
         conn = BlitzGateway(client_obj=client)
 
-        # do the editing and handle the result
-        datasetName = editDescriptions(conn, parameterMap)
-        if datasetName:
-            ouputMessage = "Script Ran OK on Dataset: %s" % datasetName
+        # do the editing...
+        editedImgIds = editDescriptions(conn, scriptParams)
+
+        # now handle the result, displaying message and returning image if appropriate
+        if editedImgIds is None:
+            message = "Script failed. See 'error' or 'info' for more details"
         else:
-            ouputMessage = "Script failed. See error message"
-        
-        client.setOutput("Message", rstring(ouputMessage))
+            if len(editedImgIds) == 1:
+                img = conn.getObject("Image", editedImgIds[0])      # image-wrapper
+                message = "One Image edited: %s" % img.getName()
+                omeroImage = img._obj                               # omero.model object
+                client.setOutput("Edited Image", robject(omeroImage))  # Insight will display 'View' link to image
+            elif len(editedImgIds) > 1:
+                message = "%s Images edited" % len(editedImgIds)
+            else:
+                message = "No images edited. See 'error' or 'info' for more details"
+
+        client.setOutput("Message", rstring(message))    # Insight will display the 'Message' parameter
     finally:
         client.closeSession()
-        printDuration()
-    
