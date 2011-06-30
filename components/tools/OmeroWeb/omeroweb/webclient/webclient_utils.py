f0d726aef66dc9f091aff14c8d3210042524c6ed
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2011-06-30 09:41:47 +0100

    this replace error/warning server messages by the appropriate string, see #6003

--- components/tools/OmeroWeb/omeroweb/webclient/webclient_utils.py
+++ components/tools/OmeroWeb/omeroweb/webclient/webclient_utils.py
@@ -27,13 +27,10 @@ def _formatReport(delete_handle):
     Added as workaround to the changes made in #3006.
     """
     delete_reports = delete_handle.report()
-    rv = []
     for report in delete_reports:
-        if report.error:
-            rv.append(report.error)
-        elif report.warning:
-            rv.append(report.warning)
-    return "; ".join(rv)
+        if report.error or report.warning:
+            return "Operation could not be completed successfully"
+
     # Might want to take advantage of other feedback here
 
 def _purgeCallback(request):
