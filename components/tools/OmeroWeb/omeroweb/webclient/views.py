b099bfaac11d96c1869dbbb451775054e5a74817
Will Moore <will@lifesci.dundee.ac.uk>
2011-07-20 23:36:20 +0100

    Allow OAV to switch between 'compressed' and 'raw' maps. Closes #6291

--- components/tools/OmeroWeb/omeroweb/webclient/views.py
+++ components/tools/OmeroWeb/omeroweb/webclient/views.py
@@ -848,11 +848,10 @@ def open_astex_viewer(request, obj_type, obj_id, **kwargs):
         return handlerInternalError("Connection is not available. Please contact your administrator.")
     
     # can only populate these for 'image'
+    imageId = None
+    data_storage_mode = None
     pixelRange = None       # (min, max) values of the raw data
     # If we convert to 8bit map, subtract dataOffset, multiply by mapPixelFactor add mapOffset. (used for js contour controls)
-    dataOffset = 0
-    mapPixelFactor = 1
-    mapOffset = 0
     if obj_type == 'file':
         ann = conn.getObject("Annotation", obj_id)
         if ann is None:
@@ -868,21 +867,21 @@ def open_astex_viewer(request, obj_type, obj_id, **kwargs):
         image = conn.getObject("Image", obj_id)     # just check the image exists
         if image is None:
             return handlerInternalError("Can't find image ID %s as data source for Open Astex Viewer." % obj_id)
+        imageId = obj_id
         imageName = image.getName()
         c = image.getChannels()[0]
         pixelRange = (c.getWindowMin(), c.getWindowMax())
         if obj_type == 'image_8bit':
-            dataOffset = c.getWindowMin()
-            mapPixelFactor = 255.0 / (c.getWindowMax() - c.getWindowMin())
-            mapOffset = -127
+            data_storage_mode = 1
             data_url = reverse("webclient_image_as_map_8bit", args=[obj_id])
         else:
+            data_storage_mode = 2
             data_url = reverse("webclient_image_as_map", args=[obj_id])
 
-    return render_to_response('webclient/annotations/open_astex_viewer.html', {'data_url': data_url, "imageName":imageName,
-        'pixelRange':pixelRange, "dataOffset": dataOffset, "mapPixelFactor":mapPixelFactor, "mapOffset": mapOffset})
-    
-    
+    return render_to_response('webclient/annotations/open_astex_viewer.html', {'data_url': data_url,
+        "imageName":imageName, "imageId": imageId, "data_storage_mode": data_storage_mode,'pixelRange':pixelRange})
+
+
 @isUserConnected
 def load_metadata_details(request, c_type, c_id, share_id=None, **kwargs):
     conn = None
