Fixed contour bug in OAV, contour sliders and 8bit scaling. Closes #6273

components/tools/OmeroWeb/omeroweb/webclient/views.py
@@ -847,7 +847,12 @@ def open_astex_viewer(request, obj_type, obj_id, **kwargs):
         logger.error(traceback.format_exc())
         return handlerInternalError("Connection is not available. Please contact your administrator.")
     
-    pixelRange = None  # can only populate this for 'image'
+    # can only populate these for 'image'
+    pixelRange = None       # (min, max) values of the raw data
+    # If we convert to 8bit map, subtract dataOffset, multiply by mapPixelFactor add mapOffset. (used for js contour controls)
+    dataOffset = 0
+    mapPixelFactor = 1
+    mapOffset = 0
     if obj_type == 'file':
         ann = conn.getObject("Annotation", obj_id)
         if ann is None:
@@ -867,12 +872,15 @@ def open_astex_viewer(request, obj_type, obj_id, **kwargs):
         c = image.getChannels()[0]
         pixelRange = (c.getWindowMin(), c.getWindowMax())
         if obj_type == 'image_8bit':
+            dataOffset = c.getWindowMin()
+            mapPixelFactor = 255.0 / (c.getWindowMax() - c.getWindowMin())
+            mapOffset = -127
             data_url = reverse("webclient_image_as_map_8bit", args=[obj_id])
         else:
             data_url = reverse("webclient_image_as_map", args=[obj_id])
 
-    return render_to_response('webclient/annotations/open_astex_viewer.html', 
-            {'data_url': data_url, 'pixelRange':pixelRange, "imageName":imageName})
+    return render_to_response('webclient/annotations/open_astex_viewer.html', {'data_url': data_url, "imageName":imageName,
+        'pixelRange':pixelRange, "dataOffset": dataOffset, "mapPixelFactor":mapPixelFactor, "mapOffset": mapOffset})
     
     
 @isUserConnected
@@ -1763,7 +1771,7 @@ def image_as_map(request, imageId, **kwargs):
     """ Converts OMERO image into mrc.map file (using tiltpicker utils) and returns the file """
 
     from tiltpicker.pyami import mrc
-    from numpy import dstack, zeros, uint8
+    from numpy import dstack, zeros, int8
 
     conn = None
     try:
@@ -1790,8 +1798,8 @@ def image_as_map(request, imageId, **kwargs):
     if npStack.dtype.name == 'uint16' or ('8bit' in kwargs and kwargs['8bit']):
         #scale from 0 - 255 and conver to 8 bit integer
         npStack = npStack - npStack.min()  # start at 0
-        npStack = (npStack / npStack.max()) * 256  # range 0 - 255
-        a = zeros(npStack.shape, dtype=uint8)
+        npStack = (npStack * 255 / npStack.max()) - 127 # range - 127 -> 128
+        a = zeros(npStack.shape, dtype=int8)
         npStack = npStack.round(out=a)
 
     # if available, use scipy.ndimage to resize