bdc2d0bc71358170d35ead9e589165986117d5d9
Chris Allan <callan@blackcat.ca>
2011-07-25 14:58:00 +0100

    Cleanup useless import.

--- components/blitz/src/ome/formats/importer/ImportLibrary.java
+++ components/blitz/src/ome/formats/importer/ImportLibrary.java
@@ -24,7 +24,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
