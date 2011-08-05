9332cf321de1cfb852a5941c0a5f39725ebeab30
Simon Wells <szwells@dundee.ac.uk>
2011-08-05 15:52:14 +0100

    Wrap code in omerovm.sh to stop it being executed if there is no network address (Fix #6431)

--- docs/install/VM/omerovm.sh
+++ docs/install/VM/omerovm.sh
@@ -76,54 +76,55 @@ $VBOX list vms | grep "$VMNAME" || {
 }
 
 $VBOX list runningvms | grep "$VMNAME" || {
-    echo "Starting VM..."
-    $VBOX startvm "$VMNAME" --type headless
-    echo "Give the VM time to boot..."
-    sleep 30
+    echo "Starting VM... first boot... give the VM time to boot..."
+    $VBOX startvm "$VMNAME" --type headless && sleep 45
 }
 
-echo "Cleaning up any old OMERO.VM keys"
-ssh-keygen -R [localhost]:2222 -f ~/.ssh/known_hosts
-rm -f omerokey omerokey.pub
+$VBOX guestproperty enumerate $VMNAME | grep "10.0.2.15" && {
+	
+	ssh-keygen -R [localhost]:2222 -f ~/.ssh/known_hosts
+	rm -f omerokey omerokey.pub
+	ssh-keygen -t dsa -f omerokey -N ''
 
-echo "Generat a new DSA keypair"
-ssh-keygen -t dsa -f omerokey -N ''
-cp omerokey ~/.ssh/omerokey
-cp omerokey.pub ~/.ssh/omerokey.pub
+	echo "Setting omerokey permissions"
+	chmod 600 ./omerokey
 
-echo "Enforce correct permissions for key"
-chmod 600 ./omerokey
-chmod 600 ~/.ssh/omerokey*
+	SCP="scp -2 -o NoHostAuthenticationForLocalhost=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o CheckHostIP=no -o PasswordAuthentication=no -o ChallengeResponseAuthentication=no -o PreferredAuthentications=publickey -i omerokey -P $SSH_PF"
 
-SCP="scp -2 -v -o NoHostAuthenticationForLocalhost=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o CheckHostIP=no -o PasswordAuthentication=no -o ChallengeResponseAuthentication=no -o PreferredAuthentications=publickey -i omerokey -P $SSH_PF"
-SSH="ssh -2 -v -o StrictHostKeyChecking=no -i omerokey -p $SSH_PF -t"
+	SSH="ssh -2 -o StrictHostKeyChecking=no -i omerokey -p $SSH_PF -t"
 
-SCP_K="scp -v -o StrictHostKeyChecking=no -o NoHostAuthenticationForLocalhost=yes -P $SSH_PF"
-SSH_K="ssh -v -o StrictHostKeyChecking=no -o NoHostAuthenticationForLocalhost=yes -p $SSH_PF -t"
+	SCP_K="scp -o StrictHostKeyChecking=no -o NoHostAuthenticationForLocalhost=yes -P $SSH_PF"
+	SSH_K="ssh -o StrictHostKeyChecking=no -p $SSH_PF -t"
 
 
-[ -f omerokey.pub ] && {    
-    echo "Copying DSA key to VM"
+[ -f omerokey.pub ] && {
+	echo "Copying my DSA key"
 	expect -c "spawn $SCP_K omerokey.pub omero@localhost:~/; expect \"*?assword:*\"; send \"omero\n\r\"; interact"
 	expect -c "spawn $SCP_K setup_keys.sh omero@localhost:~/; expect \"*?assword:*\"; send \"omero\n\r\"; interact"
 
-	echo "Execute the key setup script"
+	echo "Setup key"
 	expect -c "spawn $SSH_K omero@localhost sh /home/omero/setup_keys.sh; expect \"*?assword:*\"; send \"omero\n\r\"; interact"
-} || echo "Local DSAAuthentication key was not found. Use: $ ssh-keygen -t dsa"
 
-echo "Copying scripts to VM"
-$SCP driver.sh omero@localhost:~/
-$SCP setup_userspace.sh omero@localhost:~/
-$SCP setup_environment.sh omero@localhost:~/
-$SCP setup_omero.sh omero@localhost:~/
-$SCP omero-init.d omero@localhost:~/
+	} || echo "Local DSAAuthentication key was not found. Use: $ ssh-keygen -t dsa"
 
-echo "ssh : exec driver.sh"
-$SSH omero@localhost 'sh /home/omero/driver.sh'
 
-sleep 40
+	echo "Copying scripts to VM"
+	$SCP driver.sh omero@localhost:~/
+	$SCP setup_userspace.sh omero@localhost:~/
+	$SCP setup_postgres.sh omero@localhost:~/
+	$SCP setup_environment.sh omero@localhost:~/
+	$SCP setup_omero.sh omero@localhost:~/
+	$SCP setup_omero_daemon.sh omero@localhost:~/
+	$SCP omero-init.d omero@localhost:~/
 
-echo "ALL DONE!"
-echo "Connect to your OMERO VM using either OMERO.insight or another OMERO client or SSH using the connect.sh script"
-echo "Your VM has the following IP addresses:"
-VBoxManage guestproperty enumerate $VMNAME | grep IP
+	echo "ssh : exec driver.sh"
+	$SSH omero@localhost 'bash /home/omero/driver.sh'
+
+	sleep 60
+	
+	echo "ALL DONE!"
+	echo "Connect to your OMERO VM using either OMERO.insight or another OMERO client or SSH using the connect.sh script"
+	echo "Your VM has the following IP addresses:"
+	VBoxManage guestproperty enumerate $VMNAME | grep IP
+
+} || "VM has no network connection (VirtualBox error http://www.virtualbox.org/ticket/4038). Rerun omerovm.sh"
