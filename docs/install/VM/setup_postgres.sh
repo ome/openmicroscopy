055591b7a6f48bfe540e9ba1977bffa8557efc2a
Simon Wells <szwells@dundee.ac.uk>
2011-08-05 15:08:03 +0100

    Created setup_postgres.sh to increase modularity of the scripts & removed corresponding code from setup_userspace.sh (Fix #6425)

---
+++ docs/install/VM/setup_postgres.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e -u -x
+
+export PGPASSWORD=omero
+
+FILE=.pgpass
+sudo -u omero cat > ${FILE} << EOF
+localhost:5432:omero:omero:omero
+EOF
+chmod 600 .pgpass
+
+echo "CREATE USER omero PASSWORD 'omero'" | sudo -u postgres psql
+sudo -u postgres createdb -O omero omero
+sudo -u postgres createlang plpgsql omero
+
+echo `psql -h localhost -U omero -l`
+
+sudo sed '/127.0.0.1/s/md5/trust/' /etc/postgresql/8.4/main/pg_hba.conf > pg_hba.conf && sudo mv pg_hba.conf /etc/postgresql/8.4/main/pg_hba.conf
+
+sudo /etc/init.d/postgresql restart
+
+echo `netstat -an | egrep '5432.*LISTEN'`
