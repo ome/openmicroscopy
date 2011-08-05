4878c70925a6150c8c4139c13f3ea86fe9ada166
Simon Wells <szwells@dundee.ac.uk>
2011-08-05 15:25:44 +0100

    Refactor daemon setup into its own script: setup_omero_daemon.sh & call the functionality from driver.sh (Fix #6428)

--- docs/install/VM/driver.sh
+++ docs/install/VM/driver.sh
@@ -6,17 +6,13 @@ PASSWORD=${PASSWORD:-"omero"}
 
 # Set up stuff requiring su privileges
 echo $PASSWORD | sudo -S sh setup_userspace.sh
+echo $PASSWORD | sudo -S sh setup_postgres.sh
 
 # Set up everything else
 sudo -k
 
-sh setup_environment.sh
+bash setup_environment.sh
 bash setup_omero.sh
+bash setup_omero_daemon.sh
 
-
-# Everything is set up. Unleash the daemon
-echo $PASSWORD | sudo -S cp /home/omero/omero-init.d /etc/init.d/omero
-echo $PASSWORD | sudo -S chmod a+x /etc/init.d/omero
-echo $PASSWORD | sudo -S update-rc.d -f omero remove
-echo $PASSWORD | sudo -S update-rc.d -f omero defaults 98 02
 echo $PASSWORD | sudo -S reboot
