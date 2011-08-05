afcb3907027ff4aacfdcb552c3392bf9598edabe
Simon Wells <szwells@dundee.ac.uk>
2011-08-05 16:16:59 +0100

    misc cleanup

--- docs/install/VM/hudson.sh
+++
@@ -1,4 +0,0 @@
-URL=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-trunk-qa-builds/lastSuccessfulBuild/api/xml?xpath=/freeStyleBuild/url/text()"`
-FILE=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-trunk-qa-builds/lastSuccessfulBuild/api/xml?xpath=//relativePath[contains(.,'server')]/text()"`
-wget -q "$URL"artifact/$FILE
-echo $FILE
