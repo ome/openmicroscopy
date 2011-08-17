267804fc7661e75b29614b723804a292530d6f74
Simon Wells <szwells@dundee.ac.uk>
2011-08-17 14:34:44 +0100

    Added a script to export a named VM to a virtual appliance (Fix #6389)

---
+++ docs/install/VM/export_ova.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export VMNAME=${VMNAME:-"$1"}
+export VMNAME=${VMNAME:-"omerovm"}
+export EXPORTVMNAME=${EXPORTVMNAME:-"$2"}
+export EXPORTVMNAME=${EXPORTVMNAME:-"$VMNAME"}
+
+set -e
+set -u
+set -x
+
+VBOX="VBoxManage --nologo"
+
+($VBOX list runningvms | grep "$VMNAME") && {
+    echo "Stopping VM "
+    $VBOX controlvm "$VMNAME" poweroff && sleep 5
+}
+
+$VBOX export $VMNAME --output $EXPORTVMNAME.ova
