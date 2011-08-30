ff1c505fc4630db0085c74a953640f30e5da1ed5
Carlos Neves <carlos@glencoesoftware.com>
2011-08-30 03:00:14 +0100

    adding some extra information for the callbacks on weblitz-plateviewer

--- components/tools/OmeroWeb/omeroweb/webgateway/media/js/weblitz-plateview.js
+++ components/tools/OmeroWeb/omeroweb/webgateway/media/js/weblitz-plateview.js
@@ -74,6 +74,7 @@ jQuery._WeblitzPlateview = function (container, options) {
               if (data.grid[i][j] == null) {
 		  tr.append('<td style="width:'+opts.width+'px;height:'+opts.height+'px;line-height:'+opts.height+'px;"><div class="placeholder">&nbsp;</div></td>');
 	      } else {
+                  data.grid[i][j]._wellpos = data.rowlabels[i]+data.collabels[j];
 		  var td = $('<td class="well" style="width:'+opts.width+'px;height:'+opts.height+'px;" id="well-'+thisid+'-well-'+data.grid[i][j].wellId+'"><div class="waiting"></div><img id="image-'+data.grid[i][j].id+'" class="loading" src="'+ data.grid[i][j].thumb_url+'"></td>');
 		  $('img', td)
 		      .click(tclick(data.grid[i][j]))
