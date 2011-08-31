57429e8a30b0187810abcd9742a98e57bb2979cf
Simon Wells <szwells@dundee.ac.uk>
2011-08-31 14:46:28 +0100

    Changed the execution of .bashrc to (1) use the source command & (2) not require set +e +u +x to avoid the error checking

--- docs/install/VM/setup_environment.sh
+++ docs/install/VM/setup_environment.sh
@@ -5,7 +5,4 @@ set -e -u -x
 echo 'export OMERO_SERVER=/home/omero/OMERO.server' >> .bashrc
 echo 'export PATH=$PATH:$OMERO_SERVER/bin' >> .bashrc
 
-# Ugly fix for bug in Debian related to "HISTCONTROL parameter not set" error when we source our amended .bashrc file
-# Turn off BASH script debugging whilst we update our environment vars to avoid premature exityup
-set +e +u +x
-. /home/omero/.bashrc
+(source /home/omero/.bashrc) || true
