a3d95a5fa90274a11f5bf743e20f0dd51d81a985
Simon Wells <szwells@dundee.ac.uk>
2011-08-31 14:44:35 +0100

    Fix to path in driver.sh so that bash can find omero/bin

--- docs/install/VM/driver.sh
+++ docs/install/VM/driver.sh
@@ -4,6 +4,9 @@ set -e -u -x
 
 PASSWORD=${PASSWORD:-"omero"}
 
+OMERO_PATH="/home/omero/OMERO.server"
+OMERO_BIN=$OMERO_PATH/bin
+
 # Set up stuff requiring su privileges
 echo $PASSWORD | sudo -S sh setup_userspace.sh
 echo $PASSWORD | sudo -S sh setup_postgres.sh
@@ -15,7 +18,7 @@ bash setup_environment.sh
 bash setup_omero.sh
 bash setup_omero_daemon.sh
 
-omero admin start
+$OMERO_BIN/omero admin start
 
 echo $PASSWORD | sudo -S halt
 #echo $PASSWORD | sudo -S reboot
