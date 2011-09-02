7e695fdc6b89280b01910062a1a986c32ff3aa74
Simon Wells <szwells@dundee.ac.uk>
2011-09-02 16:08:25 +0100

    Fix for #6474 allowing build to target either QA or Release

--- docs/install/VM/setup_omero.sh
+++ docs/install/VM/setup_omero.sh
@@ -3,57 +3,49 @@
 set -e -u -x
 
 PGPASSWORD=${PGPASSWORD:-"omero"}
-
-DL_ARCHIVE=""
-if [ "x$DL_ARCHIVE" == "x" ]; then
-
-	URL=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-trunk-qa-builds/lastSuccessfulBuild/api/xml?xpath=/freeStyleBuild/url/text()"`
-	FILE=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-trunk-qa-builds/lastSuccessfulBuild/api/xml?xpath=//relativePath[contains(.,'server')]/text()"`
-	wget -q "$URL"artifact/$FILE
-
-    #DL_ARCHIVE=`sh hudson.sh`
-    DL_ARCHIVE=$FILE
-
-    DL_FOLDER=${DL_ARCHIVE%.zip}
-else
-    DL_LOC="http://hudson.openmicroscopy.org.uk/job/OMERO-trunk-qa-builds/lastSuccessfulBuild/artifact/"
-    DL_FOLDER=${DL_ARCHIVE%.zip}
-
-#	echo "Grabbing OMERO.server src"
-#	if test -e OMERO.server; then
-#		pushd OMERO.server
-#		if test -e dist; then
-#			dist/bin/omero admin stop
-#		fi
-#		git pull git://git.openmicroscopy.org/ome.git
-#		./build.py clean
-#		popd
-#	else
-#		git clone git://git.openmicroscopy.org/ome.git OMERO.server
-#	fi
-#	    pushd OMERO.server
-#	    python build.py
-#	    popd
-
-	echo "Grabbing QA Build of OMERO.server"
-	wget $DL_LOC$DL_ARCHIVE
-fi
-
+TARGET=${TARGET:-"QA"} # NB. Valid args are {QA | RELEASE}
+URL_RELEASE="http://cvs.openmicroscopy.org.uk/snapshots/omero/"
+RELEASE_ARCHIVE="OMERO.server-Beta-4.3.1.zip"
+RELEASE_FOLDER=${RELEASE_ARCHIVE%.zip}
 DB_VERSION="OMERO4.3"
 DB_REVISION="0"
 OMERO_PATH="/home/omero/OMERO.server"
 OMERO_BIN=$OMERO_PATH/bin
+INSTALL_FOLDER="OMERO.server"
 
-unzip $DL_ARCHIVE
-mv $DL_FOLDER OMERO.server
+if [[ "$TARGET" == "RELEASE" ]]; then
+	echo "Grabbing current stable release of OMERO.server: $RELEASE_ARCHIVE"
+	wget -q $URL_RELEASE$RELEASE_ARCHIVE
+	unzip $RELEASE_ARCHIVE
+	mv $RELEASE_FOLDER $INSTALL_FOLDER
+else
+	echo "Grabbing last successful QA Build of OMERO.server"
+    DL_ARCHIVE=""
+    if [ "x$DL_ARCHIVE" == "x" ]; then
+
+    	URL=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-trunk-qa-builds/lastSuccessfulBuild/api/xml?xpath=/freeStyleBuild/url/text()"`
+    	FILE=`wget -q -O- "http://hudson.openmicroscopy.org.uk/job/OMERO-trunk-qa-builds/lastSuccessfulBuild/api/xml?xpath=//relativePath[contains(.,'server')]/text()"`
+
+    	wget -q "$URL"artifact/$FILE
+
+        DL_ARCHIVE=$FILE
+    	DL_FOLDER=${DL_ARCHIVE%.zip}
+    else
+        DL_LOC="http://hudson.openmicroscopy.org.uk/job/OMERO-trunk-qa-builds/lastSuccessfulBuild/artifact/"
+        DL_FOLDER=${DL_ARCHIVE%.zip}
+    
+    	wget $DL_LOC$DL_ARCHIVE
+    fi
+    unzip $DL_ARCHIVE
+    mv $DL_FOLDER $INSTALL_FOLDER
+fi
 
 mkdir OMERO.data
-$OMERO_BIN/omero config set omero.data.dir /home/omero/OMERO.data
 
+$OMERO_BIN/omero config set omero.data.dir /home/omero/OMERO.data
 $OMERO_BIN/omero config set omero.db.name 'omero'
 $OMERO_BIN/omero config set omero.db.user 'omero'
 $OMERO_BIN/omero config set omero.db.pass 'omero'
-
 $OMERO_BIN/omero db script -f db.sql $DB_VERSION $DB_REVISION $PGPASSWORD
 
 psql -h localhost -U omero omero < db.sql
