41eadc38fecf10a2f8fcb256e51d7594b1e190d4
Aleksandra Tarkowska <atarkowska@lifesci.dundee.ac.uk>
2011-09-27 11:47:07 +0100

    connection improvement

--- examples/Training/python/Connecting.py
+++ examples/Training/python/Connecting.py
@@ -31,52 +31,55 @@ HOST = 'localhost'
 PORT = 4064
 USERNAME = 'ola'
 PASSWORD = 'ome'
-conn = BlitzGateway(USERNAME, PASSWORD, host=HOST, port=PORT)
-connected = conn.connect()
-# check if you are connected.
-if not connected:
-    import sys
-    sys.stderr.write("Error: Connection not available, please check your user name and password.\n")
-    sys.exit(1)
 
-# Using secure connection.
+if __name__ == '__main__':
+    
+    conn = BlitzGateway(USERNAME, PASSWORD, host=HOST, port=PORT)
+    connected = conn.connect()
+    # check if you are connected.
+    if not connected:
+        import sys
+        sys.stderr.write("Error: Connection not available, please check your user name and password.\n")
+        sys.exit(1)
 
-# By default, once we have logged in, data transfer is not encrypted (faster)
+    # Using secure connection.
 
-# To use secure connection
-conn.setSecure(True)
+    # By default, once we have logged in, data transfer is not encrypted (faster)
 
-# Current session details
+    # To use secure connection
+    conn.setSecure(True)
 
-# By default, you will have logged into your 'current' group in OMERO. This can be changed by
-# switching group in the OMERO insight or web clients.
+    # Current session details
 
-user = conn.getUser()
-print "Current user:"
-print "   ID:", user.getId()
-print "   Username:", user.getName()
-print "   Full Name:", user.getFullName()
+    # By default, you will have logged into your 'current' group in OMERO. This can be changed by
+    # switching group in the OMERO insight or web clients.
 
-print "Member of:"
-for g in conn.getGroupsMemberOf():
-    print "   ID:", g.getName(), " Name:", g.getId()
-group = conn.getGroupFromContext()
-print "Current group: ", group.getName()
+    user = conn.getUser()
+    print "Current user:"
+    print "   ID:", user.getId()
+    print "   Username:", user.getName()
+    print "   Full Name:", user.getFullName()
 
-print "Other Members of current group:"
-for g in conn.listColleagues():
-    print "   ID:", g.getId(), " Name:", g.getFullName()
+    print "Member of:"
+    for g in conn.getGroupsMemberOf():
+        print "   ID:", g.getName(), " Name:", g.getId()
+    group = conn.getGroupFromContext()
+    print "Current group: ", group.getName()
 
-print "Owner of:"
-for g in conn.listOwnedGroups():
-    print "   ID:", g.getName(), " Name:", g.getId()
+    print "Other Members of current group:"
+    for g in conn.listColleagues():
+        print "   ID:", g.getId(), " Name:", g.getFullName()
 
-# The 'context' of our current session
-ctx = conn.getEventContext()
-# print ctx     # for more info
+    print "Owner of:"
+    for g in conn.listOwnedGroups():
+        print "   ID:", g.getName(), " Name:", g.getId()
 
-# Close connection
+    # The 'context' of our current session
+    ctx = conn.getEventContext()
+    # print ctx     # for more info
 
-# When you're done, close the session to free up server resources. 
+    # Close connection
 
-conn._closeSession()
+    # When you're done, close the session to free up server resources. 
+
+    conn._closeSession()
