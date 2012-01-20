2eab4dbc6e24a11f14b27e1ceee68ad9312f5fc2
jburel <j.burel@dundee.ac.uk>
2012-01-20 10:01:54 +0000

    Initial revamping of the server access (see #7810)

---
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/util/SecurityContext.java
@@ -0,0 +1,90 @@
+/*
+ * org.openmicroscopy.shoola.env.data.util.SecurityContext 
+ *
+ *------------------------------------------------------------------------------
+ *  Copyright (C) 2006-2012 University of Dundee & Open Microscopy Environment.
+ *  All rights reserved.
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.env.data.util;
+
+//Java imports
+
+//Third-party libraries
+
+//Application-internal dependencies
+
+/** 
+ * Hosts information required to access correct connector.
+ *
+ * @author Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * <a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @since Beta4.4
+ */
+public class SecurityContext
+{
+
+	/** The identifier of the group.*/
+	private long groupID;
+	
+	/** The name of the server.*/
+	private String host;
+	
+	/** The port to use.*/
+	private int port;
+	
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param groupID The identifier of the group.
+	 */
+	public SecurityContext(long groupID)
+	{
+		this.groupID = groupID;
+	}
+	
+	/**
+	 * Sets the information used to connect to the correct server.
+	 * 
+	 * @param host The name of the server.
+	 * @param port The port to use.
+	 */
+	public void setServerInformation(String host, int port)
+	{
+		this.host = host;
+		this.port = port;
+	}
+	
+	/**
+	 * Sets the information used to connect to the correct server.
+	 * 
+	 * @param host The name of the server.
+	 */
+	public void setServerInformation(String host)
+	{
+		this.host = host;
+	}
+	
+	/**
+	 * Returns the identifier of the group.
+	 * 
+	 * @return See above.
+	 */
+	public long getGroupID() { return groupID; }
+	
+}
