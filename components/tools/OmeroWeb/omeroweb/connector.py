11d2e92842dd7ad41c5d66d25b21110ffe9ef7b0
Chris Allan <callan@blackcat.ca>
2012-02-02 21:43:15 +0000

    Initial version of new getBlitzConnection() logic.
    The previous getBlitzConnection() logic has now been fed in to the
    decorator and a new Connector class which encompasses many of the
    previous concepts getBlitzConnection() utilised.  The Connector now
    supercedes the previous, string encoded, "Blitz connector key" and will
    be stored in the Django session under the 'connector' key.  Furthermore,
    BlitzGateway instances are now created upon each request and an OMERO
    session either created or joined as a result.  This will need to be
    performance tested but it removes a lot of complexity and may actually
    be faster than the previous cached BlitzGateway strategy.

---
+++ components/tools/OmeroWeb/omeroweb/connector.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2011 University of Dundee & Open Microscopy Environment.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import logging
+
+from omero import client_wrapper
+
+logger = logging.getLogger(__name__)
+
+class Connector(object):
+    """
+    Object which encompasses all of the logic related to a Blitz connection
+    and its status with respect to OMERO.web.
+    """
+
+    def __init__(self, server_id, is_secure, useragent=useragent):
+        self.server_id = server_id
+        self.is_secure = is_secure
+        self.omero_session_key = None
+
+    def lookup_host_and_port(self):
+        # TODO: Needs to lookup the host and port in settings
+        return ('localhost', 4064)
+
+    def create_gateway(self, username=None, password=None):
+        host, port = self.lookup_host_and_port()
+        return client_wrapper(
+                username, password, host=host, port=port, secure=self.is_secure,
+                useragent=self.useragent)
+
+    def prepare_gateway(self, connection):
+        connection.server_id = server_id
+        # Lazy import due to the potential usage of the decorator in
+        # the omeroweb.webgateway.views package.
+        # TODO: UserProxy needs to be moved to this package or similar
+        from omeroweb.webgateway.views import UserProxy
+        connection.user = UserProxy(connection)
+        # TODO: Properly handle activating the weblitz_cache
+
+    def create_connection(self, username, password):
+        try:
+            connection = self.create_gateway(username, password)
+            connection.connect()
+            self.prepare_gateway(connection)
+        except:
+            logger.debug('Cannot create a new connection.', exc_info=True)
+            return None
+
+    def join_connection(self):
+        try:
+            connection = self.create_gateway()
+            connection.connect(sUuid=self.omero_session_key)
+            self.prepare_gateway(connection)
+        except:
+            logger.debug('Cannot create a new connection.', exc_info=True)
+            return None
+
