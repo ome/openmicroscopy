285dccf396e9f77fd93f8651e4f383e05558e840
Will Moore <will@lifesci.dundee.ac.uk>
2012-02-08 09:53:40 +0000

    Complete chgrp tests for Image

--- components/tools/OmeroPy/test/integration/chgrp.py
+++ components/tools/OmeroPy/test/integration/chgrp.py
@@ -12,20 +12,26 @@ import integration.library as lib
 import unittest
 from omero.rtypes import *
 from omero.cmd import Chgrp, State, ERR, OK
+from omero.callbacks import CmdCallbackI
+
+PRIVATE = 'rw----'
+READONLY = 'rwr---'
+COLLAB = 'rwrw--'
 
 class TestChgrp(lib.ITest):
-    
-    def doChange(self, chgrp, test_should_pass=True):
-        
-        sf = self.client.sf
 
+
+    def doChange(self, chgrp, client, test_should_pass=True):
+        """
+        Performs the change-group action, waits on completion and checks that the 
+        result is not an error.
+        """
+        sf = client.sf
         prx = sf.submit(chgrp)
 
         self.assertFalse(State.FAILURE in prx.getStatus().flags)
 
-        from omero.callbacks import CmdCallbackI 
-
-        cb = CmdCallbackI(self.client, prx)
+        cb = CmdCallbackI(client, prx)
         cb.loop(20, 500)
 
         self.assertNotEqual(prx.getResponse(), None)
@@ -44,22 +50,63 @@ class TestChgrp(lib.ITest):
         return rsp
 
 
-    def testImageChgrp(self):
+    def testChgrpImportedImage(self):
+        """
+        Tests chgrp for an imported image, moving to a collaborative group
+        """
+        # One user in two groups
+        client, exp = self.new_client_and_user()
+        grp = self.new_group(experimenters=[exp], perms=COLLAB)
+        gid = grp.id.val
+        client.sf.getAdminService().getEventContext() # Reset session
+        update = client.sf.getUpdateService()
+        query = client.sf.getQueryService()
 
-        sf = self.client.sf
-        pixID = self.import_image()[0]
-
-        pixels = sf.getQueryService().get("Pixels", pixID)
+        # Import an image into the client context
+        pixID = self.import_image(client=client)[0]
+        pixels = client.sf.getQueryService().get("Pixels", pixID)
         imageId = pixels.getImage().getId().getValue()
-        print "imageId", imageId
-        to_group = 1
-        uuid = sf.getAdminService().getEventContext().sessionUuid
 
-        chgrp = Chgrp(uuid, "/Image", imageId, None, to_group)
-        rsp = self.doChange(chgrp, True)
+        # Chgrp
+        chgrp = omero.cmd.Chgrp(type="/Image", id=imageId, options=None, grp=gid)
+        self.doChange(chgrp, client)
 
-        image = sf.getQueryService().get("Image", imageId)
-        self.asserEqual(image.getDetails().getGroup().getId().getValue(), to_group)
+        # Change our context to new group...
+        admin = client.sf.getAdminService()
+        admin.setDefaultGroup(exp, omero.model.ExperimenterGroupI(gid, False))
+        self.set_context(client, gid)
+        # ...check image
+        img = client.sf.getQueryService().get("Image", imageId)
+        self.assertEqual(img.details.group.id.val, gid)
+
+
+    def testChgrpImage(self):
+        """
+        Tests chgrp for a dummny image object (no Pixels)
+        """
+        # One user in two groups
+        client, exp = self.new_client_and_user()
+        grp = self.new_group([exp])
+        gid = grp.id.val
+        client.sf.getAdminService().getEventContext() # Reset session
+        update = client.sf.getUpdateService()
+        query = client.sf.getQueryService()
+
+        # Data Setup (image in the current group)
+        img = self.new_image()
+        img = update.saveAndReturnObject(img)
+
+        # Move image to new group
+        chgrp = omero.cmd.Chgrp(type="/Image", id=img.id.val, options=None, grp=gid)
+        self.doChange(chgrp, client)
+
+        # Change our context to new group...
+        admin = client.sf.getAdminService()
+        admin.setDefaultGroup(exp, omero.model.ExperimenterGroupI(gid, False))
+        self.set_context(client, gid)
+        # ...check image
+        img = client.sf.getQueryService().get("Image", img.id.val)
+        self.assertEqual(img.details.group.id.val, gid)
 
 
 if __name__ == '__main__':
