d520152ef9fc26bcfee273f487389aca6255b875
Chris Allan <callan@blackcat.ca>
2012-02-17 12:03:35 +0000

    Port of render_shape_thumbnail() to use the new decorators.

--- components/tools/OmeroWeb/omeroweb/webgateway/views.py
+++ components/tools/OmeroWeb/omeroweb/webgateway/views.py
@@ -645,15 +645,12 @@ def render_roi_thumbnail (request, roiId, w=None, h=None, conn=None, **kwargs):
 
     return get_shape_thumbnail (request, conn, image, s, compress_quality)
 
-
-def render_shape_thumbnail (request, shapeId, server_id=None, w=None, h=None, conn=None, **kwargs):
+@login_required()
+def render_shape_thumbnail (request, shapeId, w=None, h=None, conn=None, **kwargs):
     """
     For the given Shape, redner a region around that shape, scale to width and height (or default size) and draw the
     shape on to the region. 
     """
-    if conn is None:
-        conn = getBlitzConnection(request, server_id, useragent="OMERO.webgateway")
-
     # need to find the z indices of the first shape in T
     params = omero.sys.Parameters()
     params.map = {'id':rlong(shapeId)}
