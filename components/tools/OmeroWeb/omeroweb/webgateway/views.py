d1315a80e4663ea48d1f04bd8c4781d274252f39
Chris Allan <callan@blackcat.ca>
2012-02-17 14:48:04 +0000

    Port of search_json() to use the new decorators.

--- components/tools/OmeroWeb/omeroweb/webgateway/views.py
+++ components/tools/OmeroWeb/omeroweb/webgateway/views.py
@@ -1707,7 +1707,8 @@ def searchOptFromRequest (request):
 
 @TimeIt(logging.INFO)
 @jsonp
-def search_json (request, server_id=None, conn=None, **kwargs):
+@login_required()
+def search_json (request, conn=None, **kwargs):
     """
     Search for objects in blitz.
     Returns json encoded list of marshalled objects found by the search query
@@ -1722,11 +1723,11 @@ def search_json (request, server_id=None, conn=None, **kwargs):
         - parents:
     
     @param request:     http request
-    @param server_id:   
     @param conn:        L{omero.gateway.BlitzGateway}
     @return:            json search results
     TODO: cache
     """
+    server_id = request.session['connector'].server_id
     opts = searchOptFromRequest(request)
     rv = []
     logger.debug("searchObjects(%s)" % (opts['search']))
