442218d6b09e9ab2bd898610ade796970b459334
Will Moore <will@lifesci.dundee.ac.uk>
2012-02-21 10:17:21 +0000

    Big Fat webtest cleanup
    Removed all example code that subsequently got moved into webclient
    E.g. ROI and Big-image tests.
    Also metadata and viewport that are replicated in webclient.
    Fixed up a couple of bugs in remaining code.
    Still need a better webtest index page.

--- components/tools/OmeroWeb/omeroweb/webtest/urls.py
+++ components/tools/OmeroWeb/omeroweb/webtest/urls.py
@@ -7,45 +7,26 @@ import os
 
 urlpatterns = patterns('django.views.generic.simple',
 
-    # tell django where to find media files for webtest. From 'here' they are in /media/
-    url(r'^statictest/(?P<path>.*)$', serve, {'document_root': os.path.join(os.path.dirname(__file__), 'media')}, name="statictest"),
-
     # index 'home page' of the webtest app
     url( r'^$', views.index, name='webtest_index' ),
-
-    # login and logout: preference is to use webclient functionality for this, but these can be used as examples if needed
-    url( r'^login/$', views.login, name='webtest_login' ),
-    url( r'^logout/$', views.logout, name='webtest_logout' ),
-
-    # some of this functionality is duplicated from webclient or gateway as a stand-alone example
-    url( r'^metadata/(?P<iid>[0-9]+)/$', views.metadata, name='webtest_metadata' ),
-    url( r'^img_detail/(?P<iid>[0-9]+)/$', views.image_viewer, name="image_viewer"),
-    url( r'^viewport/$', 'direct_to_template', {'template': 'webtest/viewport.html'}, name="viewport" ),
 
     # 'Hello World' example from tutorial on http://trac.openmicroscopy.org.uk/ome/wiki/OmeroWeb
     url( r'^dataset/(?P<datasetId>[0-9]+)/$', views.dataset ),
 
-    # big image examples
-    url( r'^panojs/$', 'direct_to_template', {'template': 'webtest/bigimage/panojs.html'}, name="panojs" ),
-    url( r'^kasuari/$', 'direct_to_template', {'template': 'webtest/bigimage/kasuari.html'}, name="kasuari" ),
-    
-    # roi examples, testing various roi libraries for displaying ROIs. 
-    url( r'^roi_viewer/(?:(?P<roi_library>((?i)processing|jquery|raphael))/(?P<imageId>[0-9]+)/)?$', views.roi_viewer, name="webfigure_roi_viewer" ),
-    
     # Displays images (one per row) one channel per column in a grid. Params are passed in request, E.g. imageIds
     url( r'^split_view_figure/$', views.split_view_figure, name="webtest_split_view_figure"),
+
     # View a dataset as two panels of images, each with a different rendering setting
     url( r'^dataset_split_view/(?P<datasetId>[0-9]+)/', views.dataset_split_view, name='webtest_dataset_split_view' ),
-    
-    # view an image in grid with the Z, C, T dimensions split over the x or y axes as chosen by user. 
-    # Also displays SPIM data if available in the http://www.ome-xml.org/wiki/SPIM/InitialSupport format.
+
+    # view an image in grid with the Z, C, T dimensions split over the x or y axes as chosen by user.
     url( r'^image_dimensions/(?P<imageId>[0-9]+)/', views.image_dimensions, name='webtest_image_dimensions' ),
 
-    # overlay individual channels from the same image (or different images) and manipulate them separately..
+    # Viewer overlays individual channels from the same image (or different images) and manipulate them separately..
     # translate, scale etc relative to one-another.
-    url( r'^render_channel_overlay/', views.render_channel_overlay, name='webtest_render_channel_overlay' ),
-
     url( r'^channel_overlay_viewer/(?P<imageId>[0-9]+)/', views.channel_overlay_viewer, name='webtest_channel_overlay_viewer' ),
+    # this is the url for rendering planes for the viewer
+    url( r'^render_channel_overlay/', views.render_channel_overlay, name='webtest_render_channel_overlay' ),
 
     # post a comment annotation to images. parameters are in request: imageIds=123,234  comment=blah 
     # ns=Namespace replace=true (replaces existing comment with same ns if found)
--- components/tools/OmeroWeb/omeroweb/webtest/views.py
+++ components/tools/OmeroWeb/omeroweb/webtest/views.py
@@ -35,43 +35,6 @@ def dataset(request, datasetId, **kwargs):
     ds = conn.getObject("Dataset", datasetId)     # before OMERO 4.3 this was conn.getDataset(datasetId)
     return render_to_response('webtest/dataset.html', {'dataset': ds})    # generate html from template
 
-    
-def login (request):
-    """
-    Attempts to get a connection to the server by calling L{omeroweb.webgateway.views.getBlitzConnection} with the 'request'
-    object. If a connection is created, the user is directed to the 'webtest_index' page. 
-    If a connection is not created, this method returns a login page.
-    
-    @param request:     The django http request
-    @return:            The http response - webtest_index or login page
-    """
-    if request.method == 'POST' and request.REQUEST['server']:
-        blitz = Server.get(pk=request.REQUEST['server'])
-        request.session['server'] = blitz.id
-        request.session['host'] = blitz.host
-        request.session['port'] = blitz.port
-    
-    conn = getBlitzConnection (request, useragent="OMERO.webtest")
-    logger.debug(conn)
-    if conn is not None:
-        return HttpResponseRedirect(reverse('webtest_index'))
-    return render_to_response('webtest/login.html', {'gw':Server})
-
-
-def logout (request):
-    _session_logout(request, request.session['server'])
-    try:
-        del request.session['username']
-    except KeyError:
-        logger.error(traceback.format_exc())
-    try:
-        del request.session['password']
-    except KeyError:
-        logger.error(traceback.format_exc())
-    
-    #request.session.set_expiry(1)
-    return HttpResponseRedirect(reverse('webtest_index'))
-
 
 @isUserConnected    # wrapper handles login (or redirects to webclient login). Connection passed in **kwargs
 def index(request, **kwargs):
@@ -150,6 +113,8 @@ def render_channel_overlay (request, **kwargs):
     # E.g. planes=0|2305:7:0:0$x:-50_y:10,1|2305:7:1:0,2|2305:7:2:0&red=2&blue=0&green=1
     planes = {}
     p = request.REQUEST.get('planes', None)
+    if p is None:
+        return HttpResponse("Request needs plane info to render jpeg. E.g. ?planes=0|2305:7:0:0$x:-50_y:10,1|2305:7:1:0,2|2305:7:2:0&red=2&blue=0&green=1")
     for plane in p.split(','):
         infoMap = {}
         plane_info = plane.split('|')
@@ -199,6 +164,10 @@ def render_channel_overlay (request, **kwargs):
         img.setActiveChannels((planeInfo['c']+1,))
         img.setGreyscaleRenderingModel()
         rgb = img.renderImage(planeInfo['z'], planeInfo['t'])
+
+        # somehow this line is required to prevent an error at 'rgb.split()'
+        rgb.save(StringIO(), 'jpeg', quality=90)
+
         r,g,b = rgb.split()  # go from RGB to L
 
         x,y = 0,0
@@ -242,143 +211,6 @@ def render_channel_overlay (request, **kwargs):
 
     rsp = HttpResponse(jpeg_data, mimetype='image/jpeg')
     return rsp
-
-@isUserConnected
-def metadata (request, iid, **kwargs):
-    from omeroweb.webclient.forms import MetadataFilterForm, MetadataDetectorForm, MetadataChannelForm, \
-                        MetadataEnvironmentForm, MetadataObjectiveForm, MetadataStageLabelForm, \
-                        MetadataLightSourceForm, MetadataDichroicForm, MetadataMicroscopeForm
-                        
-    conn = kwargs['conn']
-    
-    form_environment = None
-    form_objective = None
-    form_microscope = None
-    form_stageLabel = None
-    form_filters = list()
-    form_detectors = list()
-    form_channels = list()
-    form_lasers = list()
-    
-    image = conn.getObject("Image", iid)
-    om = image.loadOriginalMetadata()
-    global_metadata = sorted(om[1])
-    series_metadata = sorted(om[2])
-        
-        
-    for ch in image.getChannels():
-        if ch.getLogicalChannel() is not None:
-            channel = dict()
-            channel['form'] = MetadataChannelForm(initial={'logicalChannel': ch.getLogicalChannel(), 
-                                    'illuminations': list(conn.getEnumerationEntries("IlluminationI")), 
-                                    'contrastMethods': list(conn.getEnumerationEntries("ContrastMethodI")), 
-                                    'modes': list(conn.getEnumerationEntries("AcquisitionModeI"))})
-            channel['form_emission_filters'] = list()
-            if ch.getLogicalChannel().getLightPath().getEmissionFilters():
-                for f in ch.getLogicalChannel().getLightPath().getEmissionFilters():
-                    channel['form_filters'].append(MetadataFilterForm(initial={'filter': f,
-                                    'types':list(conn.getEnumerationEntries("FilterTypeI"))}))
-            channel['form_excitation_filters'] = list()
-            if ch.getLogicalChannel().getLightPath().getExcitationFilters():
-                for f in ch.getLogicalChannel().getLightPath().getExcitationFilters():
-                    channel['form_excitation_filters'].append(MetadataFilterForm(initial={'filter': f,
-                                    'types':list(conn.getEnumerationEntries("FilterTypeI"))}))
-                                    
-            if ch.getLogicalChannel().getDetectorSettings()._obj is not None:
-                channel['form_detector_settings'] = MetadataDetectorForm(initial={'detectorSettings':ch.getLogicalChannel().getDetectorSettings(), 'detector': ch.getLogicalChannel().getDetectorSettings().getDetector(),
-                                    'types':list(conn.getEnumerationEntries("DetectorTypeI"))})
-            if ch.getLogicalChannel().getLightSourceSettings()._obj is not None:      
-                channel['form_light_source'] = MetadataLightSourceForm(initial={'lightSource': ch.getLogicalChannel().getLightSourceSettings(),
-                                    'types':list(conn.getEnumerationEntries("FilterTypeI")), 
-                                    'mediums': list(conn.getEnumerationEntries("LaserMediumI")),
-                                    'pulses': list(conn.getEnumerationEntries("PulseI"))})
-            if ch.getLogicalChannel().getFilterSet()._obj is not None and ch.getLogicalChannel().getFilterSet().getDichroic()._obj:
-                channel['form_dichroic'] = MetadataDichroicForm(initial={'logicalchannel': ch.getLogicalChannel().getFilterSet().getDichroic()})
-            channel['name'] = ch.getName()
-            channel['color'] = ch.getColor().getHtml()
-            form_channels.append(channel)
-            
-    if image.getObjectiveSettings() is not None:
-        form_objective = MetadataObjectiveForm(initial={'objectiveSettings': image.getObjectiveSettings(), 
-                                'mediums': list(conn.getEnumerationEntries("MediumI")), 
-                                'immersions': list(conn.getEnumerationEntries("ImmersionI")), 
-                                'corrections': list(conn.getEnumerationEntries("CorrectionI")) })
-    if image.getImagingEnvironment() is not None:
-        form_environment = MetadataEnvironmentForm(initial={'image': image})
-    if image.getStageLabel() is not None:
-        form_stageLabel = MetadataStageLabelForm(initial={'image': image })
-
-    if image.getInstrument() is not None:
-        if image.getInstrument().getMicroscope() is not None:
-            form_microscope = MetadataMicroscopeForm(initial={'microscopeTypes':list(conn.getEnumerationEntries("MicroscopeTypeI")), 'microscope': image.getInstrument().getMicroscope()})
-
-        if image.getInstrument().getFilters() is not None:
-            filters = list(image.getInstrument().getFilters())    
-            for f in filters:
-                form_filter = MetadataFilterForm(initial={'filter': f, 'types':list(conn.getEnumerationEntries("FilterTypeI"))})
-                form_filters.append(form_filter)
-    
-        if image.getInstrument().getDetectors() is not None:
-            detectors = list(image.getInstrument().getDetectors())    
-            for d in detectors:
-                form_detector = MetadataDetectorForm(initial={'detectorSettings':None, 'detector': d, 'types':list(conn.getEnumerationEntries("DetectorTypeI"))})
-                form_detectors.append(form_detector)
-    
-        if image.getInstrument().getLightSources() is not None:
-            lasers = list(image.getInstrument().getLightSources())
-            for l in lasers:
-                form_laser = MetadataLightSourceForm(initial={'lightSource': l, 
-                                'types':list(conn.getEnumerationEntries("FilterTypeI")), 
-                                'mediums': list(conn.getEnumerationEntries("LaserMediumI")),
-                                'pulses': list(conn.getEnumerationEntries("PulseI"))})
-                form_lasers.append(form_laser)
-
-    # Annotations #
-    text_annotations = list()
-    long_annotations = {'rate': 0.00 , 'votes': 0}
-    url_annotations = list()
-    file_annotations = list()
-    tag_annotations = list()
-    
-    from omero.model import CommentAnnotationI, LongAnnotationI, TagAnnotationI, FileAnnotationI
-                            
-    for ann in image.listAnnotations():
-        if isinstance(ann._obj, CommentAnnotationI):
-            text_annotations.append(ann)
-        elif isinstance(ann._obj, LongAnnotationI):
-            long_annotations['votes'] += 1
-            long_annotations['rate'] += int(ann.longValue)
-        elif isinstance(ann._obj, FileAnnotationI):
-            file_annotations.append(ann)
-        elif isinstance(ann._obj, TagAnnotationI):
-            tag_annotations.append(ann)
-
-    txannSize = len(text_annotations)
-    urlannSize = len(url_annotations)
-    fileannSize = len(file_annotations)
-    tgannSize = len(tag_annotations)
-    if long_annotations['votes'] > 0:
-        long_annotations['rate'] /= long_annotations['votes']
-    
-    return render_to_response('webtest/metadata.html', {'image': image, 'text_annotations': text_annotations, 'txannSize':txannSize, 'long_annotations': long_annotations, 'url_annotations': url_annotations, 'urlannSize':urlannSize, 'file_annotations': file_annotations, 'fileannSize':fileannSize, 'tag_annotations': tag_annotations, 'tgannSize':tgannSize, 'global_metadata':global_metadata, 'serial_metadata':series_metadata, 'form_channels':form_channels, 'form_environment':form_environment, 'form_objective':form_objective, 'form_microscope':form_microscope, 'form_filters':form_filters, 'form_detectors':form_detectors, 'form_lasers':form_lasers, 'form_stageLabel':form_stageLabel})
-    
-@isUserConnected
-def roi_viewer(request, roi_library, imageId, **kwargs):
-    """
-    Displays an image, using 'jquery.drawinglibrary.js' to draw ROIs on the image. 
-    """
-    conn = kwargs['conn']
-    
-    image = conn.getObject("Image", imageId)
-    default_z = image.getSizeZ()/2
-    
-    templates = {"processing":'webtest/roi_viewers/processing_viewer.html',
-            "jquery": "webtest/roi_viewers/jquery_drawing.html",
-            "raphael":"webtest/roi_viewers/raphael_viewer.html"}
-    
-    template = templates[roi_library]
-    
-    return render_to_response(template, {'image':image, 'default_z':default_z})
 
 
 @isUserConnected
@@ -699,17 +531,6 @@ def image_dimensions (request, imageId, **kwargs):
     return render_to_response('webtest/demo_viewers/image_dimensions.html', {'image':image, 'spim_data':spim_data, 'grid': grid, 
         "size": size, "mode":mode, 'xDim':xDim, 'xRange':xRange, 'yRange':yRange, 'yDim':yDim, 
         'xFrames':xFrames, 'yFrames':yFrames})
-
-
-@isUserConnected
-def image_viewer (request, iid, **kwargs):
-    """ This view is responsible for showing pixel data as images """
-    
-    conn = kwargs['conn']
-    
-    kwargs['viewport_server'] = '/webclient'
-    
-    return webgateway_views.full_viewer(request, iid, _conn=conn, **kwargs)
 
 
 def common_templates (request, base_template):
