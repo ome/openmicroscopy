aed5961db0a05506da19c867afc9b0d81ab40d32
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2012-02-23 13:32:39 +0000

    webclient.annotate_comment @login_required(), cleaned

--- components/tools/OmeroWeb/omeroweb/webclient/views.py
+++ components/tools/OmeroWeb/omeroweb/webclient/views.py
@@ -1393,7 +1393,7 @@ def annotate_file(request, conn, **kwargs):
     logger.debug('TEMPLATE: '+template)
     return HttpResponse(t.render(c))
     
-@isUserConnected
+@login_required()
 def annotate_comment(request, conn, **kwargs):
     """ Handle adding Comments to one or more objects """
 
