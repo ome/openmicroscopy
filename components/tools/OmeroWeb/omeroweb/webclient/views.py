1432adb5eadd3b4bd0a6353b660961fc954ac904
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2012-02-24 11:05:24 +0000

    webclient.activities @login_required(), cleaned

--- components/tools/OmeroWeb/omeroweb/webclient/views.py
+++ components/tools/OmeroWeb/omeroweb/webclient/views.py
@@ -2353,19 +2353,12 @@ def getObjectUrl(conn, obj):
 ######################
 # Activities window & Progressbar
 @login_required()
-def activities(request, **kwargs):
+def activities(request, conn, **kwargs):
     """
     Refresh callbacks (delete, scripts etc) and provide json to update Activities window & Progressbar.
     The returned json contains details for ALL callbacks in web session, regardless of their status.
     We also add counts of jobs, failures and 'in progress' to update status bar.
     """
-    conn = None
-    try:
-        conn = kwargs["conn"]
-    except:
-        logger.error(traceback.format_exc())
-        return handlerInternalError("Connection is not available. Please contact your administrator.")
-
     in_progress = 0
     failure = 0
     _purgeCallback(request)
