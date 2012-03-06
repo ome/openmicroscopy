787dd55d14448c6d052b8f50c1d396d888d2abc9
jburel <j.burel@dundee.ac.uk>
2012-03-06 15:36:41 +0000

    remove import

--- components/insight/SRC/org/openmicroscopy/shoola/MainIJPlugin.java
+++ components/insight/SRC/org/openmicroscopy/shoola/MainIJPlugin.java
@@ -30,7 +30,6 @@ import java.awt.event.WindowEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
-import java.net.URL;
 import java.security.CodeSource;
 
 //Third-party libraries
