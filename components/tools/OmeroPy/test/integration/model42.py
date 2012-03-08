02a6f684aa046b54d66c4053412b9f4f231cfd0b
jburel <j.burel@dundee.ac.uk>
2012-03-08 21:00:53 +0000

    Review tests and script following changes in model (see #8249)

--- components/tools/OmeroPy/test/integration/model42.py
+++ components/tools/OmeroPy/test/integration/model42.py
@@ -15,17 +15,10 @@ from omero.rtypes import *
 
 class TestModel42(lib.ITest):
 
-    def testNs(self):
-        ns = omero.model.NamespaceI()
-        ns.name = rstring(self.uuid())
-        ns.keywords = ["a", "b"]
-        ns = self.root.sf.getUpdateService().saveAndReturnObject(ns)
-
     def testRoi(self):
         img = self.new_image("testRoi")
         roi = omero.model.RoiI()
-        roi.namespaces = [self.uuid(), self.uuid()]
-        roi.keywords = [["a", "b"], ["c", "d"]]
+        roi.namespace = self.uuid()
         roi.image = img
         roi = self.update.saveAndReturnObject(roi)
 
@@ -36,8 +29,6 @@ class TestTicket2290(lib.ITest):
         img = self.new_image("testRoi")
         roi = omero.model.RoiI()
         roi.image = img
-        roi.keywords = []
-        roi.namespaces = []
         roi = self.update.saveAndReturnObject(roi)
         # Then resave
         roi = self.update.saveAndReturnObject(roi)
@@ -53,21 +44,17 @@ class TestTicket2290(lib.ITest):
     def testRect2290(self):
         img = self.new_image("testRoi")
         roi = omero.model.RoiI()
-        roi.namespaces = [self.uuid(), self.uuid()]
-        roi.keywords = [["a", "b"], ["c", "d"]]
         roi.image = img
-        rect = omero.model.RectI()
+        rect = omero.model.RectangleI()
         roi.addShape(rect)
         roi = self.update.saveAndReturnObject(roi)
 
     def testRectB2290(self):
         img = self.new_image("testRoi")
         roi = omero.model.RoiI()
-        roi.namespaces = [self.uuid(), self.uuid()]
-        roi.keywords = [["a", "b"], ["c", "d"]]
         roi.image = img
         roi = self.update.saveAndReturnObject(roi)
-        rect = omero.model.RectI()
+        rect = omero.model.RectangleI()
         roi.addShape(rect)
         rect.roi = roi
         rect = self.update.saveAndReturnObject(rect)
@@ -75,11 +62,9 @@ class TestTicket2290(lib.ITest):
     def testResave2290(self):
         img = self.new_image("testRoi")
         roi = omero.model.RoiI()
-        roi.namespaces = [self.uuid(), self.uuid()]
-        roi.keywords = [["a", "b"], ["c", "d"]]
         roi.image = img
         roi = self.update.saveAndReturnObject(roi)
-        rect = omero.model.RectI()
+        rect = omero.model.RectangleI()
         roi.addShape(rect)
         roi = self.update.saveAndReturnObject(roi)
 
