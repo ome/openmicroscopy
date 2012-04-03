b92bea323a85ca124ba1c9fc2a50c0389de7024e
Will Moore <will@lifesci.dundee.ac.uk>
2012-04-03 09:37:36 +0100

    Fix conn=None in script_run()

--- components/tools/OmeroWeb/omeroweb/webclient/views.py
+++ components/tools/OmeroWeb/omeroweb/webclient/views.py
@@ -2657,7 +2657,7 @@ def chgrp(request, conn=None, **kwargs):
 
 
 @login_required()
-def script_run(request, scriptId, conn, **kwargs):
+def script_run(request, scriptId, conn=None, **kwargs):
     """
     Runs a script using values in a POST
     """
