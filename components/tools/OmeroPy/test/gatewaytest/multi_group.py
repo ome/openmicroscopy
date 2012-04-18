dde1672365706d8a886d24df70b591ffc06f72c4
Will Moore <will@lifesci.dundee.ac.uk>
2012-04-18 23:41:49 +0100

    gatewaytest for getEventLogsByPeriod(). See #8580

--- components/tools/OmeroPy/test/gatewaytest/multi_group.py
+++ components/tools/OmeroPy/test/gatewaytest/multi_group.py
@@ -15,6 +15,7 @@ from omero.cmd import *
 from omero.callbacks import CmdCallbackI
 from omero.gateway import BlitzGateway
 from omero.gateway.scripts import dbhelpers
+import time
 
 PRIVATE = 'rw----'
 READONLY = 'rwr---'
@@ -23,6 +24,84 @@ READWRITE = 'rwrw--'
 
 import gatewaytest.library as lib
 
+
+class HistoryTest (lib.GTest):
+
+    def setUp (self):
+        """ Create a group with User """
+        dbhelpers.USERS['history_test_user'] = dbhelpers.UserEntry('history_test_user','ome', firstname='history', lastname='user',
+                   groupname="rw_history", groupperms=READWRITE)
+        # Calling the superclass setUp processes the dbhelpers.USERS etc to populate DB
+        super(HistoryTest, self).setUp()
+
+
+    def searchHistory(self, start, end, dtype="Dataset"):
+        
+        tm = self.gateway.getTimelineService()
+        count = tm.countByPeriod([dtype], rtime(long(start)), rtime(long(end)), None, self.gateway.CONFIG['SERVICE_OPTS'])
+        data = tm.getByPeriod([dtype], rtime(long(start)), rtime(long(end)), None, True, self.gateway.CONFIG['SERVICE_OPTS'])
+        
+        logs = tm.getEventLogsByPeriod(rtime(start), rtime(end), None, self.gateway.CONFIG['SERVICE_OPTS'])
+        entityType = 'ome.model.containers.%s' % dtype
+        filteredLogs = [{'id':i.entityId.val, 'action': i.action.val} for i in logs if i.entityType.val == entityType]
+
+        typeCount = count[dtype]
+        dataCount = len(data[dtype])
+        logCount = len(filteredLogs)
+        
+        self.assertEqual(typeCount, dataCount, "Period count should match number of objects")
+        self.assertEqual(logCount, dataCount, "Logs count should match number of objects")
+
+
+    def testCreateHistory(self):
+        
+        # Login as user...
+        self.doLogin(dbhelpers.USERS['history_test_user'])
+        userId = self.gateway.getEventContext().userId
+        uuid = self.gateway.getEventContext().sessionUuid
+        default_groupId = self.gateway.getEventContext().groupId
+        
+        start = int(round(time.time() * 1000)) - 1000
+
+        # Create Dataset in 'default' group
+        update = self.gateway.getUpdateService()
+        new_ds = omero.model.DatasetI()
+        dataset_name = "history_test_%s" % uuid
+        new_ds.name = rstring(dataset_name)
+        new_ds = update.saveAndReturnObject(new_ds)
+        new_ds_Id = new_ds.id.val
+        
+        # As Admin, create a second group with this user & upload script
+        self.loginAsAdmin()
+        gid = self.gateway.createGroup("history-test-%s" % uuid, member_Ids=[userId], perms=READWRITE)
+
+        # login as User
+        self.doLogin(dbhelpers.USERS['history_test_user'])
+
+        end = int(round(time.time() * 1000)) + 1000
+        self.searchHistory(start, end)
+        
+        # switch user into new group
+        switched = self.gateway.c.sf.setSecurityContext(omero.model.ExperimenterGroupI(gid, False))
+        self.assertTrue(switched, "Failed to switch into new group")
+        # Shouldn't be able to access Dataset...
+        self.searchHistory(start, end)
+        self.assertEqual(None, self.gateway.getObject("Dataset", new_ds_Id))
+        self.gateway.CONFIG['SERVICE_OPTS'] = {'omero.group':str(default_groupId)}
+        self.assertNotEqual(None, self.gateway.getObject("Dataset", new_ds_Id))
+
+        self.searchHistory(start, end)
+        
+        # Create Another Dataset in new group
+        update = self.gateway.getUpdateService()
+        new_ds = omero.model.DatasetI()
+        dataset_name = "history_test_%s" % uuid
+        new_ds.name = rstring(dataset_name)
+        new_ds = update.saveAndReturnObject(new_ds)
+        new_ds_Id = new_ds.id.val
+        
+        end = int(round(time.time() * 1000)) + 1000
+        self.searchHistory(start, end)
 
 
 class ScriptTest (lib.GTest):
