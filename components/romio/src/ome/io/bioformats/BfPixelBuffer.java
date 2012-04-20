ada028d67b6ed791eaaf8558d1ea1d5ba3e55122
Colin Blackburn <colin@ximenes.org.uk>
2012-04-20 16:40:11 +0100

    Close BfPixelBuffer reader. See #8591

--- components/romio/src/ome/io/bioformats/BfPixelBuffer.java
+++ components/romio/src/ome/io/bioformats/BfPixelBuffer.java
@@ -127,6 +127,7 @@ public class BfPixelBuffer implements PixelBuffer, Serializable {
 
     public void close() throws IOException {
         reader().close();
+        bfReader.close();
     }
 
     public int getByteWidth() {
