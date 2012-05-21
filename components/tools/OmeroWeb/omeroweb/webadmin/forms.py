69e4b91624f864150cbb103e92d2b721387433c0
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2012-05-21 15:49:20 +0100

    webadmin group form cleanup, see #8849

--- components/tools/OmeroWeb/omeroweb/webadmin/forms.py
+++ components/tools/OmeroWeb/omeroweb/webadmin/forms.py
@@ -144,11 +144,9 @@ class GroupForm(NonASCIIForm):
     def __init__(self, name_check=False, *args, **kwargs):
         super(GroupForm, self).__init__(*args, **kwargs)
         self.name_check=name_check
-        help_text = None
         try:
             if kwargs['initial']['owners']: pass
             self.fields['owners'] = ExperimenterModelMultipleChoiceField(queryset=kwargs['initial']['experimenters'], initial=kwargs['initial']['owners'], required=False)
-            help_text="<div class=\"error\">WARNING: Changing Permissions will change permissions of all objects in a group. This will take some time for large groups and could break the server.</div>"
         except:
             self.fields['owners'] = ExperimenterModelMultipleChoiceField(queryset=kwargs['initial']['experimenters'], required=False)
         
@@ -159,7 +157,7 @@ class GroupForm(NonASCIIForm):
             self.fields['members'] = ExperimenterModelMultipleChoiceField(queryset=kwargs['initial']['experimenters'], required=False)
         
         
-        self.fields['permissions'] = forms.ChoiceField(choices=self.PERMISSION_CHOICES, widget=forms.RadioSelect(), required=True, label="Permissions", help_text=help_text)
+        self.fields['permissions'] = forms.ChoiceField(choices=self.PERMISSION_CHOICES, widget=forms.RadioSelect(), required=True, label="Permissions")
         
         self.fields.keyOrder = ['name', 'description', 'owners', 'members', 'permissions', 'readonly']
 
