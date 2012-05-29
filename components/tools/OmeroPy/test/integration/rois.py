d1f172e67a7b58061dc264db81e27edfc6670435
Sebastien Besson <seb.besson@gmail.com>
2012-05-29 16:56:32 +0100

    Add integration tests for getROICount() method

--- components/tools/OmeroPy/test/integration/rois.py
+++ components/tools/OmeroPy/test/integration/rois.py
@@ -32,6 +32,77 @@ class TestRois(lib.ITest):
 
         svc = self.client.sf.getRoiService()
         res = svc.findByImage(img.id.val, None)
+    
+    def testROICountTypes(self):
+        """
+        Test ROI counting method
+        """
+        
+        img = self.new_image("")
+        roi1 = omero.model.RoiI()
+        roi1.addShape(omero.model.RectI())
+        roi1.addShape(omero.model.EllipseI())
+        img.addRoi(roi1)
+        roi2 = omero.model.RoiI()
+        roi2.addShape(omero.model.RectI())
+        img.addRoi(roi2)
+        img = self.update.saveAndReturnObject(img)
 
+        from omero.gateway import ImageWrapper, BlitzGateway
+        
+        conn = BlitzGateway(client_obj = self.client)
+        wrapper = ImageWrapper(conn, img)
+        
+        self.assertEqual(wrapper.getROICount(),3)
+        self.assertEqual(wrapper.getROICount("Rect"),2)
+        self.assertEqual(wrapper.getROICount("Ellipse"),1)
+        self.assertEqual(wrapper.getROICount("Line"),0)
+        self.assertEqual(wrapper.getROICount(["Rect","Ellipse"]),3)
+
+    def testROICountPermissions(self):
+        """
+        Test ROI counting method with permissions level
+        """
+        
+        # Create group with two member and one image
+        group = self.new_group(perms="rwrw--")
+        owner = self.new_client(group=group) # Owner of share
+        member = self.new_client(group=group) # Member of group
+        img = self.createTestImage(session=owner.sf)
+
+        # ROI 1 (rectangle) created by owner      
+        roi1 = omero.model.RoiI()
+        roi1.addShape(omero.model.RectI())
+        roi1.setImage(img)
+        roi1  = owner.sf.getUpdateService().saveAndReturnObject(roi1)
+
+        # ROI 2 (ellipse) created by member 
+        roi2 = omero.model.RoiI()
+        roi2.addShape(omero.model.EllipseI())
+        roi2.setImage(img)
+        roi2  = member.sf.getUpdateService().saveAndReturnObject(roi2)
+
+        from omero.gateway import ImageWrapper, BlitzGateway
+
+        # Owner gateway
+        conn = BlitzGateway(client_obj = owner)
+        wrapper = ImageWrapper(conn, img)
+        
+        self.assertEqual(wrapper.getROICount("Ellipse"),1)
+        self.assertEqual(wrapper.getROICount("Ellipse",None),1)
+        self.assertEqual(wrapper.getROICount("Ellipse",1),0)
+        self.assertEqual(wrapper.getROICount("Rect"),1)
+        self.assertEqual(wrapper.getROICount("Rect",None),1)
+        self.assertEqual(wrapper.getROICount("Rect",1),1)
+        
+        # Member gateway
+        conn = BlitzGateway(client_obj = member)
+        wrapper = ImageWrapper(conn, img)
+            
+        self.assertEqual(wrapper.getROICount("Ellipse"),1)
+        self.assertEqual(wrapper.getROICount("Ellipse",1),1)
+        self.assertEqual(wrapper.getROICount("Rect"),1)
+        self.assertEqual(wrapper.getROICount("Rect",1),0)
+    
 if __name__ == '__main__':
     unittest.main()
