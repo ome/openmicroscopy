bf4ac31eccf01db9fb71d53981d3b46475a8d131
Will Moore <will@lifesci.dundee.ac.uk>
2012-06-01 12:51:39 +0100

    basket_counter typo and remove unused history legend

--- components/tools/OmeroWeb/omeroweb/webclient/decorators.py
+++ components/tools/OmeroWeb/omeroweb/webclient/decorators.py
@@ -110,7 +110,7 @@ class render_response(omeroweb.decorators.render_response):
         context['ome'] = {}
         context['ome']['eventContext'] = conn.getEventContext
         context['ome']['user'] = conn.getUser
-        context['ome']['basket_counter'] = request.session.get('basekt_counter', 0)
+        context['ome']['basket_counter'] = request.session.get('basket_counter', 0)
         context['ome']['user_id'] = request.session.get('user_id', None)
         context['ome']['group_id'] = request.session.get('group_id', None)
         self.load_settings(request, context, conn)
