ome/openmicroscopy

commit 31fa28c918c2be560fa1f61f5a1d7ebc33309e40

Remove reference to GroupExperimenter.

components/server/src/ome/security/auth/SimpleRoleProvider.java
@@ -15,11 +15,9 @@ import ome.conditions.ApiUsageException;
 import ome.conditions.ValidationException;
 import ome.model.IObject;
 import ome.model.internal.Permissions;
-import ome.model.internal.Permissions.Right;
-import ome.model.internal.Permissions.Role;
 import ome.model.core.Experimenter;
 import ome.model.core.ExperimenterGroup;
-import ome.model.meta.GroupExperimenterMap;
+import ome.model.core.ExperimenterGroupExperimenterLink;
 import ome.security.SecureAction;
 import ome.security.SecuritySystem;
 import ome.tools.hibernate.HibernateUtils;
@@ -105,7 +103,7 @@ public class SimpleRoleProvider implements RoleProvider {
         e = sec.doAction(action, e);
         session.flush();
 
-        GroupExperimenterMap link = linkGroupAndUser(defaultGroup, e, false);
+        ExperimenterGroupExperimenterLink link = linkGroupAndUser(defaultGroup, e, false);
         if (null != otherGroups) {
             for (ExperimenterGroup group : otherGroups) {
                 linkGroupAndUser(group, e, false);
@@ -119,8 +117,8 @@ public class SimpleRoleProvider implements RoleProvider {
         Session session = sf.getSession();
         Experimenter foundUser = userById(user.getId(), session);
         ExperimenterGroup foundGroup = groupById(group.getId(), session);
-        Set<GroupExperimenterMap> foundMaps = foundUser
-                .findGroupExperimenterMap(foundGroup);
+        Set<ExperimenterGroupExperimenterLink> foundMaps = foundUser
+                .findExperimenterGroupExperimenterLink(foundGroup);
         if (foundMaps.size() < 1) {
             throw new ApiUsageException("Group " + group.getId() + " was not "
                     + "found for user " + user.getId());
@@ -129,7 +127,7 @@ public class SimpleRoleProvider implements RoleProvider {
                     + " found for " + foundUser);
         } else {
             // May throw an exception
-            foundUser.setPrimaryGroupExperimenterMap(foundMaps.iterator()
+        	foundUser.addExperimenterGroupExperimenterLink(foundMaps.iterator()
                     .next());
         }
 
@@ -174,8 +172,8 @@ public class SimpleRoleProvider implements RoleProvider {
                 toRemove.add(g.getId());
             }
         }
-        for (GroupExperimenterMap map : foundUser
-                .<GroupExperimenterMap> collectGroupExperimenterMap(null)) {
+        for (ExperimenterGroupExperimenterLink map : foundUser
+                .<ExperimenterGroupExperimenterLink>collectExperimenterGroupLinks(null)) {
             Long pId = map.parent().getId();
             Long cId = map.child().getId();
             if (toRemove.contains(pId)) {
@@ -200,7 +198,7 @@ public class SimpleRoleProvider implements RoleProvider {
     // ~ Helpers
     // =========================================================================
 
-    protected GroupExperimenterMap linkGroupAndUser(ExperimenterGroup group,
+    protected ExperimenterGroupExperimenterLink linkGroupAndUser(ExperimenterGroup group,
             Experimenter e, boolean owned) {
 
         if (group == null || group.getId() == null) {
@@ -210,14 +208,14 @@ public class SimpleRoleProvider implements RoleProvider {
         group = new ExperimenterGroup(group.getId(), false);
 
         // ticket:1021 - check for already added groups
-        for (GroupExperimenterMap link : e.unmodifiableGroupExperimenterMap()) {
+        for (ExperimenterGroupExperimenterLink link : e.unmodifiableExperimenterGroupLinks()) {
             ExperimenterGroup test = link.parent();
             if (test.getId().equals(group.getId())) {
                 return link; // EARLY EXIT!
             }
         }
 
-        GroupExperimenterMap link = e.linkExperimenterGroup(group);
+        ExperimenterGroupExperimenterLink link = e.linkExperimenterGroup(group);
         // ticket:1434
         link.setOwner(owned);
 
@@ -231,11 +229,11 @@ public class SimpleRoleProvider implements RoleProvider {
     }
 
     protected Experimenter copyUser(Experimenter e) {
-        if (e.getOmeName() == null) {
+        if (e.getUserName() == null) {
             throw new ValidationException("OmeName may not be null.");
         }
         Experimenter copy = new Experimenter();
-        copy.setOmeName(e.getOmeName());
+        copy.setUserName(e.getUserName());
         copy.setFirstName(e.getFirstName());
         copy.setMiddleName(e.getMiddleName());
         copy.setLastName(e.getLastName());

components/client/test/ome/client/itests/sec/AdminTest.java
@@ -15,7 +15,7 @@ import ome.model.internal.Permissions.Right;
 import ome.model.internal.Permissions.Role;
 import ome.model.core.Experimenter;
 import ome.model.core.ExperimenterGroup;
-import ome.model.meta.GroupExperimenterMap;
+import ome.model.core.ExperimenterGroupExperimenterLink;
 import ome.system.Login;
 import ome.system.Roles;
 import ome.system.ServiceFactory;
@@ -180,8 +180,8 @@ public class AdminTest extends AbstractAccountTest {
         self.setEmail(uuid);
         self.setInstitution(uuid);
 
-        GroupExperimenterMap map = self.linkExperimenterGroup(grpPrx);
-        self.setPrimaryGroupExperimenterMap(map);
+        ExperimenterGroupExperimenterLink map = self.linkExperimenterGroup(grpPrx);
+        self.addExperimenterGroupExperimenterLink(0, map);
 
         // Update and reacquire
         ua.updateSelf(self);

components/server/src/ome/logic/AdminImpl.java
@@ -47,7 +47,7 @@ import ome.model.internal.Permissions.Right;
 import ome.model.internal.Permissions.Role;
 import ome.model.core.Experimenter;
 import ome.model.core.ExperimenterGroup;
-import ome.model.meta.GroupExperimenterMap;
+import ome.model.core.ExperimenterGroupExperimenterLink;
 import ome.parameters.Parameters;
 import ome.security.ACLVoter;
 import ome.security.AdminAction;
@@ -405,7 +405,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
             Long id = (Long) dnId.get("experimenter_id");
             try {
                 Experimenter e = userProxy(id);
-                ldapUtil.synchronizeLdapUser(e.getOmeName());
+                ldapUtil.synchronizeLdapUser(e.getUserName());
             } catch (ApiUsageException aue) {
                 // User likely doesn't exist
                 log.debug("User not found: " + dn);
@@ -433,7 +433,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
             }
         });
         getBeanHelper().getLogger().info(
-                "Updated own user info: " + self.getOmeName());
+                "Updated own user info: " + self.getUserName());
     }
 
     protected static final String NSEXPERIMENTERPHOTO = "openmicroscopy.org/omero/experimenter/photo";
@@ -466,7 +466,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
             file.setPath(filename); // FIXME this should be something like /users/<name>/photo
             file.setSize((long) data.length);
             file.setSha1(Utils.bufferToSha1(data));
-            file.setMimetype(mimetype);
+            file.setMimeType(mimetype);
             FileAnnotation fa = new FileAnnotation();
             fa.setNs(NSEXPERIMENTERPHOTO);
             fa.setFile(file);
@@ -481,7 +481,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
         } else {
             file.setName(filename);
             file.setPath(filename);
-            file.setMimetype(mimetype);
+            file.setMimeType(mimetype);
             file = iUpdate.saveAndReturnObject(file);
         }
 
@@ -505,7 +505,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
 
         try {
             adminOrPiOfUser(experimenter);
-            String name = experimenter.getOmeName();
+            String name = experimenter.getUserName();
             copyAndSaveExperimenter(experimenter);
             getBeanHelper().getLogger().info("Updated user info for " + name);
         } catch (SecurityViolation sv) {
@@ -530,7 +530,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
         adminOrPiOfUser(experimenter);
         copyAndSaveExperimenter(experimenter);
         final Experimenter orig = userProxy(experimenter.getId());
-        String name = orig.getOmeName();
+        String name = orig.getUserName();
         changeUserPassword(name, password);
         getBeanHelper().getLogger().info(
                 "Updated user info and password for " + name);
@@ -541,7 +541,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
      */
     private void copyAndSaveExperimenter(final Experimenter experimenter) {
         final Experimenter orig = userProxy(experimenter.getId());
-        orig.setOmeName(experimenter.getOmeName());
+        orig.setUserName(experimenter.getUserName());
         orig.setEmail(experimenter.getEmail());
         orig.setFirstName(experimenter.getFirstName());
         orig.setMiddleName(experimenter.getMiddleName());
@@ -598,13 +598,13 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
             ExperimenterGroup defaultGroup, ExperimenterGroup... otherGroups) {
 
         adminOrPiOfNonUserGroups(defaultGroup, otherGroups);
-        
+
         long uid = roleProvider.createExperimenter(experimenter, defaultGroup, otherGroups);
         // If this method passes, then the Experimenter is valid.
-        changeUserPassword(experimenter.getOmeName(), " ");
+        changeUserPassword(experimenter.getUserName(), " ");
         getBeanHelper().getLogger().info(
                 "Created user with blank password: "
-                + experimenter.getOmeName());
+                + experimenter.getUserName());
         return uid;
     }
 
@@ -619,9 +619,9 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
         long uid = roleProvider.createExperimenter(experimenter,
                         defaultGroup, otherGroups);
         // If this method passes, then the Experimenter is valid.
-        changeUserPassword(experimenter.getOmeName(), password);
+        changeUserPassword(experimenter.getUserName(), password);
         getBeanHelper().getLogger().info(
-                "Created user with password: " + experimenter.getOmeName());
+                "Created user with password: " + experimenter.getUserName());
         return uid;
     }
 
@@ -652,7 +652,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
 
         getBeanHelper().getLogger().info(
                 String.format("Added user %s to groups %s", userProxy(
-                        user.getId()).getOmeName(), Arrays.asList(groups)));
+                        user.getId()).getUserName(), Arrays.asList(groups)));
     }
 
     @RolesAllowed("user")
@@ -759,7 +759,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
                     + "must be managed (i.e. have an id)");
         }
 
-        GroupExperimenterMap m = findLink(group, owner);
+        ExperimenterGroupExperimenterLink m = findLink(group, owner);
         if (m == null) {
             addGroups(owner, group);
             m = findLink(group, owner);
@@ -776,10 +776,10 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
                                 owner.getId(), group.getId()));
     }
 
-    private GroupExperimenterMap findLink(ExperimenterGroup group,
+    private ExperimenterGroupExperimenterLink findLink(ExperimenterGroup group,
             Experimenter owner) {
-        GroupExperimenterMap m = iQuery.findByQuery(
-                "select m from GroupExperimenterMap m " +
+    	ExperimenterGroupExperimenterLink m = iQuery.findByQuery(
+                "select m from ExperimenterGroupExperimenterLink m " +
 			"where m.parent.id = :pid " +
 			"and m.child.id = :cid",
 			new Parameters()
@@ -816,7 +816,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
 
         if (count == 0) {
             getBeanHelper().getLogger().info(
-                    "No password found for user " + e.getOmeName()
+                    "No password found for user " + e.getUserName()
                             + ". Cannot delete.");
         }
 
@@ -825,7 +825,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
                 iUpdate.deleteObject(e);
             }
         });
-        getBeanHelper().getLogger().info("Deleted user: " + e.getOmeName());
+        getBeanHelper().getLogger().info("Deleted user: " + e.getUserName());
     }
 
     @RolesAllowed("user")
@@ -1103,7 +1103,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
                     sendEmail(e, passwd);
                     // changeUserPassword checks adminOrPiOfUser
                     // Skipping that. See #7327
-                    _changePassword(e.getOmeName(), passwd);
+                    _changePassword(e.getUserName(), passwd);
                 }
             }
         });
@@ -1124,7 +1124,7 @@ public class AdminImpl extends AbstractLevel2Service implements LocalAdmin,
         msg.setSubject("OMERO - Reset password");
         msg.setTo(e.getEmail());
         msg.setText("Dear " + e.getFirstName() + " " + e.getLastName() + " ("
-                + e.getOmeName() + ")" + " your new password is: "
+                + e.getUserName() + ")" + " your new password is: "
                 + newPassword);
         try {
             this.mailSender.send(msg);

components/server/src/ome/logic/LdapImpl.java
@@ -34,7 +34,7 @@ import ome.conditions.ValidationException;
 import ome.model.internal.Permissions;
 import ome.model.core.Experimenter;
 import ome.model.core.ExperimenterGroup;
-import ome.model.meta.GroupExperimenterMap;
+import ome.model.core.ExperimenterGroupExperimenterLink;
 import ome.parameters.Parameters;
 import ome.security.SecuritySystem;
 import ome.security.auth.AttributeNewUserGroupBean;
@@ -199,7 +199,7 @@ public class LdapImpl extends AbstractLevel2Service implements ILdap,
 
         if (p.size() == 1 && p.get(0) != null) {
             Experimenter e = p.get(0);
-            if (e.getOmeName().equals(username)) {
+            if (e.getUserName().equals(username)) {
                 return p.get(0);
             }
         }
@@ -341,7 +341,7 @@ public class LdapImpl extends AbstractLevel2Service implements ILdap,
             log.info(String.format(
                     "%s groups for %s: %s",
                     add ? "Adding" : "Removing",
-                    e.getOmeName(), ids));
+                    e.getUserName(), ids));
             Set<ExperimenterGroup> grps = new HashSet<ExperimenterGroup>();
             for (Long id : ids) {
                 grps.add(new ExperimenterGroup(id, false));
@@ -357,10 +357,10 @@ public class LdapImpl extends AbstractLevel2Service implements ILdap,
                 // the "user" groupis at the front of the list, in which
                 // case we should assign another specific group.
                 e = iQuery.get(Experimenter.class, e.getId());
-                log.debug("sizeOfGroupExperimenterMap=" + e.sizeOfGroupExperimenterMap());
-                if (e.sizeOfGroupExperimenterMap() > 1) {
-                    GroupExperimenterMap primary = e.getGroupExperimenterMap(0);
-                    GroupExperimenterMap next = e.getGroupExperimenterMap(1);
+                log.debug("sizeOfGroupExperimenterMap=" + e.sizeOfExperimenterGroupLinks());
+                if (e.sizeOfExperimenterGroupLinks() > 1) {
+                    ExperimenterGroupExperimenterLink primary = e.getExperimenterGroupExperimenterLink(0);
+                    ExperimenterGroupExperimenterLink next = e.getExperimenterGroupExperimenterLink(1);
                     log.debug("primary=" + primary.parent().getId());
                     log.debug("next=" + next.parent().getId());
                     if (primary.parent().getId().equals(roles.getUserGroupId())) {

components/server/src/ome/security/basic/BasicSecuritySystem.java
@@ -44,7 +44,7 @@ import ome.model.meta.Event;
 import ome.model.meta.EventLog;
 import ome.model.core.Experimenter;
 import ome.model.core.ExperimenterGroup;
-import ome.model.meta.GroupExperimenterMap;
+import ome.model.core.ExperimenterGroupExperimenterLink;
 import ome.model.roi.Shape;
 import ome.security.AdminAction;
 import ome.security.SecureAction;
@@ -477,7 +477,7 @@ public class BasicSecuritySystem implements SecuritySystem,
             String a = log.getAction();
             if (Experimenter.class.getName().equals(t)
                     || ExperimenterGroup.class.getName().equals(t)
-                    || GroupExperimenterMap.class.getName().equals(t)) {
+                    || ExperimenterGroupExperimenterLink.class.getName().equals(t)) {
                 foundAdminType = true;
             }
             try {
@@ -680,7 +680,7 @@ public class BasicSecuritySystem implements SecuritySystem,
             }
             ome.model.meta.Session s = sf.getQueryService().get(
                     ome.model.meta.Session.class, shareId);
-            return s.getOwner().getId();
+            return s.getExperimenter().getId();
         }
         return ec.getCurrentUserId();
     }

components/server/src/ome/services/sessions/events/UserGroupUpdateEvent.java
@@ -9,7 +9,6 @@ package ome.services.sessions.events;
 
 import ome.model.core.Experimenter;
 import ome.model.core.ExperimenterGroup;
-import ome.model.meta.GroupExperimenterMap;
 import ome.security.SecuritySystem;
 import ome.services.sessions.SessionManager;
 import ome.util.messages.InternalMessage;

components/server/test/ome/server/itests/perms42/AdminPermsTest.java
@@ -16,7 +16,7 @@ import ome.model.enums.Format;
 import ome.model.internal.Permissions;
 import ome.model.core.Experimenter;
 import ome.model.core.ExperimenterGroup;
-import ome.model.meta.GroupExperimenterMap;
+import ome.model.core.ExperimenterGroupExperimenterLink;
 
 import org.testng.annotations.Test;
 
@@ -260,13 +260,13 @@ public class AdminPermsTest extends PermissionsTest {
         iAdmin.changePassword("UserChangesPassword");
         fixture.log_in();
         try {
-            iAdmin.changeUserPassword(member.getOmeName(), "PIChangesPass");
+            iAdmin.changeUserPassword(member.getUserName(), "PIChangesPass");
             fail("secvio");
         } catch (SecurityViolation sv) {
             // good;
         }
         fixture.make_leader();
-        iAdmin.changeUserPassword(member.getOmeName(), "PIChangesPass");
+        iAdmin.changeUserPassword(member.getUserName(), "PIChangesPass");
     }
 
     @Test
@@ -449,7 +449,7 @@ public class AdminPermsTest extends PermissionsTest {
     protected void assertMembers(ExperimenterGroup group, Long...members) {
         Set<Long> toCheck = new HashSet<Long>(Arrays.asList(members));
         Set<Long> thatHas = new HashSet<Long>();
-        for (GroupExperimenterMap map : group.unmodifiableGroupExperimenterMap()) {
+        for (ExperimenterGroupExperimenterLink map : group.unmodifiableExperimenterLinks()) {
             thatHas.add(map.child().getId());
         }
         assertEqualSets(thatHas, toCheck);
@@ -458,7 +458,7 @@ public class AdminPermsTest extends PermissionsTest {
     protected void assertLeaders(ExperimenterGroup group, Long...members) {
         Set<Long> toCheck = new HashSet<Long>(Arrays.asList(members));
         Set<Long> thatHas = new HashSet<Long>();
-        for (GroupExperimenterMap map : group.unmodifiableGroupExperimenterMap()) {
+        for (ExperimenterGroupExperimenterLink map : group.unmodifiableExperimenterLinks()) {
             if (map.getOwner()) {
                 thatHas.add(map.child().getId());
             }
@@ -482,7 +482,7 @@ public class AdminPermsTest extends PermissionsTest {
 
     private Experimenter uuidUser() {
         Experimenter e = new Experimenter();
-        e.setOmeName(uuid());
+        e.setUserName(uuid());
         e.setFirstName(uuid());
         e.setLastName(uuid());
         return e;

commit af20e13f0b906f7a34e777f0f9fda5274b266a8f

Fix bug introduced in 31fa28c9.

components/server/src/ome/security/auth/SimpleRoleProvider.java
@@ -127,7 +127,7 @@ public class SimpleRoleProvider implements RoleProvider {
                     + " found for " + foundUser);
         } else {
             // May throw an exception
-        	foundUser.addExperimenterGroupExperimenterLink(foundMaps.iterator()
+        	foundUser.setPrimaryExperimenterGroupExperimenterLink(foundMaps.iterator()
                     .next());
         }
 