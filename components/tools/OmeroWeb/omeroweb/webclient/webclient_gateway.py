876d62379b8236e345796187726e63b56cc01489
Will Moore <will@lifesci.dundee.ac.uk>
2012-06-06 15:26:50 +0100

    Removing webclient_gateway group.isReadOnly(). See #8437

--- components/tools/OmeroWeb/omeroweb/webclient/webclient_gateway.py
+++ components/tools/OmeroWeb/omeroweb/webclient/webclient_gateway.py
@@ -2009,22 +2009,6 @@ class ExperimenterGroupWrapper (OmeroWebObjectWrapper, omero.gateway.Experimente
         else:
             False
     
-    def isReadOnly(self):
-        p = None
-        if self.details.getPermissions() is None:
-            raise AttributeError('Object has no permissions')
-        else:
-            p = self.details.getPermissions()
-        
-        flag = False
-        if p.isUserRead() and not p.isUserWrite():
-            flag = True
-        if p.isGroupRead() and not p.isGroupWrite():
-            flag = True
-        if p.isWorldRead() and not p.isWorldWrite():
-            flag = True
-        return flag
-    
 omero.gateway.ExperimenterGroupWrapper = ExperimenterGroupWrapper 
 
 class ProjectWrapper (OmeroWebObjectWrapper, omero.gateway.ProjectWrapper): 
