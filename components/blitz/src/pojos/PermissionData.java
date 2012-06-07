9527e3ccf9afadb42e196dd306f3779a0425e72a
jburel <j.burel@dundee.ac.uk>
2012-06-07 21:47:20 +0100

    Fix the "setAnnotate" method when setting permission.

--- components/blitz/src/pojos/PermissionData.java
+++ components/blitz/src/pojos/PermissionData.java
@@ -146,7 +146,7 @@ public class PermissionData {
      */
     public void setGroupAnnotate(boolean groupAnnotate)
     {
-    	p.setGroupWrite(groupAnnotate);
+    	p.setGroupAnnotate(groupAnnotate);
     }
     
     /**
