a69be9d6f096570dc1ab3a6e69eb0c50fc84d21d
Chris MacLeod <ckm@glencoesoftware.com>
2012-06-15 10:53:03 -0400

    fix sudo password requirement
    needs password and -S passed to avoid tty issues

--- docs/install/VM/setup_nginx.sh
+++ docs/install/VM/setup_nginx.sh
@@ -2,12 +2,15 @@
 
 set -e -u -x
 
-sudo apt-get update && sudo apt-get install nginx
+PASSWORD=${PASSWORD:-"omero"}
+
+echo $PASSWORD | sudo -S apt-get update 
+echo $PASSWORD | sudo -S apt-get install nginx
 # ugly -- but required for nginx to start
 # as of version 0.7.53, nginx will use a compiled-in default error log
 # location until it has read the config file. If the user running nginx
 # doesn't have write permission to this log location, nginx will raise
 # an alert
-sudo chown -R omero:omero /var/log/nginx
+echo $PASSWORD | sudo -S chown -R omero:omero /var/log/nginx
 /home/omero/OMERO.server/bin/omero web config nginx --http 8080 > /home/omero/OMERO.server/omero-web-nginx.conf
 /usr/sbin/nginx -c /home/omero/OMERO.server/omero-web-nginx.conf
