a02e204268897e0c742b3a6564ee26af55e834d4
jburel <j.burel@dundee.ac.uk>
2012-07-11 11:49:10 +0100

    Fix delete and import (see #9332)

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/OMEROGateway.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/OMEROGateway.java
@@ -7865,7 +7865,7 @@ class OMEROGateway
 	{
 		isSessionAlive(ctx);
 		CmdCallbackI cb = null;
-		shutDownServices(false);
+		//shutDownServices(false);
 		try {
 	         Connector c = getConnector(ctx);
 	         return c.submit(Arrays.<Request>asList(commands), ctx);
