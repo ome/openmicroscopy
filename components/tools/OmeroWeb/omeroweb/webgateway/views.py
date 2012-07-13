1ba78cd5a2e3519611eb43b1af24dbb34ce90a07
Carlos Neves <carlos@glencoesoftware.com>
2012-07-13 14:30:07 +0100

    Adding dryrun (count only) arg to export_ome_tiff
    Also filtering out bigimages which may result in nothing to export, in which case a 404 is thrown

--- components/tools/OmeroWeb/omeroweb/webgateway/views.py
+++ components/tools/OmeroWeb/omeroweb/webgateway/views.py
@@ -810,12 +810,17 @@ def render_ome_tiff (request, ctx, cid, conn=None, **kwargs):
     """
     Renders the OME-TIFF representation of the image(s) with id cid in ctx (i)mage,
     (d)ataset, or (p)roject.
+    For multiple images export, images that require pixels pyramid (big images) will be silently skipped.
+    If exporting a single big image or if all images in a multple image export are big,
+    a 404 will be triggered.
+    A request parameter dryrun can be passed to return the count of images that would actually be exported.
     
     @param request:     http request
     @param ctx:         'p' or 'd' or 'i'
     @param cid:         Project, Dataset or Image ID
     @param conn:        L{omero.gateway.BlitzGateway} connection
     @return:            http response wrapping the tiff (or zip for multiple files), or redirect to temp file/zip
+                        if dryrun is True, returns count of images that would be exported
     """
     server_id = request.session['connector'].server_id
     imgs = []
@@ -854,6 +859,17 @@ def render_ome_tiff (request, ctx, cid, conn=None, **kwargs):
             raise Http404
         imgs.append(obj)
 
+    imgs = filter(lambda x: not x.requiresPixelsPyramid(), imgs)
+
+    if request.REQUEST.get('dryrun', False):
+        rv = simplejson.dumps(len(imgs))
+        c = request.REQUEST.get('callback', None)
+        if c is not None and not kwargs.get('_internal', False):
+            rv = '%s(%s)' % (c, rv)
+        return HttpResponse(rv, mimetype='application/javascript')
+    
+    if len(imgs) == 0:
+        raise Http404
     if len(imgs) == 1:
         obj = imgs[0]
         key = '_'.join((str(x.getId()) for x in obj.getAncestry())) + '_' + str(obj.getId()) + '_ome_tiff'
