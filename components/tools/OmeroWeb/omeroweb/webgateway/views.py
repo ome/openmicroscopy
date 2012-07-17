cf9e01949ea5fccaab4a611cce8746b612181f33
Chris Allan <callan@glencoesoftware.com>
2012-07-17 12:22:08 +0100

    Add configurabiliy to the default zoom level.

--- components/tools/OmeroWeb/omeroweb/webgateway/views.py
+++ components/tools/OmeroWeb/omeroweb/webgateway/views.py
@@ -1210,7 +1210,9 @@ def imageMarshal (image, key=None):
     tiles = image._re.requiresPixelsPyramid()
     width, height = image._re.getTileSize()
     levels = image._re.getResolutionLevels()
-    init_zoom = 0
+    init_zoom = settings.VIEWER_INITIAL_ZOOM_LEVEL
+    if init_zoom < 0:
+        init_zoom = levels + init_zoom
 
     try:
         rv.update({
