fdafa70646de94bb17f539c1b13ba8cf8048ac7b
Will Moore <will@lifesci.dundee.ac.uk>
2012-08-11 00:15:36 +0100

    Use CSS to switch icon -> table layout, NOT table plugin. See #9466

--- components/tools/OmeroWeb/omeroweb/settings.py
+++ components/tools/OmeroWeb/omeroweb/settings.py
@@ -255,7 +255,9 @@ CUSTOM_SETTINGS_MAPPINGS = {
     # E.g. Center plugin: ["Channel overlay", "webtest/webclient_plugins/center_plugin.overlay.js.html", "channel_overlay_panel"]
     "omero.web.ui.center_plugins": ["CENTER_PLUGINS", '['\
             #'["Split View", "webclient/data/includes/center_plugin.splitview.js.html", "split_view_panel"],'\
-            '["Table", "webclient/data/includes/center_plugin.table.js.html", "image_table"]]', json.loads],
+            #'["Table", "webclient/data/includes/center_plugin.table.js.html", "image_table"]'\
+            ']'
+            , json.loads],
 
     # sharing no longer use this variable. replaced by request.build_absolute_uri
     # after testing this line should be removed.
