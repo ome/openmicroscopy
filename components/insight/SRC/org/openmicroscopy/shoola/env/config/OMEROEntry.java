67fd2e19ff47455b6756734c4ce1348eae6d3c2d
jburel <j.burel@dundee.ac.uk>
2012-08-24 16:25:38 +0100

    Add more configuration options to connect to server (see #9508).
    Allow user to interact with host name and add a new encrypted
    entry.

--- components/insight/SRC/org/openmicroscopy/shoola/env/config/OMEROEntry.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/config/OMEROEntry.java
@@ -28,6 +28,7 @@ package org.openmicroscopy.shoola.env.config;
 
 //Third-party libraries
 import org.w3c.dom.DOMException;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -71,15 +72,57 @@ class OMEROEntry
      */
     private static final String     HOST_NAME_TAG = "hostName";
     
+    /** 
+     * The name of the tag, within this <i>structuredEntry</i>, that indicates
+     * if the data transfer is encrypted.
+     */
+    private static final String     ENCRYPTED_TAG = "encrypted";
+    
+    /** 
+     * The name of the attribute that indicates the user can modify in UI the 
+     * value specified in the configuration file.
+     */
+    private static final String     CONFIGURABLE_ATTRIBUTE = "configurable";
+    
+    
     /** Holds the contents of the entry. */
     private OMEROInfo value;
+    
+    /** 
+     * Returns the value of the {@link #CONFIGURABLE_ATTRIBUTE}
+     * attribute as a boolean.
+     * 
+     * @param tag The tag to handle.
+     * @return See above.
+     */
+    private static boolean getConfigAttribute(Node tag)
+    {
+    	NamedNodeMap attrList = tag.getAttributes();
+		Node attribute;
+		String value = null;
+		//Store the values of name and type attributes into ntp.
+		for (int i = 0; i < attrList.getLength(); ++i) {
+			attribute = attrList.item(i);
+			if (CONFIGURABLE_ATTRIBUTE.equals(attribute.getNodeName())) {
+				value = attribute.getNodeValue();
+			}
+		}
+		if (value == null) return true;
+		value = value.toLowerCase();
+		if (AgentInfo.TRUE.equals(value) || AgentInfo.TRUE.equals(value))
+			return true;
+		else if (AgentInfo.FALSE.equals(value) ||
+				AgentInfo.FALSE_SHORT.equals(value))
+			return false;
+    	return true;
+    }
     
     /**
      * Helper method to parse the structured entry tag.
      * 
      * @param tag The structured entry tag.
      * @return An object that holds the contents of the tag.
-     * @throws ConfigException If the tag is malformed.
+     * @throws ConfigException If the tag is not well formed.
      * @throws DOMException If the specified tag cannot be parsed.
      */
     private static OMEROInfo parseTag(Node tag)
@@ -88,6 +131,9 @@ class OMEROEntry
         String port = null; 
         String portSSL = null;
         String hostName = null;
+        String encrypted = null;
+        boolean hostConfigurable = true;
+        boolean encryptedConfigurable = true;
         NodeList children = tag.getChildNodes();
         int n = children.getLength();
         Node child, c;
@@ -101,21 +147,29 @@ class OMEROEntry
                 	 tagValue = c.getNodeValue();
                      if (PORT_TAG.equals(tagName)) port = tagValue;
                      else if (PORT_SSL_TAG.equals(tagName)) portSSL = tagValue;
-                     else if (HOST_NAME_TAG.equals(tagName)) 
-                    	 hostName = tagValue;
-                     else
+                     else if (HOST_NAME_TAG.equals(tagName)) {
+                    	hostName = tagValue;
+                    	hostConfigurable = getConfigAttribute(child);
+                     } else if (ENCRYPTED_TAG.equals(tagName)) {
+                    	encrypted = tagValue;
+                    	encryptedConfigurable = getConfigAttribute(child);
+                     } else
                          throw new ConfigException(
                                  "Unrecognized tag within the conf entry: "+
                                  tagName+".");
                 }
             }
         }
+        System.err.println(hostConfigurable+" "+encryptedConfigurable);
         if (port == null || port.trim().length() == 0)
             throw new ConfigException("Missing "+PORT_TAG+
                                       " tag within omeds-conf entry.");
         if (portSSL == null || portSSL.trim().length() == 0)
         	portSSL = port;
-        return new OMEROInfo(port, portSSL, hostName);
+        OMEROInfo info = new OMEROInfo(port, portSSL, hostName, encrypted);
+        info.setHostNameConfigurable(hostConfigurable);
+        info.setEncryptedConfigurable(encryptedConfigurable);
+        return info;
     }
     
     
