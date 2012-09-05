72269b9be761397847d93e58d35f8c5e3d1a5157
jburel <j.burel@dundee.ac.uk>
2012-09-05 11:36:40 +0100

    Simplify retrieval of images.
    Move methods to admin service to retrieve user's details
    Improve example to show how to load images from ID.

--- components/insight/TEST/org/openmicroscopy/shoola/examples/data/LoginHeadless.java
+++ components/insight/TEST/org/openmicroscopy/shoola/examples/data/LoginHeadless.java
@@ -25,16 +25,26 @@ package org.openmicroscopy.shoola.examples.data;
 
 
 //Java imports
-
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 //Third-party libraries
 
 //Application-internal dependencies
 import org.openmicroscopy.shoola.env.Container;
 import org.openmicroscopy.shoola.env.LookupNames;
 import org.openmicroscopy.shoola.env.config.Registry;
+import org.openmicroscopy.shoola.env.data.AdminService;
+import org.openmicroscopy.shoola.env.data.OmeroDataService;
 import org.openmicroscopy.shoola.env.data.events.ActivateAgents;
 import org.openmicroscopy.shoola.env.data.login.LoginService;
 import org.openmicroscopy.shoola.env.data.login.UserCredentials;
+import org.openmicroscopy.shoola.env.data.util.SecurityContext;
+
+import pojos.ExperimenterData;
+import pojos.GroupData;
+import pojos.ImageData;
 
 
 /** 
@@ -60,6 +70,32 @@ public class LoginHeadless {
 			System.err.println("connected");
 			//For testing purpose. Now start the UI if required.
 			container.getRegistry().getEventBus().post(new ActivateAgents());
+			
+			//List
+			AdminService adminSvc = container.getRegistry().getAdminService();
+			//If you need information about the user.
+			ExperimenterData exp = adminSvc.getUserDetails();
+			//All the groups the user is member of 
+			Collection<GroupData> groups = adminSvc.getAvailableUserGroups();
+			//The user is logged into his/her default group. 
+			//Security Context is mainly for now only using the group
+			// idea is to support multi-server.
+			SecurityContext ctx = new SecurityContext(
+					exp.getDefaultGroup().getId());
+			List<Long> imageIds = new ArrayList<Long>();
+			
+			//To be modified.
+			imageIds.add(122L);
+			imageIds.add(151L);
+			OmeroDataService dataSvc = container.getRegistry().getDataService();
+			try {
+				Set images = dataSvc.getImages(ctx, ImageData.class, imageIds,
+						-1);
+				System.err.println(images.size());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
 		}
 	}
 	
