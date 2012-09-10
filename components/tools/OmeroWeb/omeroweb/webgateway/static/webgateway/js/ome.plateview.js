99d3c29b1fcf8fbd8ca3accf86791c9836ca3b3c
Carlos Neves <carlos@glencoesoftware.com>
2012-09-10 17:29:21 +0100

    Starting to document and cleanup plateview javascript

--- components/tools/OmeroWeb/omeroweb/webgateway/static/webgateway/js/ome.plateview.js
+++ components/tools/OmeroWeb/omeroweb/webgateway/static/webgateway/js/ome.plateview.js
@@ -14,6 +14,38 @@
  * Author: Carlos Neves <carlos(at)glencoesoftware.com>
  */
 
+
+/**
+ * ---- Usage ----
+ *
+ * - Initializing -
+ *
+ * plate = jQuery.WeblitzPlateview(elm, options)
+ * - elm: the selector for the element to be turned into a plateview container
+ * - options: a dictionary, recognized options are
+ *   - baseurl: optional prefix path for the json query url (string, url)
+ *   - width: the individual well thumbnail width, defaults to 64 (int, pixels)
+ *   - height: the individual well thumbnail height, defaults to 48 (int, pixels)
+ *   - useParentPrefix: if true, prepends the base container id to the created elements, default true (boolean)
+ * - returns the WeblitzPlateview object
+ *
+ * - Using -
+ *
+ * plate.load(pid, field)
+ * - pid: Plate ID (int)
+ * - field: 0 based field index (int)
+ *
+ * plate.setFocus(elm, evt)
+ * plate.rmFocus(elm, evt)
+ *
+ * - Events -
+ * 
+ * thumbClick
+ * thumbLoad
+ * thumbNew
+ */
+
+
 /* Public constructors */
 
 ;jQuery.fn.WeblitzPlateview = function (options) {
@@ -55,61 +87,59 @@ jQuery._WeblitzPlateview = function (container, options) {
   var _this = this;
   var thisid = this.self.attr('id');
 
-    var _reset = function (result, data) {
-      _this.self.html("");
-      var table = $('<table></table>').appendTo(_this.self);
-      var tr = $('<tr></tr>').appendTo(table);
-      tr.append('<th>&nbsp;</th>');
-      for (i in data.collabels) {
-	  tr.append('<th>'+data.collabels[i]+'</th>');
+  var _reset = function (result, data) {
+    _this.self.html("");
+    var table = $('<table></table>').appendTo(_this.self);
+    var tr = $('<tr></tr>').appendTo(table);
+    tr.append('<th>&nbsp;</th>');
+    for (i in data.collabels) {
+      tr.append('<th>'+data.collabels[i]+'</th>');
+    }
+    var tclick = function (tdata,thumb) {
+      return function () {
+        _this.self.trigger('thumbClick', [tdata, this]);
       }
-      var tclick = function (tdata,thumb) {
-	  return function () {
-              _this.self.trigger('thumbClick', [tdata, this]);
-	  }
-      };
-      for (i in data.rowlabels) {
-	  tr = $('<tr></tr>').appendTo(table);
-          tr.append('<th>'+data.rowlabels[i]+'</th>');
-          for (j in data.grid[i]) {
-              if (data.grid[i][j] == null) {
-		  tr.append('<td><div class="placeholder" style="width:'+opts.width+'px;height:'+opts.height+'px;line-height:'+opts.height+'px;">&nbsp;</div></td>');
-	      } else {
-                  data.grid[i][j]._wellpos = data.rowlabels[i]+data.collabels[j];
-                  var parentPrefix = '';
-                  if (opts.useParentPrefix) {
-                      parentPrefix = thisid+'-';
-                  }
-		  var td = $('<td class="well" id="'+parentPrefix+'well-'+data.grid[i][j].wellId+'"><div class="waiting" style="width:'+opts.width+'px;height:'+opts.height+'px;"></div><img id="'+parentPrefix+'image-'+data.grid[i][j].id+'" class="loading" src="'+ data.grid[i][j].thumb_url+'" name="'+(data.rowlabels[i] + data.collabels[j])+'"></td>');
-		  $('img', td)
-		      .click(tclick(data.grid[i][j]))
-		      .load(function() { 
-                                $(this).removeClass('loading').siblings().remove();
-                          _this.self.trigger('thumbLoad', [$(this).parent(), $(this)]);
-                           })
-		      .data('wellpos', data.rowlabels[i] + data.collabels[j]);
-		  tr.append(td);
-		  _this.self.trigger('thumbNew', [data.grid[i][j], $('img', td)]);
-	      }
-	  }
+    };
+    for (i in data.rowlabels) {
+      tr = $('<tr></tr>').appendTo(table);
+      tr.append('<th>'+data.rowlabels[i]+'</th>');
+      for (j in data.grid[i]) {
+        if (data.grid[i][j] == null) {
+        tr.append('<td><div class="placeholder" style="width:'+opts.width+'px;height:'+opts.height+'px;line-height:'+opts.height+'px;">&nbsp;</div></td>');
+        } else {
+          data.grid[i][j]._wellpos = data.rowlabels[i]+data.collabels[j];
+          var parentPrefix = '';
+          if (opts.useParentPrefix) {
+              parentPrefix = thisid+'-';
+          }
+          var td = $('<td class="well" id="'+parentPrefix+'well-'+data.grid[i][j].wellId+'"><div class="waiting" style="width:'+opts.width+'px;height:'+opts.height+'px;"></div><img id="'+parentPrefix+'image-'+data.grid[i][j].id+'" class="loading" src="'+ data.grid[i][j].thumb_url+'" name="'+(data.rowlabels[i] + data.collabels[j])+'"></td>');
+          $('img', td)
+            .click(tclick(data.grid[i][j]))
+            .load(function() { 
+              $(this).removeClass('loading').siblings().remove();
+              _this.self.trigger('thumbLoad', [$(this).parent(), $(this)]);
+            })
+            .data('wellpos', data.rowlabels[i] + data.collabels[j]);
+          tr.append(td);
+          _this.self.trigger('thumbNew', [data.grid[i][j], $('img', td)]);
+        }
       }
-  }
+    }
+  };
 
   this.load = function (pid,field) {
-      $('table img', _this.self).remove();
-      $('table div.placeholder', _this.self).removeClass('placeholder').addClass('loading');
-      if (field === undefined) {
-	  field = 0;
-      }
-      gs_json(opts.baseurl+'/plate/'+pid+'/'+field+'/', {size:opts.size}, _reset);
-      //gs_modalJson(opts.baseurl+'/plate/'+pid+'/'+field+'/?callback=?', {}, _reset);
-      //jQuery.getJSON(opts.baseurl+'/plate/'+pid+'/'+field+'/?callback=?', _reset);
-  }
+    $('table img', _this.self).remove();
+    $('table div.placeholder', _this.self).removeClass('placeholder').addClass('loading');
+    if (field === undefined) {
+      field = 0;
+    }
+    gs_json(opts.baseurl+'/plate/'+pid+'/'+field+'/', {size:opts.size}, _reset);
+  };
 
   this.setFocus = function (elm, evt) {
     if (!$(this).data('noFocus')) {
-	$('img', elm.parents('table').eq(0)).removeClass('pv-focus');
-	elm.addClass('pv-focus');
+      $('img', elm.parents('table').eq(0)).removeClass('pv-focus');
+      elm.addClass('pv-focus');
     }
   };
 
@@ -117,7 +147,7 @@ jQuery._WeblitzPlateview = function (container, options) {
     if (elm) {
       elm.removeClass('pv-focus');
     } else {
-	$('img', _this.self).removeClass('pv-focus');
+      $('img', _this.self).removeClass('pv-focus');
     }
-  }
-}
+  };
+};
