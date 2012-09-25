0bf410dd5ed0e0cf474aa66231ecbf9f71e3ac91
jmoore <josh@glencoesoftware.com>
2012-09-25 10:05:10 +0100

    Remove comments from mv stmts

--- prep.sh
+++ prep.sh
@@ -6,9 +6,9 @@ set -x
 
 VERSION=$1
 BUILD=$2
-#mv virtualbox/omero-vm-latest-build.ova "virtualbox/omero-vm-$VERSION.ova"
-#mv virtualbox/omero-vm-latest-build.ova.md5sum "virtualbox/omero-vm-$VERSION.ova.md5sum"
 
+mv virtualbox/omero-vm-latest-build.ova "virtualbox/omero-vm-$VERSION.ova"
+mv virtualbox/omero-vm-latest-build.ova.md5sum "virtualbox/omero-vm-$VERSION.ova.md5sum"
 perl -i -pe "s/latest-build/$VERSION/" "virtualbox/omero-vm-$VERSION.ova.md5sum"
 
 
