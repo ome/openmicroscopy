5daa00e8daf8fc8e3f5463f41754c3b5f6ac33a8
jmoore <josh@glencoesoftware.com>
2012-10-01 10:30:17 +0200

    Run less of broken test

--- components/tools/OmeroPy/test/integration/repository.py
+++ components/tools/OmeroPy/test/integration/repository.py
@@ -25,33 +25,34 @@ class TestRepository(lib.ITest):
         self.assert_( len(repoMap.descriptions) > 1 )
         self.assert_( len(repoMap.proxies) > 1 )
 
-        repoPrx = repoMap.values()[0]
+        repoPrx = repoMap.proxies[0]
         self.assert_( repoPrx ) # Could be None
 
         # This is a write-only (no read, no config)
         # version of this service.
-        rawFileStore = repoPrx.write(remote_file)
-        try:
-            offset = 0
-            file = open(test_file,"rb")
+        if False:
+            rawFileStore = repoPrx.write(remote_file)
             try:
-                while True:
-                    block = file.read(block_size)
-                    if not block:
-                        break
-                    rawFileStore.write(block, offset, len(block))
-                    offset += len(block)
+                offset = 0
+                file = open(test_file,"rb")
+                try:
+                    while True:
+                        block = file.read(block_size)
+                        if not block:
+                            break
+                        rawFileStore.write(block, offset, len(block))
+                        offset += len(block)
+                finally:
+                    file.close()
             finally:
-                file.close()
-        finally:
-            rawFileStore.close()
+                rawFileStore.close()
 
-        write_end = time.time()
+            write_end = time.time()
 
-        # Check the SHA1
-        file = repoPrx.load(remote_file)
-        sha1_remote = file.sha1.val
-        sha1_local = self.client.sha1(test_file)
+            # Check the SHA1
+            file = repoPrx.load(remote_file)
+            sha1_remote = file.sha1.val
+            sha1_local = self.client.sha1(test_file)
 
 
         self.fail("HOW ARE WE CHECKING SHA1 HERE")
