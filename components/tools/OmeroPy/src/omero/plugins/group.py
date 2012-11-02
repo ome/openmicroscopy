12a8d0bf9b0d039b2c79d33f5acf49ecf7dc14c0
Sebastien Besson <seb.besson@gmail.com>
2012-11-02 14:30:33 +0000

    Move getids utility functions to UserGroupControl class
    Check user/member/owner distinction in subcommand help messages

--- components/tools/OmeroPy/src/omero/plugins/group.py
+++ components/tools/OmeroPy/src/omero/plugins/group.py
@@ -66,23 +66,23 @@ More information is available at:
         list = parser.add(sub, self.list, "List current groups")
         list.add_argument("--long", action="store_true", help = "Print comma-separated list of all groups, not just counts")
 
-        copyusers = parser.add(sub, self.copyusers, "Copy the members/owners of one group to another group")
-        copyusers.add_argument("from_group", help = "ID or name of the source group  whose members/owners will be copied")
-        copyusers.add_argument("to_group", help = "ID or name of the target group  which will have new members/owners added")
-        copyusers.add_argument("--owner", action="store_true", default=False, help="Copy the group owners. If not set, copy the group members")
+        copyusers = parser.add(sub, self.copyusers, "Copy the users of one group to another group")
+        copyusers.add_argument("from_group", help = "ID or name of the source group whose users will be copied")
+        copyusers.add_argument("to_group", help = "ID or name of the target group which will have new users added")
+        copyusers.add_argument("--owner", action="store_true", default=False, help="Copy the group owners only")
 
-        adduser = parser.add(sub, self.adduser, "Add one or more users to a group member/owner list")
-        adduser.add_argument("USER", metavar="user", nargs="+", help = "ID or name of the user to add to the group owner list")
-        adduser.add_argument("--owner", action="store_true", default=False, help="Add the users as owners of the group. If not set, add the users as members of the group")
+        adduser = parser.add(sub, self.adduser, "Add one or more users to a group")
+        adduser.add_argument("USER", metavar="user", nargs="+", help = "ID or name of the user to add to the group")
+        adduser.add_argument("--owner", action="store_true", default=False, help="Add the users as owners of the group")
         
-        removeuser = parser.add(sub, self.removeuser, "Remove one or more users from a group member/owner list")
-        removeuser.add_argument("USER", metavar="user", nargs="+", help = "ID or name of the user to remove from the group member list")
-        removeuser.add_argument("--owner", action="store_true", default=False, help="Remove the user from the group owner list. If not set, remove the user from the group member list")
+        removeuser = parser.add(sub, self.removeuser, "Remove one or more users from a group")
+        removeuser.add_argument("USER", metavar="user", nargs="+", help = "ID or name of the user to remove")
+        removeuser.add_argument("--owner", action="store_true", default=False, help="Remove the users from the group owner list")
         
         for x in (adduser, removeuser):
             group = x.add_mutually_exclusive_group()
-            group.add_argument("--id", metavar="group", help="ID of the group.")
-            group.add_argument("--name", metavar="group", help="Name of the group.")
+            group.add_argument("--id", metavar="group", help="ID of the group")
+            group.add_argument("--name", metavar="group", help="Name of the group")
         
 
     def parse_perms(self, args):
@@ -173,12 +173,14 @@ More information is available at:
             tb = TableBuilder("id", "name", "perms", "# of owners", "# of members")
         for group in groups:
             row = [group.id.val, group.name.val, str(group.details.permissions)]
+            ownerids = self.getownerids(group)
+            memberids = self.getmemberids(group)
             if args.long:
-                row.append(",".join(sorted([str(x.child.id.val) for x in group.copyGroupExperimenterMap() if x.owner.val])))
-                row.append(",".join(sorted([str(x.child.id.val) for x in group.copyGroupExperimenterMap() if not x.owner.val])))
+                row.append(",".join(sorted([str(x) for x in ownerids])))
+                row.append(",".join(sorted([str(x) for x in memberids])))
             else:
-                row.append(len([x for x in group.copyGroupExperimenterMap() if x.owner.val]))
-                row.append(len([x for x in group.copyGroupExperimenterMap() if not x.owner.val]))
+                row.append(len(ownerids))
+                row.append(len(memberids))
             tb.row(*tuple(row))
         self.ctx.out(str(tb.build()))
 
@@ -195,20 +197,20 @@ More information is available at:
     def filter_users(self, uids, group, owner = False, join = True):
 
         if owner:
-            uid_list = [x.child.id.val for x in  group.copyGroupExperimenterMap() if x.owner.val]
-            role = "owner"
+            uid_list = self.getownerids(group)
+            relation = "owner of"
         else:
-            uid_list = [x.child.id.val for x in  group.copyGroupExperimenterMap()]
-            role = "member"
+            uid_list = self.getuserids(group)
+            relation = "in"
             
         for uid in list(uids):            
             if join:
                 if uid in uid_list:
-                    self.ctx.out("%s already %s of group %s" % (uid, role, group.id.val))
+                    self.ctx.out("%s is already %s group %s" % (uid, relation, group.id.val))
                     uids.remove(uid)
             else:
                 if uid not in uid_list:
-                    self.ctx.out("%s not %s of group %s" % (uid, role, group.id.val))
+                    self.ctx.out("%s is not %s group %s" % (uid, relation, group.id.val))
                     uids.remove(uid)
         return uids
 
@@ -220,9 +222,9 @@ More information is available at:
         t_gid, t_grp = self.find_group(a, args.to_group)
 
         if args.owner:
-            uids = [x.child.id.val for x in f_grp.copyGroupExperimenterMap() if x.owner.val]
+            uids = self.getownerids(f_grp)
         else:
-            uids = [x.child.id.val for x in f_grp.copyGroupExperimenterMap()]
+            uids = self.getuserids(f_grp)
         uids = self.filter_users(uids, t_grp, args.owner, True)
         
         if args.owner:
@@ -230,7 +232,7 @@ More information is available at:
             self.ctx.out("Owners of %s copied to %s" % (args.from_group, args.to_group))
         else:
             self.addusersbyid(a, t_grp, uids)
-            self.ctx.out("Members of %s copied to %s" % (args.from_group, args.to_group))
+            self.ctx.out("Users of %s copied to %s" % (args.from_group, args.to_group))
 
     def adduser(self, args):
         import omero
