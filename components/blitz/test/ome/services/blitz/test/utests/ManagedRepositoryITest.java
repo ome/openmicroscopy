16b60cd604beb35fdcf5a8e3bd05699ba3e26dc7
Blazej Pindelski <bpindelski@dundee.ac.uk>
2012-11-06 14:04:49 +0000

    Fix MRepoITest for JDK 5

--- components/blitz/test/ome/services/blitz/test/utests/ManagedRepositoryITest.java
+++ components/blitz/test/ome/services/blitz/test/utests/ManagedRepositoryITest.java
@@ -227,7 +227,7 @@ public class ManagedRepositoryITest extends MockObjectTestCase {
     @Test
     public void testGetStringFromTokenReturnsEmptyStringOnNullToken() {
         String actual = this.tmri.getStringFromToken("", null, null);
-        Assert.assertTrue(actual.isEmpty());
+        Assert.assertTrue(actual.length() == 0);
     }
 
     @Test
