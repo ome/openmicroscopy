09d3a482e027e30918cc1844f36ce787300ea42c
jburel <j.burel@dundee.ac.uk>
2012-11-16 16:29:56 +0000

    Remove unused import.

--- components/insight/SRC/org/openmicroscopy/shoola/agents/measurement/view/IntensityView.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/measurement/view/IntensityView.java
@@ -36,7 +36,6 @@ import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
