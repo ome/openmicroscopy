311c948ca7f073da6adc6b469cb9cf54bd3f9be6
Mark Carroll <m.t.b.carroll@dundee.ac.uk>
2012-12-03 15:03:29 +0000

    Draft of incomplete code for rewriting path names.

---
+++ components/blitz/src/ome/services/blitz/repo/path/FilePathTransformer.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (C) 2012 University of Dundee & Open Microscopy Environment.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package ome.services.blitz.repo.path;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Transform client-local {@link java.io.File} to repository {@link FsFile} path,
+ * and between repository {@link FsFile} path and server-local {@link java.io.File}.
+ * The directory topology of the repository {@link FsFile} paths is matched in the
+ * server-local {@link java.io.File}s even though the path components may look different.
+ * @author m.t.b.carroll@dundee.ac.uk
+ * @since 4.5
+ */
+public class FilePathTransformer {
+	/* Default field and method visibility is used for access by unit tests. */
+	
+	/* the OMERO data directory */
+	private String omeroDataDir;
+	/* where the repository should store its files relative to the OMERO data directory */
+	private String fsSubDir;
+	/* where the repository should store its files */
+	File baseDirFile;
+	/* the parent path components that should be omitted from repository paths */
+	private FsFile baseDirFsFile;
+	
+	/* -- SERVER-SIDE METHODS -- */
+	
+	/**
+	 * Given a repository path, returns the corresponding server-local {@link java.io.File}.
+	 * Must be executed server-side.
+	 * @param fsFile a repository path
+	 * @return the corresponding server-local {@link java.io.File}
+	 */
+	public File getServerFileFromFsFile(FsFile fsFile) {
+		return fsFile.toFile(this.baseDirFile);
+	}
+	
+	/**
+	 * Given a server-local {@link java.io.File}, returns the corresponding repository path.
+	 * Must be executed server-side.
+	 * @param serverFile a server-local {@link java.io.File} within the repository
+	 * @return the corresponding repository path
+	 * @throws IOException if the absolute path of the {@link java.io.File} could not be found
+	 */
+	public FsFile getFsFileFromServerFile(File serverFile) throws IOException {
+		final FsFile fullFsFile = new FsFile(serverFile);
+		final FsFile childFsFile = fullFsFile.getPathFrom(this.baseDirFsFile);
+		if (childFsFile == null)
+			throw new IllegalArgumentException("server files must be within the repository");
+		return childFsFile;
+	}
+	
+	/* -- CLIENT-SIDE METHODS -- */
+
+	/**
+	 * Given a client-local {@link java.io.File}, and a path component depth,
+	 * returns the corresponding repository path. Must be executed client-side.
+	 * @param clientFile a client-local {@link java.io.File}
+	 * @param depth the path component depth (including filename)
+	 * @return the corresponding repository path, intended to be safe cross-platform
+	 * @throws IOException if the absolute path of the {@link java.io.File} could not be found
+	 */
+	public FsFile getFsFileFromClientFile(File clientFile, int depth) throws IOException {
+		if (depth < 1) 
+			throw new IllegalArgumentException("path depth must be strictly positive");
+		final StringTransformer st = MakePathComponentSafe.getInstance();
+		return new FsFile(new FsFile(clientFile), depth).transform(st);
+	}
+	
+	/**
+	 * Test if the given path component depth suffices for disambiguating the given set of
+	 * {@link java.io.File}s. Must be executed client-side.
+	 * @param files a set of {@link java.io.File}s
+	 * @param depth a path component depth
+	 * @return if the depth allows the {@link java.io.File}s to each be named distinctly
+	 * @throws IOException if the absolute path of any of the {@link java.io.File}s could not be found
+	 */
+	private boolean isDepthSufficient(Collection<File> files, int depth) throws IOException {
+		final Set<FsFile> fsPaths = new HashSet<FsFile>();
+		for (final File file : files)
+			if (!fsPaths.add(getFsFileFromClientFile(file, depth)))
+				return false;
+		return true;
+	}
+	
+	/**
+	 * Get the smallest path component depth that allows the given set of {@link java.io.File}s
+	 * to be disambiguated. Must be executed client-side.
+	 * @param files a set of {@link java.io.File}s
+	 * @return the minimum depth for disambiguating the {@link java.io.File}s, no less than 1
+	 * @throws IOException if the absolute path of any of the {@link java.io.File}s could not be found
+	 */
+	public int getMinimumDepth(Collection<File> files) throws IOException {
+		if (files.size() < 2)
+			/* must be a strictly positive integer */
+			return 1;
+		if (!isDepthSufficient(files, Integer.MAX_VALUE))
+			throw new IllegalArgumentException("file set is not unique, so no depth can fix it");
+		int depth;
+		for (depth = 1; !isDepthSufficient(files, depth); depth++);
+		return depth;
+	}
+	
+	/* -- SPRING-BASED SETUP -- */
+	
+	/**
+	 * Set the OMERO data directory. Expected to be set as part of Spring bean initialization.
+	 * @param omeroDataDir the OMERO data directory
+	 */
+	public void setOmeroDataDir(String omeroDataDir) {
+		this.omeroDataDir = omeroDataDir;
+	}
+	
+	/**
+	 * Set the repository subdirectory in the OMERO data directory.
+	 * Expected to be set as part of Spring bean initialization.
+	 * @param fsSubDir the subdirectory in which the repository stores files
+	 */
+	public void setFsSubDir(String fsSubDir) {
+		this.fsSubDir = fsSubDir;
+	}
+	
+	/**
+	 * Set the repository base directory.
+	 * Expected to be called through Spring as the bean's <code>init-method</code>.
+	 * @throws IOException if the absolute path of the base directory could not be found
+	 */
+	public void calculateBaseDir() throws IOException {
+		this.baseDirFile = new File(this.omeroDataDir, this.fsSubDir);
+		if (!this.baseDirFile.isDirectory())
+			throw new IllegalArgumentException(this.baseDirFile.getPath() + " must specify an existing FS repository directory");
+		this.baseDirFsFile = new FsFile(this.baseDirFile);
+	}
+}
---
+++ components/blitz/src/ome/services/blitz/repo/path/MakePathComponentSafe.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2012 University of Dundee & Open Microscopy Environment.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package ome.services.blitz.repo.path;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author m.t.b.carroll@dundee.ac.uk
+ * @since 4.5
+ */
+public class MakePathComponentSafe implements StringTransformer {
+	static final Map<Integer, Integer> transformationMatrix;
+	static final Set<String> unsafePrefixes;
+	static final Set<String> unsafeSuffixes;
+	static final Set<String> unsafeNames;
+	
+	static final char safeCharacter = '_';
+	
+	private static final StringTransformer instance = new MakePathComponentSafe();
+	
+	private static int getCodePoint(char character) {
+		final char[] singletonCharacterArray = new char[] {character};
+		return Character.codePointAt(singletonCharacterArray, 0);
+	}
+	
+	static {
+		transformationMatrix = new HashMap<Integer, Integer>();
+		final int underscore = getCodePoint(safeCharacter);
+		for (int codePoint = 0; codePoint < 0x100; codePoint++)
+			if (Character.getType(codePoint) == Character.CONTROL)
+				transformationMatrix.put(codePoint, underscore);
+		transformationMatrix.put(0x22, getCodePoint('\'')); // "
+		transformationMatrix.put(0x2A, getCodePoint('x'));  // *
+		transformationMatrix.put(0x2F, getCodePoint('!'));  // /
+		transformationMatrix.put(0x3A, getCodePoint(';'));  // :
+		transformationMatrix.put(0x3C, getCodePoint('['));  // <
+		transformationMatrix.put(0x3E, getCodePoint(']'));  // >
+		transformationMatrix.put(0x3F, getCodePoint('%'));  // ?
+		transformationMatrix.put(0x5C, getCodePoint('!'));  // \
+		transformationMatrix.put(0x7C, getCodePoint('!'));  // |
+		
+		unsafePrefixes = new HashSet<String>();
+		unsafePrefixes.add(".");
+		unsafePrefixes.add("$");
+		
+		unsafeSuffixes = new HashSet<String>();
+		unsafeSuffixes.add(".");
+		
+		unsafeNames = new HashSet<String>();
+		unsafeNames.add("AUX");
+		unsafeNames.add("CLOCK$");
+		unsafeNames.add("CON");
+		unsafeNames.add("NUL");
+		unsafeNames.add("PRN");
+		for (char number = '0'; number <= '9'; number++) {
+			unsafeNames.add("COM" + number);
+			unsafeNames.add("LPT" + number);
+		}
+		
+		for (final String unsafeName : unsafeNames)
+			unsafePrefixes.add(unsafeName + ".");
+	}
+	
+	private MakePathComponentSafe() { }
+	
+	public static StringTransformer getInstance() {
+		return instance;
+	}
+	
+	@Override
+	public String apply(String string) {
+		final String ucString = string.toUpperCase();
+		for (final String unsafeName : unsafeNames)
+			if (ucString.equals(unsafeName))
+				string = string + safeCharacter;
+		for (final String unsafePrefix : unsafePrefixes)
+			if (ucString.startsWith(unsafePrefix))
+				string = safeCharacter + string;
+		for (final String unsafeSuffix : unsafeSuffixes)
+			if (ucString.endsWith(unsafeSuffix))
+				string = string + safeCharacter;
+		final int codePointCount = string.codePointCount(0, string.length());
+		final int[] codePointArray = new int[codePointCount];
+		for (int codePointIndex = 0; codePointIndex < codePointCount; codePointIndex++) {
+			final int originalCodePoint = string.codePointAt(string.offsetByCodePoints(0, codePointIndex));
+			final Integer substituteCodePoint = transformationMatrix.get(originalCodePoint);
+			if (substituteCodePoint == null)
+				codePointArray[codePointIndex] = originalCodePoint;
+			else
+				codePointArray[codePointIndex] = substituteCodePoint;
+		}
+		return new String(codePointArray, 0, codePointCount);
+	}
+}
