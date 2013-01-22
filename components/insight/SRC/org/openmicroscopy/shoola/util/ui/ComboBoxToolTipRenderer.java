2668f154e6a0044fc002a308170344aaa0e53a03
jburel <j.burel@dundee.ac.uk>
2013-01-22 09:27:45 +0000

    Set enabled flag if cannot link to the dataset.

--- components/insight/SRC/org/openmicroscopy/shoola/util/ui/ComboBoxToolTipRenderer.java
+++ components/insight/SRC/org/openmicroscopy/shoola/util/ui/ComboBoxToolTipRenderer.java
@@ -21,12 +21,6 @@
  */
 package org.openmicroscopy.shoola.util.ui;
 
-/** 
- * Provides a wrapped renderer for displaying tooltip information on mouse hover
- *
- * @author Scott Littlewood, <a href="mailto:sylittlewood@dundee.ac.uk">sylittlewood@dundee.ac.uk</a>
- * @since Beta4.4
- */
 import java.awt.Component;
 import java.util.List;
 
@@ -34,9 +28,24 @@ import javax.swing.DefaultListCellRenderer;
 import javax.swing.JComponent;
 import javax.swing.JList;
 
-public class ComboBoxToolTipRenderer extends DefaultListCellRenderer {
-	List<String> tooltips;
+import org.openmicroscopy.shoola.agents.util.browser.DataNode;
 
+/** 
+ * Provides a wrapped renderer for displaying tooltip information on mouse hover
+ *
+ * @author Scott Littlewood, <a href="mailto:sylittlewood@dundee.ac.uk">sylittlewood@dundee.ac.uk</a>
+ * @since Beta4.4
+ */
+public class ComboBoxToolTipRenderer extends DefaultListCellRenderer {
+	
+	/** The tool tips to set.*/
+	private List<String> tooltips;
+
+	/**
+	 * Sets the tool tip.
+	 * @see DefaultListCellRenderer#getListCellRendererComponent(JList,
+	 * Object, int, boolean, boolean)
+	 */
 	public Component getListCellRendererComponent(JList list, Object value,
 			int index, boolean isSelected, boolean cellHasFocus) {
 
@@ -47,13 +56,16 @@ public class ComboBoxToolTipRenderer extends DefaultListCellRenderer {
 				&& tooltips.size() > index) {
 			list.setToolTipText(tooltips.get(index));
 		}
+		if (value instanceof DataNode) {
+			comp.setEnabled(((DataNode) value).getDataObject().canLink());
+		}
 		return comp;
 	}
 
 	/**
 	 * Populates the renderer with the tooltips provided
 	 * 
-	 * @param tooltips
+	 * @param tooltips The value to set.
 	 */
 	public void setTooltips(List<String> tooltips) {
 		this.tooltips = tooltips;
