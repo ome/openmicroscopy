e117c73c42af5e719d61ce378239a9827647e7b3
Roger Leigh <r.leigh@dundee.ac.uk>
2013-01-24 12:09:00 +0000

    All setuptools usage uses /lib/repository

--- components/tools/OmeroWeb/setup.py
+++ components/tools/OmeroWeb/setup.py
@@ -5,8 +5,12 @@
 
 """
 
+for tools in glob.glob("../../../lib/repository/setuptools*.egg"):
+    if tools.find(".".join(map(str, sys.version_info[0:2]))) > 0:
+       sys.path.insert(0, tools)
+
 from ez_setup import use_setuptools
-use_setuptools()
+use_setuptools(to_dir='../../../lib/repository')
 from setuptools import setup
 from omero_version import omero_version as ov
 
