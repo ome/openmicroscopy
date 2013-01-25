99feaad3d1622e22f2bb0f9fec6c0d5e26af8745
Sebastien Besson <seb.besson@gmail.com>
2013-01-25 10:27:17 +0000

    Add input check and initalize arry in toJavaList() and toMatlabList()
    Add unit tests for Java-Matlab array conversions

--- components/tools/OmeroM/helper/toJavaList.m
+++ components/tools/OmeroM/helper/toJavaList.m
@@ -1,5 +1,12 @@
 function [javaList] = toJavaList(matlabList)
+% Convert a MATLAB vector into a Java ArrayList
 
+% Check input
+assert(isvector(matlabList) || isempty(matlabList),...
+    'OMERO:toJavaList:wrongInputType',...
+    'Input must be a vector');
+
+% Create Java list
 javaList = java.util.ArrayList;
 for i=1:length(matlabList)
     javaList.add(matlabList(i));
