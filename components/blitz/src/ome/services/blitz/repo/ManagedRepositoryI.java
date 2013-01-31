4b39b76a18630de6a586d66dae836f8c7a61940c
Mark Carroll <m.t.b.carroll@dundee.ac.uk>
2013-01-31 14:48:43 +0000

    Elide errant import of Pair class.

--- components/blitz/src/ome/services/blitz/repo/ManagedRepositoryI.java
+++ components/blitz/src/ome/services/blitz/repo/ManagedRepositoryI.java
@@ -31,8 +31,6 @@ import org.apache.commons.lang.text.StrSubstitutor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.sun.tools.javac.util.Pair;
-
 import Ice.Current;
 
 import ome.formats.importer.ImportConfig;
