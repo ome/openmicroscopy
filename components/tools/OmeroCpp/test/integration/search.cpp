5b09d7e3e714ecbeb9e8a1dbb70c17bf10e590fd
Jesse Corrington <jesse.corrington@gmail.com>
2013-02-15 11:50:19 -0800

    Disable tests that are failing because of server bugs

--- components/tools/OmeroCpp/test/integration/search.cpp
+++ components/tools/OmeroCpp/test/integration/search.cpp
@@ -1496,7 +1496,9 @@ TEST(SearchTest, testOnlyAnnotatedWith ) {
     assertResults(1, search);
 }
 
-TEST(SearchTest, testOnlyAnnotatedWithMultiple ) {
+// Test failing due to Hibernate bug
+// https://hibernate.onjira.com/browse/HHH-879
+TEST(SearchTest, DISABLED_testOnlyAnnotatedWithMultiple ) {
 
     try {
     SearchFixture f;
@@ -1871,9 +1873,6 @@ TEST(SearchTest, testFetchAnnotations ) {
     }
 }
 
-// bugs
-// =========================================================================
-
 TEST(SearchTest, testCommentAnnotationDoesntTryToLoadUpdateEvent ) {
     SearchFixture f;
     f.login();
@@ -1890,7 +1889,12 @@ TEST(SearchTest, testCommentAnnotationDoesntTryToLoadUpdateEvent ) {
     assertResults(1, search);
 }
 
-TEST(SearchTest, testExperimenterDoesntTryToLoadOwner ) {
+// bugs
+// =========================================================================
+
+// Test failing due to OMERO server bug
+// https://trac.openmicroscopy.org.uk/ome/ticket/10408
+TEST(SearchTest, DISABLED_testExperimenterDoesntTryToLoadOwner ) {
     SearchFixture f;
     SearchPrx search = f.search();
     search->onlyType("Experimenter");
@@ -1898,7 +1902,9 @@ TEST(SearchTest, testExperimenterDoesntTryToLoadOwner ) {
     assertAtLeastResults(1, search);
 }
 
-TEST(SearchTest, testLookingForExperimenterWithOwner ) {
+// Test failing due to OMERO server bug
+// https://trac.openmicroscopy.org.uk/ome/ticket/10408
+TEST(SearchTest, DISABLED_testLookingForExperimenterWithOwner ) {
     SearchFixture f;
     SearchPrx search = f.search();
     search->onlyType("Experimenter");
