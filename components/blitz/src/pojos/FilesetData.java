f8a78181acb99c5c55ab6c80f8abe9733398ba2f
jburel <j.burel@dundee.ac.uk>
2013-02-20 16:47:26 +0000

    Add new files.

---
+++ components/blitz/src/pojos/FilesetData.java
@@ -0,0 +1,61 @@
+/*
+ * pojos.FilesetData 
+ *
+ *------------------------------------------------------------------------------
+ *  Copyright (C) 2012 University of Dundee & Open Microscopy Environment.
+ *  All rights reserved.
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *------------------------------------------------------------------------------
+ */
+package pojos;
+
+
+
+//Java imports
+
+//Third-party libraries
+
+//Application-internal dependencies
+import omero.model.Fileset;
+
+/** 
+ * Wraps a file set object.
+ *
+ * @author Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * <a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @since 4.4
+ */
+public class FilesetData
+	extends DataObject
+{
+
+	/** The file set hosted by this component.*/
+	private Fileset set;
+	
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param set The value to set.
+	 */
+	public FilesetData(Fileset set)
+	{
+		if (set == null)
+			throw new IllegalArgumentException("No set specified");
+		this.set = set;
+	}
+	
+}
