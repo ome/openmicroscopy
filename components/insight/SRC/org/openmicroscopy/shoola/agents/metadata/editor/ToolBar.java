f56094bda8d97ccb88084a43cdf32d06cb4dae44
jburel <j.burel@dundee.ac.uk>
2013-02-21 15:31:23 +0000

    Remove unused import.

--- components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/editor/ToolBar.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/editor/ToolBar.java
@@ -29,8 +29,6 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
