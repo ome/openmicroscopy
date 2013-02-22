a2e445ccbf60d4e97a6636c27c012cb505098dac
Scott Littlewood <scottlittlewood@gmail.com>
2013-02-22 16:21:40 +0000

    Introduce ComboBox Selection Helper class and model

---
+++ components/insight/SRC/org/openmicroscopy/shoola/util/ui/Selectable.java
@@ -0,0 +1,74 @@
+/*
+ *------------------------------------------------------------------------------
+ *  Copyright (C) 2006-2012 University of Dundee & Open Microscopy Environment.
+ *  All rights reserved.
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.util.ui;
+
+/** 
+ * Provides a wrapper class for a selectable option
+ *
+ * @author Scott Littlewood, <a href="mailto:sylittlewood@dundee.ac.uk">sylittlewood@dundee.ac.uk</a>
+ * @since Beta4.4
+ */
+public class Selectable<T> {
+
+	/** The wrapped object */
+	private T obj;
+	
+	/** The state of selection */
+	private boolean selectable;
+	
+	/**
+	 * Creates an instance of the selectable class
+	 * @param obj The object being wrapped
+	 * @param selectable Whether the item is selectable
+	 */
+	public Selectable(T obj, boolean selectable)
+	{
+		this.obj = obj;
+		this.selectable = selectable;
+	}
+	
+	/**
+	 * Return the wrapped object
+	 * @return
+	 */
+	public T getObject()
+	{
+		return obj;
+	}
+	
+	/**
+	 * Returns whether the item should be selectable
+	 * @return
+	 */
+	public boolean isSelectable()
+	{
+		return selectable;
+	}
+	
+	/**
+	 * Returns the String representation of the wrapped object.
+	 */
+	public String toString()
+	{
+		return obj.toString();
+	}
+}   
