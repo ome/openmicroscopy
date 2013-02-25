8b0dd181ff600351a46799e598c366666e3a1fce
Blazej Pindelski <bpindelski@dundee.ac.uk>
2013-02-25 16:50:13 +0000

    Add comments and licences and correct minor bugs

--- components/common/src/ome/util/checksum/SHA1ChecksumProviderImpl.java
+++ components/common/src/ome/util/checksum/SHA1ChecksumProviderImpl.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2013 University of Dundee & Open Microscopy Environment.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package ome.util.checksum;
 
 import com.google.common.hash.HashFunction;
@@ -12,21 +31,37 @@ import java.nio.channels.FileChannel;
 
 import ome.util.Utils;
 
+/**
+ * An implementation of the {@link ChecksumProvider} interface using SHA1
+ * as the secure hash algorithm.
+ *
+ * @author Blazej Pindelski, bpindelski at dundee.ac.uk
+ * @since 4.4.7
+ */
 public class SHA1ChecksumProviderImpl implements ChecksumProvider {
 
     private final HashFunction sha1 = Hashing.sha1();
 
     private static final int BYTEARRAYSIZE = 8192;
 
+    /**
+     * @see ChecksumProvider#getChecksum(byte[])
+     */
     public byte[] getChecksum(byte[] rawData) {
         return this.sha1.newHasher().putBytes(rawData).hash().asBytes();
     }
 
+    /**
+     * @see ChecksumProvider#getChecksum(ByteBuffer)
+     */
     public byte[] getChecksum(ByteBuffer byteBuffer) {
         throw new UnsupportedOperationException("provideChecksum() not"
                 + "implemented for ByteBuffer.");
     }
 
+    /**
+     * @see ChecksumProvider#getChecksum(String)
+     */
     public byte[] getChecksum(String filePath) {
         FileInputStream fis = null;
         FileChannel fch = null;
@@ -39,12 +74,8 @@ public class SHA1ChecksumProviderImpl implements ChecksumProvider {
             int byteCount;
             while (mbb.hasRemaining()) {
                 byteCount = Math.min(mbb.remaining(), BYTEARRAYSIZE);
-                if (byteCount < BYTEARRAYSIZE) {
-                    // This might be sub-optimal
-                    byteArray = new byte[byteCount];
-                }
                 mbb.get(byteArray, 0, byteCount);
-                sha1Hasher.putBytes(byteArray);
+                sha1Hasher.putBytes(byteArray, 0, byteCount);
             }
             return sha1Hasher.hash().asBytes();
         } catch (IOException io) {
