74a0970b8de13e5d4d15627faff958a196c4cc5e
Sebastien Besson <seb.besson@gmail.com>
2013-02-26 15:07:33 +0000

    Add helper function to create Rectangle shape

---
+++ components/tools/OmeroM/src/roi/createRectangle.m
@@ -0,0 +1,34 @@
+function rectangle = createRectangle(x, y, w, h)
+% Create a rectangle shape object from a set of coordinates and dimensions
+
+% Copyright (C) 2013 University of Dundee & Open Microscopy Environment.
+% All rights reserved.
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program; if not, write to the Free Software Foundation, Inc.,
+% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+% Check input
+ip = inputParser;
+ip.addRequired('x', @isscalar);
+ip.addRequired('y', @isscalar);
+ip.addRequired('w', @isscalar);
+ip.addRequired('h', @isscalar);
+ip.parse(x, y, w, h)
+
+% Create Rectangle shape
+rectangle = omero.model.RectI;
+rectangle.setX(rdouble(x));
+rectangle.setY(rdouble(y));
+rectangle.setWidth(rdouble(w));
+rectangle.setHeight(rdouble(h));
