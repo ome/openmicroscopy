0ad39ffb0222dcf5aab17c43c5709f8da7b8251e
Scott Littlewood <scottlittlewood@gmail.com>
2013-02-27 15:01:52 +0000

    Introduce display for ExperiementerData & display users in each group

---
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/fsimporter/chooser/ExperimenterDisplay.java
@@ -0,0 +1,45 @@
+/*
+ *------------------------------------------------------------------------------
+ *  Copyright (C) 2013 University of Dundee & Open Microscopy Environment.
+ *  All rights reserved.
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.agents.fsimporter.chooser;
+
+import pojos.ExperimenterData;
+
+/** 
+ * 
+ * Provides a wrapper to display the experimenter
+ * @author Scott Littlewood, <a href="mailto:sylittlewood@dundee.ac.uk">sylittlewood@dundee.ac.uk</a>
+ * @since 4.4
+ */
+public class ExperimenterDisplay  {
+
+	private ExperimenterData data;
+	
+	public ExperimenterDisplay(ExperimenterData data)
+	{
+		this.data = data;
+	}
+	
+	public String toString()
+	{
+		return String.format("%s %s", data.getFirstName(), data.getLastName());
+	}
+}
