d9994c41b652a7dac52a0a4a38a454e6b194a203
jburel <j.burel@dundee.ac.uk>
2013-03-11 12:11:36 +0000

    Review call to load details (see #10490)

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/OMEROGateway.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/OMEROGateway.java
@@ -5825,6 +5825,7 @@ class OMEROGateway
 			sb.append("left outer join fetch well.wellSamples as ws ");
 			sb.append("left outer join fetch ws.plateAcquisition as pa ");
 			sb.append("left outer join fetch ws.image as img ");
+			sb.append("left outer join fetch img.details.updateEvent as evt ");
 			sb.append("left outer join fetch img.pixels as pix ");
             sb.append("left outer join fetch pix.pixelsType as pt ");
             sb.append("where well.plate.id = :plateID");
@@ -5868,6 +5869,7 @@ class OMEROGateway
 				sb.append("left outer join fetch well.wellSamples as ws ");
 				sb.append("left outer join fetch ws.plateAcquisition as pa ");
 				sb.append("left outer join fetch ws.image as img ");
+				sb.append("left outer join fetch img.details.updateEvent as evt ");
 				sb.append("left outer join fetch img.pixels as pix ");
 	            sb.append("left outer join fetch pix.pixelsType as pt ");
 	            sb.append("where well.plate.id in (:plateIDs)");
