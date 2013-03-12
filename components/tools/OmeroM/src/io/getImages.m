fab5f9cc748be4cf71608402f1e02e1e8d110689
Sebastien Besson <seb.besson@gmail.com>
2013-03-12 21:05:42 +0000

    Allow getImages() to load images contained in projects/datasets
    Add an optional third argument `type` to getImages(). If type is project or
    dataset, getImages(session, ids, type) calls getProjects/getDatasets(session,
    ids) and reconstructs the list of images in the graph.

--- components/tools/OmeroM/src/io/getImages.m
+++ components/tools/OmeroM/src/io/getImages.m
@@ -8,10 +8,17 @@ function images = getImages(session, varargin)
 %   the input ids owned by the session user in the context of the session
 %   group.
 %
+%   images = getImages(session, ids, type) returns all the images contained
+%   in the objects of input type identified by the input ids and owned by
+%   the session user in the context of the session group.
+%
+%
 %   Examples:
 %
 %      images = getImages(session);
 %      images = getImages(session, ids);
+%      images = getImages(session, [], 'project);
+%      images = getImages(session, datasetIds, 'dataset');
 %
 % See also: GETOBJECTS, GETPROJECTS, GETDATASETS, GETPLATES
 
@@ -33,23 +40,46 @@ function images = getImages(session, varargin)
 % 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 % Input check
+types = {'image', 'dataset', 'project'};
 ip = inputParser;
 ip.addOptional('ids', [], @(x) isempty(x) || isvector(x));
+ip.addOptional('type', 'image', @(x) ismember(x, types));
 ip.parse(varargin{:});
 
-% Add the current user id to the loading parameters
-parameters = omero.sys.ParametersI();
-userId = session.getAdminService().getEventContext().userId;
-parameters.exp(rlong(userId));
+if strcmp(ip.Results.type, 'image')
+    % Add the current user id to the loading parameters
+    parameters = omero.sys.ParametersI();
+    userId = session.getAdminService().getEventContext().userId;
+    parameters.exp(rlong(userId));
+    
+    % Create container service to load objects
+    proxy = session.getContainerService();
 
-% Create container service to load objects
-proxy = session.getContainerService();
-if ~isempty(ip.Results.ids),
-    ids = toJavaList(ip.Results.ids, 'java.lang.Long');
-    imageList = proxy.getImages('omero.model.Image', ids, parameters);
+    if isempty(ip.Results.ids),
+        % Load all images belonging to current session user
+        imageList = proxy.getUserImages(parameters);
+    else
+        % Load all images specified by input ids
+        ids = toJavaList(ip.Results.ids, 'java.lang.Long');
+        imageList = proxy.getImages('omero.model.Image', ids, parameters);
+    end
+    
+    % Convert java.util.ArrayList into Matlab arrays
+    images = toMatlabList(imageList);
+    
 else
-    imageList = proxy.getUserImages(parameters);
+    % Get loaded projects/datasets 
+    if strcmp(ip.Results.type, 'project')
+        projects = getProjects(session, ip.Results.ids, true);
+        datasetList = arrayfun(@(x) toMatlabList(x.linkedDatasetList), projects,...
+            'UniformOutput', false);
+        datasets = [datasetList{:}];
+    elseif strcmp(ip.Results.type, 'dataset')
+        datasets = getDatasets(session, ip.Results.ids, true);
+    end
+    
+    % Reconstruct image lists from dataset array
+    imageList = arrayfun(@(x) toMatlabList(x.linkedImageList), datasets,...
+        'UniformOutput', false);
+    images = [imageList{:}];
 end
-
-% Convert java.util.ArrayList into Matlab arrays
-images = toMatlabList(imageList);
