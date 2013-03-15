07492880a34ba4f1e49374903d65a9da58e4e154
Jesse Corrington <jesse.corrington@gmail.com>
2013-03-15 08:39:34 -0700

    add basic rtype tests for all rtypes classes
    Conflicts:
    	components/tools/OmeroCpp/test/unit/rtypes.cpp

--- components/tools/OmeroCpp/test/unit/rtypes.cpp
+++ components/tools/OmeroCpp/test/unit/rtypes.cpp
@@ -25,6 +25,22 @@ TEST(RTypeTest, RBool)
     ASSERT_NE(t, f);
 }
 
+template <typename T>
+void assertRValues(T rv1, T rv2) {
+    ASSERT_LT(rv1, rv2);
+    ASSERT_LT(rv1, rv2);
+    ASSERT_NE(rv1, rv2);
+
+    ASSERT_LT(rv1->getValue(), rv2->getValue());
+    ASSERT_LT(rv1->getValue(), rv2->getValue());
+    ASSERT_NE(rv1->getValue(), rv2->getValue());
+
+    ASSERT_EQ(rv1->compare(rv2), -1);
+    ASSERT_EQ(rv2->compare(rv1), 1);
+    ASSERT_EQ(rv2->compare(rv2), 0);
+}
+
+
 TEST(RTypesTest, RDouble) {
     RDoublePtr d1 = rdouble(1.1);
     RDoublePtr d2 = rdouble(2.5);
@@ -34,44 +50,80 @@ TEST(RTypesTest, RDouble) {
     if (rd3)
         std::cout << "rd3  " << rd3->getValue() << std::endl;
 
-    ASSERT_LT(d1, d2);
-    ASSERT_NE(d1, d2);
-    ASSERT_EQ(d1->compare(d2), -1);
-    ASSERT_EQ(d2->compare(d1), 1);
-    ASSERT_EQ(d2->compare(rdouble(2.5)), 0);
+    assertRValues<RDoublePtr>(d1, d2);
 }
 
 TEST(RTypesTest, RFloat) {
     RFloatPtr f1 = rfloat(1.1);
     RFloatPtr f2 = rfloat(2.5);
 
-    ASSERT_LT(f1, f2);
-    ASSERT_NE(f1, f2);
+    assertRValues<RFloatPtr>(f1, f2);
 }
 
 TEST(RTypesTest, RInt) {
+    RIntPtr i1 = rint(1);
+    RIntPtr i2 = rint(2);
+
+    assertRValues<RIntPtr>(i1, i2);
 }
 
 TEST(RTypesTest, RLong) {
+    RLongPtr l1 = rlong(1);
+    RLongPtr l2 = rlong(2);
+
+    assertRValues<RLongPtr>(l1, l2);
 }
 
 TEST(RTypesTest, RTime) {
-}
+    RTimePtr t1 = rtime(1);
+    RTimePtr t2 = rtime(2);
 
-TEST(RTypesTest, RInternal) {
+    assertRValues<RTimePtr>(t1, t2);
 }
 
 TEST(RTypesTest, RString) {
+    RStringPtr s1 = rstring("abc");
+    RStringPtr s2 = rstring("def");
+
+    assertRValues<RStringPtr>(s1, s2);
 }
 
 TEST(RTypesTest, RArray) {
+    RArrayPtr a1 = rarray();
+    a1->add(rint(1));
+
+    RArrayPtr a2 = rarray();
+    a2->add(rint(2));
+
+    assertRValues<RArrayPtr>(a1, a2);
 }
 
 TEST(RTypesTest, RList) {
+    RListPtr l1 = rlist();
+    l1->add(rint(1));
+
+    RListPtr l2 = rlist();
+    l2->add(rint(2));
+
+    assertRValues<RListPtr>(l1, l2);
 }
 
 TEST(RTypesTest, RSet) {
+    RSetPtr s1 = rset();
+    s1->add(rint(1));
+
+    RSetPtr s2 = rset();
+    s2->add(rint(2));
+
+    assertRValues<RSetPtr>(s1, s2);
 }
 
 TEST(RTypesTest, RMap) {
+    RMapPtr m1 = rmap();
+    m1->put("a", rint(1));
+
+    RMapPtr m2 = rmap();
+    m2->put("b", rint(2));
+
+    assertRValues<RMapPtr>(m1, m2);
 }
