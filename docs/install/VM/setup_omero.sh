c1b1b4a7ed35067ad7f8316ccc177ae1189b7d89
Sebastien Besson <seb.besson@gmail.com>
2013-03-19 14:39:17 +0000

    Allow OMERO_BUILD to be set by environment variable

--- docs/install/VM/setup_omero.sh
+++ docs/install/VM/setup_omero.sh
@@ -3,6 +3,7 @@
 set -e -u -x
 
 PGPASSWORD=${PGPASSWORD:-"omero"}
+OMERO_BUILD=${OMERO_BUILD:-"OMERO-stable"}
 URL_RELEASE="http://cvs.openmicroscopy.org.uk/snapshots/omero/"
 RELEASE_ARCHIVE="OMERO.server-Beta-4.3.4.zip"
 RELEASE_FOLDER=${RELEASE_ARCHIVE%.zip}
@@ -11,7 +12,7 @@ DB_REVISION="0"
 OMERO_PATH="/home/omero/OMERO.server"
 OMERO_BIN=$OMERO_PATH/bin
 INSTALL_FOLDER="OMERO.server"
-OMERO_BUILD="http://hudson.openmicroscopy.org.uk/job/OMERO-trunk/"
+OMERO_BUILD_URL="http://hudson.openmicroscopy.org.uk/job/"$OMERO_BUILD"/lastSuccessfulBuild"
 
 readAPIValue() {
     URL=$1; shift
@@ -22,16 +23,15 @@ echo "Grabbing last successful QA Build of OMERO.server"
 DL_ARCHIVE=""
 if [ "x$DL_ARCHIVE" == "x" ]; then
 
-    URL=`readAPIValue $OMERO_BUILD"/lastSuccessfulBuild/api/xml?xpath=/freeStyleBuild/url"`
-    FILE=`readAPIValue $OMERO_BUILD"/lastSuccessfulBuild/api/xml?xpath=//relativePath[contains(.,'server')]"`
+    URL=`readAPIValue $OMERO_BUILD_URL"/api/xml?xpath=/freeStyleBuild/url"`
+    FILE=`readAPIValue $OMERO_BUILD_URL"/api/xml?xpath=//relativePath[contains(.,'server')]"`
 
     wget -q "$URL"artifact/$FILE
 
     DL_ARCHIVE=`basename $FILE`
     DL_FOLDER=${DL_ARCHIVE%.zip}
 else
-
-    DL_LOC="http://hudson.openmicroscopy.org.uk/job/OMERO-trunk/lastSuccessfulBuild/artifact/"
+    DL_LOC=$OMERO_BUILD_URL"/artifact/"
     DL_FOLDER=${DL_ARCHIVE%.zip}
 
     wget $DL_LOC$DL_ARCHIVE
