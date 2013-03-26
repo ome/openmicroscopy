7ba543ecad7cb5b1f92fab3082ac7dca9818c7e0
Blazej Pindelski <bpindelski@dundee.ac.uk>
2013-03-26 09:27:59 +0000

    Add Murmur providers and tests

---
+++ components/common/src/ome/util/checksum/Murmur32ChecksumProviderImpl.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2013 University of Dundee & Open Microscopy Environment.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package ome.util.checksum;
+
+import com.google.common.hash.Hashing;
+
+/**
+ * An implementation of the {@link ChecksumProvider} interface using Murmur3_32
+ * as the hash algorithm.
+ *
+ * @author Blazej Pindelski, bpindelski at dundee.ac.uk
+ * @since 5.0
+ */
+public final class Murmur32ChecksumProviderImpl
+    extends AbstractChecksumProvider {
+
+    public Murmur32ChecksumProviderImpl() {
+        super(Hashing.murmur3_32());
+    }
+}
