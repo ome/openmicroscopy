c951f4bf9ad8e84bd9805abe840981d001139035
jburel <j.burel@dundee.ac.uk>
2013-03-26 21:02:44 +0000

    Close derived connector.

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/OMEROGateway.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/OMEROGateway.java
@@ -8542,7 +8542,7 @@ class OMEROGateway
 	 * Removes the security context.
 	 * 
 	 * @param ctx The security context.
-	 * @throws Throwable Thrown if the connector cannot be closed.
+	 * @throws Exception Thrown if the connector cannot be closed.
 	 */
 	void removeGroup(SecurityContext ctx) 
 	throws Exception
@@ -8558,4 +8558,24 @@ class OMEROGateway
 		}
 	}
 
+	/** 
+	 * Shuts down the connectors created while creating/importing data for 
+	 * other users.
+	 * 
+	 * @param ctx
+	 * @throws Exception Thrown if the connector cannot be closed.
+	 */
+	void shutDownDerivedConnector(SecurityContext ctx)
+		throws Exception
+	{
+		Connector c = getConnector(ctx);
+		if (c == null) return;
+		isNetworkUp();
+		try {
+			c.closeDerived(networkup);
+		} catch (Throwable e) {
+			new Exception("Cannot close the derived connectors", e);
+		}
+	}
+	
 }
