3fc5c3607aa04bc6303dbe1111419a590dc1f36a
Mark Carroll <m.t.b.carroll@dundee.ac.uk>
2013-04-12 17:20:28 +0100

    Do proper hash checking for web attachment upload.

--- components/tools/OmeroWeb/omeroweb/webclient/controller/container.py
+++ components/tools/OmeroWeb/omeroweb/webclient/controller/container.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 
-# 
+# container
 # 
 # Copyright (c) 2008-2011 University of Dundee.
 # 
--- components/tools/OmeroWeb/omeroweb/webclient/webclient_gateway.py
+++ components/tools/OmeroWeb/omeroweb/webclient/webclient_gateway.py
@@ -71,6 +71,13 @@ from django.core.mail import EmailMultiAlternatives
 from omeroweb.connector import Server
 
 try:
+    import hashlib
+    hash_sha1 = hashlib.sha1
+except:
+    import sha
+    hash_sha1 = sha.new
+
+try:
     PAGE = settings.PAGE
 except:
     PAGE = 200
@@ -1323,13 +1330,26 @@ class OmeroWebGateway (omero.gateway.BlitzGateway):
         store.setFileId(oFile_id, self.SERVICE_OPTS);
         pos = 0
         rlen = 0
-        
+        hash = hash_sha1()
+
         for chunk in binary.chunks():
             rlen = len(chunk)
             store.write(chunk, pos, rlen)
+            hash.update(chunk)
             pos = pos + rlen
-        return store.save(self.SERVICE_OPTS)
-    
+        ofile = store.save(self.SERVICE_OPTS)
+        store.close()
+
+        serverhash = ofile.sha1.val
+        clienthash = hash.hexdigest()
+
+        if serverhash != clienthash:
+            msg = "SHA-1 checksums do not match in file upload: client has %s but server has %s" % (clienthash, serverhash)
+            logger.error(msg)
+            raise Exception(msg)
+
+        return ofile
+
     ##############################################
     ##   IShare
     
