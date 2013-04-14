cce5b077f93285bb9228d94687c3ed4310504a9f
Sebastien Besson <seb.besson@gmail.com>
2013-04-14 20:57:09 +0100

    Add functions to read project annotations

---
+++ components/tools/OmeroM/src/annotations/getProjectTagAnnotations.m
@@ -0,0 +1,40 @@
+function tas = getProjectTagAnnotations(session, ids, varargin)
+% GETPROJECTTAGANNOTATIONS Retrieve tag annotations linked to projects
+%
+%    tas = getProjectTagAnnotations(session, ids) returns all tag
+%    annotations linked to the projects specified by the input identifiers
+%    ids and owned by the session user.
+%
+%    tas = getProjectTagAnnotations(session,  ids, 'include', include) only
+%    returns tag annotations with the input namespace.
+%
+%    tas = getProjectTagAnnotations(session,  ids, 'exclude', exclude)
+%    excludes tag annotations with the input namespace.
+%
+%    Examples:
+%
+%        tas = getProjectTagAnnotations(session, ids)
+%        fas = getProjectTagAnnotations(session, ids, 'include', include)
+%        fas = getProjectTagAnnotations(session, ids, 'exclude', exclude)
+%
+% See also: GETOBJECTANNOTATIONS, GETPROJECTCOMMENTANNOTATIONS,
+% GETPROJECTFILEANNOTATIONS, GETPROJECTXMLANNOTATIONS
+
+% Copyright (C) 2013 University of Dundee & Open Microscopy Environment.
+% All rights reserved.
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program; if not, write to the Free Software Foundation, Inc.,
+% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+tas = getObjectAnnotations(session, 'tag', 'project', ids, varargin{:});
---
+++ components/tools/OmeroM/src/annotations/getProjectXmlAnnotations.m
@@ -0,0 +1,40 @@
+function xas = getProjectXmlAnnotations(session, ids, varargin)
+% GETPROJECTXMLANNOTATIONS Retrieve xml annotations linked to projects
+%
+%    fas = getProjectXmlAnnotations(session, ids) returns all xml
+%    annotations linked to the projects specified by the input identifiers
+%    and owned by the session user.
+%
+%    fas = getProjectXmlAnnotations(session,  ids, 'include', include) only
+%    returns xml annotations with the input namespace.
+%
+%    fas = getProjectXmlAnnotations(session,  ids, 'exclude', exclude)
+%    excludes xml annotations with the input namespace.
+%
+%    Examples:
+%
+%        fas = getProjectXmlAnnotations(session, ids)
+%        fas = getProjectXmlAnnotations(session, ids, 'include', include)
+%        fas = getProjectXmlAnnotations(session, ids, 'exclude', exclude)
+%
+% See also: GETOBJECTANNOTATIONS, GETPROJECTCOMMENTANNOTATIONS,
+% GETPROJECTTAGANNOTATIONS, GETPROJECTFILEANNOTATIONS
+
+% Copyright (C) 2013 University of Dundee & Open Microscopy Environment.
+% All rights reserved.
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program; if not, write to the Free Software Foundation, Inc.,
+% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+xas = getObjectAnnotations(session, 'xml', 'project', ids, varargin{:});
