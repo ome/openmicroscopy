575953ff40cc3721de5d19c246d9998bbefce0ae
jburel <j.burel@dundee.ac.uk>
2013-04-20 17:13:05 +0100

    Change date in header.

--- components/tools/OmeroJava/test/integration/chmod/RolesTest.java
+++ components/tools/OmeroJava/test/integration/chmod/RolesTest.java
@@ -2,7 +2,7 @@
  * integration.chmod.RolesTest 
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2012 University of Dundee & Open Microscopy Environment.
+ *  Copyright (C) 2006-2013 University of Dundee & Open Microscopy Environment.
  *  All rights reserved.
  *
  *
