202e70ca2a909267ccc99c7bf596f26d3063c9f1
Andreas Knab <knabar@gmail.com>
2013-05-01 09:59:29 +0200

    Add sha1 checksum to file listings

--- components/tools/OmeroPy/src/omero/gateway/__init__.py
+++ components/tools/OmeroPy/src/omero/gateway/__init__.py
@@ -3959,7 +3959,7 @@ class _OriginalFileWrapper (BlitzObjectWrapper):
     omero_model_OriginalFileI class wrapper extends BlitzObjectWrapper.
     """
 
-    _attrs = ('()size', '()mtime')
+    _attrs = ('()size', '()mtime', '()sha1')
 
     def __bstrap__ (self):
         self.OMERO_CLASS = 'OriginalFile'
