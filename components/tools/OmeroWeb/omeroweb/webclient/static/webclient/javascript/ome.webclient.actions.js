c7519e6feae4382d3ab4756a9b7b2ca039b3442c
Will Moore <will@lifesci.dundee.ac.uk>
2013-05-02 15:38:50 +0100

    Don't highlight non-fileset files

--- components/tools/OmeroWeb/omeroweb/webclient/static/webclient/javascript/ome.webclient.actions.js
+++ components/tools/OmeroWeb/omeroweb/webclient/static/webclient/javascript/ome.webclient.actions.js
@@ -87,7 +87,9 @@ OME.handle_tree_selection = function(data) {
                 }
                 var selected_obj = {"id":oid, "rel":$this.attr('rel')};
                 selected_obj["class"] = $this.attr('class');
-                selected_obj["fileset"] = $this.attr('data-fileset');
+                if ($this.attr('data-fileset')) {
+                    selected_obj["fileset"] = $this.attr('data-fileset');
+                }
                 if (share_id) {
                     selected_obj["share"] = share_id;
                 }
