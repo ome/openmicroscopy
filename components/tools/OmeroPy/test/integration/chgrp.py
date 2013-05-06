b9214c520d4fb4bfa74bba5c90eec0593e6b2c58
Will Moore <will@lifesci.dundee.ac.uk>
2013-05-06 16:05:48 +0100

    NEW testTwoFilesetsChgrpDataset() FAILS

--- components/tools/OmeroPy/test/integration/chgrp.py
+++ components/tools/OmeroPy/test/integration/chgrp.py
@@ -396,6 +396,49 @@ class TestChgrp(lib.ITest):
         self.assertTrue(filesetOneId in failedFilesets)
         self.assertTrue(filesetTwoId in failedFilesets)
 
+
+    def testTwoFilesetsChgrpDataset(self):
+        """
+        If we try to 'split' 2 Filesets, both should be returned
+        by the chgrp error
+        """
+        # One user in two groups
+        client, user = self.new_client_and_user(perms=PRIVATE)
+        admin = client.sf.getAdminService()
+        target_grp = self.new_group([user],perms=PRIVATE)
+        target_gid = target_grp.id.val
+
+        imagesFsOne = self.importMIF(2, client=client)
+        imagesFsTwo = self.importMIF(2, client=client)
+
+        update = client.sf.getUpdateService()
+        ds = omero.model.DatasetI()
+        ds.name = rstring("testTwoFilesetsChgrpDataset")
+        ds = update.saveAndReturnObject(ds)
+        images = self.importMIF(2, client=client)
+        for i in (imagesFsOne, imagesFsTwo):
+            link = omero.model.DatasetImageLinkI()
+            link.setParent(ds)
+            link.setChild(i[0])
+            link = update.saveAndReturnObject(link)
+
+        # Lookup the filesets
+        qs = client.sf.getQueryService()
+        filesetOneId = qs.get('Image', imagesFsOne[0].id.val).fileset.id.val
+        filesetTwoId = qs.get('Image', imagesFsTwo[0].id.val).fileset.id.val
+
+        # chgrp should fail...
+        chgrp = omero.cmd.Chgrp(type="/Dataset", id=ds.id.val, grp=target_gid)
+        rsp = self.doSubmit(chgrp, client, test_should_pass=False)
+
+        # ...due to the filesets
+        self.assertTrue('Fileset' in rsp.constraints, "chgrp should fail due to 'Fileset' constraints")
+        failedFilesets = rsp.constraints['Fileset']
+        self.assertEqual(len(failedFilesets), 2, "chgrp should fail due to a Two Filesets")
+        self.assertTrue(filesetOneId in failedFilesets)
+        self.assertTrue(filesetTwoId in failedFilesets)
+
+
     """
     Simple example of the MIF chgrp good case:
     a single fileset containing 2 images in one dataset.
