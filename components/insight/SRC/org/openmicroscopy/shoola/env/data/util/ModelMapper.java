bed3e577e7b9fcc6715abd038fc3798ffa566a35
jburel <j.burel@dundee.ac.uk>
2013-05-06 16:03:23 +0100

    Add method to convert RType to Java type

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/util/ModelMapper.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/util/ModelMapper.java
@@ -2,7 +2,7 @@
  * org.openmicroscopy.shoola.env.data.util.ModelMapper
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
  *
  *
  * 	This program is free software; you can redistribute it and/or modify
@@ -25,12 +25,25 @@ package org.openmicroscopy.shoola.env.data.util;
 
 
 //Java imports
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 //Third-party libraries
 
+
 //Application-internal dependencies
+import omero.RBool;
+import omero.RDouble;
+import omero.RInt;
+import omero.RList;
+import omero.RLong;
+import omero.RMap;
 import omero.RString;
+import omero.RType;
 import omero.model.Annotation;
 import omero.model.AnnotationAnnotationLink;
 import omero.model.AnnotationAnnotationLinkI;
@@ -703,5 +716,41 @@ public class ModelMapper
     		n.setMaximumFieldCount(o.getMaximumFieldCount());
     	}
     }
-  
+
+    /**
+     * Converts the passed OMERO type into its corresponding Java type.
+     * 
+     * @param type The type to handle.
+     * @return See above.
+     */
+    public static Object convertRTypeToJava(RType type)
+    {
+    	if (type instanceof RString) return ((RString) type).getValue();
+    	if (type instanceof RLong) return ((RLong) type).getValue();
+    	if (type instanceof RBool) return ((RBool) type).getValue();
+    	if (type instanceof RDouble) return ((RDouble) type).getValue();
+    	if (type instanceof RInt) return ((RInt) type).getValue();
+    	if (type instanceof RList) {
+    		List<RType> types = ((RList) type).getValue();
+    		List<Object> l = new ArrayList<Object>(types.size());
+    		Iterator<RType> i = types.iterator();
+    		while (i.hasNext()) {
+    			l.add(convertRTypeToJava(i.next()));
+    		}
+    		return l;
+    	}
+    	if (type instanceof RMap) {
+    		Map<String, RType> types = ((RMap) type).getValue();
+    		Map<String, Object> l = new LinkedHashMap<String, Object>(
+    				types.size());
+    		Entry<String, RType> e;
+    		Iterator<Entry<String, RType>> i = types.entrySet().iterator();
+    		while (i.hasNext()) {
+    			e = i.next();
+    			l.put(e.getKey(), convertRTypeToJava(e.getValue()));
+    		}
+    		return l;
+    	}
+    	return "";
+    }
 }
