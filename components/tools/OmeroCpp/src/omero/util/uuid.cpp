09fdba64e913f2eb23150fd0e568db2579492b64
Roger Leigh <r.leigh@dundee.ac.uk>
2013-05-22 10:36:00 +0100

    OmeroCpp: remove Boost requirement
    IceUtil::generateUUID is still present in Ice 3.5.

--- components/tools/OmeroCpp/src/omero/util/uuid.cpp
+++ components/tools/OmeroCpp/src/omero/util/uuid.cpp
@@ -5,21 +5,10 @@
 #include <omero/util/uuid.h>
 
 #include <IceUtil/Config.h>
-#if ICE_INT_VERSION / 100 >= 305
-#include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_io.hpp>
-#else
 #include <IceUtil/UUID.h>
-#endif
 
 std::string
 omero::util::generate_uuid ()
 {
-#if ICE_INT_VERSION / 100 >= 305
-  static boost::uuids::random_generator uuidgen;
-  return boost::uuids::to_string(uuidgen());
-#else
   return IceUtil::generateUUID();
-#endif
 }
