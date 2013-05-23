720ffaa9bcdb94cb1a304f210f56f95eafc3d383
Colin Blackburn <colin@ximenes.org.uk>
2013-05-23 16:14:51 +0100

    Fix query in Metadata service implementation.

--- components/server/src/ome/logic/MetadataImpl.java
+++ components/server/src/ome/logic/MetadataImpl.java
@@ -79,7 +79,7 @@ public class MetadataImpl
 
 	/** Query to load the original file related to a file annotation. */
 	private final String LOAD_ORIGINAL_FILE = 
-		"select p from OriginalFile as p join fetch p.hasher where p.id = :id";
+		"select p from OriginalFile as p left outer join fetch p.hasher where p.id = :id";
 	
 	/** Identifies the file annotation class. */
 	private final String FILE_TYPE = "ome.model.annotations.FileAnnotation";
