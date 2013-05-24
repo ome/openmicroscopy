1d042c3b16e42173ee6dafb9275ed142a3d5a728
Blazej Pindelski <bpindelski@dundee.ac.uk>
2013-05-24 16:10:49 +0100

    Update JavaDoc on TimeEstimator.

--- components/blitz/src/ome/formats/importer/util/TimeEstimator.java
+++ components/blitz/src/ome/formats/importer/util/TimeEstimator.java
@@ -21,12 +21,38 @@
 package ome.formats.importer.util;
 
 /**
- * Estimates the time left during resource upload to the server.
- * Uses a exponential moving average as the calculation algorithm.
+ * Estimates the time left (ETA) during image resource upload to the server.
+ * Uses a exponential moving average as the calculation algorithm. Internally,
+ * the object keeps track of previous estimations in a circular buffer.
+ *
+ * The user of this API is responsible for instantiating a new object every
+ * time the state has to be reset.
  *
  * @author Blazej Pindelski, bpindelski at dundee.ac.uk
  * @since 5.0
  */
 public interface TimeEstimator {
 
+    /**
+     * Starts the measurement with a specific time value.
+     *
+     * @param startTime The start time.
+     */
+    void start(long mesurementStart);
+
+    /**
+     * Stops the measurement at the provided stop time.
+     *
+     * @param stopTime The end time.
+     */
+    void stop(long measurementStop);
+
+    /**
+     * Return the estimated time left based on the calls to
+     * {@link TimeEstimator#start(long) start} and
+     * {@link TimeEstimator#stop(long) stop} methods.
+     *
+     * @return The estimated time remaining.
+     */
+    long getUploadTimeLeft();
 }
