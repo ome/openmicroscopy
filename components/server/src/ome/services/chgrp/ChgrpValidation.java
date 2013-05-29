d990e124a0de60e1a4a8192bc873d8283eeeb436
jmoore <josh@glencoesoftware.com>
2013-05-29 19:24:03 +0200

    Disable the CHGRP-VALIDATION event logs
    They don't represent an actual change to the data
    and therefore serve little or not purpose.

--- components/server/src/ome/services/chgrp/ChgrpValidation.java
+++ components/server/src/ome/services/chgrp/ChgrpValidation.java
@@ -137,16 +137,8 @@ public class ChgrpValidation extends GraphStep {
     @Override
     public void onRelease(Class<IObject> k, Set<Long> ids)
             throws GraphException {
-        EventLogMessage elm = new EventLogMessage(this, "CHGRP-VALIDATION", k,
-                new ArrayList<Long>(ids));
 
-        try {
-            ctx.publishMessage(elm);
-        } catch (Throwable t) {
-            GraphException de = new GraphException("EventLogMessage failed.");
-            de.initCause(t);
-            throw de;
-        }
+        // No special handling necessary on release.
 
     }
 
