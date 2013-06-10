84efe04a54eb1b25dbdf9bce8b92a1e7818cf343
jburel <j.burel@dundee.ac.uk>
2013-06-10 21:47:56 +0100

    Remove commented out code.

--- components/insight/SRC/org/openmicroscopy/shoola/agents/fsimporter/ImportResultLoader.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/fsimporter/ImportResultLoader.java
@@ -140,19 +140,6 @@ public class ImportResultLoader
     {
     	if (viewer.getState() == DataBrowser.DISCARDED ||
     			!ThumbnailData.class.equals(nodeType)) return;
-
-    	/*
-    	List l = (List) fe.getPartialResult();
-    	if (!CollectionUtils.isEmpty(l)) {
-    		if (result == null) result = new ArrayList<Object>();
-    		Iterator i = l.iterator();
-    		while (i.hasNext()) {
-    			result.add(i.next());
-    		}
-    		if (result.size() == ids.size())
-    			viewer.setImportResult(result, comp);
-    	}
-    	*/
     	ThumbnailData td = (ThumbnailData) fe.getPartialResult();
     	if (td != null) {
     		if (result == null) result = new ArrayList<Object>();
