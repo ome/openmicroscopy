f27525a7571570d75a826fcac6d0ffe1d33efac4
Will Moore <will@lifesci.dundee.ac.uk>
2013-06-12 22:22:31 +0100

    Bring Training/python samples up to date with docs Python.txt

--- examples/Training/python/Connect_To_OMERO.py
+++ examples/Training/python/Connect_To_OMERO.py
@@ -83,5 +83,5 @@ if __name__ == '__main__':
 
     # Close connection:
     # =================================================================
-    # When you're done, close the session to free up server resources.
+    # When you are done, close the session to free up server resources.
     conn._closeSession()
