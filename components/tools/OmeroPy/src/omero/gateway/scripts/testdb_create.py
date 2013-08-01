9663ce57dcf6e82d9b0bad7d3131a7093638269c
Carlos Neves <carlos@glencoesoftware.com>
2013-08-01 10:26:16 +0100

    Starting to separate unittest specifics from test helpers.

--- components/tools/OmeroPy/src/omero/gateway/scripts/testdb_create.py
+++ components/tools/OmeroPy/src/omero/gateway/scripts/testdb_create.py
@@ -9,7 +9,7 @@
 
 """
 
-import unittest, os
+import os
 import omero
 from omero.rtypes import rstring
 
@@ -42,9 +42,9 @@ dbhelpers.IMAGES = {
 }
 
 
-class GTest(unittest.TestCase):
+class TestDBHelper(object):
 
-    def setUp(self, skipTestDB=False):
+    def setUp(self, skipTestDB=False, skipTestImages=True):
         self.tmpfiles = []
         self._has_connected = False
         self.doDisconnect()
@@ -59,21 +59,21 @@ class GTest(unittest.TestCase):
         finally:
             gateway.seppuku()
 
-        self.prepTestDB(onlyUsers=skipTestDB)
+        self.prepTestDB(onlyUsers=skipTestDB, skipImages=skipTestImages)
         self.doDisconnect()
 
     def doConnect (self):
         if not self._has_connected:
             self.gateway.connect()
             self._has_connected = True
-        self.assert_(self.gateway.isConnected(), 'Can not connect')
-        self.failUnless(self.gateway.keepAlive(), 'Could not send keepAlive to connection')
+        assert self.gateway.isConnected(), 'Can not connect'
+        assert self.gateway.keepAlive(), 'Could not send keepAlive to connection'
     
     def doDisconnect(self):
         if self._has_connected and self.gateway:
             self.doConnect()
             self.gateway.seppuku()
-            self.assert_(not self.gateway.isConnected(), 'Can not disconnect')
+            assert not self.gateway.isConnected(), 'Can not disconnect'
         self.gateway = None
         self._has_connected = False
 
@@ -124,8 +124,8 @@ class GTest(unittest.TestCase):
     def getTestDataset2 (self, project=None):
         return dbhelpers.getDataset(self.gateway, 'testds2', project)
 
-    def getTestImage (self, dataset=None):
-        return dbhelpers.getImage(self.gateway, 'testimg1', dataset)
+    def getTestImage (self, dataset=None, create=False):
+        return dbhelpers.getImage(self.gateway, 'testimg1', dataset, create=create)
 
     def getTestImage2 (self, dataset=None):
         return dbhelpers.getImage(self.gateway, 'testimg2', dataset)
@@ -145,14 +145,14 @@ class GTest(unittest.TestCase):
     def getBigTestImage (self, dataset=None):
         return dbhelpers.getImage(self.gateway, 'bigimg', dataset)
 
-    def prepTestDB (self, onlyUsers=False):
-        dbhelpers.bootstrap(onlyUsers=onlyUsers)
+    def prepTestDB (self, onlyUsers=False, skipImages=True):
+        dbhelpers.bootstrap(onlyUsers=onlyUsers, skipImages=skipImages)
 
     def waitOnCmd(self, client, handle):
         callback = omero.callbacks.CmdCallbackI(client, handle)
         callback.loop(10, 500) # throws on timeout
         rsp = callback.getResponse()
-        self.assert_(isinstance(rsp, omero.cmd.OK))
+        assert isinstance(rsp, omero.cmd.OK)
         return callback
 
 
