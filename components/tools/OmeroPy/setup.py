72dfea214387b9d1593549d52adc6c376de05086
Carlos Neves <carlos@glencoesoftware.com>
2013-08-01 11:34:23 +0100

    adding pytest command to setup.py

--- components/tools/OmeroPy/setup.py
+++ components/tools/OmeroPy/setup.py
@@ -21,6 +21,7 @@
    Use is subject to license terms supplied in LICENSE.txt
 
 """
+from setuptools.command.test import test as TestCommand
 
 import glob
 import sys
@@ -40,6 +41,30 @@ if os.path.exists("target"):
 else:
     packages = [""]
 
+class PyTest(TestCommand):
+    user_options = TestCommand.user_options + \
+                   [('test-path=', 'p', "prepend 'path' to PYTHONPATH"),
+                    ('test-ice-config=', 'i', "use specified 'ice config' file instead of default")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.test_path = None
+        self.test_ice_config = None
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['test']
+        self.test_suite = True
+        if self.test_ice_config is None:
+            self.test_ice_config = 'ice.config'
+        if not os.environ.has_key('ICE_CONFIG'):
+            os.environ['ICE_CONFIG'] = self.test_ice_config
+    def run_tests(self):
+        if self.test_path is not None:
+            sys.path.insert(0, self.test_path)
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 setup(name="omero_client",
       version=ov,
       description="Python bindings to the OMERO.blitz server",
@@ -53,6 +78,10 @@ Python bindings to the OMERO.blitz server.
       package_dir = {"": "target"},
       packages=packages,
       package_data={'omero.gateway':['pilfonts/*'], 'omero.gateway.scripts':['imgs/*']},
-      test_suite='test.suite'
+      test_suite='test.suite',
+      cmdclass = {'pytest': PyTest},
 )
+
+#    tests_require=['pytest'],
+
 
