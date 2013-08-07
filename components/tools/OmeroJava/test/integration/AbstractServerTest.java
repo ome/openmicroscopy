30470c35ee8bd4710e440bafd5b78feede07410a
Blazej Pindelski <bpindelski@dundee.ac.uk>
2013-08-07 13:46:57 +0100

    Update dates in licence headers.

--- components/tools/OmeroJava/test/integration/AbstractServerTest.java
+++ components/tools/OmeroJava/test/integration/AbstractServerTest.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- *   Copyright 2010 Glencoe Software, Inc. All rights reserved.
+ *   Copyright 2010-2013 Glencoe Software, Inc. All rights reserved.
  *   Use is subject to license terms supplied in LICENSE.txt
  */
 package integration;
