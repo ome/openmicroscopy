74f441ee47f5217f8524c6a18235cf00797c202b
Simon Li <spli@dundee.ac.uk>
2013-08-14 13:54:19 +0100

    Remove password from sudo cmds, use relative path for source files

--- docs/install/VM/setup_omero_daemon.sh
+++ docs/install/VM/setup_omero_daemon.sh
@@ -2,14 +2,12 @@
 
 set -e -u -x
 
-PASSWORD=${PASSWORD:-"omero"}
+sudo cp omero-init.d /etc/init.d/omero
+sudo chmod a+x /etc/init.d/omero
+sudo update-rc.d -f omero remove
+sudo update-rc.d -f omero defaults 98 02
 
-echo $PASSWORD | sudo -S cp /home/omero/omero-init.d /etc/init.d/omero
-echo $PASSWORD | sudo -S chmod a+x /etc/init.d/omero
-echo $PASSWORD | sudo -S update-rc.d -f omero remove
-echo $PASSWORD | sudo -S update-rc.d -f omero defaults 98 02
-
-echo $PASSWORD | sudo -S cp /home/omero/omero-web-init.d /etc/init.d/omero-web
-echo $PASSWORD | sudo -S chmod a+x /etc/init.d/omero-web
-echo $PASSWORD | sudo -S update-rc.d -f omero-web remove
-echo $PASSWORD | sudo -S update-rc.d -f omero-web defaults 98 02
+sudo cp omero-web-init.d /etc/init.d/omero-web
+sudo chmod a+x /etc/init.d/omero-web
+sudo update-rc.d -f omero-web remove
+sudo update-rc.d -f omero-web defaults 98 02
