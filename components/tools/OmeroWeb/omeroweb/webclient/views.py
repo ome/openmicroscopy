fdb7a85d3fd75774bb0b7e7438fd68c0fc3d9024
Andreas Knab <knabar@gmail.com>
2013-08-15 15:54:50 +0200

    Only remove tags owned by current user

--- components/tools/OmeroWeb/omeroweb/webclient/views.py
+++ components/tools/OmeroWeb/omeroweb/webclient/views.py
@@ -1230,6 +1230,7 @@ def annotate_tags(request, conn=None, **kwargs):
 
     # Get appropriate manager, either to list available Tags to add to single object, or list ALL Tags (multiple objects)
     manager = None
+    self_id = conn.getEventContext().userId
     if obj_count == 1:
         for t in selected:
             if len(selected[t]) > 0:
@@ -1249,7 +1250,6 @@ def annotate_tags(request, conn=None, **kwargs):
             manager = BaseShare(conn, o_id)
 
         manager.annotationList()
-        self_id = conn.getEventContext().userId
         selected_tags = [(tag.id,
                           unwrap(tag.link.details.owner.id),
                           "%s %s" % (unwrap(tag.link.details.owner.firstName), unwrap(tag.link.details.owner.lastName)),
@@ -1311,7 +1311,7 @@ def annotate_tags(request, conn=None, **kwargs):
                         "%s-%s" % (dtype, obj.id)
                         for dtype, objs in oids.items()
                         for obj in objs
-                    ], index)
+                    ], index, tag_owner_id=self_id)
             template = "webclient/annotations/tags.html"
             context = {}
             # Now we lookup the object-annotations (same as for def batch_annotate above)
