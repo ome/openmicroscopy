283cb7ad5df532e08c27ac2f445751eab198564c
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-08-18 21:39:02 +0100

    Adjusts build and adds targets.

--- components/tests/ui/run.py
+++ components/tests/ui/run.py
@@ -6,10 +6,10 @@ Tests are run by giving a path to the tests to be executed as an argument to
 this script. Possible Robot Framework options are given before the path.
 
 Examples:
-  run.py testcases/web                          # Run all tests in a directory
-  rundemo.py testcases/web/webadmin_login.txt   # Run tests in a specific file
-  rundemo.py --variable BROWSER:IE testcases/web # Override variable
-  rundemo.py -v BROWSER:IE -v DELAY:0.25 login_tests
+  run.py -t web testcases/web                          # Run all tests in a directory
+  run.py -t web testcases/web/webadmin_login.txt   # Run tests in a specific file
+  run.py -t web -v BROWSER:IE testcases/web # Override variable
+  run.py -t web -v BROWSER:IE -v DELAY:0.25 login_tests
 
 By default tests are executed with Firefox browser, but this can be changed
 by overriding the `BROWSER` variable as illustrated above. Similarly it is
@@ -21,7 +21,7 @@ Java to be installed.
 """
 
 import os
-import sys
+import sys, getopt
 from subprocess import call
 
 try:
@@ -34,15 +34,52 @@ except ImportError, e:
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
+def run_tests(args):
+    """ parse the arguments"""
+
+    client = ''
+
+    try:
+      opts, v = getopt.getopt(args[0:2],'t:', ["help", "target="])
+    except getopt.GetoptError as err:
+      print str(err) # will print something like "option -a not recognized"
+      print_usage()
+      sys.exit(2)
+
+    print args
+    
+    for opt, a in opts:
+       print opt
+       if opt in ("-t", "--target"):
+         client = a
+
+    print client
+    if client == '':
+        print 'No target specified'
+        sys.exit(2)
+
+    if client == 'web':
+       run_web_tests(sys.argv[3:])
+    elif client == 'cli':
+       run_cli_tests(sys.argv[3:])
+    elif client == 'insight':
+       run_insight_tests(sys.argv[3:])
+
+def run_cli_tests(args):
+    call(['pybot'] + args, shell=(os.sep == '\\'))
 
 def run_web_tests(args):
     call(['pybot'] + args, shell=(os.sep == '\\'))
+
+def run_insight_tests(args):
+    call(['jybot'] + args, shell=(os.sep == '\\'))
+
 
 def print_help():
     print __doc__
 
 def print_usage():
-    print 'Usage: run.py [options] datasource'
+    print 'Usage: run.py -t web [options] datasource'
     print '   or: run.py help'
 
 
@@ -52,4 +89,4 @@ if __name__ == '__main__':
     if action:
         action()
     else:
-        run_web_tests(sys.argv[1:])
+        run_tests(sys.argv[1:])
