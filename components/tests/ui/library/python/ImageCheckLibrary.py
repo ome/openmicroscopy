7ff4c494d8c860f4d6f74e425e1dcec79f225779
Will Moore <will@lifesci.dundee.ac.uk>
2013-08-19 14:15:04 +0100

    Added ImageCheckLibrary.py for PIL usage

---
+++ components/tests/ui/library/python/ImageCheckLibrary.py
@@ -0,0 +1,27 @@
+
+import Image
+from numpy import asarray
+
+def crop_image(path, cropX, cropY, cropW, cropH):
+
+    image = Image.open(path)
+    x = int(cropX)
+    y = int(cropY)
+    x2 = int(cropW) + x
+    y2 = int(cropH) + y
+    img = image.crop((x,y,x2,y2))
+    img.save(path)
+
+
+def image_should_be_blank(path):
+
+    image = Image.open(path)
+    image.save(path)    # avoids errors on .split
+
+    for channel in image.split():
+        plane = asarray(channel)
+        pMin = plane.min()
+        pMax = plane.max()
+        # print pMin, pMax
+        if pMin != pMax:
+            raise AssertionError("Image %s is not blank. min: %s, max: %s" % (path, pMin, pMax))
