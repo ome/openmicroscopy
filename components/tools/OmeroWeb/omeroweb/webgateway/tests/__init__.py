cffef850a0698a7aded6742d6516b66b708993ad
Carlos Neves <carlos@glencoesoftware.com>
2013-08-23 19:39:51 +0100

    webgateway pytests

--- components/tools/OmeroWeb/omeroweb/webgateway/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-try:
-    from unittests import *
-except ImportError:
-    # Happens if the PYTHONPATH is not correct, which is what happens when all you are
-    # trying to import is seleniumbase
-    pass
