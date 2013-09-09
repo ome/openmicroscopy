02220a80d9e6ff80ed39599e9456b5c31dbcb89d
Will Moore <will@lifesci.dundee.ac.uk>
2013-09-09 23:15:31 +0100

    Blitz renderSplitChannel() in grey has merged panel

--- components/tools/OmeroPy/src/omero/gateway/__init__.py
+++ components/tools/OmeroPy/src/omero/gateway/__init__.py
@@ -7093,13 +7093,14 @@ class _ImageWrapper (BlitzObjectWrapper):
                 pxc = 0
                 px = border
                 py += self.getSizeY() + border
-        if not self.isGreyscaleRenderingModel():
-            self.setActiveChannels(cmap)
-            img = self.renderImage(z,t, compression)
-            if fsize > 0:
-                draw = ImageDraw.ImageDraw(img)
-                draw.text((2,2), "merged", font=font, fill="#fff")
-            canvas.paste(img, (px, py))
+        # Render merged panel with all current channels in color
+        self.setActiveChannels(cmap)
+        self.setColorRenderingModel()
+        img = self.renderImage(z,t, compression)
+        if fsize > 0:
+            draw = ImageDraw.ImageDraw(img)
+            draw.text((2,2), "merged", font=font, fill="#fff")
+        canvas.paste(img, (px, py))
         return canvas
 
     LP_PALLETE = [0,0,0,0,0,0,255,255,255]
