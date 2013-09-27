1526ca218fe0e001ea5ae57f99593bce37d8a07d
Colin Blackburn <colin@ximenes.org.uk>
2013-09-27 17:20:55 +0100

    Fix make movie test.

--- components/tools/OmeroPy/test/integration/scriptstest/test_make_movie.py
+++ components/tools/OmeroPy/test/integration/scriptstest/test_make_movie.py
@@ -26,7 +26,7 @@ class TestMakeMovie(lib.ITest):
         makeMovieID = self.svc.getScriptID("/omero/export_scripts/Make_Movie.py")
         imported_pix = ",".join(self.import_image())
         imported_img = self.query.findByQuery("select i from Image i join fetch i.pixels pixels where pixels.id in (%s)" % imported_pix, None)
-        inputs = {"IDs": imported_img.id}
+        inputs = {"IDs": omero.rtypes.rlist([imported_img.id])}
         impl = omero.processor.usermode_processor(self.root)
         try:
             process = self.svc.runScript(makeMovieID, inputs, None)
