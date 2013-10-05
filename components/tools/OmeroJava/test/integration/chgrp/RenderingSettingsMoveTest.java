1d6bdbbfd65b19aff66896eef279cb284d8a7766
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-10-05 07:05:59 +0100

    clean up.

--- components/tools/OmeroJava/test/integration/chgrp/RenderingSettingsMoveTest.java
+++ components/tools/OmeroJava/test/integration/chgrp/RenderingSettingsMoveTest.java
@@ -48,10 +48,8 @@ public class RenderingSettingsMoveTest extends AbstractServerTest {
     private void moveImage(String src, String target, int memberRole, int
             moveMemberRole) throws Exception
     {
-        //Create 2 users in the source group.
+        //Create one user in the source group.
         EventContext ctx = newUserAndGroup(src);
-        
-
         //Create an image
         Image img = mmFactory.createImage();
         img = (Image) iUpdate.saveAndReturnObject(img);
@@ -112,8 +110,10 @@ public class RenderingSettingsMoveTest extends AbstractServerTest {
 
         disconnect();
         // Log in to other group
-        loginUser(ctx); //require if log as root.
-        disconnect();
+        if (moveMemberRole == AbstractServerTest.ADMIN) {
+            loginUser(ctx); //require if log as root.
+            disconnect();
+        }
         loginUser(g);
 
         //Check that image has been moved.
