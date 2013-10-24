704698d91d2720fd175ad26b47a35a6a3f6e7e3d
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-10-24 11:59:26 +0100

    Remove unused import.

--- components/insight/SRC/org/openmicroscopy/shoola/agents/imviewer/view/ControlPane.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/imviewer/view/ControlPane.java
@@ -29,7 +29,6 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.beans.PropertyChangeEvent;
