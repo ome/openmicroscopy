060bf95d95c972e8549278f291274e9f4fde6960
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-10-27 20:48:53 +0000

    Fix java doc.

--- components/insight/SRC/org/openmicroscopy/shoola/util/file/IOUtil.java
+++ components/insight/SRC/org/openmicroscopy/shoola/util/file/IOUtil.java
@@ -2,10 +2,10 @@
  * org.openmicroscopy.shoola.util.file.IOUtil 
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2007 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -282,7 +282,6 @@ public class IOUtil
 	 * Makes the zip.
 	 * 
 	 * @param zipName The name of the zip.
-	 * @param files   The files to add.
 	 */
 	public static File zipDirectory(File zip)
 		throws Exception
