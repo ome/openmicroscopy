d0cf307dacef17c16c76fb96834a30c13e8fd0a5
Colin Blackburn <colin@ximenes.org.uk>
2013-10-29 13:38:40 +0000

    Translate repo test to pytest, including some minor refactoring.

--- components/tools/OmeroPy/test/integration/test_reporawfilestore.py
+++ components/tools/OmeroPy/test/integration/test_reporawfilestore.py
@@ -10,129 +10,107 @@
    return the same data as the equivalent rps methods would
    for imported data.
    
-   Copyright 2011 Glencoe Software, Inc. All rights reserved.
+   Copyright 2011-2013 Glencoe Software, Inc. All rights reserved.
    Use is subject to license terms supplied in LICENSE.txt
 
 """
-import unittest, time, shutil, hashlib, difflib, binascii
-import library as lib
+
+import pytest
 import omero, omero.gateway
 
 from path import path
 from omero.rtypes import *
-from integration.repository import AbstractRepoTest
+from test.integration.test_repository import AbstractRepoTest
 
-class TestRepRawFileStore(AbstractRepoTest):
+class TestRepoRawFileStore(AbstractRepoTest):
 
-    def setUpRepo(self):
+    def setup_method(self, method):
+        super(TestRepoRawFileStore, self).setup_method(method)
         sess = self.root.sf
-        self.tmp_dir = path(self.unique_dir)
+        tmp_dir = path(self.unique_dir)
         self.repoPrx = self.getManagedRepo()
+        self.repo_filename = tmp_dir / self.uuid() + ".txt"
 
     def testCreate(self):
-        self.setUpRepo()
-        repo_filename = self.tmp_dir / self.uuid() + ".txt"
-        rfs = self.repoPrx.file(repo_filename, "rw")
-        self.assert_(rfs.size() == 0)
+        rfs = self.repoPrx.file(self.repo_filename, "rw")
+        assert rfs.size() == 0
 
     def testWrite(self):
-        self.setUpRepo()
-        repo_filename = self.tmp_dir / self.uuid() + ".txt"
-        rfs = self.repoPrx.file(repo_filename, "rw")
-        self.assert_(rfs.size() == 0)
+        rfs = self.repoPrx.file(self.repo_filename, "rw")
+        assert rfs.size() == 0
         wbytes = "0123456789"
         rfs.write(wbytes,0,len(wbytes))
-        self.assert_(rfs.size() == len(wbytes))
+        assert rfs.size() == len(wbytes)
 
     def testFailedWrite(self):
-        self.setUpRepo()
-        repo_filename = self.tmp_dir / self.uuid() + ".txt"
-
         # Perform a touch
-        rfs = self.repoPrx.file(repo_filename, "rw") #create empty file
+        rfs = self.repoPrx.file(self.repo_filename, "rw") #create empty file
         rfs.write([], 0, 0)
         rfs.close()
 
-        rfs = self.repoPrx.file(repo_filename, "r")
-        self.assertEquals(0, rfs.size())
+        rfs = self.repoPrx.file(self.repo_filename, "r")
+        assert rfs.size() == 0
         wbytes = "0123456789"
         try:
             rfs.write(wbytes,0,len(wbytes))
         except:
             pass
-        self.assert_(rfs.size() == 0)
+        assert rfs.size() == 0
 
     def testFailedWriteNoFile(self):
-        self.setUpRepo()
-        repo_filename = self.tmp_dir / self.uuid() + ".txt"
-
         # Without a single write, no file is produced
-        rfs = self.repoPrx.file(repo_filename, "rw") #create empty file
+        rfs = self.repoPrx.file(self.repo_filename, "rw") #create empty file
         rfs.close()
 
-        rfs = self.repoPrx.file(repo_filename, "r")
-        try:
+        rfs = self.repoPrx.file(self.repo_filename, "r")
+        with pytest.raises(omero.ResourceError):
             rfs.size()
-            self.fail("File shouldn't exist!")
-        except omero.ResourceError:
-            pass
         wbytes = "0123456789"
         try:
             rfs.write(wbytes,0,len(wbytes))
         except:
             pass
-        try:
+        with pytest.raises(omero.ResourceError):
             rfs.size()
-            self.fail("File shouldn't exist!")
-        except omero.ResourceError:
-            pass
 
     def testWriteRead(self):
-        self.setUpRepo()
-        repo_filename = self.tmp_dir / self.uuid() + ".txt"
-        rfs = self.repoPrx.file(repo_filename, "rw")
-        self.assert_(rfs.size() == 0)
+        rfs = self.repoPrx.file(self.repo_filename, "rw")
+        assert rfs.size() == 0
         wbytes = "0123456789"
         rfs.write(wbytes,0,len(wbytes))
-        self.assert_(rfs.size() == len(wbytes))
+        assert rfs.size() == len(wbytes)
         rbytes = rfs.read(0,len(wbytes))
-        self.assert_(wbytes == rbytes)
+        assert wbytes == rbytes
 
     def testAppend(self):
-        self.setUpRepo()
-        repo_filename = self.tmp_dir / self.uuid() + ".txt"
-        rfs = self.repoPrx.file(repo_filename, "rw")
-        self.assert_(rfs.size() == 0)
+        rfs = self.repoPrx.file(self.repo_filename, "rw")
+        assert rfs.size() == 0
         wbytes = "0123456789"
         rfs.write(wbytes,0,len(wbytes))
-        self.assert_(rfs.size() == len(wbytes))
+        assert rfs.size() == len(wbytes)
         end = rfs.size()
         rfs.write(wbytes,end,len(wbytes))
-        self.assert_(rfs.size() == 2*len(wbytes))
+        assert rfs.size() == 2*len(wbytes)
         rbytes = rfs.read(0,2*len(wbytes))
-        self.assert_(wbytes+wbytes == rbytes)
+        assert wbytes+wbytes == rbytes
 
     def testTruncateToZero(self):
-        self.setUpRepo()
-        repo_filename = self.tmp_dir / self.uuid() + ".txt"
-        rfs = self.repoPrx.file(repo_filename, "rw")
-        self.assert_(rfs.size() == 0)
+        rfs = self.repoPrx.file(self.repo_filename, "rw")
+        assert rfs.size() == 0
         wbytes = "0123456789"
         rfs.write(wbytes,0,len(wbytes))
-        self.assert_(rfs.size() == len(wbytes))
-        self.assert_(rfs.truncate(0))
-        self.assert_(rfs.size() == 0)
+        assert rfs.size() == len(wbytes)
+        assert rfs.truncate(0)
+        assert rfs.size() == 0
 
     def testClose(self):
-        self.setUpRepo()
-        repo_filename = self.tmp_dir / self.uuid() + ".txt"
-        rfs = self.repoPrx.file(repo_filename, "rw")
-        self.assert_(rfs.size() == 0)
+        rfs = self.repoPrx.file(self.repo_filename, "rw")
+        assert rfs.size() == 0
         wbytes = "0123456789"
         rfs.write(wbytes,0,len(wbytes))
-        self.assert_(rfs.size() == len(wbytes))
+        assert rfs.size() == len(wbytes)
         rbytes = rfs.read(0,len(wbytes))
-        self.assert_(wbytes == rbytes)
+        assert wbytes == rbytes
         try:
             rfs.close()
         except:
@@ -141,9 +119,5 @@ class TestRepRawFileStore(AbstractRepoTest):
             rbytes = rfs.read(0,len(wbytes))
         except:
             pass #FIXME: ... so an exception should be thrown here now.
-        rfs = self.repoPrx.file(repo_filename, "r")
-        self.assert_(rfs.size() == len(wbytes))
-
-
-if __name__ == '__main__':
-    unittest.main()
+        rfs = self.repoPrx.file(self.repo_filename, "r")
+        assert rfs.size() == len(wbytes)
