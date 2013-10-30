0cd73672b6dbc22e5a3d8a424848fc13fc5f2ce8
Will Moore <will@lifesci.dundee.ac.uk>
2013-10-30 15:38:32 +0000

    Lots of indentation fixes

--- components/tests/ui/library/python/ImageCheckLibrary.py
+++ components/tests/ui/library/python/ImageCheckLibrary.py
@@ -2,6 +2,7 @@
 import Image
 from numpy import asarray
 
+
 def crop_image(path, cropX, cropY, cropW, cropH):
 
     image = Image.open(path)
@@ -9,37 +10,35 @@ def crop_image(path, cropX, cropY, cropW, cropH):
     y = int(cropY)
     x2 = int(cropW) + x
     y2 = int(cropH) + y
-    img = image.crop((x,y,x2,y2))
+    img = image.crop((x, y, x2, y2))
     img.save(path)
 
 
-def image_should_be_blank(path):
+def image_should_be_blank(path, expected=True):
 
     image = Image.open(path)
     image.save(path)    # avoids errors on .split
+    blank = True
+    minVals = []
+    maxVals = []
 
     for channel in image.split():
         plane = asarray(channel)
         pMin = plane.min()
         pMax = plane.max()
-        # print pMin, pMax
+        minVals.append(pMin)
+        maxVals.append(pMax)
         if pMin != pMax:
-            raise AssertionError("Image %s is not blank. min: %s, max: %s" % (path, pMin, pMax))
+            blank = False
+
+    if expected:
+        if not blank:
+            raise AssertionError("Image %s is not blank. min: %s, max: %s" % (path, minVals, maxVals))
+    else:
+        if blank:
+            raise AssertionError("Image %s is blank. min: %s, max: %s" % (path, minVals, maxVals))
 
 
 def image_should_not_be_blank(path):
 
-    image = Image.open(path)
-    image.save(path)    # avoids errors on .split
-
-    blank = True
-    for channel in image.split():
-        plane = asarray(channel)
-        pMin = plane.min()
-        pMax = plane.max()
-        print "pMin:", pMin, "pMax:", pMax
-        if pMin != pMax:
-            blank = False
-
-    if blank:
-        raise AssertionError("Image %s is blank. min: %s, max: %s" % (path, pMin, pMax))
+    image_should_be_blank(expected=False)
