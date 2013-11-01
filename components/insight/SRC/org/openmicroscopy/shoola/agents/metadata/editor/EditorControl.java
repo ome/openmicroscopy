1578b0cf8f704d6be6f4f3f248eab908f892b1ba
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-11-01 19:45:53 +0000

    Centralize download text and review formatting.

--- components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/editor/EditorControl.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/editor/EditorControl.java
@@ -5,7 +5,7 @@
  *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -372,7 +372,8 @@ class EditorControl
 	{
 		JFrame f = MetadataViewerAgent.getRegistry().getTaskBar().getFrame();
 		FileChooser chooser = new FileChooser(f, FileChooser.SAVE,
-				"Download", "Select where to download the file(s).", null, true);
+			FileChooser.DOWNLOAD_TEXT, FileChooser.DOWNLOAD_DESCRIPTION, null,
+			true);
 		try {
 			File file = UIUtilities.getDefaultFolder();
 			if (file != null) chooser.setCurrentDirectory(file);
--- components/insight/SRC/org/openmicroscopy/shoola/agents/treeviewer/actions/DownloadAction.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/treeviewer/actions/DownloadAction.java
@@ -2,10 +2,10 @@
  * org.openmicroscopy.shoola.agents.treeviewer.actions.DownloadAction 
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2009 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -52,9 +52,6 @@ import pojos.ImageData;
  * @author Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
  * <a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
  * @version 3.0
- * <small>
- * (<b>Internal version:</b> $Revision: $Date: $)
- * </small>
  * @since 3.0-Beta4
  */
 public class DownloadAction 
@@ -66,7 +63,7 @@ public class DownloadAction
     
     /** The description of the action. */
     private static final String DESCRIPTION = "Download the selected files.";
-    
+
     /** 
      * Sets the action enabled depending on the state of the {@link Browser}.
      * @see TreeViewerAction#onBrowserStateChange(Browser)
@@ -75,17 +72,16 @@ public class DownloadAction
     {
         if (browser == null) return;
         switch (browser.getState()) {
-	        case Browser.LOADING_DATA:
-	        case Browser.LOADING_LEAVES:
-	        //case Browser.COUNTING_ITEMS:  
-	            setEnabled(false);
-	            break;
-	        default:
-                onDisplayChange(browser.getLastSelectedDisplay());
-	            break;
+        case Browser.LOADING_DATA:
+        case Browser.LOADING_LEAVES:
+            setEnabled(false);
+            break;
+        default:
+            onDisplayChange(browser.getLastSelectedDisplay());
+            break;
         }
     }
-    
+
     /**
      * Sets the enabled flag of the action depending on the selected node.
      * @see TreeViewerAction#onDisplayChange(TreeImageDisplay)
@@ -99,13 +95,13 @@ public class DownloadAction
         setEnabled(false);
         Object ho = selectedDisplay.getUserObject();
         if (ho instanceof FileAnnotationData) {
-        	setEnabled(true);
+            setEnabled(true);
         } else if (ho instanceof ImageData) {
-        	ImageData img = (ImageData) ho;
-        	setEnabled(img.isArchived());
+            ImageData img = (ImageData) ho;
+            setEnabled(img.isArchived());
         }
     }
-    
+
     /**
      * Creates a new instance.
      * 
@@ -114,13 +110,13 @@ public class DownloadAction
     public DownloadAction(TreeViewer model)
     {
         super(model);
-        name = NAME;  
-        putValue(Action.SHORT_DESCRIPTION, 
+        name = NAME;
+        putValue(Action.SHORT_DESCRIPTION,
                 UIUtilities.formatToolTipText(DESCRIPTION));
         description = (String) getValue(Action.SHORT_DESCRIPTION);
         IconManager im = IconManager.getInstance();
         putValue(Action.SMALL_ICON, im.getIcon(IconManager.DOWNLOAD));
-    } 
+    }
 
     /**
      * Downloads the selected files.
@@ -132,22 +128,10 @@ public class DownloadAction
     	if (browser == null) return;
     	TreeImageDisplay node = browser.getLastSelectedDisplay();
     	if (node == null) return;
-    	TreeImageDisplay[] l = browser.getSelectedDisplays();
-    	Object ho = node.getUserObject();
-    	String name = null;
-    	String text = "file";
-    	if (ho instanceof FileAnnotationData)
-    		name = ((FileAnnotationData) ho).getFileName();
-    	else if (ho instanceof ImageData) {
-    		text = "image";
-    		name = ((ImageData) ho).getName();
-    	}
-    	if (name == null) return;
-    	if (l.length > 1) text += "s";
     	JFrame f = TreeViewerAgent.getRegistry().getTaskBar().getFrame();
-		FileChooser chooser = new FileChooser(f, FileChooser.SAVE, 
-				"Download", "Select where to download the "+text+".", null, 
-					true);
+		FileChooser chooser = new FileChooser(f, FileChooser.SAVE,
+		        FileChooser.DOWNLOAD_TEXT, FileChooser.DOWNLOAD_DESCRIPTION,
+		        null, true);
 		chooser.setSelectedFileFull(name);
 		IconManager icons = IconManager.getInstance();
 		chooser.setTitleIcon(icons.getIcon(IconManager.DOWNLOAD_48));
@@ -167,5 +151,5 @@ public class DownloadAction
 		});
 		chooser.centerDialog();
     }
-    
+
 }
