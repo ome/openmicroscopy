dd7f0e96f7e7f270962e86e53bbd81e9d447cb8d
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-11-05 21:59:16 +0000

    Fix tooltip.

--- components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/editor/PropertiesUI.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/editor/PropertiesUI.java
@@ -5,7 +5,7 @@
  *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -109,9 +109,6 @@ import pojos.WellSampleData;
  * @author Scott Littlewood &nbsp;&nbsp;&nbsp;&nbsp;
  * <a href="mailto:sylittlewood@dundee.ac.uk">sylittlewood@dundee.ac.uk</a>
  * @version 3.0
- * <small>
- * (<b>Internal version:</b> $Revision: $Date: $)
- * </small>
  * @since OME3.0
  */
 class PropertiesUI
@@ -645,6 +642,7 @@ class PropertiesUI
     		label += "Z";
     	}
     	label += ") ";
+    	tooltip += "</body></html>";
     	if (!number) {
     		component.setForeground(AnnotationUI.WARNING);
     		component.setToolTipText("Values stored in the file...");
