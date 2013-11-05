f424fe9ac9feb86f50f3f52cb18f0e035c7f8217
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-11-05 21:18:05 +0000

    Remove unused imports.

--- components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/StructuredDataLoader.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/StructuredDataLoader.java
@@ -2,10 +2,10 @@
  * org.openmicroscopy.shoola.agents.metadata.StructuredDataLoader 
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2008 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -33,14 +33,11 @@ import java.util.Map;
 
 
 //Application-internal dependencies
-import org.openmicroscopy.shoola.agents.metadata.browser.TreeBrowserDisplay;
 import org.openmicroscopy.shoola.agents.metadata.view.MetadataViewer;
 import org.openmicroscopy.shoola.env.data.events.DSCallAdapter;
 import org.openmicroscopy.shoola.env.data.util.SecurityContext;
 import org.openmicroscopy.shoola.env.data.util.StructuredDataResults;
 import org.openmicroscopy.shoola.env.data.views.CallHandle;
-import org.openmicroscopy.shoola.env.log.LogMessage;
-
 import pojos.DataObject;
 
 /** 
@@ -53,9 +50,6 @@ import pojos.DataObject;
  * @author Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
  * <a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
  * @version 3.0
- * <small>
- * (<b>Internal version:</b> $Revision: $Date: $)
- * </small>
  * @since OME3.0
  */
 public class StructuredDataLoader 
