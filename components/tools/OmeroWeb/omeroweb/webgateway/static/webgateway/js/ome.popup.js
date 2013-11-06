15140b1686c23aaf11b39bb20b4cea02bdea9f00
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2013-11-06 14:40:20 +0000

    updating copyrights

--- components/tools/OmeroWeb/omeroweb/webgateway/static/webgateway/js/ome.popup.js
+++ components/tools/OmeroWeb/omeroweb/webgateway/static/webgateway/js/ome.popup.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2011 University of Dundee. & Open Microscopy Environment.
+ * Copyright (c) 2008-2013 University of Dundee. & Open Microscopy Environment.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
