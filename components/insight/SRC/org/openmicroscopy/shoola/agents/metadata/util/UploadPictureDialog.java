d1aa55f90c38c68ad99b4efb81595a01e351342c
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-11-11 20:55:51 +0000

    Clean up.

--- components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/util/UploadPictureDialog.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/util/UploadPictureDialog.java
@@ -5,7 +5,7 @@
  *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -69,187 +69,184 @@ import org.openmicroscopy.shoola.util.ui.filechooser.GenericFileChooser;
  * @author Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
  * <a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
  * @version 3.0
- * <small>
- * (<b>Internal version:</b> $Revision: $Date: $)
- * </small>
  * @since 3.0-Beta4
  */
-public class UploadPictureDialog 
+public class UploadPictureDialog
 	extends JDialog
 	implements ActionListener, DocumentListener
 {
-	
-	/** Bound property indicating to upload the photo. */
-	public static final String	UPLOAD_PHOTO_PROPERTY = "uploadPhoto";
+
+    /** Bound property indicating to upload the photo. */
+    public static final String UPLOAD_PHOTO_PROPERTY = "uploadPhoto";
 
     /** 
      * The size of the invisible components used to separate buttons
      * horizontally.
      */
-    private static final Dimension	H_SPACER_SIZE = new Dimension(3, 10);
-    
+    private static final Dimension H_SPACER_SIZE = new Dimension(3, 10);
+
     /** The maximum size of the image. */
-    private static final int		MAX_SIZE_BYTES = 204800;
+    private static final int MAX_SIZE_BYTES = 204800;
 
     /** The maximum size of the image. */
     private static final String MAX_SIZE_STRING = UIUtilities.formatFileSize(MAX_SIZE_BYTES);
 
-	/** The title of the dialog. */
-	private static final String TITLE = "Upload Photo";
-	
-	/** The text of the dialog. */
-	private static final String TEXT = "Select the photo (JPEG, GIF or PNG) " +
-			"to upload. Maximum size " + MAX_SIZE_STRING + ".";
-	
-	/** Action ID indicating to upload the script to the server. */
-	private static final int	SAVE = 0;
-	
-	/** Action ID indicating to close and disposes of the dialog. */
-	private static final int	CANCEL = 1;
+    /** The title of the dialog. */
+    private static final String TITLE = "Upload Photo";
 
-	/** Collection of supported filters. */
-	private static final List<CustomizedFileFilter> FILTERS;
-	
-	static {
-		FILTERS = new ArrayList<CustomizedFileFilter>();
-		FILTERS.add(new GIFFilter());
-		FILTERS.add(new JPEGFilter());
-		FILTERS.add(new PNGFilter());
-	}
+    /** The text of the dialog. */
+    private static final String TEXT = "Select the photo (JPEG, GIF or PNG) " +
+            "to upload. Maximum size " + MAX_SIZE_STRING + ".";
 
-	/** Chooser used to select the file. */
-	private GenericFileChooser chooser;
-	
+    /** Action ID indicating to upload the script to the server. */
+    private static final int SAVE = 0;
+
+    /** Action ID indicating to close and disposes of the dialog. */
+    private static final int CANCEL = 1;
+
+    /** Collection of supported filters. */
+    private static final List<CustomizedFileFilter> FILTERS;
+
+    static {
+        FILTERS = new ArrayList<CustomizedFileFilter>();
+        FILTERS.add(new GIFFilter());
+        FILTERS.add(new JPEGFilter());
+        FILTERS.add(new PNGFilter());
+    }
+
+    /** Chooser used to select the file. */
+    private GenericFileChooser chooser;
+
     /** 
-     * Replaces the <code>ApproveButton</code> provided by the 
-     * {@link JFileChooser} class. 
+     * Replaces the <code>ApproveButton</code> provided by the
+     * {@link JFileChooser} class.
      */
-    private JButton		saveButton;
-    
+    private JButton saveButton;
+
     /** 
-     * Replaces the <code>CancelButton</code> provided by the 
-     * {@link JFileChooser} class. 
+     * Replaces the <code>CancelButton</code> provided by the
+     * {@link JFileChooser} class.
      */
-    private JButton		cancelButton;
-    
+    private JButton cancelButton;
+
     /** The text area where to enter the name of the file to save. */
-    private JTextField	scriptArea;
-    
-	/** Sets the properties of the dialog. */
+    private JTextField scriptArea;
+
+    /** Sets the properties of the dialog. */
     private void setProperties()
     {
-    	setTitle(TITLE);
+        setTitle(TITLE);
         setModal(true);
     }
-    
+
     /** Initializes the components. */
-	private void initComponents()
-	{
-		chooser = new GenericFileChooser();
-		chooser.setAcceptAllFileFilterUsed(false);
-		chooser.setDialogType(JFileChooser.SAVE_DIALOG);
-		Iterator<CustomizedFileFilter> i = FILTERS.iterator();
-		while (i.hasNext()) {
-			chooser.addChoosableFileFilter(i.next());
-		}
-		chooser.setControlButtonsAreShown(false);
-		saveButton = new JButton("Upload");
-		saveButton.setToolTipText(
-				UIUtilities.formatToolTipText("Upload the selected photo " +
-						"to the server."));
-		saveButton.addActionListener(this);
-		saveButton.setActionCommand(""+SAVE);
-		cancelButton = new JButton("Cancel");
-		cancelButton.setToolTipText(
-				UIUtilities.formatToolTipText("Closes the dialog."));
+    private void initComponents()
+    {
+        chooser = new GenericFileChooser();
+        chooser.setAcceptAllFileFilterUsed(false);
+        chooser.setDialogType(JFileChooser.SAVE_DIALOG);
+        Iterator<CustomizedFileFilter> i = FILTERS.iterator();
+        while (i.hasNext()) {
+            chooser.addChoosableFileFilter(i.next());
+        }
+        chooser.setControlButtonsAreShown(false);
+        saveButton = new JButton("Upload");
+        saveButton.setToolTipText(
+                UIUtilities.formatToolTipText("Upload the selected photo " +
+                        "to the server."));
+        saveButton.addActionListener(this);
+        saveButton.setActionCommand(""+SAVE);
+        cancelButton = new JButton("Cancel");
+        cancelButton.setToolTipText(
+                UIUtilities.formatToolTipText("Closes the dialog."));
         cancelButton.addActionListener(this);
         cancelButton.setActionCommand(""+CANCEL);
         scriptArea = (JTextField) UIUtilities.findComponent(chooser, 
-				JTextField.class);
+                JTextField.class);
         if (scriptArea != null) {
-        	scriptArea.getDocument().addDocumentListener(this);
-        	saveButton.setEnabled(false);
+            scriptArea.getDocument().addDocumentListener(this);
+            saveButton.setEnabled(false);
         }
-	}
-	
-	/** Closes and disposes. */
-	private void close()
-	{
-		setVisible(false);
-		dispose();
-	}
-	
-	/**
+    }
+
+    /** Closes and disposes. */
+    private void close()
+    {
+        setVisible(false);
+        dispose();
+    }
+
+    /**
      * Sets the <code>enabled</code> flag of not the <code>Save</code> option 
      * depending on the length of the text entered in the {@link #scriptArea}.
      */
     private void handleTextUpdate()
     {
-    	if (scriptArea == null) return; //should happen
-    	String text = scriptArea.getText();
-    	boolean b = false;
-    	if (text != null && text.trim().length() > 0) {
-    		b = true;
-    		Iterator<CustomizedFileFilter> i = FILTERS.iterator();
-    		boolean supported = false;
-    		CustomizedFileFilter filter;
-    		while (i.hasNext()) {
-    			filter = i.next();
-    			if (filter.accept(text)) {
-    				supported = true;
-    				break;
-    			}
-    		}
-    		if (!supported) {
-    			saveButton.setEnabled(false);
-    			return;
-    		}
-    	}
-    	saveButton.setEnabled(b);
+        if (scriptArea == null) return; //should happen
+        String text = scriptArea.getText();
+        boolean b = false;
+        if (text != null && text.trim().length() > 0) {
+            b = true;
+            Iterator<CustomizedFileFilter> i = FILTERS.iterator();
+            boolean supported = false;
+            CustomizedFileFilter filter;
+            while (i.hasNext()) {
+                filter = i.next();
+                if (filter.accept(text)) {
+                    supported = true;
+                    break;
+                }
+            }
+            if (!supported) {
+                saveButton.setEnabled(false);
+                return;
+            }
+        }
+        saveButton.setEnabled(b);
     }
-    
-	/** Uploads the photo. */
-	private void upload()
-	{
-		UserNotifier un = MetadataViewerAgent.getRegistry().getUserNotifier();
-		File f;
-		if (scriptArea != null)
-			f = new File(chooser.getCurrentDirectory().toString(), 
-					scriptArea.getText());
-		else f = chooser.getSelectedFile();
-		if (f == null) {
-			un.notifyInfo(TITLE, "Please select a photo.");
-			return;
-		}
-		Iterator<CustomizedFileFilter> i = FILTERS.iterator();
-		boolean supported = false;
-		CustomizedFileFilter filter;
-		String format = "";
-		while (i.hasNext()) {
-			filter = i.next();
-			if (filter.accept(f)) {
-				format = filter.getMIMEType();
-				supported = true;
-				break;
-			}
-		}
-		if (!supported) {
-			un.notifyInfo(TITLE, "Only JPEG, GIF or PNG files " +
-					"can be uploaded.");
-			return;
-		}
-		if (f.length() > MAX_SIZE_BYTES) {
-			un.notifyInfo(TITLE, "The file is too big, maximum size " + MAX_SIZE_STRING);
-			return;
-		}
-		List<Object> l = new ArrayList<Object>();
-		l.add(f);
-		l.add(format);
-		firePropertyChange(UPLOAD_PHOTO_PROPERTY, null, l);
-		setVisible(false);
-		dispose();
-	}
-	
+
+    /** Uploads the photo. */
+    private void upload()
+    {
+        UserNotifier un = MetadataViewerAgent.getRegistry().getUserNotifier();
+        File f;
+        if (scriptArea != null)
+            f = new File(chooser.getCurrentDirectory().toString(),
+                    scriptArea.getText());
+        else f = chooser.getSelectedFile();
+        if (f == null) {
+            un.notifyInfo(TITLE, "Please select a photo.");
+            return;
+        }
+        Iterator<CustomizedFileFilter> i = FILTERS.iterator();
+        boolean supported = false;
+        CustomizedFileFilter filter;
+        String format = "";
+        while (i.hasNext()) {
+            filter = i.next();
+            if (filter.accept(f)) {
+                format = filter.getMIMEType();
+                supported = true;
+                break;
+            }
+        }
+        if (!supported) {
+            un.notifyInfo(TITLE, "Only JPEG, GIF or PNG files " +
+                    "can be uploaded.");
+            return;
+        }
+        if (f.length() > MAX_SIZE_BYTES) {
+            un.notifyInfo(TITLE, "The file is too big, maximum size " + MAX_SIZE_STRING);
+            return;
+        }
+        List<Object> l = new ArrayList<Object>();
+        l.add(f);
+        l.add(format);
+        firePropertyChange(UPLOAD_PHOTO_PROPERTY, null, l);
+        setVisible(false);
+        dispose();
+    }
+
     /**
      * Builds the tool bar.
      * 
@@ -257,97 +254,97 @@ public class UploadPictureDialog
      */
     private JPanel buildToolbar()
     {
-    	JPanel bar = new JPanel();
-    	bar.setBorder(null);
-    	bar.add(cancelButton);
-    	bar.add(Box.createRigidArea(H_SPACER_SIZE));
-    	bar.add(saveButton);
-    	JPanel p = UIUtilities.buildComponentPanelRight(bar);
+        JPanel bar = new JPanel();
+        bar.setBorder(null);
+        bar.add(cancelButton);
+        bar.add(Box.createRigidArea(H_SPACER_SIZE));
+        bar.add(saveButton);
+        JPanel p = UIUtilities.buildComponentPanelRight(bar);
         p.setOpaque(true);
         return p;
     }
-    
-	/**
-	 * Builds and lays out the controls.
-	 * 
-	 * @return See above.
-	 */
-	private JPanel buildControls()
-	{	
-    	JPanel p = new JPanel();
-    	p.setLayout(new BorderLayout(0, 0));
-    	p.add(chooser, BorderLayout.CENTER);
-    	p.add(buildToolbar(), BorderLayout.SOUTH);
-		return p;
-	}
-	
+
+    /**
+     * Builds and lays out the controls.
+     * 
+     * @return See above.
+     */
+    private JPanel buildControls()
+    {	
+        JPanel p = new JPanel();
+        p.setLayout(new BorderLayout(0, 0));
+        p.add(chooser, BorderLayout.CENTER);
+        p.add(buildToolbar(), BorderLayout.SOUTH);
+        return p;
+    }
+
     /** Builds and lays out the UI. */
-	private void buildGUI()
-	{
-		IconManager icons = IconManager.getInstance();
-		TitlePanel tp = new TitlePanel(TITLE, TEXT, 
-				icons.getIcon(IconManager.USER_PHOTO_48));
-		Container c = getContentPane();
-		c.setLayout(new BorderLayout(0, 0));
-		c.add(tp, BorderLayout.NORTH);
-		c.add(buildControls(), BorderLayout.CENTER);
-		if (JDialog.isDefaultLookAndFeelDecorated()) {
-            boolean supportsWindowDecorations = 
-            UIManager.getLookAndFeel().getSupportsWindowDecorations();
+    private void buildGUI()
+    {
+        IconManager icons = IconManager.getInstance();
+        TitlePanel tp = new TitlePanel(TITLE, TEXT,
+                icons.getIcon(IconManager.USER_PHOTO_48));
+        Container c = getContentPane();
+        c.setLayout(new BorderLayout(0, 0));
+        c.add(tp, BorderLayout.NORTH);
+        c.add(buildControls(), BorderLayout.CENTER);
+        if (JDialog.isDefaultLookAndFeelDecorated()) {
+            boolean supportsWindowDecorations =
+                    UIManager.getLookAndFeel().getSupportsWindowDecorations();
             if (supportsWindowDecorations)
                 getRootPane().setWindowDecorationStyle(
-                            JRootPane.FILE_CHOOSER_DIALOG);
+                        JRootPane.FILE_CHOOSER_DIALOG);
         }
-	}
-	
-	/**
-	 * Creates a new instance.
-	 * 
-	 * @param owner The owner of the dialog.
-	 */
-	public UploadPictureDialog(JFrame owner)
-	{
-		super(owner);
-		setProperties();
-		initComponents();
-		buildGUI();
-	}
-	
-	/**
-	 * Uploads the script or closes the dialog.
-	 * @see ActionListener#actionPerformed(ActionEvent)
-	 */
-	public void actionPerformed(ActionEvent e)
-	{
-		int index = Integer.parseInt(e.getActionCommand());
-		switch (index) {
-			case CANCEL:
-				close();
-				break;
-			case SAVE:
-				upload();
-		}
-	}
-	
-    /**
-	 * Enables or not the <code>Save</code> option depending on the text 
-	 * entered in the {@link #scriptArea}.
-	 * @see DocumentListener#insertUpdate(DocumentEvent)
-	 */
-	public void insertUpdate(DocumentEvent e) { handleTextUpdate(); }
+    }
 
-	/**
-	 * Enables or not the <code>Save</code> option depending on the text 
-	 * entered in the {@link #scriptArea}.
-	 * @see DocumentListener#removeUpdate(DocumentEvent)
-	 */
-	public void removeUpdate(DocumentEvent e) { handleTextUpdate(); }
-    
-	/**
-	 * Required by the {@link DocumentListener} I/F but no-operation
-	 * implementation in our case.
-	 * @see DocumentListener#changedUpdate(DocumentEvent)
-	 */
-	public void changedUpdate(DocumentEvent e) {}
-	
+    /**
+     * Creates a new instance.
+     * 
+     * @param owner The owner of the dialog.
+     */
+    public UploadPictureDialog(JFrame owner)
+    {
+        super(owner);
+        setProperties();
+        initComponents();
+        buildGUI();
+    }
+
+    /**
+     * Uploads the script or closes the dialog.
+     * @see ActionListener#actionPerformed(ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+        int index = Integer.parseInt(e.getActionCommand());
+        switch (index) {
+        case CANCEL:
+            close();
+            break;
+        case SAVE:
+            upload();
+        }
+    }
+
+    /**
+     * Enables or not the <code>Save</code> option depending on the text
+     * entered in the {@link #scriptArea}.
+     * @see DocumentListener#insertUpdate(DocumentEvent)
+     */
+    public void insertUpdate(DocumentEvent e) { handleTextUpdate(); }
+
+    /**
+     * Enables or not the <code>Save</code> option depending on the text
+     * entered in the {@link #scriptArea}.
+     * @see DocumentListener#removeUpdate(DocumentEvent)
+     */
+    public void removeUpdate(DocumentEvent e) { handleTextUpdate(); }
+
+    /**
+     * Required by the {@link DocumentListener} I/F but no-operation
+     * implementation in our case.
+     * @see DocumentListener#changedUpdate(DocumentEvent)
+     */
+    public void changedUpdate(DocumentEvent e) {}
+
 }
