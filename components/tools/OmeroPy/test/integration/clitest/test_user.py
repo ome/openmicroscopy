cc60281334aff3e6a5c4618fac37b75513cf05da
Sebastien Besson <seb.besson@gmail.com>
2013-11-14 16:43:30 +0000

    Add integration tests for joingroup and leavegroup subcommands

--- components/tools/OmeroPy/test/integration/clitest/test_user.py
+++ components/tools/OmeroPy/test/integration/clitest/test_user.py
@@ -25,9 +25,9 @@ from test.integration.clitest.cli import CLITest, RootCLITest
 import pytest
 
 subcommands = ['add', 'list', 'password', 'email', 'joingroup', 'leavegroup']
-user_pairs = [('--id', 'id'), ('--name', 'name')]
-group_pairs = [(None, 'id'), (None, 'omeName'), ('--user-id', 'id'),
-               ('--user-name', 'omeName')]
+user_pairs = [('--id', 'id'), ('--name', 'omeName')]
+group_pairs = [(None, 'id'), (None, 'name'), ('--group-id', 'id'),
+               ('--group-name', 'name')]
 
 
 class TestUser(CLITest):
@@ -49,9 +49,80 @@ class TestUser(CLITest):
         self.cli.invoke(self.args, strict=True)
 
 
-class TestGroupRoot(RootCLITest):
+class TestUserRoot(RootCLITest):
 
     def setup_method(self, method):
-        super(TestUser, self).setup_method(method)
+        super(TestUserRoot, self).setup_method(method)
         self.cli.register("user", UserControl, "TEST")
         self.args += ["user"]
+
+    def getuserids(self, gid):
+        group = self.sf.getAdminService().getGroup(gid)
+        return [x.child.id.val for x in group.copyGroupExperimenterMap()]
+
+    def getmemberids(self, gid):
+        group = self.sf.getAdminService().getGroup(gid)
+        return [x.child.id.val for x in group.copyGroupExperimenterMap()
+                if not x.owner.val]
+
+    def getownerids(self, gid):
+        group = self.sf.getAdminService().getGroup(gid)
+        return [x.child.id.val for x in group.copyGroupExperimenterMap()
+                if x.owner.val]
+
+    # User joingroup subcommand
+    # ========================================================================
+    @pytest.mark.parametrize("user_prefix,user_attr", user_pairs)
+    @pytest.mark.parametrize("group_prefix,group_attr", group_pairs)
+    @pytest.mark.parametrize("owner_arg", [None, '--as-owner'])
+    def testJoinGroup(self, user_prefix, user_attr, group_prefix, group_attr,
+                      owner_arg):
+        user = self.new_user()
+        group = self.new_group()
+        assert user.id.val not in self.getuserids(group.id.val)
+
+        self.args += ["joingroup", user_prefix,
+                      "%s" % getattr(user, user_attr).val]
+        if group_prefix:
+            self.args += [group_prefix]
+        self.args += ["%s" % getattr(group, group_attr).val]
+        if owner_arg:
+            self.args += [owner_arg]
+        self.cli.invoke(self.args, strict=True)
+
+        # Check user has been added to the list of member/owners
+        if owner_arg:
+            assert user.id.val in self.getownerids(group.id.val)
+        else:
+            assert user.id.val in self.getmemberids(group.id.val)
+
+    # User leavegroup subcommand
+    # ========================================================================
+    @pytest.mark.parametrize("user_prefix,user_attr", user_pairs)
+    @pytest.mark.parametrize("group_prefix,group_attr", group_pairs)
+    @pytest.mark.parametrize("is_owner", [True, False])
+    @pytest.mark.parametrize("owner_arg", [None, '--as-owner'])
+    def testLeaveGroup(self, user_prefix, user_attr, group_prefix, group_attr,
+                       is_owner, owner_arg):
+        user = self.new_user()
+        group = self.new_group([user])
+        if is_owner:
+            self.root.sf.getAdminService().setGroupOwner(group, user)
+            assert user.id.val in self.getownerids(group.id.val)
+        else:
+            assert user.id.val in self.getmemberids(group.id.val)
+
+        self.args += ["leavegroup", user_prefix,
+                      "%s" % getattr(user, user_attr).val]
+        if group_prefix:
+            self.args += [group_prefix]
+        self.args += ["%s" % getattr(group, group_attr).val]
+        if owner_arg:
+            self.args += [owner_arg]
+        self.cli.invoke(self.args, strict=True)
+
+        # Check user has been added to the list of member/owners
+        if owner_arg:
+            assert user.id.val not in self.getownerids(group.id.val)
+        else:
+            assert user.id.val not in self.getuserids(group.id.val)
