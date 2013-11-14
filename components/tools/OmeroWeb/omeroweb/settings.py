8241010f9c123f423c0d27342d44e30b8fd587da
Andreas Knab <knabar@gmail.com>
2013-11-14 17:16:13 +0100

    Update manage.py to Django 1.6; move nothreading monkeypatch

--- components/tools/OmeroWeb/omeroweb/settings.py
+++ components/tools/OmeroWeb/omeroweb/settings.py
@@ -27,16 +27,6 @@
 #
 
 
-# Monkeypatch Django 1.5 development web server to always run in single thread
-# even if --nothreading is not specified on command line
-def force_nothreading(addr, port, wsgi_handler, ipv6=False, threading=False):
-    django_core_servers_basehttp_run(addr, port, wsgi_handler, ipv6, False)
-import django.core.servers.basehttp
-if django.core.servers.basehttp.run.__module__ != 'settings':
-    django_core_servers_basehttp_run = django.core.servers.basehttp.run
-    django.core.servers.basehttp.run = force_nothreading
-
-
 import os.path
 import sys
 import datetime
