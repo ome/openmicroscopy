fa510311f75248c43fe6914d1b0f19e5cb4d98d6
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-11-17 10:59:25 +0000

    Add new methods to check if system user/group.

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/AdminServiceImpl.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/AdminServiceImpl.java
@@ -2,10 +2,10 @@
  * org.openmicroscopy.shoola.env.data.AdminServiceImpl 
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2010 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -790,4 +790,21 @@ class AdminServiceImpl
 				LookupNames.USER_GROUP_DETAILS);
 	}
 
+    /**
+     * Implemented as specified by {@link AdminService}.
+     * @see AdminService#isSystemGroup(long)
+     */
+    public boolean isSystemGroup(long groupID)
+    {
+        return false;
+    }
+
+    /**
+     * Implemented as specified by {@link AdminService}.
+     * @see AdminService#isSystemUser(long)
+     */
+    public boolean isSystemUser(long userID)
+    {
+        return false;
+    }
 }
