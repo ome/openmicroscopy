5455633ef4a73e6030e5f909bcf7677cfd3ac07b
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-11-22 16:23:18 +0000

    Remove tabs and whitespace.

--- components/insight/SRC/org/openmicroscopy/shoola/svc/transport/HttpChannel.java
+++ components/insight/SRC/org/openmicroscopy/shoola/svc/transport/HttpChannel.java
@@ -2,10 +2,10 @@
  * org.openmicroscopy.shoola.svc.transport.HttpChannel 
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -37,75 +37,72 @@ import org.openmicroscopy.shoola.svc.proxy.Request;
 
 /** 
  * Top-channel that all channels using <code>HTTP</code> communication should 
- * extend. 
+ * extend.
  *
- * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * @author Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
  * <a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
  * @author Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
  * <a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
  * @version 3.0
- * <small>
- * (<b>Internal version:</b> $Revision: $Date: $)
- * </small>
  * @since OME3.0
  */
 public abstract class HttpChannel
 {
 
-	/** The default channel type. */
-	public static final int DEFAULT = 0;
-	
-	/** The type identifying that one channel is created per request. */
+    /** The default channel type. */
+    public static final int DEFAULT = 0;
+
+    /** The type identifying that one channel is created per request. */
     public static final int CONNECTION_PER_REQUEST = 1;
-    
+
     /** Default property key for proxy host. */
-    static final String		PROXY_HOST = "http.proxyHost";
-    
+    static final String PROXY_HOST = "http.proxyHost";
+
     /** Default property key for proxy port. */
-    static final String		PROXY_PORT = "http.proxyPort";
-    
+    static final String PROXY_PORT = "http.proxyPort";
+
     /** 
      * Returns the channel corresponding to the passed type.
      * 
      * @return See above.
      */ 
     protected abstract HttpClient getCommunicationLink();
-    
+
     /**
      * Returns the path derived from the server's URL.
      * 
      * @return See above.
      */
     protected abstract String getRequestPath();
-    
+
     /**
      * Posts the request and catches the reply.
      * 
-     * @param out	The request to post.
-     * @param in	The reply to fill.
+     * @param out The request to post.
+     * @param in The reply to fill.
      * @throws TransportException If an error occurred while transferring data.
      * @throws IOException	If an error occurred while unmarshalling the method.
      */
     public void exchange(Request out, Reply in) 
-    	throws TransportException, IOException
-	{
-	    //Sanity checks.
-	    if (out == null) throw new NullPointerException("No request.");
-	    if (in == null) throw new NullPointerException("No reply.");
-	    
-	    //Build HTTP request, send it, and wait for response.
-	    //Then read the response into the Reply object.
-	    HttpClient comLink = getCommunicationLink();
-	    HttpMethod method = null;
-	    try {
-	        method = out.marshal();
-	        method.setPath(getRequestPath());
-	        comLink.executeMethod(method);
-	        in.unmarshal(method, this);
-	    } finally {
-	        //Required by Http Client library.
-	        if (method != null) method.releaseConnection();
-	    }
-	}
-    
+            throws TransportException, IOException
+    {
+        //Sanity checks.
+        if (out == null) throw new NullPointerException("No request.");
+        if (in == null) throw new NullPointerException("No reply.");
+
+        //Build HTTP request, send it, and wait for response.
+        //Then read the response into the Reply object.
+        HttpClient comLink = getCommunicationLink();
+        HttpMethod method = null;
+        try {
+            method = out.marshal();
+            method.setPath(getRequestPath());
+            comLink.executeMethod(method);
+            in.unmarshal(method, this);
+        } finally {
+            //Required by Http Client library.
+            if (method != null) method.releaseConnection();
+        }
+    }
+
 }
