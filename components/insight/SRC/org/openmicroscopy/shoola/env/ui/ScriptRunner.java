3a19ac23ffea3c68c9f0c9cda7147caeac4e8a5d
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-11-25 20:24:29 +0000

    Remove tabs and whitespace.

--- components/insight/SRC/org/openmicroscopy/shoola/env/ui/ScriptRunner.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/ui/ScriptRunner.java
@@ -1,11 +1,11 @@
 /*
- * org.openmicroscopy.shoola.env.ui.ScriptRunner 
+ * org.openmicroscopy.shoola.env.ui.ScriptRunner
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2010 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -43,112 +43,110 @@ import org.openmicroscopy.shoola.env.data.views.ProcessCallback;
  * @author Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
  * <a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
  * @version 3.0
- * <small>
- * (<b>Internal version:</b> $Revision: $Date: $)
- * </small>
  * @since 3.0-Beta4
  */
-public class ScriptRunner 	
-	extends UserNotifierLoader
+public class ScriptRunner
+    extends UserNotifierLoader
 {
-	
+
     /** Handle to the asynchronous call so that we can cancel it. */
-    private CallHandle  			handle;
-    
+    private CallHandle handle;
+
     /** Reference to the script to run. */
-    private ScriptObject 			script;
+    private ScriptObject script;
 
     /** The call-back returned by the server. */
-    private ProcessCallback 		callBack;
-    
+    private ProcessCallback callBack;
+
     /** Flag indicating that the user cancelled the action.*/
     private boolean cancelled;
-    
+
     /**
      * Notifies that an error occurred.
      * @see UserNotifierLoader#onException(String)
      */
     protected void onException(String message, Throwable ex)
     { 
-    	activity.notifyError("Unable to run the script", message, ex);
+        activity.notifyError("Unable to run the script", message, ex);
     }
-    
+
     /**
      * Creates a new instance.
      * 
-     * @param viewer	The viewer this data loader is for.
-     *               	Mustn't be <code>null</code>.
-     * @param registry	Convenience reference for subclasses.
+     * @param viewer The viewer this data loader is for.
+     *               Mustn't be <code>null</code>.
+     * @param registry Convenience reference for subclasses.
      * @param ctx The security context.
-     * @param script  	The script to run.
-     * @param activity  The activity associated to this loader.
+     * @param script The script to run.
+     * @param activity The activity associated to this loader.
      */
-	public ScriptRunner(UserNotifier viewer,  Registry registry,
-		SecurityContext ctx, ScriptObject script, ActivityComponent activity)
-	{
-		super(viewer, registry, ctx, activity);
-		if (script == null)
-			throw new IllegalArgumentException("No script to run.");
-		this.script = script;
-	}
-	
-	/**
+    public ScriptRunner(UserNotifier viewer, Registry registry,
+            SecurityContext ctx, ScriptObject script,
+            ActivityComponent activity)
+    {
+        super(viewer, registry, ctx, activity);
+        if (script == null)
+            throw new IllegalArgumentException("No script to run.");
+        this.script = script;
+    }
+
+    /**
      * Runs the script.
      * @see UserNotifierLoader#load()
      */
     public void load()
     {
-    	handle = ivView.runScript(ctx, script, this);
+        handle = ivView.runScript(ctx, script, this);
     }
-    
+
     /**
      * Cancels the on-going data retrieval.
      * @see UserNotifierLoader#cancel()
      */
     public void cancel()
     {
-    	cancelled = true;
-    	try {
-    		if (callBack != null) {
-    			callBack.cancel();
-        		activity.onActivityCancelled();
-    		}
-		} catch (Exception e) {
-			handleException(e);
-		}
-    	handle.cancel();
-	}
-    
+        cancelled = true;
+        try {
+            if (callBack != null) {
+                callBack.cancel();
+                activity.onActivityCancelled();
+            }
+        } catch (Exception e) {
+            handleException(e);
+        }
+        handle.cancel();
+    }
+
     /** 
      * Stores the call-back.
      * @see UserNotifierLoader#update(DSCallFeedbackEvent)
      */
-    public void update(DSCallFeedbackEvent fe) 
+    public void update(DSCallFeedbackEvent fe)
     {
         Object o = fe.getPartialResult();
         if (o != null) {
-        	if (Boolean.valueOf(false).equals(o)) {
-        		onException(MESSAGE_RUN, null);
-        	} else if (o instanceof ProcessCallback) {
-        		callBack = (ProcessCallback) o;
-            	callBack.setAdapter(this);
-            	activity.onCallBackSet();
-            	if (cancelled) cancel();
-        	}
+            if (Boolean.valueOf(false).equals(o)) {
+                onException(MESSAGE_RUN, null);
+            } else if (o instanceof ProcessCallback) {
+                callBack = (ProcessCallback) o;
+                callBack.setAdapter(this);
+                activity.onCallBackSet();
+                if (cancelled) cancel();
+            }
         }
     }
- 
+
     /** 
      * Feeds the result back to the viewer. 
      * @see UserNotifierLoader#handleResult(Object)
      */
     public void handleResult(Object result)
     { 
-    	if (Boolean.valueOf(false).equals(result)) {
-    		onException(MESSAGE_RUN, null); 
-    	} else if (!(result instanceof Boolean)) {
-    		activity.endActivity(result);
-    	}
+        if (Boolean.valueOf(false).equals(result)) {
+            onException(MESSAGE_RUN, null);
+        } else if (!(result instanceof Boolean)) {
+            activity.endActivity(result);
+        }
     }
-	
+
 }
