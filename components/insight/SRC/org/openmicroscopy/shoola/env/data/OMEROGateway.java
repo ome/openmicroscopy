ca4e45dbab9fb0c38b51c44f8f198253606e84af
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-11-27 11:21:34 +0000

    Remove call added during testing.

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/OMEROGateway.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/OMEROGateway.java
@@ -1963,7 +1963,6 @@ class OMEROGateway
 			connector = new Connector(ctx, secureClient, entryEncrypted,
 					encrypted, dsFactory.getLogger());
 			groupConnectorMap.put(ctx.getGroupID(), connector);
-			reconnect(userName, "omero");
 			return exp;
 		} catch (Throwable e) {
 			connected = false;
