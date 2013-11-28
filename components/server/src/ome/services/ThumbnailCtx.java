106e31c53e351f8095f40206fb99fbf3e50458db
Jean-Marie Burel <j.burel@dundee.ac.uk>
2013-11-28 15:30:32 +0000

    Fix typo and date. Order owner's rendering settings.

--- components/server/src/ome/services/ThumbnailCtx.java
+++ components/server/src/ome/services/ThumbnailCtx.java
@@ -754,7 +754,7 @@ public class ThumbnailCtx
                 "join fetch r.pixels as p " +
                 "join fetch r.details.updateEvent " +
                 "join p.details.updateEvent " +
-                "where r.details.owner.id = :id and r.pixels.id in (:ids) "+
+                "where r.details.owner.id = :id and r.pixels.id in (:ids) " +
                 "order by r.details.updateEvent.time asc",
                 new Parameters().addId(userId).addIds(pixelsIds));
         s1.stop();
@@ -777,7 +777,7 @@ public class ThumbnailCtx
                 "join fetch r.details.updateEvent " +
                 "join fetch p.details.updateEvent " +
                 "where r.details.owner.id = :id " +
-                "and r.pixels.image.id in (:ids) "+
+                "and r.pixels.image.id in (:ids) " +
                 "order by r.details.updateEvent.time asc",
                 new Parameters().addId(userId).addIds(imageIds));
         s1.stop();
@@ -800,7 +800,8 @@ public class ThumbnailCtx
                 "join fetch r.details.updateEvent " +
                 "join fetch p.details.updateEvent " +
                 "where r.details.owner.id = p.details.owner.id " +
-                "and r.pixels.id in (:ids)",
+                "and r.pixels.id in (:ids) " +
+                "order by r.details.updateEvent.time asc",
                 new Parameters().addIds(pixelsIds));
         s1.stop();
         return toReturn;
