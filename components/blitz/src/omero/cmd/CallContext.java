61c7e97e4d39462d5addbc314e905c94f1cdd96f
Blazej Pindelski <bpindelski@dundee.ac.uk>
2013-12-04 10:50:07 +0000

    Update copyright date.

--- components/blitz/src/omero/cmd/CallContext.java
+++ components/blitz/src/omero/cmd/CallContext.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2012 Glencoe Software, Inc. All rights reserved.
+ *   Copyright 2012-2013 Glencoe Software, Inc. All rights reserved.
  *   Use is subject to license terms supplied in LICENSE.txt
  */
 
