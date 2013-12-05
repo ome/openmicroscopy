a9c2ce0c8415984a19477dd89758b8eff3ea1373
Simon Li <spli@dundee.ac.uk>
2013-12-05 16:20:20 +0000

    brew services doesn't seem to work, use pg_ctl instead

--- docs/hudson/OMERO-homebrew-install.sh
+++ docs/hudson/OMERO-homebrew-install.sh
@@ -105,7 +105,7 @@ if [ -d "$PSQL_DIR" ]; then
     rm -rf $PSQL_DIR
 fi
 bin/initdb $PSQL_DIR
-bin/brew services restart postgresql
+bin/pg_ctl -D $PSQL_DIR -l $PSQL_DIR/server.log start
 
 # Create user and database
 bin/createuser -w -D -R -S db_user
