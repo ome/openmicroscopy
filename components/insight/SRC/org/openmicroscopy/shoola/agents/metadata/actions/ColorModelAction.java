e5a2c911a58c36c0e2ddf600a43691be076c5bd5
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2014-01-06 15:02:45 +0000

    elide commented-out code

--- components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/actions/ColorModelAction.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/actions/ColorModelAction.java
@@ -84,14 +84,12 @@ public class ColorModelAction
     	switch (index) {
 	    	case GREY_SCALE_MODEL:
 	    	default:
-	    		//putValue(Action.NAME, "Greyscale");
 	    		putValue(Action.SHORT_DESCRIPTION, 
 	    				UIUtilities.formatToolTipText(DESCRIPTION_GREY_SCALE));
 	    		putValue(Action.SMALL_ICON, 
 	    				icons.getIcon(IconManager.GRAYSCALE));
 	    		break;
 	    	case RGB_MODEL:
-	    		//putValue(Action.NAME, "Color");
 	    		putValue(Action.SHORT_DESCRIPTION, 
 	    				UIUtilities.formatToolTipText(DESCRIPTION_RGB));
 	    		putValue(Action.SMALL_ICON, icons.getIcon(IconManager.RGB));
