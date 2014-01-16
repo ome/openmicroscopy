ce57b305749986eec84f970e62bb4685d7fed97a
jmoore <josh@glencoesoftware.com>
2014-01-16 12:07:57 +0100

    Fix header of ImportLibrary

--- components/blitz/src/ome/formats/importer/ImportLibrary.java
+++ components/blitz/src/ome/formats/importer/ImportLibrary.java
@@ -1,15 +1,20 @@
 /*
- * ome.formats.importer.ImportLibrary
+ * Copyright (C) 2005-2014 University of Dundee & Open Microscopy Environment.
+ * All rights reserved.
  *
- *------------------------------------------------------------------------------
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  Copyright (C) 2005-2013 Open Microscopy Environment
- *      Massachusetts Institute of Technology,
- *      National Institutes of Health,
- *      University of Dundee
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *
- *------------------------------------------------------------------------------
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package ome.formats.importer;
