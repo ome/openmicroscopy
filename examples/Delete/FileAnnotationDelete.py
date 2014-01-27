552cf794a72f799a61f3ebb28025736d381073a8
Will Moore <will@lifesci.dundee.ac.uk>
2014-01-27 22:11:54 +0000

    Delete example uses CmdCallback

--- examples/Delete/FileAnnotationDelete.py
+++ examples/Delete/FileAnnotationDelete.py
@@ -9,44 +9,48 @@ OriginalFile and any annotation links.
 import omero
 import omero.callbacks
 
-c = omero.client()
+c = omero.client("localhost")
 ice_config = c.getProperty("Ice.Config")
 
 from omero.rtypes import *
 from omero.model import *
 
 try:
-    s = c.createSession()
+    s = c.createSession("will", "ome")
     d = DatasetI()
     d.setName(rstring("FileAnnotationDelete"))
+    d = s.getUpdateService().saveAndReturnObject(d)
     fa = FileAnnotationI()
-    file = c.upload(ice_config)
-    fa.setFile(file)
+    file = c.upload("test.xml")
+    # fa.setFile(file)
+    fa.setFile(OriginalFileI(file.id.val, False))
+    fa = s.getUpdateService().saveAndReturnObject(fa)
+
+    # Get Dataset with annotations loaded, so we can add fa to the list
+    query = "select d from Dataset as d " \
+            "left outer join fetch d.annotationLinks as dal " \
+            "left outer join fetch dal.child as a where d.id = :d"
+    params = omero.sys.ParametersI()
+    params.addLong('d', d.id.val)
+    d = s.getQueryService().findByQuery(query, params)
     d.linkAnnotation(fa)
     d = s.getUpdateService().saveAndReturnObject(d)
-    fa = d.linkedAnnotationList()[0]
+    # fa = d.linkedAnnotationList()[0]
 
-    deleteServicePrx = s.getDeleteService();
-    dc = omero.api.delete.DeleteCommand("/Annotation", fa.id.val, None)
-    deleteHandlePrx = deleteServicePrx .queueDelete([dc])
-    cb = omero.callbacks.DeleteCallbackI(c, deleteHandlePrx)
+    graph_spec = "/Annotation"
+    options = {}
+    delCmd = omero.cmd.Delete(graph_spec, long(fa.id.val), options)
 
-    try:
+    dcs = [delCmd]
+    doall = omero.cmd.DoAll()
+    doall.requests = dcs
+    handle = s.submit(doall)
 
-        try:
-            cb.loop(10, 500)
-        except omero.LockTimeout:
-            print "Not finished in 5 seconds. Cancelling..."
-            if not deleteHandlePrx.cancel():
-                print "ERROR: Failed to cancel"
-
-        reports = deleteHandlePrx.report()
-        r = reports[0] # We only sent one command
-        print "Report:error=%s,warning=%s,deleted=%s" % \
-            (r.error, r.warning, r.actualDeletes)
-
-    finally:
-        cb.close()
+    callback = omero.callbacks.CmdCallbackI(c, handle)
+    loops = 10
+    delay = 500
+    callback.loop(loops, delay) # Throw LockTimeout
+    rsp = callback.getResponse()
 
 finally:
     c.closeSession()
