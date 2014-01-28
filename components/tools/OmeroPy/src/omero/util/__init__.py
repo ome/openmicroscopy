5daac78c8e4b68bf536f6cf64b07dde79f69d22b
jmoore <josh@glencoesoftware.com>
2014-01-28 17:20:41 +0100

    Make LOGDIR configurable for Python processes (See #11943)
    Java processes are being explicitly given the OMERO_LOGFILE
    variable. Python processes make use of omero.util.LOG_DIR for
    choosing a location. Now this is configurable via
    `omero.logging.directory`.

--- components/tools/OmeroPy/src/omero/util/__init__.py
+++ components/tools/OmeroPy/src/omero/util/__init__.py
@@ -73,13 +73,14 @@ def configure_server_logging(props):
     """
     program_name = props.getProperty("Ice.Admin.ServerId")
     # Using Ice.ProgramName on Windows failed
+    log_dir = props.getPropertyWithDefault("omero.logging.directory", LOGDIR)
     log_name = program_name+".log"
     log_timed = props.getPropertyWithDefault("omero.logging.timedlog","False")[0] in ('T', 't')
     log_num = int(props.getPropertyWithDefault("omero.logging.lognum",str(LOGNUM)))
     log_size = int(props.getPropertyWithDefault("omero.logging.logsize",str(LOGSIZE)))
     log_num = int(props.getPropertyWithDefault("omero.logging.lognum",str(LOGNUM)))
     log_level = int(props.getPropertyWithDefault("omero.logging.level",str(LOGLEVEL)))
-    configure_logging(LOGDIR, log_name, loglevel=log_level, maxBytes=log_size, backupCount=log_num, time_rollover = log_timed)
+    configure_logging(log_dir, log_name, loglevel=log_level, maxBytes=log_size, backupCount=log_num, time_rollover = log_timed)
 
     sys.stdout = StreamRedirect(logging.getLogger("stdout"))
     sys.stderr = StreamRedirect(logging.getLogger("stderr"))
