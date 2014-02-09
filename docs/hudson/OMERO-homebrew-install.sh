5ea58e04b118e5e392d4410f8ac94164be0d6fda
Sebastien Besson <seb.besson@gmail.com>
2014-02-09 20:07:26 +0000

    Update OMERO-homebrew-install.sh to install archived formulas

--- docs/hudson/OMERO-homebrew-install.sh
+++ docs/hudson/OMERO-homebrew-install.sh
@@ -7,7 +7,6 @@ set -x
 
 export PSQL_DIR=${PSQL_DIR:-/usr/local/var/postgres}
 export OMERO_DATA_DIR=${OMERO_DATA_DIR:-/tmp/var/OMERO.data}
-export BREW_OPTS=${BREW_OPTS:-}
 export SCRIPT_NAME=${SCRIPT_NAME:-OMERO.sql}
 
 # Test whether this script is run in a job environment
@@ -68,7 +67,7 @@ cd /usr/local
 ###################################################################
 
 # Install Bio-Formats
-bin/brew install bioformats $BREW_OPTS
+bin/brew install bioformats44
 showinf -version
 
 ###################################################################
@@ -76,17 +75,17 @@ showinf -version
 ###################################################################
 
 # Install PostgreSQL and OMERO
-bin/brew install omero $BREW_OPTS
+bin/brew install omero44
 bin/brew install postgres
 
 # Install OMERO Python dependencies
 bash bin/omero_python_deps
 
 # Set environment variables
-ICE_VERSION=$(bin/brew deps omero $BREW_OPTS | grep ice)
-export ICE_CONFIG=$(bin/brew --prefix omero)/etc/ice.config
+ICE_VERSION=$(bin/brew deps omero44 $BREW_OPTS | grep ice)
+export ICE_CONFIG=$(bin/brew --prefix omero44)/etc/ice.config
 export ICE_HOME=$(bin/brew --prefix $ICE_VERSION)
-export PYTHONPATH=$(bin/brew --prefix omero)/lib/python:$ICE_HOME/python
+export PYTHONPATH=$(bin/brew --prefix omero44)/lib/python:$ICE_HOME/python
 export PATH=$(bin/brew --prefix)/bin:$(bin/brew --prefix)/sbin:/usr/local/lib/node_modules:$ICE_HOME/bin:$PATH
 export DYLD_LIBRARY_PATH=$ICE_HOME/lib:$ICE_HOME/python:${DYLD_LIBRARY_PATH-}
 
