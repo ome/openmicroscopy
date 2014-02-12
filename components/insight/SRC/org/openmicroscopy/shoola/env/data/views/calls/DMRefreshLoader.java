9924bbd1de2eda7b6d00c7d79350219d5cc1e66d
Jean-Marie Burel <j.burel@dundee.ac.uk>
2014-02-12 12:33:20 +0000

    Updated header.

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/views/calls/DMRefreshLoader.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/views/calls/DMRefreshLoader.java
@@ -2,10 +2,10 @@
  * org.openmicroscopy.shoola.env.data.views.calls.DMRefreshLoader
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2014 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -62,14 +62,11 @@ import pojos.TagAnnotationData;
 /** 
  * Command to refresh a data trees.
  *
- * @author  Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
- * 				<a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
- * @author	Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
- * 				<a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
+ * @author Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ *         <a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @author Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
+ *         <a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
  * @version 3.0
- * <small>
- * (<b>Internal version:</b> $Revision: $ $Date: $)
- * </small>
  * @since OME2.2
  */
 public class DMRefreshLoader
