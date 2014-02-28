99526887d355de20d596c0c69ebfbf1177424d99
Dominik Lindner <d.lindner@dundee.ac.uk>
2014-02-28 11:43:28 +0000

    Updated copyrights to include 2014

--- components/insight/SRC/org/openmicroscopy/shoola/agents/util/ui/PermissionMenu.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/util/ui/PermissionMenu.java
@@ -2,7 +2,7 @@
  * org.openmicroscopy.shoola.agents.util.ui.PermissionMenu 
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2012 University of Dundee & Open Microscopy Environment.
+ *  Copyright (C) 2006-2014 University of Dundee & Open Microscopy Environment.
  *  All rights reserved.
  *
  *
