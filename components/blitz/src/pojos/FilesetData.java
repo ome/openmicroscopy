088ca8d25004ea5fb1ea3bca47ee87341cea3f2b
Dominik Lindner <d.lindner@dundee.ac.uk>
2014-03-13 16:20:11 +0000

    Updated copyright date

--- components/blitz/src/pojos/FilesetData.java
+++ components/blitz/src/pojos/FilesetData.java
@@ -2,7 +2,7 @@
  * pojos.FilesetData 
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2013 University of Dundee & Open Microscopy Environment.
+ *  Copyright (C) 2013-2014 University of Dundee & Open Microscopy Environment.
  *  All rights reserved.
  *
  *
