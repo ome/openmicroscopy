8b1881b7f1d91d190ec894b5c5f735c34d0640bd
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2014-03-25 15:33:08 +0000

    fixing webstart index page that allows to set custom intro page for OMERO.web

--- components/tools/OmeroWeb/omeroweb/urls.py
+++ components/tools/OmeroWeb/omeroweb/urls.py
@@ -46,7 +46,7 @@ def redirect_urlpatterns():
     """
     Helper function to return a URL pattern for index page http://host/.
     """
-    if settings.INDEX_TEMPLATE is None:
+    if settings.WEBSTART_TEMPLATE is None:
         return patterns('', url(r'^$', RedirectView.as_view(url=reverse_lazy('webindex')), name="index" ))
     else:
         return patterns('', url( r'^$', 'omeroweb.webstart.views.index', name="index" ),)
