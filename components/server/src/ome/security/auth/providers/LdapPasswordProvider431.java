0498ea6aef658f50bbda4cb6a7611b8356592009
Blazej Pindelski <bpindelski@dundee.ac.uk>
2014-04-10 12:21:36 +0100

    Revert "Update LDAP password providers with new flag."
    This reverts commit 9d99526d0f322bb7380d24c9b62cef74538c1fd7.

--- components/server/src/ome/security/auth/providers/LdapPasswordProvider431.java
+++ components/server/src/ome/security/auth/providers/LdapPasswordProvider431.java
@@ -93,8 +93,7 @@ public class LdapPasswordProvider431 extends ConfigurablePasswordProvider {
                 if (readOnly == true) {
                     throw new IllegalStateException("Cannot create user!");
                 }
-                boolean login = ldapUtil.createUserFromLdap(user, password,
-                        true);
+                boolean login = ldapUtil.createUserFromLdap(user, password);
                 // Use default logic if the user creation did not exist,
                 // because there may be another non-database login mechanism
                 // which should also be given a chance.
