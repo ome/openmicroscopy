6b49715defe147bc205473668df36da05ad680ca
jmoore <josh@glencoesoftware.com>
2014-04-10 13:53:49 +0200

    Fix flake8 errors in setup.py

--- components/tools/OmeroWeb/setup.py
+++ components/tools/OmeroWeb/setup.py
@@ -15,7 +15,7 @@ from test_setup import PyTest
 
 for tools in glob.glob("../../../lib/repository/setuptools*.egg"):
     if tools.find(".".join(map(str, sys.version_info[0:2]))) > 0:
-       sys.path.insert(0, os.path.abspath(tools))
+        sys.path.insert(0, os.path.abspath(tools))
 
 from ez_setup import use_setuptools
 use_setuptools(to_dir='../../../lib/repository')
@@ -34,7 +34,6 @@ OmeroWeb is the container of the web clients for OMERO."
       download_url="http://trac.openmicroscopy.org.uk/ome/wiki/OmeroWeb",
       packages=[''],
       test_suite='test.suite',
-      cmdclass = {'test': PyTest},
+      cmdclass={'test': PyTest},
       tests_require=['pytest'],
-)
-
+      )
