5a9b2b46f46358a89c5bc2f5aaa628389953a14b
Sebastien Besson <seb.besson@gmail.com>
2014-04-12 20:28:50 +0100

    Add first implementation of CLI download for Image:id input
    - Allow an image to be passed under the form Image:image_id
    - Run the query to retrieve original files from the image

--- components/tools/OmeroPy/src/omero/plugins/download.py
+++ components/tools/OmeroPy/src/omero/plugins/download.py
@@ -13,7 +13,9 @@
 
 import sys
 import omero
+import re
 from omero.cli import BaseControl, CLI
+from omero.rtypes import unwrap
 
 HELP = """Download the given file id to the given filename"""
 
@@ -21,7 +23,9 @@ HELP = """Download the given file id to the given filename"""
 class DownloadControl(BaseControl):
 
     def _configure(self, parser):
-        parser.add_argument("id", help="OriginalFile id")
+        parser.add_argument(
+            "object", help="ID of the OriginalFile to download or object of"
+            " form <object>:<image_id>")
         parser.add_argument(
             "filename", help="Local filename to be saved to. '-' for stdout")
         parser.set_defaults(func=self.__call__)
@@ -30,9 +34,24 @@ class DownloadControl(BaseControl):
     def __call__(self, args):
         from omero_model_OriginalFileI import OriginalFileI as OFile
 
-        orig_file = OFile(long(args.id))
-        target_file = str(args.filename)
         client = self.ctx.conn(args)
+        if ':' in args.object:
+            image_id = self.parse_image_id(args.object)
+            query = client.sf.getQueryService()
+            params = omero.sys.ParametersI()
+            params.addLong('iid', image_id)
+            sql = "select f from Image i" \
+                " join i.fileset as fs" \
+                " join fs.usedFiles as uf" \
+                " join uf.originalFile as f" \
+                " where i.id = :iid"
+            query_out = query.projection(sql, params)
+            file_id = unwrap(query_out[0])[0].id.val
+        else:
+            file_id = self.parse_file_id(args.object)
+
+        orig_file = OFile(file_id)
+        target_file = str(args.filename)
 
         try:
             if target_file == "-":
@@ -44,6 +63,23 @@ class DownloadControl(BaseControl):
             # Possible, though unlikely after previous check
             self.ctx.die(67, "Unknown ValidationException: %s"
                          % ve.message)
+
+    def parse_file_id(self, value):
+
+        try:
+            return long(value)
+        except ValueError:
+            self.ctx.die(601, 'Invalid OriginalFile ID input')
+
+    def parse_image_id(self, value):
+
+        pattern = r'Image:(?P<id>\d+)'
+        pattern = re.compile('^' + pattern + '$')
+        m = pattern.match(value)
+        if not m:
+            self.ctx.die(601, 'Invalid object input')
+        return long(m.group('id'))
+
 
 try:
     register("download", DownloadControl, HELP)
