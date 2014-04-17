481c0bf20fd95df103a9df60275c03cd80ebb368
Chris Allan <callan@glencoesoftware.com>
2014-04-17 09:42:45 +0100

    Update tests for project-dataset hierarchy

--- components/tools/OmeroWeb/test/integration/test_integration_show.py
+++ components/tools/OmeroWeb/test/integration/test_integration_show.py
@@ -14,7 +14,7 @@ import omero.clients
 import pytest
 import test.integration.library as lib
 
-from omero.gateway import BlitzGateway, ProjectWrapper
+from omero.gateway import BlitzGateway, ProjectWrapper, DatasetWrapper
 from omero.model import ProjectI, DatasetI, ImageI
 from omero.rtypes import rstring
 from omeroweb.webclient.show import Show
@@ -36,6 +36,7 @@ def request_factory():
 def itest(request):
     o = lib.ITest()
     o.setup_method(None)
+
     def finalizer():
         o.teardown_method(None)
     request.addfinalizer(finalizer)
@@ -65,6 +66,16 @@ def project(request, itest, update_service):
 
 
 @pytest.fixture(scope='function')
+def project_dataset(request, itest, update_service):
+    project = ProjectI()
+    project.name = rstring(itest.uuid())
+    dataset = DatasetI()
+    dataset.name = rstring(itest.uuid())
+    project.linkDataset(dataset)
+    return update_service.saveAndReturnObject(project)
+
+
+@pytest.fixture(scope='function')
 def project_path_request(request, project, request_factory, path):
     """
     Returns a simple GET request object with the 'path' query string
@@ -74,7 +85,30 @@ def project_path_request(request, project, request_factory, path):
     initially_select = ['project-%d' % project.id.val]
     return {
         'request': request_factory.get(path, data={'path': as_string}),
-        'initially_select': initially_select
+        'initially_select': initially_select,
+        'initially_open': initially_select
+    }
+
+
+@pytest.fixture(scope='function')
+def project_dataset_path_request(
+        request, project_dataset, request_factory, path):
+    """
+    Returns a simple GET request object with the 'path' query string
+    variable set in the legacy ("project=id|dataset=1") form.
+    """
+    dataset, = project_dataset.linkedDatasetList()
+    as_string = 'project=%d|dataset=%d' % \
+        (project_dataset.id.val, dataset.id.val)
+    initially_select = ['dataset-%d' % dataset.id.val]
+    initially_open = [
+        'project-%d' % project_dataset.id.val,
+        'dataset-%d' % dataset.id.val
+    ]
+    return {
+        'request': request_factory.get(path, data={'path': as_string}),
+        'initially_select': initially_select,
+        'initially_open': initially_open
     }
 
 
@@ -100,18 +134,36 @@ class TestIntegrationShow(object):
     correctly.
     """
 
-    def test_project_legacy_path(self, conn, project_path_request, project):
-        show = Show(conn, project_path_request['request'], None)
+    def assert_instantiation(self, show, request, conn):
         assert show.conn == conn
         assert show.initially_open is None
         assert show.initially_open_owner is None
-        assert show.initially_select == project_path_request['initially_select']
+        assert show.initially_select == request['initially_select']
         assert show.first_sel is None
+
+    def test_project_legacy_path(self, conn, project_path_request, project):
+        show = Show(conn, project_path_request['request'], None)
+        self.assert_instantiation(show, project_path_request, conn)
 
         first_selected = show.get_first_selected()
         assert first_selected is not None
         assert isinstance(first_selected, ProjectWrapper)
         assert first_selected.getId() == project.id.val
-        assert show.initially_open == project_path_request['initially_select']
+        assert show.initially_open == project_path_request['initially_open']
         assert show.initially_open_owner == project.details.owner.id.val
+        assert show.first_sel is None
+
+    def test_project_dataset_legacy_path(
+            self, conn, project_dataset_path_request, project_dataset):
+        show = Show(conn, project_dataset_path_request['request'], None)
+        self.assert_instantiation(show, project_dataset_path_request, conn)
+
+        dataset, = project_dataset.linkedDatasetList()
+        first_selected = show.get_first_selected()
+        assert first_selected is not None
+        assert isinstance(first_selected, DatasetWrapper)
+        assert first_selected.getId() == dataset.id.val
+        assert show.initially_open == \
+            project_dataset_path_request['initially_open']
+        assert show.initially_open_owner == project_dataset.details.owner.id.val
         assert show.first_sel is None
