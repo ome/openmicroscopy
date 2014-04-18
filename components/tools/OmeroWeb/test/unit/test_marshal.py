8477523d34b34c75d54b77bcf039d4fc0992c28c
Chris Allan <callan@glencoesoftware.com>
2014-04-18 12:28:40 +0100

    Switch marshaling tests over to pytest

--- components/tools/OmeroWeb/test/unit/test_marshal.py
+++ components/tools/OmeroWeb/test/unit/test_marshal.py
@@ -18,14 +18,74 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import unittest
+import pytest
 import omero
 import omero.clients
 from omero.rtypes import rlong, rstring
 from omeroweb.webgateway.marshal import shapeMarshal
 
 
-class ShapeMarshalTest(unittest.TestCase):
+@pytest.fixture(scope='module')
+def default_id():
+    return TestShapeMarshal.DEFAULT_ID
+
+
+@pytest.fixture(scope='function', params=[
+    # OME-XML version of the points
+    '1,2 2,3 4,5',
+    # OMERO.insight version of the points
+    'points[1,2 2,3 4,5] points1[1,2 2,3 4,5] '
+    'points2[1,2 2,3 4,5] mask[0,0,0]'
+])
+def basic_polyline(request, default_id):
+    points = request.param
+    shape = omero.model.PolylineI()
+    shape.id = rlong(default_id)
+    shape.points = rstring(points)
+    return shape
+
+
+@pytest.fixture(scope='function', params=[
+    # OME-XML version of the points
+    '1.5,2.5 2,3 4.1,5.1',
+    # OMERO.insight version of the points
+    'points[1.5,2.5 2,3 4.1,5.1] points1[1.5,2.5 2,3 4.1,5.1] '
+    'points2[1.5,2.5 2,3 4.1,5.1] mask[0,0,0]'
+])
+@pytest.fixture(scope='function')
+def float_polyline(request, default_id):
+    points = request.param
+    shape = omero.model.PolylineI()
+    shape.id = rlong(default_id)
+    shape.points = rstring(points)
+    return shape
+
+
+@pytest.fixture(scope='function', params=[
+    # OME-XML version of the points
+    '1,2 2,3 4,5',
+    # OMERO.insight version of the points
+    'points[1,2 2,3 4,5] points1[1,2 2,3 4,5] '
+    'points2[1,2 2,3 4,5] mask[0,0,0]'
+])
+@pytest.fixture(scope='function')
+def basic_polygon(request, default_id):
+    points = request.param
+    shape = omero.model.PolygonI()
+    shape.id = rlong(default_id)
+    shape.points = rstring(points)
+    return shape
+
+
+@pytest.fixture(scope='function')
+def empty_polygon(default_id):
+    shape = omero.model.PolygonI()
+    shape.id = rlong(default_id)
+    shape.points = rstring('')
+    return shape
+
+
+class TestShapeMarshal(object):
     """
     Tests to ensure that OME-XML model and OMERO.insight shape point
     parsing are supported correctly.
@@ -33,75 +93,29 @@ class ShapeMarshalTest(unittest.TestCase):
 
     DEFAULT_ID = 1L
 
-    def testOmeXmlPolyLineMarshal(self):
-        shape = omero.model.PolylineI()
-        shape.id = rlong(self.DEFAULT_ID)
-        shape.points = rstring('1,2 2,3 4,5')
-        marshaled = shapeMarshal(shape)
-        self.assertEqual('PolyLine', marshaled['type'])
-        self.assertEqual(self.DEFAULT_ID, marshaled['id'])
-        self.assertEquals('M 1 2 L 2 3 L 4 5', marshaled['points'])
+    def assert_polyline(self, marshaled):
+        assert marshaled['type'] == 'PolyLine'
+        assert marshaled['id'] == self.DEFAULT_ID
 
-    def testOmeXmlPolyLineFloatMarshal(self):
-        shape = omero.model.PolylineI()
-        shape.id = rlong(self.DEFAULT_ID)
-        shape.points = rstring('1.5,2.5 2,3 4.1,5.1')
-        marshaled = shapeMarshal(shape)
-        self.assertEqual('PolyLine', marshaled['type'])
-        self.assertEqual(self.DEFAULT_ID, marshaled['id'])
-        self.assertEquals('M 1.5 2.5 L 2 3 L 4.1 5.1', marshaled['points'])
+    def assert_polygon(self, marshaled):
+        assert marshaled['type'] == 'Polygon'
+        assert marshaled['id'] == self.DEFAULT_ID
 
-    def testOmeXmlPolygonMarshal(self):
-        shape = omero.model.PolygonI()
-        shape.id = rlong(self.DEFAULT_ID)
-        shape.points = rstring('1,2 2,3 4,5')
-        marshaled = shapeMarshal(shape)
-        self.assertEqual('Polygon', marshaled['type'])
-        self.assertEqual(self.DEFAULT_ID, marshaled['id'])
-        self.assertEquals('M 1 2 L 2 3 L 4 5 z', marshaled['points'])
+    def test_ployline_marshal(self, basic_polyline):
+        marshaled = shapeMarshal(basic_polyline)
+        self.assert_polyline(marshaled)
+        assert 'M 1 2 L 2 3 L 4 5' == marshaled['points']
 
-    def testInsightPolyLineMarshal(self):
-        shape = omero.model.PolylineI()
-        shape.id = rlong(self.DEFAULT_ID)
-        shape.points = rstring(
-            'points[1,2 2,3 4,5] points1[1,2 2,3 4,5] '
-            'points2[1,2 2,3 4,5] mask[0,0,0]'
-        )
-        marshaled = shapeMarshal(shape)
-        self.assertEqual('PolyLine', marshaled['type'])
-        self.assertEqual(self.DEFAULT_ID, marshaled['id'])
-        self.assertEquals('M 1 2 L 2 3 L 4 5', marshaled['points'])
+    def test_polyline_float_marshal(self, float_polyline):
+        marshaled = shapeMarshal(float_polyline)
+        self.assert_polyline(marshaled)
+        assert 'M 1.5 2.5 L 2 3 L 4.1 5.1' == marshaled['points']
 
-    def testInsightPolyLineFloatMarshal(self):
-        shape = omero.model.PolylineI()
-        shape.id = rlong(self.DEFAULT_ID)
-        shape.points = rstring(
-            'points[1.5,2.5 2,3 4.1,5.1] points1[1.5,2.5 2,3 4.1,5.1] '
-            'points2[1.5,2.5 2,3 4.1,5.1] mask[0,0,0]'
-        )
-        marshaled = shapeMarshal(shape)
-        self.assertEqual('PolyLine', marshaled['type'])
-        self.assertEqual(self.DEFAULT_ID, marshaled['id'])
-        self.assertEquals('M 1.5 2.5 L 2 3 L 4.1 5.1',  marshaled['points'])
+    def test_polygon_marshal(self, basic_polygon):
+        marshaled = shapeMarshal(basic_polygon)
+        self.assert_polygon(marshaled)
+        assert 'M 1 2 L 2 3 L 4 5 z' == marshaled['points']
 
-    def testInsightPolygonMarshal(self):
-        shape = omero.model.PolygonI()
-        shape.id = rlong(self.DEFAULT_ID)
-        shape.points = rstring(
-            'points[1,2 2,3 4,5] points1[1,2 2,3 4,5] '
-            'points2[1,2 2,3 4,5] mask[0,0,0]'
-        )
-        marshaled = shapeMarshal(shape)
-        self.assertEqual('Polygon', marshaled['type'])
-        self.assertEqual(self.DEFAULT_ID, marshaled['id'])
-        self.assertEquals('M 1 2 L 2 3 L 4 5 z', marshaled['points'])
-
-    def testShapeUnrecognisedRoiShapePointsString(self):
-        shape = omero.model.PolygonI()
-        shape.id = rlong(self.DEFAULT_ID)
-        shape.points = rstring('')
-        marshaled = shapeMarshal(shape)
-        self.assertEquals(' z', marshaled['points'])
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_unrecognised_roi_shape_points_string(self, empty_polygon):
+        marshaled = shapeMarshal(empty_polygon)
+        assert ' z' == marshaled['points']
