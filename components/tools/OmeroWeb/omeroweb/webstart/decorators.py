00665001fe25c422c238cd27f978190158cb9016
Andreas Knab <knabar@gmail.com>
2014-04-18 11:16:40 +0200

    flake8 cleanup on omeroweb/webstart

--- components/tools/OmeroWeb/omeroweb/webstart/decorators.py
+++ components/tools/OmeroWeb/omeroweb/webstart/decorators.py
@@ -29,14 +29,12 @@ from functools import update_wrapper
 
 import omeroweb.decorators
 
-from django.http import HttpResponse
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
-from omeroweb.webclient.forms import GlobalSearchForm
-from omeroweb.connector import Connector
 
 logger = logging.getLogger(__name__)
+
 
 class login_required(omeroweb.webclient.decorators.login_required):
     """
@@ -70,16 +68,19 @@ class login_required(omeroweb.webclient.decorators.login_required):
                     logger.error('Error retrieving connection.', exc_info=True)
                     error = str(x)
                 else:
-                    # various configuration & checks only performed on new 'conn'
+                    # various configuration and checks only performed
+                    # on new 'conn'
                     if conn is not None:
                         ctx.on_logged_in(request, conn)
                         ctx.verify_is_admin(conn)
                         ctx.verify_is_group_owner(conn, kwargs.get('gid'))
 
                         share_id = kwargs.get('share_id')
-                        conn_share = ctx.prepare_share_connection(request, conn, share_id)
+                        conn_share = ctx.prepare_share_connection(
+                            request, conn, share_id)
                         if conn_share is not None:
-                            ctx.on_share_connection_prepared(request, conn_share)
+                            ctx.on_share_connection_prepared(
+                                request, conn_share)
                             kwargs['conn'] = conn_share
                         else:
                             kwargs['conn'] = conn
@@ -93,8 +94,8 @@ class login_required(omeroweb.webclient.decorators.login_required):
 
             retval = f(request, *args, **kwargs)
             try:
-                logger.debug('Doing connection cleanup? %s' % \
-                        doConnectionCleanup)
+                logger.debug(
+                    'Doing connection cleanup? %s' % doConnectionCleanup)
                 if doConnectionCleanup:
                     if conn is not None and conn.c is not None:
                         for v in conn._proxies.values():
@@ -105,21 +106,30 @@ class login_required(omeroweb.webclient.decorators.login_required):
             return retval
         return update_wrapper(wrapped, f)
 
+
 class render_response(omeroweb.webclient.decorators.render_response):
-    """ Subclass for adding additional data to the 'context' dict passed to templates """
+    """
+    Subclass for adding additional data to the 'context' dict passed to
+    templates
+    """
 
     def prepare_context(self, request, context, *args, **kwargs):
         """
-        This allows templates to access the current eventContext and user from the L{omero.gateway.BlitzGateway}.
+        This allows templates to access the current eventContext and user from
+        the L{omero.gateway.BlitzGateway}.
         E.g. <h1>{{ ome.user.getFullName }}</h1>
-        If these are not required by the template, then they will not need to be loaded by the Blitz Gateway.
-        The results are cached by Blitz Gateway, so repeated calls have no additional cost.
+        If these are not required by the template, then they will not need to
+        be loaded by the Blitz Gateway.
+        The results are cached by Blitz Gateway, so repeated calls have no
+        additional cost.
         We also process some values from settings and add these to the context.
         """
 
         # we expect @login_required to pass us 'conn', but just in case...
         if 'conn' not in kwargs:
             if settings.WEBSTART and not settings.WEBSTART_ADMINS_ONLY:
-                context['insight_url'] = request.build_absolute_uri(reverse("webstart_insight"))
+                context['insight_url'] = request.build_absolute_uri(
+                    reverse("webstart_insight"))
         else:
-            super(render_response, self).prepare_context(request, context, *args, **kwargs)
+            super(render_response, self).prepare_context(
+                request, context, *args, **kwargs)
