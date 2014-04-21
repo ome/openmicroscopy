9156a8aea47fad43727518ed33e6b155016ee9f1
Colin Blackburn <colin@ximenes.org.uk>
2014-04-21 17:04:12 +0100

    flake8: main integration tests - counts

--- components/tools/OmeroPy/test/integration/test_counts.py
+++ components/tools/OmeroPy/test/integration/test_counts.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#
+# Copyright (C) 2008-2014 Glencoe Software, Inc. All Rights Reserved.
+# Use is subject to license terms supplied in LICENSE.txt
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 """
    Integration test for accessing countsPerOwner.
-
-   Copyright 2008-2013 Glencoe Software, Inc. All rights reserved.
-   Use is subject to license terms supplied in LICENSE.txt
 
 """
 
 import test.integration.library as lib
-import omero
 from omero_model_ImageI import ImageI
 from omero_model_TagAnnotationI import TagAnnotationI
 from omero.rtypes import rstring, rtime
+
 
 class TestCounts(lib.ITest):
 
@@ -24,17 +39,16 @@ class TestCounts(lib.ITest):
         img.name = rstring("name")
         img.acquisitionDate = rtime(0)
         tag = TagAnnotationI()
-        img.linkAnnotation( tag )
+        img.linkAnnotation(tag)
 
-        img = self.client.sf.getUpdateService().saveAndReturnObject( img )
+        img = self.client.sf.getUpdateService().saveAndReturnObject(img)
 
         img = self.client.sf.getQueryService().findByQuery(
-        """
-        select img from Image img
-        join fetch img.annotationLinksCountPerOwner
-        where img.id = %s
-        """ % (img.id.val), None
-        )
+            """
+            select img from Image img
+            join fetch img.annotationLinksCountPerOwner
+            where img.id = %s
+            """ % (img.id.val), None
+            )
         assert img
         assert img.getAnnotationLinksCountPerOwner()[usr] > 0
-
