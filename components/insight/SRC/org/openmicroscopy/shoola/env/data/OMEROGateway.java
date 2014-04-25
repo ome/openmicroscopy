17f596d2b678be40c08b67ee970a6e426402e828
Dominik Lindner <d.lindner@dundee.ac.uk>
2014-04-25 09:48:06 +0100

    Moved findDatasetLinks from OMEROGateway to Gateway

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/OMEROGateway.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/OMEROGateway.java
@@ -2342,25 +2342,13 @@ class OMEROGateway
 	 */
         List findDatasetLinks(SecurityContext ctx, List children, long userID) throws DSOutOfServiceException,
                 DSAccessException {
-            Connector c = getConnector(ctx, true, false);
             try {
-                IQueryPrx service = c.getQueryService();
-                // have to fetch the Dataset, too; otherwise Dataset.name won't be initialized
-                String sql = "SELECT link FROM DatasetImageLink AS link LEFT JOIN FETCH link.parent dataset WHERE "
-                        + "link.child.id IN (:childIDs)";
-                ParametersI param = new ParametersI();
-                param.addLongs("childIDs", children);
-    
-                if (userID >= 0) {
-                    sql += " and link.details.owner.id = :userID";
-                    param.map.put("userID", omero.rtypes.rlong(userID));
-                }
-                return service.findAllByQuery(sql, param);
-            } catch (Throwable t) {
-                handleException(t, "Cannot retrieve the requested datasets for "
-                        + "the specified children");
+                return gateway.findDatasetLinks(ctx, children, userID);
+            } catch (omero.gateway.exception.DSOutOfServiceException e) {
+                throw new DSOutOfServiceException(e);
+            } catch (omero.gateway.exception.DSAccessException e) {
+                throw new DSAccessException(e);
             }
-            return new ArrayList();
         }
 
 
