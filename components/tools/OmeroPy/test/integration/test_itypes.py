d803df521571e7e936fae911320ec9447a8cb3d2
Colin Blackburn <colin@ximenes.org.uk>
2014-04-25 11:26:57 +0100

    flake8: main integration tests - itypes

--- components/tools/OmeroPy/test/integration/test_itypes.py
+++ components/tools/OmeroPy/test/integration/test_itypes.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-   Integration test focused on the omero.api.ITypes interface
-   a running server.
+#
+# Copyright (C) 2009-2014 Glencoe Software, Inc. All Rights Reserved.
+# Use is subject to license terms supplied in LICENSE.txt
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-   Copyright 2009-2013 Glencoe Software, Inc. All rights reserved.
-   Use is subject to license terms supplied in LICENSE.txt
+"""
+   Integration test focused on the omero.api.ITypes interface.
 
 """
-import time
+
 import test.integration.library as lib
-import omero
-from omero.rtypes import *
+from omero.rtypes import rstring
+
 
 class TestTypes(lib.ITest):
 
     # ticket:1436
     def testGetEnumerationTypes(self):
-        i = self.client.sf.getTypesService().getEnumerationTypes()
+        self.client.sf.getTypesService().getEnumerationTypes()
 
     def testAllEnumerations(self):
         types = self.root.sf.getTypesService()
@@ -27,28 +41,27 @@ class TestTypes(lib.ITest):
             if rv.get(e.__class__.__name__) is None:
                 rv[e.__class__.__name__] = list()
             rv[e.__class__.__name__].append(e)
-        
+
         for r in rv:
             types.allEnumerations(str(r))
 
     def testGetEnumerationWithEntries(self):
-        i = self.root.sf.getTypesService().getEnumerationsWithEntries().items()
-    
+        self.root.sf.getTypesService().getEnumerationsWithEntries().items()
+
     def testManageEnumeration(self):
         from omero_model_ExperimentTypeI import ExperimentTypeI
         uuid = self.root.sf.getAdminService().getEventContext().sessionUuid
         types = self.root.sf.getTypesService()
-        
+
         # create enums
         obj = ExperimentTypeI()
         obj.setValue(rstring("test_value_%s" % uuid))
         enum = types.createEnumeration(obj)
         types.deleteEnumeration(enum)
-        
+
         obj = ExperimentTypeI()
         obj.setValue(rstring("test_value2_%s" % (uuid)))
-        new_entries=[obj]
+        new_entries = [obj]
         types.updateEnumerations(new_entries)
-        
+
         types.resetEnumerations("ExperimentTypeI")
-        
