1e44e6eab1afaddedadf8eadffa75ad20f0a128f
Colin Blackburn <colin@ximenes.org.uk>
2014-04-25 11:41:23 +0100

    flake8: main integration tests - model42

--- components/tools/OmeroPy/test/integration/test_model42.py
+++ components/tools/OmeroPy/test/integration/test_model42.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#
+# Copyright (C) 2010-2014 Glencoe Software, Inc. All Rights Reserved.
+# Use is subject to license terms supplied in LICENSE.txt
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 """
    Integration test focused on the model changes in 4.2
-
-   Copyright 2010-2013 Glencoe Software, Inc. All rights reserved.
-   Use is subject to license terms supplied in LICENSE.txt
 
 """
 
 import test.integration.library as lib
-from omero.rtypes import *
+import omero
+from omero.rtypes import rstring
 
 
 class TestModel42(lib.ITest):
@@ -82,4 +98,3 @@ class TestTicket2290(lib.ITest):
         rect = omero.model.RectI()
         roi.addShape(rect)
         roi = self.update.saveAndReturnObject(roi)
-
