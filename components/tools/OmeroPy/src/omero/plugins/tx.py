024b20da5b8411d646339603f916239b2f26fae3
jmoore <josh@glencoesoftware.com>
2014-04-30 21:04:03 +0200

    Initial skeleton for tx command

---
+++ components/tools/OmeroPy/src/omero/plugins/tx.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2013 Glencoe Software, Inc. All Rights Reserved.
+# Use is subject to license terms supplied in LICENSE.txt
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
+Plugin for performing OMERO transactional changes (DML)
+from a simple script. This macro like language is intended
+to be used simply from the command-line as well as in scripts
+and tests.
+"""
+
+
+import sys
+import shlex
+import fileinput
+
+from omero.cli import BaseControl, CLI, ExceptionHandler
+
+
+class TxAction(object):
+    """
+    Parsed transaction operation provided by the user.
+    In the current implementation these actions are
+    handled client-side, but eventually a list of them
+    should be provided to the server for processing in
+    a single transaction.
+    """
+
+    def __init__(self, tx_state, arg_list):
+        self.order = tx_state.next()
+        self.arg_list = arg_list
+
+    def go(self, ctx, args):
+        raise Exception("Unimplemented")
+
+
+class NewObjectTxAction(TxAction):
+
+    def class_name(self):
+        return "%sI" % self.arg_list[0]
+
+    def instance(self, ctx):
+        import omero
+        import omero.all
+        try:
+            kls = getattr(omero.model, self.class_name())
+        except AttributeError:
+            ctx.die(102, "No class named '%s'" % self.class_name())
+
+    def go(self, ctx, args):
+        obj = self.instance(ctx)
+        for arg in self.arg_list[1:]:
+            parts = arg.split("=", 1)
+            argname = parts[0]
+            value = parts[1]
+            setattr(obj, argname, value)
+            print obj
+
+
+class TxState(object):
+
+    def __init__(self):
+        self.count = 0
+        self.is_stdin = sys.stdin.isatty()
+
+    def next(self):
+        self.count += 1
+        return self.count
+
+
+class TxControl(BaseControl):
+    """
+    Transactional modification command
+    """
+
+    def _configure(self, parser):
+
+        self.exc = ExceptionHandler()
+        parser.add_login_arguments()
+        parser.add_argument(
+            "item",
+            nargs="*",
+        )
+        parser.set_defaults(func=self.process)
+
+    def process(self, args):
+        state = TxState()
+        actions = []
+        if len(args.item) == 0:
+            for line in fileinput.input(["-"]):
+                line = line.strip()
+                if line and not line.startswith("#"):
+                    actions.append(self.parse(state, shlex.split(line)))
+        elif len(args.item) == 1:
+            raise Exception("likely stdin '-'")
+        else:
+            actions.append(self.parse(state, args.item))
+
+        for action in actions:
+            action.go(self.ctx, args)
+        return actions
+
+    def parse(self, tx_state, arg_list):
+        """
+        Takes a single command list and turns
+        it into a TxAction object
+        """
+        if arg_list[0] == "new":
+            return NewObjectTxAction(tx_state, arg_list[1:])
+        else:
+            raise self.ctx.die(100, "Unknown command: " + arg_list[0])
+
+
+try:
+    register("tx", TxControl, TxControl.__doc__)
+except NameError:
+    if __name__ == "__main__":
+        cli = CLI()
+        cli.register("tx", TxControl, TxControl.__doc__)
+        cli.invoke(sys.argv[1:])
