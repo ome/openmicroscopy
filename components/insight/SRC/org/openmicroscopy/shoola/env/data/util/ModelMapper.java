dbd158625548043dd410d4fb1dd5696c8ce7fa75
Jean-Marie Burel <j.burel@dundee.ac.uk>
2014-05-05 15:03:23 +0100

    Fix import.

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/util/ModelMapper.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/util/ModelMapper.java
@@ -2,10 +2,10 @@
  * org.openmicroscopy.shoola.env.data.util.ModelMapper
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2013 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2014 University of Dundee. All rights reserved.
  *
  *
- * 	This program is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -32,9 +32,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.hibernate.tool.hbm2x.StringUtils;
+
 
 //Third-party libraries
+import org.apache.commons.lang.StringUtils;
 
 
 //Application-internal dependencies
