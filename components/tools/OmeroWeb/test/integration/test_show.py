a8d04e3e8bac3c038160d7dc1bf413abfa46db75
Chris Allan <callan@glencoesoftware.com>
2014-05-05 10:28:03 +0100

    Add support for using other keys for "show" functionality
    This commit adds support for using other keys for "show" functionality
    by extending the "path" and "show" query string to encompass not only
    identifier matching but also key-value matching.  The parsing of the
    query string variables is now done by regex.  An example URL utilising
    this functionality might look something like:
    
        * /webclient/?show=project.name-foo
        * /webclient/?show=tag.textValue-bar
    
    Functionality of this form is extremely useful when creating and sharing
    URLs from external applications where OMERO unique identifiers are not
    known or where specific tags are to have URLs created for them.
    
    Tests have been added to cover the regex semantics and the new
    functionality.  As a result of the new regular expression based query
    string variable parsing some of the internal strings are now potentially
    of the equals based form.

--- components/tools/OmeroWeb/test/integration/test_show.py
+++ components/tools/OmeroWeb/test/integration/test_show.py
@@ -221,7 +221,7 @@ def project_path_request(request, project, request_factory, path):
     variable set in the legacy ("project=id") form.
     """
     as_string = 'project=%d' % project.id.val
-    initially_select = ['project-%d' % project.id.val]
+    initially_select = ['project=%d' % project.id.val]
     return {
         'request': request_factory.get(path, data={'path': as_string}),
         'initially_select': initially_select,
@@ -239,10 +239,10 @@ def project_dataset_path_request(
     dataset, = project_dataset.linkedDatasetList()
     as_string = 'project=%d|dataset=%d' % \
         (project_dataset.id.val, dataset.id.val)
-    initially_select = ['dataset-%d' % dataset.id.val]
+    initially_select = ['dataset=%d' % dataset.id.val]
     initially_open = [
         'project-%d' % project_dataset.id.val,
-        'dataset-%d' % dataset.id.val
+        'dataset=%d' % dataset.id.val
     ]
     return {
         'request': request_factory.get(path, data={'path': as_string}),
@@ -262,11 +262,11 @@ def project_dataset_image_path_request(
     image, = dataset.linkedImageList()
     as_string = 'project=%d|dataset=%d|image=%d' % \
         (project_dataset_image.id.val, dataset.id.val, image.id.val)
-    initially_select = ['image-%d' % image.id.val]
+    initially_select = ['image=%d' % image.id.val]
     initially_open = [
         'project-%d' % project_dataset_image.id.val,
         'dataset-%d' % dataset.id.val,
-        'image-%d' % image.id.val
+        'image=%d' % image.id.val
     ]
     return {
         'request': request_factory.get(path, data={'path': as_string}),
@@ -282,8 +282,8 @@ def tag_path_request(request, tag, request_factory, path):
     variable set in the legacy ("tag=id") form.
     """
     as_string = 'tag=%d' % tag.id.val
-    initially_select = ['tag-%d' % tag.id.val]
-    initially_open = ['tag-%d' % tag.id.val]
+    initially_select = ['tag=%d' % tag.id.val]
+    initially_open = ['tag=%d' % tag.id.val]
     return {
         'request': request_factory.get(path, data={'path': as_string}),
         'initially_select': initially_select,
@@ -299,10 +299,10 @@ def tagset_tag_path_request(request, tagset_tag, request_factory, path):
     """
     tag, = tagset_tag.linkedAnnotationList()
     as_string = 'tag=%d|tag=%d' % (tagset_tag.id.val, tag.id.val)
-    initially_select = ['tag-%d' % tag.id.val]
+    initially_select = ['tag=%d' % tag.id.val]
     initially_open = [
         'tag-%d' % tagset_tag.id.val,
-        'tag-%d' % tag.id.val
+        'tag=%d' % tag.id.val
     ]
     return {
         'request': request_factory.get(path, data={'path': as_string}),
@@ -319,8 +319,8 @@ def image_path_request(request, image, request_factory, path):
     'orphaned-0' container.
     """
     as_string = 'image=%d' % image.id.val
-    initially_select = ['image-%d' % image.id.val]
-    initially_open = ['orphaned-0', 'image-%d' % image.id.val]
+    initially_select = ['image=%d' % image.id.val]
+    initially_open = ['orphaned-0', 'image=%d' % image.id.val]
     return {
         'request': request_factory.get(path, data={'path': as_string}),
         'initially_select': initially_select,
@@ -335,7 +335,7 @@ def screen_path_request(request, screen, request_factory, path):
     variable set in the legacy ("screen=id") form.
     """
     as_string = 'screen=%d' % screen.id.val
-    initially_select = ['screen-%d' % screen.id.val]
+    initially_select = ['screen=%d' % screen.id.val]
     return {
         'request': request_factory.get(path, data={'path': as_string}),
         'initially_select': initially_select,
@@ -351,10 +351,10 @@ def screen_plate_path_request(request, screen_plate, request_factory, path):
     """
     plate, = screen_plate.linkedPlateList()
     as_string = 'screen=%d|plate=%d' % (screen_plate.id.val, plate.id.val)
-    initially_select = ['plate-%d' % plate.id.val]
+    initially_select = ['plate=%d' % plate.id.val]
     initially_open = [
         'screen-%d' % screen_plate.id.val,
-        'plate-%d' % plate.id.val
+        'plate=%d' % plate.id.val
     ]
     return {
         'request': request_factory.get(path, data={'path': as_string}),
@@ -430,6 +430,51 @@ def project_dataset_image_show_request(
         'request': request_factory.get(path, data={'show': as_string}),
         'initially_select': initially_select,
         'initially_open': initially_open
+    }
+
+
+@pytest.fixture(scope='function')
+def project_by_id_path_request(request, project, request_factory, path):
+    """
+    Returns a simple GET request object with the 'path' query string
+    variable set in the key / value ("project.key=value") form.
+    """
+    as_string = 'project.id=%d' % project.id.val
+    initially_select = ['project.id=%d' % project.id.val]
+    return {
+        'request': request_factory.get(path, data={'path': as_string}),
+        'initially_select': initially_select,
+        'initially_open': initially_select
+    }
+
+
+@pytest.fixture(scope='function')
+def project_by_name_path_request(request, project, request_factory, path):
+    """
+    Returns a simple GET request object with the 'path' query string
+    variable set in the key / value ("project.key=value") form.
+    """
+    as_string = 'project.name=%s' % project.name.val
+    initially_select = ['project.name=%s' % project.name.val]
+    return {
+        'request': request_factory.get(path, data={'path': as_string}),
+        'initially_select': initially_select,
+        'initially_open': initially_select
+    }
+
+
+@pytest.fixture(scope='function')
+def tag_by_textvalue_path_request(request, tag, request_factory, path):
+    """
+    Returns a simple GET request object with the 'path' query string
+    variable set in the key / value ("tag.key=value") form.
+    """
+    as_string = 'tag.textValue=%s' % tag.textValue.val
+    initially_select = ['tag.textValue=%s' % tag.textValue.val]
+    return {
+        'request': request_factory.get(path, data={'path': as_string}),
+        'initially_select': initially_select,
+        'initially_open': initially_select
     }
 
 
@@ -667,3 +712,50 @@ class TestShow(object):
         assert show._first_selected == first_selected
         assert show.initially_select == \
             project_dataset_image_show_request['initially_select']
+
+    def test_project_by_id(
+            self, conn, project_by_id_path_request, project):
+        show = Show(conn, project_by_id_path_request['request'], None)
+        self.assert_instantiation(show, project_by_id_path_request, conn)
+
+        first_selected = show.first_selected
+        assert first_selected is not None
+        assert isinstance(first_selected, ProjectWrapper)
+        assert first_selected.getId() == project.id.val
+        assert show.initially_open == \
+            project_by_id_path_request['initially_open']
+        assert show.initially_open_owner == project.details.owner.id.val
+        assert show._first_selected == first_selected
+        assert show.initially_select == \
+            project_by_id_path_request['initially_select']
+
+    def test_project_by_name(
+            self, conn, project_by_name_path_request, project):
+        show = Show(conn, project_by_name_path_request['request'], None)
+        self.assert_instantiation(show, project_by_name_path_request, conn)
+
+        first_selected = show.first_selected
+        assert first_selected is not None
+        assert isinstance(first_selected, ProjectWrapper)
+        assert first_selected.getId() == project.id.val
+        assert show.initially_open == \
+            project_by_name_path_request['initially_open']
+        assert show.initially_open_owner == project.details.owner.id.val
+        assert show._first_selected == first_selected
+        assert show.initially_select == \
+            project_by_name_path_request['initially_select']
+
+    def test_tag_by_value(self, conn, tag_by_textvalue_path_request, tag):
+        show = Show(conn, tag_by_textvalue_path_request['request'], 'usertags')
+        self.assert_instantiation(show, tag_by_textvalue_path_request, conn)
+
+        first_selected = show.first_selected
+        assert first_selected is not None
+        assert isinstance(first_selected, TagAnnotationWrapper)
+        assert first_selected.getId() == tag.id.val
+        assert show.initially_open == \
+            tag_by_textvalue_path_request['initially_open']
+        assert show.initially_open_owner == \
+            tag.details.owner.id.val
+        assert show._first_selected == first_selected
+        assert show.initially_select == tag_by_textvalue_path_request['initially_select']
