dfad7a028ef1c1f8b4e7998c5942bdec020bdfe7
Chris Allan <callan@glencoesoftware.com>
2014-05-08 12:10:26 +0100

    Refactor PlateAcquisition marshaling and add unit tests

---
+++ components/tools/OmeroWeb/test/unit/test_tree.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014 Glencoe Software, Inc.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Simple unit tests for the "tree" module.
+"""
+
+import pytest
+
+from omero.rtypes import rlong, rstring, rtime
+from omeroweb.webclient.tree import marshal_plate_acquisition
+
+
+class MockConnection(object):
+
+    def getUserId(self):
+        return 1L
+
+
+@pytest.fixture(scope='module')
+def mock_conn():
+    return MockConnection()
+
+
+@pytest.fixture(scope='module')
+def owner_permissions():
+    return {
+        'canEdit': True,
+        'canAnnotate': True,
+        'canLink': True,
+        'canDelete': True
+    }
+
+
+@pytest.fixture(scope='module')
+def start_time():
+    # 2014-05-08 10:37:02 UTC; server timestamps contain ms
+    return rtime(1399545422L * 1000)
+
+
+@pytest.fixture(scope='module')
+def end_time():
+    # 2014-05-08 10:38:30 UTC; server timestamps contain ms
+    return rtime(1399545510L * 1000)
+
+
+class TestTree(object):
+    """
+    Tests to ensure that OMERO.web "tree" infrastructure is working
+    correctly.  Order and type of columns in row is:
+      * id (rlong)
+      * name (rstring)
+      * details.owner.id (rlong)
+      * details.permissions (dict)
+      * startTime (rtime)
+      * endTime (rtime)
+    """
+
+    def test_marshal_plate_acquisition_no_name_no_start_no_end(
+            self, mock_conn, owner_permissions):
+        row = [
+            rlong(1L),
+            None,
+            rlong(1L),
+            owner_permissions,
+            None,
+            None
+        ]
+        expected = {
+            'id': 1L,
+            'isOwned': True,
+            'name': 'Run 1',
+            'permsCss': 'canEdit canAnnotate canLink canDelete canChgrp'
+        }
+
+        marshaled = marshal_plate_acquisition(mock_conn, row)
+        assert marshaled == expected
+
+    def test_marshal_plate_acquisition_name_no_start_no_end(
+            self, mock_conn, owner_permissions):
+        row = [
+            rlong(1L),
+            rstring('name'),
+            rlong(1L),
+            owner_permissions,
+            None,
+            None
+        ]
+        expected = {
+            'id': 1L,
+            'isOwned': True,
+            'name': 'name',
+            'permsCss': 'canEdit canAnnotate canLink canDelete canChgrp'
+        }
+
+        marshaled = marshal_plate_acquisition(mock_conn, row)
+        assert marshaled == expected
+
+    def test_marshal_plate_acquisition_no_name_start_end(
+            self, mock_conn, owner_permissions, start_time, end_time):
+        row = [
+            rlong(1L),
+            None,
+            rlong(1L),
+            owner_permissions,
+            start_time,
+            end_time
+        ]
+        expected = {
+            'id': 1L,
+            'isOwned': True,
+            'name': '2014-05-08 11:37:02 - 2014-05-08 11:38:30',
+            'permsCss': 'canEdit canAnnotate canLink canDelete canChgrp'
+        }
+
+        marshaled = marshal_plate_acquisition(mock_conn, row)
+        assert marshaled == expected
+
+    def test_marshal_plate_acquisition_not_owner(
+            self, mock_conn, owner_permissions):
+        row = [
+            rlong(1L),
+            None,
+            rlong(2L),
+            owner_permissions,
+            None,
+            None
+        ]
+        expected = {
+            'id': 1L,
+            'isOwned': False,
+            'name': 'Run 1',
+            'permsCss': 'canEdit canAnnotate canLink canDelete'
+        }
+
+        marshaled = marshal_plate_acquisition(mock_conn, row)
+        assert marshaled == expected
