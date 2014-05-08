b5d9359ff8225e8cb50c1cf9673db03fc84cc6db
Chris Allan <callan@glencoesoftware.com>
2014-05-08 10:10:47 +0100

    Initial integration tests

---
+++ components/tools/OmeroWeb/test/integration/test_tree.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014 Glencoe Software, Inc.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Simple integration tests for the "tree" module.
+"""
+
+import pytest
+import test.integration.library as lib
+
+from omero.gateway import BlitzGateway
+from omero.model import ProjectI, DatasetI
+from omero.rtypes import rstring
+from omeroweb.webclient.tree import marshal_datasets_for_projects
+
+
+def cmp_id(x, y):
+    """Identifier comparator."""
+    return cmp(x.id.val, y.id.val)
+
+
+def cmp_name(x, y):
+    """Name comparator."""
+    return cmp(x.name.val, y.name.val)
+
+
+@pytest.fixture(scope='function')
+def itest(request):
+    """
+    Returns a new L{test.integration.library.ITest} instance.  With
+    attached finalizer so that pytest will clean it up.
+    """
+    o = lib.ITest()
+    o.setup_method(None)
+
+    def finalizer():
+        o.teardown_method(None)
+    request.addfinalizer(finalizer)
+    return o
+
+
+@pytest.fixture(scope='function')
+def client(request, itest):
+    """Returns a new user client."""
+    return itest.new_client()
+
+
+@pytest.fixture(scope='function')
+def conn(request, client):
+    """Returns a new OMERO gateway."""
+    return BlitzGateway(client_obj=client)
+
+
+@pytest.fixture(scope='function')
+def update_service(request, client):
+    """Returns a new OMERO update service."""
+    return client.getSession().getUpdateService()
+
+
+@pytest.fixture(scope='function')
+def project_dataset(request, itest, update_service):
+    """
+    Returns a new OMERO Project and linked Dataset with required fields set.
+    """
+    project = ProjectI()
+    project.name = rstring(itest.uuid())
+    dataset = DatasetI()
+    dataset.name = rstring(itest.uuid())
+    project.linkDataset(dataset)
+    return update_service.saveAndReturnObject(project)
+
+
+@pytest.fixture(scope='function')
+def projects_datasets(request, itest, update_service):
+    """
+    Returns a two new OMERO Projects and two linked Datasets for each Project
+    with required fields set.
+    """
+    projects = [ProjectI(), ProjectI()]
+    for project in projects:
+        project.name = rstring(itest.uuid())
+        datasets = [DatasetI(), DatasetI()]
+        for dataset in datasets:
+            dataset.name = rstring(itest.uuid())
+            project.linkDataset(dataset)
+    return update_service.saveAndReturnArray(projects)
+
+
+@pytest.fixture(scope='function')
+def project_dataset_image(request, itest, update_service):
+    """
+    Returns a new OMERO Project, linked Dataset and linked Image populated
+    by an L{test.integration.library.ITest} instance with required fields
+    set.
+    """
+    project = ProjectI()
+    project.name = rstring(itest.uuid())
+    dataset = DatasetI()
+    dataset.name = rstring(itest.uuid())
+    image = itest.new_image(name=itest.uuid())
+    dataset.linkImage(image)
+    project.linkDataset(dataset)
+    return update_service.saveAndReturnObject(project)
+
+
+class TestTree(object):
+    """
+    Tests to ensure that OMERO.web "tree" infrastructure is working
+    correctly.
+
+    These tests make __extensive__ use of pytest fixtures.  In particular
+    the scoping semantics allowing re-use of instances populated by the
+    *request fixtures.  It is recommended that the pytest fixture
+    documentation be studied in detail before modifications or attempts to
+    fix failing tests are made:
+
+     * https://pytest.org/latest/fixture.html
+    """
+
+    def test_marshal_project_dataset(self, conn, project_dataset):
+        project_id = project_dataset.id.val
+        dataset, = project_dataset.linkedDatasetList()
+        expected = {
+            project_id: {
+                'childCount': 1L,
+                'datasets': [{
+                    'childCount': 0L,
+                    'id': dataset.id.val,
+                    'isOwned': True,
+                    'name': dataset.name.val
+                }],
+                'permsCss': 'canEdit canAnnotate canLink canDelete canChgrp'
+            }
+        }
+
+        marshaled = marshal_datasets_for_projects(conn, [project_id])
+        assert marshaled == expected
+
+    def test_marshal_projects_datasets(self, conn, projects_datasets):
+        project_a, project_b = projects_datasets
+        expected = dict()
+        for project in (project_a, project_b):
+            datasets = list()
+            expected[project.id.val] = {
+                'childCount': 2L,
+                'datasets': datasets,
+                'permsCss': 'canEdit canAnnotate canLink canDelete canChgrp'
+            }
+            # The underlying query explicitly orders the Datasets list by
+            # name.
+            for dataset in sorted(project.linkedDatasetList(), cmp_name):
+                datasets.append({
+                    'childCount': 0L,
+                    'id': dataset.id.val,
+                    'isOwned': True,
+                    'name': dataset.name.val
+                })
+
+        marshaled = marshal_datasets_for_projects(
+            conn, [project_a.id.val, project_b.id.val]
+        )
+        assert marshaled == expected
