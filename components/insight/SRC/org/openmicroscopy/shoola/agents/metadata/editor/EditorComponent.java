f5d9e09b3bd8cd36e7661c33f4248bbddfd4b20d
Dominik Lindner <d.lindner@dundee.ac.uk>
2014-05-20 15:38:07 +0100

    Bugfix: Remove FileAnnotations when multiple images are selected

--- components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/editor/EditorComponent.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/editor/EditorComponent.java
@@ -506,7 +506,7 @@ class EditorComponent
     }
 
     public void handleFileAnnotationRemoveCheck(final FileAnnotationCheckResult result) {
-        if (!result.getSingleParentAnnotations().isEmpty()) {
+        if (!result.getDeleteCandidates().isEmpty()) {
             
             JFrame f = MetadataViewerAgent.getRegistry().getTaskBar()
                     .getFrame();
@@ -516,8 +516,8 @@ class EditorComponent
                 
                 @Override
                 public void propertyChange(PropertyChangeEvent arg0) {
-                    if(arg0.getPropertyName().equals(FileAttachmentWarningDialog.DELETE_PROPERTY)) {
-                        for (FileAnnotationData fd : result.getSingleParentAnnotations()) {
+                    if (arg0.getPropertyName().equals(FileAttachmentWarningDialog.DELETE_PROPERTY)) {
+                        for (FileAnnotationData fd : result.getDeleteCandidates()) {
                           view.deleteAnnotation(fd);
                         }
                         for (FileAnnotationData fd : result.getAllAnnotations()) {
