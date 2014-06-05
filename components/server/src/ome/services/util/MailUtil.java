1f7cd40b0100852cc123077acd5cfecb24877f4f
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2014-06-05 15:58:22 +0100

    adding comments to MailUtil.java

--- components/server/src/ome/services/util/MailUtil.java
+++ components/server/src/ome/services/util/MailUtil.java
@@ -18,8 +18,12 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 
 /**
- * Mail util
- *
+ * Static methods for dealing with the preparation of JavaMail MIME messages.
+ * The corresponding send methods of JavaMailSender will take care of the actual
+ * creation of a MimeMessage instance. Used primarily by asynchronous Ice
+ * services: {@link omero.cmd.mail.SendEmailRequestI} and
+ * {@link omero.cmd.mail.ResetPasswordRequestI}
+ * 
  * @author Aleksandra Tarkowska, A (dot) Tarkowska at dundee.ac.uk
  * @since 5.1.0
  */
@@ -38,10 +42,32 @@ public class MailUtil {
 		this.mailSender = mailSender;
 	}
 	
+	/**
+	 * Helper method that returns value of {@link omero.mail.from}.
+	 */
 	public String getSender() {
 		return sender;
 	}
 
+	/**
+	 * Main method which takes typical email fields as an arguments, to prepare
+	 * and populate the given new MimeMessage instance and send.
+	 * 
+	 * @param from
+	 *            email address message is send from
+	 * @param to
+	 *            email address message is send to
+	 * @param topic
+	 *            topic of the message
+	 * @param body
+	 *            body of the message
+	 * @param html
+	 *            flag determines the content type to apply.
+	 * @param ccrecipients
+	 *            list of email addresses message is send as copy to
+	 * @param bccrecipients
+	 *            list of email addresses message is send as blind copy to
+	 */
 	public void sendEmail(final String from, final String to,
 			final String topic, final String body, final boolean html,
 			final ArrayList<String> ccrecipients, final ArrayList<String> bccrecipients) {
