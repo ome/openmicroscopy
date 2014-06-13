12f54121c784f23f2c90877400f35fd136259a8f
Sebastien Besson <seb.besson@gmail.com>
2014-06-13 10:22:01 +0100

    Do not inherit Sphinx extensions from common/conf.py

--- docs/sphinx-api/conf.py
+++ docs/sphinx-api/conf.py
@@ -28,7 +28,7 @@ from conf import *
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions += ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath']
 
 # General information about the project.
 project = u'OMERO.py API'
