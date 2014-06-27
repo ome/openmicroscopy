c11fcae6c76de970d59229b990e48492d42e176d
Will Moore <w.moore@dundee.ac.uk>
2014-06-27 14:48:59 +0100

    Nicer error message for 'Too many terms'

--- components/tools/OmeroWeb/omeroweb/webclient/controller/search.py
+++ components/tools/OmeroWeb/omeroweb/webclient/controller/search.py
@@ -115,7 +115,8 @@ class BaseSearch(BaseController):
         except Exception, x:
             msg = x.message
             if "TooManyClauses" in msg:
-                msg = "The wildcard matched too many terms"
-            self.searchError = "Your query for '%s' caused an error: %s." % (query, msg)
+                self.searchError = "Please try to narrow down your query. The wildcard matched too many terms."
+            else:
+                self.searchError = "Your query for '%s' caused an error: %s." % (query, msg)
 
         self.c_size = resultCount
