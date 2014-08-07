8f177d95e207730b7857af016c0aee8a2d771890
Will Moore <w.moore@dundee.ac.uk>
2014-08-07 16:01:16 +0100

    Update rdef thumbs when rdef saved

--- components/tools/OmeroWeb/omeroweb/webclient/views.py
+++ components/tools/OmeroWeb/omeroweb/webclient/views.py
@@ -853,6 +853,7 @@ def load_metadata_preview(request, c_type, c_id, conn=None, share_id=None, **kwa
             chs.append('%s%s|%d:%d$%s' % (act, i+1, c['start'], c['end'], c['color']))
         rdefQueries.append({
             'id': r['id'],
+            'owner': r['owner'],
             'c': ",".join(chs),
             'm': r['model'] == 'greyscale' and 'g' or 'c'
             })
