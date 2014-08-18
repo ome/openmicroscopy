ae7192ec6cc06e0df9a58007a06cd4f6df646481
Colin Blackburn <colin@ximenes.org.uk>
2014-08-18 17:50:36 +0100

    flake8: main integration tests - librarytest

--- components/tools/OmeroPy/test/integration/test_librarytest.py
+++ components/tools/OmeroPy/test/integration/test_librarytest.py
@@ -24,13 +24,12 @@
 """
 
 import test.integration.library as lib
-import omero
+
 
 class TestLibrary(lib.ITest):
 
-   def test9188(self):
-        self.createTestImage(10,10,1,1,1)
-        self.createTestImage(10,10,10,1,1)
-        self.createTestImage(10,10,1,10,1)
-        self.createTestImage(10,10,1,1,10)
-   
+    def test9188(self):
+        self.createTestImage(10, 10, 1, 1, 1)
+        self.createTestImage(10, 10, 10, 1, 1)
+        self.createTestImage(10, 10, 1, 10, 1)
+        self.createTestImage(10, 10, 1, 1, 10)
