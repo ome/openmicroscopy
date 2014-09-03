51fdceab0648c54b67b7bfe3403058a4cc417493
Kenny Gillen <k.h.gillen@dundee.ac.uk>
2014-09-03 11:56:17 +0100

    Update webadmin_utils.py
    http://trac.openmicroscopy.org.uk/ome/wiki/MilestoneDownloads has a big red sign stating "deprecated page", with a link to donwloads.o.org. Might as well direct users to downloads.o.org from the application in the first place.

--- components/tools/OmeroWeb/omeroweb/webadmin/webadmin_utils.py
+++ components/tools/OmeroWeb/omeroweb/webadmin/webadmin_utils.py
@@ -28,7 +28,7 @@ def upgradeCheck():
         check = UpgradeCheck("web", url=settings.UPGRADES_URL)
         check.run()
         if check.isUpgradeNeeded():
-            logger.error("Upgrade is available. Please visit http://trac.openmicroscopy.org.uk/ome/wiki/MilestoneDownloads.\n")
+            logger.error("Upgrade is available. Please visit http://www.openmicroscopy.org/site/products/omero/downloads.\n")
         else:
             logger.debug("Up to date.\n")
     except Exception, x:
