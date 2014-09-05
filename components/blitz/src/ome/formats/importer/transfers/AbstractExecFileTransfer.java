2f440843ccdb75bfe4ed6075608642bc09d70307
Blazej Pindelski <bpindelski@dundee.ac.uk>
2014-09-05 12:28:29 +0100

    Revert "Fix ONEE during in-place import."
    This reverts commit fb58e1159fda3e013593c3d6f0288b0794742509.
    
    Conflicts:
    	components/blitz/src/ome/formats/importer/transfers/AbstractExecFileTransfer.java

--- components/blitz/src/ome/formats/importer/transfers/AbstractExecFileTransfer.java
+++ components/blitz/src/ome/formats/importer/transfers/AbstractExecFileTransfer.java
@@ -45,7 +45,16 @@ public abstract class AbstractExecFileTransfer extends AbstractFileTransfer {
 
     private static final String SEPARATOR = System.getProperty("line.separator");
 
-    private RawFileStorePrx rawFileStore;
+    private static final boolean ACTIVE_CLOSE;
+
+    static {
+        String ac = System.getProperty("omero.import.active_close", "true");
+        ACTIVE_CLOSE = Boolean.parseBoolean(ac);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ACTIVE_CLOSE);
+    }
 
     /**
      * "Transfer" files by soft-linking them into place. This method is likely
@@ -53,7 +62,7 @@ public abstract class AbstractExecFileTransfer extends AbstractFileTransfer {
      * {@link #createProcessBuilder(File, File)} and the other protected methods here.
      */
     public String transfer(TransferState state) throws IOException, ServerError {
-        rawFileStore = start(state);
+        RawFileStorePrx rawFileStore = start(state);
         try {
             final OriginalFile root = state.getRootFile();
             final OriginalFile ofile = state.getOriginalFile();
@@ -62,7 +71,7 @@ public abstract class AbstractExecFileTransfer extends AbstractFileTransfer {
             final long length = state.getLength();
             final ChecksumProvider cp = state.getChecksumProvider();
             state.uploadStarted();
-            checkLocation(location);
+            checkLocation(location, rawFileStore);
             exec(file, location);
             cp.putFile(file.getAbsolutePath());
             state.stop(length);
@@ -98,10 +107,11 @@ public abstract class AbstractExecFileTransfer extends AbstractFileTransfer {
      * place.
      *
      * @param location
+     * @param rawFileStore
      * @throws ServerError
      * @throws IOException
      */
-    protected void checkLocation(File location)
+    protected void checkLocation(File location, RawFileStorePrx rawFileStore)
             throws ServerError, IOException {
 
         final String uuid = UUID.randomUUID().toString();
@@ -118,7 +128,6 @@ public abstract class AbstractExecFileTransfer extends AbstractFileTransfer {
         } finally {
             if (isWindows()) {
                 rawFileStore.close();
-                rawFileStore = null;
             }
         }
         try {
