d2aa4dfa281a74835b2922cc87f6ed4ef4b9f389
Sebastien Besson <seb.besson@gmail.com>
2014-09-13 14:31:28 +0100

    Expand getPlane to allow initialized raw pixels store as an input
    When retrieving a large number of planes, initializing a store at each call
    is at the detriment of efficiency. This commit allows the user to pass a fully
    initialized raw pixels store to getPlane, retrieve the pixels data and keep
    the store active.

--- components/tools/OmeroM/src/image/getPlane.m
+++ components/tools/OmeroM/src/image/getPlane.m
@@ -1,4 +1,4 @@
-function plane = getPlane(session, image, z, c, t)
+function plane = getPlane(varargin)
 % GETPLANE Retrieve plane from an image on the OMERO server
 %
 %   plane = getPlane(session, image, z, c, t) returns the plane from input
@@ -7,10 +7,14 @@ function plane = getPlane(session, image, z, c, t)
 %   plane = getPlane(session, imageID, z, c, t) returns the plane from
 %   input image identifier at the input z, c, t coordinates.
 %
+%   plane = getPlane(store, z, c, t) returns the plane from a fully
+%   initialized pixels store at the input z, c, t coordinates.
+%
 %   Examples:
 %
 %      images = getPlane(session, image, z, c, t);
 %      images = getPlane(session, imageID, z, c, t);
+%      images = getPlane(store, z, c, t);
 %
 %
 % See also: GETRAWPIXELSSTORE, GETSTACK, GETTILE
@@ -32,23 +36,37 @@ function plane = getPlane(session, image, z, c, t)
 % with this program; if not, write to the Free Software Foundation, Inc.,
 % 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-% Initialize raw pixels store
-[store, pixels] = getRawPixelsStore(session, image);
-sizeZ = pixels.getSizeZ().getValue();
-sizeC = pixels.getSizeC().getValue();
-sizeT = pixels.getSizeT().getValue();
-
-% Input check
+% Input check for z, c, t coordinates
 ip = inputParser;
 isposint = @(x) isnumeric(x) & x >= 0 & abs(round(x)) == x;
-ip.addRequired('z', @(x) isposint(x) && x < sizeZ);
-ip.addRequired('c', @(x) isposint(x) && x < sizeC);
-ip.addRequired('t', @(x) isposint(x) && x < sizeT);
+
+if nargin > 4
+    % Initialize raw pixels store
+    [store, pixels] = getRawPixelsStore(varargin{1}, varargin{2});
+    sizeZ = pixels.getSizeZ().getValue();
+    sizeC = pixels.getSizeC().getValue();
+    sizeT = pixels.getSizeT().getValue();
+    ip.addRequired('z', @(x) isposint(x) && x < sizeZ);
+    ip.addRequired('c', @(x) isposint(x) && x < sizeC);
+    ip.addRequired('t', @(x) isposint(x) && x < sizeT);
+else
+    store = varargin{1};
+    z = varargin{2};
+    c = varargin{3};
+    t = varargin{4};
+    ip.addRequired('z', isposint);
+    ip.addRequired('c', isposint);
+    ip.addRequired('t', isposint);
+end
+
+% Check z, c, t coordinates
 ip.parse(z, c, t);
 
 % Read plane
 plane = store.getPlane(z, c, t);
 plane = toMatrix(plane, pixels)';
 
-% Close the store
-store.close();
+if nargin > 4
+    % Close the store if initialized by the funtion
+    store.close();
+end
