6aa2bafcaed0215d728fd719b69902ec560e6996
Colin Blackburn <colin@ximenes.org.uk>
2014-09-15 17:09:57 +0100

    Fix test_monitor for pytest and flake8.

--- components/tools/OmeroFS/test/unit/test_monitor.py
+++ components/tools/OmeroFS/test/unit/test_monitor.py
@@ -9,66 +9,75 @@
 
 """
 
-import unittest, logging, threading, time
-logging.basicConfig(level=logging.INFO)
+import pytest
+import logging
+import time
 
 import omero
-
 import omero.all
 import omero.grid.monitors as monitors
-from fsDropBoxMonitorClient import *
-from drivers import *
 
-from path import path
+from drivers import DirInfoEvent, MockMonitor, with_driver
 
-class TestMonitor(unittest.TestCase):
+logging.basicConfig(level=logging.INFO)
 
-    def tearDown(self):
+
+class TestMonitor(object):
+
+    def teardown_method(self, method):
         MockMonitor.static_stop()
 
     def testBadId(self):
-        self.assertRaises(omero.ApiUsageException, MockMonitor().fsEventHappened, 'foo', [])
+        with pytest.raises(omero.ApiUsageException):
+            MockMonitor().fsEventHappened('foo', [])
 
     def testBadFileId(self):
         # Could cause infinite loop
-        self.assertRaises(omero.ApiUsageException, MockMonitor().fsEventHappened, '',[monitors.EventInfo()])
+        with pytest.raises(omero.ApiUsageException):
+            MockMonitor().fsEventHappened('', [monitors.EventInfo()])
 
     def testEmptyAdd(self):
-        MockMonitor().fsEventHappened('',[]) # Does nothing.
+        MockMonitor().fsEventHappened('', [])  # Does nothing.
 
+    @with_driver
     def testBasicAdd(self):
-        self.driver.add(DirInfoEvent(1, monitors.EventInfo(self.dir / "r"/ "root" / "dirtimeout", monitors.EventType.Create)))
+        self.driver.add(DirInfoEvent(1, monitors.EventInfo(
+            self.dir / "root" / "dirtimeout",
+            monitors.EventType.Create)))
         self.driver.run()
-    testBasicAdd = with_driver(testBasicAdd)
 
+    @with_driver
     def testWithSingleImport(self):
-        f = self.dir / "r"/ "root" / "file"
-        self.client.files = {str(f):[str(f)]}
-        self.driver.add(DirInfoEvent(0, monitors.EventInfo(self.dir / "r"/ "root" / "file", monitors.EventType.Create)))
+        f = self.dir / "root" / "file"
+        self.client.files = {str(f): [str(f)]}
+        self.driver.add(DirInfoEvent(0, monitors.EventInfo(
+            self.dir / "root" / "file", monitors.EventType.Create)))
         self.driver.run()
         time.sleep(0.25)
-    testWithSingleImport = with_driver(testWithSingleImport)
 
+    @with_driver
     def testWithMultiImport(self):
-        f1 = str(self.dir / "r"/ "root" / "file1")
-        f2 = str(self.dir / "r"/ "root" / "file2")
-        f3 = str(self.dir / "r"/ "root" / "file3")
-        f4 = str(self.dir / "r"/ "root" / "file4")
-        self.client.files = {f1:[f1,f2,f3,f4]}
+        f1 = str(self.dir / "root" / "file1")
+        f2 = str(self.dir / "root" / "file2")
+        f3 = str(self.dir / "root" / "file3")
+        f4 = str(self.dir / "root" / "file4")
+        self.client.files = {f1: [f1, f2, f3, f4]}
         self.client.setDirImportWait(1)
-        self.driver.add(DirInfoEvent(0.0, monitors.EventInfo(f1, monitors.EventType.Create)))
-        self.driver.add(DirInfoEvent(100, monitors.EventInfo(f2, monitors.EventType.Create)))
-        self.driver.add(DirInfoEvent(200, monitors.EventInfo(f3, monitors.EventType.Create)))
-        self.driver.add(DirInfoEvent(300, monitors.EventInfo(f4, monitors.EventType.Create)))
+        self.driver.add(DirInfoEvent(0.0, monitors.EventInfo(
+            f1, monitors.EventType.Create)))
+        self.driver.add(DirInfoEvent(100, monitors.EventInfo(
+            f2, monitors.EventType.Create)))
+        self.driver.add(DirInfoEvent(200, monitors.EventInfo(
+            f3, monitors.EventType.Create)))
+        self.driver.add(DirInfoEvent(300, monitors.EventInfo(
+            f4, monitors.EventType.Create)))
         time.sleep(1)
         self.driver.run()
-    testWithMultiImport = with_driver(testWithMultiImport)
 
+    @with_driver
     def testDirectoryInDirectory(self):
-        self.driver.add(DirInfoEvent(1, monitors.EventInfo(self.dir / "r"/ "root" / "dir", monitors.EventType.Create)))
-        self.driver.add(DirInfoEvent(1, monitors.EventInfo(self.dir / "r"/ "root" / "dir" / "dir", monitors.EventType.Create)))
+        self.driver.add(DirInfoEvent(1, monitors.EventInfo(
+            self.dir / "root" / "dir", monitors.EventType.Create)))
+        self.driver.add(DirInfoEvent(1, monitors.EventInfo(
+            self.dir / "root" / "dir" / "dir", monitors.EventType.Create)))
         self.driver.run()
-    testDirectoryInDirectory = with_driver(testDirectoryInDirectory)
-
-if __name__ == '__main__':
-    unittest.main()
