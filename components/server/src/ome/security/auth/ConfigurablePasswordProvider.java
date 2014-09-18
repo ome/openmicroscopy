197d62a73ca14b83bc5397be7f8c2c545fb7e8d8
Blazej Pindelski <bpindelski@dundee.ac.uk>
2014-09-18 12:29:49 +0100

    Add omero.security.password_required.

--- components/server/src/ome/security/auth/ConfigurablePasswordProvider.java
+++ components/server/src/ome/security/auth/ConfigurablePasswordProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2013 Glencoe Software, Inc. All rights reserved.
+ * Copyright (C) 2009-2014 Glencoe Software, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -183,7 +183,11 @@ public abstract class ConfigurablePasswordProvider implements PasswordProvider,
         if (trusted == null) {
             return Boolean.FALSE;
         } else if ("".equals(trusted.trim())) {
-            return Boolean.TRUE;
+            if (util.isPasswordRequired(userId)) {
+                return Boolean.FALSE;
+            } else {
+                return Boolean.TRUE;
+            }
         } else {
             if (userId != null && salt) {
                 if (trusted.equals(encodeSaltedPassword(userId, provided))) {
