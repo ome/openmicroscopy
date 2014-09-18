a6436b915337bc2ba9fba652ebe006b7ea70373c
Blazej Pindelski <bpindelski@dundee.ac.uk>
2014-09-18 17:05:12 +0100

    AbstractServerTest.java: Fix initial failure.

--- components/tools/OmeroJava/test/integration/AbstractServerTest.java
+++ components/tools/OmeroJava/test/integration/AbstractServerTest.java
@@ -15,6 +15,7 @@ import static org.testng.AssertJUnit.fail;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -531,7 +532,8 @@ public class AbstractServerTest extends AbstractTest {
         e.setOmeName(omero.rtypes.rstring(uuid));
         e.setFirstName(omero.rtypes.rstring("integration"));
         e.setLastName(omero.rtypes.rstring("tester"));
-        long id = rootAdmin.createUser(e, group.getName().getValue());
+        long id = rootAdmin.createExperimenterWithPassword(e,
+                omero.rtypes.rstring(uuid), group, Collections.EMPTY_LIST);
         e = rootAdmin.getExperimenter(id);
         rootAdmin.addGroups(e, Arrays.asList(group));
         if (owner) {
