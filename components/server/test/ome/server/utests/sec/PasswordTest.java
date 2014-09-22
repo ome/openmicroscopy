b9f674861570ebd760629a49303ae31b36e0f3dc
Sebastien Besson <seb.besson@gmail.com>
2014-09-22 22:18:04 +0100

    Remove unnecessary assert in PasswordTest.testtestUtf8Encoding

--- components/server/test/ome/server/utests/sec/PasswordTest.java
+++ components/server/test/ome/server/utests/sec/PasswordTest.java
@@ -7,7 +7,6 @@
 package ome.server.utests.sec;
 
 import java.io.File;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -534,9 +533,8 @@ public class PasswordTest extends MockObjectTestCase {
         assertEquals(badHash, latin1Util.passwordDigest(good));
     }
 
-    public void testUtf8Encoding() throws UnsupportedEncodingException{
+    public void testUtf8Encoding() {
         final PasswordUtil utf8Util = new PasswordUtil(sql, utf8);
-        assertEquals(good, new String(good.getBytes(utf8), "UTF-8"));
         assertEquals(badHash, utf8Util.passwordDigest(bad));
         assertEquals(goodHash, utf8Util.passwordDigest(good));
         assertFalse(goodHash.equals(badHash));
