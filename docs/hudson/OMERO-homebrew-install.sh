1fa48a457eb5e271c928a6836157ad18d3149086
Sebastien Besson <seb.besson@gmail.com>
2014-09-22 22:19:38 +0100

    Fix (yet another time) the Homebrew installation one-liner

--- docs/hudson/OMERO-homebrew-install.sh
+++ docs/hudson/OMERO-homebrew-install.sh
@@ -26,7 +26,7 @@ TESTING_MODE=${TESTING_MODE:-$DEFAULT_TESTING_MODE}
 ###################################################################
 
 # Install Homebrew in /usr/local
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 cd /usr/local
 
 # Install git if not already installed
