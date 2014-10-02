4326b183cbd45e686ec8402464f639d4de579dbb
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2014-10-02 13:42:43 +0100

    define wrappable requests that may be used with skip-head

---
+++ components/blitz/src/omero/cmd/graphs/WrappableRequest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2014 University of Dundee & Open Microscopy Environment.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package omero.cmd.graphs;
+
+import java.util.Map;
+
+import ome.services.graphs.GraphPolicy;
+import omero.cmd.IRequest;
+import omero.cmd.Response;
+
+import com.google.common.base.Function;
+
+/**
+ * Requests that can be wrapped by a {@link SkipHeadI} request.
+ * @author m.t.b.carroll@dundee.ac.uk
+ * @since 5.1.0
+ */
+public interface WrappableRequest<X> extends IRequest {
+    /**
+     * Copy the fields of this request to that of the given request.
+     * @param request the target of the field copy
+     */
+    void copyFieldsTo(X request);
+
+    /**
+     * Transform the currently applicable graph policy for this request by the given function.
+     * @param adjuster a transformation function for graph policies
+     */
+    void adjustGraphPolicy(Function<GraphPolicy, GraphPolicy> adjuster);
+
+    /**
+     * From the response of the head-skipping request, determine which model objects are the targets of the operation.
+     * @param response the head-skipping request's response
+     * @return the model objects to target
+     */
+    Map<String, long[]> getStartFrom(Response response);
+}
