e5f0d5a7c12a43b01aa2f45225b6ff53907d5173
Dominik Lindner <d.lindner@dundee.ac.uk>
2014-10-02 15:47:43 +0100

    Overhauled export action to match other MeasurementViewerActions

--- components/insight/SRC/org/openmicroscopy/shoola/agents/measurement/view/MeasurementViewerComponent.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/measurement/view/MeasurementViewerComponent.java
@@ -1100,4 +1100,11 @@ class MeasurementViewerComponent
 		view.displayAnalysisResults();
 	}
 
+        /**
+         * Implemented as specified by the {@link MeasurementViewer} interface.
+         * @see MeasurementViewer#exportGraph()
+         */
+        public void exportGraph() {
+            view.exportGraph();
+        }
 }
