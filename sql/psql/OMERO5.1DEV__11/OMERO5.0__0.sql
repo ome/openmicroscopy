ecf8ee47170852eb000e5997f21acbb4b497c5d0
jmoore <josh@glencoesoftware.com>
2014-10-14 14:42:16 +0200

    Upgrade scripts for DEV11

--- sql/psql/OMERO5.1DEV__11/OMERO5.0__0.sql
+++ sql/psql/OMERO5.1DEV__11/OMERO5.0__0.sql
@@ -1353,6 +1353,114 @@ SELECT update_changed_from_event_log();
 
 DROP FUNCTION update_changed_from_event_log();
 
+-- 5.1DEV__11: time units
+
+CREATE SEQUENCE seq_unitstime
+	START WITH 1
+	INCREMENT BY 1
+	NO MAXVALUE
+	NO MINVALUE
+	CACHE 1;
+
+CREATE TABLE unitstime (
+	id bigint NOT NULL,
+	permissions bigint NOT NULL,
+	measurementsystem character varying(255) NOT NULL,
+	"value" character varying(255) NOT NULL,
+	external_id bigint
+);
+
+ALTER TABLE pixels
+	ADD COLUMN timeincrementunit bigint;
+
+ALTER TABLE planeinfo
+	ADD COLUMN deltatunit bigint,
+	ADD COLUMN exposuretimeunit bigint;
+
+ALTER TABLE unitstime
+	ADD CONSTRAINT unitstime_pkey PRIMARY KEY (id);
+
+ALTER TABLE pixels
+	ADD CONSTRAINT fkc51e7ead881a95fa FOREIGN KEY (timeincrementunit) REFERENCES unitstime(id);
+
+ALTER TABLE planeinfo
+	ADD CONSTRAINT fk7da1b10aa0467574 FOREIGN KEY (deltatunit) REFERENCES unitstime(id);
+
+ALTER TABLE planeinfo
+	ADD CONSTRAINT fk7da1b10ae3a7a20c FOREIGN KEY (exposuretimeunit) REFERENCES unitstime(id);
+
+ALTER TABLE unitstime
+	ADD CONSTRAINT unitstime_external_id_key UNIQUE (external_id);
+
+ALTER TABLE unitstime
+	ADD CONSTRAINT unitstime_value_key UNIQUE (value);
+
+ALTER TABLE unitstime
+	ADD CONSTRAINT fkunitstime_external_id_externalinfo FOREIGN KEY (external_id) REFERENCES externalinfo(id);
+
+CREATE INDEX i_pixels_timeincrement ON pixels USING btree (timeincrement);
+
+CREATE INDEX i_planeinfo_deltat ON planeinfo USING btree (deltat);
+
+CREATE INDEX i_planeinfo_exposuretime ON planeinfo USING btree (exposuretime);
+
+--
+-- Manual adjustments, mostly from psql-footer.sql
+--
+
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'Ys','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'Zs','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'Es','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'Ps','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'Ts','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'Gs','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'Ms','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'ks','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'hs','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'das','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'s','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'ds','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'cs','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'ms','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'µs','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'ns','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'ps','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'fs','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'as','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'zs','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'ys','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'min','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'h','SI.SECOND';
+insert into unitstime (id,permissions,value,measurementsystem)
+    select ome_nextval('seq_unitstime'),-52,'d','SI.SECOND';
+
+update pixels set timeincrementunit = (select id from unitstime where value = 's');
+update planeinfo set deltat = (select id from unitstime where value = 's');
+update planeinfo set exposuretimeunit = (select id from unitstime where value = 's');
+
 --
 -- FINISHED
 --
