9ab3d638bfa1b37ada0b0cde26e1da10dcb97a8e
Jean-Marie Burel <j.burel@dundee.ac.uk>
2014-10-21 12:55:41 +0100

    Do not display units for z/t/id

--- components/insight/SRC/org/openmicroscopy/shoola/agents/measurement/util/ui/ResultsCellRenderer.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/measurement/util/ui/ResultsCellRenderer.java
@@ -45,10 +45,12 @@ import javax.swing.table.TableCellRenderer;
 
 
 
+
 //Third-party libraries
 import org.apache.commons.lang.StringUtils;
 //Application-internal dependencies
 import org.openmicroscopy.shoola.agents.measurement.IconManager;
+import org.openmicroscopy.shoola.agents.measurement.util.model.AnnotationDescription;
 import org.openmicroscopy.shoola.agents.measurement.view.MeasurementTableModel;
 import org.openmicroscopy.shoola.util.roi.model.annotation.AnnotationKeys;
 import org.openmicroscopy.shoola.util.roi.model.util.FigureType;
@@ -246,7 +248,10 @@ public class ResultsCellRenderer
 	            s = twoDecimalPlaces(object.getValue());
 	            if (StringUtils.isNotBlank(s)) {
 	                buffer.append(s);
-	                if (!AnnotationKeys.ANGLE.getKey().equals(k)) {
+	                if (!(AnnotationKeys.ANGLE.getKey().equals(k) ||
+	                        AnnotationDescription.ZSECTION_STRING.equals(k) ||
+	                        AnnotationDescription.ROIID_STRING.equals(k) ||
+	                        AnnotationDescription.TIME_STRING.equals(k))) {
 	                    buffer.append(object.getUnits());
 	                }
 	                if (AnnotationKeys.AREA.getKey().equals(k)) {
