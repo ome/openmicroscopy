8ccd73d34fcb3d706c9e63f3086d9f935795dfed
Sebastien Besson <seb.besson@gmail.com>
2014-10-22 14:44:37 +0100

    Use absolutepath for setuptools in setup.py

--- components/tools/OmeroFS/setup.py
+++ components/tools/OmeroFS/setup.py
@@ -12,7 +12,7 @@ import os
 
 for tools in glob.glob("../../../lib/repository/setuptools*.egg"):
     if tools.find(".".join(map(str, sys.version_info[0:2]))) > 0:
-       sys.path.insert(0, tools)
+       sys.path.insert(0, os.path.abspath(tools))
 
 sys.path.append("..")
 from test_setup import PyTest
