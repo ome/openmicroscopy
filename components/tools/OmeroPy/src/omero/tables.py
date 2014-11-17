c4a288c40e3cff638a50a3d1ffb3774f77e00bff
Simon Li <spli@dundee.ac.uk>
2014-11-17 16:33:19 +0000

    Check HDF file size after close, not just after flush

--- components/tools/OmeroPy/src/omero/tables.py
+++ components/tools/OmeroPy/src/omero/tables.py
@@ -608,7 +608,6 @@ class TableI(omero.grid.Table, omero.util.SimpleServant):
                 unwrap(self.file_obj.id) if self.file_obj else None)
             return
 
-        size = self.storage.size() # Size to reset the server object to
         modified = self.storage.modified()
 
         try:
@@ -626,6 +625,9 @@ class TableI(omero.grid.Table, omero.util.SimpleServant):
             client_uuid = self.factory.ice_getIdentity().category[8:]
             ctx = {"omero.group": str(gid), omero.constants.CLIENTUUID: client_uuid}
             try:
+                # Size to reset the server object to (must be checked after
+                # the underlying HDF file has been closed)
+                size = self.storage.size()
                 rfs = self.factory.createRawFileStore(ctx)
                 try:
                     rfs.setFileId(fid, ctx)
