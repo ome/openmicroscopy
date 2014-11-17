9175af86ec1deb2d92e2aa159a6d2395980eb0bf
jmoore <josh@glencoesoftware.com>
2014-11-17 22:18:49 +0100

    Avoid GCC errors for earlier versions
    The pragma statements produce warnings under, e.g.,
    GCC 4.4.7. Since they are intended to **reduce**
    noise, disable pushing/popping until 4.6.

--- components/tools/OmeroCpp/src/omero/IceNoWarnPush.h
+++ components/tools/OmeroCpp/src/omero/IceNoWarnPush.h
@@ -1,5 +1,5 @@
 #ifndef OMEROCPP_ICE_WARN
-#  ifdef __GNUC__
+#  if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
 #    pragma GCC diagnostic push
 #    pragma GCC diagnostic ignored "-Woverloaded-virtual"
 #    pragma GCC diagnostic ignored "-Wredundant-decls"
