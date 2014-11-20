9d0f8262e2cf105780ce273b1985101834f5c322
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2014-11-20 12:37:02 +0000

    rewriting rounding algorithm

--- components/tools/OmeroWeb/omeroweb/webgateway/static/webgateway/js/ome.scalebardisplay.js
+++ components/tools/OmeroWeb/omeroweb/webgateway/static/webgateway/js/ome.scalebardisplay.js
@@ -33,38 +33,39 @@ $.fn.scalebar_display = function(options) {
         }
 
         this.setScalebarZoom = function(zoom) {
-            // scalebar shouldn't be bigger then 1/5 of the regular viewport
-            if (tiles) {
-                var width = 100;
-            } else {
-                var width = 0;
-                var viewport_width = $scalebar.parent().width();
-                if (viewport_width > imageWidth*zoom) {
-                    var width = Math.ceil(imageWidth/5);
+            var width = 100;
+
+            function round(x) {
+                if (x > 10) {
+                    var num = Math.floor(x);
+                    var factor = Math.pow(10, Math.floor(Math.log(num) / Math.LN10));
+                    var unit = factor * Math.ceil(num/factor);
+                    return unit;
                 } else {
-                    var width = Math.ceil(viewport_width/50);
+
+                    function power(n) {
+                        p = 1;
+                        while(n < 10) {
+                            n = n*1000;
+                            p++;
+                        }
+                        return p;
+                    }
+                    p = Math.pow(1000, power(x))
+
+                    var num = Math.floor(x* p);
+                    var factor = Math.pow(10, Math.floor(Math.log(num) / Math.LN10));
+                    var unit = factor * Math.ceil(num/factor) / p;
+                    return unit;
+
                 }
             }
-            // find the nearest value round to power of 10
-            // workaround for units smaller then micrometer
-            if (pixSizeX < 1) {
-                var num = Math.floor(width * pixSizeX * Math.pow(10,12));
-                var factor = Math.pow(10, Math.floor(Math.log(num) / Math.LN10));
-                var unit = factor * Math.ceil(num/factor) / Math.pow(10,12);
-            } else {
-                var num = Math.floor(width * pixSizeX);
-                var factor = Math.pow(10, Math.floor(Math.log(num) / Math.LN10));
-                var unit = factor * Math.ceil(num/factor);
-            }
-            if (tiles) {
-                var scalebar_width = Math.round(unit/pixSizeX);
-                $scalebar.width(scalebar_width);
-                $scalebar.html((unit/zoom).lengthformat(0));
-            } else {
-                var scalebar_width = Math.round(unit/pixSizeX*zoom);
-                $scalebar.width(scalebar_width);
-                $scalebar.html(unit.lengthformat(0));
-            }
+
+            unit = round(width * pixSizeX / zoom);
+            scalebar_width = Math.round(unit/pixSizeX * zoom)
+            $scalebar.width(scalebar_width);
+            $scalebar.html((unit).lengthformat(0));
+
         }
 
     });
