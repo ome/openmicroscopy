64781fb04d37d2d2bbb27956e7ee7ce8da706571
Dominik Lindner <d.lindner@dundee.ac.uk>
2014-12-04 11:09:06 +0000

    Replaced UnitsLength.PT with UnitsLength.POINT

--- components/insight/SRC/org/openmicroscopy/shoola/util/roi/io/InputServerStrategy.java
+++ components/insight/SRC/org/openmicroscopy/shoola/util/roi/io/InputServerStrategy.java
@@ -580,7 +580,7 @@ class InputServerStrategy
 		STROKE_COLOR.set(figure, data.getStroke());
 		FILL_COLOR.set(figure, data.getFill());
 		FONT_FACE.set(figure, data.getFont());
-		FONT_SIZE.set(figure, data.getFontSize(UnitsLength.PT).getValue());
+		FONT_SIZE.set(figure, data.getFontSize(UnitsLength.POINT).getValue());
 		FONT_ITALIC.set(figure, data.isFontItalic());
 		FONT_BOLD.set(figure, data.isFontBold());
 		STROKE_CAP.set(figure, data.getLineCap());
