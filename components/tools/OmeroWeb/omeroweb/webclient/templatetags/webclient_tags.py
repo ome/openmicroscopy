919d17d4cb01c6b86d419abba95307587010e06d
Sebastien Besson <seb.besson@gmail.com>
2014-12-07 23:01:14 +0000

    Flake8 webclient.templatetags

--- components/tools/OmeroWeb/omeroweb/webclient/templatetags/webclient_tags.py
+++ components/tools/OmeroWeb/omeroweb/webclient/templatetags/webclient_tags.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
-# 
-# 
-# Copyright (c) 2008 University of Dundee. 
-# 
+#
+#
+#
+# Copyright (c) 2008 University of Dundee.
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 # Author: Aleksandra Tarkowska <A(dot)Tarkowska(at)dundee(dot)ac(dot)uk>, 2008.
-# 
+#
 # Version: 1.0
 #
 
@@ -32,6 +32,7 @@ from django.templatetags.static import PrefixNode
 register = template.Library()
 
 logger = logging.getLogger(__name__)
+
 
 @register.tag()
 def get_static_webclient_prefix(parser, token):
@@ -50,6 +51,7 @@ def get_static_webclient_prefix(parser, token):
 
     """
     return PrefixNode.handle_token(parser, token, "STATIC_WEBCLIENT_URL")
+
 
 @register.filter
 def get_item(dictionary, key):
