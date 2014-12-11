bdf086566a14154ef11a909a6c5b8c5f3b8d65f7
Dominik Lindner <d.lindner@dundee.ac.uk>
2014-12-11 16:24:51 +0000

    Added MapAnnotation Pojo

---
+++ components/blitz/src/pojos/MapAnnotationData.java
@@ -0,0 +1,52 @@
+package pojos;
+
+import java.util.Iterator;
+import java.util.List;
+
+import omero.model.MapAnnotation;
+import omero.model.MapAnnotationI;
+import omero.model.NamedValue;
+
+@SuppressWarnings("unchecked")
+public class MapAnnotationData extends AnnotationData {
+
+	/**
+	 * The name space used to identify MapAnnotations created be the user
+	 */
+	public static final String NS_CLIENT_CREATED = omero.constants.metadata.NSCLIENTMAPANNOTATION.value;
+
+	public MapAnnotationData(MapAnnotation value) {
+		super(value);
+	}
+
+	public MapAnnotationData() {
+		super(MapAnnotationI.class);
+	}
+
+	@Override
+	public void setContent(Object content) {
+		MapAnnotation anno = (MapAnnotation) asAnnotation();
+		anno.setMapValue((List<NamedValue>) content);
+	}
+
+	@Override
+	public Object getContent() {
+		MapAnnotation anno = (MapAnnotation) asAnnotation();
+		return anno.getMapValue();
+	}
+
+	@Override
+	public String getContentAsString() {
+		List<NamedValue> data = (List<NamedValue>) getContent();
+		StringBuilder sb = new StringBuilder();
+		Iterator<NamedValue> it = data.iterator();
+		while (it.hasNext()) {
+			NamedValue next = it.next();
+			sb.append(next.name + "=" + next.value);
+			if (it.hasNext())
+				sb.append(';');
+		}
+		return sb.toString();
+	}
+
+}
