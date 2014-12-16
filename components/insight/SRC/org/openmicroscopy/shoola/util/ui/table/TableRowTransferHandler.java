88babe690172025b2b78ee19b60de093df2ffd70
Dominik Lindner <d.lindner@dundee.ac.uk>
2014-12-16 16:11:52 +0000

    Added 'Reorder' support for JTables

---
+++ components/insight/SRC/org/openmicroscopy/shoola/util/ui/table/TableRowTransferHandler.java
@@ -0,0 +1,111 @@
+/*
+ * org.openmicroscopy.shoola.util.ui.tdialog.BorderListener
+ *
+ *------------------------------------------------------------------------------
+ *  Copyright (C) 2014 University of Dundee. All rights reserved.
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.shoola.util.ui.table;
+
+import java.awt.Cursor;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DragSource;
+
+import javax.activation.ActivationDataFlavor;
+import javax.activation.DataHandler;
+import javax.swing.JComponent;
+import javax.swing.JTable;
+import javax.swing.TransferHandler;
+
+/**
+ * TransferHandler for reordering rows in a JTable
+ * 
+ * Based on a stackoverflow post by Aaron Davidson:
+ * http://stackoverflow.com/questions
+ * /638807/how-do-i-drag-and-drop-a-row-in-a-jtable
+ * 
+ * @author Dominik Lindner &nbsp;&nbsp;&nbsp;&nbsp; <a
+ *         href="mailto:d.lindner@dundee.ac.uk">d.lindner@dundee.ac.uk</a>
+ */
+@SuppressWarnings("serial")
+public class TableRowTransferHandler extends TransferHandler {
+
+	private final DataFlavor localObjectFlavor = new ActivationDataFlavor(
+			Integer.class, "Integer Row Index");
+
+	private JTable table = null;
+
+	public TableRowTransferHandler(JTable table) {
+		this.table = table;
+	}
+
+	@Override
+	protected Transferable createTransferable(JComponent c) {
+		return new DataHandler(new Integer(table.getSelectedRow()),
+				localObjectFlavor.getMimeType());
+	}
+
+	@Override
+	public boolean canImport(TransferHandler.TransferSupport info) {
+		boolean b = info.getComponent() == table && info.isDrop()
+				&& info.isDataFlavorSupported(localObjectFlavor);
+		table.setCursor(b ? DragSource.DefaultMoveDrop
+				: DragSource.DefaultMoveNoDrop);
+		return b;
+	}
+
+	@Override
+	public int getSourceActions(JComponent c) {
+		return TransferHandler.COPY_OR_MOVE;
+	}
+
+	@Override
+	public boolean importData(TransferHandler.TransferSupport info) {
+		JTable target = (JTable) info.getComponent();
+		JTable.DropLocation dl = (JTable.DropLocation) info.getDropLocation();
+		int index = dl.getRow();
+		int max = table.getModel().getRowCount();
+		if (index < 0 || index > max)
+			index = max;
+		target.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		try {
+			Integer rowFrom = (Integer) info.getTransferable().getTransferData(
+					localObjectFlavor);
+			if (rowFrom != -1 && rowFrom != index) {
+				((Reorderable) table.getModel()).reorder(rowFrom, index);
+				if (index > rowFrom)
+					index--;
+				target.getSelectionModel().addSelectionInterval(index, index);
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	protected void exportDone(JComponent c, Transferable t, int act) {
+		if (act == TransferHandler.MOVE || act == TransferHandler.NONE) {
+			table.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		}
+	}
+
+}
