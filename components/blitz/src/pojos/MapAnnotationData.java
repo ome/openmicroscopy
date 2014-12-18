e2c8d472b3887df12a17761a6dd6936f594434f5
Dominik Lindner <d.lindner@dundee.ac.uk>
2014-12-18 11:49:58 +0000

    Added comment/license info

--- components/blitz/src/pojos/MapAnnotationData.java
+++ components/blitz/src/pojos/MapAnnotationData.java
@@ -1,3 +1,23 @@
+/*
+ *------------------------------------------------------------------------------
+ *  Copyright (C) 2014 University of Dundee. All rights reserved.
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *------------------------------------------------------------------------------
+ */
 package pojos;
 
 import java.util.Iterator;
@@ -8,6 +28,12 @@ import omero.model.MapAnnotationI;
 import omero.model.NamedValue;
 
 @SuppressWarnings("unchecked")
+/**
+ * The data that makes up an <i>OME</i> MapAnnotation
+ *
+ * @author Dominik Lindner &nbsp;&nbsp;&nbsp;&nbsp;
+ * <a href="mailto:d.lindner@dundee.ac.uk">d.lindner@dundee.ac.uk</a>
+ */
 public class MapAnnotationData extends AnnotationData {
 
 	/**
--- components/insight/SRC/org/openmicroscopy/shoola/env/data/util/ModelMapper.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/util/ModelMapper.java
@@ -528,15 +528,15 @@ public class ModelMapper
     		annotation = new DoubleAnnotationI();
     		((DoubleAnnotation) annotation).setDoubleValue(omero.rtypes.rdouble(
     										(Double) data.getContent()));
-    	} else if (data instanceof MapAnnotationData) {
-					annotation = new MapAnnotationI();
-					String ns = data.getNameSpace();
-		    		if (StringUtils.isNotEmpty(ns)) {
-		    			annotation.setNs(omero.rtypes.rstring(ns));
-		    		}
-					((MapAnnotation) annotation).setMapValue((List<NamedValue>) data
-							.getContent());
-				}
+		} else if (data instanceof MapAnnotationData) {
+			annotation = new MapAnnotationI();
+			String ns = data.getNameSpace();
+			if (StringUtils.isNotEmpty(ns)) {
+				annotation.setNs(omero.rtypes.rstring(ns));
+			}
+			((MapAnnotation) annotation).setMapValue((List<NamedValue>) data
+					.getContent());
+		}
     	return annotation;
     }
     
