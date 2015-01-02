23af48ccaf937cbf623644fe7f5121230020bf57
Sebastien Besson <seb.besson@gmail.com>
2015-01-02 22:43:43 +0000

    Add utility functions for double and timestamp annotations
    This commit should add functions for retrieving and creating these 2 types of
    annotations.

--- components/tools/OmeroM/src/annotations/getScreenTagAnnotations.m
+++ components/tools/OmeroM/src/annotations/getScreenTagAnnotations.m
@@ -27,9 +27,10 @@ function tas = getScreenTagAnnotations(session, ids, varargin)
 %        tas = getScreenTagAnnotations(session, ids, 'owner', -1)
 %
 % See also: GETOBJECTANNOTATIONS, GETSCREENCOMMENTANNOTATIONS,
-% GETSCREENFILEANNOTATIONS, GETSCREENXMLANNOTATIONS
+% GETSCREENDOUBLEANNOTATIONS, GETSCREENFILEANNOTATIONS,
+% GETSCREENTIMESTAMPANNOTATIONS, GETSCREENXMLANNOTATIONS
 
-% Copyright (C) 2013-2014 University of Dundee & Open Microscopy Environment.
+% Copyright (C) 2013-2015 University of Dundee & Open Microscopy Environment.
 % All rights reserved.
 %
 % This program is free software; you can redistribute it and/or modify
