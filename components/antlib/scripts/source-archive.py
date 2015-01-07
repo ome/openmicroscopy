c8d7ca8684764386f5eee97e01d22393e20329bb
Sebastien Besson <seb.besson@gmail.com>
2015-01-07 15:46:23 +0000

    Flake8 source-archive.py

--- components/antlib/scripts/source-archive.py
+++ components/antlib/scripts/source-archive.py
@@ -23,9 +23,32 @@ import zipfile
 # attributes.  It excludes .gitignore files at this point to avoid
 # polluting the release with version control files.
 
+GITVERSION_XML = """<?xml version="1.0" encoding="utf-8"?>
+<project name="gitversion" basedir=".">
+        <property name="omero.shortversion" value="%s"/>
+        <property name="omero.plainversion" value="%s"/>
+        <property name="omero.vcs.revision" value="%s"/>
+        <property name="omero.vcs.date" value="%s"/>
+
+        <!-- Properties for detecting where these properties were read from -->
+        <property name="omero.shortversion.source" value="embedded"/>
+        <property name="omero.plainversion.source" value="embedded"/>
+        <property name="omero.vcs.revision.source" value="embedded"/>
+        <property name="omero.vcs.date.source" value="embedded"/>
+</project>
+"""
+GITVERSION_CMAKE = """set(OME_VERSION "%s")
+set(OME_VERSION_SHORT "%s")
+set(OME_VCS_REVISION "%s")
+set(OME_VCS_DATE "%s")
+"""
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 9:
-        raise Exception('Usage: %s releasename shortversion fullversion vcs-revision vcs-date versionfile cppversionfile targetdir')
+        raise Exception(
+            'Usage: %s releasename shortversion fullversion vcs-revision'
+            ' vcs-date versionfile cppversionfile targetdir')
 
     release = sys.argv[1]
     shortversion = sys.argv[2]
@@ -35,7 +58,7 @@ if __name__ == "__main__":
     versionfile = sys.argv[6]
     cppversionfile = sys.argv[7]
     target = os.path.abspath(sys.argv[8])
-    release = "%s-%s" % (release,version)
+    release = "%s-%s" % (release, version)
 
     if not os.path.isdir('.git'):
         raise Exception('Releasing is only possible from a git repository')
@@ -44,22 +67,25 @@ if __name__ == "__main__":
     sys.stdout.flush()
 
     # Create base archive
-    base_archive_status = call(['git', 'archive', '--format', 'zip',
-                                '--prefix', "%s/" % (release),
-                                '--output', "%s/%s-base.zip" % (target, release),
-                                'HEAD'])
+    base_archive_status = call([
+        'git', 'archive', '--format', 'zip',
+        '--prefix', "%s/" % (release),
+        '--output', "%s/%s-base.zip" % (target, release),
+        'HEAD'])
     if base_archive_status != 0:
         raise Exception('Failed to create git base archive')
 
     zips = list(["%s/%s-base.zip" % (target, release)])
 
     # Create submodule archives
-    submodule_archive = Popen(['git', 'submodule', 'foreach', '--quiet', '--recursive',
-                               "npath=\"$(echo \"$path\" | tr / _)\"; \
-                                zip=\"%s/%s-submod-${npath}.zip\"; \
-                                git archive --format zip --prefix \"%s/${path}/\" --output \"${zip}\" HEAD || exit 1; \
-                                echo \"${zip}\"" % (target, release, release)],
-                              stdout=PIPE)
+    submodule_archive = Popen([
+        'git', 'submodule', 'foreach', '--quiet', '--recursive',
+        ("npath=\"$(echo \"$path\" | tr / _)\"; "
+         "zip=\"%s/%s-submod-${npath}.zip\"; "
+         "git archive --format zip --prefix \"%s/${path}/\" "
+         "--output \"${zip}\" HEAD || exit 1; "
+         "echo \"${zip}\"") % (target, release, release)],
+        stdout=PIPE)
     submodule_zips = submodule_archive.communicate()[0]
     if submodule_archive.returncode != 0:
         raise Exception('Failed to create git submodule archives')
@@ -79,7 +105,9 @@ if __name__ == "__main__":
         # Iterate over the ZipInfo objects from the archive
         for info in subzip.infolist():
             # Skip unwanted git and travis files
-            if os.path.basename(info.filename) == '.gitignore' or os.path.basename(info.filename) == '.gitmodule' or os.path.basename(info.filename) == '.travis.yml':
+            if (os.path.basename(info.filename) == '.gitignore' or
+                    os.path.basename(info.filename) == '.gitmodule' or
+                    os.path.basename(info.filename) == '.travis.yml'):
                 continue
             # Repack a single zip object; preserve the metadata
             # directly via the ZipInfo object and rewrite the content
@@ -91,24 +119,9 @@ if __name__ == "__main__":
         os.remove(name)
 
     # Embed release number
-    basezip.writestr("%s/%s" % (release, versionfile),
-"""<?xml version="1.0" encoding="utf-8"?>
-<project name="gitversion" basedir=".">
-        <property name="omero.shortversion" value="%s"/>
-        <property name="omero.plainversion" value="%s"/>
-        <property name="omero.vcs.revision" value="%s"/>
-        <property name="omero.vcs.date" value="%s"/>
-
-        <!-- Properties for detecting where these properties were read from -->
-        <property name="omero.shortversion.source" value="embedded"/>
-        <property name="omero.plainversion.source" value="embedded"/>
-        <property name="omero.vcs.revision.source" value="embedded"/>
-        <property name="omero.vcs.date.source" value="embedded"/>
-</project>
-""" % (shortversion, version, vcs_revision, vcs_date))
-    basezip.writestr("%s/%s" % (release, cppversionfile),
-"""set(OME_VERSION "%s")
-set(OME_VERSION_SHORT "%s")
-set(OME_VCS_REVISION "%s")
-set(OME_VCS_DATE "%s")
-""" % (version, shortversion, vcs_revision, vcs_date))
+    basezip.writestr(
+        "%s/%s" % (release, versionfile),
+        GITVERSION_XML % (shortversion, version, vcs_revision, vcs_date))
+    basezip.writestr(
+        "%s/%s" % (release, cppversionfile),
+        GITVERSION_CMAKE % (version, shortversion, vcs_revision, vcs_date))
