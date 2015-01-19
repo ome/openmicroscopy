1da53c7ff18226b5212963b4862139a6e4e1c5fe
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2015-01-19 10:38:55 +0000

    update copyright header

--- components/server/src/ome/services/graphs/GraphTraversal.java
+++ components/server/src/ome/services/graphs/GraphTraversal.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 University of Dundee & Open Microscopy Environment.
+ * Copyright (C) 2014-2015 University of Dundee & Open Microscopy Environment.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
