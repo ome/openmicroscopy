1c705f600d177e42df3977086d158546596c8d4a
Colin Blackburn <colin@ximenes.org.uk>
2015-01-20 10:43:34 +0000

    Change checking logic slightly to provide better logging.

--- components/tools/OmeroFS/fsUtil.py
+++ components/tools/OmeroFS/fsUtil.py
@@ -35,7 +35,7 @@ def monitorPackage():
                   'MACOS_10_5+'            : 'fsMac-10-5-Monitor',
                   'LINUX_2_6_13+pyinotify' : 'fsPyinotifyMonitor',
                   'WIN_tested'             : 'fsWin-XP-Monitor',
-                  'WIN_any'                : 'fsWin-XP-Monitor'
+                  'WIN_other'              : 'fsWin-XP-Monitor'
                 }
     
     # Versions of Windows that have been tested.
@@ -86,17 +86,18 @@ def monitorPackage():
 
     # Windows of some flavour.
     elif system == 'Windows':
-        if platformCheck:
-            version = platform.platform().split('-')
-            if version[1] in winTested:
-                current = 'WIN_tested'
-            else:
-                errorString = "Windows XP, Vista, 7 or Server 2003, 2008 or 2008R2 required. You have: %s" % platform.platform()
+        if not platformCheck:
+            log.warn("Strict platform checking disabled!")
+        version = platform.platform().split('-')
+        if version[1] in winTested:
+            current = 'WIN_tested'
         else:
-            current = 'WIN_any'
-            log.warn("Strict platform checking disabled!"
-                     "Untested Windows version %s detected"
-                     % platform.platform())
+            if platformCheck:
+                errorString = "Windows XP, Vista, 7 or Server 2003, 2008 or 2008R2 required. You have: %s" % platform.platform()
+            else:
+                current = 'WIN_other'
+                log.warn("Untested Windows version %s detected"
+                         % platform.platform())
 
     # Unknown OS.
     else:
