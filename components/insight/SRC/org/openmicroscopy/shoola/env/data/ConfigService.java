d1e2a8de47a3a06bc3b907c29e084a8031dbc6a5
Dominik Lindner <d.lindner@dundee.ac.uk>
2015-01-22 09:59:16 +0000

    Create ConfigService wrapper to access server-side config properties

---
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/ConfigService.java
@@ -0,0 +1,41 @@
+/*
+ *------------------------------------------------------------------------------
+ *  Copyright (C) 2015 University of Dundee. All rights reserved.
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.shoola.env.data;
+
+/**
+ * Provides access to server-side configuration properties.
+ * 
+ * @author  Dominik Lindner &nbsp;&nbsp;&nbsp;&nbsp;
+ * <a href="mailto:d.lindner@dundee.ac.uk">d.lindner@dundee.ac.uk</a>
+ *
+ */
+public interface ConfigService {
+
+	/**
+	 * Lookup a config value
+	 * @param key The name of the property
+	 * @return The value, if set; otherwise <code>null</code>
+	 * @throws DSOutOfServiceException
+	 * @throws DSAccessException
+	 */
+	String lookup(String key) throws DSOutOfServiceException, DSAccessException;
+	
+}
