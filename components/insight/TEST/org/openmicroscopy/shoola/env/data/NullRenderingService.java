f2eb2df111438c6bc235264beda3eefadc2f76c3
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-01-22 15:04:13 +0000

    Update header

--- components/insight/TEST/org/openmicroscopy/shoola/env/data/NullRenderingService.java
+++ components/insight/TEST/org/openmicroscopy/shoola/env/data/NullRenderingService.java
@@ -2,7 +2,7 @@
  * org.openmicroscopy.shoola.env.data.NullRenderingService
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2015 University of Dundee. All rights reserved.
  *
  *
  * 	This program is free software; you can redistribute it and/or modify
