8e9b9c26c60de2ebeb12f1aae2a82b40a20da792
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2015-01-26 14:43:26 +0000

    adjust upgrade scripts for units null-pair constraints

--- sql/psql/OMERO5.1DEV__19/OMERO5.0__0.sql
+++ sql/psql/OMERO5.1DEV__19/OMERO5.0__0.sql
@@ -2337,6 +2337,148 @@ INSERT INTO namespace (id, name, permissions)
                             WHERE ns IS NOT NULL AND ns NOT IN (SELECT name FROM namespace)) AS id_row
                   WHERE id_row.row_n = 1);
 
+-- A property value is null if and only if the corresponding unit is null.
+
+ALTER TABLE detector ADD CONSTRAINT voltage_unitpair
+    CHECK (voltage IS NULL AND voltageunit IS NULL
+        OR voltage IS NOT NULL AND voltageunit IS NOT NULL);
+
+ALTER TABLE detectorsettings ADD CONSTRAINT readoutrate_unitpair
+    CHECK (readoutrate IS NULL AND readoutrateunit IS NULL
+        OR readoutrate IS NOT NULL AND readoutrateunit IS NOT NULL);
+
+ALTER TABLE detectorsettings ADD CONSTRAINT voltage_unitpair
+    CHECK (voltage IS NULL AND voltageunit IS NULL
+        OR voltage IS NOT NULL AND voltageunit IS NOT NULL);
+
+ALTER TABLE imagingenvironment ADD CONSTRAINT airpressure_unitpair
+    CHECK (airpressure IS NULL AND airpressureunit IS NULL
+        OR airpressure IS NOT NULL AND airpressureunit IS NOT NULL);
+
+ALTER TABLE imagingenvironment ADD CONSTRAINT temperature_unitpair
+    CHECK (temperature IS NULL AND temperatureunit IS NULL
+        OR temperature IS NOT NULL AND temperatureunit IS NOT NULL);
+
+ALTER TABLE laser ADD CONSTRAINT repetitionrate_unitpair
+    CHECK (repetitionrate IS NULL AND repetitionrateunit IS NULL
+        OR repetitionrate IS NOT NULL AND repetitionrateunit IS NOT NULL);
+
+ALTER TABLE laser ADD CONSTRAINT wavelength_unitpair
+    CHECK (wavelength IS NULL AND wavelengthunit IS NULL
+        OR wavelength IS NOT NULL AND wavelengthunit IS NOT NULL);
+
+ALTER TABLE lightsettings ADD CONSTRAINT wavelength_unitpair
+    CHECK (wavelength IS NULL AND wavelengthunit IS NULL
+        OR wavelength IS NOT NULL AND wavelengthunit IS NOT NULL);
+
+ALTER TABLE lightsource ADD CONSTRAINT power_unitpair
+    CHECK (power IS NULL AND powerunit IS NULL
+        OR power IS NOT NULL AND powerunit IS NOT NULL);
+
+ALTER TABLE logicalchannel ADD CONSTRAINT emissionwave_unitpair
+    CHECK (emissionwave IS NULL AND emissionwaveunit IS NULL
+        OR emissionwave IS NOT NULL AND emissionwaveunit IS NOT NULL);
+
+ALTER TABLE logicalchannel ADD CONSTRAINT excitationwave_unitpair
+    CHECK (excitationwave IS NULL AND excitationwaveunit IS NULL
+        OR excitationwave IS NOT NULL AND excitationwaveunit IS NOT NULL);
+
+ALTER TABLE logicalchannel ADD CONSTRAINT pinholesize_unitpair
+    CHECK (pinholesize IS NULL AND pinholesizeunit IS NULL
+        OR pinholesize IS NOT NULL AND pinholesizeunit IS NOT NULL);
+
+ALTER TABLE objective ADD CONSTRAINT workingdistance_unitpair
+    CHECK (workingdistance IS NULL AND workingdistanceunit IS NULL
+        OR workingdistance IS NOT NULL AND workingdistanceunit IS NOT NULL);
+
+ALTER TABLE pixels ADD CONSTRAINT physicalsizex_unitpair
+    CHECK (physicalsizex IS NULL AND physicalsizexunit IS NULL
+        OR physicalsizex IS NOT NULL AND physicalsizexunit IS NOT NULL);
+
+ALTER TABLE pixels ADD CONSTRAINT physicalsizey_unitpair
+    CHECK (physicalsizey IS NULL AND physicalsizeyunit IS NULL
+        OR physicalsizey IS NOT NULL AND physicalsizeyunit IS NOT NULL);
+
+ALTER TABLE pixels ADD CONSTRAINT physicalsizez_unitpair
+    CHECK (physicalsizez IS NULL AND physicalsizezunit IS NULL
+        OR physicalsizez IS NOT NULL AND physicalsizezunit IS NOT NULL);
+
+ALTER TABLE pixels ADD CONSTRAINT timeincrement_unitpair
+    CHECK (timeincrement IS NULL AND timeincrementunit IS NULL
+        OR timeincrement IS NOT NULL AND timeincrementunit IS NOT NULL);
+
+ALTER TABLE planeinfo ADD CONSTRAINT deltat_unitpair
+    CHECK (deltat IS NULL AND deltatunit IS NULL
+        OR deltat IS NOT NULL AND deltatunit IS NOT NULL);
+
+ALTER TABLE planeinfo ADD CONSTRAINT exposuretime_unitpair
+    CHECK (exposuretime IS NULL AND exposuretimeunit IS NULL
+        OR exposuretime IS NOT NULL AND exposuretimeunit IS NOT NULL);
+
+ALTER TABLE planeinfo ADD CONSTRAINT positionx_unitpair
+    CHECK (positionx IS NULL AND positionxunit IS NULL
+        OR positionx IS NOT NULL AND positionxunit IS NOT NULL);
+
+ALTER TABLE planeinfo ADD CONSTRAINT positiony_unitpair
+    CHECK (positiony IS NULL AND positionyunit IS NULL
+        OR positiony IS NOT NULL AND positionyunit IS NOT NULL);
+
+ALTER TABLE planeinfo ADD CONSTRAINT positionz_unitpair
+    CHECK (positionz IS NULL AND positionzunit IS NULL
+        OR positionz IS NOT NULL AND positionzunit IS NOT NULL);
+
+ALTER TABLE plate ADD CONSTRAINT welloriginx_unitpair
+    CHECK (welloriginx IS NULL AND welloriginxunit IS NULL
+        OR welloriginx IS NOT NULL AND welloriginxunit IS NOT NULL);
+
+ALTER TABLE plate ADD CONSTRAINT welloriginy_unitpair
+    CHECK (welloriginy IS NULL AND welloriginyunit IS NULL
+        OR welloriginy IS NOT NULL AND welloriginyunit IS NOT NULL);
+
+ALTER TABLE shape ADD CONSTRAINT fontsize_unitpair
+    CHECK (fontsize IS NULL AND fontsizeunit IS NULL
+        OR fontsize IS NOT NULL AND fontsizeunit IS NOT NULL);
+
+ALTER TABLE shape ADD CONSTRAINT strokewidth_unitpair
+    CHECK (strokewidth IS NULL AND strokewidthunit IS NULL
+        OR strokewidth IS NOT NULL AND strokewidthunit IS NOT NULL);
+
+ALTER TABLE stagelabel ADD CONSTRAINT positionx_unitpair
+    CHECK (positionx IS NULL AND positionxunit IS NULL
+        OR positionx IS NOT NULL AND positionxunit IS NOT NULL);
+
+ALTER TABLE stagelabel ADD CONSTRAINT positiony_unitpair
+    CHECK (positiony IS NULL AND positionyunit IS NULL
+        OR positiony IS NOT NULL AND positionyunit IS NOT NULL);
+
+ALTER TABLE stagelabel ADD CONSTRAINT positionz_unitpair
+    CHECK (positionz IS NULL AND positionzunit IS NULL
+        OR positionz IS NOT NULL AND positionzunit IS NOT NULL);
+
+ALTER TABLE transmittancerange ADD CONSTRAINT cutintolerance_unitpair
+    CHECK (cutintolerance IS NULL AND cutintoleranceunit IS NULL
+        OR cutintolerance IS NOT NULL AND cutintoleranceunit IS NOT NULL);
+
+ALTER TABLE transmittancerange ADD CONSTRAINT cutin_unitpair
+    CHECK (cutin IS NULL AND cutinunit IS NULL
+        OR cutin IS NOT NULL AND cutinunit IS NOT NULL);
+
+ALTER TABLE transmittancerange ADD CONSTRAINT cutouttolerance_unitpair
+    CHECK (cutouttolerance IS NULL AND cutouttoleranceunit IS NULL
+        OR cutouttolerance IS NOT NULL AND cutouttoleranceunit IS NOT NULL);
+
+ALTER TABLE transmittancerange ADD CONSTRAINT cutout_unitpair
+    CHECK (cutout IS NULL AND cutoutunit IS NULL
+        OR cutout IS NOT NULL AND cutoutunit IS NOT NULL);
+
+ALTER TABLE wellsample ADD CONSTRAINT posx_unitpair
+    CHECK (posx IS NULL AND posxunit IS NULL
+        OR posx IS NOT NULL AND posxunit IS NOT NULL);
+
+ALTER TABLE wellsample ADD CONSTRAINT posy_unitpair
+    CHECK (posy IS NULL AND posyunit IS NULL
+        OR posy IS NOT NULL AND posyunit IS NOT NULL);
+
 
 --
 -- FINISHED
