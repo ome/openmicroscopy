e3b0113d47d67ad1085f997efd7109aa0f03cf5d
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-01-29 11:54:29 +0000

    Update header.

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/Connector.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/Connector.java
@@ -2,7 +2,7 @@
  * org.openmicroscopy.shoola.env.data.Connector 
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2013 University of Dundee & Open Microscopy Environment.
+ *  Copyright (C) 2006-2015 University of Dundee & Open Microscopy Environment.
  *  All rights reserved.
  *
  *
