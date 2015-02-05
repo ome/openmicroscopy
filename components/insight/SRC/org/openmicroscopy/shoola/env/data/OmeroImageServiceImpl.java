9e24ddf49dd3edbee02f4fded68631424b4ce45f
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-02-05 12:59:43 +0000

    Avoid call if no object to save.

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/OmeroImageServiceImpl.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/OmeroImageServiceImpl.java
@@ -1015,7 +1015,9 @@ class OmeroImageServiceImpl
 			}
 			//save the tag.
 			try {
-				l = gateway.saveAndReturnObject(ctx, l, parameters, userName);
+			    if (l.size() > 0) {
+			        l = gateway.saveAndReturnObject(ctx, l, parameters, userName);
+			    }
 				Iterator<IObject> j = l.iterator();
 				Annotation a;
 				while (j.hasNext()) {
