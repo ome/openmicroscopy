62de428adce2b8f0b65b9f8bb142fe946470cb73
Colin Blackburn <colin@ximenes.org.uk>
2015-02-27 15:51:50 +0000

    Suppress stacktrace on error.

--- components/tools/OmeroPy/src/omero/plugins/fs.py
+++ components/tools/OmeroPy/src/omero/plugins/fs.py
@@ -796,7 +796,7 @@ Examples:
         """
         err = self.get_error(rsp)
         if err:
-            self.ctx.err(err)
+            self.ctx.err("Error: " + rsp.parameters['message'])
         else:
             size = sum(rsp.totalBytesUsed.values())
             if args.size_only:
