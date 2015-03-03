4bc0db01f06deef46a2b649d522ad8b353c2bbf3
Will Moore <w.moore@dundee.ac.uk>
2015-03-03 14:03:43 +0000

    Fully test webgateway/imgData for 'tinyTest.d3d.dv'

--- components/tools/OmeroWeb/test/integration/test_marshal.py
+++ components/tools/OmeroWeb/test/integration/test_marshal.py
@@ -95,15 +95,26 @@ class TestImgDetail(object):
         """
         Download of archived files for a non-SPW Image.
         """
+        admin = client.sf.getAdminService()
+        user = admin.getExperimenter(admin.getEventContext().userId)
+        userName = "%s %s" % (user.getFirstName().val, user.getLastName().val)
 
-        image = itest.importSingleImage(client=client)
+        # Import "tinyTest.d3d.dv" and get ImageID
+        pids = itest.import_image(client=client)
+        pixels = client.getSession().getQueryService().get("Pixels",
+                                                           long(pids[0]))
+        iid = pixels.image.id.val
 
-        json_url = reverse('webgateway.views.imageData_json', args=[image.id.val])
+        json_url = reverse('webgateway.views.imageData_json', args=[iid])
         data = {}
-        imgData = _get_response_json(django_client, json_url, data, status_code=200)
+        imgData = _get_response_json(django_client, json_url,
+                                     data, status_code=200)
+
+        # Useful for debugging
         print imgData
+
         # Not a big image - tiles should be False with no other tiles metadata
-        assert imgData['tiles'] == False
+        assert imgData['tiles'] is False
         assert 'levels' not in imgData
         assert 'zoomLevelScaling' not in imgData
         assert 'tile_size' not in imgData
@@ -111,35 +122,77 @@ class TestImgDetail(object):
         # Channels metadata
         assert len(imgData['channels']) == 1
         assert imgData['channels'][0] == {
-                                    'color': "808080",
-                                    'active': True,
-                                    'window': {
-                                        'max': 255,
-                                        'end': 255,
-                                        'start': 0,
-                                        'min': 0
-                                    },
-                                    'emissionWave': None,
-                                    'label': "0"
-                                }
-        assert imgData['pixel_range'] == [0, 255]
+            'color': "808080",
+            'active': True,
+            'window': {
+                'max': 651,
+                'end': 651,
+                'start': 88,
+                'min': 88
+            },
+            'emissionWave': 500,
+            'label': "500"
+        }
+        assert imgData['pixel_range'] == [-32768, 32767]
+        assert imgData['nominalMagnification'] == 100
         assert imgData['rdefs'] == {
-                            'defaultT': 0,
-                            'model': "greyscale",
-                            'invertAxis': False,
-                            'projection': "normal",
-                            'defaultZ': 0
-                        }
+            'defaultT': 0,
+            'model': "greyscale",
+            'invertAxis': False,
+            'projection': "normal",
+            'defaultZ': 2
+        }
 
         # Core image metadata
         assert imgData['size'] == {
-                                    'width': 512,
-                                    'c': 1,
-                                    'z': 1,
-                                    't': 1,
-                                    'height': 512
-                                }
-        assert imgData['meta']['pixelsType'] == "uint8"
+            'width': 20,
+            'c': 1,
+            'z': 5,
+            't': 6,
+            'height': 20
+        }
+        assert imgData['meta']['pixelsType'] == "int16"
+        assert imgData['meta']['projectName'] == "Multiple"
+        assert imgData['meta']['imageId'] == iid
+        assert imgData['meta']['imageAuthor'] == userName
+        assert imgData['meta']['datasetId'] is None
+        assert imgData['meta']['projectDescription'] == ""
+        assert imgData['meta']['datasetName'] == "Multiple"
+        assert imgData['meta']['wellSampleId'] == ""
+        assert imgData['meta']['projectId'] is None
+        assert imgData['meta']['imageDescription'] == \
+            "X:(88 107) Y:(169 188) Z:(9 13 1) T:(2 7 1)"
+        assert imgData['meta']['wellId'] == ""
+        assert imgData['meta']['imageName'] == "tinyTest.d3d.dv"
+        assert imgData['meta']['datasetDescription'] == ""
+        # Don't know exact timestamp of import
+        assert 'imageTimestamp' in imgData['meta']
+
+        # Permissions - User is owner. All perms are True
+        assert imgData['perms'] == {
+            'canAnnotate': True,
+            'canEdit': True,
+            'canDelete': True,
+            'canLink': True
+        }
+
+        # Sizes for split channel image
+        assert imgData['split_channel'] == {
+            'c': {
+                'width': 46,
+                'gridy': 1,
+                'border': 2,
+                'gridx': 2,
+                'height': 24
+            },
+            'g': {
+                'width': 24,
+                'gridy': 1,
+                'border': 2,
+                'gridx': 1,
+                'height': 24
+            }
+        }
 
 
 # Helpers
