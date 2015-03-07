6548814bba38e10859d0038b4a2af6f8f7c7295b
Sebastien Besson <seb.besson@gmail.com>
2015-03-07 16:03:09 +0000

    Update testSimple to document new attributes

--- components/tools/OmeroPy/test/integration/test_simple.py
+++ components/tools/OmeroPy/test/integration/test_simple.py
@@ -15,7 +15,13 @@ import library as lib
 
 class TestSimple(lib.ITest):
 
-    def testCurrentUser(self):
-        admin = self.client.sf.getAdminService()
-        ec = admin.getEventContext()
-        assert ec
+    DEFAULT_PERMS = 'rwra--'  # Override DEFAULT_PERMS of ITest
+
+    def testUserId(self):
+        assert self.ctx.userId == self.user.id.val
+
+    def testGroupId(self):
+        assert self.ctx.groupId == self.group.id.val
+
+    def testGroupPermissions(self):
+        assert str(self.group.details.permissions) == self.DEFAULT_PERMS
