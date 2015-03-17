3316624a792dc30837e33b65918d3f3de0779668
Sebastien Besson <seb.besson@gmail.com>
2015-03-17 20:13:57 +0000

    Fix indentation

--- components/blitz/src/ome/formats/importer/ImportConfig.java
+++ components/blitz/src/ome/formats/importer/ImportConfig.java
@@ -333,7 +333,7 @@ public class ImportConfig {
          ((ch.qos.logback.classic.Logger)LoggerFactory.getLogger("ome.formats")).setLevel(level);
          ((ch.qos.logback.classic.Logger)LoggerFactory.getLogger("ome.services.blitz")).setLevel(level);
          ((ch.qos.logback.classic.Logger)LoggerFactory.getLogger("ome.system")).setLevel(level);
-		          ((ch.qos.logback.classic.Logger)LoggerFactory.getLogger("loci")).setLevel(level);
+         ((ch.qos.logback.classic.Logger)LoggerFactory.getLogger("loci")).setLevel(level);
      }
 
     //
