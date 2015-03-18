ecde22d8a8b3a30ca24ed1c0e50a6625c6604723
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-03-18 14:38:37 +0000

    Review code to parse catalog.

--- components/insight/SRC/org/openmicroscopy/shoola/env/data/util/TransformsParser.java
+++ components/insight/SRC/org/openmicroscopy/shoola/env/data/util/TransformsParser.java
@@ -2,7 +2,7 @@
  * org.openmicroscopy.shoola.env.data.util.TransformsParser 
  *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2012 University of Dundee & Open Microscopy Environment.
+ *  Copyright (C) 2006-2015 University of Dundee & Open Microscopy Environment.
  *  All rights reserved.
  *
  *
@@ -24,7 +24,6 @@
 package org.openmicroscopy.shoola.env.data.util;
 
 
-//Java imports
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -34,15 +33,12 @@ import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-import org.openmicroscopy.shoola.util.file.IOUtil;
+import org.openmicroscopy.shoola.util.CommonsLangUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-//Third-party libraries
-
-//Application-internal dependencies
 
 /** 
  * Parses the specification file. This class retrieves the style sheets used
@@ -55,30 +51,30 @@ import org.w3c.dom.NodeList;
 public class TransformsParser
 {
 
-	/** The jar to find. */
-	public static String SPECIFICATION = "specification";
-	
-	/** The catalog file to find. */
-	private static String CATALOG = "ome-transforms.xml";
+    /** The jar to find. */
+    public static String SPECIFICATION = "specification";
 
-	/** The <i>name</i> attribute. */
-	private static String CURRENT = "current";
+    /** The catalog file to find. */
+    private static String CATALOG = "/transforms/ome-transforms.xml";
 
-	/** The <i>source</i> node. */
-	private static String SOURCE = "source";
-	
-	/** The configuration file. */
+    /** The <i>name</i> attribute. */
+    private static String CURRENT = "current";
+
+    /** The <i>source</i> node. */
+    private static String SOURCE = "source";
+
+    /** The configuration file. */
     private Document document;
-    
+
     /** The possible downgrade schema.*/
     private List<Target> targets;
-    
+
     /** The jar to load.*/
     private Map<String, InputStream> values;
-    
+
     /** The current schema.*/
     private String current;
-    
+
     /**
      * Extracts the value of the current schema.
      * 
@@ -86,108 +82,91 @@ public class TransformsParser
      * @throws Exception Thrown when an error occurred while parsing the file.
      */
     private void extractCurrentSchema(String schema)
-		throws Exception
-	{
-		NodeList list = document.getElementsByTagName(SOURCE);
-		Element n;
-		Node attribute;
-		NamedNodeMap map;
-		Target target;
-		NodeList t;
-		for (int i = 0; i < list.getLength(); ++i) {
-			n = (Element) list.item(i);
-			map = n.getAttributes();
-			for (int j = 0; j < map.getLength(); j++) {
-				attribute = map.item(j);
-				if (Target.SCHEMA.equals(attribute.getNodeName())) {
-					if (schema.equals(attribute.getNodeValue())) {
-						t = n.getElementsByTagName(Target.TARGET);
-						for (int k = 0; k < t.getLength(); k++) {
-							target = new Target((Element) t.item(k));
-							target.parse();
-							target.formatTransforms(values);
-							targets.add(target);
-						}
-					}
-				}
-			}
-		}
-	}
-    
-	/**
-	 * Creates a new instance.
-	 */
+            throws Exception
+    {
+        NodeList list = document.getElementsByTagName(SOURCE);
+        Element n;
+        Node attribute;
+        NamedNodeMap map;
+        Target target;
+        NodeList t;
+        for (int i = 0; i < list.getLength(); ++i) {
+            n = (Element) list.item(i);
+            map = n.getAttributes();
+            for (int j = 0; j < map.getLength(); j++) {
+                attribute = map.item(j);
+                if (Target.SCHEMA.equals(attribute.getNodeName())) {
+                    if (schema.equals(attribute.getNodeValue())) {
+                        t = n.getElementsByTagName(Target.TARGET);
+                        for (int k = 0; k < t.getLength(); k++) {
+                            target = new Target((Element) t.item(k));
+                            target.parse();
+                            target.formatTransforms(values);
+                            targets.add(target);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Creates a new instance.
+     */
     public TransformsParser()
-	{
-		targets = new ArrayList<Target>();
-	}
-	
+    {
+        targets = new ArrayList<Target>();
+    }
+
     /** Closes the input stream.*/
     public void close()
     {
-    	Iterator<Target> i = targets.iterator();
-    	while (i.hasNext()) {
-			i.next().close();
-		}
+        Iterator<Target> i = targets.iterator();
+        while (i.hasNext()) {
+            i.next().close();
+        }
     }
-    
+
     /**
      * Returns the collection of targets.
      * 
      * @return See above.
      */
     public List<Target> getTargets() { return targets; }
-    
+
     /**
      * Returns the value of the current schema.
      * 
      * @return See above.
      */
     public String getCurrentSchema() { return current; }
-    
-	/**
-	 * Parses the catalog.
-	 * 
-	 * @param path The relative path.
-	 * @throws Exception Thrown when an error occurred while parsing the file.
-	 */
-	public void parse(String path)
-		throws Exception
-	{
-		if (values == null || values.size() == 0)
-			values = IOUtil.extractJarFromPath(SPECIFICATION);
-		if (values.size() == 0) {
-			//going to extract from libs.
-			values = IOUtil.extractJarFromPath(path);
-		}
-		
-		if (values == null || values.size() == 0)
-    		throw new Exception("Unable to load the jar");
-		//Extract catalog.
-		Iterator<String> i = values.keySet().iterator();
-		String key;
-		InputStream stream = null;
-		while (i.hasNext()) {
-			key = i.next();
-			if (key.contains(CATALOG)) {
-				stream = values.get(key);
-				break;
-			}
-		}
-		if (stream == null)
-    		throw new Exception("No Catalog found.");
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		try {
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			document = builder.parse(stream);
-			
-			current = document.getDocumentElement().getAttribute(CURRENT);
-			if (current == null || current.trim().length() == 0)
-				throw new Exception("No schema specified.");
-			extractCurrentSchema(current);
-		} catch (Exception e) {
-			throw new Exception("Unable to parse the catalog.", e);
-		}
-	}
+
+    /**
+     * Parses the catalog.
+     * 
+     * @param path The relative path.
+     * @throws Exception Thrown when an error occurred while parsing the file.
+     */
+    public void parse(String path)
+            throws Exception
+    {
+        InputStream stream = this.getClass().getResourceAsStream(CATALOG);
+        if (stream == null)
+            throw new Exception("No Catalog found.");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            document = builder.parse(stream);
+
+            current = document.getDocumentElement().getAttribute(CURRENT);
+            if (CommonsLangUtils.isBlank(current))
+                throw new Exception("No schema specified.");
+            extractCurrentSchema(current);
+        } catch (Exception e) {
+            throw new Exception("Unable to parse the catalog.", e);
+        } finally {
+            stream.close();
+        }
+    }
 
 }
