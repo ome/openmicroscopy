c8205090e45a6283c73a6f447c4969af861a795c
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2015-03-18 16:15:25 +0000

    have C++ simple integration tests use new graph requests

--- components/tools/OmeroCpp/test/integration/chgrp.cpp
+++ components/tools/OmeroCpp/test/integration/chgrp.cpp
@@ -16,6 +16,7 @@ using namespace std;
 using namespace omero;
 using namespace omero::api;
 using namespace omero::cmd;
+using namespace omero::cmd::graphs;
 using namespace omero::callbacks;
 using namespace omero::model;
 using namespace omero::rtypes;
@@ -41,14 +42,17 @@ TEST(ChgrpTest, testSimpleChgrp ) {
     image->setName( rstring("testSimpleChgrp") );
     image = ImagePtr::dynamicCast( iupdate->saveAndReturnObject( image ) );
 
-    std::map<string, string> options;
-    ChgrpPtr chgrp = new Chgrp();
-    chgrp->type = "/Image";
-    chgrp->id = image->getId()->getValue();
-    chgrp->grp = g2->getId()->getValue();
-    chgrp->options = options;
+    LongList imageIds;
+    StringLongListMap objects;
+    ChildOptions options;
+    Chgrp2Ptr chgrpCmd = new Chgrp2();
+    imageIds.push_back( image->getId()->getValue() );
+    objects["Image"] = imageIds;
+    chgrpCmd->targetObjects = objects;
+    chgrpCmd->groupId = g2->getId()->getValue();
+    chgrpCmd->childOptions = options;
 
-    HandlePrx handle = sf->submit( chgrp );
+    HandlePrx handle = sf->submit( chgrpCmd );
     CmdCallbackIPtr cb = new CmdCallbackI(f.client, handle);
     ResponsePtr rsp = cb->loop(10, 500);
     ERRPtr err = ERRPtr::dynamicCast(rsp);
