cd6fe78c0af4cabd759bcfa674af921b2a5f421e
Sebastien Besson <seb.besson@gmail.com>
2015-03-21 09:05:20 +0000

    Refactor test_cliimport

--- components/tools/OmeroPy/test/integration/test_cliimport.py
+++ components/tools/OmeroPy/test/integration/test_cliimport.py
@@ -29,26 +29,9 @@ import pytest
 import omero
 
 from omero.rtypes import rstring
-from omero.util.temp_files import create_path
-
-try:
-    from PIL import Image  # see ticket:2597
-except ImportError:
-    import Image  # see ticket:2597
 
 
-class Fixture(object):
-
-    def __init__(self, client, user):
-        self.client = client
-        self.user = user
-        self.admin = self.client.sf.getAdminService()
-        self.query = self.client.sf.getQueryService()
-        self.update = self.client.sf.getUpdateService()
-        self.context = self.admin.getEventContext()
-        self.img = create_path("cliimportfixture.", ".png")
-        i = Image.new(mode="1", size=[8, 8])
-        i.save(self.img)
+class TestCliImport(lib.ITest):
 
     def dataset(self, name, ctx=None):
         dataset = omero.model.DatasetI()
@@ -72,96 +55,80 @@ class Fixture(object):
             join i.pixels p
             where p.id = %s""" % pix_ids[0], None)
 
-
-class TestCliImport(lib.ITest):
-
     def assertGroup(self, group, *objects):
         for obj in objects:
             assert group == obj.details.group.id.val
 
-    def cliimport(self, fixture, *extra_args):
+    def cliimport(self, *extra_args):
         extra_args = [str(x) for x in extra_args]
-        pix = self.import_image(
-            filename=fixture.img, client=fixture.client, extra_args=extra_args)
+        pix = self.import_image(extra_args=extra_args)
         if not pix:
             raise Exception("No pixels found!")
         return pix
 
     def testBasic(self):
-        fixture = Fixture(*self.new_client_and_user())
-        self.cliimport(fixture)
+        self.cliimport()
 
     def testDatasetTarget(self):
-        fixture = Fixture(*self.new_client_and_user())
-        dataset = fixture.dataset("testDatasetTarget")
-        pix = self.cliimport(fixture, "-d", dataset.id.val)
-        pix = fixture.load_pixels(pix)
-        self.assertGroup(fixture.context.groupId, dataset, pix)
+        dataset = self.dataset("testDatasetTarget")
+        pix = self.cliimport("-d", dataset.id.val)
+        pix = self.load_pixels(pix)
+        self.assertGroup(self.group.id.val, dataset, pix)
 
     @pytest.mark.broken(ticket="11539")
     def testTargetInDifferentGroup(self):
-        fixture = Fixture(*self.new_client_and_user())
-        group = self.new_group(experimenters=[fixture.user])
-        fixture.admin.getEventContext()  # Refresh
-        dataset = fixture.dataset(
-            "testTargetInDifferentGroup", {"omero.group": str(group.id.val)})
+        new_group = self.new_group(experimenters=[self.user])
+        self.sf.getAdminService().getEventContext()  # Refresh
+        dataset = self.dataset(
+            "testTargetInDifferentGroup",
+            {"omero.group": str(new_group.id.val)})
         with pytest.raises(omero.SecurityViolation):
-            fixture.query.find("Dataset", dataset.id.val)
+            self.query.find("Dataset", dataset.id.val)
 
-        self.assertGroup(group.id.val, dataset)
-        pix = self.cliimport(fixture, "-d", dataset.id.val)
+        self.assertGroup(new_group.id.val, dataset)
+        pix = self.cliimport("-d", dataset.id.val)
 
-        fixture.client.sf.setSecurityContext(group)
-        pix = fixture.load_pixels(pix)
-        self.assertGroup(group.id.val, pix)
+        self.sf.setSecurityContext(new_group)
+        pix = self.load_pixels(pix)
+        self.assertGroup(new_group.id.val, pix)
 
     def testAnnotationTextSimple(self):
-        fixture = Fixture(*self.new_client_and_user())
-        pix = self.cliimport(
-            fixture, "--annotation_ns=test", "--annotation_text=test")
-        ann = fixture.load_pixel_annotations(pix)
+        pix = self.cliimport("--annotation_ns=test", "--annotation_text=test")
+        ann = self.load_pixel_annotations(pix)
         assert 1 == len(ann)
         assert "test" == ann[0].ns.val
         assert "test" == ann[0].textValue.val
 
     def testAnnotationTextMultiple(self):
-        fixture = Fixture(*self.new_client_and_user())
         pix = self.cliimport(
-            fixture, "--annotation_ns=test", "--annotation_text=test",
+            "--annotation_ns=test", "--annotation_text=test",
             "--annotation_ns=test", "--annotation_text=test")
-        ann = fixture.load_pixel_annotations(pix)
+        ann = self.load_pixel_annotations(pix)
         assert 2 == len(ann)
         for x in ann:
             assert "test" == x.ns.val
             assert "test" == x.textValue.val
 
     def testAnnotationComment(self):
-        fixture = Fixture(*self.new_client_and_user())
         comment = omero.model.CommentAnnotationI()
         comment.ns = rstring("test")
         comment.textValue = rstring("test")
-        comment = fixture.update.saveAndReturnObject(comment)
-        pix = self.cliimport(
-            fixture, "--annotation_link=%s" % comment.id.val)
-        ann = fixture.load_pixel_annotations(pix)
+        comment = self.update.saveAndReturnObject(comment)
+        pix = self.cliimport("--annotation_link=%s" % comment.id.val)
+        ann = self.load_pixel_annotations(pix)
         assert 1 == len(ann)
         assert "test" == ann[0].ns.val
         assert "test" == ann[0].textValue.val
 
     def testAutoClose(self):
-        fixture = Fixture(*self.new_client_and_user())
-
-        pix = self.import_image(
-            filename=fixture.img,
-            client=fixture.client,
-            extra_args=["--auto_close"])
+        pix = self.import_image(extra_args=["--auto_close"])
 
         assert len(pix) == 0
 
         # Check that there are no servants leftover
         stateful = []
         for x in range(10):
-            stateful = fixture.client.getStatefulServices()
+            stateful = self.client.getStatefulServices()
             if stateful:
                 import time
                 time.sleep(0.5)  # Give the backend some time to close
