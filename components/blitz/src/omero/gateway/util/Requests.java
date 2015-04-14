021a0b8926a6fce1d3862964615fa449c5dfaae1
Dominik Lindner <d.lindner@dundee.ac.uk>
2015-04-14 10:27:41 +0100

    Moved Requests.java to blitz

--- components/blitz/src/omero/gateway/util/Requests.java
+++ components/blitz/src/omero/gateway/util/Requests.java
@@ -17,7 +17,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-package org.openmicroscopy.shoola.env.data;
+package omero.gateway.util;
 
 import java.util.Collections;
 import java.util.HashMap;
