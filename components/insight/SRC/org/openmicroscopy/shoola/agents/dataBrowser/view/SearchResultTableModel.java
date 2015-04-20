cabdbc389030d944db7f9d2e9403cede2f15d097
Dominik Lindner <d.lindner@dundee.ac.uk>
2015-04-20 13:52:12 +0100

    Fixed problem with search result sorting

--- components/insight/SRC/org/openmicroscopy/shoola/agents/dataBrowser/view/SearchResultTableModel.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/dataBrowser/view/SearchResultTableModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 University of Dundee & Open Microscopy Environment.
+ * Copyright (C) 2014-2015 University of Dundee & Open Microscopy Environment.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -128,7 +128,7 @@ public class SearchResultTableModel extends DefaultTableModel {
                 break;
         }
 
-        return result != null ? result : "--";
+        return result;
     }
 
     /**
