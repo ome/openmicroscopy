0b0176850c246cdfc2097c1a7ded93de0236c9bc
jmoore <josh@glencoesoftware.com>
2015-04-21 13:58:18 +0200

    Limit ServiceHandler string logging (Fix #12820)
    Some string arguments like figure JSON files were
    being printed in their entirety to Blitz-0.log.
    This limits those strings to a max length of 100.

--- components/server/test/ome/server/utests/ServiceHandlerUnitTest.java
+++ components/server/test/ome/server/utests/ServiceHandlerUnitTest.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
-import ome.security.basic.CurrentDetails;
 import ome.services.util.ServiceHandler;
 
 import org.jmock.MockObjectTestCase;
@@ -23,45 +22,43 @@ public class ServiceHandlerUnitTest extends MockObjectTestCase {
         
         String str;
         
-        ServiceHandler sh = new ServiceHandler(new CurrentDetails());
-
         // Plain
         
-        str = sh.getResultsString(null, null);
+        str = ServiceHandler.getResultsString(null, null);
         assertEquals("null", str);
         
-        str = sh.getResultsString(1L, null);
+        str = ServiceHandler.getResultsString(1L, null);
         assertEquals("1", str);
 
         // Arrays
         
-        str = sh.getResultsString(new Object[]{null}, null);
+        str = ServiceHandler.getResultsString(new Object[]{null}, null);
         assertEquals("[null]", str);
         
-        str = sh.getResultsString(new Long[]{1L}, null);
+        str = ServiceHandler.getResultsString(new Long[]{1L}, null);
         assertEquals("[1]", str);
 
-        str = sh.getResultsString(new Long[]{1L,1L,1L,1L}, null);
+        str = ServiceHandler.getResultsString(new Long[]{1L,1L,1L,1L}, null);
         assertEquals("[1, 1, 1, ... 1 more]", str);
         
         // Lists
 
-        str = sh.getResultsString(Arrays.asList(1L), null);
+        str = ServiceHandler.getResultsString(Arrays.asList(1L), null);
         assertEquals("(1)", str);
  
-        str = sh.getResultsString(Arrays.asList(new Object[]{null}), null);
+        str = ServiceHandler.getResultsString(Arrays.asList(new Object[]{null}), null);
         assertEquals("(null)", str);
  
         // Sets
         
-        str = sh.getResultsString(new HashSet(Arrays.asList(1L)), null);
+        str = ServiceHandler.getResultsString(new HashSet<Long>(Arrays.asList(1L)), null);
         assertEquals("(1)", str);
 
         // Maps
         
-        HashMap map = new HashMap();
+        HashMap<String, String> map = new HashMap<String, String>();
         map.put("a","b");
-        str = sh.getResultsString(map, null);
+        str = ServiceHandler.getResultsString(map, null);
         assertEquals("{a=b}", str);
 
         // Arrays of arrays
@@ -69,7 +66,21 @@ public class ServiceHandlerUnitTest extends MockObjectTestCase {
         objs[0] = new Object[]{1,2,3};
         objs[1] = new Object[]{4,5,6};
         objs[2] = new Object[]{7,8,9};
-        str = sh.getResultsString(objs, null);
+        str = ServiceHandler.getResultsString(objs, null);
         assertEquals("[[1, 2, 3], [4, 5, 6], [7, 8, 9]]", str);
+    }
+
+    @Test
+    public void testLongStringOutput() throws Exception {
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 1000; i++) {
+            sb.append("0123456789");
+        }
+
+        String rv = ServiceHandler.getResultsString(sb.toString(), null);
+        assertEquals(sb.toString().substring(0, 100), rv);
+        rv = ServiceHandler.getResultsString(Arrays.asList(sb.toString()), null);
+        assertEquals("(" + sb.toString().substring(0, 100) + ")", rv);
     }
 }
