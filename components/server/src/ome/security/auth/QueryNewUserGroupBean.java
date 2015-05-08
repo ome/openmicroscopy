b62b07db0ee6b5cab0ae1f94a9b8512147ba5cba
jmoore <josh@glencoesoftware.com>
2015-05-08 11:57:51 +0200

    LDAP-support for group-owners
    A new property `new_user_group_owner` has been
    added which is used by `NewUserGroupOwnerBean`
    implementations to query for groups that some
    user should be considered an owner of. If the
    sync_on_login property is set and the user is
    loses the *owner* attribute, then the flag in
    OMERO will also be removed.
    
    Currently, only supported for `:query:` based
    groups.

--- components/server/src/ome/security/auth/QueryNewUserGroupBean.java
+++ components/server/src/ome/security/auth/QueryNewUserGroupBean.java
@@ -8,16 +8,20 @@
 package ome.security.auth;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import ome.conditions.ValidationException;
 import ome.security.SecuritySystem;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.ldap.core.LdapOperations;
 import org.springframework.ldap.filter.AndFilter;
 import org.springframework.ldap.filter.HardcodedFilter;
 import org.springframework.util.PropertyPlaceholderHelper;
 import org.springframework.util.PropertyPlaceholderHelper.PlaceholderResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Handles the ":query:" specifier from etc/omero.properties.
@@ -30,7 +34,9 @@ import org.springframework.util.PropertyPlaceholderHelper.PlaceholderResolver;
  * @see SecuritySystem
  * @since Beta4.2
  */
-public class QueryNewUserGroupBean implements NewUserGroupBean {
+public class QueryNewUserGroupBean implements NewUserGroupBean, NewUserGroupOwnerBean {
+
+    private final static Logger log = LoggerFactory.getLogger(QueryNewUserGroupBean.class);
 
     private final String grpQuery;
 
@@ -38,29 +44,46 @@ public class QueryNewUserGroupBean implements NewUserGroupBean {
         this.grpQuery = grpQuery;
     }
 
-    @SuppressWarnings("unchecked")
-    public List<Long> groups(String username, LdapConfig config,
-            LdapOperations ldap, RoleProvider provider,
-            final AttributeSet attrSet) {
-
+    private String parseQuery(final AttributeSet attrSet, final String query) {
         PropertyPlaceholderHelper helper = new PropertyPlaceholderHelper("@{",
                 "}", null, false);
-        String query = helper.replacePlaceholders(grpQuery,
+        return helper.replacePlaceholders(query,
                 new PlaceholderResolver() {
                     public String resolvePlaceholder(String arg0) {
                         if (attrSet.size(arg0) > 1) {
                             throw new ValidationException(
                                     "Multivalued property used in @{} format:"
-                                            + grpQuery + "="
+                                            + query + "="
                                             + attrSet.getAll(arg0).toString());
                         }
                         return attrSet.getFirst(arg0);
                     }
                 });
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Long> _groups(boolean owner, String username, LdapConfig config,
+            LdapOperations ldap, RoleProvider provider, final AttributeSet attrSet) {
+
+        String query = parseQuery(attrSet, grpQuery);
+        String ownerQuery = null;
+        if (owner) {
+            ownerQuery = config.getNewUserGroupOwner();
+            if (StringUtils.isBlank(ownerQuery)) {
+                log.debug("Owner query disabled");
+                return Collections.emptyList(); // EARLY EXIT
+            }
+            ownerQuery = parseQuery(attrSet, ownerQuery);
+        }
+
         AndFilter and = new AndFilter();
         and.and(config.getGroupFilter());
-
         and.and(new HardcodedFilter(query));
+        if (owner) {
+            and.and(new HardcodedFilter(ownerQuery));
+        }
+
+        log.debug("Running query: {}", and.encode());
         List<String> groupNames = (List<String>) ldap.search("", and.encode(),
             new GroupAttributeMapper(config));
 
@@ -72,4 +95,16 @@ public class QueryNewUserGroupBean implements NewUserGroupBean {
 
     }
 
+    @Override
+    public List<Long> groups(String username, LdapConfig config,
+            LdapOperations ldap, RoleProvider provider,
+            AttributeSet attrSet) {
+        return _groups(false, username, config, ldap, provider, attrSet);
+    }
+
+    @Override
+    public List<Long> ownerOfGroups(String username, LdapConfig config,
+            LdapOperations ldap, RoleProvider provider, AttributeSet attrSet) {
+        return _groups(true, username, config, ldap, provider, attrSet);
+    }
 }
