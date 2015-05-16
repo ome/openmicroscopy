f42a6996499b8dc7c12dc7f713d8d6c49d761156
Josh Ballanco <jballanc@glencoesoftware.com>
2015-05-16 05:24:11 +0300

    Add integration tests for getChannelLabels

--- components/tools/OmeroPy/test/integration/gatewaytest/test_image_wrapper.py
+++ components/tools/OmeroPy/test/integration/gatewaytest/test_image_wrapper.py
@@ -14,10 +14,31 @@
 """
 
 import pytest
+from library import ITest
 
-from omero.model import ImageI
+from omero.model import ImageI, ChannelI, LogicalChannelI, LengthI
 from omero.rtypes import rstring, rtime
 from datetime import datetime
+
+
+@pytest.fixture(scope='module')
+def itest(request):
+    """
+    Returns a new L{library.ITest} instance. With attached
+    finalizer so that pytest will clean it up.
+    """
+    class ImageWrapperITest(ITest):
+        """
+        This class emulates py.test scoping semantics when the xunit style
+        is in use.
+        """
+        pass
+    ImageWrapperITest.setup_class()
+
+    def finalizer():
+        ImageWrapperITest.teardown_class()
+    request.addfinalizer(finalizer)
+    return ImageWrapperITest()
 
 
 @pytest.fixture()
@@ -47,6 +68,47 @@ def image_no_acquisition_date(request, gatewaywrapper):
     return gw.getObject('Image', image_id)
 
 
+@pytest.fixture()
+def image_channel_factory(itest, gatewaywrapper):
+
+    def make_image_channel(channel):
+        gatewaywrapper.loginAsAuthor()
+        gw = gatewaywrapper.gateway
+        update_service = gw.getUpdateService()
+        pixels = itest.pix(client=gw.c)
+        pixels.addChannel(channel)
+        pixels = update_service.saveAndReturnObject(pixels)
+        return gw.getObject('Image', pixels.image.id)
+    return make_image_channel
+
+
+
+@pytest.fixture()
+def labeled_channel(image_channel_factory):
+    channel = ChannelI()
+    lchannel = LogicalChannelI()
+    lchannel.name = rstring('a channel')
+    channel.logicalChannel = lchannel
+    return image_channel_factory(channel)
+
+
+@pytest.fixture()
+def emissionWave_channel(image_channel_factory):
+    channel = ChannelI()
+    lchannel = LogicalChannelI()
+    lchannel.emissionWave = LengthI(123.0, 'NANOMETER')
+    channel.logicalChannel = lchannel
+    return image_channel_factory(channel)
+
+
+@pytest.fixture()
+def unlabeled_channel(image_channel_factory):
+    channel = ChannelI()
+    lchannel = LogicalChannelI()
+    channel.logicalChannel = lchannel
+    return image_channel_factory(channel)
+
+
 class TestImageWrapper(object):
 
     def testGetDate(self, gatewaywrapper, image):
@@ -69,3 +131,33 @@ class TestImageWrapper(object):
             'id': image.getId(),
             'name': 'an image'
         }
+
+    def testChannelLabel(self, labeled_channel):
+        labels = labeled_channel.getChannelLabels()
+        channels = labeled_channel.getChannels()
+        assert labels
+        assert channels
+        assert len(labels) == len(channels)
+
+        for channel in channels:
+            assert channel.getLabel() in labels
+
+    def testChannelEmissionWaveLabel(self, emissionWave_channel):
+        labels = emissionWave_channel.getChannelLabels()
+        channels = emissionWave_channel.getChannels()
+        assert labels
+        assert channels
+        assert len(labels) == len(channels)
+
+        for channel in channels:
+            assert channel.getLabel() in labels
+
+    def testChannelNoLabel(self, unlabeled_channel):
+        labels = unlabeled_channel.getChannelLabels()
+        channels = unlabeled_channel.getChannels()
+        assert labels
+        assert channels
+        assert len(labels) == len(channels)
+
+        for channel in channels:
+            assert channel.getLabel() in labels
