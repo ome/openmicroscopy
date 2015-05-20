79b73215261a01dc5e42b3a87c58bab0d0a69007
jmoore <josh@glencoesoftware.com>
2015-05-20 11:11:42 +0200

    Fix conversions
    The `Map<Units, Conversion>` getter was taking a String
    rather than an Enum which led to nulls always being
    returned. Since UnitTest wasn't being run, this has gone
    undetected for **far** too long. Enabling.

--- components/blitz/src/omero/model/FrequencyI.java
+++ components/blitz/src/omero/model/FrequencyI.java
@@ -777,7 +777,7 @@ public class FrequencyI extends Frequency implements ModelBased {
            setUnit(value.getUnit());
         } else {
             UnitsFrequency targetUnit = UnitsFrequency.valueOf(target);
-            Conversion conversion = conversions.get(value.getUnit()).get(target);
+            Conversion conversion = conversions.get(value.getUnit()).get(targetUnit);
             if (conversion == null) {
                 throw new RuntimeException(String.format(
                     "%f %s cannot be converted to %s",
--- components/blitz/src/omero/model/LengthI.java
+++ components/blitz/src/omero/model/LengthI.java
@@ -1439,7 +1439,7 @@ public class LengthI extends Length implements ModelBased {
            setUnit(value.getUnit());
         } else {
             UnitsLength targetUnit = UnitsLength.valueOf(target);
-            Conversion conversion = conversions.get(value.getUnit()).get(target);
+            Conversion conversion = conversions.get(value.getUnit()).get(targetUnit);
             if (conversion == null) {
                 throw new RuntimeException(String.format(
                     "%f %s cannot be converted to %s",
