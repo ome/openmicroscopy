8708075b043fe4e75b23d423c47d34aada29bbef
jmoore <josh@glencoesoftware.com>
2015-06-01 12:33:09 +0200

    tests: work on unit and integration tests

--- components/blitz/test/ome/formats/utests/TargetsTest.java
+++ components/blitz/test/ome/formats/utests/TargetsTest.java
@@ -21,6 +21,10 @@ package ome.formats.utests;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import ome.formats.OMEROMetadataStoreClient;
 import ome.formats.importer.ImportContainer;
 import ome.formats.importer.targets.ImportTarget;
@@ -73,11 +77,98 @@ public class TargetsTest {
     }
 
     @Test
-    public void testModelImportTarget() throws BigResult {
+    public void testModelImportTarget() throws Exception {
         TargetBuilder b = new TargetBuilder();
         ModelImportTarget t = (ModelImportTarget) b.parse("Screen:1").build();
         assertEquals(Screen.class, t.getObjectType());
-        assertEquals(Long.valueOf(1L), t.getObjectId());
+        IObject obj = t.load(null, null);
+        assertEquals(1L, obj.getId().getValue());
     }
 
+    @Test
+    public void testTemplateRegexes() {
+        Pattern p;
+        Matcher m;
+
+        p = Pattern.compile("(?<Container1>.*)");
+        m = p.matcher("everything");
+        assertTrue(m.matches());
+        assertEquals("everything", m.group("Container1"));
+
+        p = Pattern.compile("(?<Ignore>/home)/(?<Container1>.*)");
+        m = p.matcher("/home/user/MyLab/2015-01-01/");
+        assertTrue(m.matches());
+        assertEquals("/home", m.group("Ignore"));
+        assertEquals("user/MyLab/2015-01-01/", m.group("Container1"));
+
+        // Explicit "ignore"
+        p = Pattern.compile("(?<Ignore>(/[^/]+){2})/(?<Container1>.*?)");
+        m = p.matcher("/home/user/MyLab/2015-01-01/");
+        assertTrue(m.matches());
+        assertEquals("/home/user", m.group("Ignore"));
+        assertEquals("MyLab/2015-01-01/", m.group("Container1"));
+
+        // Implicit "ignore"
+        p = Pattern.compile("^.*user/(?<Container1>.*?)");
+        m = p.matcher("/home/user/MyLab/2015-01-01/");
+        assertTrue(m.matches());
+        try {
+            m.group("Ignore"); // Not included
+        } catch (IllegalArgumentException iae) {
+            // good
+        }
+        assertEquals("MyLab/2015-01-01/", m.group("Container1"));
+
+        // Group
+        p = Pattern.compile("^.*user/(?<Group>[^/]+)/(?<Container1>.*?)");
+        m = p.matcher("/home/user/MyLab/2015-01-01/");
+        assertTrue(m.matches());
+        assertEquals("MyLab", m.group("Group"));
+        assertEquals("2015-01-01/", m.group("Container1"));
+
+        // Container2 takes all extra paths
+        p = Pattern.compile("^.*user/(?<Container2>([^/]+/)*)(?<Container1>([^/]+/))");
+        m = p.matcher("/home/user/MyLab/2015-01-01/foo/");
+        assertTrue(m.matches());
+        assertEquals("MyLab/2015-01-01/", m.group("Container2"));
+        assertEquals("foo/", m.group("Container1"));
+        m = p.matcher("/home/user/MyLab/");
+        assertTrue(m.matches());
+        assertEquals("MyLab/", m.group("Container1"));
+
+        // TODO:
+        // Guarantee whether all paths will end in / or not
+        // Add a helper for: ([^/]+/)
+        // Add a name for IDs CID1, CID2
+    }
+
+    // Absolute:
+    // --------
+    // /tmp/my-data/JRSLab/2015-05-foo/some-dir/a.fake
+    // /{-2}/{Group}/{Container}/
+    // /{-2}/{Group}/{Container*}/
+    // /{-2}/{Group}/{Container+}/
+    // /{-2}/{Group}/{Container}/{Container*}
+    // /{-2}/{Group}/{Container*}/{Container}
+
+    // Relative:
+
+    String[][] data = new String[][] {
+        new String[] {"/{-3}/{Group}/" },
+        new String[] {"{/" },
+    };
+
+    @Test
+    public void testTemplateParsing() {
+        TemplateImportTarget t;
+        t = new TemplateImportTarget();
+        t.init("{Project}/{Dataset}/");
+        assertEquals("D", t.getDataset());
+    }
+
+    @Test
+    public void testTemplateBuilding() {
+        TargetBuilder b = new TargetBuilder();
+        b.parse("");
+    }
 }
