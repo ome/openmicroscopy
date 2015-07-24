7ce16edb0a78ebdc2c266435de4552546c385843
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2015-07-24 09:53:54 +0100

    changing log level

--- components/tools/OmeroWeb/omeroweb/webgateway/webgateway_cache.py
+++ components/tools/OmeroWeb/omeroweb/webgateway/webgateway_cache.py
@@ -631,11 +631,15 @@ class WebGatewayCache (object):
             rv = 'img_%s/%s/%s/%%s-c%s-m%s-q%s-r%s-t%s' % (
                 client_base, pre, str(iid), c, m, q, region, tile)
             if p:
-                return rv % ('%s-%s' % (p, str(t)))
+                logger.debug('rv: {0} {1}'.format(rv, type(rv)))
+                logger.debug('p: {0} {1}'.format(str(p), type(p)))
+                logger.debug('t: {0} {1}'.format(str(t), type(t)))
+                pt = '%s-%s' % (p, str(t))
+                return rv % (pt)
             else:
-                logger.info('rv: {0} {1}'.format(rv, type(rv)))
-                logger.info('z: {0} {1}'.format(str(z), type(z)))
-                logger.info('t: {0} {1}'.format(str(t), type(t)))
+                logger.debug('rv: {0} {1}'.format(rv, type(rv)))
+                logger.debug('z: {0} {1}'.format(str(z), type(z)))
+                logger.debug('t: {0} {1}'.format(str(t), type(t)))
                 zt = '%sx%s' % (str(z), str(t))
                 return rv % (zt)
         else:
