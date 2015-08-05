fd2465b2c5542cd92c0ee41c362a2b31ceb42232
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-08-05 16:06:08 +0100

    Remove tabs and update doc.

--- components/blitz/src/omero/gateway/model/ROIResult.java
+++ components/blitz/src/omero/gateway/model/ROIResult.java
@@ -1,8 +1,6 @@
 /*
- * org.openmicroscopy.shoola.env.data.model.ROIResult
- *
  *------------------------------------------------------------------------------
- *  Copyright (C) 2006-2010 University of Dundee. All rights reserved.
+ *  Copyright (C) 2006-2015 University of Dundee. All rights reserved.
  *
  *
  * 	This program is free software; you can redistribute it and/or modify
@@ -23,14 +21,9 @@
 package omero.gateway.model;
 
 
-//Java imports
 import java.util.Collection;
 
 import pojos.ROIData;
-
-//Third-party libraries
-
-//Application-internal dependencies
 
 /**
  * Hosts the results of a call loading the ROI.
@@ -40,84 +33,81 @@ import pojos.ROIData;
  * @author Donald MacDonald &nbsp;&nbsp;&nbsp;&nbsp;
  * <a href="mailto:donald@lifesci.dundee.ac.uk">donald@lifesci.dundee.ac.uk</a>
  * @version 3.0
- * <small>
- * (<b>Internal version:</b> $Revision: $Date: $)
- * </small>
  * @since 3.0-Beta4
  */
 public class ROIResult
 {
 
-	/** The collection of rois. */
-	private Collection<ROIData> rois;
-	
-	/** The ID of the original file */
-	private long fileID;
-	
-	/** The result associated to the ROI. */
-	private Object result;
-	
-	/**
-	 * Creates a new instance.
-	 * 
-	 * @param rois The collection of ROIs.
-	 */
-	public ROIResult(Collection<ROIData> rois)
-	{
-		this(rois, -1, null);
-	}
-	
-	/**
-	 * Creates a new instance.
-	 * 
-	 * @param rois 	 The collection of ROIs.
-	 * @param fileID The id of the file.
-	 */
-	public ROIResult(Collection<ROIData> rois, long fileID)
-	{
-		this(rois, fileID, null);
-	}
-	
-	/**
-	 * Creates a new instance.
-	 * 
-	 * @param rois 	 The collection of ROIs.
-	 * @param fileID The id of the file.
-	 * @param result The result table associated to the ROIS.
-	 */
-	public ROIResult(Collection<ROIData> rois, long fileID, Object result)
-	{
-		this.rois = rois;
-		this.fileID = fileID;
-		this.result = result;
-	}
-	
-	/**
-	 * Sets the results.
-	 * 
-	 * @param result The value to set.
-	 */
-	public void setResult(Object result) { this.result = result; }
-	
-	/**
-	 * Returns the collection of ROIs.
-	 * 
-	 * @return See above.
-	 */
-	public Collection<ROIData> getROIs() { return rois; }
-	
-	/** 
-	 * Returns the id of the file.
-	 * 
-	 * @return See above.
-	 */
-	public long getFileID() { return fileID; }
-	
-	/** 
-	 * Returns the result.
-	 * 
-	 * @return See above.
-	 */
-	public Object getResult() { return result; }
-	
+    /** The collection of rois. */
+    private Collection<ROIData> rois;
+
+    /** The ID of the original file */
+    private long fileID;
+
+    /** The result associated to the ROI. */
+    private Object result;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param rois The collection of ROIs.
+     */
+    public ROIResult(Collection<ROIData> rois)
+    {
+        this(rois, -1, null);
+    }
+
+    /**
+     * Creates a new instance.
+     *
+     * @param rois The collection of ROIs.
+     * @param fileID The id of the file.
+     */
+    public ROIResult(Collection<ROIData> rois, long fileID)
+    {
+        this(rois, fileID, null);
+    }
+
+    /**
+     * Creates a new instance.
+     *
+     * @param rois The collection of ROIs.
+     * @param fileID The id of the file.
+     * @param result The result table associated to the ROIS.
+     */
+    public ROIResult(Collection<ROIData> rois, long fileID, Object result)
+    {
+        this.rois = rois;
+        this.fileID = fileID;
+        this.result = result;
+    }
+
+    /**
+     * Sets the results.
+     *
+     * @param result The value to set.
+     */
+    public void setResult(Object result) { this.result = result; }
+
+    /**
+     * Returns the collection of ROIs.
+     *
+     * @return See above.
+     */
+    public Collection<ROIData> getROIs() { return rois; }
+
+    /**
+     * Returns the id of the file.
+     *
+     * @return See above.
+     */
+    public long getFileID() { return fileID; }
+
+    /**
+     * Returns the result.
+     *
+     * @return See above.
+     */
+    public Object getResult() { return result; }
+
 }
