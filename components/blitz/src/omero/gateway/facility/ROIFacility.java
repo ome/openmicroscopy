405f7c67c350993850c395aa8c30160815d453d9
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-08-05 16:17:21 +0100

    Remove unused import.

--- components/blitz/src/omero/gateway/facility/ROIFacility.java
+++ components/blitz/src/omero/gateway/facility/ROIFacility.java
@@ -22,7 +22,6 @@ package omero.gateway.facility;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
