18c6ec3d0fbd228fd6dba37dd08bc2e70a9a8855
Colin Blackburn <c.blackburn@dundee.ac.uk>
2015-08-13 08:40:18 +0100

    Bump the copyright date

--- components/tools/OmeroPy/test/integration/test_rawpixelsstore.py
+++ components/tools/OmeroPy/test/integration/test_rawpixelsstore.py
@@ -4,7 +4,7 @@
 """
    Tests for the stateful RawPixelsStore service.
 
-   Copyright 2011-2014 Glencoe Software, Inc. All rights reserved.
+   Copyright 2011-2015 Glencoe Software, Inc. All rights reserved.
    Use is subject to license terms supplied in LICENSE.txt
 
 """
