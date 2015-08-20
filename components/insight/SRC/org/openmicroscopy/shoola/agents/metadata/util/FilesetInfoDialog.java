ff2bc4432dddf3c39ac852ff478b797c9c00bae7
Dominik Lindner <d.lindner@dundee.ac.uk>
2015-08-20 11:01:39 +0100

    Removed the bracket (s)

--- components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/util/FilesetInfoDialog.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/metadata/util/FilesetInfoDialog.java
@@ -96,7 +96,8 @@ public class FilesetInfoDialog extends TinyDialog {
             if (Fileset.class.isAssignableFrom(fsd.asIObject().getClass())) {
                 size = ((Fileset)fsd.asIObject()).sizeOfUsedFiles();
             }
-            JLabel l = new JLabel(size + " Image file(s)");
+            String txt = size == 1 ? "Image file" : "Image files";
+            JLabel l = new JLabel(size + " " + txt);
             l.setBackground(UIUtilities.BACKGROUND_COLOR);
             content.add(l, c);
             c.gridy++;
