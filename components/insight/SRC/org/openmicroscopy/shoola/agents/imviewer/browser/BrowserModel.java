b6a0a0a14026cb4a688f0a5ba86e24e905da6a09
Dominik Lindner <d.lindner@dundee.ac.uk>
2015-08-24 10:30:48 +0100

    Take default interpolate property from server

--- components/insight/SRC/org/openmicroscopy/shoola/agents/imviewer/browser/BrowserModel.java
+++ components/insight/SRC/org/openmicroscopy/shoola/agents/imviewer/browser/BrowserModel.java
@@ -48,6 +48,7 @@ import org.openmicroscopy.shoola.agents.imviewer.util.ImagePaintingFactory;
 import org.openmicroscopy.shoola.agents.imviewer.view.ImViewer;
 import org.openmicroscopy.shoola.agents.imviewer.view.ViewerPreferences;
 import org.openmicroscopy.shoola.env.log.LogMessage;
+import org.openmicroscopy.shoola.env.LookupNames;
 import org.openmicroscopy.shoola.env.rnd.data.Tile;
 import org.openmicroscopy.shoola.env.ui.UserNotifier;
 import org.openmicroscopy.shoola.util.image.geom.Factory;
@@ -419,6 +420,15 @@ class BrowserModel
         		unitBarColor = pref.getScaleBarColor();
         	if (pref.isFieldSelected(ViewerPreferences.ZOOM_FACTOR))
         		zoomFactor = ZoomAction.getZoomFactor(pref.getZoomIndex());
+        }
+        
+        String interpol = (String) ImViewerAgent.getRegistry().lookup(
+                LookupNames.INTERPOLATE);
+        if (interpol != null) {
+            try {
+                this.interpolation = Boolean.parseBoolean(interpol);
+            } catch (Exception e) {
+            }
         }
     }
     
