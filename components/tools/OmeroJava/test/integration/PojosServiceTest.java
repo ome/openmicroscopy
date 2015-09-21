b960cb18bdcc213ff5f48c8ea2a2493b1e0aded0
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-09-21 10:40:18 +0100

    Create group with correct permissions.

--- components/tools/OmeroJava/test/integration/PojosServiceTest.java
+++ components/tools/OmeroJava/test/integration/PojosServiceTest.java
@@ -2166,7 +2166,7 @@ public class PojosServiceTest extends AbstractServerTest {
     }
 
     /**
-     * Test to load container hierarchy with project containing an dataset
+     * Test to load container hierarchy with screen containing a plate
      * owned by another member of the group.
      *
      * @throws Exception
@@ -2176,6 +2176,8 @@ public class PojosServiceTest extends AbstractServerTest {
     public void testLoadContainerHierarchyScreenWithOtherMembersPlate()
             throws Exception {
         // first create a Screen
+        String perms = "rwrw--";
+        EventContext ctx = newUserAndGroup(perms, true);
         Screen p = (Screen) iUpdate.saveAndReturnObject(mmFactory
                 .simpleScreenData().asIObject());
 
