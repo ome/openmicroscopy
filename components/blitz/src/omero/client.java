e261375a0a2c1c6716a528ce9475a44f4e6f9c27
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-09-29 21:52:55 +0100

    Only set property for ice 3.5 and before.

--- components/blitz/src/omero/client.java
+++ components/blitz/src/omero/client.java
@@ -329,7 +329,9 @@ public class client {
 
         // Strictly necessary for this class to work
         optionallySetProperty(id, "Ice.ImplicitContext", "Shared");
-        optionallySetProperty(id, "Ice.ACM.Client", "0");
+        if (Ice.Util.intVersion() <= 30500) {
+            optionallySetProperty(id, "Ice.ACM.Client", "0");
+        }
         optionallySetProperty(id, "Ice.CacheMessageBuffers", "0");
         optionallySetProperty(id, "Ice.RetryIntervals", "-1");
         optionallySetProperty(id, "Ice.Default.EndpointSelection", "Ordered");
