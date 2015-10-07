850492699f77271f1949cb91b1412199100bb4a2
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2015-10-07 11:18:50 +0100

    adding getter for roi limit config value

--- components/tools/OmeroPy/src/omero/gateway/__init__.py
+++ components/tools/OmeroPy/src/omero/gateway/__init__.py
@@ -1530,6 +1530,14 @@ class _BlitzGateway (object):
                 int(c.getConfigValue('omero.pixeldata.max_plane_height')))
         return self._maxPlaneSize
 
+    def getRoiLimitSetting(self):
+        try:
+            roi_limit = (self.getConfigService().getConfigValue(
+                         "omero.client.viewer.roi_limit") or 100)
+        except:
+            roi_limit = 100
+        return roi_limit
+
     def getInitialZoomLevel(self):
         """
         Returns default initial zoom level set on the server.
