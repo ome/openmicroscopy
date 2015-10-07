1491e7648c9e5c18c6bcca2f9aa2bded3a97d76a
Colin Blackburn <c.blackburn@dundee.ac.uk>
2015-10-07 10:20:39 +0100

    Remove constraint that number searches are searches by ID

--- components/tools/OmeroWeb/omeroweb/webclient/views.py
+++ components/tools/OmeroWeb/omeroweb/webclient/views.py
@@ -767,30 +767,6 @@ def load_searching(request, form=None, conn=None, **kwargs):
         manager.search(query_search, onlyTypes, fields, searchGroup, ownedBy,
                        useAcquisitionDate, date)
 
-        # if the query is only numbers (separated by commas or spaces)
-        # we search for objects by ID
-        isIds = re.compile('^[\d ,]+$')
-        if isIds.search(query_search) is not None:
-            conn.SERVICE_OPTS.setOmeroGroup(-1)
-            idSet = set()
-            for queryId in re.split(' |,', query_search):
-                if len(queryId) == 0:
-                    continue
-                try:
-                    searchById = long(queryId)
-                    if searchById in idSet:
-                        continue
-                    idSet.add(searchById)
-                    for t in onlyTypes:
-                        t = t[0:-1]  # remove 's'
-                        if t in ('project', 'dataset', 'image', 'screen',
-                                 'plate'):
-                            obj = conn.getObject(t, searchById)
-                            if obj is not None:
-                                foundById.append({'otype': t, 'obj': obj})
-                except ValueError:
-                    pass
-
     else:
         # simply display the search home page.
         template = "webclient/search/search.html"
