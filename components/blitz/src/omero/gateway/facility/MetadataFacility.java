a1f1ec08924a7bc22a8dc57134e2f82f17339baf
Dominik Lindner <d.lindner@dundee.ac.uk>
2015-11-04 11:32:11 +0000

    Fixed wrong import

--- components/blitz/src/omero/gateway/facility/MetadataFacility.java
+++ components/blitz/src/omero/gateway/facility/MetadataFacility.java
@@ -21,13 +21,13 @@
 package omero.gateway.facility;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ExecutionException;
 
-import edu.emory.mathcs.backport.java.util.Arrays;
 import omero.api.IMetadataPrx;
 import omero.gateway.Gateway;
 import omero.gateway.SecurityContext;
