2c125eb6ee41b08f0e413f600f379857e7e160b5
simleo <simleo@crs4.it>
2015-11-21 12:06:00 +0000

    allow user-specified screen target to override the one from the .screen file

--- components/blitz/src/ome/formats/OMEROMetadataStoreClient.java
+++ components/blitz/src/ome/formats/OMEROMetadataStoreClient.java
@@ -273,6 +273,8 @@ public class OMEROMetadataStoreClient
      */
     private Map<String, String[]> referenceStringCache;
 
+    private LSID screenKey;
+
     /** Our model processors. Will be called on saveToDB(). */
     private List<ModelProcessor> modelProcessors;
 
@@ -1374,6 +1376,10 @@ public class OMEROMetadataStoreClient
     @Override
     public void setUserSpecifiedTarget(IObject target)
     {
+        if (target instanceof Screen && null != screenKey) {
+          log.info("deleting screen ref in favor of user-specified one");
+          referenceCache.remove(screenKey);
+        }
         this.userSpecifiedTarget = target;
     }
 
@@ -6818,8 +6824,8 @@ public class OMEROMetadataStoreClient
     public void setScreenPlateRef(String plate, int screenIndex,
             int plateRefIndex)
     {
-        LSID key = new LSID(Screen.class, screenIndex);
-        addReference(key, new LSID(plate));
+        screenKey = new LSID(Screen.class, screenIndex);
+        addReference(screenKey, new LSID(plate));
     }
 
     /* (non-Javadoc)
