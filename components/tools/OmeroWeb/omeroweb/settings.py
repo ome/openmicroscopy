40a5a5bb4c86f1cb0009ffa567115fd90c6c1979
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2015-11-23 09:42:07 +0000

    updating comment

--- components/tools/OmeroWeb/omeroweb/settings.py
+++ components/tools/OmeroWeb/omeroweb/settings.py
@@ -437,10 +437,10 @@ CUSTOM_SETTINGS_MAPPINGS = {
          '[]',
          json.loads,
          ("A tuple representing a HTTP header/value combination that "
-          "signifies a request is secure. Example ``SECURE_PROXY_SSL_HEADER"
-          " = ('HTTP_X_FORWARDED_PROTO', 'https')``. For more details see "
-          "https://docs.djangoproject.com/en/1.8/ref/settings/"
-          "#secure-proxy-ssl-header.")],
+          "signifies a request is secure. Example "
+          "``'[\"HTTP_X_FORWARDED_PROTO_OMERO_WEB\", \"https\"]'``. "
+          "For more details see :djangodoc:`secure proxy ssl header <ref/"
+          "settings/#secure-proxy-ssl-header>`.")],
 
     # Public user
     "omero.web.public.enabled":
