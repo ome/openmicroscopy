107df74fceed133850d58b27d2c1d3b5c39ea751
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2015-12-07 10:30:39 +0000

    update wsgi.py for running multiple sites

--- components/tools/OmeroWeb/omeroweb/wsgi.py
+++ components/tools/OmeroWeb/omeroweb/wsgi.py
@@ -28,7 +28,12 @@ import sys
 # OMERO.web is set up with the "omeroweb" package also on the PYTHONPATH
 sys.path.append(os.path.dirname(__file__))
 
+# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
+# if running multiple sites in the same mod_wsgi process. To fix this, use
+# mod_wsgi daemon mode with each site in its own daemon process, or use
+# os.environ["DJANGO_SETTINGS_MODULE"] = "omeroweb.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omeroweb.settings")
+
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
