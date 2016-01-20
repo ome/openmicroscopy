9604ce5fbde6963643bec92fece0d89470235aad
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-01-20 13:25:16 +0000

    5.1 tweaks (to be removed in 5.3)

--- components/tools/OmeroWeb/test/integration/test_redirect.py
+++ components/tools/OmeroWeb/test/integration/test_redirect.py
@@ -1,9 +1,23 @@
 import omero
 from weblibrary import IWebTest
-from weblibrary import _response
+# TODO: from weblibrary import _response 5.2 only  # NOQA
 
 from django.core.urlresolvers import reverse
 from omeroweb.webredirect.baseconv import base62
+
+
+# TODO: Helpers 5.1 only  # NOQA
+from django.test.client import MULTIPART_CONTENT
+
+
+def _response(django_client, request_url, method, data, status_code=403,
+              content_type=MULTIPART_CONTENT, **extra):
+    response = getattr(django_client, method)(request_url,
+                                              data=data,
+                                              content_type=content_type,
+                                              **extra)
+    assert response.status_code == status_code, response
+    return response
 
 
 class TestShortUrl(IWebTest):
@@ -13,9 +27,9 @@ class TestShortUrl(IWebTest):
 
     def test_short_url(self):
         origin = "/webclient/?show=image-1"
-        alias = omero.model.CommentAnnotationI()
+        alias = omero.model.AliasI()
         alias.setNs(omero.rtypes.rstring("host/port"))
-        alias.description = omero.rtypes.rstring(origin)
+        alias.origin = omero.rtypes.rstring(origin)
         alias.details.permissions = omero.model.PermissionsI("rwr-r-")
         alias = self.root.sf.getUpdateService().saveAndReturnObject(alias)
         short = base62.from_decimal(alias.id.val)
