3504a2d4e0fc51a156c48a0b56d01d969d65f334
Will Moore <w.moore@dundee.ac.uk>
2016-02-02 11:02:36 +0000

    Add 'PYTHONPATH=./lib/python' to populate_metadata.py call in setup.sh

--- components/tests/ui/robot_setup.sh
+++ components/tests/ui/robot_setup.sh
@@ -74,7 +74,7 @@ done
 bin/omero import $PLATE_NAME --debug ERROR > plate_import.log 2>&1
 plateid=$(sed -n -e 's/^Plate://p' plate_import.log)
 # Use populate_metadata to upload and attach bulk annotation csv
-python lib/python/omero/util/populate_metadata.py -k $key Plate:$plateid $BULK_ANNOTATION_CSV
+PYTHONPATH=./lib/python python lib/python/omero/util/populate_metadata.py -k $key Plate:$plateid $BULK_ANNOTATION_CSV
 
 # Create Screen with empty plates for Create Scenario
 scrDs=$(bin/omero obj new Screen name='CreateScenario')
