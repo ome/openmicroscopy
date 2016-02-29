c9c103c8e21e24e466523c5ebac8d00bf2c39e1b
Will Moore <w.moore@dundee.ac.uk>
2016-02-29 22:52:20 +0000

    Delete plate.html, containers_icon.html & load_data()

--- components/tools/OmeroWeb/omeroweb/webclient/urls.py
+++ components/tools/OmeroWeb/omeroweb/webclient/urls.py
@@ -68,17 +68,6 @@ urlpatterns = patterns(
         views.activities_update,
         name="activities_update"),
 
-    # loading data
-    url(r'^load_data/(?:(?P<o1_type>'
-        r'((?i)project|dataset|image|screen|plate|well|orphaned))/)'
-        r'?(?:(?P<o1_id>[0-9]+)/)'
-        r'?(?:(?P<o2_type>((?i)dataset|image|plate|acquisition|well))/)'
-        r'?(?:(?P<o2_id>[0-9]+)/)'
-        r'?(?:(?P<o3_type>((?i)image|well))/)'
-        r'?(?:(?P<o3_id>[0-9]+)/)?$',
-        views.load_data,
-        name="load_data"),
-
     # chgrp. Load potential target groups, then load target P/D within chosen
     # group
     url(r'^load_chgrp_groups/$',
