083620b12d25ce5b96853d9d440140f0772d4a3e
Will Moore <w.moore@dundee.ac.uk>
2016-04-07 16:46:54 +0100

    Basic working display of file annotations

--- components/tools/OmeroWeb/omeroweb/webclient/static/webclient/javascript/ome.general_tab.js
+++ components/tools/OmeroWeb/omeroweb/webclient/static/webclient/javascript/ome.general_tab.js
@@ -21,7 +21,7 @@ var TagPane = function TagPane($element, objects) {
         $body = $element.children('div'),
         $tags_container = $("#tags_container");
 
-    var tmplText = $('#tag_template').html();
+    var tmplText = $('#tags_template').html();
     var tagTmpl = _.template(tmplText);
 
 
@@ -89,6 +89,116 @@ var TagPane = function TagPane($element, objects) {
                 // Finish up...
                 OME.filterAnnotationsAddedBy();
                 $(".tooltip", $tags_container).tooltip_init();
+            });
+            
+        }
+    };
+
+
+    // We use the "#metadata_general" element to store this data since
+    // it is not reloaded on selection change.
+    var setExpanded = function setExpanded(expanded) {
+        var open_panes = $("#metadata_general").data('open_panes') || [];
+        if (expanded && open_panes.indexOf('tags') === -1) {
+            open_panes.push("tags");
+        }
+        if (!expanded && open_panes.indexOf('tags') > -1) {
+            open_panes = open_panes.reduce(function(l, item){
+                if (item !== 'tags') l.push(item);
+                return l;
+            }, []);
+        }
+        $("#metadata_general").data('open_panes', open_panes);
+    };
+
+    var getExpanded = function getExpanded() {
+        var open_panes = $("#metadata_general").data('open_panes') || [];
+        return open_panes.indexOf('tags') > -1;
+    };
+
+
+    initEvents();
+
+    if (getExpanded()) {
+        $header.toggleClass('closed');
+        $body.show();
+    }
+
+    this.render();
+};
+
+
+
+var FileAnnsPane = function FileAnnsPane($element, objects) {
+
+    var $header = $element.children('h1'),
+        $body = $element.children('div'),
+        $fileanns_container = $("#fileanns_container");
+
+    var tmplText = $('#fileanns_template').html();
+    var filesTempl = _.template(tmplText);
+
+
+    var initEvents = (function initEvents() {
+
+        $header.click(function(){
+            $header.toggleClass('closed');
+            $body.slideToggle();
+
+            var expanded = !$header.hasClass('closed');
+            setExpanded(expanded);
+
+            if (expanded && $fileanns_container.is(":empty")) {
+                this.render();
+            }
+        }.bind(this));
+    }).bind(this);
+
+
+    this.render = function render() {
+
+        console.log('render files', $fileanns_container.is(":visible"));
+
+        if ($fileanns_container.is(":visible")) {
+
+            if ($fileanns_container.is(":empty")) {
+                $fileanns_container.html("Loading attachments...");
+            }
+
+            var request = objects.map(function(o){
+                return o.replace("-", "=");
+            });
+            console.log(request);
+
+            $.getJSON("/webclient/api/annotations/?type=file&" + request, function(data){
+
+                // manipulate data...
+                // make an object of eid: experimenter
+                var experimenters = data.experimenters.reduce(function(prev, exp){
+                    prev[exp.id + ""] = exp;
+                    return prev;
+                }, {});
+
+                // Populate experimenters within tags
+                var tags = data.annotations.map(function(f){
+                    f.owner = experimenters[f.owner.id];
+                    if (f.link && f.link.owner) {
+                        f.link.owner = experimenters[f.link.owner.id];
+                    }
+                    f.textValue = _.escape(f.textValue);
+                    f.description = _.escape(f.description);
+                    f.file.size = f.file.size.filesizeformat();
+                    return f;
+                });
+                console.log(tags);
+
+                // Update html...
+                var html = filesTempl({'files': tags});
+                $fileanns_container.html(html);
+
+                // Finish up...
+                OME.filterAnnotationsAddedBy();
+                $(".tooltip", $fileanns_container).tooltip_init();
             });
             
         }
