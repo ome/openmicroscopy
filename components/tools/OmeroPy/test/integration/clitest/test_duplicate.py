cf88a58c940812eab7660e3c29c24889a7cc49b2
Petr Walczysko <p.walczysko@dundee.ac.uk>
2016-04-14 11:53:45 +0100

    Add basic test for duplicate

---
+++ components/tools/OmeroPy/test/integration/clitest/test_duplicate.py
@@ -0,0 +1,26 @@
+from omero.plugins.duplicate import DuplicateControl
+from test.integration.clitest.cli import CLITest
+import pytest
+
+object_types = ["Image", "Dataset", "Project", "Plate", "Screen"]
+model = ["", "I"]
+
+
+class TestDuplicate(CLITest):
+
+    def setup_method(self, method):
+        super(TestDuplicate, self).setup_method(method)
+        self.cli.register("duplicate", DuplicateControl, "TEST")
+        self.args += ["duplicate"]
+
+    @pytest.mark.parametrize("model", model)
+    @pytest.mark.parametrize("object_type", object_types)
+    def testDeleteMyData(self, object_type, model):
+        oid = self.create_object(object_type)
+
+        # Duplicate the object
+        self.args += ['/%s%s:%s' % (object_type, model, oid)]
+        self.cli.invoke(self.args, strict=True)
+
+        # Check the original object is still present
+        assert self.query.find(object_type, oid)
