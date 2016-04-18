0f6772d6ec2a5351a610e3a5b79e6ba6d31389ff
Petr Walczysko <p.walczysko@dundee.ac.uk>
2016-04-18 17:50:07 +0100

    Add test for simple P/D/I hierarchy duplication

--- components/tools/OmeroPy/test/integration/clitest/test_duplicate.py
+++ components/tools/OmeroPy/test/integration/clitest/test_duplicate.py
@@ -39,6 +39,28 @@ class TestDuplicate(CLITest):
         self.cli.invoke(self.args, strict=True)
         return capfd.readouterr()[0]
 
+    def get_dataset(self, iid):
+        """Retrieve all the parent datasets linked to the image"""
+
+        params = omero.sys.ParametersI()
+        params.addId(iid)
+        query = "select d from Dataset as d"
+        query += " where exists ("
+        query += " select l from DatasetImageLink as l"
+        query += " where l.child.id=:id and l.parent=d.id) "
+        return self.query.findAllByQuery(query, params)
+
+    def get_project(self, did):
+        """Retrieve all the parent projects linked to the dataaset"""
+
+        params = omero.sys.ParametersI()
+        params.addId(did)
+        query = "select p from Project as p"
+        query += " where exists ("
+        query += " select l from ProjectDatasetLink as l"
+        query += " where l.child.id=:id and l.parent=p.id) "
+        return self.query.findAllByQuery(query, params)
+
     @pytest.mark.parametrize("model", model)
     @pytest.mark.parametrize("object_type", object_types)
     def testDuplicateSingleObject(self, object_type, model, capfd):
@@ -128,3 +150,46 @@ class TestDuplicate(CLITest):
         assert '%s:%s' % (object_type, objs[0].id.val) in out
         # Check object has been found in two different places of the output
         assert out.find(obj_arg) != out.rfind(obj_arg)
+
+    def testBasicHierarchyDuplication(self, capfd):
+        namep = self.uuid()
+        named = self.uuid()
+        namei = self.uuid()
+        proj = self.make_project(namep)
+        dset = self.make_dataset(named)
+        img = self.make_image(namei)
+
+        self.link(proj, dset)
+        self.link(dset, img)
+
+        self.args += ['Project:%s' % proj.id.val]
+        self.duplicate(capfd)
+
+        pd = omero.sys.ParametersI()
+        pd.addString("name", named)
+        query = "select obj from Dataset obj where obj.name=:name"
+        objsd = self.query.findAllByQuery(query, pd)
+
+        # Find the Projects linked to the duplicated dataset
+        did = max(objsd[0].id.val, objsd[1].id.val)
+        proj_linked = self.get_project(did)
+
+        pi = omero.sys.ParametersI()
+        pi.addString("name", namei)
+        query = "select obj from Image obj where obj.name=:name"
+        objsi = self.query.findAllByQuery(query, pi)
+
+        # Find the Datasets linked to the duplicated image
+        iid = max(objsi[0].id.val, objsi[1].id.val)
+        dat_linked = self.get_dataset(iid)
+
+        # Check that the Dataset and Image were duplicated
+        assert len(objsd) == 2
+        assert len(objsi) == 2
+        assert objsd[0].id.val != objsd[1].id.val
+        assert objsi[0].id.val != objsi[1].id.val
+
+        # Check that the duplicated Dataset/Image is linked
+        # only to the duplicated Project/Dataset
+        assert len(proj_linked) == 1
+        assert len(dat_linked) == 1
