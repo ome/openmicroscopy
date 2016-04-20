ef019cc83a2e85cea07a6a1f9b585045707d540c
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2016-04-20 11:34:16 +0100

    have SkipHead pass through underlying request's call context

--- components/blitz/src/omero/cmd/graphs/SkipHeadI.java
+++ components/blitz/src/omero/cmd/graphs/SkipHeadI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2015 University of Dundee & Open Microscopy Environment.
+ * Copyright (C) 2014-2016 University of Dundee & Open Microscopy Environment.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -29,7 +29,6 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Function;
 import com.google.common.collect.HashMultimap;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.SetMultimap;
 
@@ -37,7 +36,6 @@ import ome.model.IObject;
 import ome.services.graphs.GraphException;
 import ome.services.graphs.GraphPathBean;
 import ome.services.graphs.GraphPolicy;
-import ome.system.Login;
 import omero.cmd.HandleI.Cancel;
 import omero.cmd.ERR;
 import omero.cmd.Helper;
@@ -57,8 +55,6 @@ import omero.cmd.Status;
 public class SkipHeadI extends SkipHead implements IRequest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SkipHeadI.class);
-
-    private static final ImmutableMap<String, String> ALL_GROUPS_CONTEXT = ImmutableMap.of(Login.OMERO_GROUP, "-1");
 
     private static final ImmutableSet<State> REQUEST_FAILURE_FLAGS = ImmutableSet.of(State.CANCELLED, State.FAILURE);
 
@@ -86,7 +82,7 @@ public class SkipHeadI extends SkipHead implements IRequest {
 
     @Override
     public Map<String, String> getCallContext() {
-        return new HashMap<String, String>(ALL_GROUPS_CONTEXT);
+        return ((IRequest) request).getCallContext();
     }
 
     @Override
