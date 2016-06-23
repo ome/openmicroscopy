41b51d2718113b031fb2d40ebe24c5de85f7dcef
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-06-23 22:37:02 +0100

    Refactor Lut readers.

---
+++ components/rendering/src/omeis/providers/re/lut/TextLutReader.java
@@ -0,0 +1,43 @@
+/*
+ *------------------------------------------------------------------------------
+ *  Copyright (C) 2016 University of Dundee. All rights reserved.
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *------------------------------------------------------------------------------
+ */
+package omeis.providers.re.lut;
+
+import java.io.File;
+
+
+/**
+ * Read the text Lut.
+ * @author Jean-Marie Burel &nbsp;&nbsp;&nbsp;&nbsp;
+ * <a href="mailto:j.burel@dundee.ac.uk">j.burel@dundee.ac.uk</a>
+ * @since 5.3
+ */
+class TextLutReader
+    extends BasicLutReader
+{
+
+    /**
+     * Reads the file. The <code>raw</code> flag is not used.
+     */
+    @Override
+    int read(File file, boolean raw) throws Exception {
+        return 0;
+    }
+}
