cdbeef5c151019051de3c7eefb317ef49f33b923
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-06-29 21:37:17 +0100

    Use new reader constructor and log error

--- components/rendering/src/omeis/providers/re/HSBStrategy.java
+++ components/rendering/src/omeis/providers/re/HSBStrategy.java
@@ -155,10 +155,11 @@ class HSBStrategy extends RenderingStrategy {
             String lutName = of.getName();
             if (lutName.equals(name) ||
                     FilenameUtils.getBaseName(lutName).equals(name)) {
-                LutReader reader = new LutReader(of);
+                LutReader reader = new LutReader(of.getPath(), of.getName());
                 try {
                     reader.read();
                 } catch (Exception e) {
+                    log.debug("cannot read lut "+of.getName(), e);
                     reader = null;
                 }
                 return reader;
