e016d4c6c329a81bdd47e68bdd2a3236334de758
Roger Leigh <rleigh@dundee.ac.uk>
2016-07-01 16:09:18 +0100

    OmeroCpp: Undefine PASCAL on Windows

---
+++ components/tools/OmeroCpp/src/omero/internal/fixes.h
@@ -0,0 +1,12 @@
+/*
+ * Windows portability fixes.
+ */
+
+#ifndef OMERO_INTERNAL_FIXES_H
+#define OMERO_INTERNAL_FIXES_H
+
+#ifdef _MSC_VER
+#undef PASCAL
+#endif
+
+#endif // OMERO_INTERNAL_FIXES_H
