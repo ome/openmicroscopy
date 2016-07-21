bbf96166f35c5b93d0f867c55b904b0fe0ec5dcf
jmoore <josh@glencoesoftware.com>
2016-07-21 21:36:11 +0200

    2016-SV2: add no-shares SQL scripts
    Rather than upgrade the server, it's possible to disable
    shares at the database-level which is a sufficient work-
    around.
    
    **Note:** all existing shares will be deleted.

---
+++ sql/psql/OMERO4.4__0/OMERO4.4-no-shares.sql
@@ -0,0 +1,120 @@
+-- Copyright (C) 2016 Glencoe Software, Inc. All rights reserved.
+-- Use is subject to license terms supplied in LICENSE.txt
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along
+-- with this program; if not, write to the Free Software Foundation, Inc.,
+-- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+--
+
+---
+--- OMERO5 in-place upgrade for the OMERO4.4__0 database to disable shares.
+---
+
+BEGIN;
+
+
+--
+-- check OMERO database version
+--
+
+CREATE OR REPLACE FUNCTION omero_assert_db_version(expected_version VARCHAR, expected_patch INTEGER) RETURNS void AS $$
+
+DECLARE
+    current_version VARCHAR;
+    current_patch INTEGER;
+
+BEGIN
+    SELECT currentversion, currentpatch INTO STRICT current_version, current_patch
+        FROM dbpatch ORDER BY id DESC LIMIT 1;
+
+    IF current_version <> expected_version OR current_patch <> expected_patch THEN
+        RAISE EXCEPTION 'wrong OMERO database version for this upgrade script';
+    END IF;
+
+END;$$ LANGUAGE plpgsql;
+
+SELECT omero_assert_db_version('OMERO4.4', 0);
+DROP FUNCTION omero_assert_db_version(varchar, int);
+
+
+--
+-- check PostgreSQL server version and database encoding
+--
+
+CREATE OR REPLACE FUNCTION db_pretty_version(version INTEGER) RETURNS TEXT AS $$
+
+BEGIN
+    RETURN (version/10000)::TEXT || '.' || ((version/100)%100)::TEXT || '.' || (version%100)::TEXT;
+
+END;$$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION assert_db_server_prerequisites(version_prereq INTEGER) RETURNS void AS $$
+
+DECLARE
+    version_num INTEGER;
+    char_encoding TEXT;
+
+BEGIN
+    SELECT CAST(setting AS INTEGER) INTO STRICT version_num
+        FROM pg_settings WHERE name = 'server_version_num';
+    SELECT pg_encoding_to_char(encoding) INTO STRICT char_encoding
+        FROM pg_database WHERE datname = current_database();
+
+    IF version_num < version_prereq THEN
+        RAISE EXCEPTION 'PostgreSQL database server version % is less than OMERO prerequisite %',
+        db_pretty_version(version_num), db_pretty_version(version_prereq);
+    END IF;
+
+    IF char_encoding != 'UTF8' THEN
+        RAISE EXCEPTION 'OMERO database character encoding must be UTF8, not %', char_encoding;
+    ELSE
+        SET client_encoding = 'UTF8';
+    END IF;
+
+END;$$ LANGUAGE plpgsql;
+
+SELECT assert_db_server_prerequisites(90300);
+
+DROP FUNCTION assert_db_server_prerequisites(INTEGER);
+DROP FUNCTION db_pretty_version(INTEGER);
+
+
+--
+-- Actual upgrade
+--
+
+
+DELETE FROM sharemember;
+DELETE FROM share;
+CREATE OR REPLACE FUNCTION disable_shares() RETURNS trigger LANGUAGE plpgsql as $$
+BEGIN
+      RAISE EXCEPTION 'Shares are disabled';
+END $$;
+DROP TRIGGER IF EXISTS disable_shares ON share;
+CREATE TRIGGER disable_shares BEFORE INSERT ON share FOR EACH ROW EXECUTE PROCEDURE disable_shares();
+
+
+--
+-- FINISHED
+--
+
+UPDATE dbpatch SET message = 'Shares disabled.', finished = clock_timestamp()
+    WHERE currentVersion  = 'OMERO4.4'    AND
+          currentPatch    = 0             AND
+          previousVersion = 'OMERO4.4'    AND
+          previousPatch   = 0;
+
+SELECT CHR(10)||CHR(10)||CHR(10)||'SHARES HAVE BEEN DISABLED FOR OMERO4.4__0'||CHR(10)||CHR(10)||CHR(10) AS Status;
+
+COMMIT;
