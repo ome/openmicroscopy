77d639320e728cb0256f9523f22f532f5ffcf1a6
William Moore <w.moore@dundee.ac.uk>
2016-08-04 16:51:45 +0100

    Paths to object json includes well{} for SPW image

--- components/tools/OmeroWeb/omeroweb/webclient/show.py
+++ components/tools/OmeroWeb/omeroweb/webclient/show.py
@@ -685,7 +685,8 @@ def paths_to_object(conn, experimenter_id=None, project_id=None,
             select coalesce(sowner.id, plowner.id, aowner.id, wsowner.id),
                    slink.parent.id,
                    plate.id,
-                   acquisition.id
+                   acquisition.id,
+                   well.id
             from WellSample wellsample
             left outer join wellsample.details.owner wsowner
             left outer join wellsample.plateAcquisition acquisition
@@ -746,6 +747,12 @@ def paths_to_object(conn, experimenter_id=None, project_id=None,
                 path.append({
                     'type': 'acquisition',
                     'id': e[3].val
+                })
+
+            if e[4] is not None:
+                path.append({
+                    'type': 'well',
+                    'id': e[4].val
                 })
 
             paths.append(path)
