6776103f709d787eb407d40e9358d51ea2d2899c
Simon Li <spli@dundee.ac.uk>
2016-08-18 18:50:36 +0100

    More thorough test of deletion in prep for following work

--- components/tools/OmeroPy/test/integration/metadata/test_populate.py
+++ components/tools/OmeroPy/test/integration/metadata/test_populate.py
@@ -40,7 +40,7 @@ from omero.model import OriginalFileI
 from omero.model import FileAnnotationI, MapAnnotationI, PlateAnnotationLinkI
 from omero.model import RoiAnnotationLinkI
 from omero.model import RoiI, PointI, ProjectI, ScreenI
-from omero.rtypes import rdouble, rstring, unwrap
+from omero.rtypes import rdouble, rlist, rstring, unwrap
 from omero.sys import ParametersI
 
 from omero.util.populate_metadata import (
@@ -149,6 +149,9 @@ class Fixture(object):
         return os.path.join(
             os.path.dirname(__file__), 'bulk_to_map_annotation_context.yml')
 
+    def get_namespaces(self):
+        return NSBULKANNOTATIONS
+
     def assert_rows(self, rows):
         assert rows == self.rowCount * self.colCount
 
@@ -165,6 +168,14 @@ class Fixture(object):
             else:
                 assert mv['Well Type'] == 'Treatment'
                 assert mv['Concentration'] == '10'
+
+    def get_all_map_annotations(self):
+        qs = self.test.client.sf.getQueryService()
+        q = "FROM MapAnnotation WHERE ns in (:nss)"
+        p = ParametersI()
+        p.map['nss'] = rlist(rstring(ns) for ns in self.get_namespaces())
+        r = qs.findAllByQuery(q, p)
+        return r
 
 
 class Screen2Plates(Fixture):
@@ -263,6 +274,9 @@ class Plate2WellsNs(Plate2Wells):
     def get_cfg(self):
         return os.path.join(os.path.dirname(__file__),
                             'bulk_to_map_annotation_context_ns.yml')
+
+    def get_namespaces(self):
+        return [NSBULKANNOTATIONS, 'openmicroscopy.org/mapr/gene']
 
     def assert_row_values(self, rowvalues):
         # First column is the WellID
@@ -734,15 +748,15 @@ class TestPopulateMetadata(lib.ITest):
         except Exception:
             skip("PyYAML not installed.")
 
-        fixture = Plate2WellsNs2()
-        fixture.init(self)
-        self._test_parsing_context(fixture, 2)
-        self._test_bulk_to_map_annotation_context(fixture, 2)
+        fixture1 = Plate2WellsNs2()
+        fixture1.init(self)
+        self._test_parsing_context(fixture1, 2)
+        self._test_bulk_to_map_annotation_context(fixture1, 2)
 
         fixture2 = Plate2WellsNs2()
         fixture2.init(self)
         self._test_parsing_context(fixture2, 2)
-        self._test_bulk_to_map_annotation_dedup(fixture, fixture2, 2)
+        self._test_bulk_to_map_annotation_dedup(fixture1, fixture2)
 
     def _test_parsing_context(self, fixture, batch_size):
         """
@@ -794,6 +808,7 @@ class TestPopulateMetadata(lib.ITest):
 
     def _test_bulk_to_map_annotation_context(self, fixture, batch_size):
         # self._testPopulateMetadataPlate()
+        assert len(fixture.get_all_map_annotations()) == 0
         assert len(fixture.get_child_annotations()) == 0
 
         cfg = fixture.get_cfg()
@@ -814,8 +829,7 @@ class TestPopulateMetadata(lib.ITest):
         assert len(oas) == fixture.annCount
         fixture.assert_child_annotations(oas)
 
-    def _test_bulk_to_map_annotation_dedup(
-            self, fixture1, fixture2, batch_size):
+    def _test_bulk_to_map_annotation_dedup(self, fixture1, fixture2):
         ann_count = fixture1.annCount
         assert fixture2.annCount == ann_count
         assert len(fixture1.get_child_annotations()) == ann_count
@@ -832,10 +846,7 @@ class TestPopulateMetadata(lib.ITest):
         assert len(fixture1.get_child_annotations()) == ann_count
         assert len(fixture2.get_child_annotations()) == 0
 
-        if batch_size is None:
-            ctx.write_to_omero()
-        else:
-            ctx.write_to_omero(batch_size=batch_size)
+        ctx.write_to_omero()
 
         oas1 = fixture1.get_child_annotations()
         oas2 = fixture2.get_child_annotations()
@@ -865,6 +876,7 @@ class TestPopulateMetadata(lib.ITest):
         else:
             ctx.write_to_omero(batch_size=batch_size)
         assert len(fixture.get_child_annotations()) == 0
+        assert len(fixture.get_all_map_annotations()) == 0
 
 
 class MockMeasurementCtx(AbstractMeasurementCtx):
