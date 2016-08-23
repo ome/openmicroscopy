4df1f4aebd5358d2ecf3e6f17aee213edfb4e503
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-08-23 16:09:10 +0100

    Fix unique constraint

--- sql/psql/OMERO5.3DEV__10/OMERO5.2__0.sql
+++ sql/psql/OMERO5.3DEV__10/OMERO5.2__0.sql
@@ -1877,8 +1877,8 @@ DROP FUNCTION codomainmapcontext_renderingDef_index_move();
 ALTER TABLE codomainmapcontext ADD channelBinding int8 NOT NULL;
 ALTER TABLE codomainmapcontext ADD channelBinding_index int4 NOT NULL;
 
-ALTER TABLE codomainmapcontext ADD UNIQUE (channelBinding);
-ALTER TABLE codomainmapcontext ADD UNIQUE (channelBinding_index);
+ALTER TABLE codomainmapcontext ADD UNIQUE (channelBinding, channelBinding_index);
+
 
 ALTER TABLE codomainmapcontext
 ADD CONSTRAINT FKcodomainmapcontext_channelBinding_channelbinding
