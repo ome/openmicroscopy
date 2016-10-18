1591c77e70553d5b5f3752405a24f62433b3fe4c
Petr Walczysko <p.walczysko@dundee.ac.uk>
2016-10-18 16:31:51 +0100

    Fix formatting of Assert.fail messages

--- components/tools/OmeroJava/test/integration/ObjectPropertiesTest.java
+++ components/tools/OmeroJava/test/integration/ObjectPropertiesTest.java
@@ -101,7 +101,7 @@ public class ObjectPropertiesTest extends AbstractServerTest {
         ann.setName(omero.rtypes.rstring(name));
         try {
             iUpdate.saveAndReturnObject(ann);
-            Assert.fail("Expected not to be able to insert"
+            Assert.fail("Expected not to be able to insert "
                       + "tag names of size 1MB into the DB");
         } catch (ServerError se) {
             /* expected */
@@ -116,7 +116,7 @@ public class ObjectPropertiesTest extends AbstractServerTest {
         name = createName(2000);
         try {
             iUpdate.saveAndReturnObject(ann);
-            Assert.fail("Expected not to be able to insert"
+            Assert.fail("Expected not to be able to insert "
                       + "tag namespaces of size 1MB into the DB");
         } catch (ServerError se) {
             /* expected */
@@ -303,7 +303,7 @@ public class ObjectPropertiesTest extends AbstractServerTest {
         ns.setName(omero.rtypes.rstring(name));
         try {
             iUpdate.saveAndReturnObject(ns);
-            Assert.fail("Expected not to be able to insert"
+            Assert.fail("Expected not to be able to insert "
                       + "namespace names of size 1MB into the DB");
         } catch (ServerError se) {
             /* expected */
@@ -318,7 +318,7 @@ public class ObjectPropertiesTest extends AbstractServerTest {
         name = createName(2000);
         try {
             iUpdate.saveAndReturnObject(ns);
-            Assert.fail("Expected not to be able to insert"
+            Assert.fail("Expected not to be able to insert "
                       + "namespace display names of size 1MB into the DB");
         } catch (ServerError se) {
             /* expected */
@@ -362,7 +362,7 @@ public class ObjectPropertiesTest extends AbstractServerTest {
         oFile.setHash(omero.rtypes.rstring(hash));
         try {
             iUpdate.saveAndReturnObject(oFile);
-            Assert.fail("Expected not to be able to insert"
+            Assert.fail("Expected not to be able to insert "
                       + "original file hashes of size 1MB into the DB");
         } catch (ServerError se) {
             /* expected */
@@ -537,7 +537,7 @@ public class ObjectPropertiesTest extends AbstractServerTest {
         roi.setName(omero.rtypes.rstring(name));
         try {
             iUpdate.saveAndReturnObject(roi);
-            Assert.fail("Expected not to be able to insert"
+            Assert.fail("Expected not to be able to insert "
                       + "roi names of size 1MB into the DB");
         } catch (ServerError se) {
             /* expected */
