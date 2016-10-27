e344a45ebea604dc8854aa968a7d0c55afb8dcc5
William Moore <w.moore@dundee.ac.uk>
2016-10-27 12:08:12 +0100

    Basic display of images in well bird's eye view

--- components/tools/OmeroWeb/omeroweb/webgateway/static/webgateway/js/ome.spwgridview.js
+++ components/tools/OmeroWeb/omeroweb/webgateway/static/webgateway/js/ome.spwgridview.js
@@ -1,6 +1,66 @@
 
 (function(){
 
+    if (OME === undefined) {
+        window.OME = {};
+    }
+
+    OME.WellBirdsEye = function() {
+
+        var $left_panel_tabs = $("#left_panel_tabs");
+        var $left_panel_bottom = $("#left_panel_bottom");
+        var $well_birds_eye = $("#well_birds_eye");
+
+        var minX,
+            maxX,
+            minY,
+            maxY;
+
+        function showPanel() {
+            var height = 300;
+            $left_panel_tabs.css('bottom', height + 'px');
+            $left_panel_bottom.css('height', height + 'px');
+        }
+
+        function getPos(attr) {
+            return function(ws) {
+                return ws.position[attr] ? ws.position[attr].value : undefined;
+            }
+        }
+        function notUndef(p) {
+            return p !== undefined;
+        }
+
+        return {
+            clear: function() {
+                $well_birds_eye.empty();
+            },
+            addWell: function(data) {
+                showPanel();
+                console.log('addWell', data);
+
+                // first filter for well-samples that have positions
+                data = data.filter(function(ws){ return ws.position !== undefined });
+
+                var xVals = data.map(getPos('x')).filter(notUndef);
+                var yVals = data.map(getPos('y')).filter(notUndef);
+                minX = Math.min.apply(null, xVals);
+                maxX = Math.max.apply(null, xVals);
+                var midX = ((maxX - minX)/2) + minX;
+                minY = Math.min.apply(null, yVals);
+                maxY = Math.max.apply(null, yVals);
+
+                var html = data.map(function(ws){
+                    // check if min===max to avoid zero-division error
+                    var x = maxX === minX ? 0.5 : (ws.position.x.value - minX)/(maxX - minX);
+                    var y = maxY === minY ? 0.5 : (ws.position.y.value - minY)/(maxY - minY);
+                    return '<img style="left: ' + (x * 100) + '%; top: ' + (y * 100) + '%" title="' + ws.name + '" data-imageId="' + ws.id + '" src="' + ws.thumb_url + '" />';
+                }, "");
+                $well_birds_eye.append(html.join(""));
+            }
+        }
+    }
+
     // Used by WellIndexForm in forms.py
     window.changeField = function changeField(field) {
 
@@ -29,9 +89,6 @@
     }
 
     var primaryIndex = -1;
-    if (OME === undefined) {
-        window.OME = {};
-    }
     OME.handleClickSelection = function (event, target, elementsSelector) {
         
         var $clickedImage = target || $(event.target);
