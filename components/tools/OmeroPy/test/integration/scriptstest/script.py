b509aed6f90dcb5f99d3ab135b2d6b2ee778be7a
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-11-04 21:47:28 +0000

    Add tests for analysis scripts

---
+++ components/tools/OmeroPy/test/integration/scriptstest/script.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2016 University of Dundee & Open Microscopy Environment.
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import omero
+from library import ITest
+import uuid
+from omero import ApiUsageException
+from omero.gateway import BlitzGateway
+
+
+class ScriptTest(ITest):
+
+    def upload(self, path):
+        return uploadScript(self.root, path)
+
+
+def uploadScript(client, scriptPath):
+    scriptService = client.sf.getScriptService()
+    _uuid = str(uuid.uuid4())
+
+    file = open(scriptPath)
+    scriptText = file.read()
+    file.close()
+    try:
+        scriptId = scriptService.uploadOfficialScript(
+            "/%s/%s" % (_uuid, scriptPath), scriptText)
+    except ApiUsageException:
+        raise  # The next line will never be run!
+        scriptId = editScript(scriptService, scriptPath)
+    return scriptId
+
+
+def runScript(client, scriptId, argMap, returnKey=None):
+    scriptService = client.sf.getScriptService()
+    proc = scriptService.runScript(scriptId, argMap, None)
+    try:
+        cb = omero.scripts.ProcessCallbackI(client, proc)
+        while not cb.block(1000):  # ms.
+            pass
+        cb.close()
+        results = proc.getResults(0)    # ms
+    finally:
+        proc.close(False)
+
+    if 'stdout' in results:
+        origFile = results['stdout'].getValue()
+        print "Script generated StdOut in file:", origFile.getId().getValue()
+    if 'stderr' in results:
+        origFile = results['stderr'].getValue()
+        # But, we still get stderr from EMAN2 import (duplicate numpy etc.)
+        print "Script generated StdErr in file:", origFile.getId().getValue()
+    if returnKey and returnKey in results:
+        return results[returnKey]
+    else:
+        return results
+
+
+def editScript(scriptService, scriptPath):
+    file = open(scriptPath)
+    scriptText = file.read()
+    file.close()
+    # need the script Original File to edit
+    # scripts = scriptService.getScripts()
+    # if not scriptPath.startswith("/"): scriptPath =  "/" + scriptPath
+    # namedScripts =\
+    #     [s for s in scripts if s.path.val + s.name.val == scriptPath]
+    # script = namedScripts[-1]
+    script = getScript(scriptService, scriptPath)
+    print "Editing script:", scriptPath
+    scriptService.editScript(script, scriptText)
+    return script.id.val
+
+
+def getScript(scriptService, scriptPath):
+
+    scripts = scriptService.getScripts()     # returns list of OriginalFiles
+
+    for s in scripts:
+        print s.id.val, s.path.val + s.name.val
+
+    # make sure path starts with a slash.
+    # ** If you are a Windows client - will need to convert all path separators
+    #    to "/" since server stores /path/to/script.py **
+    if not scriptPath.startswith("/"):
+        scriptPath = "/" + scriptPath
+
+    namedScripts = [
+        s for s in scripts if s.path.val + s.name.val == scriptPath]
+
+    if len(namedScripts) == 0:
+        print "Didn't find any scripts with specified path: %s" % scriptPath
+        return
+
+    if len(namedScripts) > 1:
+        print "Found more than one script with specified path: %s" % scriptPath
+
+    return namedScripts[0]
+
+
+def pointsToString(points):
+    """ Returns legacy format supported by Insight """
+    points = ["%s,%s" % (p[0], p[1]) for p in points]
+    csv = ", ".join(points)
+    return "points[%s] points1[%s] points2[%s]" % (csv, csv, csv)
+
+
+def checkFileAnnotation(client, fileAnnotation, hasFileAnnotation=True,
+                        parentType="Image", isLinked=True):
+    """
+    Check validity of file annotation. If hasFileAnnotation, check the size,
+    name and number of objects linked to the original file.
+    """
+    if hasFileAnnotation:
+        assert fileAnnotation is not None
+        assert fileAnnotation.val._file._size._val > 0
+        assert fileAnnotation.val._file._name._val is not None
+
+        conn = BlitzGateway(client_obj=client)
+        faWrapper = conn.getObject("FileAnnotation", fileAnnotation.val.id.val)
+        nLinks = sum(1 for i in faWrapper.getParentLinks(parentType))
+        if isLinked:
+            assert nLinks == 1
+        else:
+            assert nLinks == 0
+    else:
+        assert fileAnnotation is None
+
---
+++ components/tools/OmeroPy/test/integration/scriptstest/test_analysis_scripts.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2016 University of Dundee & Open Microscopy Environment.
+# All rights reserved. Use is subject to license terms supplied in LICENSE.txt
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
+   Integration test for anaylis scripts.
+"""
+
+import omero
+from omero.rtypes import rdouble, rint, rstring
+import omero.scripts
+from test.integration.scriptstest.script import ScriptTest
+from test.integration.scriptstest.script import runScript, pointsToString
+from test.integration.scriptstest.script import checkFileAnnotation
+
+kymograph_path = "scripts/omero/analysis_scripts/Kymograph.py"
+plot_profile_path = "scripts/omero/analysis_scripts/Plot_Profile.py"
+
+
+class TestAnalysisScripts(ScriptTest):
+
+    def testKymograph(self):
+        scriptId = super(TestAnalysisScripts, self).upload(kymograph_path)
+
+        # root session is root.sf
+        session = self.root.sf
+        client = self.root
+
+        # create a test image
+        sizeT = 3
+        sizeX = 100
+        sizeY = 100
+        image = self.createTestImage(sizeX, sizeY, 1, 2, sizeT)    # x,y,z,c,t
+        image_id = image.getId().getValue()
+        roi = createROI(image_id, 0, sizeX / 2, 0, sizeY / 2, sizeT)
+        session.getUpdateService().saveAndReturnObject(roi)
+        imageIds = []
+        imageIds.append(omero.rtypes.rlong(image_id))
+        argMap = {
+            "Data_Type": omero.rtypes.rstring("Image"),
+            "IDs": omero.rtypes.rlist(imageIds),
+            "Line_Width": omero.rtypes.rint(5)
+        }
+
+        kymograph_img = runScript(client, scriptId, argMap, "First_Image")
+
+        # check the result
+        assert kymograph_img is not None
+        assert kymograph_img.getId().getValue() > 0
+
+    def testPlotProfile(self):
+        scriptId = super(TestAnalysisScripts, self).upload(plot_profile_path)
+        # root session is root.sf
+        session = self.root.sf
+        client = self.root
+
+        # create a test image
+        sizeT = 3
+        sizeX = 100
+        sizeY = 100
+        image = self.createTestImage(sizeX, sizeY, 1, 2, sizeT)    # x,y,z,c,t
+        image_id = image.getId().getValue()
+        roi = createROI(image_id, 0, sizeX / 2, 0, sizeY / 2, sizeT)
+        session.getUpdateService().saveAndReturnObject(roi)
+        imageIds = []
+        imageIds.append(omero.rtypes.rlong(image_id))
+        argMap = {
+            "Data_Type": omero.rtypes.rstring("Image"),
+            "IDs": omero.rtypes.rlist(imageIds),
+            "Line_Width": omero.rtypes.rint(2),
+            "Sum_or_Average": omero.rtypes.rstring("Average")
+        }
+        ann = runScript(client, scriptId, argMap, "Line_Data")
+        checkFileAnnotation(self, ann, False)
+
+
+def createROI(imageId, x1, x2, y1, y2, sizeT):
+    """
+    Create an ROI with lines and polylines.
+    """
+    roi = omero.model.RoiI()
+    roi.setImage(omero.model.ImageI(imageId, False))
+    # create lines and polylines on each timepoint
+    for t in range(sizeT):
+        # lines
+        line = omero.model.LineI()
+        line.x1 = rdouble(x1)
+        line.x2 = rdouble(x2)
+        line.y1 = rdouble(y1)
+        line.y2 = rdouble(y2)
+        line.theZ = rint(0)
+        line.theT = rint(t)
+        roi.addShape(line)
+        # polylines
+        polyline = omero.model.PolylineI()
+        polyline.theZ = rint(0)
+        polyline.theT = rint(t)
+        points = [[10, 20], [50, 50], [75, 60]]
+        polyline.points = rstring(pointsToString(points))
+        roi.addShape(polyline)
+    return roi
