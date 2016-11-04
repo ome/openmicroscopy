8c71a6151801d601983cf70451d6b52736801226
William Moore <w.moore@dundee.ac.uk>
2016-11-04 17:07:51 +0000

    Robot setup populates posX and posY on WellSamples

--- components/tests/ui/robot_setup.sh
+++ components/tests/ui/robot_setup.sh
@@ -19,7 +19,7 @@ CONFIG_FILENAME=${CONFIG_FILENAME:-robot_ice.config}
 IMAGE_NAME=${IMAGE_NAME:-test&acquisitionDate=2012-01-01_00-00-00&sizeZ=3&sizeT=10.fake}
 TINY_IMAGE_NAME=${TINY_IMAGE_NAME:-test&acquisitionDate=2012-01-01_00-00-00.fake}
 MIF_IMAGE_NAME=${MIF_IMAGE_NAME:-test&series=3.fake}
-PLATE_NAME=${PLATE_NAME:-test&plates=1&plateAcqs=1&plateRows=2&plateCols=3&fields=1&screens=0.fake}
+PLATE_NAME=${PLATE_NAME:-test&plates=1&plateAcqs=1&plateRows=2&plateCols=3&fields=5&screens=0.fake}
 BULK_ANNOTATION_CSV=${BULK_ANNOTATION_CSV:-bulk_annotation.csv}
 FILE_ANNOTATION=${FILE_ANNOTATION:-robot_file_annotation.txt}
 
@@ -44,6 +44,32 @@ echo "A2,Treatment,10" >> "$BULK_ANNOTATION_CSV"
 
 # Create file for upload as File Annotation
 echo "Robot test file annotations" > "$FILE_ANNOTATION"
+
+# Create a python script for setting posX and posY on wellsamples
+# Used below after importing a plate
+WELLSCRIPT="wsScript.py"
+echo "import argparse
+import omero
+from omero.gateway import BlitzGateway
+from omero.model.enums import UnitsLength
+parser = argparse.ArgumentParser()
+parser.add_argument('key', help='OMERO session key')
+parser.add_argument('plateId', help='Plate ID to process', type=int)
+args = parser.parse_args()
+conn = BlitzGateway()
+conn.connect(args.key)
+update = conn.getUpdateService()
+plate = conn.getObject('Plate', args.plateId)
+r = UnitsLength.REFERENCEFRAME
+cols = 3
+for well in plate.listChildren():
+    for i, ws in enumerate(well.listChildren()):
+        x = i % cols
+        y = i / cols
+        ws = conn.getQueryService().get('WellSample', ws.id)
+        ws.posY = omero.model.LengthI(y, r)
+        ws.posX = omero.model.LengthI(x, r)
+        update.saveObject(ws)" > "$WELLSCRIPT"
 
 # Create robot setup
 bin/omero login $USER_NAME@$HOSTNAME:$PORT -w $USER_PASSWORD
@@ -88,6 +114,9 @@ bin/omero import $PLATE_NAME --debug ERROR > plate_import.log 2>&1
 plateid=$(sed -n -e 's/^Plate://p' plate_import.log)
 # Use populate_metadata to upload and attach bulk annotation csv
 OMERO_DEV_PLUGINS=1 bin/omero metadata populate Plate:$plateid --file $BULK_ANNOTATION_CSV
+
+# Run script to populate WellSamples with posX and posY values
+python $WELLSCRIPT $key $plateid
 
 # Import Plate and rename for test ?show=image.name-NAME
 bin/omero import $PLATE_NAME --debug ERROR > show_import.log 2>&1
@@ -136,3 +165,4 @@ echo "omero.datasetid=${dataset##*:}" >> "$CONFIG_FILENAME"
 rm *.fake
 rm plate_import.log
 rm show_import.log
+rm $WELLSCRIPT
