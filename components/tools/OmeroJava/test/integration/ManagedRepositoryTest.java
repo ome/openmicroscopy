9eeffbef2a1a5991b894036c9449124e2e3fc744
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2016-11-22 11:16:11 +0000

    test that an admin may import into any group

--- components/tools/OmeroJava/test/integration/ManagedRepositoryTest.java
+++ components/tools/OmeroJava/test/integration/ManagedRepositoryTest.java
@@ -23,6 +23,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -564,6 +565,27 @@ public class ManagedRepositoryTest extends AbstractServerImportTest {
             assertFileExists("Delete failed. File deleted!: ",
                     pathToUsedFile(data2, index));
         }
+    }
+
+    /**
+     * Test that an administrator can import into a group of which they are not a member.
+     * @throws Exception unexpected
+     */
+    @Test
+    public void testAdminImportIntoAnotherGroup() throws Exception {
+        /* prepare as admin to import into another group */
+        final long targetGroup = newUserAndGroup("rw----").groupId;
+        newUserInGroup(iAdmin.lookupGroup(SYSTEM_GROUP), false);
+        client.getImplicitContext().put("omero.group", Long.toString(targetGroup));
+
+        /* create and import a fake image */
+        final File localPath = tempFileManager.createPath(UUID.randomUUID().toString(), null, true);
+        final File localFile = ensureFileExists(localPath, UUID.randomUUID().toString() + ".fake");
+        importFileset(Collections.singletonList(localFile.toString()));
+
+        /* check that the import was into the intended group */
+        final OriginalFile remoteFile = (OriginalFile) iQuery.findByString("OriginalFile", "name", localFile.getName());
+        Assert.assertEquals(remoteFile.getDetails().getGroup().getId().getValue(), targetGroup);
     }
 
     /**
