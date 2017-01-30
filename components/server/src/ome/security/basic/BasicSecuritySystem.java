f288d323c6887514dbe3c9af161db6884984b060
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2017-01-30 10:07:05 +0000

    if user not in group then reload session to be sure

--- components/server/src/ome/security/basic/BasicSecuritySystem.java
+++ components/server/src/ome/security/basic/BasicSecuritySystem.java
@@ -395,9 +395,13 @@ public class BasicSecuritySystem implements SecuritySystem,
             // tickets:2950, 1940, 3529
             if (!isAdmin && !ec.getMemberOfGroupsList().contains(groupId)) {
                 if (!callPerms.isGranted(Role.WORLD, Right.READ)) {
-                    throw new SecurityViolation(String.format(
-                        "User %s is not a member of group %s and cannot login",
+                    /* reload to address problems with PR 4957: user wrongly thought not to be in group */
+                    ec = sessionManager.reload(ec.getCurrentSessionUuid());
+                    if (!ec.getMemberOfGroupsList().contains(groupId)) {
+                        throw new SecurityViolation(String.format(
+                                "User %s is not a member of group %s and cannot login",
                                 ec.getCurrentUserId(), groupId));
+                    }
                 }
             }
 
