63c28140d9c110ac693260fef679762974219ac5
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-02-03 10:33:33 +0000

    Fix test
    Add delta of 0.0

--- components/romio/test/ome/io/nio/utests/PixelDataBitTest.java
+++ components/romio/test/ome/io/nio/utests/PixelDataBitTest.java
@@ -28,42 +28,42 @@ public class PixelDataBitTest
  
     public void testGetBits()
     {
-        Assert.assertEquals(data.getPixelValue(0), 1.0);  // Start byte 1
-        Assert.assertEquals(data.getPixelValue(1), 1.0);
-        Assert.assertEquals(1.0, data.getPixelValue(2), 1.0);
-        Assert.assertEquals(1.0, data.getPixelValue(3), 1.0);
-        Assert.assertEquals(1.0, data.getPixelValue(4), 1.0);
-        Assert.assertEquals(1.0, data.getPixelValue(5), 1.0);
-        Assert.assertEquals(1.0, data.getPixelValue(6), 1.0);
-        Assert.assertEquals(1.0, data.getPixelValue(7), 1.0);
-        Assert.assertEquals(1.0, data.getPixelValue(8), 1.0);  // Start byte 2 
-        Assert.assertEquals(1.0, data.getPixelValue(9), 1.0);
-        Assert.assertEquals(0.0, data.getPixelValue(10), 0.0);
-        Assert.assertEquals(0.0, data.getPixelValue(11), 0.0);
-        Assert.assertEquals(0.0, data.getPixelValue(12), 0.0);
-        Assert.assertEquals(0.0, data.getPixelValue(13), 0.0);
-        Assert.assertEquals(0.0, data.getPixelValue(14), 0.0);
-        Assert.assertEquals(1.0, data.getPixelValue(15), 1.0);
+        Assert.assertEquals(data.getPixelValue(0), 1.0, 0.0);  // Start byte 1
+        Assert.assertEquals(data.getPixelValue(1), 1.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(2), 1.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(3), 1.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(4), 1.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(5), 1.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(6), 1.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(7), 1.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(8), 1.0, 0.0);  // Start byte 2 
+        Assert.assertEquals(data.getPixelValue(9), 1.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(10), 0.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(11), 0.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(12), 0.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(13), 0.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(14), 0.0, 0.0);
+        Assert.assertEquals(data.getPixelValue(15), 1.0, 0.0);
     }
     
     public void testSetAndGetBits()
     {
     	// 0
-        Assert.assertEquals(data.getPixelValue(0), 1.0);
+        Assert.assertEquals(data.getPixelValue(0), 1.0, 0.0);
     	data.setPixelValue(0, 0.0);
-    	Assert.assertEquals(data.getPixelValue(0), 0.0);
+    	Assert.assertEquals(data.getPixelValue(0), 0.0, 0.0);
     	// 7
-    	Assert.assertEquals(data.getPixelValue(7), 1.0);
+    	Assert.assertEquals(data.getPixelValue(7), 1.0, 0.0);
     	data.setPixelValue(7, 0.0);
-    	Assert.assertEquals(data.getPixelValue(7), 0.0);
+    	Assert.assertEquals(data.getPixelValue(7), 0.0, 0.0);
     	// 8
-    	Assert.assertEquals(data.getPixelValue(8), 1.0);
+    	Assert.assertEquals(data.getPixelValue(8), 1.0, 0.0);
     	data.setPixelValue(8, 1.0);
-    	Assert.assertEquals(data.getPixelValue(8), 1.0);
+    	Assert.assertEquals(data.getPixelValue(8), 1.0, 0.0);
     	// 14
-    	Assert.assertEquals(data.getPixelValue(14), 0.0);
+    	Assert.assertEquals(data.getPixelValue(14), 0.0, 0.0);
     	data.setPixelValue(14, 1.0);
-    	Assert.assertEquals(data.getPixelValue(14), 1.0);
+    	Assert.assertEquals(data.getPixelValue(14), 1.0, 0.0);
     }
     
     public void testSize()
