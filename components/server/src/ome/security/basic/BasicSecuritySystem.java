5a0d4d1c30777e5b4c90181c0f6d0ad73aecff99
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2017-02-10 15:20:01 +0000

    instead of reloading session, check groups with admin service

--- components/server/src/ome/security/basic/BasicSecuritySystem.java
+++ components/server/src/ome/security/basic/BasicSecuritySystem.java
@@ -395,9 +395,9 @@ public class BasicSecuritySystem implements SecuritySystem,
             // tickets:2950, 1940, 3529
             if (!isAdmin && !ec.getMemberOfGroupsList().contains(groupId)) {
                 if (!callPerms.isGranted(Role.WORLD, Right.READ)) {
-                    /* reload to address problems with PR 4957: user wrongly thought not to be in group */
-                    ec = sessionManager.reload(ec.getCurrentSessionUuid());
-                    if (!ec.getMemberOfGroupsList().contains(groupId)) {
+                    /* recheck memberships to address problems with PR 4957: user wrongly thought not to be in group */
+                    final Experimenter currentUser = new Experimenter(ec.getCurrentUserId(), false);
+                    if (!sf.getAdminService().getMemberOfGroupIds(currentUser).contains(groupId)) {
                         throw new SecurityViolation(String.format(
                                 "User %s is not a member of group %s and cannot login",
                                 ec.getCurrentUserId(), groupId));
