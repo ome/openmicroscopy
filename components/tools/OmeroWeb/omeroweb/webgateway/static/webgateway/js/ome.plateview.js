4ae09834f173d0b38ad51aa0691160a188d4f344
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2017-02-14 13:48:38 +0000

    load thumbnails in a batches

--- components/tools/OmeroWeb/omeroweb/webgateway/static/webgateway/js/ome.plateview.js
+++ components/tools/OmeroWeb/omeroweb/webgateway/static/webgateway/js/ome.plateview.js
@@ -133,6 +133,7 @@ jQuery._WeblitzPlateview = function (container, options) {
     table.addClass('showWellLabel wellSize' + opts.width);
 
     var imgIds = new Array();
+    var dim = 0;
     for (i=0; i < data.rowlabels.length; i++) {
       tr = $('<tr></tr>').appendTo(table);
       tr.append('<th>'+data.rowlabels[i]+'</th>');
@@ -140,6 +141,7 @@ jQuery._WeblitzPlateview = function (container, options) {
         if (data.grid[i][j] === null) {
         tr.append('<td class="placeholder"><img src="' + spacer_gif_src + '" /></td>');
         } else {
+          dim++
           imgIds.push(data.grid[i][j].id);
           data.grid[i][j]._wellpos = data.rowlabels[i]+data.collabels[j];
           var parentPrefix = '';
@@ -163,14 +165,29 @@ jQuery._WeblitzPlateview = function (container, options) {
       }
     }
 
-    // load thumbnails to the grid
-    var thumbnails_url = opts.baseurl+'/get_thumbnails/?' + $.param( { id: imgIds }, true);
-    var _load_thumbnails = function (result, data) {
-      $.each(data, function(key, value) {
-        $("img#"+parentPrefix+"image-"+key).attr("src", value);
-      });
+    // load thumbnails in a batches
+    var load_thumbnails = function(input, batch) {
+      if (input.length > 0 && batch > 0) {
+        var iids = input.slice(0 , batch)
+        if (iids.length > 0) {
+          var thumbnails_url = opts.baseurl+'/get_thumbnails/';
+          thumbnails_url += '?' + $.param( { id: iids }, true);
+          var _load_thumbnails = function (result, data) {
+            $.each(data, function(key, value) {
+              $("img#"+parentPrefix+"image-"+key).attr("src", value);
+            });
+          }
+          gs_json(thumbnails_url, null, _load_thumbnails);
+          input = input.filter(function(x) {
+              return iids.indexOf(x) < 0;
+          });
+          load_thumbnails(input, batch);
+        }
+      }
     }
-    gs_json(thumbnails_url, null, _load_thumbnails);
+
+    batch = 50; //dim > 100 ? Math.ceil(dim / 5) : 100;
+    load_thumbnails(imgIds, batch);
     _this.self.trigger('_resetLoaded');
   };
 
