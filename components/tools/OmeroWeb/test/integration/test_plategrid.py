7693f464870718f725decd0f28f01d8e3b0df8fe
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2017-02-28 13:17:43 +0000

    fix failing test_get_plate_grid_metadata

--- components/tools/OmeroWeb/test/integration/test_plategrid.py
+++ components/tools/OmeroWeb/test/integration/test_plategrid.py
@@ -294,7 +294,8 @@ class TestPlateGrid(object):
                     assert well_metadata['name'] == img.name.val
                     # by default thumbprefix is not set,
                     # thumbnail url is not restored
-                    assert well_metadata['thumb_url'] == img.id.val
+                    # this is string, see PlateGrid
+                    assert well_metadata['thumb_url'] == str(img.id.val)
 
     def test_instantiation(self, plate_wells, conn):
         """
