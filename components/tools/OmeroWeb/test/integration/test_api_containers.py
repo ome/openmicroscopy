68ba82d11273cc7c3807cc697a58d93153109125
William Moore <w.moore@dundee.ac.uk>
2017-02-28 12:27:58 +0000

    flake8 fix

--- components/tools/OmeroWeb/test/integration/test_api_containers.py
+++ components/tools/OmeroWeb/test/integration/test_api_containers.py
@@ -559,7 +559,7 @@ class TestContainers(IWebTest):
         conn = get_connection(user1)
         user_name = conn.getUser().getName()
         client = self.new_django_client(user_name, user_name)
-        version = settings.API_VERSIONS[-1]
+        version = api_settings.API_VERSIONS[-1]
 
         # Get image...
         project, dataset = project_datasets
