1fa7c9b5c60229579b1b9e5b8c32dbee55c94285
Simon Li <spli@dundee.ac.uk>
2017-04-10 14:48:05 +0100

    Use omeroweb.api.views.build_url for absolute urls

--- components/tools/OmeroWeb/omeroweb/webgateway/views.py
+++ components/tools/OmeroWeb/omeroweb/webgateway/views.py
@@ -36,6 +36,7 @@ from omero_version import build_year
 from marshal import imageMarshal, shapeMarshal, rgb_int2rgba
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.views.generic import View
+from omeroweb.api.views import build_url
 from omeroweb.webadmin.forms import LoginForm
 from omeroweb.decorators import get_client_ip
 from omeroweb.webadmin.webadmin_utils import upgradeCheck
@@ -2282,13 +2283,9 @@ def full_viewer(request, iid, conn=None, **kwargs):
         if opengraph or twitter:
             prefix = kwargs.get(
                 'thumbprefix', 'webgateway.views.render_thumbnail')
-
-            def urlprefix(iid):
-                return reverse(prefix, args=(iid,))
-
-            image_preview = request.build_absolute_uri(urlprefix(iid))
-            page_url = request.build_absolute_uri(reverse(
-                'webgateway.views.full_viewer', args=(iid,)))
+            image_preview = build_url(request, prefix, None, iid)
+            page_url = build_url(
+                request, 'webgateway.views.full_viewer', None, iid)
 
         d = {'blitzcon': conn,
              'image': image,
