2df1cdae5fe472de19b39652c40446ad08b74e65
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2017-06-08 08:57:12 +0100

    add new bulk shapes training script to list of those to run

--- examples/Training/python/__main__.py
+++ examples/Training/python/__main__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Copyright (C) 2015 University of Dundee & Open Microscopy Environment.
+# Copyright (C) 2015-2017 University of Dundee & Open Microscopy Environment.
 #                    All Rights Reserved.
 # Use is subject to license terms supplied in LICENSE.txt
 #
@@ -12,6 +12,7 @@ training_dir = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
         print "Running training suite"
+        execfile(os.path.join(training_dir, 'Bulk_Shapes.py'))
         execfile(os.path.join(training_dir, 'Connect_To_OMERO.py'))
         execfile(os.path.join(training_dir, 'Create_Image.py'))
         execfile(os.path.join(training_dir, 'Delete.py'))
