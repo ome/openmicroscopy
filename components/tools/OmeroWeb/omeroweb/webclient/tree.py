13a5f0875d64ba736fcbac35fca6c655c2b8e605
William Moore <w.moore@dundee.ac.uk>
2017-06-29 15:22:50 +0100

    Use eventContext adminPrivileges 'Chgrp' for 'canChgrp' flag in jsTree

--- components/tools/OmeroWeb/omeroweb/webclient/tree.py
+++ components/tools/OmeroWeb/omeroweb/webclient/tree.py
@@ -59,7 +59,8 @@ def parse_permissions_css(permissions, ownerid, conn):
     permissionsCss = [r for r in restrictions if permissions.get(r)]
     if ownerid == conn.getUserId():
         permissionsCss.append("isOwned")
-    if ownerid == conn.getUserId() or conn.isAdmin():
+    if (ownerid == conn.getUserId() or
+            "Chgrp" in conn.getEventContext().adminPrivileges):
         permissionsCss.append("canChgrp")
     return ' '.join(permissionsCss)
 
