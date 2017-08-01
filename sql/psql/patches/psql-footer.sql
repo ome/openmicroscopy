c899032986b7c1ce72a506c1f67e8d2acca0bb03
Simon Li <spli@dundee.ac.uk>
2017-08-01 11:01:40 +0100

    Replace pg_advisory_{lock,unlock} with pg_advisory_xact_lock

--- sql/psql/patches/psql-footer.sql
+++ sql/psql/patches/psql-footer.sql
@@ -1,6 +1,9 @@
 --
--- Copyright 2006-2016 University of Dundee. All rights reserved.
--- Use is subject to license terms supplied in LICENSE.txt
+-- This is a manual patch for ome_nextval($1, $2)
+-- to prevent deadlocks if nextval(seq) fails in
+-- https://github.com/openmicroscopy/openmicroscopy/blob/v5.3.3/sql/psql/OMERO5.3__0/psql-footer.sql#L795
+-- pg_advisory_xact_lock should automatically release the lock at the end of
+-- the transaction
 --
 
 CREATE OR REPLACE FUNCTION ome_nextval(seq VARCHAR, increment int4) RETURNS INT8 AS '
@@ -14,20 +17,9 @@ BEGIN
           INSERT INTO _lock_ids (id, name) VALUES (Lid, seq);
       END IF;
 
-      BEGIN
-        PERFORM pg_advisory_lock(1, Lid);
-      EXCEPTION
-        WHEN undefined_function THEN
-          RAISE DEBUG ''No function pg_advisory_lock'';
-      END;
+      PERFORM pg_advisory_xact_lock(1, Lid);
       PERFORM nextval(seq) FROM generate_series(1, increment);
       SELECT currval(seq) INTO nv;
-      BEGIN
-        PERFORM pg_advisory_unlock(1, Lid);
-      EXCEPTION
-        WHEN undefined_function THEN
-          RAISE DEBUG ''No function pg_advisory_unlock'';
-      END;
 
       RETURN nv;
 
