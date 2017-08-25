d93e9b96fbcff9c9f97cae93cace815419952da6
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2017-08-25 13:17:46 +0100

    move INDEX creation to correct place and add NULL checks

--- sql/psql/OMERO5.4__0/OMERO5.4DEV__3.sql
+++ sql/psql/OMERO5.4__0/OMERO5.4DEV__3.sql
@@ -59,7 +59,8 @@ INSERT INTO dbpatch (currentVersion, currentPatch, previousVersion, previousPatc
 DROP INDEX originalfile_repo_path_index;
 
 CREATE UNIQUE INDEX originalfile_repo_path_index ON originalfile
-    (repo, regexp_split_to_array('/' || path || name, '/+'));
+    (repo, regexp_split_to_array('/' || path || name, '/+'))
+    WHERE repo IS NOT NULL;
 
 
 --
