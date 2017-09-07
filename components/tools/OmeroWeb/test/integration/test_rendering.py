5ccd05fe405d183a1737df16c5fe953c1293773c
William Moore <w.moore@dundee.ac.uk>
2017-09-07 11:09:14 +0100

    Add 'inverted' check to test_rendering.py

--- components/tools/OmeroWeb/test/integration/test_rendering.py
+++ components/tools/OmeroWeb/test/integration/test_rendering.py
@@ -170,7 +170,7 @@ class TestRendering(IWebTest):
         image = conn.getObject("Image", iid)
         image.resetDefaults()
         image.setColorRenderingModel()
-        image.setReverseIntensity(0, True)
+        image.setChannelInverted(0, True)
         image.saveDefaults()
         image = conn.getObject("Image", iid)
 
@@ -197,13 +197,14 @@ class TestRendering(IWebTest):
 
         # channel info is supposed to match
         expChannels = image.getChannels()
-        revs = [True, False, False]    # expected reverse intensity flags
+        inverted = [True, False, False]    # expected reverse intensity flags
         for i, c in enumerate(channels):
             assert c['active'] == expChannels[i].isActive()
             assert c['start'] == expChannels[i].getWindowStart()
             assert c['end'] == expChannels[i].getWindowEnd()
             assert c['color'] == expChannels[i].getColor().getHtml()
-            assert c['reverseIntensity'] == revs[i]
+            assert c['reverseIntensity'] == inverted[i]
+            assert c['inverted'] == inverted[i]
 
         # id and owner check
         assert rdefs[0].get("id") is not None
