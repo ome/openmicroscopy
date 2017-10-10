c01c9ccdffa67a7badb81b42dd4cac66f3bf53e4
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2017-10-10 09:52:37 +0100

    add the OMERO_PASSWORD environment variable to CLI help text

--- components/tools/OmeroPy/src/omero/cli.py
+++ components/tools/OmeroPy/src/omero/cli.py
@@ -405,6 +405,8 @@ ENV_HELP = """Environment variables:
                     Default: $OMERO_USERDIR/sessions
   OMERO_TMPDIR      Set the base directory containing temporary files.
                     Default: $OMERO_USERDIR/tmp
+  OMERO_PASSWORD    Set the user's password for creating new sessions.
+                    Suppresses interactive prompt. Ignored if -w is used.
 """
 
 SUDO_HELP = """
