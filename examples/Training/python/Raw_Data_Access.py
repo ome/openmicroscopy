f8449c8deecef9f2db18cb33a6e4dd73e6acb609
Dominik Lindner <d.lindner@dundee.ac.uk>
2017-11-09 14:13:24 +0000

    Add get histogram to examples

--- examples/Training/python/Raw_Data_Access.py
+++ examples/Training/python/Raw_Data_Access.py
@@ -78,6 +78,11 @@ planes = pixels.getPlanes(zct_list)
 for i, p in enumerate(planes):
     print "plane zct:", zct_list[i], " min:", p.min(), " max:", p.max()
 
+# Retrieve a histogram
+# ====================
+# Get a 256 bin histogram for channel 0 and plane z=0/t=0:
+hist = pixels.getHistogram(0, 0, 0, 256)
+print hist
 
 # Close connection
 # ================
