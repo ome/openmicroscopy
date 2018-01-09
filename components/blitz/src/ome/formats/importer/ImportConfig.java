c341f696ba74964d5a4017b81b825a2df093ed44
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2018-01-09 10:35:15 +0000

    tidy class header

--- components/blitz/src/ome/formats/importer/ImportConfig.java
+++ components/blitz/src/ome/formats/importer/ImportConfig.java
@@ -1,13 +1,9 @@
 /*
- *   $Id$
- *
  *   Copyright 2009-2016 Glencoe Software, Inc. All rights reserved.
  *   Use is subject to license terms supplied in LICENSE.txt
  */
 
 package ome.formats.importer;
-
-import static omero.rtypes.rstring;
 
 import java.awt.Rectangle;
 import java.io.BufferedReader;
