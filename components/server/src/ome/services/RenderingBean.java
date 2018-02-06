88c544e743ed0d4b6ed642adea185b91a5388968
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-02-06 13:27:38 +0000

    Handle case when no channels are active

--- components/server/src/ome/services/RenderingBean.java
+++ components/server/src/ome/services/RenderingBean.java
@@ -609,6 +609,9 @@ public class RenderingBean implements RenderingEngine, Serializable {
                     projectedSizeC += 1;
                 }
             }
+            if (projectedSizeC == 0) {
+                return new int[0];
+            }
             Pixels projectedPixels = new Pixels();
             projectedPixels.setSizeX(pixelsObj.getSizeX());
             projectedPixels.setSizeY(pixelsObj.getSizeY());
@@ -650,6 +653,9 @@ public class RenderingBean implements RenderingEngine, Serializable {
             }
             int[] buf = renderProjectedAsPackedInt(algorithm, timepoint,
                     stepping, start, end);
+            if (buf.length == 0) {
+                return new byte[0];
+            }
             int sizeX = pixelsObj.getSizeX();
             int sizeY = pixelsObj.getSizeY();
             BufferedImage image = ImageUtil.createBufferedImage(buf, sizeX,
