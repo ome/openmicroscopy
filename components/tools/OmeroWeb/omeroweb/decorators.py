041f857d6c49cde4687f44eeec6c44dab611bb6e
William Moore <w.moore@dundee.ac.uk>
2018-03-28 11:28:47 +0100

    On public user login, clear active_group and user_id from session

--- components/tools/OmeroWeb/omeroweb/decorators.py
+++ components/tools/OmeroWeb/omeroweb/decorators.py
@@ -323,6 +323,10 @@ class login_required(object):
                 self.useragent, username, password, is_public=True,
                 userip=get_client_ip(request))
             request.session['connector'] = connector
+            # Clear any previous context so we don't try to access this
+            del request.session['active_group']
+            del request.session['user_id']
+            request.session.modified = True
             self.set_public_user_connector(connector)
         elif connection is not None:
             is_anonymous = connection.isAnonymous()
