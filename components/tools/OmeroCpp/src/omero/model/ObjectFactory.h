9d8b42b2b1d172b26cedda4eca0a7965f0fb92ca
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2018-04-17 10:24:44 +0100

    fix obvious Copyright spelling typo

--- components/tools/OmeroCpp/src/omero/model/ObjectFactory.h
+++ components/tools/OmeroCpp/src/omero/model/ObjectFactory.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2014 Unversity of Dundee. All rights reserved.
+ *   Copyright 2014 University of Dundee. All rights reserved.
  *   Use is subject to license terms supplied in LICENSE.txt
  *
  */
