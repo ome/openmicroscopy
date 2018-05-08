d30d2cde68c631b4cb7fae401bbb914bb6010e00
Dominik Lindner <d.lindner@dundee.ac.uk>
2018-05-08 15:28:56 +0100

    Also check ZCT when saving/loading ROI

--- components/blitz/src/omero/gateway/model/ShapeData.java
+++ components/blitz/src/omero/gateway/model/ShapeData.java
@@ -1,6 +1,6 @@
 /*
  *------------------------------------------------------------------------------
- * Copyright (C) 2006-2015 University of Dundee. All rights reserved.
+ * Copyright (C) 2006-2018 University of Dundee. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
