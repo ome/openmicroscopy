adf54a2baf56e44d75938406e67287e01386c876
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2018-07-27 14:07:43 +0100

    capitalize "WARNING:" to match other warnings and errors

--- components/tools/OmeroPy/src/omero/plugins/prefs.py
+++ components/tools/OmeroPy/src/omero/plugins/prefs.py
@@ -280,7 +280,7 @@ class PrefsControl(WriteableConfigControl):
 
     @with_config
     def list(self, args, config):
-        self.ctx.err('warning: "config list" is deprecated, '
+        self.ctx.err('WARNING: "config list" is deprecated, '
                      'use "config get" instead')
         args.KEY = []
         self.get(args, config)
