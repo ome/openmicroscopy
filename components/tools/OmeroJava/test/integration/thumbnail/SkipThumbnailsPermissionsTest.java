262c16bd091db61f4954f74ef5add4278b1c793d
Riad Gozim <rgozim1@gmail.com>
2018-10-03 17:21:56 +0100

    Added test for large file import (to trigger pyramid generation)

--- components/tools/OmeroJava/test/integration/thumbnail/SkipThumbnailsPermissionsTest.java
+++ components/tools/OmeroJava/test/integration/thumbnail/SkipThumbnailsPermissionsTest.java
@@ -3,35 +3,67 @@ package integration.thumbnail;
 import integration.AbstractServerImportTest;
 import integration.ModelMockFactory;
 import ome.formats.importer.ImportConfig;
+import omero.ServerError;
 import omero.api.RenderingEnginePrx;
 import omero.api.ThumbnailStorePrx;
 import omero.model.ExperimenterGroup;
 import omero.model.ExperimenterGroupI;
 import omero.model.Pixels;
+import omero.model.StatsInfo;
 import omero.sys.EventContext;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Tests utilising single thumbnail loading APIs by users other than the
  * owner of the image. In each test an image is imported into OMERO without
  * thumbnails (--skip thumbnails).
  */
+@SuppressWarnings("Duplicates")
 public class SkipThumbnailsPermissionsTest extends AbstractServerImportTest {
 
-    private ExperimenterGroup systemGroup;
+    /**
+     * The format tested here.
+     */
+    private static final String FORMAT = "png";
+
+    /* Total wait time will be WAITS * INTERVAL milliseconds */
+    /** Maximum number of intervals to wait for pyramid **/
+    private static final int WAITS = 100;
+
+    /** Wait time in milliseconds **/
+    private static final long INTERVAL = TimeUnit.HOURS.toMillis(1);
+
+    /** The collection of files that have to be deleted. */
+    private List<File> files;
 
     @BeforeMethod
     protected void startup() throws Throwable {
+        files = new ArrayList<>();
         createImporter();
+    }
+
+    @AfterMethod
+    public void cleanup() {
+        for (File file : files) {
+            file.delete();
+        }
+        files.clear();
     }
 
     @Test(dataProvider = "permissions")
@@ -80,8 +112,7 @@ public class SkipThumbnailsPermissionsTest extends AbstractServerImportTest {
     }
 
     @Test(dataProvider = "permissions")
-    public void testGetThumbnailWithoutDefault(String permissions, boolean isAdmin,
-                                               boolean isGroupOwner) throws Throwable {
+    public void testGetThumbnailWithoutDefault(String permissions, boolean isAdmin, boolean isGroupOwner) throws Throwable {
         // Create two users in same group
         EventContext user1 = newUserAndGroup(permissions);
         loginUser(user1);
@@ -100,16 +131,7 @@ public class SkipThumbnailsPermissionsTest extends AbstractServerImportTest {
         svc.close();
 
         // Create new user in group and login as that user
-        EventContext user2 = newUserInGroup(user1, isGroupOwner);
-
-        // If user is an admin, add them to the system group
-        if (isAdmin) {
-            ExperimenterGroup systemGroup = new ExperimenterGroupI(iAdmin.getSecurityRoles().systemGroupId, false);
-            addUsers(systemGroup, Collections.singletonList(user2.userId), false);
-        }
-
-        // Login as user2
-        loginUser(user2);
+        addUserAndLogin(user1, isAdmin, isGroupOwner);
 
         // Try to load image
         svc = factory.createThumbnailStore();
@@ -126,17 +148,43 @@ public class SkipThumbnailsPermissionsTest extends AbstractServerImportTest {
     }
 
     @Test(dataProvider = "permissions")
-    public void testGetThumbnailLarge(String permissions, boolean isAdmin,
-                                 boolean isGroupOwner) throws Throwable {
+    public void testGetThumbnailLarge(String permissions, boolean isAdmin, boolean isGroupOwner) throws Throwable {
+        // Create two users in same group
+        EventContext user1 = newUserAndGroup(permissions);
+        loginUser(user1);
 
+        // Obtain image
+        ImportConfig config = new ImportConfig();
+        config.doThumbnails.set(false); // skip thumbnails
 
+        // Create a png file, with all RGB values FFFFFF
+        Pixels pixels = importLargeFile(config);
 
+        // View image as user 1
+        ThumbnailStorePrx svc = factory.createThumbnailStore();
+        Utils.setThumbnailStoreToPixels(svc, pixels.getId().getValue());
+        Utils.getThumbnailWithoutDefault(svc);
+        svc.close();
+
+        // Create new user in group and login as that user
+        addUserAndLogin(user1, isAdmin, isGroupOwner);
+
+        // Try to load image
+        svc = factory.createThumbnailStore();
+        try {
+            Utils.setThumbnailStoreToPixels(svc, pixels.getId().getValue());
+            Utils.getThumbnailWithoutDefault(svc);
+        } catch (omero.ResourceError e) {
+            // With permission rw----, the image is private to user 1.
+            // Expect this to fail for user 2.
+            if (!permissions.equalsIgnoreCase("rw----")) {
+                throw e;
+            }
+        }
     }
 
     @Test(dataProvider = "permissions")
-    public void testGetThumbnailWithoutDefaultLarge(String permissions, boolean isAdmin,
-                                      boolean isGroupOwner) throws Throwable {
-
+    public void testGetThumbnailWithoutDefaultLarge(String permissions, boolean isAdmin, boolean isGroupOwner) throws Throwable {
 
 
     }
@@ -260,6 +308,24 @@ public class SkipThumbnailsPermissionsTest extends AbstractServerImportTest {
         return testCases.toArray(new Object[testCases.size()][]);
     }
 
+    private EventContext addUser(EventContext user1, boolean isAdmin, boolean isGroupOwner) throws Exception {
+        // Create new user in group and login as that user
+        EventContext newUser = newUserInGroup(user1, isGroupOwner);
+        if (isAdmin) {
+            // If user is an admin, add them to the system group
+            ExperimenterGroup systemGroup = new ExperimenterGroupI(iAdmin.getSecurityRoles().systemGroupId, false);
+            addUsers(systemGroup, Collections.singletonList(newUser.userId), false);
+        }
+        return newUser;
+    }
+
+    private EventContext addUserAndLogin(EventContext user1, boolean isAdmin, boolean isGroupOwner) throws Exception {
+        // Login as new user
+        EventContext newUser = addUser(user1, isAdmin, isGroupOwner);
+        loginUser(newUser);
+        return newUser;
+    }
+
     private Pixels importFile(ImportConfig config) throws Throwable {
         String format = ModelMockFactory.FORMATS[0];
         File file = createImageFile(format);
@@ -267,15 +333,66 @@ public class SkipThumbnailsPermissionsTest extends AbstractServerImportTest {
     }
 
     private Pixels importLargeFile(ImportConfig config) throws Throwable {
-        String format = ModelMockFactory.FORMATS[0];
-        File file = createImageFile(format);
-        return importFile(config, file, format).get(0);
+        BufferedImage bi = new BufferedImage(4096, 4096, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < bi.getWidth(); x++) {
+            for (int y = 0; y < bi.getHeight(); y++) {
+                bi.setRGB(x, y, Integer.valueOf("FFFFFF", 16));
+            }
+        }
+        File f = createImageFileWithBufferedImage(bi, FORMAT);
+        files.add(f);
+        return importAndWaitForPyramid(config, f, FORMAT);
     }
 
-    private File createImageFile(String format) throws Throwable {
-        File f = File.createTempFile("testImportGraphicsImages" + format, "."
-                + format);
-        mmFactory.createImageFile(f, format);
+    private File createImageFile(String extension) throws Throwable {
+        File f = File.createTempFile("testImportGraphicsImages" + extension, "."
+                + extension);
+        mmFactory.createImageFile(f, extension);
         return f;
+    }
+
+    /**
+     * Create an image file from a BufferedImage of the given format.
+     */
+    private File createImageFileWithBufferedImage(BufferedImage bi, String format) throws Exception {
+        File f = File.createTempFile("testImage", "." + format);
+        Iterator writers = ImageIO.getImageWritersByFormatName(format);
+        ImageWriter writer = (ImageWriter) writers.next();
+        ImageOutputStream ios = ImageIO.createImageOutputStream(f);
+        writer.setOutput(ios);
+        writer.write(bi);
+        ios.close();
+        return f;
+    }
+
+    /**
+     * Import an image file of the given format then wait for a pyramid file to
+     * be generated by checking if stats exists.
+     */
+    private Pixels importAndWaitForPyramid(ImportConfig config, File f, String format)
+            throws Exception {
+        List<Pixels> pixels = null;
+        try {
+            pixels = importFile(config, f, format);
+        } catch (Throwable e) {
+            Assert.fail("Cannot import image file: " + f.getAbsolutePath()
+                    + " Reason: " + e.toString());
+        }
+        // Wait for a pyramid to be built (stats will be not null)
+        Pixels p = factory.getPixelsService().retrievePixDescription(
+                pixels.get(0).getId().getValue());
+        StatsInfo stats = p.getChannel(0).getStatsInfo();
+        int waits = 0;
+        while (stats == null && waits < WAITS) {
+            Thread.sleep(INTERVAL);
+            waits++;
+            p = factory.getPixelsService().retrievePixDescription(
+                    pixels.get(0).getId().getValue());
+            stats = p.getChannel(0).getStatsInfo();
+        }
+        if (stats == null) {
+            Assert.fail("No pyramid after " + WAITS * INTERVAL / 1000.0 + " seconds");
+        }
+        return p;
     }
 }
