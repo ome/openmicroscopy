b3d929bde81c6c1063ae1cff299f1554a8145d0a
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-11-09 08:43:10 +0000

    Adjust date since files contain older code

--- components/tools/OmeroJava/test/integration/thumbnail/SkipThumbnailsPermissionsTest.java
+++ components/tools/OmeroJava/test/integration/thumbnail/SkipThumbnailsPermissionsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 University of Dundee & Open Microscopy Environment.
+ * Copyright (C) 2006-2018 University of Dundee & Open Microscopy Environment.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
