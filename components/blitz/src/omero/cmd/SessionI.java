c8a5e40af0ec5d4da369e91d03d9ab571a912360
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2018-11-29 15:47:25 +0000

    Mark submitted graph requests for background processing.

--- components/blitz/src/omero/cmd/SessionI.java
+++ components/blitz/src/omero/cmd/SessionI.java
@@ -147,7 +147,14 @@ public class SessionI implements _SessionOperations {
 
     public void submit_async(AMD_Session_submit __cb, omero.cmd.Request req,
             Ice.Current current) {
-        submit_async(__cb, req, current, null); // null == USER priority
+        final Executor.Priority priority;
+        if (req instanceof GraphQuery) {
+            priority = Executor.Priority.BACKGROUND;
+        } else {
+            /* defaults to Priority.USER */
+            priority = null;
+        }
+        submit_async(__cb, req, current, priority);
     }
 
     public void submit_async(AMD_Session_submit __cb, omero.cmd.Request req,
