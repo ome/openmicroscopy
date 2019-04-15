f832271f5d66dac6ee77167ec838f09a1dbdd87f
Simon Li <spli@dundee.ac.uk>
2019-04-15 16:14:27 +0100

    Add tests for protocol://host Python client hosturl

--- components/tools/OmeroPy/test/unit/test_clients.py
+++ components/tools/OmeroPy/test/unit/test_clients.py
@@ -18,6 +18,7 @@ Module documentation
  */
 """
 
+import pytest
 import Ice
 import logging
 import threading
@@ -137,3 +138,58 @@ class TestKeepAlive(object):
         self.mc.assertResources()
         self.mc.enableKeepAlive(-1)
         self.mc.assertNoResources()
+
+
+class TestHostUrlParsing(object):
+    """
+    Test that protocol://host/url parsing works
+    """
+
+    def setup_method(self, method):
+        self.mc = MockClient()
+
+    def teardown_method(self, method):
+        self.mc.__del__()
+
+    def _get_hosturl(self, values):
+        return {
+            'protocol': values[0],
+            'server': values[1],
+            'port': values[2],
+            'path': values[3],
+        }
+
+    @pytest.mark.parametrize('host,port,pmap,expected', [
+        ('test-host', None, {}, None),
+        ('test-host:12345', None, {}, None),
+        ('wss://test', None, {}, ('wss', 'test', 443, None)),
+        ('wss://test:12345', None, {}, ('wss', 'test', 12345, None)),
+        ('wss://test/sub/path', None, {}, ('wss', 'test', 443, '/sub/path')),
+        ('ws://test', None, {}, ('ws', 'test', 80, None)),
+        ('tcp://test', None, {}, ('tcp', 'test', 4063, None)),
+        ('ssl://test', None, {}, ('ssl', 'test', 4064, None)),
+        (None, None, {'omero.host': 'wss://test'},
+            ('wss', 'test', 443, None)),
+        (None, None, {'omero.host': 'wss://test:12345'},
+            ('wss', 'test', 12345, None)),
+        (None, None, {'omero.host': 'wss://test', 'omero.port': '12345'},
+            ('wss', 'test', 12345, None)),
+    ])
+    def test_check_for_hosturl(self, host, port, pmap, expected):
+        hosturl = self.mc._check_for_hosturl(host, port, pmap)
+        if expected:
+            expected_hosturl = self._get_hosturl(expected)
+        else:
+            expected_hosturl = {}
+        assert expected_hosturl == hosturl
+
+    @pytest.mark.parametrize('values,expected', [
+        (('ssl', 'test', 12345, ''),
+         '--Ice.Default.Router=OMERO.Glacier2/router:ssl -p 12345 -h test'),
+        (('wss', 'test', 12345, '/sub/path'),
+         '--Ice.Default.Router=OMERO.Glacier2/router:wss -p 12345 -h test '
+         '-r /sub/path'),
+    ])
+    def test_get_endpoint_from_hosturl(self, values, expected):
+        hosturl = self._get_hosturl(values)
+        assert expected == self.mc._get_endpoint_from_hosturl(hosturl)
