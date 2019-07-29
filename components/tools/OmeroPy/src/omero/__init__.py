084209b4030e9eff8edc06ed3ac041a8ee216f5a
jmoore <josh@glencoesoftware.com>
2019-07-29 17:57:27 +0200

    CmdError: implement __str__
    Provide a better representation of the CmdError instance
    even without unwrapping the `err` instance.

--- components/tools/OmeroPy/src/omero/__init__.py
+++ components/tools/OmeroPy/src/omero/__init__.py
@@ -85,6 +85,11 @@ class CmdError(ClientError):
         ClientError.__init__(self, *args, **kwargs)
         self.err = err
 
+    def __str__(self):
+        sb = ClientError.__str__(self)
+        sb += "\n"
+        sb += str(self.err)
+        return sb
 
 class UnloadedEntityException(ClientError):
     pass
