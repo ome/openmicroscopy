b8fcdcefbeb0007b9bf82dd2ca6cfb15d7f486fb
jmoore <josh@glencoesoftware.com>
2019-08-21 16:22:00 +0100

    More extensive build cleanup

--- components/tools/OmeroFS/setup.py
+++ components/tools/OmeroFS/setup.py
@@ -14,7 +14,7 @@ for tools in glob.glob("../../../lib/repository/setuptools*.egg"):
     if tools.find(".".join(map(str, sys.version_info[0:2]))) > 0:
         sys.path.insert(0, os.path.abspath(tools))
 
-sys.path.append("../target/python/lib")
+sys.path.append("../target/lib/python")
 from omero_setup import PyTest
 
 from ez_setup import use_setuptools
--- components/tools/OmeroPy/setup.py
+++ components/tools/OmeroPy/setup.py
@@ -35,7 +35,7 @@ import glob
 import sys
 import os
 
-sys.path.append("../target/python/lib")
+sys.path.append("../target/lib/python")
 from omero_setup import PyTest
 
 for tools in glob.glob("../../../lib/repository/setuptools*.egg"):
