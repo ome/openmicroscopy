aaca6c20bf23562c11cb99daf900eb565d67182d
Sebastien Besson <seb.besson@gmail.com>
2019-11-01 14:23:47 +0000

    Remove unused import

--- components/tools/OmeroWeb/test/integration/test_tree_annotations.py
+++ components/tools/OmeroWeb/test/integration/test_tree_annotations.py
@@ -23,7 +23,6 @@ Integration tests for annotations methods in the "tree" module.
 from __future__ import division
 from __future__ import print_function
 
-from past.builtins import cmp
 from builtins import zip
 from future.utils import native_str
 from past.utils import old_div
