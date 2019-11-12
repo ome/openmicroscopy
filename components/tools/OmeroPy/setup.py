b1564e95c2a4affdbccd314802997f164768c60c
Sebastien Besson <seb.besson@gmail.com>
2019-11-12 19:56:05 +0000

    Add mox3 to setup.py

--- components/tools/OmeroPy/setup.py
+++ components/tools/OmeroPy/setup.py
@@ -69,4 +69,4 @@ setup(
         'omero.gateway': ['pilfonts/*'],
         'omero.gateway.scripts': ['imgs/*']},
     cmdclass={'test': PyTest},
-    tests_require=['pytest', 'pytest-xdist'])
+    tests_require=['pytest', 'pytest-xdist', 'mox3'])
