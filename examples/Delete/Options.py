cf2e3a4fd2b0ca6e9eecd0af7bca3550a2a3e8e3
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-11-27 15:22:06 +0000

    py3 fixes

--- examples/Delete/Options.py
+++ examples/Delete/Options.py
@@ -21,23 +21,18 @@ try:
     dc = omero.api.delete.DeleteCommand("/Image", 1, {"/Annotation": "KEEP"})
     deleteHandlePrx = deleteServicePrx .queueDelete([dc])
     cb = omero.callbacks.DeleteCallbackI(c, deleteHandlePrx)
-
     try:
-
         try:
             cb.loop(10, 500)
         except omero.LockTimeout:
-            print "Not finished in 5 seconds. Cancelling..."
+            print("Not finished in 5 seconds. Cancelling...")
             if not deleteHandlePrx.cancel():
-                print "ERROR: Failed to cancel"
-
+                print("ERROR: Failed to cancel")
         reports = deleteHandlePrx.report()
         r = reports[0]  # We only sent one command
-        print "Report:error=%s,warning=%s,deleted=%s" % \
-            (r.error, r.warning, r.actualDeletes)
-
+        print("Report:error=%s,warning=%s,deleted=%s" %
+              (r.error, r.warning, r.actualDeletes))
     finally:
         cb.close()
-
 finally:
     c.closeSession()
