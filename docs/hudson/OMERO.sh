9e668e10c75ad833476d501bd154704917daf93f
Chris Allan <callan@glencoesoftware.com>
2020-03-20 09:56:58 +0000

    When building on CI, log ivy:retrieve more verbosely

--- docs/hudson/OMERO.sh
+++ docs/hudson/OMERO.sh
@@ -12,7 +12,7 @@ set -x
 source docs/hudson/functions.sh
 echo Building $OMERO_BRANCH
 
-./build.py build-dev release-all
+./build.py -Divy.retrieve.log=default build-dev release-all
 if [ -d .git ]
 then
   ./build.py release-src
