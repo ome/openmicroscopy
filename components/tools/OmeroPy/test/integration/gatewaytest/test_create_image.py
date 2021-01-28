9af5ca1936c8afbd77b148125823ef9e54679824
William Moore <w.moore@dundee.ac.uk>
2021-01-28 21:50:04 +0000

    flake8 fixes

--- components/tools/OmeroPy/test/integration/gatewaytest/test_create_image.py
+++ components/tools/OmeroPy/test/integration/gatewaytest/test_create_image.py
@@ -1,6 +1,5 @@
 
 import numpy as np
-from itertools import chain
 import pytest
 
 import omero
@@ -11,25 +10,28 @@ from omero.util.tiles import TileLoopIteration, RPSTileLoop
 
 class TestCreateImage(ITest):
 
-    def get_planes(self, size_x, size_y, size_z, size_c, size_t, dtype, func):
+    def get_planes(self, size_x, size_y, size_z, size_c, size_t,
+                   dtype, func):
         for z in range(size_z):
             for c in range(size_c):
                 for t in range(size_t):
                     yield np.full((size_y, size_x), func(z, c, t), dtype=dtype)
 
-    def get_big_planes(self, size_x, size_y, size_z, size_c, size_t, dtype, func):
+    def get_big_planes(self, size_x, size_y, size_z, size_c, size_t,
+                       dtype, func):
         for t in range(size_t):
             for c in range(size_c):
                 for z in range(size_z):
                     yield np.full((size_y, size_x), func(z, c, t), dtype=dtype)
 
     @pytest.mark.parametrize('size_xy', [[200, 300], [3000, 4500]])
-    @pytest.mark.parametrize('dtype', [np.int8, np.int16, np.uint16, np.int32, np.float32, np.float64])
+    @pytest.mark.parametrize('dtype', [np.int8, np.int16, np.uint16,
+                                       np.int32, np.float32, np.float64])
     def test_create_image(self, size_xy, dtype):
         """
         Tests conn.createImageFromNumpySeq()
         """
-        conn = BlitzGateway(client_obj = self.client)
+        conn = BlitzGateway(client_obj=self.client)
 
         size_x, size_y = size_xy
         size_c = size_t = 2
@@ -39,22 +41,26 @@ class TestCreateImage(ITest):
         big_image = size_x * size_y > maxplanesize[0] * maxplanesize[1]
         if big_image:
             if dtype in [np.float32, np.float64]:
-                # setTile() fails with ROMIO pixel buffer only supports full row writes.
+                # setTile() fails with
+                # "ROMIO pixel buffer only supports full row writes.:
                 return
 
         name = "test_create_image_%s_%s_%s_%s_%s_%s" % (
             size_x, size_y, size_z, size_c, size_t, dtype)
         description = "Description of %s" % name
         dataset = self.make_dataset(name)
+
         def pix_func(z, c, t):
             return (10 * c) + z + t
+
         if big_image:
             # TileLoopIteration needs planes in different order
             plane_gen = self.get_big_planes(size_x, size_y, size_z,
                                             size_c, size_t, dtype, pix_func)
         else:
             plane_gen = self.get_planes(size_x, size_y,
-                                        size_z, size_c, size_t, dtype, pix_func)
+                                        size_z, size_c,
+                                        size_t, dtype, pix_func)
         new_image = conn.createImageFromNumpySeq(
             plane_gen, name, dataset=dataset,
             sizeZ=size_z, sizeC=size_c, sizeT=size_t,
@@ -82,14 +88,15 @@ class TestCreateImage(ITest):
                     tile = primary_pix.getTile(z, c, t, region)
                     assert tile.min() == tile.max()
                     # known failure for int32 - don't know why!
-                    assert tile.min() == pix_func(z, c, t) or (dtype == np.int32 and big_image)
+                    assert (tile.min() == pix_func(z, c, t)
+                            or (dtype == np.int32 and big_image))
 
         ds = conn.getObject("Dataset", dataset.id.val)
         assert len(list(ds.listChildren())) == 1
 
     @pytest.mark.parametrize('size_xy', [[200, 300], [3000, 4500]])
     def test_copy_image(self, size_xy):
-        conn = BlitzGateway(client_obj = self.client)
+        conn = BlitzGateway(client_obj=self.client)
         # size_x = size_y = 256
         size_x, size_y = size_xy
         size_c = size_z = 2
@@ -100,8 +107,10 @@ class TestCreateImage(ITest):
         name = "test_create_image_from_image"
         description = "Description of %s" % name
         dataset = self.make_dataset(name)
+
         def pix_func(z, c, t):
             return (10 * c) + z + t
+
         maxplanesize = conn.getMaxPlaneSize()
         big_image = size_x * size_y > maxplanesize[0] * maxplanesize[1]
         zctList = []
@@ -112,14 +121,15 @@ class TestCreateImage(ITest):
             for t in range(size_t):
                 for c in range(size_c):
                     for z in range(size_z):
-                        zctList.append((z,c,t))
+                        zctList.append((z, c, t))
         else:
             plane_gen = self.get_planes(size_x, size_y,
-                                        size_z, size_c, size_t, dtype, pix_func)
+                                        size_z, size_c,
+                                        size_t, dtype, pix_func)
             for z in range(size_z):
                 for c in range(size_c):
                     for t in range(size_t):
-                        zctList.append((z,c,t))
+                        zctList.append((z, c, t))
 
         original = conn.createImageFromNumpySeq(
             plane_gen, name, dataset=dataset,
@@ -140,14 +150,15 @@ class TestCreateImage(ITest):
         image_name = "new image from ID: %s" % original.id
         description = "Description of %s" % name
         clist = range(original.getSizeC())
+
         def planeGen():
             planes = original.getPrimaryPixels().getPlanes(zctList)
             for p in planes:
                 # Test that this int32 data will be converted to int8
                 p = p.astype(np.int32)
                 new_plane = p
-                print('planeGen...', new_plane.min(), new_plane.max())
                 yield new_plane
+
         new_image = conn.createImageFromNumpySeq(
             planeGen(), image_name, sizeZ=size_z, sizeC=size_c, sizeT=size_t,
             sourceImageId=original.id, channelList=clist,
@@ -169,7 +180,8 @@ class TestCreateImage(ITest):
 
         for index, ch in enumerate(new_image.getChannels()):
             assert ch.getWindowMin() == new_pix.getPlane(0, index, 0).min()
-            assert ch.getWindowMax() == new_pix.getPlane(size_z - 1, index, size_t - 1).max()
+            assert ch.getWindowMax() == new_pix.getPlane(size_z - 1, index,
+                                                         size_t - 1).max()
 
         ds = conn.getObject("Dataset", dataset.id.val)
         assert len(list(ds.listChildren())) == 2
@@ -179,7 +191,7 @@ class TestCreateBigImage(ITest):
 
     def test_set_tiles(self):
 
-        conn = BlitzGateway(client_obj = self.client)
+        conn = BlitzGateway(client_obj=self.client)
 
         pixels_service = conn.getPixelsService()
         query_service = conn.getQueryService()
@@ -215,12 +227,15 @@ class TestCreateBigImage(ITest):
             for t in range(0, size_t):
                 for c in range(0, size_c):
                     for z in range(0, size_z):
-                        plane = np.full((size_y, size_x), get_pixel_value(z, c, t), dtype=np.int16)
-                        print('tile_gen z %s, c %s, t %s value %s' % (z, c, t, plane.min()))
+                        plane = np.full((size_y, size_x),
+                                        get_pixel_value(z, c, t),
+                                        dtype=np.int16)
                         for tile_offset_y in range(
-                                0, ((size_y + tile_height - 1) // tile_height)):
+                                0, ((size_y + tile_height - 1) //
+                                    tile_height)):
                             for tile_offset_x in range(
-                                    0, ((size_x + tile_width - 1) // tile_width)):
+                                    0, ((size_x + tile_width - 1) //
+                                        tile_width)):
                                 x = tile_offset_x * tile_width
                                 y = tile_offset_y * tile_height
                                 x2 = min(x + tile_width, size_x)
@@ -244,8 +259,9 @@ class TestCreateBigImage(ITest):
         loop.forEachTile(tile_width, tile_height, Iteration())
 
         for the_c in range(size_c):
-            pixels_service.setChannelGlobalMinMax(pid, the_c, float(0),
-                                                float(255), conn.SERVICE_OPTS)
+            pixels_service.setChannelGlobalMinMax(pid, the_c,
+                                                  float(0), float(255),
+                                                  conn.SERVICE_OPTS)
 
         primary_pix = new_image.getPrimaryPixels()
         region = (0, 0, 1000, 1000)
