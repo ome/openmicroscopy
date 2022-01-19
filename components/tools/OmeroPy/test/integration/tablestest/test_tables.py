ebef2d1d1a33c963ce6dc186be4647a4daf2177d
Muhanad Zahra <Muhanad.Zahra@outlook.com>
2022-01-19 10:34:29 +0000

    Undid changes in the previous commits. Moved changes to a new test_tables.py. New script simply tests the integrity of created table with all header types. Table is no longer uploaded to server.

--- components/tools/OmeroPy/test/integration/tablestest/test_tables.py
+++ components/tools/OmeroPy/test/integration/tablestest/test_tables.py
@@ -19,22 +19,17 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 """
-Check backwards compatibility of the tables service
+Creates a table and tests it's integrity
 """
 
-import os.path
-import bz2
-import tempfile
 import omero
 import omero.clients
 import omero.grid
 from omero.testlib import ITest
-import pytest
 
 from omero import columns
 from omero.rtypes import (
     RFloatI,
-    # RIntI,
     RLongI,
     RStringI,
     rfloat,
@@ -44,26 +39,7 @@ from omero.rtypes import (
 )
 
 
-class BackwardsCompatibilityBase(ITest):
-
-    def uploadHdf5(self, file):
-        """
-        Decompress the BZ2-compressed HDF5 test file and upload to server.
-        file should be relative to the directory containing this file.
-        """
-        dir = os.path.dirname(os.path.realpath(__file__))
-        file = os.path.join(dir, file)
-
-        tmpf = tempfile.NamedTemporaryFile(delete=False)
-        bzf = bz2.BZ2File(file)
-        tmpf.write(bzf.read())
-        bzf.close()
-        tmpf.close()
-
-        ofile = self.client.upload(
-            tmpf.name, name=file, type='application/x-hdf')
-        print("Uploaded OriginalFile:", ofile.getId().val)
-        return ofile
+class TableIntegrityBase(ITest):
 
     def createMaskCol(self):
         mask = columns.MaskColumnI('mask', 'desc', None)
@@ -106,243 +82,21 @@ class BackwardsCompatibilityBase(ITest):
             assert x[i] == y[i]
 
 
-class TestBackwardsCompatibility_4_4_5(BackwardsCompatibilityBase):
+class TestTableIntegrity(TableIntegrityBase):
 
-    def testCreateAllColumns_4_4_5(self):
+    def testCreateAllColumnsAndMetadata(self):
         """
-        Call this method to create the reference HDF5 table under a 4.4.5
-        or older server. The OriginalFile ID of the table will be printed,
-        and can be used to find the file under ${omero.data.dir}/Files/.
-
-        To run manually goto ``components/tools/OmeroPy``, and run:
-        ``py.test test/integration/tablestest/test_backwards_compatibility.py\
-        -s -k testCreateAllColumns_4_4_5``
-        """
-        grid = self.client.sf.sharedResources()
-        repoMap = grid.repositories()
-        repoObj = repoMap.descriptions[0]
-        table = grid.newTable(repoObj.id.val, "/test")
-        assert table
-
-        fcol = columns.FileColumnI('filecol', 'file col')
-        fcol.values = [10, 20]
-        icol = columns.ImageColumnI('imagecol', 'image col')
-        icol.values = [30, 40]
-        rcol = columns.RoiColumnI('roicol', 'roi col')
-        rcol.values = [50, 60]
-        wcol = columns.WellColumnI('wellcol', 'well col')
-        wcol.values = [70, 80]
-        pcol = columns.PlateColumnI('platecol', 'plate col')
-        pcol.values = [90, 100]
-
-        bcol = columns.BoolColumnI('boolcol', 'bool col')
-        bcol.values = [True, False]
-        dcol = columns.DoubleColumnI('doublecol', 'double col')
-        dcol.values = [0.25, 0.5]
-        lcol = columns.LongColumnI('longcol', 'long col')
-        lcol.values = [-1, -2]
-
-        scol = columns.StringColumnI('stringcol', 'string col', 3)
-        scol.values = ["abc", "de"]
-
-        # larr = columns.LongArrayColumnI('longarr', 'longarr col', 2)
-        # larr.values = [[-2, -1], [1, 2]]
-        # farr = columns.FloatArrayColumnI('floatarr', 'floatarr col', 2)
-        # farr.values = [[-0.25, -0.5], [0.125, 0.0625]]
-        # darr = columns.DoubleArrayColumnI('doublearr', 'doublearr col', 2)
-        # darr.values = [[-0.25, -0.5], [0.125, 0.0625]]
-
-        mask = self.createMaskCol()
-
-        cols = [fcol, icol, rcol, wcol, pcol,
-                bcol, dcol, lcol, scol, mask]
-        # larr, farr, darr]
-
-        table.initialize(cols)
-        table.addData(cols)
-        data = table.readCoordinates([0, 1])
-
-        testf = data.columns[0].values
-        assert 10 == testf[0]
-        assert 20 == testf[1]
-        testi = data.columns[1].values
-        assert 30 == testi[0]
-        assert 40 == testi[1]
-        testr = data.columns[2].values
-        assert 50 == testr[0]
-        assert 60 == testr[1]
-        testw = data.columns[3].values
-        assert 70 == testw[0]
-        assert 80 == testw[1]
-        testp = data.columns[4].values
-        assert 90 == testp[0]
-        assert 100 == testp[1]
-
-        testb = data.columns[5].values
-        assert testb[0]
-        assert not testb[1]
-        testd = data.columns[6].values
-        assert 0.25 == testd[0]
-        assert 0.5 == testd[1]
-        testl = data.columns[7].values
-        assert -1 == testl[0]
-        assert -2 == testl[1]
-
-        tests = data.columns[8].values
-        assert "abc" == tests[0]
-        assert "de" == tests[1]
-
-        testm = data.columns[9]
-        self.checkMaskCol(testm)
-
-        # testla = data.columns[10].values
-        # assert [-2, -1] == testla[0]
-        # assert [1, 2] == testla[1]
-        # testda = data.columns[11].values
-        # assert [-0.25, -0.5] == testda[0]
-        # assert [0.125, 0.0625] == testda[1]
-
-        ofile = table.getOriginalFile()
-        print("Created OriginalFile:", ofile.getId().val)
-
-    def testAllColumns_4_4_5(self):
-        """
-        Check whether a table created under 4.4.5 or older is still usable
-        with a newer server
-        """
-        ofile = self.uploadHdf5("service-reference-dev_4_4_5.h5.bz2")
-
-        grid = self.client.sf.sharedResources()
-        table = grid.openTable(ofile)
-        assert table
-
-        expectedTypes = [
-            omero.grid.FileColumn,
-            omero.grid.ImageColumn,
-            omero.grid.RoiColumn,
-            omero.grid.WellColumn,
-            omero.grid.PlateColumn,
-            omero.grid.BoolColumn,
-            omero.grid.DoubleColumn,
-            omero.grid.LongColumn,
-            omero.grid.StringColumn,
-            omero.grid.MaskColumn
-            ]
-        # omero.grid.FloatArrayColumn,
-        # omero.grid.DoubleArrayColumn,
-        # omero.grid.LongArrayColumn,
-
-        expectedNames = [
-            'filecol',
-            'imagecol',
-            'roicol',
-            'wellcol',
-            'platecol',
-            'boolcol',
-            'doublecol',
-            'longcol',
-            'stringcol',
-            'mask'
-            ]
-        # 'longarr'
-        # 'floatarr'
-        # 'doublearr'
-
-        headers = table.getHeaders()
-        assert [type(x) for x in headers] == expectedTypes
-        assert [x.name for x in headers] == expectedNames
-
-        assert table.getNumberOfRows() == 2
-
-        data = table.readCoordinates([0, 1])
-
-        testf = data.columns[0].values
-        assert 10 == testf[0]
-        assert 20 == testf[1]
-        testi = data.columns[1].values
-        assert 30 == testi[0]
-        assert 40 == testi[1]
-        testr = data.columns[2].values
-        assert 50 == testr[0]
-        assert 60 == testr[1]
-        testw = data.columns[3].values
-        assert 70 == testw[0]
-        assert 80 == testw[1]
-        testp = data.columns[4].values
-        assert 90 == testp[0]
-        assert 100 == testp[1]
-
-        testb = data.columns[5].values
-        assert testb[0]
-        assert not testb[1]
-        testd = data.columns[6].values
-        assert 0.25 == testd[0]
-        assert 0.5 == testd[1]
-        testl = data.columns[7].values
-        assert -1 == testl[0]
-        assert -2 == testl[1]
-
-        tests = data.columns[8].values
-        assert "abc" == tests[0]
-        assert "de" == tests[1]
-
-        testm = data.columns[9]
-        self.checkMaskCol(testm)
-
-        # testla = data.columns[10].values
-        # assert [-2, -1] == testla[0]
-        # assert [1, 2] == testla[1]
-        # testda = data.columns[11].values
-        # assert [-0.25, -0.5] == testda[0]
-        # assert [0.125, 0.0625] == testda[1]
-
-        # Now try an update
-        updatel = omero.grid.LongColumn('longcol', '', [12345])
-        updateData = omero.grid.Data(rowNumbers=[1], columns=[updatel])
-        table.update(updateData)
-
-        assert table.getNumberOfRows() == 2
-        data2 = table.readCoordinates([0, 1])
-
-        for n in [0, 1, 2, 3, 4, 5, 6, 8]:
-            assert data.columns[n].values == data2.columns[n].values
-        self.checkMaskCol(data2.columns[9])
-
-        testl2 = data2.columns[7].values
-        assert -1 == testl2[0]
-        assert 12345 == testl2[1]
-
-    def testMetadataException(self):
-        """
-        Check whether metadata set methods are blocked on a v1 (pre-5.1) table
-        """
-        ofile = self.uploadHdf5("service-reference-dev_4_4_5.h5.bz2")
-
-        grid = self.client.sf.sharedResources()
-        table = grid.openTable(ofile)
-
-        expected = 'Tables metadata is only supported for OMERO.tables >= 2'
-        with pytest.raises(omero.ApiUsageException) as exc:
-            table.setMetadata('a', rint(1))
-        with pytest.raises(omero.ApiUsageException) as exc:
-            table.setAllMetadata({'a': rint(1)})
-        assert exc.value.message == expected
-
-
-class TestBackwardsCompatibility_5_3_4(BackwardsCompatibilityBase):
-
-    def testCreateAllColumnsAndMetadata_5_3_4(self):
-        """
-        Call this method to create the reference HDF5 table under a 5.3.4
-        Python 2.7 server. The OriginalFile ID of the table will be printed,
+        Call this method to create the reference HDF5 table.
+        The OriginalFile ID of the table will be printed,
         and can be used to find the file under ${omero.data.dir}/Files/.
         Alternatively download it using
         ``omero download OriginalFile:FileID output.h5``
 
         To run manually goto ``components/tools/OmeroPy``, and run:
-        ``pytest test/integration/tablestest/test_backwards_compatibility.py\
-        -s -k testCreateAllColumnsAndMetadata_5_3_4``
+        ``pytest test/integration/tablestest/test_tables.py\
+        -s -k testCreateAllColumnsAndMetadata``
         """
+
         grid = self.client.sf.sharedResources()
         repoMap = grid.repositories()
         repoObj = repoMap.descriptions[0]
@@ -386,13 +140,14 @@ class TestBackwardsCompatibility_5_3_4(BackwardsCompatibilityBase):
         darr = columns.DoubleArrayColumnI('doublearr', 'doublearr col', 2)
         darr.values = [[-0.25, -0.5], [0.125, 0.0625]]
 
-        # DatasetColumn is broken!
+        dscol = columns.DatasetColumnI('datasetcol', 'dataset col')
+        dscol.values = [110, 120]
 
         mask = self.createMaskCol()
 
         cols = [fcol, icol, rcol, wcol, pcol,
                 bcol, dcol, lcol, scol, mask,
-                larr, farr, darr]
+                larr, farr, darr, dscol]
 
         table.initialize(cols)
         table.setAllMetadata(metadata)
@@ -443,30 +198,27 @@ class TestBackwardsCompatibility_5_3_4(BackwardsCompatibilityBase):
         assert [-0.25, -0.5] == testda[0]
         assert [0.125, 0.0625] == testda[1]
 
+        testds = data.columns[13].values
+        assert 110 == testds[0]
+        assert 120 == testds[1]
+
         ofile = table.getOriginalFile()
         print("Created OriginalFile:", ofile.getId().val)
 
-        table.close()
+        return table
 
-    def testAllColumnsAndMetadata_5_3_4(self):
+    def testAllColumnsAndMetadata(self):
         """
-        Check whether a table created under 5.3.4-5.5.1 prior to the Python 3
-        work is still usable with a newer server
+        Check the integrity of a created table created.
         """
-        ofile = self.uploadHdf5("service-reference-dev_5_3_4.h5.bz2")
 
-        grid = self.client.sf.sharedResources()
-        table = grid.openTable(ofile)
+        table = self.testCreateAllColumnsAndMetadata()
         assert table
 
         metadata = table.getAllMetadata()
         assert metadata['__version'] == rstring('2')
         assert isinstance(metadata['double'], RFloatI)
         assert abs(metadata['double'].val - 0.1) < 1e-8
-        # WARNING: On the OMERO-server 5.3.4 Docker image this returns an
-        # RLongI not an RIntI! Maybe pytables already made the decision to
-        # treat int and longs the same?
-        # assert isinstance(metadata['int'], RIntI)
         assert isinstance(metadata['int'], RLongI)
         assert metadata['int'].val == 1
         assert isinstance(metadata['long'], RLongI)
@@ -488,6 +240,7 @@ class TestBackwardsCompatibility_5_3_4(BackwardsCompatibilityBase):
             omero.grid.LongArrayColumn,
             omero.grid.FloatArrayColumn,
             omero.grid.DoubleArrayColumn,
+            omero.grid.DatasetColumn
         ]
 
         expectedNames = [
@@ -504,6 +257,7 @@ class TestBackwardsCompatibility_5_3_4(BackwardsCompatibilityBase):
             'longarr',
             'floatarr',
             'doublearr',
+            'datasetcol',
         ]
 
         headers = table.getHeaders()
@@ -557,6 +311,10 @@ class TestBackwardsCompatibility_5_3_4(BackwardsCompatibilityBase):
         assert [-0.25, -0.5] == testda[0]
         assert [0.125, 0.0625] == testda[1]
 
+        testds = data.columns[13].values
+        assert 110 == testds[0]
+        assert 120 == testds[1]
+
         # Now try an update
         updatel = omero.grid.LongColumn('longcol', '', [12345])
         updateData = omero.grid.Data(rowNumbers=[1], columns=[updatel])
@@ -565,7 +323,7 @@ class TestBackwardsCompatibility_5_3_4(BackwardsCompatibilityBase):
         assert table.getNumberOfRows() == 2
         data2 = table.readCoordinates([0, 1])
 
-        for n in [0, 1, 2, 3, 4, 5, 6, 8, 10, 11, 12]:
+        for n in [0, 1, 2, 3, 4, 5, 6, 8, 10, 11, 12, 13]:
             assert data.columns[n].values == data2.columns[n].values
         self.checkMaskCol(data2.columns[9])
 
