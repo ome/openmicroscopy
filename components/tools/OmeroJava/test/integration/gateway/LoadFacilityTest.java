35fe004c377ceac3bde4b8515df2d6240ff0095a
Dominik Lindner <d.lindner@dundee.ac.uk>
2023-09-01 12:42:26 +0100

    Add more tests

--- components/tools/OmeroJava/test/integration/gateway/LoadFacilityTest.java
+++ components/tools/OmeroJava/test/integration/gateway/LoadFacilityTest.java
@@ -41,6 +41,8 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -75,14 +77,32 @@ public class LoadFacilityTest extends GatewayTest {
     }
 
     @Test
+    public void testGetDatasetsForPlate() throws DSOutOfServiceException, DSAccessException {
+        DatasetData obj = this.loadFacility.getDatasets(this.ctx, this.proj.getId()).iterator().next();
+        Assert.assertEquals(obj.getName(), this.ds.getName());
+    }
+
+    @Test
     public void testGetImage() throws DSOutOfServiceException, DSAccessException {
         ImageData obj = this.loadFacility.getImage(this.ctx, this.img.getId());
         Assert.assertEquals(obj.getName(), this.img.getName());
     }
 
     @Test
+    public void testGetImagesForDataset() throws DSOutOfServiceException, DSAccessException {
+        ImageData obj = this.loadFacility.getImages(this.ctx, this.ds.getId()).iterator().next();
+        Assert.assertEquals(obj.getName(), this.img.getName());
+    }
+
+    @Test
     public void testGetPlate() throws DSOutOfServiceException, DSAccessException {
         PlateData obj = this.loadFacility.getPlate(this.ctx, this.plate.getId());
+        Assert.assertEquals(obj.getName(), this.plate.getName());
+    }
+
+    @Test
+    public void testGetPlatesForScreen() throws DSOutOfServiceException, DSAccessException {
+        PlateData obj = this.loadFacility.getPlates(this.ctx, this.screen.getId()).iterator().next();
         Assert.assertEquals(obj.getName(), this.plate.getName());
     }
 
@@ -107,19 +127,34 @@ public class LoadFacilityTest extends GatewayTest {
         Assert.assertEquals(obj.getWellSamples().get(0).getImage().getId(),
                 test.getWellSamples().get(0).getImage().getId());
     }
+
+    @Test
+    public void testGetWellsForPlate() throws DSOutOfServiceException, DSAccessException {
+        Set<String> test = new HashSet<>();
+        for (WellData w : this.plate.getWells()) {
+            String key = w.getColumn()+"|"+w.getRow()+"|"+w.getId();
+            test.add(key);
+        }
+        for (WellData w : this.loadFacility.getWells(this.ctx, this.plate.getId())) {
+            String key = w.getColumn()+"|"+w.getRow()+"|"+w.getId();
+            Assert.assertTrue(test.contains(key));
+            test.remove(key);
+        }
+        Assert.assertTrue(test.isEmpty());
+    }
     
     private void initData() throws Exception {
         this.group = createGroup();
         this.user = createExperimenter(group);
         this.ctx = new SecurityContext(group.getId());
 
-        this.img = createImage(ctx, null);
-        
         this.proj = createProject(ctx);
-        this.ds = createDataset(ctx, null);
+        this.ds = createDataset(ctx, proj);
+        this.img = createImage(ctx, ds);
+
         this.screen = createScreen(ctx);
         this.plate = createPlateWithWells(ctx, screen);
-        
+        System.out.println("ID: "+this.plate.getId());
         this.wellIds = new ArrayList<Long>();
         Plate p = this.plate.asPlate();
         for (Well w : p.copyWells()) {
