5d1f4d4e99a10b80a15e4dc6315b1d1395a644c0
Sébastien Besson <sbesson@glencoesoftware.com>
2024-01-23 07:00:29 +0000

    Fix exec invocation for training scripts

--- examples/Training/python/__main__.py
+++ examples/Training/python/__main__.py
@@ -12,26 +12,33 @@ training_dir = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
     print("Running training suite")
-    exec(os.path.join(training_dir, 'Bulk_Shapes.py'))
-    exec(os.path.join(training_dir, 'Connect_To_OMERO.py'))
-    exec(os.path.join(training_dir, 'Create_Image.py'))
-    exec(os.path.join(training_dir, 'Delete.py'))
-    exec(os.path.join(training_dir, 'Filesets.py'))
-    exec(os.path.join(training_dir, 'Groups_Permissions.py'))
-    exec(os.path.join(training_dir, 'Metadata.py'))
-    exec(os.path.join(training_dir, 'Raw_Data_Access.py'))
-    exec(os.path.join(training_dir, 'Read_Data.py'))
-    exec(os.path.join(training_dir, 'Render_Images.py'))
-    exec(os.path.join(training_dir, 'ROIs.py'))
-    exec(os.path.join(training_dir, 'Tables.py'))
-    exec(os.path.join(training_dir, 'Write_Data.py'))
-    exec(os.path.join(training_dir,
-         'Advanced/Create_Image_advanced.py'))
-    exec(os.path.join(training_dir, 'Advanced/Raw_Data_advanced.py'))
-    exec(os.path.join(training_dir, 'Advanced/Read_Data_advanced.py'))
-    exec(os.path.join(training_dir, 'Advanced/Write_data_advanced.py'))
-    exec(os.path.join(training_dir, 'Task_Scripts/Raw_Data2.py'))
-    exec(os.path.join(training_dir, 'Task_Scripts/Raw_Data_Task.py'))
-    exec(os.path.join(training_dir, 'Task_Scripts/Write_Data_4.py'))
-    exec(os.path.join(training_dir, 'Task_Scripts/Write_Data_3.py'))
-    exec(os.path.join(training_dir, 'Json_Api/Login.py'))
+    exec(open(os.path.join(training_dir, 'Bulk_Shapes.py')).read())
+    exec(open(os.path.join(training_dir, 'Connect_To_OMERO.py')).read())
+    exec(open(os.path.join(training_dir, 'Create_Image.py')).read())
+    exec(open(os.path.join(training_dir, 'Delete.py')).read())
+    exec(open(os.path.join(training_dir, 'Filesets.py')).read())
+    exec(open(os.path.join(training_dir, 'Groups_Permissions.py')).read())
+    exec(open(os.path.join(training_dir, 'Metadata.py')).read())
+    exec(open(os.path.join(training_dir, 'Raw_Data_Access.py')).read())
+    exec(open(os.path.join(training_dir, 'Read_Data.py')).read())
+    exec(open(os.path.join(training_dir, 'Render_Images.py')).read())
+    exec(open(os.path.join(training_dir, 'ROIs.py')).read())
+    exec(open(os.path.join(training_dir, 'Tables.py')).read())
+    exec(open(os.path.join(training_dir, 'Write_Data.py')).read())
+    exec(open(os.path.join(training_dir,
+         'Advanced/Create_Image_advanced.py')).read())
+    exec(open(os.path.join(training_dir,
+         'Advanced/Raw_Data_advanced.py')).read())
+    exec(open(os.path.join(training_dir,
+         'Advanced/Read_Data_advanced.py')).read())
+    exec(open(os.path.join(training_dir,
+         'Advanced/Write_data_advanced.py')).read())
+    exec(open(os.path.join(training_dir,
+         'Task_Scripts/Raw_Data2.py')).read())
+    exec(open(os.path.join(training_dir,
+         'Task_Scripts/Raw_Data_Task.py')).read())
+    exec(open(os.path.join(training_dir,
+         'Task_Scripts/Write_Data_4.py')).read())
+    exec(open(os.path.join(training_dir,
+         'Task_Scripts/Write_Data_3.py')).read())
+    exec(open(os.path.join(training_dir, 'Json_Api/Login.py')).read())
